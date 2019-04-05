b361d2f3c1db9c5c0fb8117dd3006ad8026164dd
Tiviacz1337 <prusek.kamil@o2.pl>
2019-04-05 21:30:42 +0200

    Changed bull armor's description
    closes #31

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/armor/ItemArmorBull.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/armor/ItemArmorBull.java
@@ -1,9 +1,17 @@
 package com.teamcqr.chocolatequestrepoured.objects.armor;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import org.lwjgl.input.Keyboard;
+
 import com.google.common.collect.Multimap;
 import com.teamcqr.chocolatequestrepoured.init.ModItems;
 import com.teamcqr.chocolatequestrepoured.init.base.ArmorBase;
 
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,7 +19,10 @@ import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemArmorBull extends ArmorBase
 {
@@ -34,6 +45,20 @@ public class ItemArmorBull extends ArmorBase
 			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), this.strength);
 		}
 		return multimap;
+    }
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT))
+		{
+			tooltip.add(TextFormatting.BLUE + I18n.format("description.bull_armor.name"));
+		}
+		else
+		{
+			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
+		}
     }
 	
 	@Override
