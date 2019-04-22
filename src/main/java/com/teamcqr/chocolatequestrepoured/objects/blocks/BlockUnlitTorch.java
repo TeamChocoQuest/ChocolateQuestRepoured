7e4538f4981361bfa002bc2421897e30916fd732
Tiviacz1337 <prusek.kamil@o2.pl>
2019-04-22 19:06:10 +0200

    Imports cleanup

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/blocks/BlockUnlitTorch.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/blocks/BlockUnlitTorch.java
@@ -1,7 +1,5 @@
 package com.teamcqr.chocolatequestrepoured.objects.blocks;
 
-import java.util.Random;
-
 import javax.annotation.Nullable;
 
 import com.google.common.base.Predicate;
@@ -15,7 +13,6 @@ import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -25,7 +22,6 @@ import net.minecraft.item.Item;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.AxisAlignedBB;
