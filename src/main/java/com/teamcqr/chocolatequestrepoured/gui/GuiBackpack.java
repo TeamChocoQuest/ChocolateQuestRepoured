ef589205afadf0aa6fac7c76372ea966b862b9e9
DerToaster <seiler.oli@web.de>
2019-05-01 12:32:24 +0200

    Organized Imports

--- src/main/java/com/teamcqr/chocolatequestrepoured/gui/GuiBackpack.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/gui/GuiBackpack.java
@@ -5,7 +5,6 @@ import com.teamcqr.chocolatequestrepoured.gui.container.ContainerBackpack;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.util.ResourceLocation;
 
