d4cc9a67f85bf4ecf010c0c285489ba871f33f0c
Tiviacz1337 <prusek.kamil@o2.pl>
2019-05-04 03:27:02 +0200

    Added thunder staff

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/staves/ItemStaffThunder.java
@@ -0,0 +1,100 @@
+package com.teamcqr.chocolatequestrepoured.objects.items.staves;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import org.lwjgl.input.Keyboard;
+
+import com.teamcqr.chocolatequestrepoured.init.ModBlocks;
+import com.teamcqr.chocolatequestrepoured.objects.base.ItemBase;
+import com.teamcqr.chocolatequestrepoured.objects.blocks.BlockUnlitTorch;
+
+import net.minecraft.block.BlockTorch;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemStaffThunder extends ItemBase
+{
+	public ItemStaffThunder(String name) 
+	{
+		super(name);
+		setMaxDamage(2048);
+		setMaxStackSize(1);
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+		ItemStack stack = playerIn.getHeldItem(handIn);
+		
+		if(isNotAirBlock(worldIn))
+		{
+			playerIn.swingArm(handIn);
+			spawnLightningBolt(playerIn, worldIn);
+			stack.damageItem(1, playerIn);
+			playerIn.getCooldownTracker().setCooldown(stack.getItem(), 20);
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
+		}
+		
+        return new ActionResult<ItemStack>(EnumActionResult.FAIL, stack);
+    }
+	
+	public void spawnLightningBolt(EntityPlayer player, World worldIn)
+	{
+		RayTraceResult result = Minecraft.getMinecraft().getRenderViewEntity().rayTrace(20D, 1.0F);
+		
+		if(result != null)
+		{
+			EntityLightningBolt entity = new EntityLightningBolt(worldIn, result.getBlockPos().getX(), result.getBlockPos().getY(), result.getBlockPos().getZ(), false);
+			worldIn.spawnEntity(entity);
+		}
+	}
+	
+	public boolean isNotAirBlock(World worldIn)
+	{
+		RayTraceResult result = Minecraft.getMinecraft().getRenderViewEntity().rayTrace(20D, 1.0F);
+		
+		if(result != null)
+		{
+			BlockPos pos = new BlockPos(result.getBlockPos().getX(), result.getBlockPos().getY(), result.getBlockPos().getZ());
+			
+			if(!worldIn.isAirBlock(pos))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT))
+		{
+			tooltip.add(TextFormatting.BLUE + I18n.format("description.staff_thunder.name"));
+		}		
+		else
+		{
+			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
+		}
+    }
+}
