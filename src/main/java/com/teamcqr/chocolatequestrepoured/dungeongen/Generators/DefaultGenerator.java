926510b20031a39fd4a792c73913a84a643dde4a
DerToaster <seiler.oli@web.de>
2019-05-04 19:33:44 +0200

    Removed unnecessary TODO marks and added entries for JUNGLE and SWAMP cave

--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/Generators/DefaultGenerator.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/Generators/DefaultGenerator.java
@@ -11,40 +11,35 @@ import net.minecraft.world.chunk.Chunk;
  * GitHub: https://github.com/DerToaster98
  */
 public class DefaultGenerator implements IDungeonGenerator{
+	//This class has no actual use, it just floats around here for no reason, maybe because it can
 
 	@Override
 	public void preProcess(World world, Chunk chunk, int x, int y, int z) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void buildStructure(World world, Chunk chunk, int x, int y, int z) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void postProcess(World world, Chunk chunk, int x, int y, int z) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void fillChests(World world, Chunk chunk, int x, int y, int z) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void placeSpawners(World world, Chunk chunk, int x, int y, int z) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void placeCoverBlocks(World world, Chunk chunk, int x, int y, int z) {
-		// TODO Auto-generated method stub
 		
 	}
 
