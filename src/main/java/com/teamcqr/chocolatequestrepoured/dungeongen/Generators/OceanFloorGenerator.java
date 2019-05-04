0b687d3cba02c67844356ce3b442e0e58ad33d4d
DerToaster <seiler.oli@web.de>
2019-05-04 19:25:45 +0200

    Fixes, Improvements, began work on ruin and abandoned dungeon generator

--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/Generators/OceanFloorGenerator.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/Generators/OceanFloorGenerator.java
@@ -2,6 +2,7 @@ package com.teamcqr.chocolatequestrepoured.dungeongen.Generators;
 
 import java.util.Random;
 
+import com.teamcqr.chocolatequestrepoured.API.events.CQDungeonStructureGenerateEvent;
 import com.teamcqr.chocolatequestrepoured.dungeongen.IDungeonGenerator;
 import com.teamcqr.chocolatequestrepoured.dungeongen.PlateauBuilder;
 import com.teamcqr.chocolatequestrepoured.dungeongen.dungeons.DefaultSurfaceDungeon;
@@ -12,6 +13,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.structure.template.PlacementSettings;
+import net.minecraftforge.common.MinecraftForge;
 
 /**
  * Copyright (c) 29.04.2019
@@ -46,6 +48,9 @@ public class OceanFloorGenerator implements IDungeonGenerator{
 	public void buildStructure(World world, Chunk chunk, int x, int y, int z) {
 		//Simply puts the structure at x,y,z
 		this.structure.placeBlocksInWorld(world, new BlockPos(x, y, z), this.placeSettings);
+		
+		CQDungeonStructureGenerateEvent event = new CQDungeonStructureGenerateEvent(this.dungeon, new BlockPos(x,y,z), new BlockPos(this.structure.getSizeX(), this.structure.getSizeY(), this.structure.getSizeZ()),chunk.getPos());
+		MinecraftForge.EVENT_BUS.post(event);
 	}
 
 	@Override
