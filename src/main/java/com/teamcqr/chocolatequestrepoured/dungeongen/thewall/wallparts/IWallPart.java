d63afc53d4ea21e5ed24316a033a43a62602ba0e
DerToaster <seiler.oli@web.de>
2019-05-18 12:32:19 +0200

    Code for wall generators to get their bottom border

--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/thewall/wallparts/IWallPart.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/thewall/wallparts/IWallPart.java
@@ -1,5 +1,37 @@
 package com.teamcqr.chocolatequestrepoured.dungeongen.thewall.wallparts;
 
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+
 public interface IWallPart {
+	
+	int getTopY();
+	void generateWall(int chunkX, int chunkZ, World world, Chunk chunk);
+	
+	default int getLowerY(World world, Chunk chunk) {
+		int[] yValues = new int[] {256};
+		int index = 0;
+		for(int x = 0; x < 16; x++) {
+			for(int z = 0; z < 16; z++) {
+				int y = world.getTopSolidOrLiquidBlock(new BlockPos((chunk.x *16) +x, 0, (chunk.z *16) +z)).getY();
+				yValues[index] = y;
+				index++;
+			}
+		}
+		int lowestY = yValues[0];
+		for(int i = 1; i < yValues.length; i++) {
+			int y = yValues[i];
+			if(y < lowestY) {
+				lowestY = y;
+			}
+		}
+		if(lowestY <= 10) {
+			lowestY = 3;
+		} else {
+			lowestY -= 6;
+		}
+		return lowestY;
+	}
 
 }
