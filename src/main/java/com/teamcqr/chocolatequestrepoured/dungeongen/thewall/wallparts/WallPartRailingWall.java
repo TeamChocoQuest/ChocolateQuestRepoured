95a12004655ac1889d51ea5a0001e53a0a50707a
DerToaster <seiler.oli@web.de>
2019-05-22 07:30:30 +0200

    Wall Parts
    Wall Part for normal wall is finished, however, it should be put in a
    separate thread so it does not crash the game. However, im not that
    familar with forge. So i dont know how i can implement that cause i dont
    know if forge allows modifying the world from a separate thread...

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/thewall/wallparts/WallPartRailingWall.java
@@ -0,0 +1,25 @@
+package com.teamcqr.chocolatequestrepoured.dungeongen.thewall.wallparts;
+
+import com.teamcqr.chocolatequestrepoured.util.Reference;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+
+class WallPartRailingWall implements IWallPart {
+
+	public WallPartRailingWall() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getTopY() {
+		return Reference.CONFIG_HELPER.getWallTopY() - 12;
+	}
+
+	@Override
+	public void generateWall(int chunkX, int chunkZ, World world, Chunk chunk) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/thewall/wallparts/WallPartWall.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/thewall/wallparts/WallPartWall.java
@@ -1,14 +1,19 @@
 package com.teamcqr.chocolatequestrepoured.dungeongen.thewall.wallparts;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.teamcqr.chocolatequestrepoured.util.Reference;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
 class WallPartWall implements IWallPart {
 
 	public WallPartWall() {
-		// TODO Auto-generated constructor stub
+		// I dont think this needs a constructor
 	}
 
 	@Override
@@ -18,7 +23,40 @@ class WallPartWall implements IWallPart {
 
 	@Override
 	public void generateWall(int chunkX, int chunkZ, World world, Chunk chunk) {
-		// TODO Auto-generated method stub
+		// TODO Put all this calculation and block placing into a separate thread...
+		
+		int startX = chunkX *16;
+		int startZ = chunkZ *16;
+		
+		//All the calculated block positions are stored within these lists
+		List<BlockPos> outerBlocks = new ArrayList<BlockPos>();
+		List<BlockPos> innerBlocks = new ArrayList<BlockPos>();
+		
+		//Calculates all the block positions
+		for(int y = getLowerY(world, chunk); y <= getTopY(); y++) {
+			for(int z = 4; z < 13; z++) {
+				for(int x = 0; x < 16; x++) {
+					BlockPos pos = new BlockPos(startX +x, y, startZ +z);
+					if((z >= 6 && z <= 9) || y == getTopY()) {
+						innerBlocks.add(pos);
+					} else {
+						outerBlocks.add(pos);
+					}
+				}
+			}
+		}
+		
+		//Places the blocks at the calculated positions
+		if(!outerBlocks.isEmpty() && !innerBlocks.isEmpty()) {
+			//Inner Obsidian core
+			for(BlockPos pos : innerBlocks) {
+				world.setBlockState(pos, Blocks.OBSIDIAN.getDefaultState());
+			}
+			//Outer Stoneblock cover
+			for(BlockPos pos : outerBlocks) {
+				world.setBlockState(pos, Blocks.STONEBRICK.getDefaultState());
+			}
+		}
 		
 	}
 
