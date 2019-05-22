f4286e96a6e57e25ec9a071f9c1dd45ac293aff4
DerToaster <seiler.oli@web.de>
2019-05-22 06:53:51 +0200

    fixed compilation errors

--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/thewall/wallparts/WallPartRailing.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/thewall/wallparts/WallPartRailing.java
@@ -1,9 +1,25 @@
 package com.teamcqr.chocolatequestrepoured.dungeongen.thewall.wallparts;
 
+import com.teamcqr.chocolatequestrepoured.util.Reference;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+
 class WallPartRailing implements IWallPart {
 
 	public WallPartRailing() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getTopY() {
+		return Reference.CONFIG_HELPER.getWallTopY() - 12;
+	}
+
+	@Override
+	public void generateWall(int chunkX, int chunkZ, World world, Chunk chunk) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/thewall/wallparts/WallPartWall.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/thewall/wallparts/WallPartWall.java
@@ -1,9 +1,25 @@
 package com.teamcqr.chocolatequestrepoured.dungeongen.thewall.wallparts;
 
+import com.teamcqr.chocolatequestrepoured.util.Reference;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+
 class WallPartWall implements IWallPart {
 
 	public WallPartWall() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getTopY() {
+		return Reference.CONFIG_HELPER.getWallTopY() - 7;
+	}
+
+	@Override
+	public void generateWall(int chunkX, int chunkZ, World world, Chunk chunk) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
