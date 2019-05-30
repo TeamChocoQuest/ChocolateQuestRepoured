832f2cbf30ba1df70439c9f559a3eff93aebd632
DerToaster <seiler.oli@web.de>
2019-05-30 22:26:52 +0200

    removed unused comments

--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/PlateauBuilder.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/PlateauBuilder.java
@@ -28,10 +28,8 @@ public class PlateauBuilder {
 	}
 
 	public void generate(Random random, World world, int startX, int startY, int startZ, int sizeX, int sizeZ) {
-		//generateCubeUnderStructure(startX, startY, startZ, sizeX, sizeZ, world);
 		System.out.println("Trying to construct support platform...");
-		//Chunk startChunk = world.getChunkFromBlockCoords(new BlockPos(startX, startY, startZ));
-		//long seed = WorldDungeonGenerator.getSeed(world, startChunk.x, startChunk.z);
+
 		Perlin3D p = new Perlin3D(world.getSeed(), 8, random);
 		Perlin3D p2 = new Perlin3D(world.getSeed(), 32, random);
 
@@ -41,52 +39,6 @@ public class PlateauBuilder {
 
 		startX -= wallSize;
 		startZ -= wallSize;
-
-		/*int platFormHeight = 0;
-	    for (int x = 0; x < sizeX; x++)
-	    {
-	      for (int z = 0; z < sizeZ; z++)
-	      {
-	        int maxHeight = j - world.getTopSolidOrLiquidBlock(new BlockPos(x + i, 0, z + k)).getY();
-	        if (maxHeight > platFormHeight) {
-	        	platFormHeight = maxHeight;
-	        }
-	      }
-	    }
-	    platFormHeight = Math.max(1, platFormHeight);
-		
-		for(int x = 0; x <sizeX; x++) {
-			for(int z = 0; z <sizeZ; z++) {
-				int maxHeight = j -world.getTopSolidOrLiquidBlock(new BlockPos(x +i, 0, z +k)).getY();
-				int maxY = 0;
-				
-				for(int y = platFormHeight - maxHeight; y < platFormHeight; y++) {
-					if((x > wallSize) && (z > wallSize) && (x < sizeX - wallSize) && (z < sizeZ - wallSize)) {
-						world.setBlockState(new BlockPos(x,j -y,z), this.structureBlock.getDefaultState());
-						maxY = y;
-					} else {
-						float noise = (y -maxHeight) / (Math.max(1, maxHeight) * 1.5F);
-						
-						int wallSizeTMP = wallSize;
-						
-						noise += Math.max(0.0F, (wallSizeTMP -x) /wallSize);
-						noise += Math.max(0.0F, (wallSizeTMP - (sizeX -x)) /wallSize);
-						
-						noise += Math.max(0.0F, (wallSizeTMP -z) /wallSize);
-						noise += Math.max(0.0F, (wallSizeTMP - (sizeZ -z)) /wallSize);
-						
-						double noiseValue = (p.getNoiseAt(x +i, y, z +k) +p2.getNoiseAt(x +i, y, z +k) +noise) /3.0D +y /platFormHeight *0.25D;
-						if(noiseValue < 0.5D) {
-							world.setBlockState(new BlockPos(x,j -y,z), this.structureBlock.getDefaultState());
-							maxY = y;
-						}
-					}
-				}
-				if(maxHeight <= j) {
-					world.setBlockState(new BlockPos(x,j -maxY,z), this.structureTopBlock.getDefaultState());
-				}
-			}
-		}*/
 		
 		for (int x = 0; x < sizeX; ++x) {
 			for (int z = 0; z < sizeZ; ++z) {
@@ -111,6 +63,7 @@ public class PlateauBuilder {
 							world.setBlockState(new BlockPos(startX + x, posY + y, startZ + z),this.structureBlock.getDefaultState());
 					}
 				}
+				//This places the top layer blocks
 				maxHeight = world.getTopSolidOrLiquidBlock(new BlockPos(x+startX,0,z+startZ)).getY();//DungeonGenUtils.getHighestYAt(world.getChunkFromBlockCoords(new BlockPos(x + i, 0, z + k)),x + i, z + k, true);// world.getTopSolidOrLiquidBlock(new BlockPos(x + i, 0, z + k)).getY();
 				if (maxHeight <= startY) {
 					world.setBlockState(new BlockPos(startX + x, maxHeight -1, startZ + z),
