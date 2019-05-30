a2b93d8076371b0c683ba712245f2f5d092a7fb1
DerToaster <seiler.oli@web.de>
2019-05-30 22:09:50 +0200

    fixed plateau building

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/Perlin3D.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/Perlin3D.java
@@ -12,31 +12,34 @@ public class Perlin3D {
 	public Perlin3D(long seed, int octave, Random random) {
 		this.seed = seed;
 		frequency = octave;
-		rand = new Random();
+		rand = random;//new Random();
 	}
 
 	public double getNoiseAt(int x, int y, int z) {
-		int ymin = (int) Math.floor(y / frequency);
+		int ymin = (int) Math.floor((double) y / (double) this.frequency);
 		int ymax = ymin + 1;
-		return cosineInterpolate((float) getNoiseLevelAtPosition(x, ymin, z),
-				(float) getNoiseLevelAtPosition(x, ymax, z), (y - ymin * frequency) / frequency);
+		return (double) this.cosineInterpolate((float) this.getNoiseLevelAtPosition(x, ymin, z),
+				(float) this.getNoiseLevelAtPosition(x, ymax, z),
+				((float) y - (float) ymin * (float) this.frequency) / (float) this.frequency);
 	}
 
 	private double getNoiseLevelAtPosition(int x, int y, int z) {
-		int xmin = (int) Math.floor(x / frequency);
+		int xmin = (int) Math.floor((double) x / (double) this.frequency);
 		int xmax = xmin + 1;
-		int zmin = (int) Math.floor(z / frequency);
+		int zmin = (int) Math.floor((double) z / (double) this.frequency);
 		int zmax = zmin + 1;
-		return cosineInterpolate(
+		return (double)cosineInterpolate(
 				cosineInterpolate((float) getRandomAtPosition(xmin, y, zmin),
-						(float) getRandomAtPosition(xmax, y, zmin), (x - xmin * frequency) / frequency),
+						(float) getRandomAtPosition(xmax, y, zmin), 
+						(float) (x - xmin * frequency) / (float)frequency),
 				cosineInterpolate((float) getRandomAtPosition(xmin, y, zmax),
-						(float) getRandomAtPosition(xmax, y, zmax), (x - xmin * frequency) / frequency),
-				(z - zmin * frequency) / frequency);
+						(float) getRandomAtPosition(xmax, y, zmax), 
+						(float) (x - xmin * frequency) / (float) frequency),
+				((float) z - (float) zmin * (float) this.frequency) / (float) this.frequency);
 	}
 
 	private float cosineInterpolate(float a, float b, float x) {
-		float f = (float) ((1.0D - Math.cos(x * Math.PI)) * 0.5D);
+		float f = (float) ((1.0D - Math.cos((double)x * Math.PI)) * 0.5D);
 		return a * (1.0F - f) + b * f;
 	}
 
@@ -46,7 +49,12 @@ public class Perlin3D {
 	}
 
 	private double getRandomAtPosition(int x, int y, int z) {
-		rand.setSeed((long) (10000.0D * (Math.sin(x) + Math.cos(z) + Math.cos(y) + Math.tan(seed))));
+		Double seedD = new Double((10000.0D * (Math.sin(x) + Math.cos(z) + Math.cos(y) + Math.tan((double)this.seed))));
+		//System.out.println("Double value: " + seedD);
+		//System.out.println("double value: " + seedD.doubleValue());
+		rand.setSeed(seedD.longValue());
+		//System.out.println("long value: " + seedD.longValue());
+		//System.out.println("long value with cast: " + ((long) seedD.doubleValue()));
 		return rand.nextDouble();
 	}
 
