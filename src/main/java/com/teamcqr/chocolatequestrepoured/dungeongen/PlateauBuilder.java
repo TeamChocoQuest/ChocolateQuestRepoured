00e4950db901090262f92ab31cf09a3bdc2bff5a
DerToaster <seiler.oli@web.de>
2019-05-30 22:09:50 +0200

    fixed plateau building

--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/PlateauBuilder.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/PlateauBuilder.java
@@ -27,18 +27,20 @@ public class PlateauBuilder {
 		structureTopBlock = top;
 	}
 
-	public void generate(Random random, World world, int i, int j, int k, int sizeX, int sizeZ) {
+	public void generate(Random random, World world, int startX, int startY, int startZ, int sizeX, int sizeZ) {
+		//generateCubeUnderStructure(startX, startY, startZ, sizeX, sizeZ, world);
 		System.out.println("Trying to construct support platform...");
-		long seed = WorldDungeonGenerator.getSeed(world, i, k);
-		Perlin3D p = new Perlin3D(seed, 8, random);
-		Perlin3D p2 = new Perlin3D(seed, 32, random);
+		//Chunk startChunk = world.getChunkFromBlockCoords(new BlockPos(startX, startY, startZ));
+		//long seed = WorldDungeonGenerator.getSeed(world, startChunk.x, startChunk.z);
+		Perlin3D p = new Perlin3D(world.getSeed(), 8, random);
+		Perlin3D p2 = new Perlin3D(world.getSeed(), 32, random);
 
 		int wallSize = 8;
 		sizeX += wallSize * 2;
 		sizeZ += wallSize * 2;
 
-		i -= wallSize;
-		k -= wallSize;
+		startX -= wallSize;
+		startZ -= wallSize;
 
 		/*int platFormHeight = 0;
 	    for (int x = 0; x < sizeX; x++)
@@ -86,37 +88,37 @@ public class PlateauBuilder {
 			}
 		}*/
 		
-		for (int x = 0; x < sizeX; x++) {
-			for (int z = 0; z < sizeZ; z++) {
-				int maxHeight = j - 1 -  world.getTopSolidOrLiquidBlock(new BlockPos(x+i,0,z+k)).getY();//DungeonGenUtils.getHighestYAt(world.getChunkFromBlockCoords(new BlockPos(x + i, 0, z + k)), x + i, z + k, true);
-				int posY = world.getTopSolidOrLiquidBlock(new BlockPos(x+i,0,z+k)).getY();//DungeonGenUtils.getHighestYAt(world.getChunkFromBlockCoords(new BlockPos(x + i, 0, z + k)),x + i, z + k, true);
-				int topY = posY +1;
-				for (int y = 0; y <= maxHeight; y++) {
+		for (int x = 0; x < sizeX; ++x) {
+			for (int z = 0; z < sizeZ; ++z) {
+				int maxHeight = startY - 1 -  world.getTopSolidOrLiquidBlock(new BlockPos(x+startX,0,z+startZ)).getY();//DungeonGenUtils.getHighestYAt(world.getChunkFromBlockCoords(new BlockPos(x + i, 0, z + k)), x + i, z + k, true);
+				int posY = world.getTopSolidOrLiquidBlock(new BlockPos(x+startX,0,z+startZ)).getY();//DungeonGenUtils.getHighestYAt(world.getChunkFromBlockCoords(new BlockPos(x + i, 0, z + k)),x + i, z + k, true);
+				for (int y = 0; y <= maxHeight; ++y) {
+					//This generates the "cube" that goes under the structure
 					if ((x > wallSize) && (z > wallSize) && (x < sizeX - wallSize) && (z < sizeZ - wallSize)) {
-						world.setBlockState(new BlockPos(i + x, posY, k + z), this.structureBlock.getDefaultState());
-						topY = posY;
-					} else {
-						float noiseVar = (y - maxHeight) / (Math.max(1, maxHeight) * 1.5F);
+						world.setBlockState(new BlockPos(startX + x, posY +y, startZ + z), this.structureBlock.getDefaultState());
+					}
+					//This generates the fancy "curved" walls around the cube
+					else {
+						float noiseVar = (float)(y - maxHeight) / ((float)Math.max(1, maxHeight) * 1.5F);
 
-						int tWallSize = wallSize;
-						noiseVar += Math.max(0.0F, (tWallSize - x) / 8.0F);
-						noiseVar += Math.max(0.0F, (tWallSize - (sizeX - x)) / 8.0F);
+						noiseVar += Math.max(0.0F, (wallSize - x) / 8.0F);
+						noiseVar += Math.max(0.0F, (wallSize - (sizeX - x)) / 8.0F);
 
-						noiseVar += Math.max(0.0F, (tWallSize - z) / 8.0F);
-						noiseVar += Math.max(0.0F, (tWallSize - (sizeZ - z)) / 8.0F);
-						double value = (p.getNoiseAt(x + i, y, z + k) + p2.getNoiseAt(x + i, y, z + k) + noiseVar)/ 3.0D;
+						noiseVar += Math.max(0.0F, (wallSize - z) / 8.0F);
+						noiseVar += Math.max(0.0F, (wallSize - (sizeZ - z)) / 8.0F);
+						double value = (p.getNoiseAt(x + startX, y, z + startZ) + p2.getNoiseAt(x + startX, y, z + startZ) + noiseVar) / 3.0D + y / (maxHeight == 0 ? 1:maxHeight) *0.25D;
 						if (value < 0.5D)
-							world.setBlockState(new BlockPos(i + x, posY + y, k + z),this.structureBlock.getDefaultState());
-						topY = posY +y;
+							world.setBlockState(new BlockPos(startX + x, posY + y, startZ + z),this.structureBlock.getDefaultState());
 					}
 				}
-				maxHeight = world.getTopSolidOrLiquidBlock(new BlockPos(x+i,0,z+k)).getY();//DungeonGenUtils.getHighestYAt(world.getChunkFromBlockCoords(new BlockPos(x + i, 0, z + k)),x + i, z + k, true);// world.getTopSolidOrLiquidBlock(new BlockPos(x + i, 0, z + k)).getY();
-				if (maxHeight <= j) {
-					world.setBlockState(new BlockPos(i + x, topY +1, k + z),
+				maxHeight = world.getTopSolidOrLiquidBlock(new BlockPos(x+startX,0,z+startZ)).getY();//DungeonGenUtils.getHighestYAt(world.getChunkFromBlockCoords(new BlockPos(x + i, 0, z + k)),x + i, z + k, true);// world.getTopSolidOrLiquidBlock(new BlockPos(x + i, 0, z + k)).getY();
+				if (maxHeight <= startY) {
+					world.setBlockState(new BlockPos(startX + x, maxHeight -1, startZ + z),
 							this.structureTopBlock.getDefaultState());
 				}
 			}
 		}
 	}
+
 
 }
