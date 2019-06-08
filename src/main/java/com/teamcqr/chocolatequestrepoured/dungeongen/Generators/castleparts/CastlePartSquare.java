52ee39244113f704815805b9af6fa40c28f5cfc5
KalgogSmash <trevor.seyf@gmail.com>
2019-06-08 05:58:32 -0700

    Updating casle generation to use the part interface, and adding a Tower part (though it isn't used yet)

--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/Generators/castleparts/CastlePartSquare.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/Generators/castleparts/CastlePartSquare.java
@@ -1,12 +1,16 @@
 package com.teamcqr.chocolatequestrepoured.dungeongen.Generators.castleparts;
 
 import com.teamcqr.chocolatequestrepoured.dungeongen.dungeons.CastleDungeon;
+import com.teamcqr.chocolatequestrepoured.util.DungeonGenUtils;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Copyright (c) 01.06.2019 Developed by KalgogSmash:
@@ -14,38 +18,49 @@ import java.util.List;
  */
 public class CastlePartSquare implements ICastlePart
 {
-    boolean hasBoss;
+    private BlockPos start;
+    private int sizeX;
+    private int sizeY;
+    private int sizeZ;
+    private int floors;
+    private EnumFacing facing;
     CastleDungeon dungeon;
+    private Random random;
 
-    public CastlePartSquare(boolean hasBoss, CastleDungeon dungeon)
+
+    public CastlePartSquare(BlockPos origin, int sizeX, int sizeZ, int floors, CastleDungeon dungeon, EnumFacing facing)
     {
-        this.hasBoss = hasBoss;
         this.dungeon = dungeon;
-    }
+        this.floors = floors;
+        this.random = new Random(origin.getX() + origin.getY() + origin.getZ());
 
-    public CastlePartSquare()
-    {
-        this.hasBoss = false;
+        this.start = origin;
+        this.sizeX = sizeX;
+        this.sizeY = this.dungeon.getFloorHeight() * floors;
+        this.sizeZ = sizeZ;
+        this.facing = facing;
     }
 
     @Override
-    public void generatePart(World world, int x, int y, int z, int sizeX, int sizeZ, int roomSize, int floors)
+    public void generatePart(World world)
     {
-        int roomsX = Math.max(1, sizeX / roomSize);
-        int roomsZ = Math.max(1, sizeZ / roomSize);
+        int roomsX = Math.max(1, sizeX / dungeon.getRoomSize());
+        int roomsZ = Math.max(1, sizeZ / dungeon.getRoomSize());
         int roomSizeX = sizeX / roomsX;
         int roomSizeZ = sizeZ / roomsZ;
         //int lastRoomOffsetX = sizeX - roomsX * roomSizeX;
         //int lastRoomOffsetZ = sizeZ - roomsZ * roomSizeZ;
         int currentY;
         int floorHeight = dungeon.getFloorHeight();
+        int x = start.getX();
+        int y = start.getY();
+        int z = start.getZ();
 
         List<BlockPos> floorBlocks = new ArrayList<>();
         List<BlockPos> wallBlocks = new ArrayList<>();
         List<BlockPos> ceilingBlocks = new ArrayList<>();
 
-        System.out.println("Building a square structure at " + x + ", " + y + ", " + z + ". sizeX = " + sizeX + ", sizeZ = " + sizeZ + ". Floors = " + floors);
-        //build the first floor
+        System.out.println("Building a square part at " + x + ", " + y + ", " + z + ". sizeX = " + sizeX + ", sizeZ = " + sizeZ + ". Floors = " + floors + ". Facing = " + facing.toString());
 
         //for each floor
         for (int currentFloor = 0; currentFloor < floors; currentFloor++)
@@ -82,24 +97,30 @@ public class CastlePartSquare implements ICastlePart
                     wallBlocks.add(new BlockPos(x + sizeX - 1, currentY + j, z + i));
                 }
             }
+        }
 
-            if(!floorBlocks.isEmpty()) {
-                for(BlockPos pos : floorBlocks) {
-                    world.setBlockState(pos, dungeon.getFloorBlock().getDefaultState());
-                }
-            }
-
-            if(!ceilingBlocks.isEmpty()) {
-                for(BlockPos pos : ceilingBlocks) {
-                    world.setBlockState(pos, dungeon.getWallBlock().getDefaultState());
-                }
-            }
-
-            if(!wallBlocks.isEmpty()) {
-                for(BlockPos pos : wallBlocks) {
-                    world.setBlockState(pos, dungeon.getWallBlock().getDefaultState());
-                }
+        if(!floorBlocks.isEmpty()) {
+            for(BlockPos pos : floorBlocks) {
+                world.setBlockState(pos, dungeon.getFloorBlock().getDefaultState());
             }
         }
+
+        if(!ceilingBlocks.isEmpty()) {
+            for(BlockPos pos : ceilingBlocks) {
+                world.setBlockState(pos, dungeon.getWallBlock().getDefaultState());
+            }
+        }
+
+        if(!wallBlocks.isEmpty()) {
+            for(BlockPos pos : wallBlocks) {
+                world.setBlockState(pos, dungeon.getWallBlock().getDefaultState());
+            }
+        }
+    }
+
+    @Override
+    public boolean isTower()
+    {
+        return false;
     }
 }
