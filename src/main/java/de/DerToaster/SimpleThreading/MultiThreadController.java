dbba12b30f6bc23e5786b5c792b4a3537543419f
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-07-03 21:39:39 +0200

    uploaded my multi thread util (which sadly is untested but it should work)

---
+++ src/main/java/de/DerToaster/SimpleThreading/MultiThreadController.java
@@ -0,0 +1,34 @@
+package de.DerToaster.SimpleThreading;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class MultiThreadController {
+	
+	private List<SimpleThread> threads = new ArrayList<SimpleThread>();
+	
+	private Queue<Runnable> tasksToAdd;
+
+	public MultiThreadController(int threadCount) {
+		for(int i = 0; i < (Math.abs(threadCount) > 0 ? Math.abs(threadCount):1); i++) {
+			this.threads.add(new SimpleThread(true));
+		}
+	}
+	
+	public void addTask(Runnable task) {
+		this.tasksToAdd.add(task);
+		
+		boolean taskNotAdded = true;
+		while(taskNotAdded) {
+			for(SimpleThread st : this.threads) {
+				if(!st.isQueueLocked()) {
+					st.addTask(task);
+					taskNotAdded = false;
+					break;
+				}
+			}
+		}
+	}
+
+}
---
+++ src/main/java/de/DerToaster/SimpleThreading/SimpleThread.java
@@ -0,0 +1,81 @@
+package de.DerToaster.SimpleThreading;
+
+import java.util.Queue;
+
+public class SimpleThread extends Thread {
+
+	private Thread mainThread;
+	
+	private Queue<Runnable> tasks;
+	
+	private boolean queueLocked = true;
+	
+	public SimpleThread(boolean async) {
+		this.mainThread = Thread.currentThread();
+		
+		this.setDaemon(async);
+		
+		this.queueLocked = false;
+	}
+	
+	public void addTask(Runnable task) {
+		lockQueue();
+		
+		boolean runAfterAdd = false;
+		if(this.tasks.isEmpty()) {
+			runAfterAdd = true;
+		}
+		
+		tasks.add(task);
+		
+		unlockQueue();
+		
+		if(runAfterAdd) {
+			this.run();
+		}
+	}
+	
+	@Override
+	public synchronized void start() {
+		if(!this.tasks.isEmpty()) {
+			run();
+		}
+	}
+	
+	@Override
+	public void run() {
+		if(!this.tasks.isEmpty()) {
+			lockQueue();
+			
+			this.tasks.remove().run();
+			
+			unlockQueue();
+			
+			try {
+				sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			this.run();
+		}
+	}
+	
+	
+	
+	public boolean isQueueLocked() {
+		return this.queueLocked;
+	}
+	
+	private void lockQueue() {
+		this.queueLocked = true;
+	}
+	
+	private void unlockQueue() {
+		this.queueLocked = false;
+	}
+	
+	public Thread getMainThread() {
+		return this.mainThread;
+	}
+
+}
