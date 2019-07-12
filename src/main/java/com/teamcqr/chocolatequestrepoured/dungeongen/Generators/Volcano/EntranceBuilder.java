44831f08edd24150741d0b154ce86edf1c1aacef
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-07-12 18:00:52 +0200

    volcano entrance to stronghold

--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/Generators/Volcano/EntranceBuilder.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/Generators/Volcano/EntranceBuilder.java
@@ -1,14 +1,15 @@
 package com.teamcqr.chocolatequestrepoured.dungeongen.Generators.Volcano;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.teamcqr.chocolatequestrepoured.dungeongen.dungeons.VolcanoDungeon;
 import com.teamcqr.chocolatequestrepoured.init.ModBlocks;
+import com.teamcqr.chocolatequestrepoured.objects.blocks.BlockUnlitTorch;
 
+import net.minecraft.block.BlockRotatedPillar;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 
 public class EntranceBuilder {
@@ -27,26 +28,132 @@ public class EntranceBuilder {
 		this.world = world;
 	}
 	
+	public void generate() {
+		
+		Vec3i expansionVector = new Vec3i(0,0,0);
+		switch(direction) {
+		case EAST:
+			expansionVector = new Vec3i(3,0,0);
+			break;
+		case NORTH:
+			expansionVector = new Vec3i(0,0,-3);
+			break;
+		case SOUTH:
+			expansionVector = new Vec3i(0,0,3);
+			break;
+		case WEST:
+			expansionVector = new Vec3i(-3,0,0);
+			break;
+		default:
+			break;
+		}
+		//DONE: Place fire pots and "porch"
+		BlockPos pos = startPos;//.add(expansionV);
+		
+		buildFirePotPillar(pos.subtract(expansionVector));
+		for(int i = 0; i < blocksRemainingToWall /3; i++) {
+			buildSegment(pos);
+			pos = pos.add(expansionVector);
+		}
+	}
+	
+	private void buildSegment(BlockPos startPosCentered) {
+		BlockPos corner1, corner2,pillar1,pillar2,torch1,torch2,air1,air2;
+		corner1 = null;
+		corner2 = null;
+		pillar1 = null;
+		pillar2 = null;
+		torch1 = null;
+		torch2 = null;
+		air1 = null;
+		air2 = null;
+		switch(direction) {
+		case EAST:
+			corner1 = startPosCentered.add(0,0,-3);
+			corner2 = startPosCentered.add(3,0,3);
+			air1 = startPosCentered.add(0,1,-2);
+			air2 = startPosCentered.add(3,5,-2);
+			pillar1 = startPosCentered.add(1,0,2);
+			pillar2 = startPosCentered.add(1,0,-2);
+			torch1 = startPosCentered.add(1,4,1);
+			torch2 = startPosCentered.add(1,4,-1);
+			break;
+		case NORTH:
+			corner1 = startPosCentered.add(-3,0,0);
+			corner2 = startPosCentered.add(3,0,-3);
+			air1 = startPosCentered.add(-2,1,0);
+			air2 = startPosCentered.add(2,5,-3);
+			pillar1 = startPosCentered.add(2,0,-1);
+			pillar2 = startPosCentered.add(-2,0,-1);
+			torch1 = startPosCentered.add(1,4,-1);
+			torch2 = startPosCentered.add(-1,4,-1);
+			break;
+		case SOUTH:
+			corner1 = startPosCentered.add(3,0,0);
+			corner2 = startPosCentered.add(-3,0,3);
+			air1 = startPosCentered.add(2,1,0);
+			air2 = startPosCentered.add(-2,5,3);
+			pillar1 = startPosCentered.add(-2,0,1);
+			pillar2 = startPosCentered.add(2,0,1);
+			torch1 = startPosCentered.add(-1,4,1);
+			torch2 = startPosCentered.add(1,4,1);
+			break;
+		case WEST:
+			corner1 = startPosCentered.add(0,0,3);
+			corner2 = startPosCentered.add(-3,0,-3);
+			air1 = startPosCentered.add(0,1,2);
+			air2 = startPosCentered.add(-3,5,2);
+			pillar1 = startPosCentered.add(-1,0,-2);
+			pillar1 = startPosCentered.add(-1,0,2);
+			torch1 = startPosCentered.add(-1,4,-1);
+			torch2 = startPosCentered.add(-1,4,1);
+			break;
+		default:
+			break;		
+		}
+		if(corner1 != null && corner2 != null && pillar1 != null && pillar2 != null) {
+			for(BlockPos airPos : BlockPos.getAllInBox(air1, air2)) {
+				world.setBlockToAir(airPos);
+			}
+			buildFloorAndCeiling(corner1, corner2, 5);
+			//world.setBlockState(torch1, Blocks.RED_SHULKER_BOX.getDefaultState());
+			//Left torch -> Facing side: rotate right (90.0°)
+			world.setBlockState(torch1, ModBlocks.UNLIT_TORCH.getDefaultState().withProperty(BlockUnlitTorch.FACING, StairCaseHelper.getFacingWithRotation(direction, Rotation.COUNTERCLOCKWISE_90)));
+			//world.setBlockState(torch2, Blocks.GREEN_SHULKER_BOX.getDefaultState());
+			//Right torch -> Facing side: rotate left (-90.0°)
+			world.setBlockState(torch2, ModBlocks.UNLIT_TORCH.getDefaultState().withProperty(BlockUnlitTorch.FACING, StairCaseHelper.getFacingWithRotation(direction, Rotation.CLOCKWISE_90)));
+			buildPillar(pillar1);
+			buildPillar(pillar2);
+		}
+	}
+	
 	private void buildPillar(BlockPos bottom) {
-		List<BlockPos> pillarBlocks = new ArrayList<BlockPos>();
 		for(int iY = 1; iY <= 4; iY++) {
-			world.setBlockState(bottom.add(0,iY,0), ModBlocks.GRANITE_PILLAR.getDefaultState().withRotation(Rotation.NONE));
+			BlockPos pos = bottom.add(0,iY,0);
+			world.setBlockState(pos, ModBlocks.GRANITE_PILLAR.getDefaultState().withProperty(BlockRotatedPillar.AXIS, EnumFacing.Axis.Y));
 		}
 		world.setBlockState(bottom.add(0,5,0), ModBlocks.GRANITE_CARVED.getDefaultState());
 	}
 	
 	private void buildFloorAndCeiling(BlockPos start, BlockPos end, int ceilingHeight) {
 		BlockPos endP = new BlockPos(end.getX(), start.getY(), end.getZ());
-		
+
 		//Floor
 		for(BlockPos p : BlockPos.getAllInBox(start, endP)) {
 			world.setBlockState(p, ModBlocks.GRANITE_BRICK_SMALL.getDefaultState());
 		}
 		
 		//Ceiling
-		for(BlockPos p : BlockPos.getAllInBox(start.add(0,ceilingHeight, 0), endP.add(0,ceilingHeight, 0))) {
+		for(BlockPos p : BlockPos.getAllInBox(start.add(0,ceilingHeight +1, 0), endP.add(0,ceilingHeight +1, 0))) {
 			world.setBlockState(p, ModBlocks.GRANITE_SQUARE.getDefaultState());
 		}
+	}
+	
+	private void buildFirePotPillar(BlockPos bottom) {
+		world.setBlockState(bottom.add(0,1,0), ModBlocks.GRANITE_PILLAR.getDefaultState().withProperty(BlockRotatedPillar.AXIS, EnumFacing.Axis.Y));
+		world.setBlockState(bottom.add(0,2,0), ModBlocks.GRANITE_PILLAR.getDefaultState().withProperty(BlockRotatedPillar.AXIS, EnumFacing.Axis.Y));
+		world.setBlockState(bottom.add(0,3,0), Blocks.NETHERRACK.getDefaultState());
+		world.setBlockState(bottom.add(0,4,0), Blocks.FIRE.getDefaultState());
 	}
 
 }
--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/dungeons/VolcanoDungeon.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/dungeons/VolcanoDungeon.java
@@ -27,7 +27,7 @@ import net.minecraft.world.chunk.Chunk;
 public class VolcanoDungeon extends StrongholdDungeon {
 	
 	private boolean buildStairwell = true;
-	private boolean buildPillars = false;
+	private boolean buildDungeon = false;
 	private boolean damagedVolcano = true;
 	private boolean ores = true;
 	private int oreConcentration = 5;
@@ -81,7 +81,7 @@ public class VolcanoDungeon extends StrongholdDungeon {
 		}
 		if(prop != null && configFile != null && fis != null) {
 			this.buildStairwell = PropertyFileHelper.getBooleanProperty(prop, "buildPath", true);
-			this.buildPillars = PropertyFileHelper.getBooleanProperty(prop, "buildPillars", false);
+			this.buildDungeon = PropertyFileHelper.getBooleanProperty(prop, "buildDungeon", true);
 			this.minHeight = PropertyFileHelper.getIntProperty(prop, "minHeight", 100);
 			this.maxHeight = PropertyFileHelper.getIntProperty(prop, "maxHeight", 130);
 			this.innerRadius = PropertyFileHelper.getIntProperty(prop, "innerRadius", 5);
@@ -161,8 +161,8 @@ public class VolcanoDungeon extends StrongholdDungeon {
 		return this.buildStairwell;
 	}
 	
-	public boolean doBuildPillars() {
-		return this.buildPillars;
+	public boolean doBuildDungeon() {
+		return this.buildDungeon;
 	}
 	
 	public boolean isVolcanoDamaged() {
