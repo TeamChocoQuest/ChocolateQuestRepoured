56b3fde5da1b291f3c36921f5f849d9d597a3576
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-07-20 11:49:48 +0200

    basic stuff that it compiles

--- src/main/java/com/teamcqr/chocolatequestrepoured/factions/EFactions.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/factions/EFactions.java
@@ -10,19 +10,28 @@ public enum EFactions {
 	WALKERS(new String[] {"UNDEAD", "PIRATE", "DWARVES_AND_GOLEMS", "GOBLINS", "ENDERMEN", "OGRES_AND_GREMLINS", "INQUISITION", "ILLAGERS", "VILLAGERS", "PLAYER", "NPC"}, new String[] {}, EReputationState.ARCH_ENEMY),
 	DWARVES_AND_GOLEMS(new String[] {"WALKERS", "ENDERMEN", "ILLAGERS"}, new String[] {"VILLAGERS", "NPC", "INQUISITION"}, EReputationState.ACCEPTED),
 	GOBLINS(new String[] {"OGRES_AND_GREMLINS", "WALKERS", "VILLAGERS", "INQUISITION"}, new String[] {"ENDERMEN", "ILLAGERS"}, EReputationState.ENEMY),
-	ENDERMEN(),
-	OGRES_AND_GREMLINS(),
-	INQUISITION(),
-	ILLAGERS(),
-	VILLAGERS();
+	ENDERMEN(new String[] {}, new String[] {}, EReputationState.NEUTRAL),
+	OGRES_AND_GREMLINS(new String[] {}, new String[] {}, EReputationState.NEUTRAL),
+	INQUISITION(new String[] {}, new String[] {}, EReputationState.NEUTRAL),
+	ILLAGERS(new String[] {}, new String[] {}, EReputationState.NEUTRAL),
+	VILLAGERS(new String[] {}, new String[] {}, EReputationState.NEUTRAL),
+	;
 	
 	
 	private EFactions(String[] enemies, String[] allies, EReputationState startState) {
-		// TODO Auto-generated constructor stub
+		//TODO: Create fields and fill them
 	}
+	
+	//TODO: Methods to check wether a faction is an ally or an enemy
 	
 	public EReputationStateRough getRelation(ICQREntity e1, ICQREntity e2) {
 		
+		EFactions e1Fac = e1.getFaction();
+		EFactions e2Fac = e2.getFaction();
+		
+		//TODO: Check for allies or enemy
+		
+		return EReputationStateRough.NEUTRAL;
 	}
 
 }
