75a663f8eb4eb790f676a6dd7f9cbe5eaf7cce27
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-07-23 18:13:34 +0200

    began code for floating city
    moved code for circle/sphere checks to dungeonGenUtil

--- src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/Generators/FloatingNetherCityGenerator.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/dungeongen/Generators/FloatingNetherCityGenerator.java
@@ -1,22 +1,26 @@
 package com.teamcqr.chocolatequestrepoured.dungeongen.Generators;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import com.teamcqr.chocolatequestrepoured.dungeongen.dungeons.FloatingNetherCity;
+import com.teamcqr.chocolatequestrepoured.util.DungeonGenUtils;
 
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
 public class FloatingNetherCityGenerator implements IDungeonGenerator {
 
-	private FloatingNetherCity generator;
+	private FloatingNetherCity dungeon;
 	private int islandCount = 1;
 	
 	//This needs to calculate async (island blocks, chain blocks, air blocks)
 	
 	public FloatingNetherCityGenerator(FloatingNetherCity generator) {
-		this.generator = generator;
-		this.islandCount = this.generator.getBuildingCount(new Random());
+		this.dungeon = generator;
+		this.islandCount = this.dungeon.getBuildingCount(new Random());
 	}
 
 	@Override
@@ -56,6 +60,35 @@ public class FloatingNetherCityGenerator implements IDungeonGenerator {
 	public void placeCoverBlocks(World world, Chunk chunk, int x, int y, int z) {
 		// Unused  or maybe later implemented
 
+	}
+	
+	//Constructs an Island in this shape:
+	/*						Dec  Rad
+	 * ####################  0    10
+	 *  ##################   1    9
+	 *    ##############     2    7
+	 *       ########        3    4
+	 *           
+	 */
+	private void buildPlatform(BlockPos center, int radius, World world) {
+		List<BlockPos> blocks = new ArrayList<>();
+		int decrementor = 0;
+		int rad = radius;
+		while(decrementor < (rad /2)) {
+			rad -= decrementor;
+			
+			for(int iX = -rad; iX <= rad; iX++) {
+				for(int iZ = -rad; iZ <= rad; iZ++) {
+					if(DungeonGenUtils.isInsideCircle(iX, iZ, rad, center)) {
+						blocks.add(center.add(iX, -decrementor, iZ));
+					}
+				}
+			}
+			
+			decrementor++;
+		}
+		
+		DungeonGenUtils.passListWithBlocksToThreads(blocks, dungeon.getIslandBlock(), world, 100);
 	}
 
 }
