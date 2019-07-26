538a68470de7316923352d0e4a9e3e3a1e55888a
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-07-26 22:27:47 +0200

    more entity experiments
    WIP: Armor breaking
    
    Armor is now displayed
    
    NIY: AI

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRDwarf.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRDwarf.java
@@ -1,11 +1,17 @@
 package com.teamcqr.chocolatequestrepoured.client.render.entity;
 
+import org.lwjgl.opengl.GL11;
+
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRDwarf;
 import com.teamcqr.chocolatequestrepoured.util.Reference;
 
-import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerArrow;
+import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
+import net.minecraft.client.renderer.entity.layers.LayerElytra;
+import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
 import net.minecraft.util.ResourceLocation;
 
 public class RenderCQRDwarf extends RenderLiving<EntityCQRDwarf> {
@@ -15,7 +21,12 @@ public class RenderCQRDwarf extends RenderLiving<EntityCQRDwarf> {
 	 public RenderCQRDwarf(RenderManager manager)
 	    {
 	        //Using the vanilla model - if we wanted to use a custom model this is where we could insert that instead
-	        super(manager, new ModelPlayer(0.5F, false), 0.5F);
+	        super(manager, new ModelBiped(), 0.5F);
+	        
+	        this.addLayer(new LayerBipedArmor(this));
+	        this.addLayer(new LayerHeldItem(this));
+	        this.addLayer(new LayerArrow(this));
+	        this.addLayer(new LayerElytra(this));
 	    }
 	 
 	@Override
@@ -27,5 +38,15 @@ public class RenderCQRDwarf extends RenderLiving<EntityCQRDwarf> {
 	protected void applyRotations(EntityCQRDwarf entityLiving, float p_77043_2_, float rotationYaw, float partialTicks) {
         super.applyRotations(entityLiving, p_77043_2_, rotationYaw, partialTicks);
     }
+	
+	@Override
+	protected void preRenderCallback(EntityCQRDwarf entitylivingbaseIn, float partialTickTime) {
+		if(entitylivingbaseIn != null) {
+			super.preRenderCallback(entitylivingbaseIn, partialTickTime);
+			
+			GL11.glScalef(0.9F, 0.65F, 0.9F);
+		}
+		
+	}
 
 }
--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRPigman.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRPigman.java
@@ -1,11 +1,17 @@
 package com.teamcqr.chocolatequestrepoured.client.render.entity;
 
+import org.lwjgl.opengl.GL11;
+
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRPigman;
 import com.teamcqr.chocolatequestrepoured.util.Reference;
 
 import net.minecraft.client.model.ModelZombie;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerArrow;
+import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
+import net.minecraft.client.renderer.entity.layers.LayerElytra;
+import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
 import net.minecraft.util.ResourceLocation;
 
 public class RenderCQRPigman extends RenderLiving<EntityCQRPigman> {
@@ -16,6 +22,11 @@ public class RenderCQRPigman extends RenderLiving<EntityCQRPigman> {
 	    {
 	        //Using the vanilla model - if we wanted to use a custom model this is where we could insert that instead
 	        super(manager, new ModelZombie(), 0.5F);
+	        
+	        this.addLayer(new LayerBipedArmor(this));
+	        this.addLayer(new LayerHeldItem(this));
+	        this.addLayer(new LayerArrow(this));
+	        this.addLayer(new LayerElytra(this));
 	    }
 	 
 	@Override
@@ -27,5 +38,14 @@ public class RenderCQRPigman extends RenderLiving<EntityCQRPigman> {
 	protected void applyRotations(EntityCQRPigman entityLiving, float p_77043_2_, float rotationYaw, float partialTicks) {
         super.applyRotations(entityLiving, p_77043_2_, rotationYaw, partialTicks);
     }
+	
+	@Override
+	protected void preRenderCallback(EntityCQRPigman entitylivingbaseIn, float partialTickTime) {
+		if(entitylivingbaseIn != null) {
+			super.preRenderCallback(entitylivingbaseIn, partialTickTime);
+			
+			GL11.glScalef(1.2F, 1.1F, 1.2F);
+		}
+	}
 
 }
