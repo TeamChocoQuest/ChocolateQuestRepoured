651b05a6f7952324a09d18ccc1b15820914aa672
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-08-01 17:51:03 +0200

    fixed threading

--- src/main/java/de/DerToaster/SimpleThreading/SimpleThread_BROKEN.java
+++ src/main/java/de/DerToaster/SimpleThreading/SimpleThread_BROKEN.java
@@ -4,7 +4,10 @@ import java.util.Queue;
 //DONE: Replace current queue with a queue that is thread safe!!! This will make the whole thing faster as it must not wait for the SimpleThread to unlock its queue
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-class SimpleThread extends Thread {
+class SimpleThread_BROKEN extends Thread {
+	
+	private int rerunsTillStop = 20;
+	private boolean running = false;
 
 	private Thread mainThread;
 	
@@ -12,7 +15,7 @@ class SimpleThread extends Thread {
 	
 	private boolean queueLocked = true;
 	
-	public SimpleThread(boolean async) {
+	public SimpleThread_BROKEN(boolean async) {
 		this.mainThread = Thread.currentThread();
 		
 		this.setDaemon(async);
@@ -21,25 +24,32 @@ class SimpleThread extends Thread {
 	}
 	
 	public void addTask(Runnable task) {
+		//System.out.println("Adding task to CQ-Thread: " + this.getName() + "...");
+		
 		lockQueue();
 		
-		boolean runAfterAdd = false;
+		/*boolean runAfterAdd = false;
 		if(this.tasks.isEmpty()) {
 			runAfterAdd = true;
-		}
+		}*/
 		
 		tasks.add(task);
 		
 		unlockQueue();
 		
-		if(runAfterAdd) {
-			this.run();
+		if(!this.running) {
+			this.start();
 		}
+		
+		/*if(runAfterAdd) {
+			this.run();
+		}*/
 	}
 	
 	@Override
-	public synchronized void start() {
+	public void start() {
 		if(!this.tasks.isEmpty()) {
+			this.running = true;
 			run();
 		}
 	}
@@ -59,6 +69,20 @@ class SimpleThread extends Thread {
 				e.printStackTrace();
 			}*/
 			this.run();
+		} else {
+			try {
+				sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			/*this.rerunsTillStop--;
+			if(this.rerunsTillStop <= 0) {
+				this.running = false;
+				this.rerunsTillStop = 20;
+			} else {*/
+				//System.out.println("CQ Thread: " + this.getName() + ": Remaing tasks: " + this.tasks.size());
+				this.run();
+			//}
 		}
 	}
 	
