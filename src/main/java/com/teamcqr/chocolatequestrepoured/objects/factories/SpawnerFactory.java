541a6ed5f0384701071dad77ab2f351b9df09594
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-08-26 15:19:50 +0200

    began spawner factory

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/factories/SpawnerFactory.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/factories/SpawnerFactory.java
@@ -1,10 +1,26 @@
 package com.teamcqr.chocolatequestrepoured.objects.factories;
 
+import javax.annotation.Nullable;
 import javax.swing.text.html.parser.Entity;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public abstract class SpawnerFactory {
 
-	public static void placeSpawnerForMob(Entity entity, boolean multiUseSpawner) {
+	public static void placeSpawnerForMob(Entity entity, boolean multiUseSpawner, @Nullable MultiUseSpawnerSettings spawnerSettings) {
+		
+	}
+	
+	public static void placeSpawnerForMobs(Entity[] entities, boolean multiUseSpawner, @Nullable MultiUseSpawnerSettings spawnerSettings) {
+		
+	}
+	
+	public static void convertCQSpawnerToVanillaSpawner(World world, BlockPos pos, MultiUseSpawnerSettings spawnerSettings) {
+		
+	}
+	
+	public static void convertVanillaSpawnerToCQSpawner(World world, BlockPos pos) {
 		
 	}
 	
