1c0af8af4f244f555650774bfc052174fd20031d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-08-27 12:21:48 +0200

    rearranged packages

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/AbandonedGenerator.java
@@ -0,0 +1,49 @@
+package com.teamcqr.chocolatequestrepoured.structuregen.generators;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+
+/**
+ * Copyright (c) 29.04.2019
+ * Developed by DerToaster98
+ * GitHub: https://github.com/DerToaster98
+ */
+public class AbandonedGenerator implements IDungeonGenerator{
+
+	@Override
+	public void preProcess(World world, Chunk chunk, int x, int y, int z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buildStructure(World world, Chunk chunk, int x, int y, int z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postProcess(World world, Chunk chunk, int x, int y, int z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fillChests(World world, Chunk chunk, int x, int y, int z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void placeSpawners(World world, Chunk chunk, int x, int y, int z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void placeCoverBlocks(World world, Chunk chunk, int x, int y, int z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/DefaultGenerator.java
@@ -0,0 +1,44 @@
+package com.teamcqr.chocolatequestrepoured.structuregen.generators;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+
+/**
+ * Copyright (c) 29.04.2019
+ * Developed by DerToaster98
+ * GitHub: https://github.com/DerToaster98
+ */
+public class DefaultGenerator implements IDungeonGenerator{
+	//This class has no actual use, it just floats around here for no reason, maybe because it can
+
+	@Override
+	public void preProcess(World world, Chunk chunk, int x, int y, int z) {
+		
+	}
+
+	@Override
+	public void buildStructure(World world, Chunk chunk, int x, int y, int z) {
+		
+	}
+
+	@Override
+	public void postProcess(World world, Chunk chunk, int x, int y, int z) {
+		
+	}
+
+	@Override
+	public void fillChests(World world, Chunk chunk, int x, int y, int z) {
+		
+	}
+
+	@Override
+	public void placeSpawners(World world, Chunk chunk, int x, int y, int z) {
+		
+	}
+
+	@Override
+	public void placeCoverBlocks(World world, Chunk chunk, int x, int y, int z) {
+		
+	}
+
+}
