96e27d49a467dfff94a96b50e3049f5e39c90665
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-08-30 12:57:10 +0200

    removed dev comments

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/tileentity/TileEntityExporterRenderer.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/tileentity/TileEntityExporterRenderer.java
@@ -14,47 +14,10 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class TileEntityExporterRenderer extends TileEntitySpecialRenderer<TileEntityExporter> {
 
-	// Code: see as how the TileEntityStructureRenderer does this
-	/**
-	 * //TODO: Use Tesselator to draw a box that contains the structure (Box texture
-	 * like worldborder, but not that bold?)
-	 * //https://www.minecraftforge.net/forum/topic/66168-1122-using-minecrafts-tessellator-and-bufferbuilder/
-	 * //https://www.minecraftforge.net/forum/topic/41255-question-regarding-the-vertexbuffer-and-the-old-tesselator/
-	 * //http://jabelarminecraft.blogspot.com/p/minecraft-forge-172-quick-tips-gl11-and.html
-	 * 
-	 * Tessellator tessellator = Tessellator.getInstance(); BufferBuilder
-	 * bufferBuilder = tessellator.getBuffer();
-	 * 
-	 * GlStateManager.disableTexture2D(); GlStateManager.disableBlend();
-	 * 
-	 * GlStateManager.glLineWidth(2.0F); bufferBuilder.begin(3,
-	 * DefaultVertexFormats.POSITION_COLOR); //Lines //Bottom lines
-	 * bufferBuilder.pos((float)startX, (float)startY, (float)startZ).color(255, 0,
-	 * 0, 0).endVertex(); bufferBuilder.pos((float)endX, (float)startY,
-	 * (float)startZ).color(255,0,0,0).endVertex();
-	 * 
-	 * bufferBuilder.pos((float)startX, (float)startY, (float)endZ).color(255, 0, 0,
-	 * 0).endVertex(); bufferBuilder.pos((float)endX, (float)startY,
-	 * (float)endZ).color(255,0,0,0).endVertex();
-	 * 
-	 * bufferBuilder.pos((float)startX, (float)startY, (float)startZ).color(255, 0,
-	 * 0, 0).endVertex(); bufferBuilder.pos((float)startX, (float)startY,
-	 * (float)endZ).color(255,0,0,0).endVertex();
-	 * 
-	 * 
-	 * tessellator.draw(); GlStateManager.glLineWidth(1.0f);
-	 * GlStateManager.enableTexture2D(); GlStateManager.enableBlend();
-	 */
-
 	@Override
 	public void render(TileEntityExporter te, double x, double y, double z, float partialTicks, int destroyStage,
 			float alpha) {
 		super.render(te, x, y, z, partialTicks, destroyStage, alpha);
-
-		/*if (te.startX == te.endX && te.startY == te.endY && te.startZ == te.endZ) {
-			// System.out.println("No Data on Client!");
-			return;
-		}*/
 
 		//Both positions are relative coordinates, the first one is the vector from the block's position to the lower corner of the selection
 		//The second position is a vector from the lower corner to the upper corner of the selection -> structures size!
