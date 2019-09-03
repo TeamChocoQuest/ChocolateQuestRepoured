efa659ae0af4626c90bbe1ed3b59858c440dd9b7
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-09-04 01:44:24 +0200

    Update EntityCQRZombie.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQRZombie.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQRZombie.java
@@ -3,6 +3,7 @@ package com.teamcqr.chocolatequestrepoured.objects.entity.mobs;
 import com.teamcqr.chocolatequestrepoured.factions.EFaction;
 import com.teamcqr.chocolatequestrepoured.objects.entity.EBaseHealths;
 
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
 public class EntityCQRZombie extends AbstractEntityCQR {
@@ -10,6 +11,11 @@ public class EntityCQRZombie extends AbstractEntityCQR {
 	public EntityCQRZombie(World worldIn) {
 		super(worldIn);
 		this.setSize(0.6F, 1.95F);
+	}
+
+	@Override
+	protected void setEquipmentBasedOnDifficulty(DifficultyInstance difficulty) {
+
 	}
 
 	@Override
