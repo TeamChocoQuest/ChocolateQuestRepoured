177f02b5c5c668e03b116f4d1da66145db0d962d
Meldexun <micha.kotlowski@gmx.de>
2019-09-04 00:01:30 +0200

    Add files via upload

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/InjectionUtil.java
@@ -0,0 +1,8 @@
+package com.teamcqr.chocolatequestrepoured.util;
+
+public class InjectionUtil {
+	@SuppressWarnings({"ConstantConditions", "SameReturnValue"})
+	public static <T> T Null() {
+		return null;
+	}
+}
