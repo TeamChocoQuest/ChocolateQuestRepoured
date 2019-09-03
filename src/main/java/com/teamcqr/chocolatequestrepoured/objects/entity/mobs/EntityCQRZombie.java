e54bcf5538e53369a04169167b82b53e37b43050
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-09-03 22:22:16 +0200

    Update EntityCQRZombie.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQRZombie.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQRZombie.java
@@ -1,69 +1,15 @@
 package com.teamcqr.chocolatequestrepoured.objects.entity.mobs;
 
-import java.util.UUID;
-
 import com.teamcqr.chocolatequestrepoured.factions.EFaction;
 import com.teamcqr.chocolatequestrepoured.objects.entity.EBaseHealths;
-import com.teamcqr.chocolatequestrepoured.objects.entity.ICQREntity;
-import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIMoveToHome;
-import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIMoveToLeader;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.attributes.IAttributeInstance;
-import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTUtil;
-import net.minecraft.pathfinding.PathNavigate;
-import net.minecraft.pathfinding.PathNavigateGround;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class EntityCQRZombie extends EntityZombie implements ICQREntity {
-
-	protected BlockPos home;
-	protected UUID leaderUUID;
+public class EntityCQRZombie extends AbstractEntityCQR {
 
 	public EntityCQRZombie(World worldIn) {
-        	super(worldIn);
-	}
-
-	@Override
-	public EFaction getFaction() {
-		return EFaction.UNDEAD;
-	}
-
-	@Override
-	public UUID getUUID() {
-		return getUniqueID();
-	}
-
-	@Override
-	public int getRemainingHealingPotions() {
-		return 0;
-	}
-
-	@Override
-	public boolean isBoss() {
-		return false;
-	}
-
-	@Override
-	public boolean isRideable() {
-		return false;
-	}
-
-	@Override
-	public boolean isFriendlyTowardsPlayer() {
-		return false;
-	}
-
-	@Override
-	public boolean hasFaction() {
-		return true;
+		super(worldIn);
+		this.setSize(0.6F, 1.95F);
 	}
 
 	@Override
@@ -72,130 +18,8 @@ public class EntityCQRZombie extends EntityZombie implements ICQREntity {
 	}
 
 	@Override
-	public void setPosition(double x, double y, double z) {
-		super.setPosition(x, y, z);
-		this.home = new BlockPos(x, y, z);
-		
-		spawnAt(new Double(x).intValue(), new Double(y).intValue(), new Double(z).intValue());
-	}
-
-	@Override
-	public void spawnAt(int x, int y, int z) {
-		if(getEntityWorld() != null && !getEntityWorld().isRemote) {
-			//sets the actual health
-			//changes the right attribute to apply
-			IAttributeInstance attribute = getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
-			float newHP = getBaseHealthForLocation(new BlockPos(x,y,z), this.getBaseHealth());
-			//System.out.println("New HP: " + newHP);
-			if(attribute != null) {
-				attribute.setBaseValue(newHP);
-				setHealth(getMaxHealth());
-			}
-			
-			//setPosition(x, y, z);
-		}
-	}
-
-	@Override
-	protected SoundEvent getAmbientSound() {
-		return super.getAmbientSound();
-	}
-
-	@Override
-	protected SoundEvent getHurtSound(DamageSource source) {
-		return super.getHurtSound(source);
-	}
-
-	@Override
-	protected SoundEvent getDeathSound() {
-		return super.getDeathSound();
-	}
-
-	@Override
-	public void onSpawnFromCQRSpawnerInDungeon(int x, int y, int z) {
-		if (!this.world.isRemote) {
-			IAttributeInstance attribute = getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
-			
-			if (attribute != null) {
-				float newHP = getBaseHealthForLocation(new BlockPos(x, y, z), this.getBaseHealth());
-				attribute.setBaseValue(newHP);
-				this.setHealth(newHP);
-			}
-			
-			this.home = new BlockPos(x, y, z);
-		}
-	}
-
-	@Override
-	public EntityLivingBase getLeader() {
-		for (Entity entity : this.world.loadedEntityList) {
-			if (entity instanceof EntityLivingBase && this.leaderUUID.equals(entity.getPersistentID())) {
-				return (EntityLivingBase) entity;
-			}
-		}
-		return null;
-	}
-
-	@Override
-	public void setLeader(EntityLivingBase leader) {
-		this.leaderUUID = leader.getPersistentID();
-	}
-
-	@Override
-	public BlockPos getHome() {
-		return this.home;
-	}
-
-	@Override
-	public void setHome(BlockPos home) {
-		this.home = home;
-	}
-
-	@Override
-	protected PathNavigate createNavigator(World worldIn) {
-		return new PathNavigateGround(this, worldIn) {
-			@Override
-			public float getPathSearchRange() {
-				return 128.0F;
-			}
-		};
-	}
-
-	@Override
-	protected void initEntityAI() {
-		super.initEntityAI();
-		this.tasks.addTask(3, new EntityAIMoveToHome(this));
-		this.tasks.addTask(4, new EntityAIMoveToLeader(this));
-	}
-
-	@Override
-	public void writeEntityToNBT(NBTTagCompound compound) {
-		super.writeEntityToNBT(compound);
-
-		boolean hasHome = this.home != null;
-		compound.setBoolean("hasHome", hasHome);
-		if (hasHome) {
-			compound.setTag("home", NBTUtil.createPosTag(this.home));
-		}
-
-		boolean hasLeader = this.leaderUUID != null;
-		compound.setBoolean("hasLeader", hasLeader);
-		if (hasLeader) {
-			compound.setTag("leader", NBTUtil.createUUIDTag(this.leaderUUID));
-		}
-	}
-
-	@Override
-	public void readEntityFromNBT(NBTTagCompound compound) {
-		super.readEntityFromNBT(compound);
-
-		if (compound.getBoolean("hasHome")) {
-			this.home = NBTUtil.getPosFromTag(compound.getCompoundTag("home"));
-		}
-
-		if (compound.getBoolean("hasLeader")) {
-			this.leaderUUID = NBTUtil.getUUIDFromTag(compound.getCompoundTag("leader"));
-		}
+	public EFaction getFaction() {
+		return EFaction.UNDEAD;
 	}
 
 }
