5874f30f7a312179d3a41dfdc30a7f64cd0b5314
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-09-05 14:16:26 +0200

    Add files via upload

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/armor/ItemBackpack.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/armor/ItemBackpack.java
@@ -8,7 +8,6 @@ import org.lwjgl.input.Keyboard;
 
 import com.teamcqr.chocolatequestrepoured.CQRMain;
 import com.teamcqr.chocolatequestrepoured.client.models.armor.ModelBackpack;
-import com.teamcqr.chocolatequestrepoured.objects.base.ArmorBase;
 import com.teamcqr.chocolatequestrepoured.util.Reference;
 
 import net.minecraft.client.model.ModelBiped;
@@ -29,78 +28,69 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemBackpack extends ArmorBase
-{
-	public ItemBackpack(String name, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) 
-	{
-		super(name, materialIn, renderIndexIn, equipmentSlotIn);
-	}
-	
-	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
-    {
-		ItemStack itemstack = playerIn.getHeldItem(handIn);
-        EntityEquipmentSlot entityequipmentslot = EntityLiving.getSlotForItemStack(itemstack);
-        ItemStack itemstack1 = playerIn.getItemStackFromSlot(entityequipmentslot);
+public class ItemBackpack extends ItemArmor {
 
-        if((itemstack1.isEmpty() && (playerIn.getHeldItemMainhand().getItem() != this)) || (itemstack1.isEmpty() && (playerIn.getHeldItemMainhand().getItem() == this && playerIn.isSneaking())))
-        {
-            playerIn.setItemStackToSlot(entityequipmentslot, itemstack.copy());
-            itemstack.setCount(0);
-            return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
-        }
-        if(!worldIn.isRemote)
-        {
-        	if(playerIn.getHeldItemMainhand().getItem() == this && !playerIn.isSneaking())
-        	{
-        		playerIn.openGui(CQRMain.INSTANCE, Reference.BACKPACK_GUI_ID, worldIn, playerIn.getPosition().getX(), playerIn.getPosition().getY(), playerIn.getPosition().getZ());
-        		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
-        	}
-        }
-        return new ActionResult<ItemStack>(EnumActionResult.FAIL, itemstack);    
-    }
-	
+	public ItemBackpack(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+		super(materialIn, renderIndexIn, equipmentSlotIn);
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		ItemStack itemstack = playerIn.getHeldItem(handIn);
+		EntityEquipmentSlot entityequipmentslot = EntityLiving.getSlotForItemStack(itemstack);
+		ItemStack itemstack1 = playerIn.getItemStackFromSlot(entityequipmentslot);
+
+		if ((itemstack1.isEmpty() && (playerIn.getHeldItemMainhand().getItem() != this)) || (itemstack1.isEmpty()
+				&& (playerIn.getHeldItemMainhand().getItem() == this && playerIn.isSneaking()))) {
+			playerIn.setItemStackToSlot(entityequipmentslot, itemstack.copy());
+			itemstack.setCount(0);
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
+		}
+		if (!worldIn.isRemote) {
+			if (playerIn.getHeldItemMainhand().getItem() == this && !playerIn.isSneaking()) {
+				playerIn.openGui(CQRMain.INSTANCE, Reference.BACKPACK_GUI_ID, worldIn, playerIn.getPosition().getX(),
+						playerIn.getPosition().getY(), playerIn.getPosition().getZ());
+				return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
+			}
+		}
+		return new ActionResult<ItemStack>(EnumActionResult.FAIL, itemstack);
+	}
+
 	@SideOnly(Side.CLIENT)
-    @Override
-    public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot, ModelBiped _default)
-    {
-		if(itemStack != null)
-		{
-			if(itemStack.getItem() instanceof ItemArmor)
-			{
+	@Override
+	public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot,
+			ModelBiped _default) {
+		if (itemStack != null) {
+			if (itemStack.getItem() instanceof ItemArmor) {
 				ModelBackpack model = new ModelBackpack();
-				
+
 				model.bipedBody.showModel = armorSlot == EntityEquipmentSlot.CHEST;
-				
-			    model.isChild = _default.isChild;
-			    model.isRiding = _default.isRiding;
-			    model.isSneak = _default.isSneak;
-			    model.rightArmPose = _default.rightArmPose;
-			    model.leftArmPose = _default.leftArmPose;
-			     
-			    return model;
+
+				model.isChild = _default.isChild;
+				model.isRiding = _default.isRiding;
+				model.isSneak = _default.isSneak;
+				model.rightArmPose = _default.rightArmPose;
+				model.leftArmPose = _default.leftArmPose;
+
+				return model;
 			}
 		}
 		return null;
-    }
-	
+	}
+
 	@Override
-	public String getArmorTexture(ItemStack stack, Entity entity, EntityEquipmentSlot slot, String type)
-	{
+	public String getArmorTexture(ItemStack stack, Entity entity, EntityEquipmentSlot slot, String type) {
 		return "cqrepoured:textures/models/armor/backpack_layer_1.png";
 	}
-	
+
 	@Override
 	@SideOnly(Side.CLIENT)
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
-    {
-		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT))
-		{
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
 			tooltip.add(TextFormatting.BLUE + I18n.format("description.backpack.name"));
-		}		
-		else
-		{
+		} else {
 			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
 		}
-    }
+	}
+
 }
