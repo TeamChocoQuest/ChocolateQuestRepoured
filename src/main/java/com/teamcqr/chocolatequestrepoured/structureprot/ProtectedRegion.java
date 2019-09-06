16179b649f9288cd946f9281f28da6ff36f3ae57
jdawg3636 <jdawg3636minecraft@gmail.com>
2019-09-06 18:25:04 -0500

    Renamed Protection Package Changed from 'dungeonprot' to 'structureprot' to match recent change from 'dungeongen' to 'structuregen'

--- src/main/java/com/teamcqr/chocolatequestrepoured/structureprot/ProtectedRegion.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structureprot/ProtectedRegion.java
@@ -1,4 +1,4 @@
-package com.teamcqr.chocolatequestrepoured.dungeonprot;
+package com.teamcqr.chocolatequestrepoured.structureprot;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
