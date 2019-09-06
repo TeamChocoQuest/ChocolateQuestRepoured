4820f45f6ab68a58f3ff68fa3dd55c6f8c2d739a
Meldexun <micha.kotlowski@gmx.de>
2019-09-06 14:58:23 +0200

    Update BlockExporter.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/blocks/BlockExporter.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/blocks/BlockExporter.java
@@ -19,9 +19,6 @@ import net.minecraft.world.World;
 
 public class BlockExporter extends Block implements ITileEntityProvider {
 
-	// DONE: rewrite as a container
-	// If that does not work, create a packet that tells the server to save the
-	// structure -> Saving bug fixed
 	public BlockExporter() {
 		super(Material.IRON);
 
@@ -33,13 +30,10 @@ public class BlockExporter extends Block implements ITileEntityProvider {
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
 			EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-		// DONE: Send data on server to client, otherwise it wont work
-
-		// When it is server side -> Request sync???
 		if (!worldIn.isRemote) {
-			((TileEntityExporter) worldIn.getTileEntity(pos)).requestSync();
+			playerIn.openGui(CQRMain.INSTANCE, Reference.EXPORTER_GUI_ID, worldIn, pos.getX(), pos.getY(), pos.getZ());
 		}
-		playerIn.openGui(CQRMain.INSTANCE, Reference.EXPORTER_GUI_ID, worldIn, pos.getX(), pos.getY(), pos.getZ());
+
 		return true;
 	}
 
