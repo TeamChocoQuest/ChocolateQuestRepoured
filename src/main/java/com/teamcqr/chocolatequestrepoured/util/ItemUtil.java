b3687b53d450f353e3dbf0ef80e80350a0ce47be
Meldexun <micha.kotlowski@gmx.de>
2019-09-06 03:26:30 +0200

    Update ItemUtil.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/ItemUtil.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/ItemUtil.java
@@ -1,19 +1,37 @@
 package com.teamcqr.chocolatequestrepoured.util;
 
-import net.minecraft.entity.player.EntityPlayer;
+import java.util.Iterator;
+
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 
 public class ItemUtil {
 
-	public static boolean isFullSet(EntityPlayer playerIn, ItemStack stack) {
-		Class itemClass = stack.getItem().getClass();
-		ItemStack helm = playerIn.inventory.armorInventory.get(3);
-		ItemStack chest = playerIn.inventory.armorInventory.get(2);
-		ItemStack legs = playerIn.inventory.armorInventory.get(1);
-		ItemStack feet = playerIn.inventory.armorInventory.get(0);
+	public static boolean hasFullSet(EntityLivingBase entity, Class itemClass) {
+		Iterator<ItemStack> iterable = entity.getArmorInventoryList().iterator();
+		Class helm = iterable.next().getItem().getClass();
+		Class chest = iterable.next().getItem().getClass();
+		Class legs = iterable.next().getItem().getClass();
+		Class feet = iterable.next().getItem().getClass();
 
-		return helm.getClass() == itemClass && chest.getClass() == itemClass && legs.getClass() == itemClass
-				&& feet.getClass() == itemClass;
+		return helm == itemClass && chest == itemClass && legs == itemClass && feet == itemClass;
+	}
+
+	public static boolean compareRotations(double yaw1, double yaw2, double maxDiff) {
+		double d = (yaw1 - maxDiff) % 360;
+		double d1 = (yaw1 + maxDiff) % 360;
+		double d2 = yaw2 % 360;
+		if (d < 0.0D) {
+			d += 360.0D;
+		}
+		if (d1 < 0.0D) {
+			d1 += 360.0D;
+		}
+		if (d2 < 0.0D) {
+			d2 += 360.0D;
+		}
+
+		return d2 > d && d2 < d1;
 	}
 
 }
