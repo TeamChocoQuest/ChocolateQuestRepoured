2320456a3c66d1490de305543fc8d40139d1bda8
Meldexun <micha.kotlowski@gmx.de>
2019-09-07 20:01:14 +0200

    Add files via upload

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQRDummy.java
@@ -0,0 +1,35 @@
+package com.teamcqr.chocolatequestrepoured.objects.entity.mobs;
+
+import com.teamcqr.chocolatequestrepoured.factions.EFaction;
+
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.World;
+
+public class EntityCQRDummy extends AbstractEntityCQR {
+
+	public EntityCQRDummy(World worldIn) {
+		super(worldIn);
+		this.setHealingPotions(0);
+	}
+
+	@Override
+	protected void setEquipmentBasedOnDifficulty(DifficultyInstance difficulty) {
+
+	}
+
+	@Override
+	public float getBaseHealth() {
+		return 1;
+	}
+
+	@Override
+	public EFaction getFaction() {
+		return null;
+	}
+
+	@Override
+	protected void initEntityAI() {
+
+	}
+
+}
