1e0fb8680b396215c17f18d52af4c63e75204c8f
jdawg3636 <jdawg3636minecraft@gmail.com>
2019-09-06 20:41:03 -0500

    (Mostly) Finished ProtectionHandler Implementation
    ProtectionHandler now implements all the things (Block/Entity Dependencies not currently testable bc not implemented elsewhere in code)
    ProtectedRegion constructor no longer flips its shit when passed null values for lists/maps
    Fixed ArrayCollectionMapUtil's genericAddValueToArrayish method

--- src/main/java/com/teamcqr/chocolatequestrepoured/structureprot/ProtectionHandler.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structureprot/ProtectionHandler.java
@@ -7,10 +7,12 @@ import com.teamcqr.chocolatequestrepoured.API.events.CQDungeonStructureGenerateE
 import com.teamcqr.chocolatequestrepoured.intrusive.IntrusiveModificationHelper;
 
 import com.teamcqr.chocolatequestrepoured.util.data.ArchiveManipulationUtil;
+import com.teamcqr.chocolatequestrepoured.util.data.ArrayCollectionMapManipulationUtil;
 import com.teamcqr.chocolatequestrepoured.util.data.FileIOUtil;
 import com.teamcqr.chocolatequestrepoured.util.data.ObjectSerializationUtil;
 import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.ExplosionEvent;
@@ -40,15 +42,11 @@ public class ProtectionHandler {
     // Region Data
     private HashMap<Integer, ArrayList<ProtectedRegion>> activeRegions;
 
-    /* ************* */
-    /* Serialization */
-    /* ************* */
-
     // ProtectedRegion Serialization
     @SubscribeEvent
     public void eventHandleDungeonSpawn(CQDungeonStructureGenerateEvent e) {
         // Create ProtectedRegion obj
-        ProtectedRegion regionToBeRegistered = new ProtectedRegion(e.getDungeonID().toString(), e.getPos(), new BlockPos(e.getPos().getX() + e.getSize().getX(), e.getPos().getY() + e.getSize().getY(), e.getPos().getZ() + e.getSize().getZ()));
+        ProtectedRegion regionToBeRegistered = new ProtectedRegion(e.getDungeonID().toString(), e.getPos(), new BlockPos(e.getPos().getX() + e.getSize().getX(), e.getPos().getY() + e.getSize().getY(), e.getPos().getZ() + e.getSize().getZ()), null, (ArrayList<BlockPos>)ArrayCollectionMapManipulationUtil.genericAddValueToArrayish(new ArrayList<BlockPos>(), e.getShieldCorePosition(), null), null);
         int dimID = e.getWorld().provider.getDimension();
         // Register in Memory
         ArrayList<ProtectedRegion> updated = activeRegions.get(dimID);
@@ -66,10 +64,11 @@ public class ProtectionHandler {
     // ProtectedRegion Deserialization
     @SubscribeEvent
     public void eventHandleWorldLoad(WorldEvent.Load e) {
-        HashMap<String, byte[]> protectedRegionsFromDisc = ArchiveManipulationUtil.unzip(FileIOUtil.loadFromFile(FileIOUtil.getAbsoluteWorldPath() + "prot_region_defs.zip"));
+        HashMap<String, byte[]> protectedRegionsFromDisc = ArchiveManipulationUtil.unzip(FileIOUtil.loadFromFile(FileIOUtil.getAbsoluteWorldPath() + "data\\CQR\\prot_region_defs.zip"));
         if(protectedRegionsFromDisc != null) {
             for(String regionFileName : protectedRegionsFromDisc.keySet()) {
                 ArrayList<ProtectedRegion> temp = activeRegions.get(Integer.parseInt(regionFileName.substring(4,5)));
+                if(temp == null) temp = new ArrayList<>();
                 temp.add((ProtectedRegion)ObjectSerializationUtil.readObectFromByteArray(protectedRegionsFromDisc.get(regionFileName)));
                 activeRegions.put(Integer.parseInt(regionFileName.substring(4,5)), temp);
             }
@@ -80,19 +79,52 @@ public class ProtectionHandler {
     /* Event Handling */
     /* ************** */
 
-    // TODO Add checking for entity dependency death
+    @SubscribeEvent
+    public void eventHandleEntityDeath(LivingDeathEvent e) {
+        // Loop through all dims present in registry
+        for(int dimID : activeRegions.keySet()) {
+            // Temp var
+            ArrayList<ProtectedRegion> toRemoveFromRegistry = new ArrayList<>();
+            // Loop through all registered regions for dim
+            for( ProtectedRegion region : activeRegions.get(dimID) ) {
+                for(BlockPos pos : region.getBlockDependencies()) {
+                    if(pos.equals(e.getEntityLiving().getPosition())) {
+                        toRemoveFromRegistry.add(region);
+                    }
+                }
+            }
+            // Remove flagged regions from registry
+            ArrayList<ProtectedRegion> updated = activeRegions.get(dimID);
+            for(ProtectedRegion pr : toRemoveFromRegistry) {
+                updated.remove(pr);
+            }
+        }
+    }
 
     @SubscribeEvent
     public void eventHandleBlockBreak(BlockEvent.BreakEvent e) {
 
-        // TODO Add checking for block dependency destruction
-
         // Loop through all dims present in registry
         for(int dimID : activeRegions.keySet()) {
+            ArrayList<ProtectedRegion> toRemoveFromRegistry = new ArrayList<>();
             // Loop through all registered regions for dim
             for( ProtectedRegion region : activeRegions.get(dimID) ) {
+
+                // Check if block is a dependency
+                boolean isBlockDependency = false;
+                for(BlockPos pos : region.getBlockDependencies()) {
+                    if(e.getPos().equals(pos)) {
+                        isBlockDependency = true;
+                        toRemoveFromRegistry.add(region);
+                    }
+                }
+
+                // Check if already handled
+                if(isBlockDependency) {
+                    // noop
+                }
                 // Noop if global setting disabled
-                if(!region.settings.get("preventBlockBreak")) {
+                else if(!region.settings.get("preventBlockBreak")) {
                     // noop
                 }
                 // Noop if in creative mode and specific setting disabled
@@ -108,6 +140,10 @@ public class ProtectionHandler {
                     e.setCanceled(true);
                 }
             }
+            // Remove flagged regions
+            ArrayList<ProtectedRegion> updated = activeRegions.get(dimID);
+            for(ProtectedRegion pr : toRemoveFromRegistry) updated.remove(pr);
+            activeRegions.put(dimID, updated);
         }
 
     }
@@ -190,8 +226,27 @@ public class ProtectionHandler {
     }
 
     @SubscribeEvent
-    public void eventHandleNaturalSpawn(LivingSpawnEvent.CheckSpawn e) {
-        // TODO Implement - previous attempt unsuccessful, will likely require subscription to a different event
+    public void eventHandleNaturalSpawn(LivingSpawnEvent.SpecialSpawn e) {
+
+        // Loop through all dims present in registry
+        for(int dimID : activeRegions.keySet()) {
+            // Loop through all registered regions for dim
+            for( ProtectedRegion region : activeRegions.get(dimID) ) {
+                // Noop if setting disabled
+                if(!region.settings.get("preventNaturalMobSpawn")) {
+                    // noop
+                }
+                // Noop if different dim
+                else if(dimID != e.getWorld().provider.getDimension()) {
+                    // noop
+                }
+                // Otherwise check block pos and cancel if overlapping
+                else if(region.checkIfBlockPosInRegion(new BlockPos(e.getX(), e.getY(), e.getZ()))) {
+                    e.setCanceled(true);
+                }
+            }
+        }
+
     }
 
 }
