bf9ab6493cee428c152cacc1c6fdbd40c764ed33
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-09-07 19:51:17 +0200

    Update AbstractEntityCQR.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/AbstractEntityCQR.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/AbstractEntityCQR.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import javax.annotation.Nullable;
 
 import com.teamcqr.chocolatequestrepoured.factions.EFaction;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIHealingPotion;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIMoveToHome;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIMoveToLeader;
 import com.teamcqr.chocolatequestrepoured.util.Reference;
@@ -24,7 +25,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTUtil;
 import net.minecraft.pathfinding.PathNavigate;
-import net.minecraft.pathfinding.PathNavigateGround;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -77,9 +77,10 @@ public abstract class AbstractEntityCQR extends EntityMob {
 	@Override
 	protected void initEntityAI() {
 		this.tasks.addTask(0, new EntityAISwimming(this));
-		this.tasks.addTask(5, new EntityAIAttackMelee(this, 1.0D, false));
-		this.tasks.addTask(10, new EntityAIMoveToLeader(this));
-		this.tasks.addTask(15, new EntityAIMoveToHome(this));
+		this.tasks.addTask(5, new EntityAIHealingPotion(this));
+		this.tasks.addTask(10, new EntityAIAttackMelee(this, 1.0D, false));
+		this.tasks.addTask(15, new EntityAIMoveToLeader(this));
+		this.tasks.addTask(20, new EntityAIMoveToHome(this));
 
 		this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
 	}
@@ -219,8 +220,16 @@ public abstract class AbstractEntityCQR extends EntityMob {
 		}
 	}
 
-	public int getRemainingPotions() {
+	public int getHealingPotions() {
 		return this.healingPotions;
+	}
+
+	public void setHealingPotions(int amount) {
+		this.healingPotions = amount;
+	}
+
+	public void removeHealingPotion() {
+		this.healingPotions--;
 	}
 
 	public abstract EFaction getFaction();
@@ -256,10 +265,6 @@ public abstract class AbstractEntityCQR extends EntityMob {
 	public void onSpawnFromCQRSpawnerInDungeon() {
 		this.setHomePosition(this.getPosition());
 		this.setBaseHealthForPosition(this.posX, this.posZ, this.getBaseHealth());
-	}
-
-	public void removePotion() {
-		healingPotions--;
 	}
 	
 }
