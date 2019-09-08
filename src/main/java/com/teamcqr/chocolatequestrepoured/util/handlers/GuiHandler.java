99bc816e08ba34b1c1320175f448bd292aa57eb3
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-09-08 20:33:22 +0200

    Update GuiHandler.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/handlers/GuiHandler.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/handlers/GuiHandler.java
@@ -3,19 +3,27 @@ package com.teamcqr.chocolatequestrepoured.util.handlers;
 import com.teamcqr.chocolatequestrepoured.gui.GuiAlchemyBag;
 import com.teamcqr.chocolatequestrepoured.gui.GuiBackpack;
 import com.teamcqr.chocolatequestrepoured.gui.GuiBadge;
+import com.teamcqr.chocolatequestrepoured.gui.GuiCQREntity;
 import com.teamcqr.chocolatequestrepoured.gui.GuiExporter;
 import com.teamcqr.chocolatequestrepoured.gui.GuiSpawner;
-import com.teamcqr.chocolatequestrepoured.gui.container.*;
+import com.teamcqr.chocolatequestrepoured.gui.container.ContainerAlchemyBag;
+import com.teamcqr.chocolatequestrepoured.gui.container.ContainerBackpack;
+import com.teamcqr.chocolatequestrepoured.gui.container.ContainerBadge;
+import com.teamcqr.chocolatequestrepoured.gui.container.ContainerCQREntity;
+import com.teamcqr.chocolatequestrepoured.gui.container.ContainerExporter;
+import com.teamcqr.chocolatequestrepoured.gui.container.ContainerSpawner;
 import com.teamcqr.chocolatequestrepoured.gui.inventory.InventoryAlchemyBag;
 import com.teamcqr.chocolatequestrepoured.gui.inventory.InventoryBackpack;
 import com.teamcqr.chocolatequestrepoured.gui.inventory.InventoryBadge;
 import com.teamcqr.chocolatequestrepoured.init.ModItems;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.AbstractEntityCQR;
 import com.teamcqr.chocolatequestrepoured.objects.items.ItemBadge;
 import com.teamcqr.chocolatequestrepoured.tileentity.TileEntityExporter;
 import com.teamcqr.chocolatequestrepoured.tileentity.TileEntitySpawner;
 import com.teamcqr.chocolatequestrepoured.util.Reference;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
@@ -23,7 +31,7 @@ import net.minecraftforge.fml.common.network.IGuiHandler;
 public class GuiHandler implements IGuiHandler 
 {
 	@Override
-	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) 
+	public Container getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) 
 	{
 		if(ID == Reference.BADGE_GUI_ID)
 		{
@@ -48,6 +56,11 @@ public class GuiHandler implements IGuiHandler
 		if(ID == Reference.EXPORTER_GUI_ID)
 		{
 			return new ContainerExporter((TileEntityExporter)world.getTileEntity(new BlockPos(x,y,z)));
+		}
+
+		if(ID == Reference.CQR_ENTITY_GUI_ID)
+		{
+			return new ContainerCQREntity(player.inventory, (AbstractEntityCQR) world.getEntityByID(x));
 		}
 		
 		return null;
@@ -79,6 +92,11 @@ public class GuiHandler implements IGuiHandler
 		if(ID == Reference.ALCHEMY_BAG_GUI_ID && player.getHeldItemMainhand().getItem() == ModItems.ALCHEMY_BAG)
 		{
 			return new GuiAlchemyBag(player.inventory, new InventoryAlchemyBag(player.getHeldItemMainhand()));
+		}
+		
+		if(ID == Reference.CQR_ENTITY_GUI_ID)
+		{
+			return new GuiCQREntity(this.getServerGuiElement(ID, player, world, x, y, z), (AbstractEntityCQR) world.getEntityByID(x));
 		}
 		
 		return null;
