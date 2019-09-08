26b10c3d0303ae00ba6e150d06676d9853e03b36
Meldexun <micha.kotlowski@gmx.de>
2019-09-09 01:30:27 +0200

    Update ItemBadge.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/ItemBadge.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/ItemBadge.java
@@ -1,78 +1,58 @@
 package com.teamcqr.chocolatequestrepoured.objects.items;
 
 import java.util.List;
-import java.util.UUID;
 
 import javax.annotation.Nullable;
 
 import org.lwjgl.input.Keyboard;
 
 import com.teamcqr.chocolatequestrepoured.CQRMain;
+import com.teamcqr.chocolatequestrepoured.capability.itemhandler.CapabilityItemStackHandlerProvider;
 import com.teamcqr.chocolatequestrepoured.util.Reference;
 
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemBadge extends Item
-{
-	public static UUID EntityUUID;
-	
-	public ItemBadge() 
-	{
-		setMaxStackSize(1);
+public class ItemBadge extends Item {
+
+	public ItemBadge() {
+		this.setMaxStackSize(1);
 	}
-	
+
 	@Override
-	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity)
-    {
-		 if(player.capabilities.isCreativeMode && !player.world.isRemote) 
-		 {
-			 this.setEntityUUID(entity);
-			 player.openGui(CQRMain.INSTANCE, Reference.BADGE_GUI_ID, player.world, player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ());
-		 }
-		 return true;
-    }
-	
-	public void setEntityUUID(Entity entity)
-	{
-		if(entity != null)
-		{
-			UUID uuid = entity.getUniqueID();
-			EntityUUID = uuid;
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		if (!worldIn.isRemote) {
+			playerIn.openGui(CQRMain.INSTANCE, Reference.BADGE_GUI_ID, worldIn, handIn.ordinal(), 0, 0);
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
 		}
+		return new ActionResult<ItemStack>(EnumActionResult.FAIL, playerIn.getHeldItem(handIn));
 	}
-	
-	public static Entity getEntityByUniqueUUID(World world)
-	{
-		for(Entity entity : world.loadedEntityList)
-		{
-			if(entity.getUniqueID() == EntityUUID)
-			{
-				return entity;
-			}
-		}
-		return null;
-	}
-	
+
 	@Override
 	@SideOnly(Side.CLIENT)
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
-    {
-		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT))
-		{
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
 			tooltip.add(TextFormatting.BLUE + I18n.format("description.badge.name"));
-		}		
-		else
-		{
+		} else {
 			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
 		}
-    }
+	}
+
+	@Override
+	public ICapabilityProvider initCapabilities(ItemStack stack, NBTTagCompound nbt) {
+		return new CapabilityItemStackHandlerProvider(9);
+	}
+
 }
