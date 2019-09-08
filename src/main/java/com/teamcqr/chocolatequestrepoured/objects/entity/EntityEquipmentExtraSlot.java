318323c51e66abb0cf60fa884c92cc35d01a3839
Meldexun <micha.kotlowski@gmx.de>
2019-09-08 20:38:22 +0200

    Add files via upload

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/EntityEquipmentExtraSlot.java
@@ -0,0 +1,17 @@
+package com.teamcqr.chocolatequestrepoured.objects.entity;
+
+public enum EntityEquipmentExtraSlot {
+
+	PotionSlot(0), BadgeSlot(1);
+
+	private final int index;
+
+	private EntityEquipmentExtraSlot(int index) {
+		this.index = index;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+}
