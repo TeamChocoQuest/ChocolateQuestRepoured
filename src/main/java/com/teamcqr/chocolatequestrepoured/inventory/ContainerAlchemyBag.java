77c6e106bb98380a7248a5205f2e64e475b51a30
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-09-09 01:23:01 +0200

    Add files via upload

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/inventory/ContainerAlchemyBag.java
@@ -0,0 +1,83 @@
+package com.teamcqr.chocolatequestrepoured.inventory;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
+
+public class ContainerAlchemyBag extends Container {
+
+	private final int numRows = 1;
+	private final int numColumns = 5;
+
+	public ContainerAlchemyBag(InventoryPlayer playerInv, ItemStack stack) {
+		IItemHandler inventory = stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+		int currentItemIndex = playerInv.currentItem;
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 9; j++) {
+				this.addSlotToContainer(new Slot(playerInv, j + i * 9 + 9, 8 + j * 18, 51 + i * 18));
+			}
+		}
+
+		for (int k = 0; k < 9; k++) {
+			if (k != currentItemIndex) {
+				this.addSlotToContainer(new Slot(playerInv, k, 8 + k * 18, 109));
+			} else {
+				this.addSlotToContainer(new Slot(playerInv, k, 8 + k * 18, 109) {
+					@Override
+					public boolean canTakeStack(EntityPlayer playerIn) {
+						return false;
+					}
+				});
+			}
+		}
+
+		for (int l = 0; l < this.numColumns; l++) {
+			this.addSlotToContainer(new SlotItemHandler(inventory, l, 44 + l * 18, 20) {
+				@Override
+				public boolean isItemValid(ItemStack stack) {
+					return stack.getItem() == Items.SPLASH_POTION;
+				}
+			});
+		}
+	}
+
+	@Override
+	public boolean canInteractWith(EntityPlayer playerIn) {
+		return true;
+	}
+
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
+		ItemStack itemstack = ItemStack.EMPTY;
+		Slot slot = this.inventorySlots.get(index);
+
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+
+			if (index < this.numRows * 9) {
+				if (!this.mergeItemStack(itemstack1, this.numRows * 9, this.inventorySlots.size(), true)) {
+					return ItemStack.EMPTY;
+				}
+			} else if (!this.mergeItemStack(itemstack1, 0, this.numRows * 9, false)) {
+				return ItemStack.EMPTY;
+			}
+
+			if (itemstack1.isEmpty()) {
+				slot.putStack(ItemStack.EMPTY);
+			} else {
+				slot.onSlotChanged();
+			}
+		}
+
+		return itemstack;
+	}
+
+}
