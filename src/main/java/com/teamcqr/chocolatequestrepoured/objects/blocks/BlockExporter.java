bb9526e2b909407539af8c9a54e3804b3fce8791
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-09-09 19:14:04 +0200

    Update BlockExporter.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/blocks/BlockExporter.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/blocks/BlockExporter.java
@@ -1,14 +1,14 @@
 package com.teamcqr.chocolatequestrepoured.objects.blocks;
 
-import com.teamcqr.chocolatequestrepoured.client.gui.GuiExporter;
+import com.teamcqr.chocolatequestrepoured.CQRMain;
 import com.teamcqr.chocolatequestrepoured.tileentity.TileEntityExporter;
+import com.teamcqr.chocolatequestrepoured.util.Reference;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
@@ -31,7 +31,7 @@ public class BlockExporter extends Block implements ITileEntityProvider {
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
 			EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
 		if (worldIn.isRemote) {
-			Minecraft.getMinecraft().displayGuiScreen(new GuiExporter((TileEntityExporter) worldIn.getTileEntity(pos)));
+			playerIn.openGui(CQRMain.INSTANCE, Reference.EXPORTER_GUI_ID, worldIn, pos.getX(), pos.getY(), pos.getZ());
 		}
 
 		return true;
