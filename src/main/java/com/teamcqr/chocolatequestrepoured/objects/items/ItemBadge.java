757a2d6e24286300419a10bb367e966fcd6e6400
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-09-10 00:32:49 +0200

    Revert "Update ItemBadge.java"
    This reverts commit fed27f7d8fb062c24b4e43140813b099e90d3d7f.

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/ItemBadge.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/ItemBadge.java
@@ -16,23 +16,25 @@ import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class ItemBadge extends Item {
 
@@ -46,7 +48,6 @@ public class ItemBadge extends Item {
 			if (!player.world.isRemote) {
 				if (entity instanceof AbstractEntityCQR) {
 					((AbstractEntityCQR) entity).setItemStackToExtraSlot(EntityEquipmentExtraSlot.BadgeSlot, stack.copy());
-					((WorldServer) player.world).spawnParticle((EntityPlayerMP) player, EnumParticleTypes.SPELL_WITCH, false, entity.posX, entity.posY + 0.5D, entity.posZ, 8, 0.5D, 0.5D, 0.5D, 0.1D);
 				} else {
 					IItemHandler capability = stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
 					NBTTagList itemList = new NBTTagList();
@@ -57,10 +58,7 @@ public class ItemBadge extends Item {
 						stack1.writeToNBT(slotTag);
 						itemList.appendTag(slotTag);
 					}
-					if (!itemList.hasNoTags()) {
-						entity.getEntityData().setTag("Items", itemList);
-						((WorldServer) player.world).spawnParticle((EntityPlayerMP) player, EnumParticleTypes.SPELL_WITCH, false, entity.posX, entity.posY + 0.5D, entity.posZ, 8, 0.5D, 0.5D, 0.5D, 0.1D);
-					}
+					entity.getEntityData().setTag("Items", itemList);
 				}
 			}
 			return true;
