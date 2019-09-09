b15855e6e4e13fb6aa6efc38a18bb34689b4ec9b
Nico Mexis <nico.mexis@kabelmail.de>
2019-09-09 18:17:22 +0200

    Fixed Proxy errors

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/Reference.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/Reference.java
@@ -9,7 +9,7 @@ public class Reference
 	public static final String VERSION = "0.1.2";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.teamcqr.chocolatequestrepoured.proxy.ClientProxy";
-	public static final String COMMON_PROXY_CLASS = "com.teamcqr.chocolatequestrepoured.proxy.CommonProxy";
+	public static final String COMMON_PROXY_CLASS = "com.teamcqr.chocolatequestrepoured.proxy.ServerProxy";
 	public static final String DUNGEON_REGISTRY = "com.teamcqr.chocolatequestrepoured.structuregen.DungeonRegistry";
 	public static final String DUNGEON_TERRAIN_POPULATOR = "com.teamcqr.chocolatequestrepoured.structuregen.WorldDungeonGenerator";
 	public static final String WALL_TERRAIN_POPULATOR = "com.teamcqr.chocolatequestrepoured.structuregen.thewall.WorldWallGenerator";
