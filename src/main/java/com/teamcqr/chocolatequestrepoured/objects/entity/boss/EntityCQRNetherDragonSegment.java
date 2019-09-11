d6e2b9dd2c8e49df73dce25750294bdab90ea775
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-09-11 22:09:08 +0200

    alligning dragon again

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRNetherDragonSegment.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRNetherDragonSegment.java
@@ -7,9 +7,14 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 
 public class EntityCQRNetherDragonSegment extends MultiPartEntityPart {
+	
+	//TODO: Add the tail peek
 
 	private final EntityCQRNetherDragon dragon;
 	private final int partID;
+	
+	private boolean tailPeek = false;
+	private int tailPart = -1;
 	
 	public EntityCQRNetherDragonSegment(EntityCQRNetherDragon dragon, int partID) {
 		super((IEntityMultiPart)dragon, "dragonPart" + partID, 0.5F, 0.5F);
@@ -18,6 +23,14 @@ public class EntityCQRNetherDragonSegment extends MultiPartEntityPart {
 		
 		this.dragon = dragon;
 		this.partID = partID;
+		
+		if(partID > EntityCQRNetherDragon.SEGMENT_COUNT -3) {
+			this.tailPeek = true;
+			
+			this.tailPart = EntityCQRNetherDragon.SEGMENT_COUNT - this.partID +1;
+			System.out.println("part id: " + this.tailPart);
+		}
+		
 		//String partName, float width, float height
 		this.setInvisible(false);
 	}
@@ -53,6 +66,14 @@ public class EntityCQRNetherDragonSegment extends MultiPartEntityPart {
 	@Override
 	public void setRotation(float yaw, float pitch) {
 		super.setRotation(yaw, pitch);
+	}
+	
+	public boolean isTailPeek() {
+		return this.tailPeek;
+	}
+	
+	public int getTailPartIndex() {
+		return this.tailPart;
 	}
 
 }
