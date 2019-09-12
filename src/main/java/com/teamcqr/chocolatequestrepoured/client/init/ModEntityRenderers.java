a50af938eccc7327dca9f33672dd3918b8c03488
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-09-12 11:42:39 +0200

    Tail rendering
    Not done, need to create model for the tail to better render it, this
    solution is only temporary and looks buggy

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/init/ModEntityRenderers.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/init/ModEntityRenderers.java
@@ -1,6 +1,5 @@
 package com.teamcqr.chocolatequestrepoured.client.init;
 
-import com.teamcqr.chocolatequestrepoured.client.models.entities.boss.ModelNetherDragonBodyPart;
 import com.teamcqr.chocolatequestrepoured.client.render.entity.RenderCQRDwarf;
 import com.teamcqr.chocolatequestrepoured.client.render.entity.RenderCQREntity;
 import com.teamcqr.chocolatequestrepoured.client.render.entity.RenderCQRNetherDragon;
@@ -66,7 +65,7 @@ public class ModEntityRenderers {
 				renderManager -> new RenderCQRNetherDragon(renderManager));
 		
 		RenderingRegistry.registerEntityRenderingHandler(EntityCQRNetherDragonSegment.class,
-				renderManager -> new RenderCQRNetherDragonSegment(renderManager, new ModelNetherDragonBodyPart()));
+				renderManager -> new RenderCQRNetherDragonSegment(renderManager/*, new ModelNetherDragonBodyPart()*/));
 	}
 
 }
--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRNetherDragonSegment.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRNetherDragonSegment.java
@@ -1,5 +1,6 @@
 package com.teamcqr.chocolatequestrepoured.client.render.entity;
 
+import com.teamcqr.chocolatequestrepoured.client.models.entities.boss.ModelNetherDragonBodyPart;
 import com.teamcqr.chocolatequestrepoured.objects.entity.boss.EntityCQRNetherDragonSegment;
 import com.teamcqr.chocolatequestrepoured.util.Reference;
 
@@ -17,16 +18,37 @@ public class RenderCQRNetherDragonSegment extends Render<EntityCQRNetherDragonSe
 
 	private final ModelBase model;
 
-	public RenderCQRNetherDragonSegment(RenderManager manager, ModelBase model) {
+	public RenderCQRNetherDragonSegment(RenderManager manager) {
 		super(manager);
-		this.model = model;
+		this.model = new ModelNetherDragonBodyPart();
 	}
 
 	@Override
 	public void doRender(EntityCQRNetherDragonSegment entity, double x, double y, double z, float entityYaw, float partialTicks) {
 		GlStateManager.pushMatrix();
 		GlStateManager.translate((float) x, (float) y, (float) z);
-		GlStateManager.scale(-1.0F, -1.0F, 1.0F);
+		
+		if(entity.isTailPeek()) {
+			//start of the tail
+			if(entity.getTailPartIndex() == 3) {
+				//GL11.glScalef(0.8f,0.8f,0.8f);
+				GlStateManager.scale(0.8F, 0.8F, 1.0F);
+				GlStateManager.translate(0, 1.7, 0);
+				//Middle tail part
+			} else if(entity.getTailPartIndex() == 2) {
+				//GL11.glScalef(0.6f,0.6f,0.6f);
+				GlStateManager.scale(0.6F, 0.6F, 1.0F);
+				GlStateManager.translate(0, 2.0, 0);
+				//Tail end
+			} else {
+				//GL11.glScalef(0.4f,0.4f,0.4f);
+				GlStateManager.scale(0.4F, 0.4F, 1.0F);
+				GlStateManager.translate(0, 2.6, 0);
+			}
+		} else {
+			GlStateManager.scale(-1.0F, -1.0F, 1.0F);
+			//GL11.glScalef(1.0f, 1.0f, 1.0f);
+		}
 
 		float yawDiff = entity.rotationYaw - entity.prevRotationYaw;
 		if (yawDiff > 180) {
