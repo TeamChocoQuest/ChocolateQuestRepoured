916f6b63c7ceb322117cb6f36e7a73e2f43c8bbd
Meldexun <micha.kotlowski@gmx.de>
2019-09-12 15:07:23 +0200

    Update AbstractRenderCQREntity.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/AbstractRenderCQREntity.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/AbstractRenderCQREntity.java
@@ -1,10 +1,10 @@
 package com.teamcqr.chocolatequestrepoured.client.render.entity;
 
+import org.lwjgl.opengl.GL11;
+
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.AbstractEntityCQR;
-import com.teamcqr.chocolatequestrepoured.util.Reference;
 
 import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.layers.LayerArrow;
@@ -16,14 +16,27 @@ import net.minecraft.util.ResourceLocation;
 public abstract class AbstractRenderCQREntity<T extends AbstractEntityCQR> extends RenderLiving<T> {
 
 	public ResourceLocation texture;
+	public double widthScale;
+	public double heightScale;
 
-	public AbstractRenderCQREntity(RenderManager rendermanagerIn, ModelBase modelbaseIn, float shadowsizeIn, ResourceLocation textureLocation) {
+	public AbstractRenderCQREntity(RenderManager rendermanagerIn, ModelBase modelbaseIn, float shadowsizeIn,
+			ResourceLocation textureLocation, double widthScale, double heightScale) {
 		super(rendermanagerIn, modelbaseIn, shadowsizeIn);
 		this.texture = textureLocation;
+		this.widthScale = widthScale;
+		this.heightScale = heightScale;
 		this.addLayer(new LayerBipedArmor(this));
 		this.addLayer(new LayerHeldItem(this));
 		this.addLayer(new LayerArrow(this));
 		this.addLayer(new LayerElytra(this));
+	}
+
+	@Override
+	public void doRender(T entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		GL11.glPushMatrix();
+		GL11.glScaled(this.widthScale, this.heightScale, this.widthScale);
+		super.doRender(entity, x, y, z, entityYaw, partialTicks);
+		GL11.glPopMatrix();
 	}
 
 	protected ResourceLocation getEntityTexture(T entity) {
