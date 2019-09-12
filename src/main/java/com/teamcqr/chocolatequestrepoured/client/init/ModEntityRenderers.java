889fad351c84e72f31b57e4954529fce99f6479d
Meldexun <micha.kotlowski@gmx.de>
2019-09-12 02:41:17 +0200

    Update ModEntityRenderers.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/init/ModEntityRenderers.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/init/ModEntityRenderers.java
@@ -18,7 +18,23 @@ import com.teamcqr.chocolatequestrepoured.objects.entity.boss.EntityCQRNetherDra
 import com.teamcqr.chocolatequestrepoured.objects.entity.boss.EntityCQRNetherDragonSegment;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRDummy;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRDwarf;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQREnderman;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRGoblin;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRGolem;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRHuman;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRIllager;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRInquisitor;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRMinotaur;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRMummy;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQROgre;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQROrc;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRPigman;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRPirate;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRSkeleton;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRSpectre;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRTestificate;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRTriton;
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRWalker;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRZombie;
 import com.teamcqr.chocolatequestrepoured.objects.entity.projectiles.ProjectileBullet;
 import com.teamcqr.chocolatequestrepoured.objects.entity.projectiles.ProjectileCannonBall;
@@ -57,8 +73,40 @@ public class ModEntityRenderers {
 				renderManager -> new RenderCQREntity<EntityCQRDummy>(renderManager, "entity_mob_cqrdummy"));
 		RenderingRegistry.registerEntityRenderingHandler(EntityCQRDwarf.class,
 				renderManager -> new RenderCQRDwarf(renderManager));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQREnderman.class,
+				renderManager -> new RenderCQREntity<EntityCQREnderman>(renderManager, "entity_mob_cqrenderman"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRGoblin.class,
+				renderManager -> new RenderCQREntity<EntityCQRGoblin>(renderManager, "entity_mob_cqrgoblin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRGolem.class,
+				renderManager -> new RenderCQREntity<EntityCQRGolem>(renderManager, "entity_mob_cqrgolem"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRHuman.class,
+				renderManager -> new RenderCQREntity<EntityCQRHuman>(renderManager, "entity_mob_cqrhuman"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRIllager.class,
+				renderManager -> new RenderCQREntity<EntityCQRIllager>(renderManager, "entity_mob_cqrillager"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRInquisitor.class,
+				renderManager -> new RenderCQREntity<EntityCQRInquisitor>(renderManager, "entity_mob_cqrinquisitor"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRMinotaur.class,
+				renderManager -> new RenderCQREntity<EntityCQRMinotaur>(renderManager, "entity_mob_cqrminotaur"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRMummy.class,
+				renderManager -> new RenderCQREntity<EntityCQRMummy>(renderManager, "entity_mob_cqrmummy"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQROgre.class,
+				renderManager -> new RenderCQREntity<EntityCQROgre>(renderManager, "entity_mob_cqrogre"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQROrc.class,
+				renderManager -> new RenderCQREntity<EntityCQROrc>(renderManager, "entity_mob_cqrorc"));
 		RenderingRegistry.registerEntityRenderingHandler(EntityCQRPigman.class,
 				renderManager -> new RenderCQRPigman(renderManager));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRPirate.class,
+				renderManager -> new RenderCQREntity<EntityCQRPirate>(renderManager, "entity_mob_cqrpirate"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRSkeleton.class,
+				renderManager -> new RenderCQREntity<EntityCQRSkeleton>(renderManager, "entity_mob_cqrskeleton"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRSpectre.class,
+				renderManager -> new RenderCQREntity<EntityCQRSpectre>(renderManager, "entity_mob_cqrspectre"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRTestificate.class,
+				renderManager -> new RenderCQREntity<EntityCQRTestificate>(renderManager, "entity_mob_cqrtestificate"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRTriton.class,
+				renderManager -> new RenderCQREntity<EntityCQRTriton>(renderManager, "entity_mob_cqrtriton"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityCQRWalker.class,
+				renderManager -> new RenderCQREntity<EntityCQRWalker>(renderManager, "entity_mob_cqrwalker"));
 		RenderingRegistry.registerEntityRenderingHandler(EntityCQRZombie.class,
 				renderManager -> new RenderCQREntity<EntityCQRZombie>(renderManager, "entity_mob_cqrzombie"));
 
