8c2b57a8fb5db22c54a60628d2c1e2d4df016b9b
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-09-14 14:22:33 +0200

    model for CQR Skellies

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRSkeleton.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRSkeleton.java
@@ -1,14 +1,14 @@
 package com.teamcqr.chocolatequestrepoured.client.render.entity;
 
+import com.teamcqr.chocolatequestrepoured.client.models.entities.ModelCQRSkeleton;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRSkeleton;
 
-import net.minecraft.client.model.ModelSkeleton;
 import net.minecraft.client.renderer.entity.RenderManager;
 
 public class RenderCQRSkeleton extends RenderCQREntity<EntityCQRSkeleton> {
 
 	public RenderCQRSkeleton(RenderManager rendermanagerIn) {
-		super(rendermanagerIn, new ModelSkeleton(), 0.5F, "entity_mob_cqrskeleton", 1.0D, 1.0D);
+		super(rendermanagerIn, new ModelCQRSkeleton(), 0.5F, "entity_mob_cqrskeleton", 1.0D, 1.0D);
 	}
 
 }
