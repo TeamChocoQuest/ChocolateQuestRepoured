86c41b49fc040ca40ba88e4957bbfcb722c2dbec
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-09-14 12:43:55 +0200

    update

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQREntity.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQREntity.java
@@ -25,6 +25,7 @@ public class RenderCQREntity<T extends AbstractEntityCQR> extends RenderLiving<T
 	public ResourceLocation texture;
 	public double widthScale;
 	public double heightScale;
+	private boolean scaleVarApplied = false;
 	
 	public RenderCQREntity(RenderManager rendermanagerIn, String entityName) {
 		this(rendermanagerIn, entityName, 1.0D, 1.0D);
@@ -38,9 +39,9 @@ public class RenderCQREntity<T extends AbstractEntityCQR> extends RenderLiving<T
 			double widthScale, double heightScale) {
 		super(rendermanagerIn, model, shadowSize);
 		this.texture = new ResourceLocation(Reference.MODID, "textures/entity/" + entityName + ".png");
-		Random rand = new Random();
-		this.widthScale = widthScale + (0.5D * (-0.25D +(rand.nextDouble() *0.5D)));
-		this.heightScale = heightScale + (-0.25D +(rand.nextDouble() *0.5D));;
+		//Random rand = new Random();
+		this.widthScale = widthScale;// + (0.5D * (-0.25D +(rand.nextDouble() *0.5D)));
+		this.heightScale = heightScale;// + (-0.25D +(rand.nextDouble() *0.5D));;
 		this.addLayer(new LayerBipedArmor(this));
 		this.addLayer(new LayerHeldItem(this));
 		this.addLayer(new LayerArrow(this));
@@ -50,6 +51,12 @@ public class RenderCQREntity<T extends AbstractEntityCQR> extends RenderLiving<T
 
 	@Override
 	protected void preRenderCallback(T entitylivingbaseIn, float partialTickTime) {
+		if(!this.scaleVarApplied) {
+			Random rand = new Random();
+			this.widthScale = widthScale + (0.5D * (-0.25D +(rand.nextDouble() *0.5D)));
+			this.heightScale = heightScale + (-0.25D +(rand.nextDouble() *0.5D));;
+			this.scaleVarApplied = true;
+		}
 		double width = this.widthScale;// * (1.0D + 0.5D * entitylivingbaseIn.getSizeVariation());
 		double height = this.heightScale;// * (1.0D + entitylivingbaseIn.getSizeVariation());
 		GL11.glScaled(width, height, width);
