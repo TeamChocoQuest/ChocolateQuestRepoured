6289ab8787a797f6ad89a9b8d668ed1aa25d4b5a
Meldexun <micha.kotlowski@gmx.de>
2019-09-17 17:15:01 +0200

    Update ModCapabilities.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/init/ModCapabilities.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/init/ModCapabilities.java
@@ -1,23 +1,17 @@
 package com.teamcqr.chocolatequestrepoured.init;
 
-import com.teamcqr.chocolatequestrepoured.capability.armorturtle.CapabilityArmorTurtle;
-import com.teamcqr.chocolatequestrepoured.capability.armorturtle.CapabilityArmorTurtleStorage;
-import com.teamcqr.chocolatequestrepoured.capability.armorturtle.ICapabilityArmorTurtle;
-import com.teamcqr.chocolatequestrepoured.capability.extraitemhandler.ExtraItemHandler;
-import com.teamcqr.chocolatequestrepoured.capability.extraitemhandler.ExtraItemHandlerStorage;
-import com.teamcqr.chocolatequestrepoured.capability.extraitemhandler.IExtraItemHandler;
-import com.teamcqr.chocolatequestrepoured.capability.structureselector.IStructureSelector;
-import com.teamcqr.chocolatequestrepoured.capability.structureselector.StructureSelector;
-import com.teamcqr.chocolatequestrepoured.capability.structureselector.StructureSelectorStorage;
-
-import net.minecraftforge.common.capabilities.CapabilityManager;
+import com.teamcqr.chocolatequestrepoured.capability.armor.slime.CapabilitySlimeArmorProvider;
+import com.teamcqr.chocolatequestrepoured.capability.armor.turtle.CapabilityTurtleArmorProvider;
+import com.teamcqr.chocolatequestrepoured.capability.extraitemhandler.CapabilityExtraItemHandlerProvider;
+import com.teamcqr.chocolatequestrepoured.capability.structureselector.CapabilityStructureSelectorProvider;
 
 public class ModCapabilities {
 
 	public static void registerCapabilities() {
-		CapabilityManager.INSTANCE.register(ICapabilityArmorTurtle.class, new CapabilityArmorTurtleStorage(), () -> new CapabilityArmorTurtle());
-		CapabilityManager.INSTANCE.register(IExtraItemHandler.class, new ExtraItemHandlerStorage(), () -> new ExtraItemHandler());
-		CapabilityManager.INSTANCE.register(IStructureSelector.class, new StructureSelectorStorage(), () -> new StructureSelector());
+		CapabilityTurtleArmorProvider.register();
+		CapabilitySlimeArmorProvider.register();
+		CapabilityExtraItemHandlerProvider.register();
+		CapabilityStructureSelectorProvider.register();
 	}
 
 }
