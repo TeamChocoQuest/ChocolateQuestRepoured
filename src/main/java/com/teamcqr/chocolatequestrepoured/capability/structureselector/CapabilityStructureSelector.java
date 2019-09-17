17863fdb6e4fea10c65b159ebf53da91ebdc3132
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-09-17 17:17:45 +0200

    Update and rename StructureSelector.java to CapabilityStructureSelector.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/capability/structureselector/CapabilityStructureSelector.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/capability/structureselector/CapabilityStructureSelector.java
@@ -2,27 +2,23 @@ package com.teamcqr.chocolatequestrepoured.capability.structureselector;
 
 import net.minecraft.util.math.BlockPos;
 
-public class StructureSelector implements IStructureSelector {
+public class CapabilityStructureSelector {
 
 	private BlockPos pos1;
 	private BlockPos pos2;
 
-	@Override
 	public void setPos1(BlockPos pos) {
 		this.pos1 = pos;
 	}
 
-	@Override
 	public BlockPos getPos1() {
 		return pos1;
 	}
 
-	@Override
 	public void setPos2(BlockPos pos) {
 		this.pos2 = pos;
 	}
 
-	@Override
 	public BlockPos getPos2() {
 		return pos2;
 	}
