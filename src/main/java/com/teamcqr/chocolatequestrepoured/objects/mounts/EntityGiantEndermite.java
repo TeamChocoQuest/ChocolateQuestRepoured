f8921fa7411ac74954a831aa4326ee0c9bd14a59
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-09-19 16:21:59 +0200

    sounds

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/mounts/EntityGiantEndermite.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/mounts/EntityGiantEndermite.java
@@ -2,13 +2,30 @@ package com.teamcqr.chocolatequestrepoured.objects.mounts;
 
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.EntityCQRMountBase;
 
+import net.minecraft.init.SoundEvents;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 
 public class EntityGiantEndermite extends EntityCQRMountBase {
 
 	public EntityGiantEndermite(World worldIn) {
 		super(worldIn);
-		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected SoundEvent getAmbientSound() {
+		return SoundEvents.ENTITY_ENDERMITE_AMBIENT;
+	}
+	
+	@Override
+	protected SoundEvent getHurtSound(DamageSource damageSourceIn) {
+		return SoundEvents.ENTITY_ENDERMITE_HURT;
+	}
+	
+	@Override
+	protected SoundEvent getDeathSound() {
+		return SoundEvents.ENTITY_ENDERMITE_DEATH;
 	}
 
 }
--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/mounts/EntityPollo.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/mounts/EntityPollo.java
@@ -2,6 +2,9 @@ package com.teamcqr.chocolatequestrepoured.objects.mounts;
 
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.EntityCQRMountBase;
 
+import net.minecraft.init.SoundEvents;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 
 public class EntityPollo extends EntityCQRMountBase {
@@ -9,6 +12,21 @@ public class EntityPollo extends EntityCQRMountBase {
 	public EntityPollo(World worldIn) {
 		super(worldIn);
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected SoundEvent getHurtSound(DamageSource damageSourceIn) {
+		return SoundEvents.ENTITY_CHICKEN_HURT;
+	}
+	
+	@Override
+	protected SoundEvent getAmbientSound() {
+		return SoundEvents.ENTITY_CHICKEN_AMBIENT;
+	}
+	
+	@Override
+	protected SoundEvent getDeathSound() {
+		return SoundEvents.ENTITY_CHICKEN_DEATH;
 	}
 
 }
