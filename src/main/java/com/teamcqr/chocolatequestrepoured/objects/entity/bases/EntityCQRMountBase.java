9720489eccbf42d9f4d032de8ff3a181f93782c7
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-09-19 16:34:06 +0200

    ai

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/bases/EntityCQRMountBase.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/bases/EntityCQRMountBase.java
@@ -2,8 +2,13 @@ package com.teamcqr.chocolatequestrepoured.objects.entity.bases;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAgeable;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
 public abstract class EntityCQRMountBase extends EntityAnimal {
@@ -12,6 +17,14 @@ public abstract class EntityCQRMountBase extends EntityAnimal {
 		super(worldIn);
 		// TODO Auto-generated constructor stub
 	}
+	
+	protected void initEntityAI() {
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIPanic(this, 1.25D));
+        this.tasks.addTask(6, new EntityAIWanderAvoidWater(this, 1.0D));
+        this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(8, new EntityAILookIdle(this));
+    }
 	
 	@Override
 	protected boolean canBeRidden(Entity entityIn) {
