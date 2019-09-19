05581d883faa1892a9f8e00c239d506f10394f19
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-09-19 14:51:22 +0200

    Update RenderCQREnderman.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQREnderman.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQREnderman.java
@@ -1,26 +1,39 @@
 package com.teamcqr.chocolatequestrepoured.client.render.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.teamcqr.chocolatequestrepoured.client.models.entities.ModelCQREndermanArmor;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQREnderman;
 
 import net.minecraft.client.model.ModelEnderman;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
+import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class RenderCQREnderman extends RenderCQREntity<EntityCQREnderman> {
 
 	public RenderCQREnderman(RenderManager rendermanagerIn) {
 		super(rendermanagerIn, new ModelEnderman(0.0F), 0.5F, "entity_mob_cqrenderman", 1.0D, 1.0D);
-		try {
-			for(LayerRenderer layer : this.layerRenderers) {
-				if(layer instanceof LayerHeldItem) {
-					this.layerRenderers.remove(layer);
-				}
+		List<LayerRenderer> toRemove = new ArrayList<LayerRenderer>();
+		for (LayerRenderer layer : this.layerRenderers) {
+			if (layer instanceof LayerBipedArmor) {
+				toRemove.add(layer);
 			}
-			//TODO: Add enderman equipment layer
-		} catch(Exception ex) {
-			ex.printStackTrace();
 		}
+		for (LayerRenderer layer : toRemove) {
+			this.layerRenderers.remove(layer);
+		}
+		this.addLayer(new LayerBipedArmor(this) {
+			@Override
+			protected void initArmor() {
+				this.modelLeggings = new ModelCQREndermanArmor(0.5F, true);
+				this.modelArmor = new ModelCQREndermanArmor(1.0F, false);
+			}
+		});
 	}
 
 }
