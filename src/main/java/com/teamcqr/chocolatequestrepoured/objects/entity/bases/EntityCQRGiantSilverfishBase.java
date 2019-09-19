e0b6ccd31f865d41fab6d3bacef3b0938a3dc59d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-09-19 16:15:45 +0200

    base class for mounts

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/bases/EntityCQRGiantSilverfishBase.java
@@ -0,0 +1,12 @@
+package com.teamcqr.chocolatequestrepoured.objects.entity.bases;
+
+import net.minecraft.world.World;
+
+public abstract class EntityCQRGiantSilverfishBase extends EntityCQRMountBase {
+
+	public EntityCQRGiantSilverfishBase(World worldIn) {
+		super(worldIn);
+		// TODO Auto-generated constructor stub
+	}
+
+}
