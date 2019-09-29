30a2c5cd397fda395a87012ae767b75d6c3ee2cb
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-09-29 20:09:11 +0200

    more updates

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/blocks/BlockBossBlock.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/blocks/BlockBossBlock.java
@@ -8,6 +8,10 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockBossBlock extends Block {
 
@@ -32,6 +36,17 @@ public class BlockBossBlock extends Block {
 	@Override
 	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
 		return Items.AIR;
+	}
+	
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return EnumBlockRenderType.MODEL;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.CUTOUT;
 	}
 
 }
