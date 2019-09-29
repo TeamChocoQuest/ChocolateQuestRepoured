f7be9489f5199410338b02ec88966636092015a9
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-09-29 16:45:18 +0200

    adaption to boss base class

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRNetherDragon.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRNetherDragon.java
@@ -2,7 +2,7 @@ package com.teamcqr.chocolatequestrepoured.objects.entity.boss;
 
 import com.teamcqr.chocolatequestrepoured.factions.EFaction;
 import com.teamcqr.chocolatequestrepoured.objects.entity.EBaseHealths;
-import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR;
+import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQRBoss;
 import com.teamcqr.chocolatequestrepoured.util.handlers.SoundsHandler;
 
 import io.netty.buffer.ByteBuf;
@@ -32,7 +32,7 @@ import net.minecraft.world.BossInfoServer;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
-public class EntityCQRNetherDragon extends AbstractEntityCQR implements IEntityMultiPart, IRangedAttackMob {
+public class EntityCQRNetherDragon extends /*AbstractEntityCQR*/AbstractEntityCQRBoss implements IEntityMultiPart, IRangedAttackMob {
 	
 	public enum EDragonMovementState {
 		CHARGING,
@@ -83,7 +83,7 @@ public class EntityCQRNetherDragon extends AbstractEntityCQR implements IEntityM
 	 */
 	
 	public EntityCQRNetherDragon(World worldIn) {
-		super(worldIn);
+		super(worldIn, 32);
 		/*this.dragonBodyParts = new MultiPartEntityPart[] { this.headPart, this.body1, this.body2, this.body3,
 				this.body4, this.body5, this.body6, this.body7, this.body8, this.body9, this.body10, this.body11,
 				this.body12, this.body13, this.body14, this.body15, this.body16 };*/
