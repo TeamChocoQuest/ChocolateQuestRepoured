aef4e23c9f2be39916e97203cfc9052f9a8cec05
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-10-03 12:04:33 +0200

    loot tables for mounts

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/mounts/EntityGiantEndermite.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/mounts/EntityGiantEndermite.java
@@ -1,9 +1,11 @@
 package com.teamcqr.chocolatequestrepoured.objects.mounts;
 
+import com.teamcqr.chocolatequestrepoured.objects.entity.ELootTablesNormal;
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.EntityCQRMountBase;
 
 import net.minecraft.init.SoundEvents;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 
@@ -27,6 +29,11 @@ public class EntityGiantEndermite extends EntityCQRMountBase {
 	@Override
 	protected SoundEvent getDeathSound() {
 		return SoundEvents.ENTITY_ENDERMITE_DEATH;
+	}
+	
+	@Override
+	protected ResourceLocation getLootTable() {
+		return ELootTablesNormal.ENTITY_GIANT_ENDERMITE.getLootTable();
 	}
 
 }
