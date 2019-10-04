ae8b4c0faf85f79b94d234bcaa31aff7aa8a39eb
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-10-04 23:54:52 +0200

    golem model

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRGolem.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRGolem.java
@@ -1,14 +1,14 @@
 package com.teamcqr.chocolatequestrepoured.client.render.entity;
 
+import com.teamcqr.chocolatequestrepoured.client.models.entities.ModelCQRGolem;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRGolem;
 
-import net.minecraft.client.model.ModelIronGolem;
 import net.minecraft.client.renderer.entity.RenderManager;
 
 public class RenderCQRGolem extends RenderCQREntity<EntityCQRGolem> {
 
 	public RenderCQRGolem(RenderManager rendermanagerIn) {
-		super(rendermanagerIn, new ModelIronGolem(), 0.5F, "entity_mob_cqrgolem", 1.0D, 1.0D);
+		super(rendermanagerIn, new ModelCQRGolem(0F), 0.5F, "entity_mob_cqrgolemsmall", 1.0D, 1.0D);
 	}
 
 }
