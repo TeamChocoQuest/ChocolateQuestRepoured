956274b6663cbb012274f26b7884f21e3d5c5152
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-10-05 00:03:54 +0200

    sounds

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQRGolem.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQRGolem.java
@@ -5,7 +5,10 @@ import com.teamcqr.chocolatequestrepoured.objects.entity.EBaseHealths;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ELootTablesNormal;
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR;
 
+import net.minecraft.init.SoundEvents;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
@@ -33,6 +36,21 @@ public class EntityCQRGolem extends AbstractEntityCQR {
 	@Override
 	protected ResourceLocation getLootTable() {
 		return ELootTablesNormal.ENTITY_GOLEM.getLootTable();
+	}
+	
+	@Override
+	protected SoundEvent getHurtSound(DamageSource damageSourceIn) {
+		return SoundEvents.ENTITY_IRONGOLEM_HURT;
+	}
+	
+	@Override
+	protected SoundEvent getAmbientSound() {
+		return SoundEvents.ENTITY_IRONGOLEM_STEP;
+	}
+	
+	@Override
+	protected SoundEvent getDeathSound() {
+		return SoundEvents.ENTITY_IRONGOLEM_DEATH;
 	}
 
 }
