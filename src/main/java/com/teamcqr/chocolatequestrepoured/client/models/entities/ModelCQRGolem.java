8fa483d5e63d6be18c2beb92e5059e9e1495b1eb
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-10-05 23:01:10 +0200

    fixes + armor renderer for golems

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/ModelCQRGolem.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/ModelCQRGolem.java
@@ -85,19 +85,12 @@ public class ModelCQRGolem extends ModelCQRBiped {
 
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
 		super.render(entity, f, f1, f2, f3, f4, f5);
-		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-		//nose.render(f5);
-		//bodyLower.render(f5);
 	}
 
 	private void setRotation(ModelRenderer model, float x, float y, float z) {
 		model.rotateAngleX = x;
 		model.rotateAngleY = y;
 		model.rotateAngleZ = z;
-	}
-
-	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity) {
-		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
 	}
 
 }
