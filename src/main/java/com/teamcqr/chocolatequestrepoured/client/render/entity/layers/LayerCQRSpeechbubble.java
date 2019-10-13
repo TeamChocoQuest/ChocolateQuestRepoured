6a3449e1af8f914da92937a21627e83784688862
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-10-13 15:28:46 +0200

    SPEECH BUBBLES !!!1111!!!1!!!!!eleven!!!111

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/layers/LayerCQRSpeechbubble.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/layers/LayerCQRSpeechbubble.java
@@ -7,9 +7,9 @@ import com.teamcqr.chocolatequestrepoured.client.render.entity.RenderCQREntity;
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
@@ -24,6 +24,7 @@ public class LayerCQRSpeechbubble extends AbstractLayerCQR {
 		super(livingEntityRendererIn);
 		this.bipedHead = headBox;
 		this.currentBubble = ESpeechBubble.ITEM_BEER;
+		//System.out.println("NEW INSTANCE");
 	}
 	
 	
@@ -31,47 +32,33 @@ public class LayerCQRSpeechbubble extends AbstractLayerCQR {
 	public void doRenderLayer(AbstractEntityCQR entity, float limbSwing, float limbSwingAmount, float partialTicks,
 			float ageInTicks, float netHeadYaw, float headPitch, float scale) {
 		if(entity.isChatting()) {
-			super.doRenderLayer(entity, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scale);
-			
 			Tessellator tessellator = Tessellator.getInstance();
 			Minecraft minecraft = Minecraft.getMinecraft();
 			
-			EntityPlayerSP player = minecraft.player;
+			GlStateManager.pushMatrix();
+			GlStateManager.rotate(entity.rotationYawHead +90, 0.0F, 1.0F, 0.0F);
+			GlStateManager.translate(-0.5, -1.5, 0);
 			
-			double diffX = (entity.prevPosX + (entity.posX - entity.prevPosX)) - (player.prevPosX +(player.posX - player.prevPosX));
-			double diffY = (entity.prevPosY + (entity.posY - entity.prevPosY)) - (player.prevPosY +(player.posY - player.prevPosY));
-			double diffZ = (entity.prevPosZ + (entity.posZ - entity.prevPosZ)) - (player.prevPosZ +(player.posZ - player.prevPosZ));
-			
-			GL11.glPushMatrix();
-			GL11.glTranslated(diffX, diffY +3, diffZ);
-			GL11.glRotatef(-entity.rotationYaw, 0.0F, 1.0F, 0.0F);
-			GL11.glPushMatrix();
-			
-			GL11.glRotatef(-90, 1, 0, 0);
-			GL11.glTranslated(0, -1, 0);
-			
+			//System.out.println("Entity age: " + ageInTicks);
 			if(ageInTicks % CHANGE_BUBBLE_INTERVAL == 0) {
-				currentBubble = ESpeechBubble.getRandom(entity.getRNG());
+				//currentBubble = ESpeechBubble.getRandom(entity.getRNG());
+				entity.chooseNewRandomSpeechBubble();
+				//System.out.println("Bubble: " + currentBubble.name());
 			}
 			
-			minecraft.renderEngine.bindTexture(currentBubble.getResourceLocation());
+			//minecraft.renderEngine.bindTexture(currentBubble.getResourceLocation());
+			minecraft.renderEngine.bindTexture(entity.getCurrentSpeechBubble().getResourceLocation());
 			
 			BufferBuilder buffer = tessellator.getBuffer();
-			buffer.begin(3, DefaultVertexFormats.POSITION_COLOR);
-			buffer.addVertexData(new int[] {-1, 1, 1, 0, 0});
-			buffer.addVertexData(new int[] {-1, 1, -1, 0, 1});
-			buffer.addVertexData(new int[] {1, 1, -1, 1, 1});
-			buffer.addVertexData(new int[] {1, 1, 1, 1, 0});
-			
-			buffer.addVertexData(new int[] {-1, 1, 1, 0, 0});
-			buffer.addVertexData(new int[] {1, 1, 1, 1, 0});
-			buffer.addVertexData(new int[] {1, 1, -1, 1, 1});
-			buffer.addVertexData(new int[] {-1, 1, -1, 0, 1});
+			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+			buffer.pos(0,1,0).tex(0, 1).endVertex();
+			buffer.pos(1,1,0).tex(1,1).endVertex();
+			buffer.pos(1,0,0).tex(1,0).endVertex();
+			buffer.pos(0,0,0).tex(0,0).endVertex();
 			
 			tessellator.draw();
 			
-			GL11.glPopMatrix();
-			GL11.glPopMatrix();
+			GlStateManager.popMatrix();
 			
 		}
 		
