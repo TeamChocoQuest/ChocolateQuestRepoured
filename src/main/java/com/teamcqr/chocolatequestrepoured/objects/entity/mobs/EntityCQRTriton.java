97c18393ccbbf51522dafafa730165fccc252c8c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-10-15 15:04:49 +0200

    triton model & texture

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQRTriton.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQRTriton.java
@@ -1,15 +1,20 @@
 package com.teamcqr.chocolatequestrepoured.objects.entity.mobs;
 
+import com.teamcqr.chocolatequestrepoured.client.mca.animations.entities.triton.AnimationHandlerTriton;
 import com.teamcqr.chocolatequestrepoured.factions.EFaction;
 import com.teamcqr.chocolatequestrepoured.objects.entity.EBaseHealths;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ELootTablesNormal;
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR;
 
+import mcalibrary.IMCAnimatedEntity;
+import mcalibrary.animation.AnimationHandler;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
-public class EntityCQRTriton extends AbstractEntityCQR {
+public class EntityCQRTriton extends AbstractEntityCQR implements IMCAnimatedEntity {
+	
+	protected AnimationHandler animHandler = new AnimationHandlerTriton(this);
 
 	public EntityCQRTriton(World worldIn) {
 		super(worldIn);
@@ -33,6 +38,11 @@ public class EntityCQRTriton extends AbstractEntityCQR {
 	@Override
 	protected ResourceLocation getLootTable() {
 		return ELootTablesNormal.ENTITY_TRITON.getLootTable();
+	}
+
+	@Override
+	public AnimationHandler getAnimationHandler() {
+		return animHandler;
 	}
 
 }
