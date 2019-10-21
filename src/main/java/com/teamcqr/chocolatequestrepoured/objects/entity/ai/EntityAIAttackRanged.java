b64dad6ce872256b785340b163505a3f6412c437
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-10-21 13:37:10 +0200

    began AI for ranged weapons

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/EntityAIAttackRanged.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/EntityAIAttackRanged.java
@@ -10,15 +10,19 @@ public class EntityAIAttackRanged extends AbstractCQREntityAI {
 
 	public EntityAIAttackRanged(AbstractEntityCQR entity) {
 		super(entity);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean shouldExecute() {
+		if(entity == null || entity.isDead) return false;
 		if(entity.getHeldEquipment() == null) return false;
 		Item item = entity.getHeldItemMainhand().getItem();
 		if(item instanceof ItemBow || item instanceof IRangedWeapon) {
-			
+			return true;
+		}
+		
+		if(entity.getAttackTarget() == null || (entity.getAttackTarget() != null && !entity.getEntitySenses().canSee(entity.getAttackTarget())) ) {
+			return false;
 		}
 		return false;
 	}
