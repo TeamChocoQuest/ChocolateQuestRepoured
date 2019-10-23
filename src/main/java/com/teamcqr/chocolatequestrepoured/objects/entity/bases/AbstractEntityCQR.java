d1f6ccb3a0fc56549fb1a63cc83934a383db878a
Meldexun <27779321+Meldexun@users.noreply.github.com>
2019-10-23 19:38:49 +0200

    Update AbstractEntityCQR.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/bases/AbstractEntityCQR.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/bases/AbstractEntityCQR.java
@@ -27,6 +27,7 @@ import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAITorchIgniter
 import com.teamcqr.chocolatequestrepoured.objects.factories.SpawnerFactory;
 import com.teamcqr.chocolatequestrepoured.objects.items.ItemBadge;
 import com.teamcqr.chocolatequestrepoured.objects.items.ItemPotionHealing;
+import com.teamcqr.chocolatequestrepoured.util.ItemUtil;
 import com.teamcqr.chocolatequestrepoured.util.Reference;
 
 import io.netty.buffer.ByteBuf;
@@ -673,8 +674,21 @@ public abstract class AbstractEntityCQR extends EntityCreature implements IMob,I
 	public boolean inAttackReach(EntityLivingBase target) {
 		return this.getDistance(target) <= this.getAttackReach(target);
 	}
-	public boolean canSeeEntity(EntityLivingBase possibleTarget) {
-		return (possibleTarget != null && (canEntityBeSeen(possibleTarget) || getEntitySenses().canSee(possibleTarget)));
+
+	public boolean canSeeEntity(EntityLivingBase target) {
+		double x = target.posX - this.posX;
+		double z = target.posZ - this.posZ;
+		double d = Math.toDegrees(Math.atan2(-x, z));
+		if (!ItemUtil.compareRotations(this.rotationYawHead, d, 80.0D)) {
+			return false;
+		}
+		double y = target.posY + target.getEyeHeight() - this.posY - this.getEyeHeight();
+		double xz = Math.sqrt(x * x + z * z);
+		double d1 = Math.toDegrees(Math.atan2(y, xz));
+		if (!ItemUtil.compareRotations(this.rotationPitch, d1, 50.0D)) {
+			return false;
+		}
+		return true;
 	}
 
 }
