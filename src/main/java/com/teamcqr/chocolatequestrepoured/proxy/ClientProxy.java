79c31bb149b60ca6b36ed3efeab911d032d096c0
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-10-25 18:28:55 +0200

    key binding

--- src/main/java/com/teamcqr/chocolatequestrepoured/proxy/ClientProxy.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/proxy/ClientProxy.java
@@ -1,12 +1,20 @@
 package com.teamcqr.chocolatequestrepoured.proxy;
 
+import org.lwjgl.input.Keyboard;
+
 import com.teamcqr.chocolatequestrepoured.client.init.ModEntityRenderers;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 public class ClientProxy implements IProxy {
+	
+	static final String KEY_CATEGORY_MAIN = "Chocolate Quest Repoured";
+	
+	public static KeyBinding keybindReputationGUI = new KeyBinding("Reputation GUI", Keyboard.KEY_F4, KEY_CATEGORY_MAIN);
 
 	@Override
 	public void preInit() {
@@ -15,7 +23,7 @@ public class ClientProxy implements IProxy {
 
 	@Override
 	public void init() {
-
+		ClientRegistry.registerKeyBinding(keybindReputationGUI);
 	}
 
 	@Override
