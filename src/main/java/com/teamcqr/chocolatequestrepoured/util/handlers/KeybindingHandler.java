e5c1e2008eb4f7a3131917fd48afd00d79c491f1
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-10-25 18:28:55 +0200

    key binding

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/handlers/KeybindingHandler.java
@@ -0,0 +1,27 @@
+package com.teamcqr.chocolatequestrepoured.util.handlers;
+
+import com.teamcqr.chocolatequestrepoured.CQRMain;
+import com.teamcqr.chocolatequestrepoured.proxy.ClientProxy;
+import com.teamcqr.chocolatequestrepoured.util.Reference;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraftforge.client.event.GuiScreenEvent.KeyboardInputEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class KeybindingHandler {
+
+	
+	//Opens repu GUI
+	@SubscribeEvent(receiveCanceled = true)
+	public static void onKeyPress(KeyboardInputEvent event) {
+		if(ClientProxy.keybindReputationGUI.isPressed()) {
+			EntityPlayerSP player = Minecraft.getMinecraft().player;
+			player.openGui(CQRMain.INSTANCE, Reference.REPUTATION_GUI_ID, Minecraft.getMinecraft().world, (int)player.posX, (int)player.posY, (int)player.posZ);
+		}
+	}
+
+}
