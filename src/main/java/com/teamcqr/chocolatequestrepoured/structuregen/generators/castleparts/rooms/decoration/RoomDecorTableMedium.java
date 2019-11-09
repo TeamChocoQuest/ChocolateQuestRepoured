03c7f74bb6dde514e8892b6c255f7deed9db2edc
KalgogSmash <trevor.seyf@gmail.com>
2019-11-09 06:06:05 -0800

    Adding air tiles to shelves so they don't overlap with other stuff

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/castleparts/rooms/decoration/RoomDecorTableMedium.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/castleparts/rooms/decoration/RoomDecorTableMedium.java
@@ -1,5 +1,6 @@
 package com.teamcqr.chocolatequestrepoured.structuregen.generators.castleparts.rooms.decoration;
 
+import net.minecraft.block.BlockSlab;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 
@@ -20,7 +21,8 @@ public class RoomDecorTableMedium extends RoomDecorBase
         schematic.add(new DecoPlacement(0, 0, 1, blockType));
         schematic.add(new DecoPlacement(1, 0, 1, blockType));
 
-        blockType = Blocks.WOODEN_PRESSURE_PLATE.getDefaultState();
+        blockType = Blocks.WOODEN_SLAB.getDefaultState();
+        blockType = blockType.withProperty(BlockSlab.HALF, BlockSlab.EnumBlockHalf.BOTTOM);
         schematic.add(new DecoPlacement(0, 1, 0, blockType));
         schematic.add(new DecoPlacement(1, 1, 0, blockType));
         schematic.add(new DecoPlacement(0, 1, 1, blockType));
