fca0ee0767daea1d3f0036ff22e12d1320feebd8
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-12 08:13:35 +0100

    began implementing turtle armor
    Not good to implement now
    Reasons:
    1) Model has no child relations -> Misalignment of shell part
    2) Model seems to be parted into mutliple models, should be one model

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/init/ModArmorModels.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/init/ModArmorModels.java
@@ -2,11 +2,13 @@ package com.teamcqr.chocolatequestrepoured.client.init;
 
 import com.teamcqr.chocolatequestrepoured.client.models.armor.ModelArmorTransparent;
 import com.teamcqr.chocolatequestrepoured.client.models.armor.ModelBackpack;
+import com.teamcqr.chocolatequestrepoured.client.models.armor.ModelTurtleChestplate;
 
 public class ModArmorModels {
 
 	public static ModelBackpack backpack = new ModelBackpack(0.0F);
 	public static ModelArmorTransparent slimeArmor = new ModelArmorTransparent(1.0F);
 	public static ModelArmorTransparent slimeArmorLegs = new ModelArmorTransparent(0.5F);
+	public static ModelTurtleChestplate turtleChestplate = new ModelTurtleChestplate(0F);
 
 }
