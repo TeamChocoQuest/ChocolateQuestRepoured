22b444d7daf40512c123e4e1d91f7e800ab5b77a
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-12 09:22:01 +0100

    updated comments

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/models/armor/ModelArmorHeavyIron.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/models/armor/ModelArmorHeavyIron.java
@@ -3,7 +3,7 @@ package com.teamcqr.chocolatequestrepoured.client.models.armor;
 import net.minecraft.client.model.ModelRenderer;
 
 /**
- * ModelCustomArmor - Either Mojang or a mod author
+ * ModelCustomArmor - Silentine
  * Created using Tabula 7.0.1
  */
 public class ModelArmorHeavyIron extends ModelCustomArmorBase {
@@ -14,19 +14,6 @@ public class ModelArmorHeavyIron extends ModelCustomArmorBase {
 
     public ModelArmorHeavyIron(float scale) {
     	super(scale, 128, 128);
-        /*this.HeadHelmet = new ModelRenderer(this, 0, 64);
-        this.HeadHelmet.setRotationPoint(-4.0F, -8.0F, -4.0F);
-        this.HeadHelmet.addBox(-1.0F, -1.0F, -1.0F, 10, 10, 10, 0.0F);
-        this.ChestArmor = new ModelRenderer(this, 0, 94);
-        this.ChestArmor.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.ChestArmor.addBox(-5.0F, -1.0F, -3.0F, 10, 14, 6, 0.0F);
-        this.LeftArmPauldron = new ModelRenderer(this, 0, 84);
-        this.LeftArmPauldron.mirror = true;
-        this.LeftArmPauldron.setRotationPoint(5.0F, 2.0F, 0.0F);
-        this.LeftArmPauldron.addBox(-1.5F, -2.5F, -2.5F, 5, 5, 5, 0.0F);
-        this.RightArmPauldon = new ModelRenderer(this, 0, 84);
-        this.RightArmPauldon.setRotationPoint(-5.0F, 2.0F, 0.0F);
-        this.RightArmPauldon.addBox(-3.5F, -2.5F, -2.5F, 5, 5, 5, 0.0F);*/
     	
     	this.ChestArmor = new ModelRenderer(this, 0, 88);
         this.ChestArmor.setRotationPoint(0.0F, 0.0F, 0.0F);
@@ -50,14 +37,6 @@ public class ModelArmorHeavyIron extends ModelCustomArmorBase {
          this.bipedLeftArm.addChild(this.LeftArmPauldron);
          this.bipedRightArm.addChild(this.RightArmPauldon);
     }
-
-    /*@Override
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
-        this.HeadHelmet.render(f5);
-        this.ChestArmor.render(f5);
-        this.LeftArmPauldron.render(f5);
-        this.RightArmPauldon.render(f5);
-    }*/
 
     /**
      * This is a helper function from Tabula to set the rotation of model parts
