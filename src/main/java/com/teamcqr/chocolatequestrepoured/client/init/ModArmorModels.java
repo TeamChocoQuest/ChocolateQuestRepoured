9ac155e635c6a9777f7a21dd756d9b2504c9a10d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-14 13:55:43 +0100

    turtle armor
    Not correctly made, does not extend biped model

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/init/ModArmorModels.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/init/ModArmorModels.java
@@ -3,15 +3,15 @@ package com.teamcqr.chocolatequestrepoured.client.init;
 import com.teamcqr.chocolatequestrepoured.client.models.armor.ModelArmorHeavyIron;
 import com.teamcqr.chocolatequestrepoured.client.models.armor.ModelArmorTransparent;
 import com.teamcqr.chocolatequestrepoured.client.models.armor.ModelBackpack;
-import com.teamcqr.chocolatequestrepoured.client.models.armor.ModelTurtleChestplate;
+import com.teamcqr.chocolatequestrepoured.client.models.armor.ModelArmorTurtle;
 
 public class ModArmorModels {
 
 	public static ModelBackpack backpack = new ModelBackpack(0.0F);
 	public static ModelArmorTransparent slimeArmor = new ModelArmorTransparent(1.0F);
 	public static ModelArmorTransparent slimeArmorLegs = new ModelArmorTransparent(0.5F);
-	public static ModelTurtleChestplate turtleArmor = new ModelTurtleChestplate(1F);
-	public static ModelTurtleChestplate turtleArmorLegs = new ModelTurtleChestplate(0.5F);
+	public static ModelArmorTurtle turtleArmor = new ModelArmorTurtle(1F);
+	public static ModelArmorTurtle turtleArmorLegs = new ModelArmorTurtle(0.5F);
 	public static ModelArmorHeavyIron heavyIronArmor = new ModelArmorHeavyIron(1F);
 	public static ModelArmorHeavyIron heavyIronArmorLegs = new ModelArmorHeavyIron(0.5F);
 
