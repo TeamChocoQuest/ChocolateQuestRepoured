a23cf9442aadd5c37a9ec6e9a0f7137ccd848d62
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-14 16:55:34 +0100

    changed scale for turtle armor

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/init/ModArmorModels.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/init/ModArmorModels.java
@@ -10,8 +10,8 @@ public class ModArmorModels {
 	public static ModelBackpack backpack = new ModelBackpack(0.0F);
 	public static ModelArmorTransparent slimeArmor = new ModelArmorTransparent(1.0F);
 	public static ModelArmorTransparent slimeArmorLegs = new ModelArmorTransparent(0.5F);
-	public static ModelArmorTurtle turtleArmor = new ModelArmorTurtle(1F);
-	public static ModelArmorTurtle turtleArmorLegs = new ModelArmorTurtle(0.5F);
+	public static ModelArmorTurtle turtleArmor = new ModelArmorTurtle(0.5F);
+	public static ModelArmorTurtle turtleArmorLegs = new ModelArmorTurtle(0.25F);
 	public static ModelArmorHeavyIron heavyIronArmor = new ModelArmorHeavyIron(1F);
 	public static ModelArmorHeavyIron heavyIronArmorLegs = new ModelArmorHeavyIron(0.5F);
 
