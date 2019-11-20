4242ce9bff77ef9e1b9bd401a171532c719b3811
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-20 21:41:52 +0100

    stronghold WIP & NYI

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/stronghold/rooms/StrongholdFloor.java
@@ -0,0 +1,23 @@
+package com.teamcqr.chocolatequestrepoured.structuregen.generators.stronghold.rooms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StrongholdFloor {
+	
+	private int floorY;
+	private int roomCount;
+	
+	private List<AbstractStrongholdRoom> rooms = new ArrayList<>();
+	private List<AbstractStrongholdRoom> roomsWithFreeNeighbors = new ArrayList<>();
+
+	public StrongholdFloor(int roomCount) {
+		this.roomCount = roomCount;
+	}
+	
+	public int getRoomSize() {
+		//TODO Calculate this
+		return 16;
+	}
+
+}
