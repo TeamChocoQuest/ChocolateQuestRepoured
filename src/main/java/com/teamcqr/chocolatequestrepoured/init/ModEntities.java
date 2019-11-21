493e08368a288eeaad1966de0883a894631cbe47
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-21 21:05:28 +0100

    began lich boss

--- src/main/java/com/teamcqr/chocolatequestrepoured/init/ModEntities.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/init/ModEntities.java
@@ -6,6 +6,7 @@ import javax.annotation.Nonnull;
 
 import com.teamcqr.chocolatequestrepoured.objects.entity.EntitySlimePart;
 import com.teamcqr.chocolatequestrepoured.objects.entity.boss.EntityCQRGiantTortoise;
+import com.teamcqr.chocolatequestrepoured.objects.entity.boss.EntityCQRLich;
 import com.teamcqr.chocolatequestrepoured.objects.entity.boss.EntityCQRNetherDragon;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRDummy;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRDwarf;
@@ -87,6 +88,7 @@ public class ModEntities {
 
 	public static final EntityEntry NETHER_DRAGON = Null();
 	public static final EntityEntry GIANT_TORTOISE = Null();
+	public static final EntityEntry LICH = Null();
 
 	@EventBusSubscriber(modid = Reference.MODID)
 	public static class EntityRegistrationHandler {
@@ -130,7 +132,8 @@ public class ModEntities {
 					
 					//Bosses
 					createEntityEntry(EntityCQRNetherDragon.class, "nether_dragon", 64, 1, true, 3289650, 000000),
-					createEntityEntry(EntityCQRGiantTortoise.class, "giant_tortoise", 64, 1, true, 3289650, 000000)
+					createEntityEntry(EntityCQRGiantTortoise.class, "giant_tortoise", 64, 1, true, 3289650, 000000),
+					createEntityEntry(EntityCQRLich.class, "lich", 64,1,true, 3289650,000000)
 			};
 
 			event.getRegistry().registerAll(entityEntries);
