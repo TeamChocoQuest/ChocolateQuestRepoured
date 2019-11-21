24a823886856ccfd016a48e365b7573659180f22
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-21 20:51:03 +0100

    base renderer for lich, necromancer, pig mage

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/boss/RenderCQRMage.java
@@ -0,0 +1,29 @@
+package com.teamcqr.chocolatequestrepoured.client.render.entity.boss;
+
+import com.teamcqr.chocolatequestrepoured.client.render.entity.RenderCQREntity;
+import com.teamcqr.chocolatequestrepoured.objects.entity.boss.AbstractEntityCQRMageBase;
+import com.teamcqr.chocolatequestrepoured.util.Reference;
+
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderCQRMage extends RenderCQREntity<AbstractEntityCQRMageBase> {
+	
+	public static final ResourceLocation TEXTURES_HIDDEN = new ResourceLocation((Reference.MODID + ":textures/entity/boss/mages_black.png"));
+	public static final ResourceLocation TEXTURES_REVEALED = new ResourceLocation((Reference.MODID + ":textures/entity/boss/mages.png"));
+
+	public RenderCQRMage(RenderManager rendermanagerIn, ModelBiped model, float shadowSize, String entityName,
+			double widthScale, double heightScale) {
+		super(rendermanagerIn, model, shadowSize, entityName, widthScale, heightScale);
+	}
+	
+	@Override
+	protected ResourceLocation getEntityTexture(AbstractEntityCQRMageBase entity) {
+		if(entity.isIdentityHidden()) {
+			return TEXTURES_HIDDEN;
+		}
+		return TEXTURES_REVEALED;
+	}
+
+}
