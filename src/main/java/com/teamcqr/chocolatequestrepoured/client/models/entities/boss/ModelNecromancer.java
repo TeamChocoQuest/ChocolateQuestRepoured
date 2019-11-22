979d2f8a524705d7430de64cd3c7202eab6e6403
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-22 13:04:58 +0100

    adapted models to CQR Biped

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/boss/ModelNecromancer.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/boss/ModelNecromancer.java
@@ -1,6 +1,7 @@
 package com.teamcqr.chocolatequestrepoured.client.models.entities.boss;
 
-import net.minecraft.client.model.ModelBase;
+import com.teamcqr.chocolatequestrepoured.client.models.entities.ModelCQRBiped;
+
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
@@ -8,13 +9,7 @@ import net.minecraft.entity.Entity;
  * Necromancer - Arlo The Epic
  * Created using Tabula 7.0.1
  */
-public class ModelNecromancer extends ModelBase {
-    public ModelRenderer Torso;
-    public ModelRenderer Head;
-    public ModelRenderer Arm_Left;
-    public ModelRenderer Arm_Right;
-    public ModelRenderer Leg_Right;
-    public ModelRenderer Leg_Left;
+public class ModelNecromancer extends ModelCQRBiped {
     public ModelRenderer Leg_Cape;
     public ModelRenderer Buckle;
     public ModelRenderer Nose;
@@ -25,9 +20,13 @@ public class ModelNecromancer extends ModelBase {
     public ModelRenderer Hood_Right;
     public ModelRenderer Hood_Front;
 
-    public ModelNecromancer() {
-        this.textureWidth = 128;
-        this.textureHeight = 64;
+    public ModelNecromancer(float modelSize) {
+		this(modelSize, 0F, 128, 64);
+	}
+    
+    public ModelNecromancer(float size, float p_i1149_2_, int width, int height) {
+    	super(size, p_i1149_2_, width, height, false);
+    	
         this.Buckle = new ModelRenderer(this, 78, 2);
         this.Buckle.setRotationPoint(0.0F, 12.0F, -2.1F);
         this.Buckle.addBox(-2.0F, -2.0F, 0.0F, 4, 4, 0, 0.0F);
@@ -38,13 +37,13 @@ public class ModelNecromancer extends ModelBase {
         this.Leg_Cape.setRotationPoint(0.0F, 12.0F, 2.0F);
         this.Leg_Cape.addBox(-4.0F, 0.0F, 0.0F, 8, 12, 0, 0.0F);
         this.setRotateAngle(Leg_Cape, 0.2792526803190927F, 0.0F, 0.0F);
-        this.Head = new ModelRenderer(this, 0, 0);
-        this.Head.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.Head.addBox(-4.0F, -10.0F, -4.0F, 8, 10, 8, 0.0F);
-        this.setRotateAngle(Head, 0.0F, 0.017453292519943295F, 0.0F);
-        this.Arm_Right = new ModelRenderer(this, 16, 34);
-        this.Arm_Right.setRotationPoint(-4.0F, 2.0F, 0.0F);
-        this.Arm_Right.addBox(-4.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
+        this.bipedHead = new ModelRenderer(this, 0, 0);
+        this.bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.bipedHead.addBox(-4.0F, -10.0F, -4.0F, 8, 10, 8, 0.0F);
+        this.setRotateAngle(bipedHead, 0.0F, 0.017453292519943295F, 0.0F);
+        this.bipedRightArm = new ModelRenderer(this, 16, 34);
+        this.bipedRightArm.setRotationPoint(-4.0F, 2.0F, 0.0F);
+        this.bipedRightArm.addBox(-4.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
         this.Eyes = new ModelRenderer(this, 54, 18);
         this.Eyes.setRotationPoint(0.0F, -4.0F, -4.1F);
         this.Eyes.addBox(-3.0F, 0.0F, 0.0F, 6, 1, 0, 0.0F);
@@ -54,46 +53,46 @@ public class ModelNecromancer extends ModelBase {
         this.Hood_Top = new ModelRenderer(this, 24, 14);
         this.Hood_Top.setRotationPoint(0.0F, -10.5F, 0.0F);
         this.Hood_Top.addBox(-4.5F, 0.0F, -9.0F, 9, 0, 9, 0.0F);
-        this.Arm_Left = new ModelRenderer(this, 16, 34);
-        this.Arm_Left.setRotationPoint(4.0F, 2.0F, 0.0F);
-        this.Arm_Left.addBox(-0.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
-        this.Leg_Right = new ModelRenderer(this, 0, 34);
-        this.Leg_Right.setRotationPoint(-2.0F, 12.0F, 0.0F);
-        this.Leg_Right.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
-        this.Torso = new ModelRenderer(this, 0, 18);
-        this.Torso.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.Torso.addBox(-4.0F, 0.0F, -2.0F, 8, 12, 4, 0.0F);
+        this.bipedLeftArm = new ModelRenderer(this, 16, 34);
+        this.bipedLeftArm.setRotationPoint(4.0F, 2.0F, 0.0F);
+        this.bipedLeftArm.addBox(-0.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
+        this.bipedRightLeg = new ModelRenderer(this, 0, 34);
+        this.bipedRightLeg.setRotationPoint(-2.0F, 12.0F, 0.0F);
+        this.bipedRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
+        this.bipedBody = new ModelRenderer(this, 0, 18);
+        this.bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.bipedBody.addBox(-4.0F, 0.0F, -2.0F, 8, 12, 4, 0.0F);
         this.Hood_Right = new ModelRenderer(this, 24, 14);
         this.Hood_Right.setRotationPoint(-4.5F, 0.0F, 0.0F);
         this.Hood_Right.addBox(0.0F, 0.0F, -9.0F, 0, 11, 9, 0.0F);
         this.Hood_Left = new ModelRenderer(this, 24, 14);
         this.Hood_Left.setRotationPoint(4.5F, 0.0F, 0.0F);
         this.Hood_Left.addBox(0.0F, 0.0F, -9.0F, 0, 11, 9, 0.0F);
-        this.Leg_Left = new ModelRenderer(this, 0, 34);
-        this.Leg_Left.setRotationPoint(2.0F, 12.0F, 0.0F);
-        this.Leg_Left.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
+        this.bipedLeftLeg = new ModelRenderer(this, 0, 34);
+        this.bipedLeftLeg.setRotationPoint(2.0F, 12.0F, 0.0F);
+        this.bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
         this.Nose = new ModelRenderer(this, 24, 2);
         this.Nose.setRotationPoint(0.0F, -3.0F, -4.0F);
         this.Nose.addBox(-1.0F, 0.0F, -2.0F, 2, 4, 2, 0.0F);
-        this.Torso.addChild(this.Buckle);
+        this.bipedBody.addChild(this.Buckle);
         this.Hood_Top.addChild(this.Hood_Front);
-        this.Torso.addChild(this.Leg_Cape);
-        this.Torso.addChild(this.Head);
-        this.Torso.addChild(this.Arm_Right);
-        this.Head.addChild(this.Eyes);
-        this.Head.addChild(this.Hood_Rear);
+        this.bipedBody.addChild(this.Leg_Cape);
+        this.bipedBody.addChild(this.bipedHead);
+        this.bipedBody.addChild(this.bipedRightArm);
+        this.bipedHead.addChild(this.Eyes);
+        this.bipedHead.addChild(this.Hood_Rear);
         this.Hood_Rear.addChild(this.Hood_Top);
-        this.Torso.addChild(this.Arm_Left);
-        this.Torso.addChild(this.Leg_Right);
+        this.bipedBody.addChild(this.bipedLeftArm);
+        this.bipedBody.addChild(this.bipedRightLeg);
         this.Hood_Top.addChild(this.Hood_Right);
         this.Hood_Top.addChild(this.Hood_Left);
-        this.Torso.addChild(this.Leg_Left);
-        this.Head.addChild(this.Nose);
+        this.bipedBody.addChild(this.bipedLeftLeg);
+        this.bipedHead.addChild(this.Nose);
     }
 
     @Override
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
-        this.Torso.render(f5);
+        this.bipedBody.render(f5);
     }
 
     /**
