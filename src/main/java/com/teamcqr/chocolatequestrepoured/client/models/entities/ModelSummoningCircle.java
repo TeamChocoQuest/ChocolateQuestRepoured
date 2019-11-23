d6f2798c4350dcf7bd845fb3e55b7a3b8d852d1b
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-23 12:35:39 +0100

    summoning circle model + textures

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/ModelSummoningCircle.java
@@ -0,0 +1,43 @@
+package com.teamcqr.chocolatequestrepoured.client.models.entities;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.Entity;
+
+/**
+ * SummoningCircle - DerToaster
+ * Created using Tabula 7.0.1
+ */
+public class ModelSummoningCircle extends ModelBase {
+    public ModelRenderer circle;
+
+    public ModelSummoningCircle(float scale) {
+        this.textureWidth = 128;
+        this.textureHeight = 64;
+        this.circle = new ModelRenderer(this, -64, 0);
+        this.circle.setRotationPoint(0.0F, 24.0F, 0.0F);
+        this.circle.addBox(-32.0F, 0.0F, -32.0F, 64, 0, 64, scale);
+    }
+
+    @Override
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(this.circle.offsetX, this.circle.offsetY, this.circle.offsetZ);
+        GlStateManager.translate(this.circle.rotationPointX * f5, this.circle.rotationPointY * f5, this.circle.rotationPointZ * f5);
+        GlStateManager.scale(0.5D, 1.0D, 0.5D);
+        GlStateManager.translate(-this.circle.offsetX, -this.circle.offsetY, -this.circle.offsetZ);
+        GlStateManager.translate(-this.circle.rotationPointX * f5, -this.circle.rotationPointY * f5, -this.circle.rotationPointZ * f5);
+        this.circle.render(f5);
+        GlStateManager.popMatrix();
+    }
+
+    /**
+     * This is a helper function from Tabula to set the rotation of model parts
+     */
+    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+        modelRenderer.rotateAngleX = x;
+        modelRenderer.rotateAngleY = y;
+        modelRenderer.rotateAngleZ = z;
+    }
+}
