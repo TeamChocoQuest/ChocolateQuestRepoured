6791bce7f9e567d73e50e1eebe8d4c30d96c2941
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-24 12:06:44 +0100

    began AI of lich

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/boss/lich/BossAISummonZombie.java
@@ -0,0 +1,124 @@
+package com.teamcqr.chocolatequestrepoured.objects.entity.ai.boss.lich;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.teamcqr.chocolatequestrepoured.objects.entity.ECQREntityArmPoses;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.AbstractCQREntityAI;
+import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR;
+import com.teamcqr.chocolatequestrepoured.objects.entity.misc.EntitySummoningCircle;
+import com.teamcqr.chocolatequestrepoured.objects.entity.misc.EntitySummoningCircle.ECircleTexture;
+import com.teamcqr.chocolatequestrepoured.util.Reference;
+import com.teamcqr.chocolatequestrepoured.util.VectorUtil;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+
+public class BossAISummonZombie extends AbstractCQREntityAI {
+
+	private final int COOLDOWN = 20;
+	private final int ANIMATION_TIME = 40;
+	private int currentCooldownValue = COOLDOWN;
+	private int currentAnimationTime = 0;
+	private int maxMinions = 5;
+	
+	private List<Entity> summonedMinions = new ArrayList<>();
+	private boolean animationRunning;
+	
+	public BossAISummonZombie(AbstractEntityCQR entity) {
+		super(entity);
+	}
+
+	@Override
+	public boolean shouldExecute() {
+		if(entity == null || entity.isDead || entity.getAttackTarget() == null) {
+			return false;
+		}
+		if(this.animationRunning) {
+			currentAnimationTime++;
+			if(currentAnimationTime >= ANIMATION_TIME) {
+				this.entity.setArmPose(ECQREntityArmPoses.HOLDING_ITEM);
+				this.currentAnimationTime = 0;
+				this.animationRunning = false;
+			} else {
+				this.entity.setVelocity(0, 0, 0);
+				this.entity.setArmPose(ECQREntityArmPoses.SPELLCASTING);
+			}
+		}
+		currentCooldownValue--;
+		//System.out.println("Current value: " + currentCooldownValue);
+		if(currentCooldownValue <= 0) {
+			return (summonedMinions.isEmpty() || getAliveMinionCount() < maxMinions);
+		}
+		return false;
+	}
+	
+	private int getAliveMinionCount() {
+		if(summonedMinions.isEmpty()) {
+			return 0;
+		}
+		int aliveMinions = 0;
+		List<Entity> toRemove = new ArrayList<>();
+		for(Entity minio : summonedMinions) {
+			if(minio != null && !minio.isDead) {
+				aliveMinions++;
+			} else {
+				toRemove.add(minio);
+			}
+		}
+		summonedMinions.removeAll(toRemove);
+		return aliveMinions;
+	}
+	
+	@Override
+	public void updateTask() {
+		if(!shouldContinueExecuting()) {
+			return;
+		}
+		currentCooldownValue = COOLDOWN;
+		Vec3d vector = entity.getLookVec().normalize();
+		vector = vector.add(vector).add(vector).add(vector);
+		int minionCount = maxMinions - getAliveMinionCount();
+		if(minionCount > 0) {
+			double angle = 180D / (double)minionCount;
+			vector = VectorUtil.rotateVectorAroundY(vector, 270);
+			BlockPos[] spawnPositions = new BlockPos[minionCount];
+			for(int i = 0; i < minionCount; i++) {
+				spawnPositions[i] = entity.getPosition().add(new BlockPos(VectorUtil.rotateVectorAroundY(vector, angle*i)));
+			}
+			for(BlockPos p : spawnPositions) {
+				if(entity.getNavigator().getPathToPos(p) != null) {
+					System.out.println("Pos: " + p.toString());
+					ResourceLocation summon = new ResourceLocation(Reference.MODID, "zombie");
+					EntitySummoningCircle circle = new EntitySummoningCircle(entity.world, summon, 1.1F, ECircleTexture.ZOMBIE);
+					circle.setSummon(summon);
+					circle.setLocationAndAngles(p.getX(), entity.posY +0.05, p.getZ(), 0F, 0F);
+					circle.setPositionAndUpdate(p.getX(), p.getY(), p.getZ());
+					summonedMinions.add(circle);
+				}
+			}
+			this.currentAnimationTime = 0;
+			this.animationRunning = true;
+			this.entity.setArmPose(ECQREntityArmPoses.SPELLCASTING);
+			this.currentAnimationTime++;
+			this.currentCooldownValue = 2* COOLDOWN;
+		}
+	}
+	
+	@Override
+	public boolean shouldContinueExecuting() {
+		return shouldExecute();
+	}
+	
+	@Override
+	public void resetTask() {
+		this.summonedMinions.clear();
+		this.currentAnimationTime = ANIMATION_TIME;
+		this.currentCooldownValue = COOLDOWN;
+		this.entity.setArmPose(ECQREntityArmPoses.HOLDING_ITEM);
+		this.animationRunning = false;
+	}
+
+}
--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRLich.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRLich.java
@@ -3,7 +3,14 @@ package com.teamcqr.chocolatequestrepoured.objects.entity.boss;
 import com.teamcqr.chocolatequestrepoured.factions.EFaction;
 import com.teamcqr.chocolatequestrepoured.objects.entity.EBaseHealths;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ELootTablesBoss;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIAttack;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAICQRNearestAttackTarget;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIHealingPotion;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIIdleSit;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIMoveToHome;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.boss.lich.BossAISummonZombie;
 
+import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.BossInfo.Color;
 import net.minecraft.world.BossInfo.Overlay;
@@ -24,6 +31,18 @@ public class EntityCQRLich extends AbstractEntityCQRMageBase {
 		bossInfoServer.setOverlay(Overlay.PROGRESS);
 		
 		setSize(0.6F, 1.8F);
+	}
+	
+	@Override
+	protected void initEntityAI() {
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(5, new EntityAIHealingPotion(this));
+		this.tasks.addTask(8, new BossAISummonZombie(this));
+		this.tasks.addTask(10, new EntityAIAttack(this));
+		this.tasks.addTask(20, new EntityAIMoveToHome(this));
+		this.tasks.addTask(21, new EntityAIIdleSit(this));
+
+		this.targetTasks.addTask(0, new EntityAICQRNearestAttackTarget(this));
 	}
 	
 	@Override
