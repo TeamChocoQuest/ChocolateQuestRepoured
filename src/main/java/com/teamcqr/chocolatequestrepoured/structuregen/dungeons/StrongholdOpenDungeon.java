ff90859b98fef14027267c65b1797bcc49b2bd60
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-11-24 17:38:20 +0100

    added fields + getters for them

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/dungeons/StrongholdOpenDungeon.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/dungeons/StrongholdOpenDungeon.java
@@ -1,11 +1,16 @@
 package com.teamcqr.chocolatequestrepoured.structuregen.dungeons;
 
 import java.io.File;
+import java.util.Properties;
 
 import com.teamcqr.chocolatequestrepoured.structuregen.DungeonBase;
 import com.teamcqr.chocolatequestrepoured.structuregen.generators.IDungeonGenerator;
 import com.teamcqr.chocolatequestrepoured.structuregen.generators.stronghold.StrongholdOpenGenerator;
 import com.teamcqr.chocolatequestrepoured.util.DungeonGenUtils;
+import com.teamcqr.chocolatequestrepoured.util.PropertyFileHelper;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 
 /**
  * Copyright (c) 29.04.2019
@@ -25,12 +30,41 @@ public class StrongholdOpenDungeon extends DungeonBase {
 	private int minRoomsPerFloor = 7;
 	private int maxRoomsPerFloor = 10;
 	
+	private int roomSizeX = 17;
+	private int roomSizeY = 10;
+	private int roomSizeZ = 17;
+	
+	private Block wallBlock = Blocks.STONEBRICK;
+	
 	//Important: All rooms must have the same dimensions!!!
 
 	//Generator for 1.7 release strongholds -> not linear, but open strongholds, for old strongholds: see linearDungeon
 	
 	public StrongholdOpenDungeon(File configFile) {
 		super(configFile);
+		Properties prop = loadConfig(configFile);
+		if(prop != null) {
+			stairFolder = PropertyFileHelper.getFileProperty(prop, "stairFolder", "strongholds/open/stairs");
+			entranceStairFolder = PropertyFileHelper.getFileProperty(prop, "entranceStairFolder", "strongholds/open/entrance/stairs");
+			entranceBuildingFolder = PropertyFileHelper.getFileProperty(prop, "entranceBuildingFolder", "strongholds/open/entrance/buildings");
+			roomFolder = PropertyFileHelper.getFileProperty(prop, "roomFolder", "strongholds/open/rooms");
+			bossRoomFolder = PropertyFileHelper.getFileProperty(prop, "bossRoomFolder", "strongholds/open/boss");
+			
+			minFloors = PropertyFileHelper.getIntProperty(prop, "minFloors", 2);
+			maxFloors = PropertyFileHelper.getIntProperty(prop, "maxFloors", 4);
+			minRoomsPerFloor = PropertyFileHelper.getIntProperty(prop, "minRoomsPerFloor", 4);
+			maxRoomsPerFloor = PropertyFileHelper.getIntProperty(prop, "maxRoomsPerFloor", 16);
+			
+			roomSizeX = PropertyFileHelper.getIntProperty(prop, "roomSizeX", 17);
+			roomSizeY = PropertyFileHelper.getIntProperty(prop, "roomSizeY", 10);
+			roomSizeZ = PropertyFileHelper.getIntProperty(prop, "roomSizeZ", 17);
+			
+			wallBlock = PropertyFileHelper.getBlockProperty(prop, "wallBlock", Blocks.STONEBRICK);
+			
+			closeConfigFile();
+		} else {
+			registeredSuccessful = false;
+		}
 	}
 
 	@Override
@@ -99,5 +133,17 @@ public class StrongholdOpenDungeon extends DungeonBase {
 	}
 	public File getEntranceStair() {
 		return getStructureFileFromDirectory(entranceStairFolder);
+	}
+	public int getRoomSizeX() {
+		return roomSizeX;
+	}
+	public int getRoomSizeY() {
+		return roomSizeY;
+	}
+	public int getRoomSizeZ() {
+		return roomSizeZ;
+	}
+	public Block getWallBlock() {
+		return wallBlock;
 	}
 }
