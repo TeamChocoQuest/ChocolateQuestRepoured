9bb0612724344f68c96b9b8a0966e1a9660399be
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-18 14:20:42 +0100

    default color for armors

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/armor/ItemArmorDyable.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/armor/ItemArmorDyable.java
@@ -1,26 +1,105 @@
 package com.teamcqr.chocolatequestrepoured.objects.items.armor;
 
-import net.minecraft.client.model.ModelBiped;
-import net.minecraft.entity.EntityLivingBase;
+import com.teamcqr.chocolatequestrepoured.init.ModMaterials;
+
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 public class ItemArmorDyable extends ItemArmor {
 
 	public ItemArmorDyable(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
 		super(materialIn, renderIndexIn, equipmentSlotIn);
 	}
-	
+
 	@Override
 	public boolean hasOverlay(ItemStack stack) {
 		return true;
 	}
-	
-	@Override
-	public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot,
-			ModelBiped _default) {
-		return super.getArmorModel(entityLiving, itemStack, armorSlot, _default);
+
+	/**
+	 * Return whether the specified armor ItemStack has a color.
+	 */
+	public boolean hasColor(ItemStack stack) {
+		if (getArmorMaterial() == ModMaterials.ArmorMaterials.DIAMOND_DYABLE
+				|| getArmorMaterial() == ModMaterials.ArmorMaterials.IRON_DYABLE) {
+			NBTTagCompound nbttagcompound = stack.getTagCompound();
+			return nbttagcompound != null && nbttagcompound.hasKey("display", 10)
+					? nbttagcompound.getCompoundTag("display").hasKey("color", 3)
+					: false;
+		} else {
+			return super.hasColor(stack);
+		}
+	}
+
+	/**
+	 * Return the color for the specified armor ItemStack.
+	 */
+	public int getColor(ItemStack stack) {
+		if (getArmorMaterial() == ModMaterials.ArmorMaterials.DIAMOND_DYABLE
+				|| getArmorMaterial() == ModMaterials.ArmorMaterials.IRON_DYABLE) {
+			NBTTagCompound nbttagcompound = stack.getTagCompound();
+
+			if (nbttagcompound != null) {
+				NBTTagCompound nbttagcompound1 = nbttagcompound.getCompoundTag("display");
+
+				if (nbttagcompound1 != null && nbttagcompound1.hasKey("color", 3)) {
+					return nbttagcompound1.getInteger("color");
+				}
+			}
+
+			if(getArmorMaterial() == ModMaterials.ArmorMaterials.DIAMOND_DYABLE) {
+				return 65535;
+			} else {
+				return 13421772;
+			}
+		} else {
+			return super.getColor(stack);
+		}
+	}
+
+	/**
+	 * Remove the color from the specified armor ItemStack.
+	 */
+	public void removeColor(ItemStack stack) {
+		if (getArmorMaterial() == ModMaterials.ArmorMaterials.DIAMOND_DYABLE
+				|| getArmorMaterial() == ModMaterials.ArmorMaterials.IRON_DYABLE) {
+			NBTTagCompound nbttagcompound = stack.getTagCompound();
+
+			if (nbttagcompound != null) {
+				NBTTagCompound nbttagcompound1 = nbttagcompound.getCompoundTag("display");
+
+				if (nbttagcompound1.hasKey("color")) {
+					nbttagcompound1.removeTag("color");
+				}
+			}
+		}
+	}
+
+	/**
+	 * Sets the color of the specified armor ItemStack
+	 */
+	public void setColor(ItemStack stack, int color) {
+		if (getArmorMaterial() == ModMaterials.ArmorMaterials.DIAMOND_DYABLE
+				|| getArmorMaterial() == ModMaterials.ArmorMaterials.IRON_DYABLE) {
+			NBTTagCompound nbttagcompound = stack.getTagCompound();
+
+			if (nbttagcompound == null) {
+				nbttagcompound = new NBTTagCompound();
+				stack.setTagCompound(nbttagcompound);
+			}
+
+			NBTTagCompound nbttagcompound1 = nbttagcompound.getCompoundTag("display");
+
+			if (!nbttagcompound.hasKey("display", 10)) {
+				nbttagcompound.setTag("display", nbttagcompound1);
+			}
+
+			nbttagcompound1.setInteger("color", color);
+		} else {
+			super.setColor(stack, color);
+		}
 	}
 
 }
