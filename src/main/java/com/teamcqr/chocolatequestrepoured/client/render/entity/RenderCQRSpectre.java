bd763f985a63e141d4df6934c438503669111fd0
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-20 19:31:13 +0100

    model & renderer for spectres (!Untested!)

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRSpectre.java
@@ -0,0 +1,35 @@
+package com.teamcqr.chocolatequestrepoured.client.render.entity;
+
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRSpectre;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderManager;
+
+public class RenderCQRSpectre extends RenderCQREntity<EntityCQRSpectre> {
+
+	public RenderCQRSpectre(RenderManager rendermanagerIn, ModelBase model, float shadowSize, String entityName,
+			double widthScale, double heightScale) {
+		super(rendermanagerIn, model, shadowSize, entityName, widthScale, heightScale);
+	}
+	
+	@Override
+	public void doRender(EntityCQRSpectre entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		GlStateManager.pushMatrix();
+		
+		GlStateManager.enableAlpha();
+		GlStateManager.enableBlend();
+		
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 0.5F);
+		
+		super.doRender(entity, x, y, z, entityYaw, partialTicks);
+		
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+		
+		GlStateManager.disableAlpha();
+		GlStateManager.disableBlend();
+		
+		GlStateManager.popMatrix();
+	}
+
+}
