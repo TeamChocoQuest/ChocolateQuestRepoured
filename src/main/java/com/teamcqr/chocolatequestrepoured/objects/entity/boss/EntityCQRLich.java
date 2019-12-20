17684542b23838adc4f1e94e7d27c6d5f88a74f6
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-20 20:55:59 +0100

    poison spell

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRLich.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRLich.java
@@ -12,6 +12,7 @@ import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIHealingPotio
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIIdleSit;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIMoveToHome;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.spells.EntityAIFangAttack;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.spells.EntityAIShootPoisonProjectiles;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.spells.EntityAISummonMinionSpell;
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.ISummoner;
 
@@ -61,8 +62,8 @@ public class EntityCQRLich extends AbstractEntityCQRMageBase implements ISummone
 	protected void initEntityAI() {
 		this.tasks.addTask(0, new EntityAISwimming(this));
 		this.tasks.addTask(5, new EntityAIHealingPotion(this));
-		//this.tasks.addTask(8, new BossAISummonZombie(this));
 		this.tasks.addTask(6, new EntityAIFangAttack(this));
+		this.tasks.addTask(7, new EntityAIShootPoisonProjectiles(this));
 		this.tasks.addTask(8, new EntityAISummonMinionSpell(this));
 		this.tasks.addTask(10, new EntityAIAttack(this));
 		this.tasks.addTask(20, new EntityAIMoveToHome(this));
