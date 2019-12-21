4759557a8c99ff90c9b600c0339b3bb805b7d08e
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-21 11:05:35 +0100

    phylactery block

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/blocks/BlockPhylactery.java
@@ -0,0 +1,69 @@
+package com.teamcqr.chocolatequestrepoured.objects.blocks;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockPhylactery extends Block {
+
+	public BlockPhylactery(Material materialIn) {
+		super(materialIn);
+		this.setHardness(0.5F);
+		this.setLightOpacity(8);
+		this.setLightLevel(1.5F);
+		setSoundType(SoundType.GLASS);
+	}
+
+	public BlockPhylactery(Material blockMaterialIn, MapColor blockMapColorIn) {
+		super(blockMaterialIn, blockMapColorIn);
+		this.setHardness(0.5F);
+		this.setLightOpacity(8);
+		this.setLightLevel(1.5F);
+		setSoundType(SoundType.GLASS);
+	}
+	
+	@Override
+	public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state) {
+		super.onBlockDestroyedByPlayer(worldIn, pos, state);
+		worldIn.createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 1.5F, true);
+	}
+	
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return Items.AIR;
+	}
+	
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return EnumBlockRenderType.MODEL;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.CUTOUT;
+	}
+
+}
