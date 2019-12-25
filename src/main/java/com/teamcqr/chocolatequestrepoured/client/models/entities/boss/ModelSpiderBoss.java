85c161467f0a1d04ff86712efb914818fbaf07df
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-25 13:56:34 +0100

    moved boss models to right folder

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/boss/ModelSpiderBoss.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/boss/ModelSpiderBoss.java
@@ -1,4 +1,4 @@
-package com.teamcqr.chocolatequestrepoured.client.models.entities;
+package com.teamcqr.chocolatequestrepoured.client.models.entities.boss;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
