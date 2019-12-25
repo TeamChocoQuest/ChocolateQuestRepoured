b581e797e18a6e16d843f40c5f74b3366e57133a
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-25 17:35:44 +0100

    updated model

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/ModelCQRPollo.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/ModelCQRPollo.java
@@ -3,6 +3,7 @@ package com.teamcqr.chocolatequestrepoured.client.models.entities;
 import net.minecraft.client.model.ModelChicken;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
 
 /**
  * ModelChicken - DerToaster98
@@ -56,9 +57,30 @@ public class ModelCQRPollo extends ModelChicken {
         this.chin.isHidden = true;
     }
 
-    @Override
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
-        this.body.render(f5);
+    /**
+     * Sets the models various rotation angles then renders the model.
+     */
+    public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale)
+    {
+        this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entityIn);
+        this.body.render(scale);
+    }
+
+    /**
+     * Sets the model's various rotation angles. For bipeds, par1 and par2 are used for animating the movement of arms
+     * and legs, where par1 represents the time(so that arms and legs swing back and forth) and par2 represents how
+     * "far" arms and legs can swing at most.
+     */
+    public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn)
+    {
+        this.head.rotateAngleX = headPitch * 0.017453292F + new Float(Math.PI);
+        this.head.rotateAngleY = netHeadYaw * 0.017453292F;
+        this.rightLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 0.7F * limbSwingAmount;
+        this.leftLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 0.7F * limbSwingAmount;
+        if(entityIn.isAirBorne) {
+        	this.rightWing.rotateAngleZ = ageInTicks;
+            this.leftWing.rotateAngleZ = -ageInTicks;
+        }
     }
 
     /**
