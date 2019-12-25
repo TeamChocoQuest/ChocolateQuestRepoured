99cdad7d1038e652e54ffcd2b3517cc5bb717974
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-25 13:33:28 +0100

    fixed illager model + improvements on illager entity

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/ModelCQRIllager.java
@@ -0,0 +1,170 @@
+package com.teamcqr.chocolatequestrepoured.client.models.entities;
+
+import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRIllager;
+
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.AbstractIllager;
+import net.minecraft.util.EnumHandSide;
+import net.minecraft.util.math.MathHelper;
+
+public class ModelCQRIllager extends ModelCQRBiped {
+
+    public ModelRenderer arms;
+    public ModelRenderer nose;
+	
+	public ModelCQRIllager(float scale) {
+		this(scale, 0F, 64, 64);
+	}
+
+	public ModelRenderer getArm(EnumHandSide hand) {
+		return hand == EnumHandSide.LEFT ? this.bipedLeftArm : this.bipedRightArm;
+	}
+	
+    public ModelCQRIllager(float scaleFactor, float p_i47227_2_, int textureWidthIn, int textureHeightIn)
+    {
+    	super(scaleFactor, false);
+    	this.textureWidth = textureWidthIn;
+    	this.textureHeight = textureHeightIn;
+    	
+        this.bipedHead = (new ModelRenderer(this)).setTextureSize(textureWidthIn, textureHeightIn);
+        this.bipedHead.setRotationPoint(0.0F, 0.0F + p_i47227_2_, 0.0F);
+        this.bipedHead.setTextureOffset(0, 0).addBox(-4.0F, -10.0F, -4.0F, 8, 10, 8, scaleFactor);
+        this.bipedHeadwear = (new ModelRenderer(this, 32, 0)).setTextureSize(textureWidthIn, textureHeightIn);
+        this.bipedHeadwear.addBox(-4.0F, -10.0F, -4.0F, 8, 12, 8, scaleFactor + 0.45F);
+        this.bipedHead.addChild(this.bipedHeadwear);
+        this.bipedHeadwear.showModel = false;
+        this.nose = (new ModelRenderer(this)).setTextureSize(textureWidthIn, textureHeightIn);
+        this.nose.setRotationPoint(0.0F, p_i47227_2_ - 2.0F, 0.0F);
+        this.nose.setTextureOffset(24, 0).addBox(-1.0F, -1.0F, -6.0F, 2, 4, 2, scaleFactor);
+        this.bipedHead.addChild(this.nose);
+        this.bipedBody = (new ModelRenderer(this)).setTextureSize(textureWidthIn, textureHeightIn);
+        this.bipedBody.setRotationPoint(0.0F, 0.0F + p_i47227_2_, 0.0F);
+        this.bipedBody.setTextureOffset(16, 20).addBox(-4.0F, 0.0F, -3.0F, 8, 12, 6, scaleFactor);
+        this.bipedBody.setTextureOffset(0, 38).addBox(-4.0F, 0.0F, -3.0F, 8, 18, 6, scaleFactor + 0.5F);
+        this.arms = (new ModelRenderer(this)).setTextureSize(textureWidthIn, textureHeightIn);
+        this.arms.setRotationPoint(0.0F, 0.0F + p_i47227_2_ + 2.0F, 0.0F);
+        this.arms.setTextureOffset(44, 22).addBox(-8.0F, -2.0F, -2.0F, 4, 8, 4, scaleFactor);
+        ModelRenderer modelrenderer = (new ModelRenderer(this, 44, 22)).setTextureSize(textureWidthIn, textureHeightIn);
+        modelrenderer.mirror = true;
+        modelrenderer.addBox(4.0F, -2.0F, -2.0F, 4, 8, 4, scaleFactor);
+        this.arms.addChild(modelrenderer);
+        this.arms.setTextureOffset(40, 38).addBox(-4.0F, 2.0F, -2.0F, 8, 4, 4, scaleFactor);
+        this.bipedRightLeg = (new ModelRenderer(this, 0, 22)).setTextureSize(textureWidthIn, textureHeightIn);
+        this.bipedRightLeg.setRotationPoint(-2.0F, 12.0F + p_i47227_2_, 0.0F);
+        this.bipedRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, scaleFactor);
+        this.bipedLeftLeg = (new ModelRenderer(this, 0, 22)).setTextureSize(textureWidthIn, textureHeightIn);
+        this.bipedLeftLeg.mirror = true;
+        this.bipedLeftLeg.setRotationPoint(2.0F, 12.0F + p_i47227_2_, 0.0F);
+        this.bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, scaleFactor);
+        this.bipedRightArm = (new ModelRenderer(this, 40, 46)).setTextureSize(textureWidthIn, textureHeightIn);
+        this.bipedRightArm.addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4, scaleFactor);
+        this.bipedRightArm.setRotationPoint(-5.0F, 2.0F + p_i47227_2_, 0.0F);
+        this.bipedLeftArm = (new ModelRenderer(this, 40, 46)).setTextureSize(textureWidthIn, textureHeightIn);
+        this.bipedLeftArm.mirror = true;
+        this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, scaleFactor);
+        this.bipedLeftArm.setRotationPoint(5.0F, 2.0F + p_i47227_2_, 0.0F);
+    }
+
+    /**
+     * Sets the models various rotation angles then renders the model.
+     */
+    @Override
+    public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale)
+    {
+        if(entityIn instanceof EntityCQRIllager) {
+        	this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entityIn);
+            this.bipedHead.render(scale);
+            this.bipedBody.render(scale);
+            this.bipedRightLeg.render(scale);
+            this.bipedLeftLeg.render(scale);
+            EntityCQRIllager abstractillager = (EntityCQRIllager)entityIn;
+
+            if (!abstractillager.isAggressive())
+            {
+                this.arms.render(scale);
+            }
+            else
+            {
+                this.bipedRightArm.render(scale);
+                this.bipedLeftArm.render(scale);
+            }
+        }
+    }
+
+    /**
+     * Sets the model's various rotation angles. For bipeds, par1 and par2 are used for animating the movement of arms
+     * and legs, where par1 represents the time(so that arms and legs swing back and forth) and par2 represents how
+     * "far" arms and legs can swing at most.
+     */
+    @Override
+    public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn)
+    {
+    	if(!(entityIn instanceof EntityCQRIllager)) {
+    		return;
+    	}
+        this.bipedHead.rotateAngleY = netHeadYaw * 0.017453292F;
+        this.bipedHead.rotateAngleX = headPitch * 0.017453292F;
+        this.arms.rotationPointY = 3.0F;
+        this.arms.rotationPointZ = -1.0F;
+        this.arms.rotateAngleX = -0.75F;
+        this.bipedRightLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount * 0.5F;
+        this.bipedLeftLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount * 0.5F;
+        this.bipedRightLeg.rotateAngleY = 0.0F;
+        this.bipedLeftLeg.rotateAngleY = 0.0F;
+        AbstractIllager.IllagerArmPose abstractillager$illagerarmpose = ((EntityCQRIllager)entityIn).getIllagerArmPose();
+
+        if (abstractillager$illagerarmpose == AbstractIllager.IllagerArmPose.ATTACKING)
+        {
+            float f = MathHelper.sin(this.swingProgress * (float)Math.PI);
+            float f1 = MathHelper.sin((1.0F - (1.0F - this.swingProgress) * (1.0F - this.swingProgress)) * (float)Math.PI);
+            this.bipedRightArm.rotateAngleZ = 0.0F;
+            this.bipedLeftArm.rotateAngleZ = 0.0F;
+            this.bipedRightArm.rotateAngleY = 0.15707964F;
+            this.bipedLeftArm.rotateAngleY = -0.15707964F;
+
+            if (((EntityLivingBase)entityIn).getPrimaryHand() == EnumHandSide.RIGHT)
+            {
+                this.bipedRightArm.rotateAngleX = -1.8849558F + MathHelper.cos(ageInTicks * 0.09F) * 0.15F;
+                this.bipedLeftArm.rotateAngleX = -0.0F + MathHelper.cos(ageInTicks * 0.19F) * 0.5F;
+                this.bipedRightArm.rotateAngleX += f * 2.2F - f1 * 0.4F;
+                this.bipedLeftArm.rotateAngleX += f * 1.2F - f1 * 0.4F;
+            }
+            else
+            {
+                this.bipedRightArm.rotateAngleX = -0.0F + MathHelper.cos(ageInTicks * 0.19F) * 0.5F;
+                this.bipedLeftArm.rotateAngleX = -1.8849558F + MathHelper.cos(ageInTicks * 0.09F) * 0.15F;
+                this.bipedRightArm.rotateAngleX += f * 1.2F - f1 * 0.4F;
+                this.bipedLeftArm.rotateAngleX += f * 2.2F - f1 * 0.4F;
+            }
+
+            this.bipedRightArm.rotateAngleZ += MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
+            this.bipedLeftArm.rotateAngleZ -= MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
+            this.bipedRightArm.rotateAngleX += MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
+            this.bipedLeftArm.rotateAngleX -= MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
+        }
+        else if (abstractillager$illagerarmpose == AbstractIllager.IllagerArmPose.SPELLCASTING)
+        {
+            this.bipedRightArm.rotationPointZ = 0.0F;
+            this.bipedRightArm.rotationPointX = -5.0F;
+            this.bipedLeftArm.rotationPointZ = 0.0F;
+            this.bipedLeftArm.rotationPointX = 5.0F;
+            this.bipedRightArm.rotateAngleX = MathHelper.cos(ageInTicks * 0.6662F) * 0.25F;
+            this.bipedLeftArm.rotateAngleX = MathHelper.cos(ageInTicks * 0.6662F) * 0.25F;
+            this.bipedRightArm.rotateAngleZ = 2.3561945F;
+            this.bipedLeftArm.rotateAngleZ = -2.3561945F;
+            this.bipedRightArm.rotateAngleY = 0.0F;
+            this.bipedLeftArm.rotateAngleY = 0.0F;
+        }
+        else if (abstractillager$illagerarmpose == AbstractIllager.IllagerArmPose.BOW_AND_ARROW)
+        {
+            this.bipedRightArm.rotateAngleY = -0.1F + this.bipedHead.rotateAngleY;
+            this.bipedRightArm.rotateAngleX = -((float)Math.PI / 2F) + this.bipedHead.rotateAngleX;
+            this.bipedLeftArm.rotateAngleX = -0.9424779F + this.bipedHead.rotateAngleX;
+            this.bipedLeftArm.rotateAngleY = this.bipedHead.rotateAngleY - 0.4F;
+            this.bipedLeftArm.rotateAngleZ = ((float)Math.PI / 2F);
+        }
+    }
+
+}
--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRIllager.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/RenderCQRIllager.java
@@ -1,15 +1,59 @@
 package com.teamcqr.chocolatequestrepoured.client.render.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.teamcqr.chocolatequestrepoured.client.models.entities.ModelCQRIllager;
+import com.teamcqr.chocolatequestrepoured.client.render.entity.layers.LayerCQRHeldItem;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRIllager;
 
-import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
+import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumHandSide;
 
 public class RenderCQRIllager extends RenderCQREntity<EntityCQRIllager> {
 
-	public RenderCQRIllager(RenderManager rendermanagerIn) {
-		//TODO: Own illager model that extends the biped model
-		super(rendermanagerIn, new ModelBiped(), 0.5F, "entity_mob_cqrillager", 1.0D, 1.0D);
-	}
-
+	public RenderCQRIllager(RenderManager rendermanager)
+    {
+		super(rendermanager, new ModelCQRIllager(0F), 0.5F, "entity_mob_cqrillager", 1.0D, 1.0D);
+		
+		List<LayerRenderer<?>> toRemove = new ArrayList<LayerRenderer<?>>();
+		for (LayerRenderer<?> layer : this.layerRenderers) {
+			if (layer instanceof LayerBipedArmor || layer instanceof LayerHeldItem) {
+				toRemove.add(layer);
+			}
+		}
+		for (LayerRenderer<?> layer : toRemove) {
+			this.layerRenderers.remove(layer);
+		}
+		//TODO: Illager armor
+		/*this.addLayer(new LayerBipedArmor(this) {
+			@Override
+			protected void initArmor() {
+				this.modelLeggings = new ModelCQREndermanArmor(0.5F, true);
+				this.modelArmor = new ModelCQREndermanArmor(1.0F, false);
+			}
+		});*/
+		
+		this.addLayer(new LayerCQRHeldItem(this)
+        {
+        	@Override
+            public void doRenderLayer(EntityLivingBase entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale)
+            {
+                if (entity instanceof EntityCQRIllager && ((EntityCQRIllager) entity).isAggressive())
+                {
+                    super.doRenderLayer(entity, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scale);
+                }
+            }
+        	@Override
+            protected void translateToHand(EnumHandSide hand)
+            {
+                ((ModelCQRIllager)this.livingEntityRenderer.getMainModel()).getArm(hand).postRender(0.0625F);
+            }
+        });
+    }
+    
 }
