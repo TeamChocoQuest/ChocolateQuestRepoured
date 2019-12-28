b8f38f65c29221769ac36abde39b5ff21e9d8e64
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-28 17:15:28 +0100

    update

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/data/ArrayManipulationUtil.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/data/ArrayManipulationUtil.java
@@ -1,10 +1,12 @@
 package com.teamcqr.chocolatequestrepoured.util.data;
 
+import com.teamcqr.chocolatequestrepoured.factions.CQRFaction;
+
 public class ArrayManipulationUtil {
 	
 
-	public static Object[] enlargeArray(Object[] arrayOld, int newSize) {
-		Object[] array = new Object[newSize];
+	public static CQRFaction[] enlargeFactionArray(CQRFaction[] arrayOld, int newSize) {
+		CQRFaction[] array = new CQRFaction[newSize];
 		for(int i = 0; i < newSize; i++) {
 			if(i < arrayOld.length) {
 				array[i] = arrayOld[i];
