87e5df638f11fb9247ffcf80fe618059c76a4c2c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-29 00:54:00 +0100

    saving faction data

--- src/main/java/com/teamcqr/chocolatequestrepoured/factions/CQRFaction.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/factions/CQRFaction.java
@@ -8,19 +8,22 @@ import javax.annotation.Nonnull;
 
 import com.teamcqr.chocolatequestrepoured.factions.EReputationState.EReputationStateRough;
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR;
+import com.teamcqr.chocolatequestrepoured.util.data.FileIOUtil;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.world.EnumDifficulty;
+import net.minecraftforge.common.util.Constants;
 
 public class CQRFaction {
 
 	private boolean savedGlobally = true;
 	private boolean repuMayChange = true;
 	private String name;
-	//private CQRFaction[] allies = new CQRFaction[12];
 	private ArrayList<CQRFaction> allies = new ArrayList<>();
-	//private CQRFaction[] enemies = new CQRFaction[12];
 	private ArrayList<CQRFaction> enemies = new ArrayList<>();
 	private EReputationState defaultRelation;
 	
@@ -79,7 +82,23 @@ public class CQRFaction {
 		return a;
 	}*/
 
-	//TODO: Special case for player faction!!
+	//DONE: Special case for player faction!!
+	public boolean isEnemy(Entity ent) {
+		if(ent.getEntityWorld().getDifficulty().equals(EnumDifficulty.PEACEFUL)) {
+			return false;
+		}
+		if(ent instanceof EntityPlayer) {
+			//Special case for player
+			return FactionRegistry.instance().getReputationOf(ent.getPersistentID(), this) == EReputationStateRough.ENEMY;
+		}
+		return isEnemy(FactionRegistry.instance().getFactionOf(ent));
+	}
+	public boolean isEnemy(AbstractEntityCQR ent) {
+		if(ent.getEntityWorld().getDifficulty().equals(EnumDifficulty.PEACEFUL)) {
+			return false;
+		}
+		return isEnemy(ent.getFaction());
+	}
 	public boolean isEnemy(CQRFaction faction) {
 		if(faction == this || (faction != null && faction.getName().equalsIgnoreCase("ALL_ALLY"))) {
 			return false;
@@ -93,7 +112,17 @@ public class CQRFaction {
 		}
 		return false;
 	}
-	//TODO: Special case for player faction!!
+	//DONE: Special case for player faction!!
+	public boolean isAlly(Entity ent) {
+		if(ent instanceof EntityPlayer) {
+			//Special case for player
+			return FactionRegistry.instance().getReputationOf(ent.getPersistentID(), this) == EReputationStateRough.ALLY;
+		}
+		return isAlly(FactionRegistry.instance().getFactionOf(ent));
+	}
+	public boolean isAlly(AbstractEntityCQR ent) {
+		return isAlly(ent.getFaction());
+	}
 	public boolean isAlly(CQRFaction faction) {
 		if(faction == this || (faction != null && faction.getName().equalsIgnoreCase("ALL_ALLY"))) {
 			return true;
@@ -108,30 +137,6 @@ public class CQRFaction {
 		return false;
 	}
 
-	public boolean isAlly(AbstractEntityCQR ent) {
-		return isAlly(ent.getFaction());
-	}
-	public boolean isEnemy(AbstractEntityCQR ent) {
-		if(ent.getEntityWorld().getDifficulty().equals(EnumDifficulty.PEACEFUL)) {
-			return false;
-		}
-		return isEnemy(ent.getFaction());
-	}
-	public boolean isAlly(Entity ent) {
-		if(ent instanceof EntityPlayer) {
-			return FactionRegistry.instance().getReputationOf(ent.getPersistentID(), this) == EReputationStateRough.ALLY;
-		}
-		return isAlly(FactionRegistry.instance().getFactionOf(ent));
-	}
-	public boolean isEnemy(Entity ent) {
-		if(ent.getEntityWorld().getDifficulty().equals(EnumDifficulty.PEACEFUL)) {
-			return false;
-		}
-		if(ent instanceof EntityPlayer) {
-			return FactionRegistry.instance().getReputationOf(ent.getPersistentID(), this) == EReputationStateRough.ENEMY;
-		}
-		return isEnemy(FactionRegistry.instance().getFactionOf(ent));
-	}
 
 	public void decrementReputation(EntityPlayer player, int score) {
 		if(repuMayChange) {
@@ -151,7 +156,43 @@ public class CQRFaction {
 	
 	public void saveToFile(File folder) {
 		if(savedGlobally) {
-			//TODO: SAVE DATA
+			//DONE: SAVE DATA
+			Thread t = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					File file = FileIOUtil.getOrCreateFile(folder.getAbsolutePath(), getName() + ".nbt");
+					NBTTagCompound root = FileIOUtil.getRootNBTTagOfFile(file);
+					if(root != null) {
+						root.setString("type", "faction");
+						root.setString("name", name);
+						root.setBoolean("staticreputation", isRepuStatic());
+						root.setString("defaultrelation", getDefaultReputation().toString());
+						root.setInteger("repuchangekillally", getRepuAllyKill());
+						root.setInteger("repuchangekillmember", getRepuMemberKill());
+						root.setInteger("repuchangekillenemy", getRepuEnemyKill());
+						NBTTagCompound relationInfo = new NBTTagCompound();
+						if(relationInfo.hasKey("allies")) {
+							relationInfo.removeTag("allies");
+						}
+						NBTTagList allyTag = FileIOUtil.getOrCreateTagList(relationInfo, "allies", Constants.NBT.TAG_STRING);
+						for(CQRFaction af : allies) {
+							allyTag.appendTag(new NBTTagString(af.getName()));
+						}
+						if(relationInfo.hasKey("enemies")) {
+							relationInfo.removeTag("enemies");
+						}
+						NBTTagList enemyTag = FileIOUtil.getOrCreateTagList(relationInfo, "enemies", Constants.NBT.TAG_STRING);
+						for(CQRFaction ef : enemies) {
+							enemyTag.appendTag(new NBTTagString(ef.getName()));
+						}
+						relationInfo.setTag("allies", allyTag);
+						relationInfo.setTag("enemies", enemyTag);
+						root.setTag("relations", relationInfo);
+						FileIOUtil.saveNBTCompoundToFile(root, file);
+					}
+				}
+			});
+			t.start();
 		}
 	}
 
