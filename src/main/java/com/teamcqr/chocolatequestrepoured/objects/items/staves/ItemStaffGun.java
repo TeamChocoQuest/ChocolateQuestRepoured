3dbb29024a923d610b47c7d1ad3201c1536086c4
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-29 12:53:55 +0100

    readded functionality

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/staves/ItemStaffGun.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/staves/ItemStaffGun.java
@@ -13,6 +13,7 @@ import com.teamcqr.chocolatequestrepoured.util.IRangedWeapon;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -32,18 +33,6 @@ public class ItemStaffGun extends Item implements IRangedWeapon{
 		setMaxStackSize(1);
 	}
 
-	/*
-	@Override
-	public int getMaxItemUseDuration(ItemStack stack) {
-		return 72000;
-	}
-
-	@Override
-	public EnumAction getItemUseAction(ItemStack stack) {
-		return EnumAction.BOW;
-	}
-	*/
-
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		ItemStack stack = playerIn.getHeldItem(handIn);
@@ -51,29 +40,18 @@ public class ItemStaffGun extends Item implements IRangedWeapon{
 		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
 	}
 
-	public void shootStaff(World worldIn, EntityPlayer playerIn, ItemStack stack, EnumHand handIn) {
-		
-	}
+	public void shootStaff(World worldIn, EntityPlayer player, ItemStack stack, EnumHand handIn) {
+		worldIn.playSound(player.posX, player.posY, player.posZ, ModSounds.GUN_SHOOT, SoundCategory.MASTER,
+				4.0F, (1.0F + (itemRand.nextFloat() - itemRand.nextFloat()) * 0.2F) * 0.7F, false);
 
-	/*
-	@Override
-	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entityLiving, int timeLeft) {
-		if (entityLiving instanceof EntityPlayer) {
-			EntityPlayer player = (EntityPlayer) entityLiving;
-
-			worldIn.playSound(player.posX, player.posY, player.posZ, SoundsHandler.GUN_SHOOT, SoundCategory.MASTER,
-					4.0F, (1.0F + (itemRand.nextFloat() - itemRand.nextFloat()) * 0.2F) * 0.7F, false);
-
-			if (!worldIn.isRemote) {
-				ProjectileBulletCannon ball = new ProjectileBulletCannon(worldIn, player);
-				ball.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, 3.5F, 0F);
-				worldIn.spawnEntity(ball);
-				stack.damageItem(1, player);
-				player.getCooldownTracker().setCooldown(stack.getItem(), 20);
-			}
+		if (!worldIn.isRemote) {
+			ProjectileCannonBall ball = new ProjectileCannonBall(worldIn, player);
+			ball.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, 3.5F, 0F);
+			worldIn.spawnEntity(ball);
+			stack.damageItem(1, player);
+			player.getCooldownTracker().setCooldown(stack.getItem(), 20);
 		}
 	}
-	*/
 
 	@Override
 	@SideOnly(Side.CLIENT)
@@ -86,8 +64,7 @@ public class ItemStaffGun extends Item implements IRangedWeapon{
 	}
 
 	@Override
-	public void shoot(World world, Entity shooter, double x, double y, double z) {
-		// TODO Auto-generated method stub
+	public void shoot(World worldIn, EntityLivingBase shooter, Entity target, EnumHand handIn) {
 		
 	}
 
