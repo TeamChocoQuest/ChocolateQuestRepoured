bcd51c356e1abf4dded403dd953c85899994068a
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-29 12:56:03 +0100

    made staffs (hopefully) usable by mobs

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/staves/ItemStaffVampiric.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/staves/ItemStaffVampiric.java
@@ -12,6 +12,7 @@ import com.teamcqr.chocolatequestrepoured.util.IRangedWeapon;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
@@ -20,6 +21,7 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -65,9 +67,20 @@ public class ItemStaffVampiric extends Item implements IRangedWeapon{
 	}
 
 	@Override
-	public void shoot(World world, Entity shooter, double x, double y, double z) {
-		// TODO Auto-generated method stub
-		
+	public void shoot(World worldIn, EntityLivingBase shooter, Entity target, EnumHand handIn) {
+		worldIn.playSound(shooter.posX, shooter.posY, shooter.posZ, SoundEvents.ENTITY_ENDERPEARL_THROW,
+				SoundCategory.MASTER, 4.0F, (1.0F + (itemRand.nextFloat() - itemRand.nextFloat()) * 0.2F) * 0.7F,
+				false);
+		shooter.swingArm(handIn);
+
+		if (!worldIn.isRemote) {
+			ProjectileVampiricSpell spell = new ProjectileVampiricSpell(worldIn, shooter);
+			Vec3d v = target.getPositionVector().subtract(shooter.getPositionVector());
+			v = v.normalize();
+			v = v.scale(2D);
+			spell.setVelocity(v.x, v.y, v.z);
+			worldIn.spawnEntity(spell);
+		}
 	}
 
 }
