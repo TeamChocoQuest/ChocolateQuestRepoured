e7b5b8850fa882155280c3e8292398148c37282b
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2019-12-29 01:45:26 +0100

    fixed bug that reputation doesnt get saved

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/handlers/EventsHandler.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/handlers/EventsHandler.java
@@ -39,8 +39,6 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 @EventBusSubscriber
 public class EventsHandler {
@@ -207,7 +205,7 @@ public class EventsHandler {
 		}
 	}
 	
-	@SideOnly(Side.SERVER)
+	
 	@SubscribeEvent
 	public static void onPlayerLogin(PlayerLoggedInEvent event) {
 		if(event.isCanceled()) {
@@ -216,7 +214,7 @@ public class EventsHandler {
 		FactionRegistry.instance().handlePlayerLogin(event);
 	}
 	
-	@SideOnly(Side.SERVER)
+	//@SideOnly(Side.SERVER)
 	@SubscribeEvent
 	public static void onPlayerLogout(PlayerLoggedOutEvent event) {
 		if(event.isCanceled()) {
