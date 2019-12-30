4467e8898ad2d95e99074f880700fb2b8868ff77
KalgogSmash <trevor.seyf@gmail.com>
2019-12-29 17:15:23 -0800

    Removing unused imports

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/castleparts/rooms/decoration/RoomDecorTableSmall.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/castleparts/rooms/decoration/RoomDecorTableSmall.java
@@ -1,16 +1,6 @@
 package com.teamcqr.chocolatequestrepoured.structuregen.generators.castleparts.rooms.decoration;
 
 import com.teamcqr.chocolatequestrepoured.init.ModBlocks;
-import com.teamcqr.chocolatequestrepoured.objects.blocks.BlockTable;
-import com.teamcqr.chocolatequestrepoured.structuregen.dungeons.CastleDungeon;
-import com.teamcqr.chocolatequestrepoured.structuregen.generators.castleparts.rooms.CastleRoom;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-import java.util.HashSet;
 
 public class RoomDecorTableSmall extends RoomDecorBlocks
 {
