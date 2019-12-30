da2c5256d912333964521fc35235f264c801eceb
KalgogSmash <trevor.seyf@gmail.com>
2019-12-29 17:15:09 -0800

    Changing small table to use CQR block table instead

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/castleparts/rooms/decoration/RoomDecorTableSmall.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/castleparts/rooms/decoration/RoomDecorTableSmall.java
@@ -1,7 +1,16 @@
 package com.teamcqr.chocolatequestrepoured.structuregen.generators.castleparts.rooms.decoration;
 
+import com.teamcqr.chocolatequestrepoured.init.ModBlocks;
+import com.teamcqr.chocolatequestrepoured.objects.blocks.BlockTable;
+import com.teamcqr.chocolatequestrepoured.structuregen.dungeons.CastleDungeon;
+import com.teamcqr.chocolatequestrepoured.structuregen.generators.castleparts.rooms.CastleRoom;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.HashSet;
 
 public class RoomDecorTableSmall extends RoomDecorBlocks
 {
@@ -13,9 +22,7 @@ public class RoomDecorTableSmall extends RoomDecorBlocks
     @Override
     protected void makeSchematic()
     {
-        schematic.add(new DecoBlockOffset(0, 0, 0, Blocks.OAK_FENCE));
-
-        schematic.add(new DecoBlockOffset(0, 1, 0, Blocks.WOODEN_PRESSURE_PLATE));
+        schematic.add(new DecoBlockOffset(0, 0, 0, ModBlocks.TABLE_OAK));
     }
 
 }
