9c53ab88afe232d1796c14c8fee5dd4d1c0b895e
KalgogSmash <trevor.seyf@gmail.com>
2019-12-31 20:33:36 -0800

    Base classes for hookshot and projectile

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/ItemHookshotBase.java
@@ -0,0 +1,92 @@
+package com.teamcqr.chocolatequestrepoured.objects.items;
+
+import com.teamcqr.chocolatequestrepoured.init.ModSounds;
+import com.teamcqr.chocolatequestrepoured.objects.entity.projectiles.ProjectileHookShotHook;
+import com.teamcqr.chocolatequestrepoured.objects.entity.projectiles.ProjectileBullet;
+import com.teamcqr.chocolatequestrepoured.objects.items.guns.ItemBullet;
+import com.teamcqr.chocolatequestrepoured.util.IRangedWeapon;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.*;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.input.Keyboard;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class ItemHookshotBase extends Item implements IRangedWeapon{
+
+	public ItemHookshotBase() {
+		setMaxDamage(300);
+		setMaxStackSize(1);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add(TextFormatting.BLUE + "5.0 " + I18n.format("description.bullet_damage.name"));
+		tooltip.add(TextFormatting.RED + "-30 " + I18n.format("description.fire_rate.name"));
+		tooltip.add(TextFormatting.RED + "-50" + "% " + I18n.format("description.accuracy.name"));
+		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+			tooltip.add(TextFormatting.BLUE + I18n.format("description.gun.name"));
+		} else {
+			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
+		}
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		System.out.println("Firing hookshot");
+
+		ItemStack stack = playerIn.getHeldItem(handIn);
+
+		shoot(stack, worldIn, playerIn);
+
+		return new ActionResult(EnumActionResult.SUCCESS, stack);
+	}
+
+	public void shoot(ItemStack stack, World worldIn, EntityPlayer player) {
+
+		if (!worldIn.isRemote) {
+			ProjectileHookShotHook hookEntity = new ProjectileHookShotHook(worldIn, player);
+			hookEntity.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, 2.0F, 0F);
+			player.getCooldownTracker().setCooldown(stack.getItem(), 10);
+			worldIn.spawnEntity(hookEntity);
+			stack.damageItem(1, player);
+		}
+		worldIn.playSound(player.posX, player.posY, player.posZ, ModSounds.GUN_SHOOT, SoundCategory.MASTER,
+				1.0F, 1.0F, false);
+	}
+
+	protected boolean isBullet(ItemStack stack) {
+		return stack.getItem() instanceof ItemBullet;
+	}
+
+	@Override
+	public void shoot(World worldIn, EntityLivingBase shooter, Entity target, EnumHand handIn) {
+		if (!worldIn.isRemote) {
+			ProjectileBullet bulletE = new ProjectileBullet(worldIn, shooter, 1/* 1 is Iron bullet */);
+			Vec3d v = target.getPositionVector().subtract(shooter.getPositionVector());
+			v = v.normalize();
+			v = v.scale(3.5D);
+			bulletE.setVelocity(v.x, v.y, v.z);
+			worldIn.spawnEntity(bulletE);
+		}
+	}
+
+	@Override
+	public SoundEvent getShootSound() {
+		return ModSounds.GUN_SHOOT;
+	}
+
+}
