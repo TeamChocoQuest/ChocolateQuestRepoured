0f34efdae9f7eaecc83d8a29ec98bd15bb167aaf
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-01-04 20:53:04 +0100

    Fixed bullet bug (#114)

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/projectiles/ProjectileCannonBall.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/projectiles/ProjectileCannonBall.java
@@ -7,7 +7,6 @@ import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 public class ProjectileCannonBall extends ProjectileBase {
-	private EntityLivingBase shooter;
 
 	public ProjectileCannonBall(World worldIn) {
 		super(worldIn);
@@ -19,25 +18,20 @@ public class ProjectileCannonBall extends ProjectileBase {
 
 	public ProjectileCannonBall(World worldIn, EntityLivingBase shooter) {
 		super(worldIn, shooter);
-		this.shooter = shooter;
-		this.isImmuneToFire = true;
 	}
 
 	@Override
 	protected void onImpact(RayTraceResult result) {
 		if (!this.world.isRemote) {
 			if (result.typeOfHit == RayTraceResult.Type.ENTITY) {
+				if (result.entityHit == this.thrower) {
+					return;
+				}
+
 				if (result.entityHit instanceof EntityLivingBase) {
 					EntityLivingBase entity = (EntityLivingBase) result.entityHit;
-					float damage = 5F;
 
-					if (result.entityHit == this.shooter) {
-						return;
-					}
-
-					damage += damage;
-
-					entity.attackEntityFrom(DamageSource.causeIndirectDamage(this, entity), damage);
+					entity.attackEntityFrom(DamageSource.causeIndirectDamage(this, this.thrower), 10.0F);
 					this.setDead();
 				}
 			}
@@ -50,8 +44,9 @@ public class ProjectileCannonBall extends ProjectileBase {
 	protected void onUpdateInAir() {
 		if (this.world.isRemote) {
 			if (this.ticksExisted < 10) {
-				this.world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, this.posX, this.posY + 0.1D, this.posZ, 0.0D, 0.0D, 0.0D);
+				this.world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
 			}
 		}
 	}
+
 }
