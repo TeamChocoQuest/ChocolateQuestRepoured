ccd43a429000bf962ba0c3aef223dc770c359a31
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-01-11 15:52:57 +0100

    helper class for volcano stronghold

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/volcano/brickfortress/ESpiralStrongholdRoomType.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/volcano/brickfortress/ESpiralStrongholdRoomType.java
@@ -2,25 +2,36 @@ package com.teamcqr.chocolatequestrepoured.structuregen.generators.volcano.brick
 
 public enum ESpiralStrongholdRoomType {
 	
-	NONE,
+	NONE("NONE"),
+	BOSS("BOSS"),
 	
-	CURVE_NE,
-	CURVE_EN,
-	CURVE_ES,
-	CURVE_SE,
-	CURVE_SW,
-	CURVE_WS,
-	CURVE_WN,
-	CURVE_NW,
+	CURVE_NE("CURVE_EN"),
+	CURVE_EN("CURVE_NE"),
+	CURVE_ES("CURVE_SE"),
+	CURVE_SE("CURVE_ES"),
+	CURVE_SW("CURVE_WS"),
+	CURVE_WS("CURVE_SW"),
+	CURVE_WN("CURVE_NW"),
+	CURVE_NW("CURVE_WN"),
 	
-	HALLWAY_NS,
-	HALLWAY_SN,
-	HALLWAY_EW,
-	HALLWAY_WE,
+	HALLWAY_NS("HALLWAY_SN"),
+	HALLWAY_SN("HALLWAY_NS"),
+	HALLWAY_EW("HALLWAY_WE"),
+	HALLWAY_WE("HALLWAY_EW"),
 	
-	STAIR_NN,
-	STAIR_SS,
-	STAIR_EE,
-	STAIR_WW;
+	STAIR_NN("STAIR_NN"),
+	STAIR_SS("STAIR_SS"),
+	STAIR_EE("STAIR_EE"),
+	STAIR_WW("STAIR_WW");
+	
+	private ESpiralStrongholdRoomType(String reversed) {
+		this.reversed = reversed;
+	}
+	
+	private String reversed;
+	
+	public ESpiralStrongholdRoomType getReversed() {
+		return valueOf(reversed);
+	}
 
 }
