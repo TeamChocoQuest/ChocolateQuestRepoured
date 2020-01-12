08fdd3c73d5cd51fb06b8cd86c13ed7903963087
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-01-12 16:51:08 +0100

    endermen behave more like endermen

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQREnderman.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/mobs/EntityCQREnderman.java
@@ -3,13 +3,16 @@ package com.teamcqr.chocolatequestrepoured.objects.entity.mobs;
 import com.teamcqr.chocolatequestrepoured.factions.EDefaultFaction;
 import com.teamcqr.chocolatequestrepoured.objects.entity.EBaseHealths;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ELootTablesNormal;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAITorchIgniter;
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR;
 
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EntityDamageSourceIndirect;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.DifficultyInstance;
@@ -29,6 +32,53 @@ public class EntityCQREnderman extends AbstractEntityCQR {
 	protected void setEquipmentBasedOnDifficulty(DifficultyInstance difficulty) {
 
 	}
+	
+	@Override
+	protected void initEntityAI() {
+		super.initEntityAI();
+		this.tasks.addTask(22, new EntityAITorchIgniter(this));
+	}
+	
+	@Override
+	public boolean attackEntityFrom(DamageSource source, float amount) {
+		if(super.attackEntityFrom(source, amount)) {
+			if(source instanceof EntityDamageSourceIndirect) {
+				 for (int i = 0; i < 64; ++i)
+		            {
+		                if (this.teleportRandomly())
+		                {
+		                    return true;
+		                }
+		            }
+
+		            return false;
+			}
+		}
+		return super.attackEntityFrom(source, amount);
+	}
+	
+	protected boolean teleportRandomly()
+    {
+        double d0 = this.posX + (this.rand.nextDouble() - 0.5D) * 64.0D;
+        double d1 = this.posY + (double)(this.rand.nextInt(64) - 32);
+        double d2 = this.posZ + (this.rand.nextDouble() - 0.5D) * 64.0D;
+        return this.teleportTo(d0, d1, d2);
+    }
+	
+	private boolean teleportTo(double x, double y, double z)
+    {
+        net.minecraftforge.event.entity.living.EnderTeleportEvent event = new net.minecraftforge.event.entity.living.EnderTeleportEvent(this, x, y, z, 0);
+        if (net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(event)) return false;
+        boolean flag = this.attemptTeleport(event.getTargetX(), event.getTargetY(), event.getTargetZ());
+
+        if (flag)
+        {
+            this.world.playSound((EntityPlayer)null, this.prevPosX, this.prevPosY, this.prevPosZ, SoundEvents.ENTITY_ENDERMEN_TELEPORT, this.getSoundCategory(), 1.0F, 1.0F);
+            this.playSound(SoundEvents.ENTITY_ENDERMEN_TELEPORT, 1.0F, 1.0F);
+        }
+
+        return flag;
+    }
 
 	@Override
 	public float getBaseHealth() {
