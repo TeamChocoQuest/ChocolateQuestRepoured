de5072eaf8119951ffb427a629d137127a896af9
KalgogSmash <trevor.seyf@gmail.com>
2020-01-23 05:58:10 -0800

    Greatly slow the player while the hookshot is firing

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/network/packets/handlers/HookShotPlayerStopPacketHandler.java
@@ -0,0 +1,36 @@
+package com.teamcqr.chocolatequestrepoured.network.packets.handlers;
+
+import com.teamcqr.chocolatequestrepoured.CQRMain;
+import com.teamcqr.chocolatequestrepoured.network.packets.toClient.HookShitPlayerStopPacket;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+
+public class HookShotPlayerStopPacketHandler implements IMessageHandler<HookShitPlayerStopPacket, IMessage> {
+    @Override
+    public IMessage onMessage(final HookShitPlayerStopPacket message, MessageContext ctx) {
+        if (ctx.side != Side.CLIENT) {
+            return null;
+        }
+
+        Minecraft minecraft = Minecraft.getMinecraft();
+
+        minecraft.addScheduledTask(new Runnable() {
+            @Override
+            public void run() {
+                HookShotPlayerStopPacketHandler.this.processMessage(message, ctx);
+            }
+        });
+
+        return null;
+    }
+
+    private void processMessage(final HookShitPlayerStopPacket message, MessageContext ctx) {
+        EntityPlayer player = CQRMain.proxy.getPlayer(ctx);
+        player.setVelocity(0, 0, 0);
+        player.velocityChanged = true;
+    }
+}
