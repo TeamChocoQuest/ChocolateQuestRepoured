51286202a8b0b75662c700ead96be437e2a1acab
KalgogSmash <trevor.seyf@gmail.com>
2020-02-11 06:28:29 -0800

    Fixing chain render but I am not 100% sure why this works

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/projectile/RenderProjectileHookShotHook.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/projectile/RenderProjectileHookShotHook.java
@@ -57,15 +57,24 @@ public class RenderProjectileHookShotHook extends Render<ProjectileHookShotHook>
 
 		GlStateManager.disableRescaleNormal();
 		GlStateManager.popMatrix();
-		
+
 		renderChain(entity);
 		
 		super.doRender(entity, x, y, z, entityYaw, partialTicks);
 	}
 
 	private void renderChain(ProjectileHookShotHook entity) {
+        GlStateManager.pushMatrix();
+        GlStateManager.enableBlend();
+        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA,
+				GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.color(0.0F, 1.0F, 0.0F, 0.75F);
+        GlStateManager.disableTexture2D();
+        GlStateManager.glLineWidth(6.0F);
+
 		//DONE: render chain elements
-		Vec3d v = entity.getPositionVector().subtract(entity.getShooterPosition());//.add(new Vec3d(0,1.7,0));
+		Vec3d entityEyePos = entity.getPositionVector().add(new Vec3d(0, entity.getEyeHeight(), 0));
+		Vec3d v = entity.getPositionVector().subtract(entityEyePos);//.add(new Vec3d(0,1.7,0));
 		int iterations = (int) Math.ceil(v.lengthVector());
 		v = v.normalize();
 		Vec3d loc = entity.getShooterPosition().add(v);
@@ -82,6 +91,10 @@ public class RenderProjectileHookShotHook extends Render<ProjectileHookShotHook>
 
 			loc = loc.add(v);
 		}
+
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+        GlStateManager.popMatrix();
 	}
 
 	@Override
