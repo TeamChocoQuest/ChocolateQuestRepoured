29cc421547a7894b2f348674b142af42cc951a52
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-02-24 19:36:01 +0100

    preparations for pathing AI
    TODO:
      - Recalculate the path points when the 	entity spawns from a spawner
    	rotating
      - Actually applying those points
      - Setting home position if it has none 	when you apply the first
    	position

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/EntityAIFollowPath.java
@@ -0,0 +1,58 @@
+package com.teamcqr.chocolatequestrepoured.objects.entity.ai;
+
+import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR;
+
+import net.minecraft.entity.MoverType;
+import net.minecraft.util.math.BlockPos;
+
+public class EntityAIFollowPath extends AbstractCQREntityAI {
+
+	private boolean isReversingPath = false;
+	
+	public EntityAIFollowPath(AbstractEntityCQR entity) {
+		super(entity);
+		setMutexBits(8);
+	}
+
+	@Override
+	public boolean shouldExecute() {
+		if(this.entity.getGuardPathPoints().length > 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public void updateTask() {
+		BlockPos pos = entity.getGuardPathPoints()[entity.getCurrentGuardPathTargetPoint()].add(entity.getHomePositionCQR());
+		float width = entity.width;
+		if(entity.getRidingEntity() != null) {
+			width = entity.getRidingEntity().width;
+		}
+		if(width <= 0) {
+			width = 1;
+		}
+		width *= width;
+		if(entity.getDistanceSq(pos) <= width +1) {
+			//Cycle to next position
+			int newIndex = entity.getCurrentGuardPathTargetPoint() +1;
+			if(newIndex == entity.getGuardPathPoints().length) {
+				if(entity.isGuardPathLoop()) {
+					newIndex = -1;
+				} else {
+					isReversingPath = !isReversingPath;
+				}
+			}
+			if(isReversingPath) {
+				newIndex--;
+			} else {
+				newIndex++;
+			}
+			entity.setCurrentGuardPathTargetPoint(newIndex);
+			pos = entity.getGuardPathPoints()[entity.getCurrentGuardPathTargetPoint()].add(entity.getHomePositionCQR());
+		}
+		entity.move(MoverType.SELF, pos.getX(), pos.getY(), pos.getZ());
+		super.updateTask();
+	}
+
+}
