ef13aa6bca83ac2f61dce63611610eab9c590a10
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-02-25 16:14:45 +0100

    ...

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/ItemPathTool.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/ItemPathTool.java
@@ -6,18 +6,15 @@ import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
 public class ItemPathTool extends Item {
@@ -37,21 +34,23 @@ public class ItemPathTool extends Item {
 		 * Left click -> apply path points and overwrite the existing ones
 		 * Shift left click -> get existing path points
 		 */
-		CapabilityPathTool capa = stack.getCapability(CapabilityPathToolProvider.PATH_TOOL, null);
-		if(entity instanceof AbstractEntityCQR && capa.getPathPoints().length > 0) {
-			if(player.isSneaking()) {
-				((AbstractEntityCQR)entity).setPath(capa.getPathPoints());
-				((WorldServer) player.world).spawnParticle((EntityPlayerMP) player, EnumParticleTypes.VILLAGER_HAPPY, false, entity.posX, entity.posY + 0.5D, entity.posZ, 8, 0.5D, 0.5D, 0.5D, 0.1D);
-			} else {
-				BlockPos[] oldPath = ((AbstractEntityCQR)entity).getGuardPathPoints();
-				final BlockPos home = ((AbstractEntityCQR)entity).getHomePositionCQR();
-				if(oldPath.length > 0) {
-					final BlockPos[] path = new BlockPos[oldPath.length];
-					for(int i = 0; i < path.length; i++) {
-						path[i] = oldPath[i];
-						path[i] = path[i].add(home);
+		if(!player.world.isRemote) {
+			CapabilityPathTool capa = stack.getCapability(CapabilityPathToolProvider.PATH_TOOL, null);
+			if(entity instanceof AbstractEntityCQR && capa.getPathPoints().length > 0) {
+				if(player.isSneaking()) {
+					((AbstractEntityCQR)entity).setPath(capa.getPathPoints());
+					//((WorldServer) player.world).spawnParticle((EntityPlayerMP) player, EnumParticleTypes.VILLAGER_HAPPY, false, entity.posX, entity.posY + 0.5D, entity.posZ, 8, 0.5D, 0.5D, 0.5D, 0.1D);
+				} else {
+					BlockPos[] oldPath = ((AbstractEntityCQR)entity).getGuardPathPoints();
+					final BlockPos home = ((AbstractEntityCQR)entity).getHomePositionCQR();
+					if(oldPath.length > 0) {
+						final BlockPos[] path = new BlockPos[oldPath.length];
+						for(int i = 0; i < path.length; i++) {
+							path[i] = oldPath[i];
+							path[i] = path[i].add(home);
+						}
+						capa.setPathPoints(path);
 					}
-					capa.setPathPoints(path);
 				}
 			}
 		}
