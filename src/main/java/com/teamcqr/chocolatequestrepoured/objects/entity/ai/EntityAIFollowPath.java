efb1b0b8fb78d1b3e6c22d26d2b32a40e1bb4369
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-02-25 21:42:57 +0100

    fixed AI, works now (if moveToHome and search mount is disabled .__.)

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/EntityAIFollowPath.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/EntityAIFollowPath.java
@@ -2,8 +2,7 @@ package com.teamcqr.chocolatequestrepoured.objects.entity.ai;
 
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR;
 
-import net.minecraft.entity.MoverType;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 
 public class EntityAIFollowPath extends AbstractCQREntityAI {
 
@@ -16,7 +15,7 @@ public class EntityAIFollowPath extends AbstractCQREntityAI {
 
 	@Override
 	public boolean shouldExecute() {
-		if(this.entity.getGuardPathPoints().length > 0) {
+		if(this.entity.getGuardPathPoints().length > 0 && this.entity.getHomePositionCQR() != null) {
 			return true;
 		}
 		return false;
@@ -24,7 +23,7 @@ public class EntityAIFollowPath extends AbstractCQREntityAI {
 	
 	@Override
 	public void updateTask() {
-		BlockPos pos = entity.getGuardPathPoints()[entity.getCurrentGuardPathTargetPoint()].add(entity.getHomePositionCQR());
+		Vec3d pos = new Vec3d(entity.getGuardPathPoints()[entity.getCurrentGuardPathTargetPoint()]).add(new Vec3d(entity.getHomePositionCQR())).addVector(0.5,0,0.5);
 		float width = entity.width;
 		if(entity.getRidingEntity() != null) {
 			width = entity.getRidingEntity().width;
@@ -33,29 +32,24 @@ public class EntityAIFollowPath extends AbstractCQREntityAI {
 			width = 1;
 		}
 		width *= width;
-		if(entity.getDistanceSq(pos) <= width +1) {
+		if(entity.getDistance(pos.x, pos.y, pos.z) <= width +1) {
 			//Cycle to next position
 			int newIndex = isReversingPath ? entity.getCurrentGuardPathTargetPoint() -1 : entity.getCurrentGuardPathTargetPoint() +1;
 			if(newIndex == entity.getGuardPathPoints().length) {
 				if(entity.isGuardPathLoop()) {
-					newIndex = -1;
+					newIndex = 0;
 				} else {
-					isReversingPath = !isReversingPath;
+					isReversingPath = true;
+					newIndex--;
 				}
 			}
-			if(newIndex < 0 && isReversingPath) {
-				newIndex = 0;
-				isReversingPath = !isReversingPath;
-			}
-			if(isReversingPath) {
-				newIndex--;
-			} else {
-				newIndex++;
+			if(newIndex == 0 && !entity.isGuardPathLoop()) {
+				isReversingPath = false;
 			}
 			entity.setCurrentGuardPathTargetPoint(newIndex);
-			pos = entity.getGuardPathPoints()[entity.getCurrentGuardPathTargetPoint()].add(entity.getHomePositionCQR());
+			pos = new Vec3d(entity.getGuardPathPoints()[entity.getCurrentGuardPathTargetPoint()]).add(new Vec3d(entity.getHomePositionCQR())).addVector(0.5,0,0.5);
 		}
-		entity.move(MoverType.SELF, pos.getX(), pos.getY(), pos.getZ());
+		entity.getMoveHelper().setMoveTo(pos.x, pos.y, pos.z, 1.0);
 		super.updateTask();
 	}
 
