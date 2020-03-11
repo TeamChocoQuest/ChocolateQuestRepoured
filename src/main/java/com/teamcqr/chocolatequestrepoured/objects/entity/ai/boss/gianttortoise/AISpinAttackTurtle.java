db34532611716aaae6858ee55073b62959d25443
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-03-11 16:57:18 +0100

    lowered bubble shooting duration (should be 20 ticks now)

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/boss/gianttortoise/AISpinAttackTurtle.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/boss/gianttortoise/AISpinAttackTurtle.java
@@ -19,7 +19,7 @@ public class AISpinAttackTurtle extends AnimationAI<EntityCQRGiantTortoise> {
 	private static final int MAX_BLOCKED_SPINS = 1;
 	
 	private final int AFTER_IDLE_TIME = 5;
-	private final int BUBBLE_SHOOT_DURATION = 40;
+	private final int BUBBLE_SHOOT_DURATION = 20;
 	
 	static final int MIN_DISTANCE_TO_BEGIN_SPIN = 16; 
 
