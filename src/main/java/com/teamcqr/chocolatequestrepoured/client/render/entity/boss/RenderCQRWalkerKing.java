7278fadb500030042fe404cdb66b7f4007231256
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-03-13 10:42:04 +0100

    began walker king

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/boss/RenderCQRWalkerKing.java
@@ -0,0 +1,15 @@
+package com.teamcqr.chocolatequestrepoured.client.render.entity.boss;
+
+import com.teamcqr.chocolatequestrepoured.client.render.entity.RenderCQREntity;
+import com.teamcqr.chocolatequestrepoured.objects.entity.boss.EntityCQRWalkerBoss;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.RenderManager;
+
+public class RenderCQRWalkerKing extends RenderCQREntity<EntityCQRWalkerBoss> {
+
+	public RenderCQRWalkerKing(RenderManager rendermanagerIn, ModelBase model, String entityName) {
+		super(rendermanagerIn, model, 0.5F, entityName, 1D, 1D);
+	}
+
+}
