106dd38518f1966449d0376c7f6b4b17a25124ce
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-03-13 10:56:56 +0100

    death

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/boss/RenderCQRWalkerKing.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/boss/RenderCQRWalkerKing.java
@@ -1,6 +1,7 @@
 package com.teamcqr.chocolatequestrepoured.client.render.entity.boss;
 
 import com.teamcqr.chocolatequestrepoured.client.render.entity.RenderCQREntity;
+import com.teamcqr.chocolatequestrepoured.client.render.entity.layers.LayerBossDeath;
 import com.teamcqr.chocolatequestrepoured.objects.entity.boss.EntityCQRWalkerBoss;
 
 import net.minecraft.client.model.ModelBase;
@@ -10,6 +11,8 @@ public class RenderCQRWalkerKing extends RenderCQREntity<EntityCQRWalkerBoss> {
 
 	public RenderCQRWalkerKing(RenderManager rendermanagerIn, ModelBase model, String entityName) {
 		super(rendermanagerIn, model, 0.5F, entityName, 1D, 1D);
+		
+		this.addLayer(new LayerBossDeath(191,0,255,0));
 	}
 
 }
