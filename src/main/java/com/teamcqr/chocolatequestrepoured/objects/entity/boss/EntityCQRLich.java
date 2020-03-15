547d400c1f07a9e84e58d2b996536ce75cf85ebe
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-03-15 11:32:31 +0100

    AI update

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRLich.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRLich.java
@@ -20,6 +20,8 @@ import com.teamcqr.chocolatequestrepoured.objects.entity.ai.spells.EntityAISummo
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.target.EntityAICQRNearestAttackTarget;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.target.EntityAIHurtByTarget;
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.ISummoner;
+import com.teamcqr.chocolatequestrepoured.objects.entity.misc.EntitySummoningCircle.ECircleTexture;
+import com.teamcqr.chocolatequestrepoured.util.Reference;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -30,6 +32,7 @@ import net.minecraft.nbt.NBTUtil;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.BossInfo.Color;
 import net.minecraft.world.BossInfo.Overlay;
 import net.minecraft.world.World;
@@ -88,7 +91,7 @@ public class EntityCQRLich extends AbstractEntityCQRMageBase implements ISummone
 		this.tasks.addTask(6, new EntityAIArmorSpell(this));
 		this.tasks.addTask(8, new EntityAIFangAttack(this));
 		this.tasks.addTask(9, new EntityAIShootPoisonProjectiles(this));
-		this.tasks.addTask(7, new EntityAISummonMinionSpell(this));
+		this.tasks.addTask(7, new EntityAISummonMinionSpell(this, new ResourceLocation(Reference.MODID, "zombie"), ECircleTexture.ZOMBIE, true, 12, 4, new Vec3d(0,0,0)));
 		this.tasks.addTask(10, new EntityAIAttackRanged(this));
 		this.tasks.addTask(11, new EntityAIAttack(this));
 		this.tasks.addTask(20, new EntityAIMoveToHome(this));
