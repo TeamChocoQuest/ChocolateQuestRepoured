bdc37e4210c93d60a90b2b79038abd749840cdfc
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-03-19 12:24:55 +0100

    unused

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/ModelCQRWasp.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/models/entities/ModelCQRWasp.java
@@ -1,103 +1,64 @@
 package com.teamcqr.chocolatequestrepoured.client.models.entities;
 
-import java.util.HashMap;
-
-import mcalibrary.MCAVersionChecker;
-import mcalibrary.client.MCAModelRenderer;
-import mcalibrary.math.Matrix4f;
-import mcalibrary.math.Quaternion;
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
+/**
+ * CQRWasp - DerToaster
+ * Created using Tabula 7.0.1
+ */
 public class ModelCQRWasp extends ModelBase {
-	public final int MCA_MIN_REQUESTED_VERSION = 5;
-	public HashMap<String, MCAModelRenderer> parts = new HashMap<String, MCAModelRenderer>();
+    public ModelRenderer bodyMain;
+    public ModelRenderer head;
+    public ModelRenderer bodyBack;
+    public ModelRenderer wingL;
+    public ModelRenderer wingR;
+    public ModelRenderer sting;
 
-	MCAModelRenderer bodyMain;
-	MCAModelRenderer head;
-	MCAModelRenderer wingL;
-	MCAModelRenderer wingR;
-	MCAModelRenderer body2;
-	MCAModelRenderer sting1;
+    public ModelCQRWasp() {
+        this.textureWidth = 64;
+        this.textureHeight = 64;
+        this.head = new ModelRenderer(this, 0, 10);
+        this.head.setRotationPoint(0.0F, 0.0F, -2.9F);
+        this.head.addBox(-1.5F, -1.5F, -3.0F, 3, 3, 3, 0.0F);
+        this.wingR = new ModelRenderer(this, 0, 23);
+        this.wingR.setRotationPoint(-1.0F, -1.0F, 0.0F);
+        this.wingR.addBox(0.0F, 0.0F, -2.0F, 8, 0, 4, 0.0F);
+        this.setRotateAngle(wingR, 0.0F, -2.8797932657906435F, 0.3839724354387525F);
+        this.wingL = new ModelRenderer(this, 0, 19);
+        this.wingL.setRotationPoint(1.0F, -1.0F, 0.0F);
+        this.wingL.addBox(0.0F, 0.0F, -2.0F, 8, 0, 4, 0.0F);
+        this.setRotateAngle(wingL, 0.0F, -0.2617993877991494F, -0.3839724354387525F);
+        this.sting = new ModelRenderer(this, 15, 10);
+        this.sting.setRotationPoint(0.0F, 0.3F, 4.0F);
+        this.sting.addBox(-1.0F, -1.0F, -1.0F, 2, 2, 4, 0.0F);
+        this.setRotateAngle(sting, -0.7853981633974483F, 0.0F, 0.0F);
+        this.bodyMain = new ModelRenderer(this, 0, 0);
+        this.bodyMain.setRotationPoint(0.0F, 16.0F, 0.0F);
+        this.bodyMain.addBox(-2.0F, -2.0F, -3.0F, 4, 4, 6, 0.0F);
+        this.bodyBack = new ModelRenderer(this, 29, 9);
+        this.bodyBack.setRotationPoint(0.0F, 0.0F, 2.4F);
+        this.bodyBack.addBox(-1.5F, -1.5F, 0.0F, 3, 3, 4, 0.0F);
+        this.setRotateAngle(bodyBack, -0.40142572795869574F, 0.0F, 0.0F);
+        this.bodyMain.addChild(this.head);
+        this.bodyMain.addChild(this.wingR);
+        this.bodyMain.addChild(this.wingL);
+        this.bodyBack.addChild(this.sting);
+        this.bodyMain.addChild(this.bodyBack);
+    }
 
-	public ModelCQRWasp() {
-		MCAVersionChecker.checkForLibraryVersion(this.getClass(), this.MCA_MIN_REQUESTED_VERSION);
+    @Override
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
+        this.bodyMain.render(f5);
+    }
 
-		this.textureWidth = 64;
-		this.textureHeight = 64;
-
-		this.bodyMain = new MCAModelRenderer(this, "bodyMain", 0, 0);
-		this.bodyMain.mirror = false;
-		this.bodyMain.addBox(-2.0F, -2.0F, 0.0F, 4, 4, 6);
-		this.bodyMain.setInitialRotationPoint(0.0F, 0.0F, 0.0F);
-		this.bodyMain.setInitialRotationMatrix(new Matrix4f().set(new Quaternion(0.0F, 0.0F, 0.0F, 1.0F)).transpose());
-		this.bodyMain.setTextureSize(64, 64);
-		this.parts.put(this.bodyMain.boxName, this.bodyMain);
-
-		this.head = new MCAModelRenderer(this, "head", 0, 10);
-		this.head.mirror = false;
-		this.head.addBox(-1.5F, -1.5F, 6.0F, 3, 3, 3);
-		this.head.setInitialRotationPoint(0.0F, 0.0F, 0.0F);
-		this.head.setInitialRotationMatrix(new Matrix4f().set(new Quaternion(0.0F, 0.0F, 0.0F, 1.0F)).transpose());
-		this.head.setTextureSize(64, 64);
-		this.parts.put(this.head.boxName, this.head);
-		this.bodyMain.addChild(this.head);
-
-		this.wingL = new MCAModelRenderer(this, "wingL", 0, 19);
-		this.wingL.mirror = false;
-		this.wingL.addBox(0.5F, 1.0F, 1.5F, 8, 0, 4);
-		this.wingL.setInitialRotationPoint(0.0F, 0.0F, 0.0F);
-		this.wingL.setInitialRotationMatrix(new Matrix4f().set(new Quaternion(0.0F, 0.1305262F, 0.0F, 0.9914449F)).transpose());
-		this.wingL.setTextureSize(64, 64);
-		this.parts.put(this.wingL.boxName, this.wingL);
-		this.bodyMain.addChild(this.wingL);
-
-		this.wingR = new MCAModelRenderer(this, "wingR", 0, 23);
-		this.wingR.mirror = false;
-		this.wingR.addBox(0.5F, 1.0F, -5.5F, 8, 0, 4);
-		this.wingR.setInitialRotationPoint(0.0F, 0.0F, 0.0F);
-		this.wingR.setInitialRotationMatrix(new Matrix4f().set(new Quaternion(0.0F, 0.9914448F, 0.0F, 0.13052624F)).transpose());
-		this.wingR.setTextureSize(64, 64);
-		this.parts.put(this.wingR.boxName, this.wingR);
-		this.bodyMain.addChild(this.wingR);
-
-		this.body2 = new MCAModelRenderer(this, "body2", 29, 9);
-		this.body2.mirror = false;
-		this.body2.addBox(-1.5F, -1.5F, -3.25F, 3, 3, 4);
-		this.body2.setInitialRotationPoint(0.0F, 0.0F, 0.0F);
-		this.body2.setInitialRotationMatrix(new Matrix4f().set(new Quaternion(-0.19936793F, 0.0F, 0.0F, 0.9799247F)).transpose());
-		this.body2.setTextureSize(64, 64);
-		this.parts.put(this.body2.boxName, this.body2);
-		this.bodyMain.addChild(this.body2);
-
-		this.sting1 = new MCAModelRenderer(this, "sting1", 15, 10);
-		this.sting1.mirror = false;
-		this.sting1.addBox(-1.0F, 1.0F, -5.0F, 2, 2, 4);
-		this.sting1.setInitialRotationPoint(0.0F, 0.0F, 0.0F);
-		this.sting1.setInitialRotationMatrix(new Matrix4f().set(new Quaternion(-0.38268346F, 0.0F, 0.0F, 0.9238795F)).transpose());
-		this.sting1.setTextureSize(64, 64);
-		this.parts.put(this.sting1.boxName, this.sting1);
-		this.body2.addChild(this.sting1);
-
-	}
-
-	@Override
-	public void render(Entity par1Entity, float par2, float par3, float par4, float par5, float par6, float par7) {
-		/*
-		 * EntityCQRBee entity = (EntityCQRBee) par1Entity;
-		 * //TODO: Implement WASPS first
-		 * AnimationHandler.performAnimationInModel(parts, entity);
-		 */
-
-		// Render every non-child part
-		this.bodyMain.render(par7);
-	}
-
-	@Override
-	public void setRotationAngles(float par1, float par2, float par3, float par4, float par5, float par6, Entity par7Entity) {
-	}
-
-	public MCAModelRenderer getModelRendererFromName(String name) {
-		return this.parts.get(name);
-	}
+    /**
+     * This is a helper function from Tabula to set the rotation of model parts
+     */
+    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+        modelRenderer.rotateAngleX = x;
+        modelRenderer.rotateAngleY = y;
+        modelRenderer.rotateAngleZ = z;
+    }
 }
