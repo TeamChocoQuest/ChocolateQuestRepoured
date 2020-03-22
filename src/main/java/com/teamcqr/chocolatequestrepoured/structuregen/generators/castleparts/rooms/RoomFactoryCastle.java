d6ae3a90aea0d407675a03dc6783cf013612c3cc
KalgogSmash <trevor.seyf@gmail.com>
2020-03-22 15:58:44 -0700

    Adding library rooms and fixing unpathable room bug

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/castleparts/rooms/RoomFactoryCastle.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/castleparts/rooms/RoomFactoryCastle.java
@@ -3,22 +3,25 @@ package com.teamcqr.chocolatequestrepoured.structuregen.generators.castleparts.r
 import net.minecraft.util.math.BlockPos;
 
 public class RoomFactoryCastle {
-	public static CastleRoomGenericBase CreateGenericRoom(EnumRoomType type, BlockPos startPos, int sideLength, int height, int floor) {
+	public static CastleRoomDecoratedBase CreateGenericRoom(EnumRoomType type, BlockPos startPos, int sideLength, int height, int floor) {
 		switch (type) {
-		case KITCHEN:
-			return new CastleRoomKitchen(startPos, sideLength, height, floor);
+			case KITCHEN:
+				return new CastleRoomKitchen(startPos, sideLength, height, floor);
 
-		case BEDROOM:
-			return new CastleRoomBedroom(startPos, sideLength, height, floor);
+			case BEDROOM:
+				return new CastleRoomBedroom(startPos, sideLength, height, floor);
 
-		case ARMORY:
-			return new CastleRoomArmory(startPos, sideLength, height, floor);
+			case ARMORY:
+				return new CastleRoomArmory(startPos, sideLength, height, floor);
 
-		case ALCHEMY_LAB:
-			return new CastleRoomAlchemyLab(startPos, sideLength, height, floor);
+			case ALCHEMY_LAB:
+				return new CastleRoomAlchemyLab(startPos, sideLength, height, floor);
 
-		default:
-			return null;
+			case LIBRARY:
+				return new CastleRoomLibrary(startPos, sideLength, height, floor);
+
+			default:
+				return null;
 		}
 	}
 }
