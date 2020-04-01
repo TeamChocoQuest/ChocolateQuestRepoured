b9d2b61705136465278d32fc73b80fe2b8b9831d
Meldexun <micha.kotlowski@gmx.de>
2020-04-01 20:20:23 +0200

    Updated WalkerLightningBolt -> ColoredLightningBolt

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/boss/walkerking/BossAIWalkerLightningCircles.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/boss/walkerking/BossAIWalkerLightningCircles.java
@@ -2,7 +2,7 @@ package com.teamcqr.chocolatequestrepoured.objects.entity.ai.boss.walkerking;
 
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.AbstractCQREntityAI;
 import com.teamcqr.chocolatequestrepoured.objects.entity.boss.EntityCQRWalkerKing;
-import com.teamcqr.chocolatequestrepoured.objects.entity.misc.EntityWalkerLightningBolt;
+import com.teamcqr.chocolatequestrepoured.objects.entity.misc.EntityColoredLightningBolt;
 import com.teamcqr.chocolatequestrepoured.util.DungeonGenUtils;
 import com.teamcqr.chocolatequestrepoured.util.VectorUtil;
 
@@ -59,9 +59,8 @@ public class BossAIWalkerLightningCircles extends AbstractCQREntityAI {
 		Vec3d v = new Vec3d(circleRad, 0, 0);
 		for(int i = 0; i < (2* circleRad); i++) {
 			v = VectorUtil.rotateVectorAroundY(v, angle * i);
-			EntityWalkerLightningBolt lightning = new EntityWalkerLightningBolt(entity.world, entity.posX + v.x, entity.posY + v.y, entity.posZ + v.z, false);
+			EntityColoredLightningBolt lightning = new EntityColoredLightningBolt(entity.world, entity.posX + v.x, entity.posY + v.y, entity.posZ + v.z, true, false, 0.34F, 0.08F, 0.43F, 0.4F);
 			lightning.setPosition(entity.posX + v.x, entity.posY + v.y, entity.posZ + v.z);
-			//entity.world.addWeatherEffect(lightning);
 			entity.world.spawnEntity(lightning);
 		}
 	}
