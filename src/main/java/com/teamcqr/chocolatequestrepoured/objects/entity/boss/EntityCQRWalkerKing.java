8e09c1e3eb9ad7e0843cc4d03047591c4820b02b
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-04-13 21:23:49 +0200

    arrows are now purely cosmetic

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRWalkerKing.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRWalkerKing.java
@@ -205,7 +205,7 @@ public class EntityCQRWalkerKing extends AbstractEntityCQRBoss {
 		for(int i = 0; i < lightningCount; i++) {
 			Vec3d p = VectorUtil.rotateVectorAroundY(v, i * angle);
 			int dY = -3 + getRNG().nextInt(7);
-			EntityColoredLightningBolt clb = new EntityColoredLightningBolt(world, dragon.posX + p.x, dragon.posY + dY, dragon.posZ + p.z, true, false, 1F, 0.00F, 0.0F, 0.4F);
+			EntityColoredLightningBolt clb = new EntityColoredLightningBolt(world, dragon.posX + p.x, dragon.posY + dY, dragon.posZ + p.z, false, false, 1F, 0.00F, 0.0F, 0.4F);
 			world.spawnEntity(clb);
 		}
 		dragon.attackEntityFrom(DamageSource.MAGIC, 10F);
