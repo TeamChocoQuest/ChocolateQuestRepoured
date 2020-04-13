f18c4d7487f695c22fa4f2c50e4b617ba2cb293a
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-04-13 13:04:23 +0200

    tweaking the walker king & experience drops

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/bases/AbstractEntityCQRBoss.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/bases/AbstractEntityCQRBoss.java
@@ -118,7 +118,7 @@ public abstract class AbstractEntityCQRBoss extends AbstractEntityCQR {
 			super.onDeathUpdate();
 		}
 	}
-
+	
 	protected SoundEvent getFinalDeathSound() {
 		return this.getDeathSound();
 	}
