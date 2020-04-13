868c4347955ff26f1d05743c6e75c2605cde485f
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-04-13 14:36:12 +0200

    NPE patch

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/spells/EntityAIBlindTargetSpell.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/spells/EntityAIBlindTargetSpell.java
@@ -23,15 +23,17 @@ public class EntityAIBlindTargetSpell extends AbstractEntityAISpell implements I
 	@Override
 	protected void startCastingSpell() {
 		EntityLivingBase attackTarget = this.entity.getAttackTarget();
-		Vec3d vec = attackTarget.getPositionVector();
-		vec = vec.subtract(attackTarget.getLookVec().scale(8.0D));
-		vec = vec.subtract(0.0D, 0.001D, 0.0D);
-		BlockPos pos = new BlockPos(vec);
+		if(attackTarget != null) {
+			Vec3d vec = attackTarget.getPositionVector();
+			vec = vec.subtract(attackTarget.getLookVec().scale(8.0D));
+			vec = vec.subtract(0.0D, 0.001D, 0.0D);
+			BlockPos pos = new BlockPos(vec);
 
-		attackTarget.addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, this.duration));
-		if (this.entity.world.getBlockState(pos).isSideSolid(this.entity.world, pos, EnumFacing.UP)) {
-			this.entity.playSound(SoundEvents.ENTITY_ENDERMEN_TELEPORT, 0.7F, 1.1F);
-			this.entity.attemptTeleport(vec.x, vec.y, vec.z);
+			attackTarget.addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, this.duration));
+			if (this.entity.world.getBlockState(pos).isSideSolid(this.entity.world, pos, EnumFacing.UP)) {
+				this.entity.playSound(SoundEvents.ENTITY_ENDERMEN_TELEPORT, 0.7F, 1.1F);
+				this.entity.attemptTeleport(vec.x, vec.y, vec.z);
+			}
 		}
 	}
 
