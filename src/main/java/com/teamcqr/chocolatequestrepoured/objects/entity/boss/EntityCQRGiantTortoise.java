cd7efe5007e6690cbfe9c577de693cdec7f0eb87
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-04-14 19:33:06 +0200

    fixed tortoise walk to leader AI

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRGiantTortoise.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/boss/EntityCQRGiantTortoise.java
@@ -8,6 +8,7 @@ import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIMoveToHome;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.EntityAIMoveToLeader;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.boss.gianttortoise.AISpinAttackTurtle;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.boss.gianttortoise.AISwitchStates;
+import com.teamcqr.chocolatequestrepoured.objects.entity.ai.boss.gianttortoise.AITortoiseMoveToLeader;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.boss.gianttortoise.AITortoiseWalkHome;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.boss.gianttortoise.BossAIHealingTurtle;
 import com.teamcqr.chocolatequestrepoured.objects.entity.ai.boss.gianttortoise.BossAIStunTurtle;
@@ -101,27 +102,7 @@ public class EntityCQRGiantTortoise extends AbstractEntityCQRBoss implements IEn
 		this.tasks.addTask(2, new BossAIStunTurtle(this));
 		this.tasks.addTask(4, new BossAIHealingTurtle(this));
 		this.tasks.addTask(6, new AISpinAttackTurtle(this));
-		this.tasks.addTask(15, new EntityAIMoveToLeader(this) {
-			@Override
-			public boolean shouldExecute() {
-				if(super.shouldExecute() && !((EntityCQRGiantTortoise) entity).isInShell() && !isHealing && !isStunned() && !isSpinning()) {
-					return true;
-				} else if(super.shouldExecute() && !isHealing && !isStunned() && !isSpinning()){
-					((EntityCQRGiantTortoise) entity).targetNewState(TARGET_MOVE_OUT);
-				}
-				return false;
-			}
-			
-			@Override
-			public boolean shouldContinueExecuting() {
-				if(super.shouldContinueExecuting() && !((EntityCQRGiantTortoise) entity).isInShell() && !isHealing && !isStunned() && !isSpinning()) {
-					return true;
-				} else if(super.shouldContinueExecuting() && !isHealing && !isStunned() && !isSpinning()){
-					((EntityCQRGiantTortoise) entity).targetNewState(TARGET_MOVE_OUT);
-				}
-				return false;
-			}
-		});
+		this.tasks.addTask(19, new AITortoiseMoveToLeader(this));
 		this.tasks.addTask(20, new AITortoiseWalkHome(this));
 		this.tasks.addTask(21, new EntityAIIdleSit(this) {
 			@Override
