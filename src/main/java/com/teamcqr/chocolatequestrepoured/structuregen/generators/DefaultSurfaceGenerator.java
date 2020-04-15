8a4a870d4261f8fa7bf35b0e783ff88891ea9c56
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-04-15 17:59:27 +0200

    updated cover block generation to new system

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/DefaultSurfaceGenerator.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/DefaultSurfaceGenerator.java
@@ -1,25 +1,29 @@
 package com.teamcqr.chocolatequestrepoured.structuregen.generators;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import com.teamcqr.chocolatequestrepoured.structuregen.PlateauBuilder;
 import com.teamcqr.chocolatequestrepoured.structuregen.dungeons.DefaultSurfaceDungeon;
+import com.teamcqr.chocolatequestrepoured.structuregen.generation.ExtendedBlockStatePart;
 import com.teamcqr.chocolatequestrepoured.structuregen.generation.IStructure;
 import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.CQStructure;
 import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.EPosType;
+import com.teamcqr.chocolatequestrepoured.util.CQRConfig;
 import com.teamcqr.chocolatequestrepoured.util.DungeonGenUtils;
 
+import net.minecraft.block.Block;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.structure.template.PlacementSettings;
 import net.minecraft.world.gen.structure.template.Template;
 
 /**
- * Copyright (c) 29.04.2019
- * Developed by DerToaster98
- * GitHub: https://github.com/DerToaster98
+ * Copyright (c) 29.04.2019 Developed by DerToaster98 GitHub: https://github.com/DerToaster98
  */
 public class DefaultSurfaceGenerator implements IDungeonGenerator {
 
@@ -65,13 +69,9 @@ public class DefaultSurfaceGenerator implements IDungeonGenerator {
 		}
 
 		/*
-		 * List<String> bosses = new ArrayList<>();
-		 * for(UUID id : structure.getBossIDs()) {
-		 * bosses.add(id.toString());
-		 * }
+		 * List<String> bosses = new ArrayList<>(); for(UUID id : structure.getBossIDs()) { bosses.add(id.toString()); }
 		 * 
-		 * CQDungeonStructureGenerateEvent event = new CQDungeonStructureGenerateEvent(this.dungeon, new BlockPos(x, y, z), this.structure.getSize(), world, bosses);
-		 * event.setShieldCorePosition(this.structure.getShieldCorePosition());
+		 * CQDungeonStructureGenerateEvent event = new CQDungeonStructureGenerateEvent(this.dungeon, new BlockPos(x, y, z), this.structure.getSize(), world, bosses); event.setShieldCorePosition(this.structure.getShieldCorePosition());
 		 * MinecraftForge.EVENT_BUS.post(event);
 		 */
 	}
@@ -94,49 +94,49 @@ public class DefaultSurfaceGenerator implements IDungeonGenerator {
 	@Override
 	public void placeCoverBlocks(World world, Chunk chunk, int x, int y, int z, List<List<? extends IStructure>> lists) {
 		if (this.dungeon.isCoverBlockEnabled()) {
-			// TODO: Adjust to the new system
-			/*
-			 * int sizeX = this.structure.getSize().getX();
-			 * int sizeZ = this.structure.getSize().getZ();
-			 * switch (this.placeSettings.getRotation()) {
-			 * case CLOCKWISE_90:
-			 * x -= sizeZ; {
-			 * int i = sizeX;
-			 * int j = sizeZ;
-			 * sizeX = j;
-			 * sizeZ = i;
-			 * }
-			 * break;
-			 * case CLOCKWISE_180:
-			 * x -= sizeX;
-			 * z -= sizeZ;
-			 * break;
-			 * case COUNTERCLOCKWISE_90:
-			 * z -= sizeX; {
-			 * int i = sizeX;
-			 * int j = sizeZ;
-			 * sizeX = j;
-			 * sizeZ = i;
-			 * }
-			 * break;
-			 * default:
-			 * break;
-			 * }
-			 * int startX = x - sizeX / 3 - CQRConfig.general.supportHillWallSize / 2;
-			 * int startZ = z - sizeZ / 3 - CQRConfig.general.supportHillWallSize / 2;
-			 * 
-			 * int endX = x + sizeX + sizeX / 3 + CQRConfig.general.supportHillWallSize / 2;
-			 * int endZ = z + sizeZ + sizeZ / 3 + CQRConfig.general.supportHillWallSize / 2;
-			 * 
-			 * for (int iX = startX; iX <= endX; iX++) {
-			 * for (int iZ = startZ; iZ <= endZ; iZ++) {
-			 * BlockPos pos = new BlockPos(iX, world.getTopSolidOrLiquidBlock(new BlockPos(iX, 0, iZ)).getY(), iZ);
-			 * if (!Block.isEqualTo(world.getBlockState(pos.subtract(new Vec3i(0, 1, 0))).getBlock(), this.dungeon.getCoverBlock())) {
-			 * world.setBlockState(pos, this.dungeon.getCoverBlock().getDefaultState());
-			 * }
-			 * }
-			 * }
-			 */
+			Map<BlockPos, ExtendedBlockStatePart.ExtendedBlockState> stateMap = new HashMap<>();
+			
+			int sizeX = this.structure.getSize().getX();
+			int sizeZ = this.structure.getSize().getZ();
+			switch (this.placeSettings.getRotation()) {
+			case CLOCKWISE_90:
+				x -= sizeZ; {
+				int i = sizeX;
+				int j = sizeZ;
+				sizeX = j;
+				sizeZ = i;
+			}
+				break;
+			case CLOCKWISE_180:
+				x -= sizeX;
+				z -= sizeZ;
+				break;
+			case COUNTERCLOCKWISE_90:
+				z -= sizeX; {
+				int i = sizeX;
+				int j = sizeZ;
+				sizeX = j;
+				sizeZ = i;
+			}
+				break;
+			default:
+				break;
+			}
+			int startX = x - sizeX / 3 - CQRConfig.general.supportHillWallSize / 2;
+			int startZ = z - sizeZ / 3 - CQRConfig.general.supportHillWallSize / 2;
+
+			int endX = x + sizeX + sizeX / 3 + CQRConfig.general.supportHillWallSize / 2;
+			int endZ = z + sizeZ + sizeZ / 3 + CQRConfig.general.supportHillWallSize / 2;
+
+			for (int iX = startX; iX <= endX; iX++) {
+				for (int iZ = startZ; iZ <= endZ; iZ++) {
+					BlockPos pos = new BlockPos(iX, world.getTopSolidOrLiquidBlock(new BlockPos(iX, 0, iZ)).getY(), iZ);
+					if (!Block.isEqualTo(world.getBlockState(pos.subtract(new Vec3i(0, 1, 0))).getBlock(), this.dungeon.getCoverBlock())) {
+						stateMap.put(pos, new ExtendedBlockStatePart.ExtendedBlockState(this.dungeon.getCoverBlock().getDefaultState(), null));
+					}
+				}
+			}
+			lists.add(ExtendedBlockStatePart.splitExtendedBlockStateMap(stateMap));
 		}
 
 	}
