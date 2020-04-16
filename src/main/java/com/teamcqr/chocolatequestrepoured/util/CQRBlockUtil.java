c9a944daf334b5a58fed293d1eb6ac3be2cf2f6c
KalgogSmash <trevor.seyf@gmail.com>
2020-04-16 15:42:52 -0700

    Rename Blockutil

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/CQRBlockUtil.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/CQRBlockUtil.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class BlockUtil {
+public class CQRBlockUtil {
     public static final Block[] VANILLA_STONE = {
             Blocks.STONE,
             Blocks.STONEBRICK,
