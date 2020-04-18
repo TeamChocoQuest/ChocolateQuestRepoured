a64e4233b767e9e1408f82d2b4af6a19ca0a6a3c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-04-18 10:54:17 +0200

    adapted to new system

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/thewall/wallparts/WallPartRailingTower.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/thewall/wallparts/WallPartRailingTower.java
@@ -1,11 +1,13 @@
 package com.teamcqr.chocolatequestrepoured.structuregen.thewall.wallparts;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.teamcqr.chocolatequestrepoured.structuregen.generation.ExtendedBlockStatePart;
+import com.teamcqr.chocolatequestrepoured.structuregen.generation.ExtendedBlockStatePart.ExtendedBlockState;
 import com.teamcqr.chocolatequestrepoured.structuregen.generation.IStructure;
 import com.teamcqr.chocolatequestrepoured.util.CQRConfig;
-import com.teamcqr.chocolatequestrepoured.util.Reference;
 
 import net.minecraft.block.BlockStoneSlab;
 import net.minecraft.init.Blocks;
@@ -35,86 +37,49 @@ public class WallPartRailingTower implements IWallPart {
 
 		int[] xValues = new int[] { 0, 1, 6, 7 };
 		int[] zValues = new int[] { 2, 3, 12, 13 };
+		
+		Map<BlockPos, ExtendedBlockStatePart.ExtendedBlockState> stateMap = new HashMap<>();
+		ExtendedBlockStatePart.ExtendedBlockState stateBlock = new ExtendedBlockState(Blocks.DOUBLE_STONE_SLAB.getDefaultState().withProperty(BlockStoneSlab.VARIANT, BlockStoneSlab.EnumType.STONE).withProperty(BlockStoneSlab.SEAMLESS, true), null);
+		ExtendedBlockStatePart.ExtendedBlockState stateAir = new ExtendedBlockState(Blocks.AIR.getDefaultState(), null);
 
 		// Normal railing
-		List<BlockPos> railingBlocks = new ArrayList<BlockPos>();
 		for (int y = 0; y <= 7; y++) {
 			for (int z : zValues) {
 				for (int x : xValues) {
 					if (this.isBiggerPart(x)) {
 						// the check is needed to specify wether its the north or south side of the wall (greater than 3 is north side)
 						if (y < 3 && z == (z > 3 ? 12 : 3)) {
-							railingBlocks.add(new BlockPos(startX + x * 2, this.getTopY() + y, startZ + z));
-							railingBlocks.add(new BlockPos(startX + x * 2 + 1, this.getTopY() + y, startZ + z));
+							stateMap.put(new BlockPos(startX + x * 2, this.getTopY() + y, startZ + z), stateBlock);
+							stateMap.put(new BlockPos(startX + x * 2 + 1, this.getTopY() + y, startZ + z), stateBlock);
 						} else if (y >= 3) {
 							// This is for the "thick" part of the bigger railing
-							railingBlocks.add(new BlockPos(startX + x * 2, this.getTopY() + y, startZ + z));
-							railingBlocks.add(new BlockPos(startX + x * 2 + 1, this.getTopY() + y, startZ + z));
+							stateMap.put(new BlockPos(startX + x * 2, this.getTopY() + y, startZ + z), stateBlock);
+							stateMap.put(new BlockPos(startX + x * 2 + 1, this.getTopY() + y, startZ + z), stateBlock);
 						}
 						// This builds the lower pillars, again the check is needed to identify the side
 					} else if (y >= 4 && z == (z > 3 ? 12 : 3) && y <= 6) {
-						railingBlocks.add(new BlockPos(startX + x * 2, this.getTopY() + y, startZ + z));
-						railingBlocks.add(new BlockPos(startX + x * 2 + 1, this.getTopY() + y, startZ + z));
+						stateMap.put(new BlockPos(startX + x * 2, this.getTopY() + y, startZ + z), stateBlock);
+						stateMap.put(new BlockPos(startX + x * 2 + 1, this.getTopY() + y, startZ + z), stateBlock);
 					}
 				}
 			}
 		}
 
-		List<BlockPos> doorwayBlocks = new ArrayList<BlockPos>();
 		// Builds the "doorway"
 		for (int y = 6; y <= 9; y++) {
 			for (int z = 6; z <= 9; z++) {
 				for (int x = 4; x <= 11; x++) {
 					if (y < 9) {
-						doorwayBlocks.add(new BlockPos(startX + x, this.getTopY() + y, startZ + z));
+						stateMap.put(new BlockPos(startX + x, this.getTopY() + y, startZ + z), stateAir);
 					} else if (z == 7 || z == 8) {
 						// This is for the thinner gap at the top
-						doorwayBlocks.add(new BlockPos(startX + x, this.getTopY() + y, startZ + z));
+						stateMap.put(new BlockPos(startX + x, this.getTopY() + y, startZ + z), stateAir);
 					}
 				}
 			}
 		}
-		if (!doorwayBlocks.isEmpty()) {
-			/*
-			 * for(BlockPos pos : doorwayBlocks) {
-			 * world.setBlockToAir(pos);
-			 * }
-			 */
-			final List<BlockPos> posL = new ArrayList<BlockPos>(doorwayBlocks);
-			doorwayBlocks.clear();
-			Reference.BLOCK_PLACING_THREADS_INSTANCE.addTask(new Runnable() {
-
-				@Override
-				public void run() {
-
-					for (BlockPos p : posL) {
-						world.setBlockToAir(p);
-					}
-
-				}
-			});
-		}
-
-		if (!railingBlocks.isEmpty()) {
-			/*
-			 * for(BlockPos pos : railingBlocks) {
-			 * world.setBlockState(pos, Blocks.DOUBLE_STONE_SLAB.getDefaultState().withProperty(BlockStoneSlab.VARIANT, BlockStoneSlab.EnumType.STONE));
-			 * }
-			 */
-			final List<BlockPos> posL = new ArrayList<BlockPos>(railingBlocks);
-			railingBlocks.clear();
-			Reference.BLOCK_PLACING_THREADS_INSTANCE.addTask(new Runnable() {
-
-				@Override
-				public void run() {
-
-					for (BlockPos p : posL) {
-						world.setBlockState(p, Blocks.DOUBLE_STONE_SLAB.getDefaultState().withProperty(BlockStoneSlab.VARIANT, BlockStoneSlab.EnumType.STONE).withProperty(BlockStoneSlab.SEAMLESS, true), 2);
-					}
-
-				}
-			});
-		}
+		
+		lists.add(ExtendedBlockStatePart.splitExtendedBlockStateMap(stateMap));
 	}
 
 	private boolean isBiggerPart(int xAsChunkRelativeCoord) {
