cad0ba257af00a45e343f91fbbc12122ecb40ce4
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-04-20 19:36:44 +0200

    Fixed wall generation

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/dungeons/DungeonOceanFloor.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/dungeons/DungeonOceanFloor.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 import com.teamcqr.chocolatequestrepoured.structuregen.generators.IDungeonGenerator;
 import com.teamcqr.chocolatequestrepoured.structuregen.generators.OceanFloorGenerator;
 import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.CQStructure;
+import com.teamcqr.chocolatequestrepoured.util.DungeonGenUtils;
 
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
@@ -29,7 +30,7 @@ public class DungeonOceanFloor extends DungeonSurface {
 		int y = 0;
 		for (int ix = 0; ix < 16; ix++) {
 			for (int iz = 0; iz < 16; iz++) {
-				y += this.getYForPos(world, chunk.x * 16 + ix, chunk.z * 16 + iz, true);
+				y += DungeonGenUtils.getYForPos(world, chunk.x * 16 + ix, chunk.z * 16 + iz, true);
 			}
 		}
 		y /= 256;
