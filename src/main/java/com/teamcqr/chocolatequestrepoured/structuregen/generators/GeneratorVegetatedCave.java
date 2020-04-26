11b0c8653686990c3657744a82be19257d6d5b5d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-04-26 17:10:17 +0200

    giant mushrooms

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/GeneratorVegetatedCave.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/GeneratorVegetatedCave.java
@@ -17,14 +17,15 @@ import com.teamcqr.chocolatequestrepoured.structuregen.dungeons.DungeonBase;
 import com.teamcqr.chocolatequestrepoured.structuregen.dungeons.DungeonVegetatedCave;
 import com.teamcqr.chocolatequestrepoured.structuregen.generation.ExtendedBlockStatePart;
 import com.teamcqr.chocolatequestrepoured.structuregen.generation.ExtendedBlockStatePart.ExtendedBlockState;
-import com.teamcqr.chocolatequestrepoured.structuregen.lootchests.ELootTable;
 import com.teamcqr.chocolatequestrepoured.structuregen.generation.IStructure;
+import com.teamcqr.chocolatequestrepoured.structuregen.lootchests.ELootTable;
 import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.CQStructure;
 import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.EPosType;
 import com.teamcqr.chocolatequestrepoured.util.DungeonGenUtils;
 import com.teamcqr.chocolatequestrepoured.util.VectorUtil;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockHugeMushroom;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
@@ -37,8 +38,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.gen.feature.WorldGenBigMushroom;
-import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraft.world.gen.structure.template.PlacementSettings;
 
 public class GeneratorVegetatedCave implements IDungeonGenerator {
@@ -68,24 +67,24 @@ public class GeneratorVegetatedCave implements IDungeonGenerator {
 		Block[][][] blocks = getRandomBlob(dungeon.getAirBlock(), dungeon.getCentralCaveSize(), (int) (dungeon.getCentralCaveSize() * 0.75), random);
 		this.floorBlocks.addAll(getFloorBlocksOfBlob(blocks, new BlockPos(x, y, z), random));
 		storeBlockArrayInMap(blocks, new BlockPos(x, y, z));
-		//lists.add(ExtendedBlockStatePart.split(new BlockPos(x - dungeon.getCentralCaveSize(), y, z - dungeon.getCentralCaveSize()), blocks));
+		// lists.add(ExtendedBlockStatePart.split(new BlockPos(x - dungeon.getCentralCaveSize(), y, z - dungeon.getCentralCaveSize()), blocks));
 		Vec3d center = new Vec3d(x, y - (dungeon.getCentralCaveSize() / 2), z);
-		Vec3d rad = new Vec3d(dungeon.getCentralCaveSize() *1.75, 0, 0);
+		Vec3d rad = new Vec3d(dungeon.getCentralCaveSize() * 1.75, 0, 0);
 		double angle = 360D / dungeon.getCaveCount();
 		for (int i = 0; i < dungeon.getCaveCount(); i++) {
 			Vec3d v = VectorUtil.rotateVectorAroundY(rad, angle * i);
 			Vec3d startPos = center.add(v);
 			createTunnel(startPos, angle * i, dungeon.getCentralCaveSize() / (dungeon.getCaveCount() - 1), dungeon.getCaveSegmentCount(), random, lists);
 		}
-		//Filter floorblocks
+		// Filter floorblocks
 		filterFloorBlocks();
-		
-		//Flowers, Mushrooms and Weed
-		if(this.dungeon.placeVegetation()) {
+
+		// Flowers, Mushrooms and Weed
+		if (this.dungeon.placeVegetation()) {
 			createVegetation(random);
 		}
-		
-		//Build
+
+		// Build
 		lists.add(ExtendedBlockStatePart.splitExtendedBlockStateMap(this.blocks));
 	}
 
@@ -97,7 +96,7 @@ public class GeneratorVegetatedCave implements IDungeonGenerator {
 			if (file != null) {
 				CQStructure structure = new CQStructure(file);
 				structure.setDungeonMob(this.mobtype);
-				//TODO: Support platform
+				// TODO: Support platform
 				PlacementSettings settings = new PlacementSettings();
 				settings.setMirror(Mirror.NONE);
 				settings.setRotation(Rotation.NONE);
@@ -113,25 +112,21 @@ public class GeneratorVegetatedCave implements IDungeonGenerator {
 
 	@Override
 	public void postProcess(World world, Chunk chunk, int x, int y, int z, List<List<? extends IStructure>> lists) {
-		// TODO: Place giant shrooms
+		// DONE: Place giant shrooms
 		Map<BlockPos, ExtendedBlockStatePart.ExtendedBlockState> stateMap = new HashMap<>();
 		Random random = new Random(WorldDungeonGenerator.getSeed(world, x / 16, z / 16));
-		for(BlockPos p : this.giantMushrooms) {
-			//Place shroom
-			if(random.nextBoolean()) {
-				//Red shroom
-				
-			} else {
-				//Brown shroom
-				
+		for (BlockPos p : this.giantMushrooms) {
+			// Place shroom
+			if (random.nextBoolean()) {
+				generateGiantMushroom(p, random, stateMap);
 			}
-			
-			if(random.nextInt(3) == 0) {
-				//Spawner
-				BlockPos spawner = new BlockPos(p.getX() + (random.nextBoolean() ? -1 : 1), p.getY() +1, p.getZ() + (random.nextBoolean() ? -1 : 1));
+
+			if (random.nextInt(3) == 0) {
+				// Spawner
+				BlockPos spawner = new BlockPos(p.getX() + (random.nextBoolean() ? -1 : 1), p.getY() + 1, p.getZ() + (random.nextBoolean() ? -1 : 1));
 				this.spawners.add(spawner);
-				if(random.nextInt(3) >= 1) {
-					//Chest
+				if (random.nextInt(3) >= 1) {
+					// Chest
 					this.chests.add(spawner.down());
 				}
 			}
@@ -144,11 +139,11 @@ public class GeneratorVegetatedCave implements IDungeonGenerator {
 		// DONE: Place and fill chests
 		Map<BlockPos, ExtendedBlockStatePart.ExtendedBlockState> stateMap = new HashMap<>();
 		Random random = new Random(WorldDungeonGenerator.getSeed(world, x / 16, z / 16));
-		for(BlockPos pos : this.chests) {
+		for (BlockPos pos : this.chests) {
 			Block block = Blocks.CHEST;
 			IBlockState state = block.getDefaultState();
-			TileEntityChest chest = (TileEntityChest)block.createTileEntity(world, state);
-			
+			TileEntityChest chest = (TileEntityChest) block.createTileEntity(world, state);
+
 			int eltID = dungeon.getChestID(random);
 			if (chest != null) {
 				ResourceLocation resLoc = null;
@@ -162,7 +157,7 @@ public class GeneratorVegetatedCave implements IDungeonGenerator {
 					chest.setLootTable(resLoc, seed);
 				}
 			}
-			
+
 			NBTTagCompound nbt = chest.writeToNBT(new NBTTagCompound());
 			stateMap.put(pos, new ExtendedBlockStatePart.ExtendedBlockState(state, nbt));
 		}
@@ -173,13 +168,13 @@ public class GeneratorVegetatedCave implements IDungeonGenerator {
 	public void placeSpawners(World world, Chunk chunk, int x, int y, int z, List<List<? extends IStructure>> lists) {
 		// DONE: Place spawners
 		Map<BlockPos, ExtendedBlockStatePart.ExtendedBlockState> stateMap = new HashMap<>();
-		for(BlockPos pos : this.spawners) {
+		for (BlockPos pos : this.spawners) {
 			Block block = Blocks.MOB_SPAWNER;
 			IBlockState state = block.getDefaultState();
-			TileEntityMobSpawner spawner = (TileEntityMobSpawner)block.createTileEntity(world, state);
+			TileEntityMobSpawner spawner = (TileEntityMobSpawner) block.createTileEntity(world, state);
 			spawner.getSpawnerBaseLogic().setEntityId(mobtype.getEntityResourceLocation());
 			spawner.updateContainingBlockInfo();
-			
+
 			NBTTagCompound nbt = spawner.writeToNBT(new NBTTagCompound());
 			stateMap.put(pos, new ExtendedBlockStatePart.ExtendedBlockState(state, nbt));
 		}
@@ -251,22 +246,6 @@ public class GeneratorVegetatedCave implements IDungeonGenerator {
 		return getRandomBlob(block, radius, radius, random);
 	}
 
-	//TODO: Rewrite to generate 3d array, using worldgenerators doesnt work with our system
-	private boolean getGiantMushroom(Random random, BlockPos pos, World world) {
-		WorldGenerator worldgenerator = null;
-
-		if (random.nextBoolean()) {
-			worldgenerator = new WorldGenBigMushroom(Blocks.BROWN_MUSHROOM_BLOCK);
-		} else {
-			worldgenerator = new WorldGenBigMushroom(Blocks.RED_MUSHROOM_BLOCK);
-		}
-
-		if (worldgenerator != null && worldgenerator.generate(world, random, pos)) {
-			return true;
-		}
-		return false;
-	}
-
 	private Block[][][] getRandomBlob(Block block, int radius, int radiusY, Random random) {
 		Block[][][] blocks = new Block[radius * 4][radiusY * 4][radius * 4];
 		int subSphereCount = radius * 3;
@@ -316,14 +295,14 @@ public class GeneratorVegetatedCave implements IDungeonGenerator {
 		}
 		return blocks;
 	}
-	
+
 	private void filterFloorBlocks() {
 		this.floorBlocks.removeIf(new Predicate<BlockPos>() {
 
 			@Override
 			public boolean test(BlockPos floorPos) {
 				BlockPos lower = floorPos.down();
-				if(blocks.containsKey(lower)) {
+				if (blocks.containsKey(lower)) {
 					blocks.put(floorPos, new ExtendedBlockState(dungeon.getAirBlock().getDefaultState(), null));
 					return true;
 				}
@@ -331,45 +310,153 @@ public class GeneratorVegetatedCave implements IDungeonGenerator {
 			}
 		});
 	}
-	
+
 	private void createVegetation(Random random) {
-		for(BlockPos floorPos : this.floorBlocks) {
+		for (BlockPos floorPos : this.floorBlocks) {
 			int number = random.nextInt(300);
 			IBlockState state = null;
-			if(number >= 295) {
-				//Giant mushroom
-				for(BlockPos shroom : giantMushrooms) {
-					if(shroom.getDistance(floorPos.getX(), floorPos.getY(), floorPos.getZ()) < 8) {
+			if (number >= 295) {
+				// Giant mushroom
+				for (BlockPos shroom : giantMushrooms) {
+					if (shroom.getDistance(floorPos.getX(), floorPos.getY(), floorPos.getZ()) < 8) {
 						continue;
 					}
 				}
 				giantMushrooms.add(floorPos.up());
-			}
-			else if(number >= 290) {
-				//Lantern
+			} else if (number >= 290) {
+				// Lantern
 				state = dungeon.getPumpkinBlock().getDefaultState();
-			}
-			else if(number <= 150) {
-				if(number <= 100) {
-					//Grass
+			} else if (number <= 150) {
+				if (number <= 100) {
+					// Grass
 					state = dungeon.getGrassBlock(random).getDefaultState();
 				} else {
-					//Flower or mushroom
-					if(random.nextBoolean()) {
-						//Flower
+					// Flower or mushroom
+					if (random.nextBoolean()) {
+						// Flower
 						state = dungeon.getFlowerBlock(random).getDefaultState();
 					} else {
-						//Mushroom
+						// Mushroom
 						state = dungeon.getMushroomBlock(random).getDefaultState();
 					}
 				}
 			}
-			if(state != null) {
+			if (state != null) {
 				blocks.put(floorPos.up(), new ExtendedBlockState(state, null));
 			}
 		}
 		System.out.println("Floor blocks: " + floorBlocks.size());
 		System.out.println("Giant mushrooms: " + giantMushrooms.size());
+	}
+
+	private void generateGiantMushroom(BlockPos position, Random rand, Map<BlockPos, ExtendedBlockState> stateMap) {
+		Block block = rand.nextBoolean() ? Blocks.BROWN_MUSHROOM_BLOCK : Blocks.RED_MUSHROOM_BLOCK;
+		int i = 6;
+
+		if (position.getY() >= 1 && position.getY() + i + 1 < 256) {
+
+			int k2 = position.getY() + i;
+
+			if (block == Blocks.RED_MUSHROOM_BLOCK) {
+				k2 = position.getY() + i - 3;
+			}
+
+			for (int l2 = k2; l2 <= position.getY() + i; ++l2) {
+				int j3 = 1;
+
+				if (l2 < position.getY() + i) {
+					++j3;
+				}
+
+				if (block == Blocks.BROWN_MUSHROOM_BLOCK) {
+					j3 = 3;
+				}
+
+				int k3 = position.getX() - j3;
+				int l3 = position.getX() + j3;
+				int j1 = position.getZ() - j3;
+				int k1 = position.getZ() + j3;
+
+				for (int l1 = k3; l1 <= l3; ++l1) {
+					for (int i2 = j1; i2 <= k1; ++i2) {
+						int j2 = 5;
+
+						if (l1 == k3) {
+							--j2;
+						} else if (l1 == l3) {
+							++j2;
+						}
+
+						if (i2 == j1) {
+							j2 -= 3;
+						} else if (i2 == k1) {
+							j2 += 3;
+						}
+
+						BlockHugeMushroom.EnumType blockhugemushroom$enumtype = BlockHugeMushroom.EnumType.byMetadata(j2);
+
+						if (block == Blocks.BROWN_MUSHROOM_BLOCK || l2 < position.getY() + i) {
+							if ((l1 == k3 || l1 == l3) && (i2 == j1 || i2 == k1)) {
+								continue;
+							}
+
+							if (l1 == position.getX() - (j3 - 1) && i2 == j1) {
+								blockhugemushroom$enumtype = BlockHugeMushroom.EnumType.NORTH_WEST;
+							}
+
+							if (l1 == k3 && i2 == position.getZ() - (j3 - 1)) {
+								blockhugemushroom$enumtype = BlockHugeMushroom.EnumType.NORTH_WEST;
+							}
+
+							if (l1 == position.getX() + (j3 - 1) && i2 == j1) {
+								blockhugemushroom$enumtype = BlockHugeMushroom.EnumType.NORTH_EAST;
+							}
+
+							if (l1 == l3 && i2 == position.getZ() - (j3 - 1)) {
+								blockhugemushroom$enumtype = BlockHugeMushroom.EnumType.NORTH_EAST;
+							}
+
+							if (l1 == position.getX() - (j3 - 1) && i2 == k1) {
+								blockhugemushroom$enumtype = BlockHugeMushroom.EnumType.SOUTH_WEST;
+							}
+
+							if (l1 == k3 && i2 == position.getZ() + (j3 - 1)) {
+								blockhugemushroom$enumtype = BlockHugeMushroom.EnumType.SOUTH_WEST;
+							}
+
+							if (l1 == position.getX() + (j3 - 1) && i2 == k1) {
+								blockhugemushroom$enumtype = BlockHugeMushroom.EnumType.SOUTH_EAST;
+							}
+
+							if (l1 == l3 && i2 == position.getZ() + (j3 - 1)) {
+								blockhugemushroom$enumtype = BlockHugeMushroom.EnumType.SOUTH_EAST;
+							}
+						}
+
+						if (blockhugemushroom$enumtype == BlockHugeMushroom.EnumType.CENTER && l2 < position.getY() + i) {
+							blockhugemushroom$enumtype = BlockHugeMushroom.EnumType.ALL_INSIDE;
+						}
+
+						if (position.getY() >= position.getY() + i - 1 || blockhugemushroom$enumtype != BlockHugeMushroom.EnumType.ALL_INSIDE) {
+							BlockPos blockpos = new BlockPos(l1, l2, i2);
+							//IBlockState state = worldIn.getBlockState(blockpos);
+
+							// PUT IN MAP
+							//this.setBlockAndNotifyAdequately(worldIn, blockpos, block.getDefaultState().withProperty(BlockHugeMushroom.VARIANT, blockhugemushroom$enumtype));
+							stateMap.put(blockpos, new ExtendedBlockState(block.getDefaultState().withProperty(BlockHugeMushroom.VARIANT, blockhugemushroom$enumtype), null));
+						}
+					}
+				}
+			}
+
+			for (int i3 = 0; i3 < i; ++i3) {
+				//IBlockState iblockstate = worldIn.getBlockState(position.up(i3));
+				// PUT IN MAP
+				//this.setBlockAndNotifyAdequately(worldIn, position.up(i3), block.getDefaultState().withProperty(BlockHugeMushroom.VARIANT, BlockHugeMushroom.EnumType.STEM));
+				stateMap.put(position.up(i3), new ExtendedBlockState(block.getDefaultState().withProperty(BlockHugeMushroom.VARIANT, BlockHugeMushroom.EnumType.STEM), null));
+			}
+
+		}
 	}
 
 }
