234e66850cfb936b531f5c870f4ebe65c938aeee
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-04-28 15:46:55 +0200

    Updated slime armor

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/EntitySlimePart.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/EntitySlimePart.java
@@ -1,103 +1,76 @@
 package com.teamcqr.chocolatequestrepoured.objects.entity;
 
+import java.util.UUID;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.monster.EntitySlime;
-import net.minecraft.init.MobEffects;
-import net.minecraft.potion.PotionEffect;
-import net.minecraft.scoreboard.Team;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTUtil;
 import net.minecraft.world.World;
 
 public class EntitySlimePart extends EntitySlime {
-	private EntityLivingBase owner;
-	private float healthValue;
+
+	private UUID ownerUuid;
 
 	public EntitySlimePart(World worldIn) {
 		super(worldIn);
-		this.fallDistance = -5F;
 	}
 
-	public EntitySlimePart(World worldIn, EntityLivingBase owner, float size) {
+	public EntitySlimePart(World worldIn, EntityLivingBase owner) {
 		this(worldIn);
-		this.owner = owner;
-		this.setSlimeSize(Math.max(1, (int) size / 3), false);
-		this.setHealth(1.0F);
-		this.experienceValue = 0;
-		this.healthValue = size;
+		this.ownerUuid = owner.getPersistentID();
+	}
+
+	@Override
+	protected void initEntityAI() {
+		super.initEntityAI();
+		this.targetTasks.taskEntries.clear();
 	}
 
 	@Override
 	protected void applyEntityAttributes() {
 		super.applyEntityAttributes();
-		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(1.4D);
-		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(1.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(15.0D);
+		IAttributeInstance iattributeinstance = this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED);
+		iattributeinstance.setBaseValue(iattributeinstance.getBaseValue() * 0.5D);
 	}
 
 	@Override
 	public void onUpdate() {
-		super.onUpdate();
-
 		if (this.ticksExisted > 400) {
+			this.setDead();
+		}
+
+		super.onUpdate();
+	}
+
+	@Override
+	protected void collideWithEntity(Entity entityIn) {
+		if (entityIn instanceof EntityLivingBase && entityIn.getPersistentID().equals(this.ownerUuid)) {
+			((EntityLivingBase) entityIn).heal(2.0F);
 			this.setDead();
 		}
 	}
 
 	@Override
-	public void applyEntityCollision(Entity entityIn) {
-		if (entityIn == this.owner) {
-			if (this.ticksExisted > 10) {
-				this.owner.setHealth(this.owner.getHealth() + 2F);
-				this.setDead();
-			} else {
-				this.owner.setHealth(this.owner.getHealth() + 1F);
-				this.setDead();
-			}
-		}
-
-		if (entityIn instanceof EntityLivingBase && entityIn.getClass() != this.getClass()) {
-			EntityLivingBase entity = (EntityLivingBase) entityIn;
-
-			if (!entity.isOnSameTeam(this)) {
-				entity.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 40, 1, false, false));
-			}
-			super.applyEntityCollision(entityIn);
-		}
+	protected boolean canDropLoot() {
+		return false;
 	}
 
 	@Override
-	public boolean canAttackClass(Class<? extends EntityLivingBase> cls) {
-		return (super.canAttackClass(cls)) && (cls != this.getClass()) && (cls != EntitySlimePart.class);
+	public void writeEntityToNBT(NBTTagCompound compound) {
+		super.writeEntityToNBT(compound);
+		compound.setInteger("ticksExisted", this.ticksExisted);
+		compound.setTag("ownerUuid", NBTUtil.createUUIDTag(this.ownerUuid));
 	}
 
 	@Override
-	public void setDead() {
-		if (!this.world.isRemote && !this.isDead && this.getHealth() <= 0.0F && this.getSlimeSize() > 1) {
-			int size = MathHelper.floor(this.healthValue * 0.34F);
-
-			for (int a = 0; a < 2; a++) {
-				EntitySlimePart part = new EntitySlimePart(this.world, this.owner, size);
-				part.setPosition(this.posX, this.posY + 1.0D, this.posZ);
-				part.motionX = this.rand.nextGaussian();
-				part.motionZ = this.rand.nextGaussian();
-				this.world.spawnEntity(part);
-			}
-		}
-		this.isDead = true;
+	public void readEntityFromNBT(NBTTagCompound compound) {
+		super.readEntityFromNBT(compound);
+		this.ticksExisted = compound.getInteger("ticksExisted");
+		this.ownerUuid = NBTUtil.getUUIDFromTag(compound.getCompoundTag("ownerUuid"));
 	}
 
-	@Override
-	public Team getTeam() {
-		if (this.owner != null) {
-			return this.owner.getTeam();
-		}
-		return super.getTeam();
-	}
-
-	@Override
-	protected int getAttackStrength() {
-		return 0;
-	}
 }
