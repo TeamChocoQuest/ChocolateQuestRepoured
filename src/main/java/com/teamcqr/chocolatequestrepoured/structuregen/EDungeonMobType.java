63f4990a87fa315f85dfa331cd99003e1aa6550e
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-05-01 14:01:50 +0200

    update

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/EDungeonMobType.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/EDungeonMobType.java
@@ -10,6 +10,7 @@ import com.teamcqr.chocolatequestrepoured.util.CQRConfig;
 import com.teamcqr.chocolatequestrepoured.util.Reference;
 
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
@@ -19,22 +20,22 @@ public enum EDungeonMobType {
 
 	DEFAULT(null, null, null),
 	// DONT_REPLACE(null),
-	DWARF(new ResourceLocation(Reference.MODID, "dwarf"), null, null, new ItemStack(ModItems.SHIELD_CARL)),
-	SKELETON(new ResourceLocation(Reference.MODID, "skeleton"), new ResourceLocation(Reference.MODID, "necromancer"), EBanners.SKELETON_BANNER, new ItemStack(ModItems.SHIELD_SKELETON_FRIENDS)),
-	ZOMBIE(new ResourceLocation(Reference.MODID, "zombie"), new ResourceLocation(Reference.MODID, "lich"), null, new ItemStack(ModItems.SHIELD_ZOMBIE)),
-	PIRATE(new ResourceLocation(Reference.MODID, "pirate"), null, EBanners.PIRATE_BANNER, new ItemStack(ModItems.SHIELD_PIRATE2)),
+	DWARF(new ResourceLocation(Reference.MODID, "dwarf"), null, null, ModItems.SHIELD_CARL),
+	SKELETON(new ResourceLocation(Reference.MODID, "skeleton"), new ResourceLocation(Reference.MODID, "necromancer"), EBanners.SKELETON_BANNER, ModItems.SHIELD_SKELETON_FRIENDS),
+	ZOMBIE(new ResourceLocation(Reference.MODID, "zombie"), new ResourceLocation(Reference.MODID, "lich"), null, ModItems.SHIELD_ZOMBIE),
+	PIRATE(new ResourceLocation(Reference.MODID, "pirate"), null, EBanners.PIRATE_BANNER, ModItems.SHIELD_PIRATE2),
 	ILLAGER(new ResourceLocation(Reference.MODID, "illager"), null, EBanners.ILLAGER_BANNER),
-	WALKER(new ResourceLocation(Reference.MODID, "walker"), new ResourceLocation(Reference.MODID, "walker_king"), EBanners.WALKER_ORDO, new ItemStack(ModItems.SHIELD_WALKER)),
-	SPECTER(new ResourceLocation(Reference.MODID, "spectre"), null, null, new ItemStack(ModItems.SHIELD_SPECTER)),
+	WALKER(new ResourceLocation(Reference.MODID, "walker"), new ResourceLocation(Reference.MODID, "walker_king"), EBanners.WALKER_ORDO, ModItems.SHIELD_WALKER),
+	SPECTER(new ResourceLocation(Reference.MODID, "spectre"), null, null, ModItems.SHIELD_SPECTER),
 	ENDERMAN(new ResourceLocation(Reference.MODID, "enderman"), null, EBanners.ENDERMEN_BANNER),
-	BOARMAN(new ResourceLocation(Reference.MODID, "boarman"), new ResourceLocation(Reference.MODID, "boar_mage"), EBanners.PIGMAN_BANNER, new ItemStack(ModItems.SHIELD_FIRE)),
-	MINOTAUR(new ResourceLocation(Reference.MODID, "minotaur"), null, null, new ItemStack(ModItems.SHIELD_PIGMAN)),
-	ORC(new ResourceLocation(Reference.MODID, "orc"), null, null, new ItemStack(ModItems.SHIELD_WARPED)),
+	BOARMAN(new ResourceLocation(Reference.MODID, "boarman"), new ResourceLocation(Reference.MODID, "boar_mage"), EBanners.PIGMAN_BANNER, ModItems.SHIELD_FIRE),
+	MINOTAUR(new ResourceLocation(Reference.MODID, "minotaur"), null, null, ModItems.SHIELD_PIGMAN),
+	ORC(new ResourceLocation(Reference.MODID, "orc"), null, null, ModItems.SHIELD_WARPED),
 	GOLEM(new ResourceLocation(Reference.MODID, "golem"), null, null),
-	GOBLIN(new ResourceLocation(Reference.MODID, "goblin"), null, null, new ItemStack(ModItems.SHIELD_GOBLIN)),
-	MUMMY(new ResourceLocation(Reference.MODID, "mummy"), null, null, new ItemStack(ModItems.SHIELD_MUMMY)),
-	OGRE(new ResourceLocation(Reference.MODID, "ogre"), null, null, new ItemStack(ModItems.SHIELD_TOMB)),
-	GREMLIN(new ResourceLocation(Reference.MODID, "gremlin"), null, EBanners.GREMLIN_BANNER, new ItemStack(ModItems.SHIELD_RUSTED)),
+	GOBLIN(new ResourceLocation(Reference.MODID, "goblin"), null, null, ModItems.SHIELD_GOBLIN),
+	MUMMY(new ResourceLocation(Reference.MODID, "mummy"), null, null, ModItems.SHIELD_MUMMY),
+	OGRE(new ResourceLocation(Reference.MODID, "ogre"), null, null, ModItems.SHIELD_TOMB),
+	GREMLIN(new ResourceLocation(Reference.MODID, "gremlin"), null, EBanners.GREMLIN_BANNER, ModItems.SHIELD_RUSTED),
 	TRITON(new ResourceLocation(Reference.MODID, "triton"), null, null);
 
 	private static final Random RAND = new Random();
@@ -49,10 +50,10 @@ public enum EDungeonMobType {
 
 	private ResourceLocation resLoc;
 	private ResourceLocation bossResLoc;
-	private ItemStack shieldItem;
+	private Item shieldItem;
 	private EBanners banner;
 
-	private EDungeonMobType(ResourceLocation resLoc, ResourceLocation bossResLoc, EBanners banner, ItemStack shieldItem) {
+	private EDungeonMobType(ResourceLocation resLoc, ResourceLocation bossResLoc, EBanners banner, Item shieldItem) {
 		this.resLoc = resLoc;
 		this.banner = banner;
 		this.shieldItem = shieldItem;
@@ -62,7 +63,7 @@ public enum EDungeonMobType {
 	private EDungeonMobType(ResourceLocation resLoc, ResourceLocation bossResLoc, EBanners banner) {
 		this.resLoc = resLoc;
 		this.banner = banner;
-		this.shieldItem = new ItemStack(Items.SHIELD);
+		this.shieldItem = Items.SHIELD;
 		this.bossResLoc = bossResLoc;
 	}
 
@@ -79,7 +80,7 @@ public enum EDungeonMobType {
 	}
 	
 	public ItemStack getShieldItem() {
-		return this.shieldItem;
+		return new ItemStack(this.shieldItem, 1);
 	}
 
 	/**
