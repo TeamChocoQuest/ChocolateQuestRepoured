c11784c0b573f4352754b2f268698acdda1a3d13
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-05-02 04:42:14 +0200

    Added rainbow coloring

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/armor/ItemArmorDyable.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/armor/ItemArmorDyable.java
@@ -2,10 +2,12 @@ package com.teamcqr.chocolatequestrepoured.objects.items.armor;
 
 import com.teamcqr.chocolatequestrepoured.init.ModMaterials;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.Constants;
 
 public class ItemArmorDyable extends ItemArmor {
 
@@ -25,7 +27,7 @@ public class ItemArmorDyable extends ItemArmor {
 	public boolean hasColor(ItemStack stack) {
 		if (this.getArmorMaterial() == ModMaterials.ArmorMaterials.DIAMOND_DYABLE || this.getArmorMaterial() == ModMaterials.ArmorMaterials.IRON_DYABLE) {
 			NBTTagCompound nbttagcompound = stack.getTagCompound();
-			return nbttagcompound != null && nbttagcompound.hasKey("display", 10) ? nbttagcompound.getCompoundTag("display").hasKey("color", 3) : false;
+			return nbttagcompound != null && nbttagcompound.hasKey("display", Constants.NBT.TAG_COMPOUND) ? nbttagcompound.getCompoundTag("display").hasKey("color", Constants.NBT.TAG_INT) : false;
 		} else {
 			return super.hasColor(stack);
 		}
@@ -42,8 +44,41 @@ public class ItemArmorDyable extends ItemArmor {
 			if (nbttagcompound != null) {
 				NBTTagCompound nbttagcompound1 = nbttagcompound.getCompoundTag("display");
 
-				if (nbttagcompound1 != null && nbttagcompound1.hasKey("color", 3)) {
-					return nbttagcompound1.getInteger("color");
+				if (nbttagcompound1 != null && nbttagcompound1.hasKey("color", Constants.NBT.TAG_INT)) {
+					int color = nbttagcompound1.getInteger("color");
+					if ((color >> 24 & 15) > 0) {
+						Minecraft mc = Minecraft.getMinecraft();
+						if (mc.world != null) {
+							float j = 1530.0F / (color >> 24 & 15);
+							float k = j / 6.0F;
+							float time = mc.world.getTotalWorldTime() % j;
+							int r = 0;
+							int g = 0;
+							int b = 0;
+							int i = Math.round(time % k * 255.0F / k);
+							if (time < k) {
+								r = 255;
+								g = i;
+							} else if (time < k * 2) {
+								g = 255;
+								r = 255 - i;
+							} else if (time < k * 3) {
+								g = 255;
+								b = i;
+							} else if (time < k * 4) {
+								b = 255;
+								g = 255 - i;
+							} else if (time < k * 5) {
+								b = 255;
+								r = i;
+							} else {
+								r = 255;
+								b = 255 - i;
+							}
+							return r << 16 | g << 8 | b;
+						}
+					}
+					return color;
 				}
 			}
 
@@ -90,7 +125,7 @@ public class ItemArmorDyable extends ItemArmor {
 
 			NBTTagCompound nbttagcompound1 = nbttagcompound.getCompoundTag("display");
 
-			if (!nbttagcompound.hasKey("display", 10)) {
+			if (!nbttagcompound.hasKey("display", Constants.NBT.TAG_COMPOUND)) {
 				nbttagcompound.setTag("display", nbttagcompound1);
 			}
 
