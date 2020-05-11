ddbabbaa564710e70eaa4fa2af5226acf64c47e4
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-05-11 19:41:59 +0200

    tweak

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/boss/netherdragon/BossAIFlyToTarget.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/boss/netherdragon/BossAIFlyToTarget.java
@@ -8,7 +8,6 @@ public class BossAIFlyToTarget extends BossAIFlyToLocation {
 	
 	private int attackCooldown = 50;
 	private int aiCooldown = 100;
-	private boolean attackedMelee = false;
 
 	public BossAIFlyToTarget(EntityCQRNetherDragon entity) {
 		super(entity);
@@ -32,16 +31,14 @@ public class BossAIFlyToTarget extends BossAIFlyToLocation {
 	
 	@Override
 	public void updateTask() {
-		if(entity.getPositionVector().distanceTo(getTargetLocation()) <= 4 && !attackedMelee) {
+		if(entity.getPositionVector().distanceTo(getTargetLocation()) <= 4 ) {
 			entity.attackEntityAsMob(entity.getAttackTarget());
-			attackCooldown = 20;
-			attackedMelee = true;
+			resetTask();
 		}
 		super.updateTask();
 		attackCooldown--;
 		if(attackCooldown <= 0) {
 			attackCooldown = 30 + entity.getRNG().nextInt(41);
-			attackedMelee = false;
 			entity.attackEntityWithRangedAttack(entity.getAttackTarget(), entity.getDistance(entity.getAttackTarget()));
 		}
 	}
@@ -49,7 +46,6 @@ public class BossAIFlyToTarget extends BossAIFlyToLocation {
 	@Override
 	public void resetTask() {
 		super.resetTask();
-		attackedMelee = false;
 		this.aiCooldown = 300;
 		this.entity.setTargetLocation(new Vec3d(entity.getCirclingCenter().getX(), entity.getCirclingCenter().getY(), entity.getCirclingCenter().getZ()));
 	}
