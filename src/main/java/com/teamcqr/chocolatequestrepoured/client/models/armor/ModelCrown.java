252ad1de3d3fc8a5314fa6d736639d66fa52c31c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-05-16 11:53:28 +0200

    updated model

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/models/armor/ModelCrown.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/models/armor/ModelCrown.java
@@ -1,8 +1,12 @@
 package com.teamcqr.chocolatequestrepoured.client.models.armor;
 
+import com.teamcqr.chocolatequestrepoured.client.render.entity.layers.LayerCQREntityArmor;
+
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.inventory.EntityEquipmentSlot;
 
 /**
  * ModelCrown - DerToaster
@@ -23,30 +27,29 @@ public class ModelCrown extends ModelCustomArmorBase {
     	
     	this.crownBorderLeft = new ModelRenderer(this, 18, 16);
         this.crownBorderLeft.setRotationPoint(3.0F, 1.3F, 0.0F);
-        this.crownBorderLeft.addBox(0.0F, -10.0F, -3.0F, 1, 2, 6, 0.0F);
+        this.crownBorderLeft.addBox(0.0F, -9.0F, -3.0F, 1, 2, 6, 0.0F);
         this.crownJewelFrontBase = new ModelRenderer(this, 0, 0);
-        this.crownJewelFrontBase.setRotationPoint(0.0F, -9.5F, -0.5F);
-        this.crownJewelFrontBase.addBox(-1.0F, -1.0F, -1.0F, 2, 2, 1, 0.0F);
-        this.setRotateAngle(crownJewelFrontBase, 0.0F, 0.0F, 0.7853981633974483F);
+        this.crownJewelFrontBase.setRotationPoint(0.0F, -9.2F, -0.5F);
+        this.crownJewelFrontBase.addBox(-1.0F, 0.0F, -1.0F, 2, 2, 1, 0.0F);
         this.crownBorderRight = new ModelRenderer(this, 18, 24);
         this.crownBorderRight.setRotationPoint(-3.0F, 1.3F, 0.0F);
-        this.crownBorderRight.addBox(-1.0F, -10.0F, -3.0F, 1, 2, 6, 0.0F);
+        this.crownBorderRight.addBox(-1.0F, -9.0F, -3.0F, 1, 2, 6, 0.0F);
         this.crownUpper = new ModelRenderer(this, 0, 0);
         this.crownUpper.setRotationPoint(0.0F, -2.7F, 0.0F);
-        this.crownUpper.addBox(-4.0F, -8.0F, -4.0F, 8, 2, 8, 0.0F);
+        this.crownUpper.addBox(-4.0F, -7.0F, -4.0F, 8, 2, 8, 0.0F);
         this.crownJewelFront = new ModelRenderer(this, 0, 3);
         this.crownJewelFront.setRotationPoint(0.0F, 0.0F, -0.7F);
-        this.crownJewelFront.addBox(-0.5F, -0.5F, -0.5F, 1, 1, 1, 0.0F);
-        this.setRotateAngle(crownJewelFront, 0.0F, 0.0F, 0.7853981633974483F);
+        this.crownJewelFront.addBox(-0.5F, 0.5F, -0.5F, 1, 1, 1, 0.0F);
         this.crownBorderBack = new ModelRenderer(this, 0, 24);
         this.crownBorderBack.setRotationPoint(0.0F, 1.3F, 2.0F);
-        this.crownBorderBack.addBox(-4.0F, -10.0F, 1.0F, 8, 2, 1, 0.0F);
+        this.crownBorderBack.addBox(-4.0F, -9.0F, 1.0F, 8, 2, 1, 0.0F);
         this.bipedHead = new ModelRenderer(this, 0, 10);
         this.bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.bipedHead.addBox(-3.0F, -8.0F, -3.0F, 6, 1, 6, 0.0F);
+        this.bipedHead.addBox(-3.0F, -7.0F, -3.0F, 6, 1, 6, 0.0F);
         this.crownBorderFront = new ModelRenderer(this, 0, 29);
         this.crownBorderFront.setRotationPoint(0.0F, 1.3F, -3.0F);
-        this.crownBorderFront.addBox(-4.0F, -10.0F, -1.0F, 8, 2, 1, 0.0F);
+        this.crownBorderFront.addBox(-4.0F, -9.0F, -1.0F, 8, 2, 1, 0.0F);
+        
         this.bipedHead.addChild(this.crownBorderLeft);
         this.crownBorderFront.addChild(this.crownJewelFrontBase);
         this.bipedHead.addChild(this.crownBorderRight);
@@ -63,6 +66,14 @@ public class ModelCrown extends ModelCustomArmorBase {
     	GlStateManager.pushMatrix();
     	GlStateManager.scale(1.2, 1.2, 1.2);
     	super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+    	GlStateManager.popMatrix();
+    }
+    
+    @Override
+    public void render(Entity entityIn, float scale, LayerCQREntityArmor layer, ModelBiped model, EntityEquipmentSlot slot) {
+    	GlStateManager.pushMatrix();
+    	GlStateManager.scale(1.2, 1.2, 1.2);
+    	super.render(entityIn, scale, layer, model, slot);
     	GlStateManager.popMatrix();
     }
     
