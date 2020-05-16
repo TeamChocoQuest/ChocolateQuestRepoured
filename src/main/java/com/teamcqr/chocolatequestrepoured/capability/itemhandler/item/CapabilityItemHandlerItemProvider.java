b513208e50e80a54d70d874ba619f7d42eae8c19
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-05-16 22:47:59 +0200

    Updated capabilities

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/capability/itemhandler/item/CapabilityItemHandlerItemProvider.java
@@ -0,0 +1,20 @@
+package com.teamcqr.chocolatequestrepoured.capability.itemhandler.item;
+
+import com.teamcqr.chocolatequestrepoured.capability.CapabilityProviderCQR;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+
+public class CapabilityItemHandlerItemProvider extends CapabilityProviderCQR<IItemHandler> {
+
+	public CapabilityItemHandlerItemProvider(Capability<IItemHandler> capability, IItemHandler instance) {
+		super(capability, instance);
+	}
+
+	public static CapabilityItemHandlerItemProvider createProvider(ItemStack stack, int slots) {
+		return new CapabilityItemHandlerItemProvider(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, new CapabilityItemHandlerItem(stack, slots));
+	}
+
+}
--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/armor/ItemArmorTurtle.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/armor/ItemArmorTurtle.java
@@ -7,8 +7,7 @@ import javax.annotation.Nullable;
 import org.lwjgl.input.Keyboard;
 
 import com.google.common.collect.Multimap;
-import com.teamcqr.chocolatequestrepoured.capability.armor.CapabilitySpecialArmor;
-import com.teamcqr.chocolatequestrepoured.capability.armor.turtle.CapabilityTurtleArmorProvider;
+import com.teamcqr.chocolatequestrepoured.capability.armor.CapabilityCooldownHandlerHelper;
 import com.teamcqr.chocolatequestrepoured.client.init.ModArmorModels;
 import com.teamcqr.chocolatequestrepoured.init.ModItems;
 import com.teamcqr.chocolatequestrepoured.util.ItemUtil;
@@ -24,15 +23,21 @@ import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -64,9 +69,9 @@ public class ItemArmorTurtle extends ItemArmor {
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
 		EntityPlayer player = Minecraft.getMinecraft().player;
 		if (player != null) {
-			CapabilitySpecialArmor icapability = player.getCapability(CapabilityTurtleArmorProvider.CAPABILITY_TURTLE_ARMOR, null);
-			if (icapability != null && icapability.onCooldown()) {
-				tooltip.add(TextFormatting.RED + I18n.format("description.turtle_armor_charging.name") + this.convertCooldown(icapability.getCooldown()));
+			int cooldown = CapabilityCooldownHandlerHelper.getCooldown(player, ModItems.CHESTPLATE_TURTLE);
+			if (cooldown > 0) {
+				tooltip.add(TextFormatting.RED + I18n.format("description.turtle_armor_charging.name") + this.convertCooldown(cooldown));
 			}
 		}
 
@@ -100,29 +105,55 @@ public class ItemArmorTurtle extends ItemArmor {
 	public static class EventHandler {
 
 		@SubscribeEvent
-		public static void onLivingHurtEvent(LivingAttackEvent event) {
-			if (event.getEntityLiving().getItemStackFromSlot(EntityEquipmentSlot.CHEST).getItem() == ModItems.CHESTPLATE_TURTLE) {
-				if (event.getSource().getDamageLocation() != null) {
-					EntityLivingBase entity = event.getEntityLiving();
-					Vec3d hitVec = event.getSource().getDamageLocation();
-					double x = entity.posX - hitVec.x;
-					double z = entity.posZ - hitVec.z;
-					double yaw = Math.toDegrees(Math.atan2(-x, z));
+		public static void onLivingUpdateEvent(LivingUpdateEvent event) {
+			EntityLivingBase entity = event.getEntityLiving();
 
-					if (ItemUtil.compareRotations((double) entity.renderYawOffset, yaw, 50.0D)) {
-						double y = (entity.posY + (double) entity.height * 0.5D) - hitVec.y;
-						double d = Math.sqrt(x * x + z * z);
-						double pitch = -Math.toDegrees(Math.atan2(y, d));
+			if (ItemUtil.hasFullSet(entity, ItemArmorTurtle.class)) {
+				if (!entity.world.isRemote && entity.ticksExisted % 200 == 0) {
+					entity.heal(1.0F);
+					double x = entity.posX;
+					double y = entity.posY + entity.getEyeHeight();
+					double z = entity.posZ;
+					((WorldServer) entity.world).spawnParticle(EnumParticleTypes.HEART, x, y, z, 2, 0.5D, 0.5D, 0.5D, 1.0D);
+				}
 
-						if (ItemUtil.compareRotations(0.0D, pitch, 50.0D)) {
-							if (event.getSource().getImmediateSource() instanceof EntityArrow) {
-								event.getSource().getImmediateSource().setDead();
-							}
-							if (!entity.world.isRemote) {
-								entity.world.playSound(null, hitVec.x, hitVec.y, hitVec.z, SoundEvents.ITEM_SHIELD_BLOCK, SoundCategory.NEUTRAL, 0.5F, 0.8F);
-							}
-							event.setCanceled(true);
+				if (!CapabilityCooldownHandlerHelper.onCooldown(entity, ModItems.CHESTPLATE_TURTLE) && entity.getHealth() < Math.max(5.0F, entity.getMaxHealth() * 0.2F)) {
+					if (!entity.world.isRemote) {
+						entity.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 100, 2, false, true));
+						double x = entity.posX;
+						double y = entity.posY + entity.getEyeHeight();
+						double z = entity.posZ;
+						((WorldServer) entity.world).spawnParticle(EnumParticleTypes.HEART, x, y, z, 4, 0.5D, 0.5D, 0.5D, 1.0D);
+						entity.world.playSound(null, x, y, z, SoundEvents.ENTITY_GENERIC_DRINK, SoundCategory.PLAYERS, 0.6F, 1.2F);
+					}
+					CapabilityCooldownHandlerHelper.setCooldown(entity, ModItems.CHESTPLATE_TURTLE, 12000);
+				}
+			}
+		}
+
+		@SubscribeEvent
+		public static void onLivingHurtEvent(LivingHurtEvent event) {
+			EntityLivingBase entity = event.getEntityLiving();
+			DamageSource source = event.getSource();
+
+			if (entity.getItemStackFromSlot(EntityEquipmentSlot.CHEST).getItem() == ModItems.CHESTPLATE_TURTLE && source.getDamageLocation() != null) {
+				Vec3d hitVec = source.getDamageLocation();
+				double x = entity.posX - hitVec.x;
+				double z = entity.posZ - hitVec.z;
+				double yaw = Math.toDegrees(Math.atan2(-x, z));
+
+				if (ItemUtil.compareRotations((double) entity.renderYawOffset, yaw, 45.0D)) {
+					double y = (entity.posY + (double) entity.height * 0.5D) - hitVec.y;
+					double pitch = -Math.toDegrees(Math.asin(y));
+
+					if (ItemUtil.compareRotations(0.0D, pitch, 60.0D)) {
+						if (event.getSource().getImmediateSource() instanceof EntityArrow) {
+							event.getSource().getImmediateSource().setDead();
 						}
+						if (!entity.world.isRemote) {
+							entity.world.playSound(null, hitVec.x, hitVec.y, hitVec.z, SoundEvents.ITEM_SHIELD_BLOCK, SoundCategory.NEUTRAL, 0.5F, 0.8F);
+						}
+						event.setCanceled(true);
 					}
 				}
 			}
