2ebcee1ff2ad9d6a57d9f8a2b852570af156ccc8
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-06-01 18:26:56 +0200

    changed durations

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/boss/piratecaptain/BossAIPirateTeleportBehindEnemy.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/boss/piratecaptain/BossAIPirateTeleportBehindEnemy.java
@@ -31,7 +31,7 @@ public class BossAIPirateTeleportBehindEnemy extends AbstractCQREntityAI<EntityC
 	
 	@Override
 	public boolean shouldContinueExecuting() {
-		return timer < 45;
+		return timer < 160;
 	}
 	
 	@Override
@@ -42,7 +42,7 @@ public class BossAIPirateTeleportBehindEnemy extends AbstractCQREntityAI<EntityC
 			entity.setHeldItem(EnumHand.MAIN_HAND, new ItemStack(ModItems.DAGGER_NINJA, 1));
 		}
 		
-		if(timer == 40) {
+		if(timer == 140) {
 			Vec3d v = entity.getAttackTarget().getLookVec().normalize().scale(2);
 			Vec3d p = entity.getPositionVector().subtract(v).addVector(0,0.5,0);
 			entity.attemptTeleport(p.x, p.y, p.z);
