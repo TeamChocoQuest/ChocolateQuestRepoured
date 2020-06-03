90408df52c689b111271cdafaf5d7a1f6b0b15e4
Meldexun <micha.kotlowski@gmx.de>
2020-06-03 15:36:40 +0200

    Updated ocean floor dungeon and generator

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/dungeons/DungeonOceanFloor.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/dungeons/DungeonOceanFloor.java
@@ -1,18 +1,11 @@
 package com.teamcqr.chocolatequestrepoured.structuregen.dungeons;
 
-import java.io.File;
 import java.util.Properties;
 
-import com.teamcqr.chocolatequestrepoured.structuregen.generators.AbstractDungeonGenerator;
-import com.teamcqr.chocolatequestrepoured.structuregen.generators.GeneratorOceanFloor;
-import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.CQStructure;
 import com.teamcqr.chocolatequestrepoured.util.DungeonGenUtils;
 
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.gen.structure.template.PlacementSettings;
 
 /**
  * Copyright (c) 29.04.2019
@@ -23,6 +16,7 @@ public class DungeonOceanFloor extends DungeonSurface {
 
 	public DungeonOceanFloor(String name, Properties prop) {
 		super(name, prop);
+		this.useCoverBlock = false;
 	}
 
 	public void generate(World world, int x, int z) {
@@ -33,28 +27,10 @@ public class DungeonOceanFloor extends DungeonSurface {
 				y += DungeonGenUtils.getYForPos(world, chunk.x * 16 + ix, chunk.z * 16 + iz, true);
 			}
 		}
-		y /= 256;
+		y >>= 8;
 		y -= this.getUnderGroundOffset();
 		y += this.getYOffset();
 		this.generate(world, x, y, z);
-	}
-
-	@Override
-	public void generate(World world, int x, int y, int z) {
-		File file = this.getStructureFileFromDirectory(this.structureFolderPath);
-
-		if (file != null && file.exists() && file.isFile()) {
-			CQStructure structure = new CQStructure(file);
-			PlacementSettings settings = new PlacementSettings();
-
-			if (this.rotateDungeon()) {
-				settings.setRotation(Rotation.values()[this.random.nextInt(Rotation.values().length)]);
-				settings.setMirror(Mirror.values()[this.random.nextInt(Mirror.values().length)]);
-			}
-
-			AbstractDungeonGenerator generator = new GeneratorOceanFloor(this, structure, settings);
-			generator.generate(world, world.getChunkFromChunkCoords(x >> 4, z >> 4), x, y, z);
-		}
 	}
 
 }
