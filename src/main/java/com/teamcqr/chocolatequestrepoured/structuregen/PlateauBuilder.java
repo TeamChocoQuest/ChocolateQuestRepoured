dae7fbe88e674db225544b9d7a12d1530aeaa5a4
Meldexun <micha.kotlowski@gmx.de>
2020-06-03 15:40:39 +0200

    Updated Nether City

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/PlateauBuilder.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/PlateauBuilder.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.Random;
 
 import com.teamcqr.chocolatequestrepoured.CQRMain;
-import com.teamcqr.chocolatequestrepoured.structuregen.generation.RandomBlobPart;
-import com.teamcqr.chocolatequestrepoured.structuregen.generation.SupportHillPart;
+import com.teamcqr.chocolatequestrepoured.structuregen.generation.DungeonGenerator;
+import com.teamcqr.chocolatequestrepoured.structuregen.generation.DungeonPartBlock;
+import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.AbstractBlockInfo;
+import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.BlockInfo;
 import com.teamcqr.chocolatequestrepoured.util.CQRConfig;
 import com.teamcqr.chocolatequestrepoured.util.Perlin3D;
 import com.teamcqr.chocolatequestrepoured.util.VectorUtil;
@@ -17,6 +19,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.template.PlacementSettings;
 
 public class PlateauBuilder {
 
@@ -219,6 +222,49 @@ public class PlateauBuilder {
 				}
 			}
 		}
+	}
+	
+	public static DungeonPartBlock makeRandomBlob2(Block fillBlock, BlockPos startPos, BlockPos endPos, int wallSize, long seed, World world, DungeonGenerator dungeonGenerator) {
+		List<AbstractBlockInfo> blockInfoList = new ArrayList<>();
+		Perlin3D perlinNoise1 = new Perlin3D(seed, 8, new Random());
+		Perlin3D perlinNoise2 = new Perlin3D(seed, 32, new Random());
+
+		int sizeX = endPos.getX() - startPos.getX();
+		int sizeZ = endPos.getZ() - startPos.getZ();
+		int sizeY = endPos.getY() - startPos.getY();
+
+		sizeX *= 1.25;
+		sizeZ *= 1.25;
+		sizeY *= 1.35;
+
+		for (int iX = 0; iX < sizeX; ++iX) {
+			for (int iY = 0; iY < sizeY; ++iY) {
+				for (int iZ = 0; iZ < sizeZ; ++iZ) {
+
+					float noise = Math.max(0.0F, 2.0F - (float) (sizeY - iY) / 4.0F);
+					noise += Math.max(0.0F, (float) wallSize - (float) iX / 2.0F);
+					noise += Math.max(0.0F, (float) wallSize - (float) (sizeX - iX) / 2.0F);
+					noise += Math.max(0.0F, (float) wallSize - (float) iZ / 2.0F);
+					noise += Math.max(0.0F, (float) wallSize - (float) (sizeZ - iZ) / 2.0F);
+
+					if (noise >= 0.5F) {
+						double perlin1 = perlinNoise1.getNoiseAt(startPos.getX() + iX, startPos.getY() + iY, startPos.getZ() + iZ);
+
+						if (perlin1 * (double) noise >= 0.5D) {
+							double perlin2 = perlinNoise2.getNoiseAt(startPos.getX() + iX, startPos.getY() + iY, startPos.getZ() + iZ);
+
+							if (perlin1 * perlin2 * (double) noise >= 0.5D) {
+								continue;
+							}
+						}
+					}
+
+					blockInfoList.add(new BlockInfo(new BlockPos(iX, iY, iZ), fillBlock.getDefaultState(), null));
+				}
+			}
+		}
+
+		return new DungeonPartBlock(world, dungeonGenerator, startPos, blockInfoList, new PlacementSettings(), EDungeonMobType.DEFAULT);
 	}
 
 	public List<RandomBlobPart> makeRandomBlobList(Random random, Block fillBlock, BlockPos startPos, BlockPos endPos, int wallSize, long seed) {
--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/GeneratorGridCity.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/generators/GeneratorGridCity.java
@@ -1,39 +1,37 @@
 package com.teamcqr.chocolatequestrepoured.structuregen.generators;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
-import java.util.function.Consumer;
 
-import com.teamcqr.chocolatequestrepoured.structuregen.EPosType;
+import com.teamcqr.chocolatequestrepoured.structuregen.EDungeonMobType;
 import com.teamcqr.chocolatequestrepoured.structuregen.PlateauBuilder;
 import com.teamcqr.chocolatequestrepoured.structuregen.WorldDungeonGenerator;
 import com.teamcqr.chocolatequestrepoured.structuregen.dungeons.DungeonNetherCity;
-import com.teamcqr.chocolatequestrepoured.structuregen.dungeons.DungeonBase;
-import com.teamcqr.chocolatequestrepoured.structuregen.generation.ExtendedBlockStatePart;
-import com.teamcqr.chocolatequestrepoured.structuregen.generation.IStructure;
+import com.teamcqr.chocolatequestrepoured.structuregen.generation.DungeonPartBlock;
+import com.teamcqr.chocolatequestrepoured.structuregen.generation.DungeonPartEntity;
+import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.AbstractBlockInfo;
+import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.BlockInfo;
 import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.CQStructure;
 
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.structure.template.PlacementSettings;
 
-public class GeneratorGridCity implements AbstractDungeonGenerator {
+public class GeneratorGridCity extends AbstractDungeonGenerator<DungeonNetherCity> {
 
-	private DungeonNetherCity dungeon;
+	private int longestSide;
+	private int distanceBetweenBuildings;
 
 	// TODO: Dont make this a Set, sets are slow as they need to calculate the hash keys every time you add something to them...
-	private Set<BlockPos> gridPositions = new HashSet<>();
+	private BlockPos[][] gridPositions;
 	private Set<BlockPos> bridgeBuilderStartPositionsX = new HashSet<>();
 	private Set<BlockPos> bridgeBuilderStartPositionsZ = new HashSet<>();
 
@@ -41,7 +39,6 @@ public class GeneratorGridCity implements AbstractDungeonGenerator {
 	private Set<BlockPos> lavaBlocks = new HashSet<>();
 
 	private Map<BlockPos, Block> blockMap = new HashMap<>();
-	private List<List<? extends IStructure>> structureList = new LinkedList<>();
 
 	private int minX;
 	private int maxX;
@@ -50,23 +47,52 @@ public class GeneratorGridCity implements AbstractDungeonGenerator {
 
 	// private static int tunnelHeight = 3;
 
-	public GeneratorGridCity(DungeonNetherCity dungeon) {
-		this.dungeon = dungeon;
+	private CQStructure[][] structures;
+	private Map<File, CQStructure> cachedStructures = new HashMap<>();
+
+	public GeneratorGridCity(World world, BlockPos pos, DungeonNetherCity dungeon) {
+		super(world, pos, dungeon);
 	}
 
 	@Override
-	public void preProcess(World world, Chunk chunk, int x, int y, int z, List<List<? extends IStructure>> lists) {
+	public void preProcess() {
 		// Calculate the grid for the buildings
-		int rowsX = this.dungeon.getXRows();
-		int rowsZ = this.dungeon.getZRows();
+		int rowsX = this.dungeon.getXRows() >> 1;
+		int rowsZ = this.dungeon.getZRows() >> 1;
 
-		rowsX /= 2;
-		rowsZ /= 2;
+		this.structures = new CQStructure[(rowsX << 1) + 1][(rowsZ << 1) + 1];
+		this.gridPositions = new BlockPos[(rowsX << 1) + 1][(rowsZ << 1) + 1];
+		for (int iX = -rowsX; iX <= rowsX; iX++) {
+			for (int iZ = -rowsZ; iZ <= rowsZ; iZ++) {
+				File file;
+				if (this.dungeon.centralBuildingIsSpecial() && iX == 0 && iZ == 0) {
+					file = this.dungeon.getRandomCentralBuilding();
+				} else {
+					file = this.dungeon.getRandomBuilding();
+				}
+				CQStructure structure;
+				if (this.cachedStructures.containsKey(file)) {
+					structure = this.cachedStructures.get(file);
+				} else {
+					structure = CQStructure.createFromFile(file);
+					this.cachedStructures.put(file, structure);
+				}
+				this.structures[iX + rowsX][iZ + rowsZ] = structure;
 
-		this.minX = x - (rowsX * this.dungeon.getDistanceBetweenBuildingCenters());
-		this.minZ = z - (rowsZ * this.dungeon.getDistanceBetweenBuildingCenters());
-		this.maxX = x + (rowsX * this.dungeon.getDistanceBetweenBuildingCenters());
-		this.maxZ = z + (rowsZ * this.dungeon.getDistanceBetweenBuildingCenters());
+				if (structure != null) {
+					int k = Math.max(structure.getSize().getX(), structure.getSize().getZ());
+					if (k > this.longestSide) {
+						this.longestSide = k;
+					}
+				}
+			}
+		}
+		this.distanceBetweenBuildings = (int) (this.longestSide * this.dungeon.getBridgeSizeMultiplier());
+
+		this.minX = this.pos.getX() - (rowsX * this.distanceBetweenBuildings);
+		this.minZ = this.pos.getZ() - (rowsZ * this.distanceBetweenBuildings);
+		this.maxX = this.pos.getX() + (rowsX * this.distanceBetweenBuildings);
+		this.maxZ = this.pos.getZ() + (rowsZ * this.distanceBetweenBuildings);
 
 		// This constructs the grid around the center
 		for (int iX = -rowsX; iX <= rowsX; iX++) {
@@ -77,9 +103,9 @@ public class GeneratorGridCity implements AbstractDungeonGenerator {
 					noAddFlag = true;
 				}
 
-				BlockPos p = new BlockPos(x + (iX * this.dungeon.getDistanceBetweenBuildingCenters()), y, z + (iZ * this.dungeon.getDistanceBetweenBuildingCenters()));
+				BlockPos p = this.pos.add((iX * this.distanceBetweenBuildings), 0, (iZ * this.distanceBetweenBuildings));
 				if (!noAddFlag) {
-					this.gridPositions.add(p);
+					this.gridPositions[iX + rowsX][iZ + rowsZ] = p;
 				}
 				// Bridge starter positions, in total there will be rowsX +rowsZ -1 bridges
 				if (iX == 0) {
@@ -93,7 +119,7 @@ public class GeneratorGridCity implements AbstractDungeonGenerator {
 	}
 
 	@Override
-	public void buildStructure(World world, Chunk chunk, int x, int y, int z, List<List<? extends IStructure>> lists) {
+	public void buildStructure() {
 		// Dig out the big air pocket or the small ones
 		if (this.dungeon.makeSpaceForBuildings()) {
 			/*
@@ -107,19 +133,15 @@ public class GeneratorGridCity implements AbstractDungeonGenerator {
 			 * }
 			 * } else {
 			 */
-			BlockPos cLower = new BlockPos(this.minX, y + 1, this.minZ).add(-this.dungeon.getDistanceBetweenBuildingCenters(), 0, -this.dungeon.getDistanceBetweenBuildingCenters());
+			BlockPos cLower = new BlockPos(this.minX, this.pos.getY() + 1, this.minZ).add(-this.distanceBetweenBuildings, 0, -this.distanceBetweenBuildings);
+			BlockPos cUpper = new BlockPos(this.maxX, this.pos.getY() + this.dungeon.getCaveHeight(), this.maxZ).add(this.distanceBetweenBuildings * 0.1, 0, this.distanceBetweenBuildings * 0.05);
 
-			BlockPos cUpper = new BlockPos(this.maxX, y + this.dungeon.getCaveHeight(), this.maxZ).add(this.dungeon.getDistanceBetweenBuildingCenters() * 0.1, 0, this.dungeon.getDistanceBetweenBuildingCenters() * 0.05);
+			this.dungeonGenerator.add(PlateauBuilder.makeRandomBlob2(Blocks.AIR, cLower, cUpper, 4, WorldDungeonGenerator.getSeed(this.world, this.minX, this.maxZ), this.world, this.dungeonGenerator));
 
-			PlateauBuilder pB = new PlateauBuilder();
-			// pB.makeRandomBlob(new Random(), this.dungeon.getAirPocketBlock(), cLower, cUpper, WorldDungeonGenerator.getSeed(world, this.minX, this.maxZ), world);
-			lists.add(pB.makeRandomBlobList(new Random(), this.dungeon.getAirPocketBlock(), cLower, cUpper, 4, WorldDungeonGenerator.getSeed(world, this.minX, this.maxZ)));
-			// }
 		}
 
 		// Build the roads / bridges and the floors
-		for (BlockPos lavaPos : BlockPos.getAllInBox(this.minX - this.dungeon.getDistanceBetweenBuildingCenters(), y, this.minZ - this.dungeon.getDistanceBetweenBuildingCenters(),
-				this.maxX + this.dungeon.getDistanceBetweenBuildingCenters(), y, this.maxZ + this.dungeon.getDistanceBetweenBuildingCenters())) {
+		for (BlockPos lavaPos : BlockPos.getAllInBox(this.minX - this.distanceBetweenBuildings, this.pos.getY(), this.minZ - this.distanceBetweenBuildings, this.maxX + this.distanceBetweenBuildings, this.pos.getY(), this.maxZ + this.distanceBetweenBuildings)) {
 			this.lavaBlocks.add(lavaPos);
 		}
 		// Bridges from south to north
@@ -168,125 +190,42 @@ public class GeneratorGridCity implements AbstractDungeonGenerator {
 		}
 
 		for (BlockPos p : this.lavaBlocks) {
-			// world.setBlockState(p, this.dungeon.getFloorBlock().getDefaultState());
 			this.blockMap.put(p, this.dungeon.getFloorBlock());
 		}
 		for (BlockPos p : this.bridgeBlocks) {
-			// world.setBlockState(p, this.dungeon.getBridgeBlock().getDefaultState());
 			this.blockMap.put(p, this.dungeon.getBridgeBlock());
 		}
 	}
 
 	@Override
-	public void postProcess(World world, Chunk chunk, int x, int y, int z, List<List<? extends IStructure>> lists) {
-		// Place the buildings
-		// Random rdm = new Random();
+	public void postProcess() {
+		Map<BlockPos, CQStructure> structureMap = new HashMap<>();
+		for (int iX = 0; iX < this.gridPositions.length && iX < this.structures.length; iX++) {
+			for (int iZ = 0; iZ < this.gridPositions[iX].length && iZ < this.structures[iX].length; iZ++) {
+				BlockPos structurePos = this.gridPositions[iX][iZ];
+				CQStructure structure = this.structures[iX][iZ];
 
-		PlacementSettings settings = new PlacementSettings();
-		settings.setMirror(Mirror.NONE);
-		settings.setRotation(Rotation.NONE);
-		settings.setReplacedBlock(Blocks.STRUCTURE_VOID);
-		settings.setIntegrity(1.0F);
+				if (structurePos != null && structure != null) {
+					BlockPos cL = structurePos.subtract(new Vec3i(structure.getSize().getX() / 2 + 2, 0, structure.getSize().getZ() / 2 + 2));
+					BlockPos cU = structurePos.add(structure.getSize().getX() / 2 + 2, 0, structure.getSize().getZ() / 2 + 2);
+					BlockPos.getAllInBox(cL, cU).forEach(p -> this.blockMap.put(p, this.dungeon.getBridgeBlock()));
 
-		CQStructure centralStructure = null;
-		if (this.dungeon.centralBuildingIsSpecial()) {
-			// DONE: Choose a central building, figure out the size, then build the platform and then the building
-
-			/*
-			 * if(dungeon.getCentralBuildingFolder().exists() && dungeon.getCentralBuildingFolder().isDirectory() && dungeon.getCentralBuildingFolder().listFiles().length > 0) {
-			 * centralStructure = new CQStructure(dungeon.getCentralBuildingFolder().listFiles()[rdm.nextInt(dungeon.getCentralBuildingFolder().listFiles().length)], dungeon, chunk.x, chunk.z, dungeon.isProtectedFromModifications());
-			 * } else if(dungeon.getCentralBuildingFolder().exists() && dungeon.getCentralBuildingFolder().isFile()) {
-			 * centralStructure = new CQStructure(dungeon.getCentralBuildingFolder(), dungeon, chunk.x, chunk.z, dungeon.isProtectedFromModifications());
-			 * }
-			 */
-			if (this.dungeon.getRandomCentralBuilding() != null) {
-				centralStructure = new CQStructure(this.dungeon.getRandomCentralBuilding());
-			}
-
-			if (centralStructure != null) {
-				BlockPos cL = new BlockPos(x - (centralStructure.getSize().getX() / 2 + 2), y, z - (centralStructure.getSize().getZ() / 2 + 2));
-				BlockPos cU = cL.add(centralStructure.getSize().getX() + 4, 0, centralStructure.getSize().getZ() + 4);
-				BlockPos.getAllInBox(cL, cU).forEach(new Consumer<BlockPos>() {
-
-					@Override
-					public void accept(BlockPos t) {
-						// world.setBlockState(t, NetherCityGenerator.this.dungeon.getBridgeBlock().getDefaultState());
-						GeneratorGridCity.this.blockMap.put(t, GeneratorGridCity.this.dungeon.getBridgeBlock());
-					}
-				});
-
-				this.structureList.addAll(centralStructure.addBlocksToWorld(world, new BlockPos(x, y + 1, z), settings, EPosType.CENTER_XZ_LAYER, this.dungeon, chunk.x, chunk.z));
-			}
-		}
-		CQStructure structure = null;
-		// int filesInFolder = dungeon.getBuildingFolder().exists() && dungeon.getBuildingFolder().isDirectory() ? dungeon.getBuildingFolder().listFiles().length : -1;
-		// List<String> bosses = new ArrayList<>();
-		for (BlockPos centerPos : this.gridPositions) {
-			// DONE: Choose a building, figure out the size, then build the platform and then the building
-
-			/*
-			 * if(dungeon.getBuildingFolder().exists() && dungeon.getBuildingFolder().isDirectory() && filesInFolder > 1) {
-			 * structure = new CQStructure(dungeon.getBuildingFolder().listFiles()[rdm.nextInt(filesInFolder)], dungeon, chunk.x, chunk.z, dungeon.isProtectedFromModifications());
-			 * } else if(dungeon.getBuildingFolder().exists() && dungeon.getBuildingFolder().isFile()) {
-			 * structure = new CQStructure(dungeon.getBuildingFolder(), dungeon, chunk.x, chunk.z, dungeon.isProtectedFromModifications());
-			 * }
-			 */
-			if (this.dungeon.getRandomBuilding() != null) {
-				structure = new CQStructure(this.dungeon.getRandomBuilding());
-			}
-
-			if (structure != null) {
-				BlockPos cL = centerPos.subtract(new Vec3i(structure.getSize().getX() / 2 + 2, 0, structure.getSize().getZ() / 2 + 2));
-				BlockPos cU = centerPos.add(structure.getSize().getX() / 2 + 2, 0, structure.getSize().getZ() / 2 + 2);
-				BlockPos.getAllInBox(cL, cU).forEach(new Consumer<BlockPos>() {
-
-					@Override
-					public void accept(BlockPos t) {
-						// world.setBlockState(t, NetherCityGenerator.this.dungeon.getBridgeBlock().getDefaultState());
-						GeneratorGridCity.this.blockMap.put(t, GeneratorGridCity.this.dungeon.getBridgeBlock());
-					}
-				});
-				/*
-				 * for(UUID id : structure.getBossIDs()) {
-				 * bosses.add(id.toString());
-				 * }
-				 */
-				this.structureList.addAll(structure.addBlocksToWorld(world, centerPos.add(0, 1, 0), settings, EPosType.CENTER_XZ_LAYER, this.dungeon, chunk.x, chunk.z));
+					structureMap.put(structurePos.up(), structure);
+				}
 			}
 		}
 
-		lists.add(ExtendedBlockStatePart.splitBlockMap(this.blockMap));
-		lists.addAll(this.structureList);
-		/*
-		 * BlockPos posLower = new BlockPos(this.minX - this.dungeon.getDistanceBetweenBuildingCenters(), y, this.minZ - this.dungeon.getDistanceBetweenBuildingCenters());
-		 * BlockPos posUpper = new BlockPos(this.maxX + this.dungeon.getDistanceBetweenBuildingCenters(), y + this.dungeon.getCaveHeight(), this.maxZ + this.dungeon.getDistanceBetweenBuildingCenters());
-		 *
-		 * 
-		 * CQDungeonStructureGenerateEvent event = new CQDungeonStructureGenerateEvent(this.dungeon, posLower, posUpper.subtract(posLower), world, bosses);
-		 * if (centralStructure != null) {
-		 * event.setShieldCorePosition(centralStructure.getShieldCorePosition());
-		 * }
-		 * MinecraftForge.EVENT_BUS.post(event);
-		 */
-	}
+		List<AbstractBlockInfo> blockInfoList = new ArrayList<>(this.blockMap.size());
+		for (Map.Entry<BlockPos, Block> entry : this.blockMap.entrySet()) {
+			blockInfoList.add(new BlockInfo(entry.getKey().subtract(this.pos), entry.getValue().getDefaultState(), null));
+		}
+		this.dungeonGenerator.add(new DungeonPartBlock(world, dungeonGenerator, this.pos, blockInfoList, new PlacementSettings(), EDungeonMobType.DEFAULT));
 
-	@Override
-	public void fillChests(World world, Chunk chunk, int x, int y, int z, List<List<? extends IStructure>> lists) {
-		// UNUSED HERE
-	}
-
-	@Override
-	public void placeSpawners(World world, Chunk chunk, int x, int y, int z, List<List<? extends IStructure>> lists) {
-	}
-
-	@Override
-	public void placeCoverBlocks(World world, Chunk chunk, int x, int y, int z, List<List<? extends IStructure>> lists) {
-		// UNUSED HERE, it would place blocks above the nether
-	}
-
-	@Override
-	public DungeonBase getDungeon() {
-		return this.dungeon;
+		for (Map.Entry<BlockPos, CQStructure> entry : structureMap.entrySet()) {
+			this.dungeonGenerator.add(new DungeonPartBlock(this.world, this.dungeonGenerator, entry.getKey(), entry.getValue().getBlockInfoList(), new PlacementSettings(), EDungeonMobType.DEFAULT));
+			this.dungeonGenerator.add(new DungeonPartBlock(this.world, this.dungeonGenerator, entry.getKey(), entry.getValue().getSpecialBlockInfoList(), new PlacementSettings(), EDungeonMobType.DEFAULT));
+			this.dungeonGenerator.add(new DungeonPartEntity(this.world, this.dungeonGenerator, entry.getKey(), entry.getValue().getEntityInfoList(), new PlacementSettings(), EDungeonMobType.DEFAULT));
+		}
 	}
 
 }
