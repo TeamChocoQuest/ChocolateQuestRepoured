941e698edfc27947e24e313c10fc9a9a66529b81
Meldexun <micha.kotlowski@gmx.de>
2020-06-05 13:50:06 +0200

    Added structure exported chat message

--- src/main/java/com/teamcqr/chocolatequestrepoured/network/packets/handlers/SaveStructureRequestPacketHandler.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/network/packets/handlers/SaveStructureRequestPacketHandler.java
@@ -7,6 +7,7 @@ import com.teamcqr.chocolatequestrepoured.network.packets.toServer.SaveStructure
 import com.teamcqr.chocolatequestrepoured.structuregen.structurefile.CQStructure;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
@@ -22,7 +23,10 @@ public class SaveStructureRequestPacketHandler implements IMessageHandler<SaveSt
 				EntityPlayer player = CQRMain.proxy.getPlayer(ctx);
 				World world = CQRMain.proxy.getWorld(ctx);
 				CQStructure structure = CQStructure.createFromWorld(world, message.getStartPos(), message.getEndPos(), message.ignoreEntities(), player.getName());
-				new Thread(() -> structure.writeToFile(new File(CQRMain.CQ_EXPORT_FILES_FOLDER, message.getName() + ".nbt"))).start();
+				new Thread(() -> {
+					structure.writeToFile(new File(CQRMain.CQ_EXPORT_FILES_FOLDER, message.getName() + ".nbt"));
+					player.sendMessage(new TextComponentString("Successfully exported structure: " + message.getName()));
+				}).start();
 			}
 		});
 		return null;
