177bb6ade61db662254ca8d33a5e568940bc9251
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-06-14 10:51:32 +0200

    better boss placeholder

--- src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/structurefile/BlockInfoBoss.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/structuregen/structurefile/BlockInfoBoss.java
@@ -1,6 +1,7 @@
 package com.teamcqr.chocolatequestrepoured.structuregen.structurefile;
 
 import com.teamcqr.chocolatequestrepoured.CQRMain;
+import com.teamcqr.chocolatequestrepoured.init.ModItems;
 import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQRBoss;
 import com.teamcqr.chocolatequestrepoured.structuregen.inhabitants.DungeonInhabitant;
 import com.teamcqr.chocolatequestrepoured.structuregen.inhabitants.DungeonInhabitantManager;
@@ -12,8 +13,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.IEntityLivingData;
-import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.init.Blocks;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagIntArray;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -58,16 +61,37 @@ public class BlockInfoBoss extends AbstractBlockInfo {
 					protectedRegion.addEntityDependency(entity.getPersistentID());
 				}
 			} else {
-				EntityArmorStand indicator = new EntityArmorStand(world);
+				/*EntityArmorStand indicator = new EntityArmorStand(world);
 				indicator.setCustomNameTag("Oops! We haven't added this boss yet! Treat yourself to some free loot!");
 				indicator.setPosition(transformedPos.getX() + 0.5D, transformedPos.getY(), transformedPos.getZ() + 0.5D);
 				indicator.setEntityInvulnerable(true);
 				indicator.setInvisible(true);
 				indicator.setAlwaysRenderNameTag(true);
 				indicator.setSilent(true);
-				indicator.setNoGravity(true);
+				indicator.setNoGravity(true);*/
+				Entity indicator = EntityList.createEntityByIDFromName(inha.getEntityID(), world);
+				if(indicator instanceof EntityLiving) {
+					((EntityLiving)indicator).getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(1024);
+					((EntityLiving) indicator).setHealth(((EntityLiving) indicator).getMaxHealth());
+					
+					//Some gear
+					((EntityLiving)indicator).setItemStackToSlot(EntityEquipmentSlot.MAINHAND, new ItemStack(ModItems.GREAT_SWORD_DIAMOND, 1));
+					((EntityLiving)indicator).setItemStackToSlot(EntityEquipmentSlot.OFFHAND, new ItemStack(inha.getShieldReplacement(), 1));
+					
+					((EntityLiving)indicator).setItemStackToSlot(EntityEquipmentSlot.HEAD, new ItemStack(ModItems.HELMET_HEAVY_DIAMOND, 1));
+					((EntityLiving)indicator).setItemStackToSlot(EntityEquipmentSlot.CHEST, new ItemStack(ModItems.CHESTPLATE_HEAVY_DIAMOND, 1));
+					((EntityLiving)indicator).setItemStackToSlot(EntityEquipmentSlot.LEGS, new ItemStack(ModItems.LEGGINGS_HEAVY_DIAMOND, 1));
+					((EntityLiving)indicator).setItemStackToSlot(EntityEquipmentSlot.FEET, new ItemStack(ModItems.BOOTS_HEAVY_DIAMOND, 1));
+					
+					indicator.setPosition(transformedPos.getX() + 0.5D, transformedPos.getY(), transformedPos.getZ() + 0.5D);
+					((EntityLiving) indicator).enablePersistence();
+					indicator.setCustomNameTag("Temporary Boss");
+				}
 
 				world.spawnEntity(indicator);
+				if (protectedRegion != null) {
+					protectedRegion.addEntityDependency(indicator.getPersistentID());
+				}
 			}
 		} else {
 			CQRMain.logger.warn("Failed to place boss at {}", transformedPos);
