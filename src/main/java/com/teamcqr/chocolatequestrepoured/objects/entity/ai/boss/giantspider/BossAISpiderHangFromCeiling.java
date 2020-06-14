1e0987688d55eecad86e3b32c99ff78741675c08
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-06-14 13:17:30 +0200

    began work

---
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/boss/giantspider/BossAISpiderHangFromCeiling.java
@@ -0,0 +1,9 @@
+package com.teamcqr.chocolatequestrepoured.objects.entity.ai.boss.giantspider;
+
+public class BossAISpiderHangFromCeiling {
+
+	public BossAISpiderHangFromCeiling() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
