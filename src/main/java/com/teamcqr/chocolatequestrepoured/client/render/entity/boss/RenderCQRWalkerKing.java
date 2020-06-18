3982e4a2941010bd30e0457b96b4cbcaf95c506e
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-06-18 13:15:57 +0200

    Fixed walker king rendering

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/boss/RenderCQRWalkerKing.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/boss/RenderCQRWalkerKing.java
@@ -12,33 +12,32 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 
 public class RenderCQRWalkerKing extends RenderCQREntity<EntityCQRWalkerKing> {
-	
+
 	private static final ResourceLocation WALKER_KING_EXPLODING = new ResourceLocation(Reference.MODID, "textures/entity/boss/walker_king_exploding.png");
 	private static final ResourceLocation TEXTURES = new ResourceLocation(Reference.MODID, "textures/entity/boss/walker_king.png");
-	
+
 	public RenderCQRWalkerKing(RenderManager rendermanagerIn, ModelBase model, String entityName) {
 		super(rendermanagerIn, model, 0.5F, entityName, 1D, 1D);
-		
-		this.addLayer(new LayerBossDeath(191,0,255));
+
+		this.addLayer(new LayerBossDeath(191, 0, 255));
 	}
-	
+
 	@Override
 	protected void renderModel(EntityCQRWalkerKing entitylivingbaseIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor) {
-		if (entitylivingbaseIn.deathTicks > 0)
-        {
-            float f = (float)entitylivingbaseIn.deathTicks / AbstractEntityCQRBoss.MAX_DEATH_TICKS;
-            GlStateManager.depthFunc(515);
-            GlStateManager.enableAlpha();
-            GlStateManager.alphaFunc(516, f);
-            this.bindTexture(WALKER_KING_EXPLODING);
-            this.mainModel.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
-            GlStateManager.alphaFunc(516, 0.1F);
-            GlStateManager.depthFunc(514);
-        }
-		this.bindTexture(getEntityTexture(entitylivingbaseIn));
+		if (entitylivingbaseIn.deathTicks > 0) {
+			float f = (float) entitylivingbaseIn.deathTicks / AbstractEntityCQRBoss.MAX_DEATH_TICKS;
+			GlStateManager.depthFunc(515);
+			GlStateManager.enableAlpha();
+			GlStateManager.alphaFunc(516, f);
+			this.bindTexture(WALKER_KING_EXPLODING);
+			this.mainModel.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+			GlStateManager.alphaFunc(516, 0.1F);
+			GlStateManager.depthFunc(514);
+		}
 		super.renderModel(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+		GlStateManager.depthFunc(515);
 	}
-	
+
 	@Override
 	protected ResourceLocation getEntityTexture(EntityCQRWalkerKing entity) {
 		return TEXTURES;
