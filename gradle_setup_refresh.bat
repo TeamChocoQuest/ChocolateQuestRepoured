87e85174b63452c30d7fa9695460361cb834dec5
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-06-26 18:30:08 +0200

    Updated gradle files

--- gradle_setup_refresh.bat
+++
@@ -1,4 +0,0 @@
-::Gradle forge updated
-@echo off
-echo Updating forge version....
-gradlew cleanCache && gradlew eclipse && gradlew genEclipseRuns && pause
--- gradle_setup_refresh_no_clean.bat
+++
@@ -1,4 +0,0 @@
-::Gradle forge updated
-@echo off
-echo Updating forge version....
-gradlew && gradlew eclipse && gradlew genEclipseRuns && pause
