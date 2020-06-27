ba2681f08576aab55496e97a5d596d6155fc8639
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-06-27 14:47:21 +0200

    Update reflection util

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/reflection/ReflectionMethod.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/reflection/ReflectionMethod.java
@@ -9,7 +9,7 @@ public class ReflectionMethod<C, T> {
 
 	private final Method method;
 
-	public ReflectionMethod(Class<? extends C> clazz, String obfuscatedName, String deobfuscatedName, Class<?>... parameterTypes) {
+	public ReflectionMethod(Class<C> clazz, String obfuscatedName, String deobfuscatedName, Class<?>... parameterTypes) {
 		Method m = null;
 		try {
 			try {
@@ -25,10 +25,27 @@ public class ReflectionMethod<C, T> {
 		this.method = m;
 	}
 
+	public ReflectionMethod(String className, String obfuscatedName, String deobfuscatedName, Class<?>... parameterTypes) {
+		Method m = null;
+		try {
+			Class<C> clazz = (Class<C>) Class.forName(className);
+			try {
+				m = clazz.getDeclaredMethod(obfuscatedName, parameterTypes);
+				m.setAccessible(true);
+			} catch (NoSuchMethodException e) {
+				m = clazz.getDeclaredMethod(deobfuscatedName, parameterTypes);
+				m.setAccessible(true);
+			}
+		} catch (ClassNotFoundException | ClassCastException | NoSuchMethodException | SecurityException e) {
+			CQRMain.logger.error("Failed to get method from class " + obfuscatedName + " for name " + deobfuscatedName, e);
+		}
+		this.method = m;
+	}
+
 	public T invoke(C obj, Object... args) {
 		try {
 			return (T) this.method.invoke(obj, args);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | ClassCastException e) {
 			CQRMain.logger.error("Failed to invoke method " + this.method.getName() + " for object " + obj + " with parameters " + args, e);
 		}
 		return null;
