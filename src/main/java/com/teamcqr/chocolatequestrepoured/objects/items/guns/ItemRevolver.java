440077547eb24adda62922ebe0ec984047e3dca8
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-07-09 17:06:32 +0200

    ...

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/guns/ItemRevolver.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/items/guns/ItemRevolver.java
@@ -18,7 +18,6 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
