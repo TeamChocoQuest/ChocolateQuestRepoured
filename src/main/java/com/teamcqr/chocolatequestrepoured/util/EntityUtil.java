aeef326c5b8367ee3b61a1e8b01099870c8b62ee
Meldexun <micha.kotlowski@gmx.de>
2020-07-10 17:34:22 +0200

    Updated isEntityFlying
    (cherry picked from commit 65dfa643f469465216d03cb597ec7ca99ea3e5fe)

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/EntityUtil.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/EntityUtil.java
@@ -1,6 +1,8 @@
 package com.teamcqr.chocolatequestrepoured.util;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
 
 public class EntityUtil {
 
@@ -16,12 +18,70 @@ public class EntityUtil {
 			strafe *= d;
 			forward *= d;
 
-			double d1 = Math.sin(yaw * 0.017453292D);
-			double d2 = Math.cos(yaw * 0.017453292D);
+			double d1 = Math.sin(Math.toRadians(yaw));
+			double d2 = Math.cos(Math.toRadians(yaw));
 
 			entity.motionX += strafe * d2 - forward * d1;
 			entity.motionZ += forward * d2 + strafe * d1;
 		}
+	}
+
+	public static void move3D(Entity entity, double strafe, double up, double forward, double speed, double yaw, double pitch) {
+		double d = strafe * strafe + up * up + forward * forward;
+		if (d >= 1.0E-4D) {
+			d = Math.sqrt(d);
+			if (d < 1.0D) {
+				d = 1.0D;
+			}
+			d = speed / d;
+
+			strafe *= d;
+			up *= d;
+			forward *= d;
+
+			double d1 = Math.sin(Math.toRadians(yaw));
+			double d2 = Math.cos(Math.toRadians(yaw));
+			double d3 = Math.sin(Math.toRadians(pitch));
+			double d4 = Math.cos(Math.toRadians(pitch));
+
+			entity.motionX += strafe * d2 - forward * d1 * d4;
+			entity.motionY += up - forward * d3;
+			entity.motionZ += forward * d2 * d4 + strafe * d1;
+		}
+	}
+
+	public static boolean isEntityFlying(Entity entity) {
+		if (entity.onGround) {
+			return false;
+		}
+		if (entity.collided) {
+			return false;
+		}
+		if (entity.motionY < -0.1D) {
+			return false;
+		}
+		BlockPos pos = new BlockPos(entity);
+		int y = 0;
+		int count = 0;
+		BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				mutablePos.setPos(pos.getX() + i, pos.getY(), pos.getZ() + j);
+				if (!entity.getEntityWorld().isBlockLoaded(mutablePos)) {
+					continue;
+				}
+				while (mutablePos.getY() > 0 && entity.getEntityWorld().getBlockState(mutablePos).getCollisionBoundingBox(entity.getEntityWorld(), mutablePos) == Block.NULL_AABB) {
+					mutablePos.setY(mutablePos.getY() - 1);
+				}
+				y += mutablePos.getY() + 1;
+				count++;
+			}
+		}
+		y = count > 0 ? y / count : (int) entity.posY;
+		if (entity.posY < y + 8) {
+			return false;
+		}
+		return !entity.getEntityWorld().checkBlockCollision(entity.getEntityBoundingBox());
 	}
 
 }
