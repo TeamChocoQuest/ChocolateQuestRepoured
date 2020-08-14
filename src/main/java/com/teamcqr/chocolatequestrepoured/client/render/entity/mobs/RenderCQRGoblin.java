07e801dab97cec486e96ace18cdd93f38ba0a87d
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-08-14 13:59:47 +0200

    Updated goblin armor rendering

--- src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/mobs/RenderCQRGoblin.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/client/render/entity/mobs/RenderCQRGoblin.java
@@ -4,8 +4,10 @@ import com.teamcqr.chocolatequestrepoured.client.models.entities.mobs.ModelCQRGo
 import com.teamcqr.chocolatequestrepoured.client.render.entity.RenderCQREntity;
 import com.teamcqr.chocolatequestrepoured.objects.entity.mobs.EntityCQRGoblin;
 
+import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.inventory.EntityEquipmentSlot;
 
 public class RenderCQRGoblin extends RenderCQREntity<EntityCQRGoblin> {
 
@@ -20,5 +22,97 @@ public class RenderCQRGoblin extends RenderCQREntity<EntityCQRGoblin> {
 		}
 		super.renderModel(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
 	}
-	
+
+	@Override
+	public void setupHeadOffsets(ModelRenderer modelRenderer, EntityEquipmentSlot slot) {
+		this.applyTranslations(modelRenderer);
+		this.applyRotations(modelRenderer);
+		GlStateManager.scale(0.8F, 0.8F, 0.8F);
+		this.resetRotations(modelRenderer);
+		this.resetTranslations(modelRenderer);
+	}
+
+	@Override
+	public void setupHeadwearOffsets(ModelRenderer modelRenderer, EntityEquipmentSlot slot) {
+		this.applyTranslations(modelRenderer);
+		this.applyRotations(modelRenderer);
+		GlStateManager.scale(0.8F, 0.8F, 0.8F);
+		this.resetRotations(modelRenderer);
+		this.resetTranslations(modelRenderer);
+	}
+
+	@Override
+	public void setupBodyOffsets(ModelRenderer modelRenderer, EntityEquipmentSlot slot) {
+		if (slot == EntityEquipmentSlot.CHEST) {
+			this.applyTranslations(modelRenderer);
+			this.applyRotations(modelRenderer);
+			GlStateManager.scale(0.8F, 0.6F, 0.75F);
+			this.resetRotations(modelRenderer);
+			this.resetTranslations(modelRenderer);
+		} else if (slot == EntityEquipmentSlot.LEGS) {
+			this.applyTranslations(modelRenderer);
+			this.applyRotations(modelRenderer);
+			GlStateManager.scale(0.8F, 0.7F, 0.75F);
+			this.resetRotations(modelRenderer);
+			this.resetTranslations(modelRenderer);
+		}
+	}
+
+	@Override
+	public void setupLeftArmOffsets(ModelRenderer modelRenderer, EntityEquipmentSlot slot) {
+		this.applyTranslations(modelRenderer);
+		this.applyRotations(modelRenderer);
+		GlStateManager.scale(0.85F, 1.0F, 0.85F);
+		this.resetRotations(modelRenderer);
+		this.resetTranslations(modelRenderer);
+	}
+
+	@Override
+	public void setupRightArmOffsets(ModelRenderer modelRenderer, EntityEquipmentSlot slot) {
+		this.applyTranslations(modelRenderer);
+		this.applyRotations(modelRenderer);
+		GlStateManager.scale(0.85F, 1.0F, 0.85F);
+		this.resetRotations(modelRenderer);
+		this.resetTranslations(modelRenderer);
+	}
+
+	@Override
+	public void setupLeftLegOffsets(ModelRenderer modelRenderer, EntityEquipmentSlot slot) {
+		if (slot == EntityEquipmentSlot.LEGS) {
+			this.applyTranslations(modelRenderer);
+			this.applyRotations(modelRenderer);
+			GlStateManager.scale(0.75F, 0.75F, 0.75F);
+			this.resetRotations(modelRenderer);
+			this.resetTranslations(modelRenderer);
+		} else if (slot == EntityEquipmentSlot.FEET) {
+			this.applyTranslations(modelRenderer);
+			this.applyRotations(modelRenderer);
+			GlStateManager.scale(0.75F, 0.75F, 0.75F);
+			this.resetRotations(modelRenderer);
+			this.resetTranslations(modelRenderer);
+		}
+	}
+
+	@Override
+	public void setupRightLegOffsets(ModelRenderer modelRenderer, EntityEquipmentSlot slot) {
+		if (slot == EntityEquipmentSlot.LEGS) {
+			this.applyTranslations(modelRenderer);
+			this.applyRotations(modelRenderer);
+			GlStateManager.scale(0.75F, 0.75F, 0.75F);
+			this.resetRotations(modelRenderer);
+			this.resetTranslations(modelRenderer);
+		} else if (slot == EntityEquipmentSlot.FEET) {
+			this.applyTranslations(modelRenderer);
+			this.applyRotations(modelRenderer);
+			GlStateManager.scale(0.75F, 0.75F, 0.75F);
+			this.resetRotations(modelRenderer);
+			this.resetTranslations(modelRenderer);
+		}
+	}
+
+	@Override
+	public void setupPotionOffsets(ModelRenderer modelRenderer) {
+		GlStateManager.translate(0.04F, 0.225F, 0.0F);
+	}
+
 }
