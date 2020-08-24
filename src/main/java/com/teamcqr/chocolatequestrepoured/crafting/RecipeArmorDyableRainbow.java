2d306eb2f35f420a53d6b1c546ac100bc8ff936c
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-08-24 12:56:13 +0200

    Fixed dyable armor recipes
    (cherry picked from commit 7bdcda285991c22af475f8d72f9a8b5013921e0f)

--- src/main/java/com/teamcqr/chocolatequestrepoured/crafting/RecipeArmorDyableRainbow.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/crafting/RecipeArmorDyableRainbow.java
@@ -1,101 +1,97 @@
 package com.teamcqr.chocolatequestrepoured.crafting;
 
-import java.util.List;
+import java.util.Optional;
 
-import com.google.common.collect.Lists;
 import com.teamcqr.chocolatequestrepoured.objects.items.armor.ItemArmorDyable;
 import com.teamcqr.chocolatequestrepoured.util.Reference;
 
-import net.minecraft.client.util.RecipeItemHelper;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.RecipeMatcher;
-import net.minecraftforge.oredict.OreIngredient;
+import net.minecraftforge.oredict.DyeUtils;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public class RecipeArmorDyableRainbow extends IForgeRegistryEntry.Impl<IRecipe> implements IRecipe {
 
-	protected final NonNullList<Ingredient> input = NonNullList.create();
-	protected boolean isSimple = true;
-
 	public RecipeArmorDyableRainbow() {
 		this.setRegistryName(Reference.MODID, "rainbow");
-		this.input.add(new OreIngredient("dyeRed"));
-		this.input.add(new OreIngredient("dyeGreen"));
-		this.input.add(new OreIngredient("dyeBlue"));
-		for (Ingredient i : input) {
-			this.isSimple &= i.isSimple();
-		}
 	}
 
 	@Override
 	public boolean matches(InventoryCrafting inv, World world) {
-		// Start copy from shapeless ore recipe
-		int ingredientCount = 0;
-		RecipeItemHelper recipeItemHelper = new RecipeItemHelper();
-		List<ItemStack> items = Lists.newArrayList();
-
-		for (int i = 0; i < inv.getSizeInventory(); ++i) {
-			ItemStack itemstack = inv.getStackInSlot(i);
-
-			if (!itemstack.isEmpty()) {
-				++ingredientCount;
-				if (this.isSimple) {
-					recipeItemHelper.accountStack(itemstack, 1);
-				} else {
-					items.add(itemstack);
-				}
-			}
-		}
-
-		if (ingredientCount < this.input.size()) {
-			return false;
-		}
-
-		if (this.isSimple) {
-			if (!recipeItemHelper.canCraft(this, null)) {
-				return false;
-			}
-		} else {
-			if (RecipeMatcher.findMatches(items, this.input) == null) {
-				return false;
-			}
-		}
-		// End copy from shapeless ore recipe
-
 		ItemStack itemstack = ItemStack.EMPTY;
 		int slimeBallCount = 0;
+		boolean dyeRed = false;
+		boolean dyeGreen = false;
+		boolean dyeBlue = false;
 
 		for (int i = 0; i < inv.getSizeInventory(); ++i) {
 			ItemStack itemstack1 = inv.getStackInSlot(i);
 
-			if (itemstack1.getItem() instanceof ItemArmorDyable) {
-				itemstack = itemstack1;
-			} else if (itemstack1.getItem() == Items.SLIME_BALL) {
-				slimeBallCount++;
+			if (!itemstack1.isEmpty()) {
+				if (itemstack == ItemStack.EMPTY && itemstack1.getItem() instanceof ItemArmorDyable) {
+					itemstack = itemstack1;
+				} else if (itemstack1.getItem() == Items.SLIME_BALL) {
+					slimeBallCount++;
+				} else {
+					Optional<EnumDyeColor> optionalColor = DyeUtils.colorFromStack(itemstack1);
+					if (optionalColor.isPresent()) {
+						EnumDyeColor color = optionalColor.get();
+						if (!dyeRed && color == EnumDyeColor.RED) {
+							dyeRed = true;
+						} else if (!dyeGreen && color == EnumDyeColor.GREEN) {
+							dyeGreen = true;
+						} else if (!dyeBlue && color == EnumDyeColor.BLUE) {
+							dyeBlue = true;
+						} else {
+							return false;
+						}
+					} else {
+						return false;
+					}
+				}
 			}
 		}
 
-		return !itemstack.isEmpty() && slimeBallCount != 0;
+		return !itemstack.isEmpty() && slimeBallCount != 0 && dyeRed && dyeGreen && dyeBlue;
 	}
 
 	@Override
 	public ItemStack getCraftingResult(InventoryCrafting inv) {
 		ItemStack itemstack = ItemStack.EMPTY;
 		int slimeBallCount = 0;
+		boolean dyeRed = false;
+		boolean dyeGreen = false;
+		boolean dyeBlue = false;
 
 		for (int i = 0; i < inv.getSizeInventory(); ++i) {
 			ItemStack itemstack1 = inv.getStackInSlot(i);
 
-			if (itemstack1.getItem() instanceof ItemArmorDyable) {
-				itemstack = itemstack1;
-			} else if (itemstack1.getItem() == Items.SLIME_BALL) {
-				slimeBallCount++;
+			if (!itemstack1.isEmpty()) {
+				if (itemstack == ItemStack.EMPTY && itemstack1.getItem() instanceof ItemArmorDyable) {
+					itemstack = itemstack1;
+				} else if (itemstack1.getItem() == Items.SLIME_BALL) {
+					slimeBallCount++;
+				} else {
+					Optional<EnumDyeColor> optionalColor = DyeUtils.colorFromStack(itemstack1);
+					if (optionalColor.isPresent()) {
+						EnumDyeColor color = optionalColor.get();
+						if (!dyeRed && color == EnumDyeColor.RED) {
+							dyeRed = true;
+						} else if (!dyeGreen && color == EnumDyeColor.GREEN) {
+							dyeGreen = true;
+						} else if (!dyeBlue && color == EnumDyeColor.BLUE) {
+							dyeBlue = true;
+						} else {
+							return ItemStack.EMPTY;
+						}
+					} else {
+						return ItemStack.EMPTY;
+					}
+				}
 			}
 		}
 
@@ -112,7 +108,7 @@ public class RecipeArmorDyableRainbow extends IForgeRegistryEntry.Impl<IRecipe> 
 
 	@Override
 	public boolean canFit(int width, int height) {
-		return width * height >= this.input.size() + 2;
+		return width * height >= 5;
 	}
 
 }
