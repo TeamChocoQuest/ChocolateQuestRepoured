89704b1f4fd8c0b7b141fbb95c2183126f8ed9c4
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-09-14 22:45:10 +0200

    Update FactionRegistry.java

--- src/main/java/com/teamcqr/chocolatequestrepoured/factions/FactionRegistry.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/factions/FactionRegistry.java
@@ -377,7 +377,7 @@ public class FactionRegistry {
 						}
 						FactionRegistry.this.uuidsBeingLoaded.add(uuid);
 						try {
-							Map<String, Integer> mapping = FactionRegistry.this.playerFactionRepuMap.get(event.player.getPersistentID());
+							Map<String, Integer> mapping = FactionRegistry.this.playerFactionRepuMap.getOrDefault(event.player.getPersistentID(), new ConcurrentHashMap<>());
 							repuDataList.forEach(new Consumer<NBTBase>() {
 
 								@Override
