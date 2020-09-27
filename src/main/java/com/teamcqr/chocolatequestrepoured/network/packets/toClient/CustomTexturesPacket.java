TeamChocoQuest/ChocolateQuestRepoured

commit 0f5b17e6fd35aaaf2ecc1e377a9ffa583a328458

Updated custom texture syncing

src/main/java/com/teamcqr/chocolatequestrepoured/network/packets/toClient/CustomTexturesPacket.java
@@ -14,14 +14,14 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 
 public class CustomTexturesPacket implements IMessage {
 
-	private Map<String, String> entries = new HashMap<>();
+	private Map<String, byte[]> entries = new HashMap<>();
 	private Map<String, Map<ResourceLocation, Set<ResourceLocation>>> textureSets = new HashMap<>();
 
 	public CustomTexturesPacket() {
 
 	}
 
-	public void addPair(String file, String path) {
+	public void addPair(byte[] file, String path) {
 		this.entries.put(path, file);
 	}
 
@@ -35,7 +35,7 @@ public class CustomTexturesPacket implements IMessage {
 		int tscount = buf.readInt();
 		while (keys > 0) {
 			String k = ByteBufUtils.readUTF8String(buf);
-			String v = ByteBufUtils.readUTF8String(buf);
+			byte[] v = buf.readBytes(buf.readInt()).array();
 			this.entries.put(k, v);
 			keys--;
 		}
@@ -66,9 +66,10 @@ public class CustomTexturesPacket implements IMessage {
 		buf.writeInt(this.entries.size());
 		buf.writeInt(this.textureSets.size());
 		// Textures
-		for (Map.Entry<String, String> entry : this.entries.entrySet()) {
+		for (Map.Entry<String, byte[]> entry : this.entries.entrySet()) {
 			ByteBufUtils.writeUTF8String(buf, entry.getKey());
-			ByteBufUtils.writeUTF8String(buf, entry.getValue());
+			buf.writeInt(entry.getValue().length);
+			buf.writeBytes(entry.getValue());
 		}
 		// Texture sets
 		for (Map.Entry<String, Map<ResourceLocation, Set<ResourceLocation>>> entry : this.textureSets.entrySet()) {
@@ -87,7 +88,7 @@ public class CustomTexturesPacket implements IMessage {
 
 	}
 
-	public Map<String, String> getTextureMap() {
+	public Map<String, byte[]> getTextureMap() {
 		return this.entries;
 	}
 

src/main/java/com/teamcqr/chocolatequestrepoured/customtextures/ClientPacketHandler.java
@@ -7,22 +7,20 @@ import java.util.Set;
 
 import com.teamcqr.chocolatequestrepoured.CQRMain;
 import com.teamcqr.chocolatequestrepoured.network.packets.toClient.CustomTexturesPacket;
-import com.teamcqr.chocolatequestrepoured.util.reflection.ReflectionField;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.IReloadableResourceManager;
+import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.client.resources.SimpleReloadableResourceManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.resource.VanillaResourceType;
+import net.minecraftforge.fml.client.FMLClientHandler;
 
 public class ClientPacketHandler {
 
-	private static final ReflectionField<IReloadableResourceManager> RESOURCE_MANAGER = new ReflectionField<>(Minecraft.class, "field_110451_am", "resourceManager");
-
-	@SuppressWarnings("deprecation")
 	public static void handleCTPacketClientside(CustomTexturesPacket message) {
 		CTResourcepack.clear();
 		Map<String, File> fileMap = new HashMap<>();
-		for (Map.Entry<String, String> textureEntry : message.getTextureMap().entrySet()) {
+		for (Map.Entry<String, byte[]> textureEntry : message.getTextureMap().entrySet()) {
 			String path = textureEntry.getKey();
 
 			// !!Path already contains the .png extension
@@ -57,11 +55,11 @@ public class ClientPacketHandler {
 		}
 
 		// and finally we need to reload our resourcepack
-		IReloadableResourceManager rm = RESOURCE_MANAGER.get(Minecraft.getMinecraft());
+		IResourceManager rm = Minecraft.getMinecraft().getResourceManager();
 		if (rm instanceof SimpleReloadableResourceManager) {
 			((SimpleReloadableResourceManager) rm).reloadResourcePack(CTResourcepack.getInstance());
 		} else {
-			Minecraft.getMinecraft().scheduleResourcesRefresh();
+			FMLClientHandler.instance().refreshResources(VanillaResourceType.TEXTURES);
 		}
 		CTResourcepack.loadAllTextures();
 	}

src/main/java/com/teamcqr/chocolatequestrepoured/customtextures/CompressionUtil.java
@@ -8,29 +8,26 @@ import java.util.Base64;
 
 public class CompressionUtil {
 
-	public static String encodeFileToBase64(File file) {
+	public static byte[] encodeFileToBase64(File file) {
 		try {
 			byte[] fileContent = Files.readAllBytes(file.toPath());
-			return Base64.getEncoder().encodeToString(fileContent);
+			return Base64.getEncoder().encode(fileContent);
 		} catch (IOException e) {
-			throw new IllegalStateException("could not read file " + file, e);
+			e.printStackTrace();
 		}
+		return new byte[0];
 	}
 
-	public static boolean decodeBase64ToFile(String filePathWithNameAndExtension, String base64) {
+	public static boolean decodeBase64ToFile(String filePathWithNameAndExtension, byte[] base64) {
 		return decodeBase64ToFile(new File(filePathWithNameAndExtension), base64);
 	}
 
-	public static boolean decodeBase64ToFile(File targetFile, String base64) {
-		byte dearr[] = Base64.getDecoder().decode(base64);
-		FileOutputStream fos;
-		try {
-			fos = new FileOutputStream(targetFile);
+	public static boolean decodeBase64ToFile(File targetFile, byte[] base64) {
+		byte[] dearr = Base64.getDecoder().decode(base64);
+		try (FileOutputStream fos = new FileOutputStream(targetFile)) {
 			fos.write(dearr);
-			fos.close();
 			return true;
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return false;

src/main/java/com/teamcqr/chocolatequestrepoured/customtextures/TextureSetManager.java
@@ -95,7 +95,7 @@ public class TextureSetManager {
 			packet.addPair(base64, path);
 		}*/
 		for (Map.Entry<String, File> entry : TextureSet.getLoadedTextures().entrySet()) {
-			String base64 = CompressionUtil.encodeFileToBase64(entry.getValue());
+			byte[] base64 = CompressionUtil.encodeFileToBase64(entry.getValue());
 			String path = entry.getKey();
 			
 			packet.addPair(base64, path);

src/main/java/com/teamcqr/chocolatequestrepoured/network/packets/handlers/CPacketHandlerSyncTextureSets.java
@@ -3,20 +3,23 @@ package com.teamcqr.chocolatequestrepoured.network.packets.handlers;
 import com.teamcqr.chocolatequestrepoured.customtextures.ClientPacketHandler;
 import com.teamcqr.chocolatequestrepoured.network.packets.toClient.CustomTexturesPacket;
 
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 public class CPacketHandlerSyncTextureSets implements IMessageHandler<CustomTexturesPacket, IMessage> {
-	
+
 	public CPacketHandlerSyncTextureSets() {
 	}
 
 	@Override
 	public IMessage onMessage(CustomTexturesPacket message, MessageContext ctx) {
-		if (ctx.side.isClient()) {
-			ClientPacketHandler.handleCTPacketClientside(message);
-		}
+		FMLCommonHandler.instance().getWorldThread(ctx.netHandler).addScheduledTask(() -> {
+			if (ctx.side.isClient()) {
+				ClientPacketHandler.handleCTPacketClientside(message);
+			}
+		});
 
 		return null;
 	}

commit c4e3dac581f71cbc4a7f21c7927e4c4f01f3fe30

patch for errors introduced in 0f5b17e6fd35aaaf2ecc1e377a9ffa583a328458

src/main/java/com/teamcqr/chocolatequestrepoured/network/packets/toClient/CustomTexturesPacket.java
@@ -35,7 +35,8 @@ public class CustomTexturesPacket implements IMessage {
 		int tscount = buf.readInt();
 		while (keys > 0) {
 			String k = ByteBufUtils.readUTF8String(buf);
-			byte[] v = buf.readBytes(buf.readInt()).array();
+			byte[] v = new byte[buf.readInt()];
+			buf.readBytes(v);
 			this.entries.put(k, v);
 			keys--;
 		}