9735ec7277e7be06dac71ac3bba91185af3f7f6b
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-11-24 14:01:33 +0100

    begin JEI Plugin

--- src/main/java/com/teamcqr/chocolatequestrepoured/integration/jei/JEIPluginCQR.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/integration/jei/JEIPluginCQR.java
@@ -1,7 +1,17 @@
 package com.teamcqr.chocolatequestrepoured.integration.jei;
 
-import mezz.jei.api.IModPlugin;
+import com.teamcqr.chocolatequestrepoured.client.gui.npceditor.GuiMerchant;
+import com.teamcqr.chocolatequestrepoured.integration.jei.gui.GUIHandlerMerchant;
 
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.JEIPlugin;
+
+@JEIPlugin
 public class JEIPluginCQR implements IModPlugin {
 
+	@Override
+	public void register(IModRegistry registry) {
+		registry.addAdvancedGuiHandlers(new GUIHandlerMerchant<>(GuiMerchant.class));
+	}
 }
