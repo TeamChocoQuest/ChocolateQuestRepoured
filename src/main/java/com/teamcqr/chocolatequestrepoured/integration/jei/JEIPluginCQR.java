8d3962308592e1f3035d783e56b39251f64ca54d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-11-24 14:19:09 +0100

    more gui handlers, yet not complete

--- src/main/java/com/teamcqr/chocolatequestrepoured/integration/jei/JEIPluginCQR.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/integration/jei/JEIPluginCQR.java
@@ -1,7 +1,11 @@
 package com.teamcqr.chocolatequestrepoured.integration.jei;
 
+import com.teamcqr.chocolatequestrepoured.client.gui.npceditor.GuiCQREntity;
 import com.teamcqr.chocolatequestrepoured.client.gui.npceditor.GuiMerchant;
+import com.teamcqr.chocolatequestrepoured.client.gui.npceditor.GuiMerchantEditTrade;
+import com.teamcqr.chocolatequestrepoured.integration.jei.gui.GUIHandlerCQREntity;
 import com.teamcqr.chocolatequestrepoured.integration.jei.gui.GUIHandlerMerchant;
+import com.teamcqr.chocolatequestrepoured.integration.jei.gui.GUIHandlerMerchantEditTrade;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
@@ -13,5 +17,7 @@ public class JEIPluginCQR implements IModPlugin {
 	@Override
 	public void register(IModRegistry registry) {
 		registry.addAdvancedGuiHandlers(new GUIHandlerMerchant<>(GuiMerchant.class));
+		registry.addAdvancedGuiHandlers(new GUIHandlerMerchantEditTrade<>(GuiMerchantEditTrade.class));
+		registry.addAdvancedGuiHandlers(new GUIHandlerCQREntity<>(GuiCQREntity.class));
 	}
 }
