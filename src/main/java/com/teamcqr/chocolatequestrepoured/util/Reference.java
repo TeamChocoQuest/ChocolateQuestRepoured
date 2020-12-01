b6772bffec096c9ae82dc324dc7f8e86dab47c48
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-12-01 07:57:50 +0100

    version increment

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/Reference.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/Reference.java
@@ -3,7 +3,7 @@ package com.teamcqr.chocolatequestrepoured.util;
 public class Reference {
 
 	public static final String MODID = "cqrepoured";
-	public static final String VERSION = "2.1.2B";
+	public static final String VERSION = "2.1.3B";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12,1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.teamcqr.chocolatequestrepoured.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.teamcqr.chocolatequestrepoured.proxy.ServerProxy";
