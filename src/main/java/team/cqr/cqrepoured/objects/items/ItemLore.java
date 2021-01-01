ed24670e0998bcdc7fae829cbdd372847cd0236c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2021-01-01 14:09:29 +0100

    item base class
    base class for all items, adds lore to items by default

---
+++ src/main/java/team/cqr/cqrepoured/objects/items/ItemLore.java
@@ -0,0 +1,30 @@
+package team.cqr.cqrepoured.objects.items;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import org.lwjgl.input.Keyboard;
+
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemLore extends Item {
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+			tooltip.add(TextFormatting.BLUE + I18n.format("description." + this.getRegistryName().getPath() + ".name"));
+		} else {
+			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
+		}
+	}
+
+}
