b89828f44e6ef4a17c1d05aea3425e3655801bf1
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2021-01-23 22:01:51 +0100

    fix shelob leap attack

--- src/main/java/team/cqr/cqrepoured/objects/entity/ai/boss/giantspider/BossAISpiderLeapAttack.java
+++ src/main/java/team/cqr/cqrepoured/objects/entity/ai/boss/giantspider/BossAISpiderLeapAttack.java
@@ -11,7 +11,7 @@ public class BossAISpiderLeapAttack extends EntityAIBase {
 	/** The entity that the leaper is leaping towards. */
 	EntityLivingBase leapTarget;
 	/** The entity's motionY after leaping. */
-	float leapMotionY;
+	float leapMotionY = 0.8F;
 
 	private int cooldown;
 
@@ -72,10 +72,12 @@ public class BossAISpiderLeapAttack extends EntityAIBase {
 		double d0 = this.leapTarget.posX - this.leaper.posX;
 		double d1 = this.leapTarget.posZ - this.leaper.posZ;
 		float f = MathHelper.sqrt(d0 * d0 + d1 * d1);
+		
+		this.leaper.faceEntity(this.leapTarget, 100F, 100F);
 
 		if ((double) f >= 1.0E-4D) {
-			this.leaper.motionX += d0 / (double) f * 0.5D * 0.800000011920929D + this.leaper.motionX * 0.20000000298023224D;
-			this.leaper.motionZ += d1 / (double) f * 0.5D * 0.800000011920929D + this.leaper.motionZ * 0.20000000298023224D;
+			this.leaper.motionX += d0 / (double) f * 0.5D * 8.4F + this.leaper.motionX * 8.4F;
+			this.leaper.motionZ += d1 / (double) f * 0.5D * 8.4F + this.leaper.motionZ * 8.4F;
 		}
 
 		this.leaper.motionY = (this.leapTarget.posY - this.leaper.posY) * 0.5;
