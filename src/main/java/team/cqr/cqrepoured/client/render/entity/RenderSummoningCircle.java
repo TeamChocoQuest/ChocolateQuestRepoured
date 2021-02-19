58390f2767d05b7c40df51cb9e87ca834dca0f4d
DerToaster98 <dertoaster@cq-repoured.net>
2021-02-19 20:43:42 +0100

    spinning animation

--- src/main/java/team/cqr/cqrepoured/client/render/entity/RenderSummoningCircle.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/RenderSummoningCircle.java
@@ -38,15 +38,17 @@ public class RenderSummoningCircle extends Render<EntitySummoningCircle> {
 
 	@Override
 	public void doRender(EntitySummoningCircle entity, double x, double y, double z, float entityYaw, float partialTicks) {
-		//GlStateManager.translate((float) x, (float) y, (float) z);
+		// GlStateManager.translate((float) x, (float) y, (float) z);
 
-		/*GlStateManager.color(new Float(0.3F * (Math.sin(0.125 * entity.ticksExisted) + 1)), 0F, 0F);
+		/*
+		 * GlStateManager.color(new Float(0.3F * (Math.sin(0.125 * entity.ticksExisted) + 1)), 0F, 0F);
+		 * 
+		 * this.bindTexture(this.getEntityTexture(entity)); this.model.render(entity, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
+		 */
 
-		this.bindTexture(this.getEntityTexture(entity));
-		this.model.render(entity, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);*/
-		
 		GlStateManager.pushMatrix();
-        GlStateManager.translate((float)x, (float)y, (float)z);
+		GlStateManager.translate((float) x, (float) y + 0.02, (float) z);
+		GlStateManager.rotate((float) ( 4 * entity.ticksExisted), 0F, 1F, 0F);
 		GlStateManager.disableFog();
 		GlStateManager.disableLighting();
 		GlStateManager.disableCull();
@@ -54,40 +56,40 @@ public class RenderSummoningCircle extends Render<EntitySummoningCircle> {
 		GlStateManager.enableBlend();
 		GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
 		this.setLightmapDisabled(true);
-		
+
 		Tessellator tess = Tessellator.getInstance();
 		BufferBuilder builder = tess.getBuffer();
-		
+
 		GlStateManager.glLineWidth(8.0F);
 		builder.begin(3, DefaultVertexFormats.POSITION_COLOR);
-		
-		double radius = 1.5D;
+
+		double radius = 0.75D;
 		double corners = 5;
-		int r =  55 + (int)  Math.round(200 * 0.5 * (Math.sin(0.125 * entity.ticksExisted) + 1));
+		int r = 55 + (int) Math.round(200 * 0.5 * (Math.sin(0.25 * entity.ticksExisted) + 1));
 		int g = 1;
 		int b = 1;
 
 		Vec3d vector = new Vec3d(radius, 0, 0);
 		double alpha = 360D / corners;
 		int skipCorners = 2;
-		alpha *= (double)skipCorners;
-		for(int i = 0; i <= corners; i++) {
-			
+		alpha *= (double) skipCorners;
+		for (int i = 0; i <= corners; i++) {
+
 			builder.pos(vector.x, 0, vector.z).color(r, g, b, 255).endVertex();
-			
+
 			vector = VectorUtil.rotateVectorAroundY(vector, alpha);
 		}
 		vector = VectorUtil.rotateVectorAroundY(vector, -alpha);
-		alpha /= (double)skipCorners;
+		alpha /= (double) skipCorners;
 		vector = VectorUtil.rotateVectorAroundY(vector, alpha);
-		for(int i = 0; i < corners; i++) {
+		for (int i = 0; i < corners; i++) {
 			builder.pos(vector.x, 0, vector.z).color(r, g, b, 255).endVertex();
-			
+
 			vector = VectorUtil.rotateVectorAroundY(vector, alpha);
 		}
-		
+
 		tess.draw();
-		
+
 		this.setLightmapDisabled(false);
 		GlStateManager.glLineWidth(1.0F);
 		GlStateManager.enableLighting();
@@ -97,24 +99,20 @@ public class RenderSummoningCircle extends Render<EntitySummoningCircle> {
 		GlStateManager.depthMask(true);
 		GlStateManager.enableFog();
 		GlStateManager.popMatrix();
-		
+
 	}
-	
-	protected void setLightmapDisabled(boolean disabled)
-    {
-        GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
 
-        if (disabled)
-        {
-            GlStateManager.disableTexture2D();
-        }
-        else
-        {
-            GlStateManager.enableTexture2D();
-        }
+	protected void setLightmapDisabled(boolean disabled) {
+		GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
 
-        GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
-    }
+		if (disabled) {
+			GlStateManager.disableTexture2D();
+		} else {
+			GlStateManager.enableTexture2D();
+		}
+
+		GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
+	}
 
 	@Override
 	public void doRenderShadowAndFire(Entity entityIn, double x, double y, double z, float yaw, float partialTicks) {
