dd5f4eec84b7a118990fc1181b50d5b2a8b6277e
DerToaster98 <dertoaster@cq-repoured.net>
2021-02-21 19:36:05 +0100

    own creature attributes

---
+++ src/main/java/team/cqr/cqrepoured/init/CQRCreatureAttributes.java
@@ -0,0 +1,13 @@
+package team.cqr.cqrepoured.init;
+
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraftforge.common.util.EnumHelper;
+
+public class CQRCreatureAttributes {
+	
+	public static final EnumCreatureAttribute CREATURE_TYPE_ENDERMAN = EnumHelper.addCreatureAttribute("CREATURE_TYPE_ENDERMAN");
+	public static final EnumCreatureAttribute CREATURE_TYPE_ABYSS_WALKER = EnumHelper.addCreatureAttribute("CREATURE_TYPE_ABYSS_WALKER");
+	public static final EnumCreatureAttribute CREATURE_TYPE_WATER = EnumHelper.addCreatureAttribute("CREATURE_TYPE_WATER");
+	public static final EnumCreatureAttribute CREATURE_TYPE_MECHANICAL = EnumHelper.addCreatureAttribute("CREATURE_TYPE_MECHANICAL");
+	
+}
