23e32e57f5e3666f0925c5a8319a6d9f1f6a3f6e
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-03-13 16:39:06 +0100

    Updated rotating laser

--- src/main/java/team/cqr/cqrepoured/objects/entity/boss/spectrelord/EntityRotatingLaser.java
+++ src/main/java/team/cqr/cqrepoured/objects/entity/boss/spectrelord/EntityRotatingLaser.java
@@ -2,26 +2,30 @@ package team.cqr.cqrepoured.objects.entity.boss.spectrelord;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import team.cqr.cqrepoured.objects.entity.boss.AbstractEntityLaser;
 
 public class EntityRotatingLaser extends AbstractEntityLaser {
 
-	private float rotationPerTick;
+	private float deltaRotationYawPerTick;
+	private float deltaRotationPitchPerTick;
 
 	public EntityRotatingLaser(World worldIn) {
-		this(worldIn, null, 4.0F, 1.0F);
+		this(worldIn, null, 4.0F, 1.0F, 0.0F);
 	}
 
-	public EntityRotatingLaser(World worldIn, EntityLivingBase caster, float length, float rotationPerTick) {
+	public EntityRotatingLaser(World worldIn, EntityLivingBase caster, float length, float deltaRotationYawPerTick, float deltaRotationPitchPerTick) {
 		super(worldIn, caster, length);
-		this.rotationPerTick = rotationPerTick;
+		this.deltaRotationYawPerTick = deltaRotationYawPerTick;
+		this.deltaRotationPitchPerTick = deltaRotationPitchPerTick;
 	}
 
 	@Override
 	public void updatePositionAndRotation() {
-		this.rotationYawCQR += this.rotationPerTick;
+		this.rotationYawCQR = MathHelper.wrapDegrees(this.rotationYawCQR + this.deltaRotationYawPerTick);
+		this.rotationPitchCQR = MathHelper.wrapDegrees(this.rotationPitchCQR + this.deltaRotationPitchPerTick);
 		Vec3d vec1 = new Vec3d(this.caster.posX, this.caster.posY + this.caster.height * 0.6D, this.caster.posZ);
 		vec1 = vec1.add(this.getOffsetVector());
 		Vec3d vec4 = Vec3d.fromPitchYaw(this.rotationPitchCQR, this.rotationYawCQR);
@@ -31,13 +35,15 @@ public class EntityRotatingLaser extends AbstractEntityLaser {
 	@Override
 	public void writeSpawnData(ByteBuf buffer) {
 		super.writeSpawnData(buffer);
-		buffer.writeFloat(this.rotationPerTick);
+		buffer.writeFloat(this.deltaRotationYawPerTick);
+		buffer.writeFloat(this.deltaRotationPitchPerTick);
 	}
 
 	@Override
 	public void readSpawnData(ByteBuf additionalData) {
 		super.readSpawnData(additionalData);
-		this.rotationPerTick = additionalData.readFloat();
+		this.deltaRotationYawPerTick = additionalData.readFloat();
+		this.deltaRotationPitchPerTick = additionalData.readFloat();
 	}
 
 }
