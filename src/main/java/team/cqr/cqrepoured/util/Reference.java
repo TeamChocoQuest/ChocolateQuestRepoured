a3cbd63c2a90713270b9933dcd6074039c3d917e
DerToaster98 <seiler.oli@web.de>
2021-04-02 13:55:06 +0200

    version increment

--- src/main/java/team/cqr/cqrepoured/util/Reference.java
+++ src/main/java/team/cqr/cqrepoured/util/Reference.java
@@ -3,7 +3,7 @@ package team.cqr.cqrepoured.util;
 public class Reference {
 
 	public static final String MODID = "cqrepoured";
-	public static final String VERSION = "2.4.0B";
+	public static final String VERSION = "2.4.1B";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12,1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "team.cqr.cqrepoured.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "team.cqr.cqrepoured.proxy.ServerProxy";
