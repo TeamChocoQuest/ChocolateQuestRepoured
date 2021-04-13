264f7ec4d7d0d63c3dbfda7fa44c1b644d8b582c
DerToaster98 <seiler.oli@web.de>
2021-04-13 20:19:03 +0200

    alchemy bag functionality

--- src/main/java/team/cqr/cqrepoured/objects/items/ItemAlchemyBag.java
+++ src/main/java/team/cqr/cqrepoured/objects/items/ItemAlchemyBag.java
@@ -9,17 +9,24 @@ import org.lwjgl.input.Keyboard;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityPotion;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemLingeringPotion;
+import net.minecraft.item.ItemSplashPotion;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.capability.itemhandler.item.CapabilityItemHandlerItemProvider;
 import team.cqr.cqrepoured.util.Reference;
@@ -31,10 +38,33 @@ public class ItemAlchemyBag extends Item {
 	}
 
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
-		if (!worldIn.isRemote) {
-			playerIn.openGui(CQRMain.INSTANCE, Reference.ALCHEMY_BAG_GUI_ID, worldIn, handIn.ordinal(), 0, 0);
-			return new ActionResult<>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer playerIn, EnumHand handIn) {
+		if (!world.isRemote) {
+			if(playerIn.isSneaking()) {
+				playerIn.openGui(CQRMain.INSTANCE, Reference.ALCHEMY_BAG_GUI_ID, world, handIn.ordinal(), 0, 0);
+				return new ActionResult<>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+			}
+			ItemStack stack = playerIn.getHeldItem(handIn);
+			Item item = stack.getItem();
+			if(item == this) {
+				IItemHandler inventory = stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+				
+				for(int i = 0; i < inventory.getSlots(); i++) {
+					ItemStack potion = inventory.extractItem(i, 1, false);
+					if(potion != null && !potion.isEmpty()) {
+						if (potion.getItem() instanceof ItemSplashPotion || potion.getItem() instanceof ItemLingeringPotion) {
+							EntityPotion entitypotion = new EntityPotion(world, playerIn, potion);
+				            entitypotion.shoot(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, -20.0F, 0.5F, 1.0F);
+				            world.spawnEntity(entitypotion);
+							
+							world.playSound((EntityPlayer)null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ENTITY_SPLASH_POTION_THROW, SoundCategory.PLAYERS, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+							
+							return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn)); 
+						}
+					}
+				}
+			}
+			
 		}
 		return new ActionResult<>(EnumActionResult.FAIL, playerIn.getHeldItem(handIn));
 	}
