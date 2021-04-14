bb9b0373517264614052c273d8764d0b515989c6
DerToaster98 <seiler.oli@web.de>
2021-04-14 17:26:32 +0200

    render layer for glowing eyes

---
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/layers/LayerGlowingEyes.java
@@ -0,0 +1,65 @@
+package team.cqr.cqrepoured.client.render.entity.layers;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.util.ResourceLocation;
+
+public class LayerGlowingEyes<T extends EntityLiving> implements LayerRenderer<T> {
+	
+	protected final ResourceLocation EYE_TEXTURES;
+	protected final RenderLiving<T> renderer;
+	
+	public LayerGlowingEyes(RenderLiving<T> renderer, ResourceLocation eyeTextures) {
+		this.EYE_TEXTURES = eyeTextures;
+		this.renderer = renderer;
+	}
+
+	@Override
+	public void doRenderLayer(T entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+		GlStateManager.pushAttrib();
+		GlStateManager.pushMatrix();
+		
+		this.renderer.bindTexture(EYE_TEXTURES);
+        GlStateManager.enableBlend();
+        GlStateManager.disableAlpha();
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
+
+        if (entitylivingbaseIn.isInvisible())
+        {
+            GlStateManager.depthMask(false);
+        }
+        else
+        {
+            GlStateManager.depthMask(true);
+        }
+
+        int i = 61680;
+        int j = i % 65536;
+        int k = i / 65536;
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        Minecraft.getMinecraft().entityRenderer.setupFogColor(true);
+        this.renderer.getMainModel().render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+        Minecraft.getMinecraft().entityRenderer.setupFogColor(false);
+        i = entitylivingbaseIn.getBrightnessForRender();
+        j = i % 65536;
+        k = i / 65536;
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
+        this.renderer.setLightmap(entitylivingbaseIn);
+        GlStateManager.disableBlend();
+        GlStateManager.enableAlpha();
+
+		GlStateManager.popMatrix();
+		GlStateManager.popAttrib();
+	}
+
+	@Override
+	public boolean shouldCombineTextures() {
+		return false;
+	}
+
+}
