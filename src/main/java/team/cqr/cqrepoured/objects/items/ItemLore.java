dfc12833f1be5c590c130a4c0502de1b5daa11ed
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-04-15 16:44:38 +0200

    Updated default tooltip to use line breaks.

--- src/main/java/team/cqr/cqrepoured/objects/items/ItemLore.java
+++ src/main/java/team/cqr/cqrepoured/objects/items/ItemLore.java
@@ -21,7 +21,7 @@ public class ItemLore extends Item {
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
 		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description." + this.getRegistryName().getPath() + ".name"));
+			tooltip.add(TextFormatting.BLUE + I18n.format("description." + this.getRegistryName().getPath() + ".name", '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n'));
 		} else {
 			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
 		}
