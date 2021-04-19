4ab1d36d991a6d53a581a51ed98ac62c3aa4e923
DerToaster98 <seiler.oli@web.de>
2021-04-19 08:19:20 +0200

    workaround for #268

--- src/main/java/team/cqr/cqrepoured/structureprot/ProtectedRegionHelper.java
+++ src/main/java/team/cqr/cqrepoured/structureprot/ProtectedRegionHelper.java
@@ -181,11 +181,15 @@ public class ProtectedRegionHelper {
 		Item item = stack.getItem();
 		if (item instanceof ItemBlock) {
 			int meta = ((ItemBlock) item).getMetadata(stack.getItemDamage());
+			IBlockState result = ((ItemBlock) item).getBlock().getDefaultState();
 			if (hitVec != null) {
-				return ((ItemBlock) item).getBlock().getStateForPlacement(world, pos, facing, (float) hitVec.x, (float) hitVec.y, (float) hitVec.z, meta, placer, hand);
-			} else {
-				return ((ItemBlock) item).getBlock().getDefaultState();
+				try{
+					result =  ((ItemBlock) item).getBlock().getStateForPlacement(world, pos, facing, (float) hitVec.x, (float) hitVec.y, (float) hitVec.z, meta, placer, hand);
+				} catch(IllegalArgumentException iae) {
+					result = ((ItemBlock) item).getBlock().getDefaultState();
+				}
 			}
+			return result;
 		}
 		FluidStack fluidStack = FluidUtil.getFluidContained(stack);
 		if (fluidStack != null && fluidStack.amount != 0 && fluidStack.getFluid() != null && fluidStack.getFluid().canBePlacedInWorld()) {
