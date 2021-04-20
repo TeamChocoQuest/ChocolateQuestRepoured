7b992b6ee36c6e4e8cf65502cee2257bb9649604
DerToaster98 <seiler.oli@web.de>
2021-04-20 08:43:29 +0200

    patch some render bugs

--- src/main/java/team/cqr/cqrepoured/client/render/entity/layers/LayerGlowingEyes.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/layers/LayerGlowingEyes.java
@@ -28,15 +28,6 @@ public class LayerGlowingEyes<T extends EntityLiving> implements LayerRenderer<T
         GlStateManager.disableAlpha();
         GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
 
-        if (entitylivingbaseIn.isInvisible())
-        {
-            GlStateManager.depthMask(false);
-        }
-        else
-        {
-            GlStateManager.depthMask(true);
-        }
-
         int i = 61680;
         int j = i % 65536;
         int k = i / 65536;
@@ -50,9 +41,6 @@ public class LayerGlowingEyes<T extends EntityLiving> implements LayerRenderer<T
         k = i / 65536;
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
         this.renderer.setLightmap(entitylivingbaseIn);
-        if(entitylivingbaseIn.isInvisible()) {
-        	GlStateManager.depthMask(true);
-        }
         GlStateManager.disableBlend();
         GlStateManager.enableAlpha();
 
