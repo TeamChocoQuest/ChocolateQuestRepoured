67635ecfd2ad9f2546026dd1d204b400c6f06ebb
DerToaster98 <seiler.oli@web.de>
2021-04-28 19:32:45 +0200

    add some other methods for easier access

--- src/main/java/team/cqr/cqrepoured/client/util/ElectricFieldRenderUtil.java
+++ src/main/java/team/cqr/cqrepoured/client/util/ElectricFieldRenderUtil.java
@@ -11,8 +11,8 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 
 public class ElectricFieldRenderUtil {
-
-	public static void renderElectricField(Entity entity, double x, double y, double z, int bolts) {
+	
+	public static void renderElectricField(Entity entity, double x, double y, double z, int bolts, double boltSize) {
 		GlStateManager.pushMatrix();
 
 		GlStateManager.disableTexture2D();
@@ -32,11 +32,10 @@ public class ElectricFieldRenderUtil {
 			builder.begin(3, DefaultVertexFormats.POSITION_COLOR);
 
 			for (int i = 0; i <= steps; ++i) {
-				double distance = entity.width;
-				double boltSize = 80;
+				double radius = entity.width;
 
-				double vX = Math.sin((i + rng.nextInt()) / boltSize * Math.PI * 2.0D) * distance + (rng.nextDouble() - 0.5D) * (entity.width / 2);
-				double vZ = Math.cos((i + rng.nextInt()) / boltSize * Math.PI * 2.0D) * distance + (rng.nextDouble() - 0.5D) * (entity.width / 2);
+				double vX = Math.sin((i + rng.nextInt()) / boltSize * Math.PI * 2.0D) * radius + (rng.nextDouble() - 0.5D) * (entity.width / 2);
+				double vZ = Math.cos((i + rng.nextInt()) / boltSize * Math.PI * 2.0D) * radius + (rng.nextDouble() - 0.5D) * (entity.width / 2);
 				double vY = Math.sin((entity.ticksExisted + i + rng.nextInt()) / boltSize * Math.PI) * (entity.height / 2) + rng.nextDouble() + entity.height / 2;
 
 				builder.pos(x + vX, y + vY, z + vZ).color(0.5F, 0.64F, 1.0F, 0.6F).endVertex();
@@ -50,6 +49,10 @@ public class ElectricFieldRenderUtil {
 		GlStateManager.enableLighting();
 
 		GlStateManager.popMatrix();
+	}
+
+	public static void renderElectricField(Entity entity, double x, double y, double z) {
+		renderElectricField(entity, x, y, z, 5, 80);
 	}
 
 }
