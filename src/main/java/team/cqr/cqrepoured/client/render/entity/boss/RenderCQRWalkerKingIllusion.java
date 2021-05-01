c4ebd749735e403f7fb67c472f890df883af226b
DerToaster98 <seiler.oli@web.de>
2021-05-01 18:07:41 +0200

    patch rendering of walker king illusions

---
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/boss/RenderCQRWalkerKingIllusion.java
@@ -0,0 +1,16 @@
+package team.cqr.cqrepoured.client.render.entity.boss;
+
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.util.ResourceLocation;
+import team.cqr.cqrepoured.client.render.entity.RenderCQREntity;
+import team.cqr.cqrepoured.client.render.entity.layers.LayerGlowingEyes;
+import team.cqr.cqrepoured.objects.entity.misc.EntityWalkerKingIllusion;
+import team.cqr.cqrepoured.util.Reference;
+
+public class RenderCQRWalkerKingIllusion extends RenderCQREntity<EntityWalkerKingIllusion> {
+
+	public RenderCQRWalkerKingIllusion(RenderManager rendermanagerIn) {
+		super(rendermanagerIn, "boss/walker_king_exploding.png", true);
+		this.addLayer(new LayerGlowingEyes<EntityWalkerKingIllusion>(this, new ResourceLocation(Reference.MODID, "textures/entity/boss/walker_king_exploding.png")));
+	}
+}
