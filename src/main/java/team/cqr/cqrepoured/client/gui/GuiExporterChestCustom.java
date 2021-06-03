ef3eaf5dd6f3f348c5cea920c898208a3ee2560b
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-06-03 20:00:54 +0200

    Fix title of custom exporter chest gui

--- src/main/java/team/cqr/cqrepoured/client/gui/GuiExporterChestCustom.java
+++ src/main/java/team/cqr/cqrepoured/client/gui/GuiExporterChestCustom.java
@@ -53,7 +53,7 @@ public class GuiExporterChestCustom extends GuiScreen {
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		this.drawDefaultBackground();
-		this.drawCenteredString(this.fontRenderer, I18n.format("tile.exporter.name"), this.width / 2, 20, 16777215);
+		this.drawCenteredString(this.fontRenderer, I18n.format("tile.exporter_chest_custom.name"), this.width / 2, 20, 16777215);
 
 		this.drawString(this.fontRenderer, "Loot Table", this.width / 2 - 70, this.height / 2 - 80, 10526880);
 		this.lootTableTextField.drawTextBox();
