7b69da1ab809d7540283f08571fb07942d7ec802
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-07-02 01:24:33 +0200

    Fix force field nexus spinning faster with higher fps

--- src/main/java/team/cqr/cqrepoured/client/render/tileentity/ModelNexusCrystal.java
+++ src/main/java/team/cqr/cqrepoured/client/render/tileentity/ModelNexusCrystal.java
@@ -1,5 +1,6 @@
 package team.cqr.cqrepoured.client.render.tileentity;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.GlStateManager;
@@ -32,19 +33,20 @@ public class ModelNexusCrystal extends ModelBase {
 	 */
 	@Override
 	public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+		Minecraft mc = Minecraft.getMinecraft();
 		GlStateManager.pushMatrix();
 		GlStateManager.scale(2.0F, 2.0F, 2.0F);
 		GlStateManager.translate(0.0F, 0.3F, 0.0F);
 		GlStateManager.rotate(60.0F, 0.7071F, 0.0F, 0.7071F);
-		this.glass.rotateAngleY = this.glass.rotateAngleY + 0.015625F;
+		this.glass.rotateAngleY = (mc.world.getTotalWorldTime() + ageInTicks) * 0.15F;
 		this.glass.render(scale);
 		GlStateManager.scale(0.875F, 0.875F, 0.875F);
 		GlStateManager.rotate(60.0F, 0.7071F, 0.0F, 0.7071F);
-		this.glass.rotateAngleY = this.glass.rotateAngleY + 0.015625F;
+		this.glass.rotateAngleY = (mc.world.getTotalWorldTime() + ageInTicks) * 0.1F;
 		this.glass.render(scale);
 		GlStateManager.scale(0.875F, 0.875F, 0.875F);
 		GlStateManager.rotate(60.0F, 0.7071F, 0.0F, 0.7071F);
-		this.cube.rotateAngleY = this.cube.rotateAngleY + 0.015625F;
+		this.cube.rotateAngleY = (mc.world.getTotalWorldTime() + ageInTicks) * 0.125F;
 		this.cube.render(scale);
 		GlStateManager.popMatrix();
 	}
--- src/main/java/team/cqr/cqrepoured/client/render/tileentity/TileEntityForceFieldNexusRenderer.java
+++ src/main/java/team/cqr/cqrepoured/client/render/tileentity/TileEntityForceFieldNexusRenderer.java
@@ -4,7 +4,6 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import team.cqr.cqrepoured.tileentity.TileEntityForceFieldNexus;
@@ -25,16 +24,13 @@ public class TileEntityForceFieldNexusRenderer extends TileEntitySpecialRenderer
 	public void render(TileEntityForceFieldNexus te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
 		super.render(te, x, y, z, partialTicks, destroyStage, alpha);
 		// Crystal code
-		float f = partialTicks;
 		GlStateManager.pushMatrix();
 		GlStateManager.disableLighting();
 		GlStateManager.enableAlpha();
 		GlStateManager.translate((float) x + 0.5F, (float) y + 0.7F, (float) z + 0.5F);
 		this.bindTexture(CRYSTAL_TEXTURES);
-		float f1 = MathHelper.sin(f * 0.02F) / 2.0F + 0.5F;
-		f1 = f1 * f1 + f1;
 
-		this.crystal.render(null, 0.0F, f, f1 * 0.1F, 0.0F, 0.0F, 0.025F);
+		this.crystal.render(null, 0.0F, 0.0F, partialTicks, 0.0F, 0.0F, 0.025F);
 		GlStateManager.enableLighting();
 		GlStateManager.disableAlpha();
 		GlStateManager.popMatrix();
