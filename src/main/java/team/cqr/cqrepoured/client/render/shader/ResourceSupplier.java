da0eba7ae10edf04ff998674c055b6e5b061fc39
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-08-23 04:16:11 +0200

    Create ResourceSupplier.java

---
+++ src/main/java/team/cqr/cqrepoured/client/render/shader/ResourceSupplier.java
@@ -0,0 +1,39 @@
+package team.cqr.cqrepoured.client.render.shader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.IResource;
+import net.minecraft.util.ResourceLocation;
+
+public class ResourceSupplier implements Supplier<String> {
+
+	private final ResourceLocation file;
+
+	public ResourceSupplier(ResourceLocation file) {
+		this.file = file;
+	}
+
+	@Override
+	public String get() {
+		StringBuilder sb = new StringBuilder();
+
+		try (IResource resource = Minecraft.getMinecraft().getResourceManager().getResource(this.file)) {
+			try (Stream<String> stream = new BufferedReader(new InputStreamReader(resource.getInputStream())).lines()) {
+				stream.forEach(s -> {
+					sb.append(s);
+					sb.append('\n');
+				});
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+	}
+
+}
