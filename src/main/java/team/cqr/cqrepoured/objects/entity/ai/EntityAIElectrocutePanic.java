003b63740af26ac048bb8e75150effa12662f135
DerToaster98 <seiler.oli@web.de>
2021-09-12 11:07:00 +0200

    add panic AI for electrocuted entities

---
+++ src/main/java/team/cqr/cqrepoured/objects/entity/ai/EntityAIElectrocutePanic.java
@@ -0,0 +1,27 @@
+package team.cqr.cqrepoured.objects.entity.ai;
+
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.ai.EntityAIPanic;
+import team.cqr.cqrepoured.capability.electric.CapabilityElectricShockProvider;
+import team.cqr.cqrepoured.init.CQRCreatureAttributes;
+import team.cqr.cqrepoured.objects.entity.IMechanical;
+
+public class EntityAIElectrocutePanic extends EntityAIPanic {
+
+	public EntityAIElectrocutePanic(EntityCreature creature, double speedIn) {
+		super(creature, speedIn);
+	}
+
+	@Override
+	public boolean shouldExecute() {
+		if (this.creature instanceof IMechanical || this.creature.getCreatureAttribute() == CQRCreatureAttributes.CREATURE_TYPE_MECHANICAL) {
+			return false;
+		}
+
+		if (this.creature.hasCapability(CapabilityElectricShockProvider.ELECTROCUTE_HANDLER_CQR, null)) {
+			return this.creature.getCapability(CapabilityElectricShockProvider.ELECTROCUTE_HANDLER_CQR, null).getRemainingTicks() > 0;
+		}
+		return false;
+	}
+
+}
