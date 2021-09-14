fe35caa8c6c3d8e8e4235de80529507b67a12423
DerToaster98 <seiler.oli@web.de>
2021-09-14 19:33:53 +0200

    create sizable electric field entity

---
+++ src/main/java/team/cqr/cqrepoured/objects/entity/misc/EntityElectricFieldSizable.java
@@ -0,0 +1,54 @@
+package team.cqr.cqrepoured.objects.entity.misc;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import team.cqr.cqrepoured.objects.entity.ISizable;
+
+public class EntityElectricFieldSizable extends EntityElectricField implements ISizable {
+
+	private float sizeScaling = 1.0F;
+	
+	public EntityElectricFieldSizable(World worldIn, int charge) {
+		super(worldIn, charge);
+		
+		this.initializeSize();
+	}
+	
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound compound) {
+		super.readEntityFromNBT(compound);
+		this.callOnReadFromNBT(compound);
+	}
+	
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound compound) {
+		super.writeEntityToNBT(compound);
+		this.callOnWriteToNBT(compound);
+	}
+
+	@Override
+	public float getDefaultWidth() {
+		return 1.0F;
+	}
+
+	@Override
+	public float getDefaultHeight() {
+		return 1.0F;
+	}
+
+	@Override
+	public float getSizeVariation() {
+		return this.sizeScaling;
+	}
+
+	@Override
+	public void applySizeVariation(float value) {
+		this.sizeScaling = value;
+	}
+
+	@Override
+	public void setSizeWrapper(float width, float height) {
+		this.setSize(width, height);
+	}
+
+}
