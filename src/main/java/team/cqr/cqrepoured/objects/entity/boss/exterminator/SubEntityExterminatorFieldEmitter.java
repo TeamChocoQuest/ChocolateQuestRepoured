9ed36d4629d017e0a18f6d4bd1f5451bf5ac91ba
DerToaster98 <seiler.oli@web.de>
2021-09-21 21:08:24 +0200

    add in size handling code to scale the field

--- src/main/java/team/cqr/cqrepoured/objects/entity/boss/exterminator/SubEntityExterminatorFieldEmitter.java
+++ src/main/java/team/cqr/cqrepoured/objects/entity/boss/exterminator/SubEntityExterminatorFieldEmitter.java
@@ -10,9 +10,11 @@ import team.cqr.cqrepoured.objects.entity.misc.EntityElectricFieldSizable;
 public class SubEntityExterminatorFieldEmitter extends MultiPartEntityPart implements IBlacklistedFromStatues, IDontRenderFire, ISizable {
 
 	private EntityElectricFieldSizable electricField = null;
-	
+	private EntityCQRExterminator exterminator;
+
 	public SubEntityExterminatorFieldEmitter(EntityCQRExterminator parent, String partName) {
 		super(parent, partName, 0.5F, 0.5F);
+		this.exterminator = parent;
 		this.initializeSize();
 	}
 
@@ -20,8 +22,8 @@ public class SubEntityExterminatorFieldEmitter extends MultiPartEntityPart imple
 	public boolean canbeTurnedToStone() {
 		return false;
 	}
-	
-	//ISizable stuff
+
+	// ISizable stuff
 	@Override
 	public float getDefaultWidth() {
 		return 0.5F;
@@ -39,7 +41,48 @@ public class SubEntityExterminatorFieldEmitter extends MultiPartEntityPart imple
 
 	@Override
 	public void applySizeVariation(float value) {
-		//No, this should not be done in this entity, it happens in the parent...
+		// No, this should not be done in this entity, it happens in the parent...
+		try {
+			this.electricField.applySizeVariation(value);
+		} catch (NullPointerException npe) {
+			// Ignore
+		}
+	}
+
+	@Override
+	public void setSizeVariation(float size) {
+		ISizable.super.setSizeVariation(size);
+		try {
+			this.electricField.setSizeVariation(size);
+		} catch (NullPointerException npe) {
+			// Ignore
+		}
+	}
+
+	@Override
+	public void resize(float widthScale, float heightSacle) {
+		ISizable.super.resize(widthScale, heightSacle);
+		try {
+			this.electricField.resize(widthScale, heightSacle);
+		} catch (NullPointerException npe) {
+			// Ignore
+		}
+	}
+
+	public void createElectricField(int charge) {
+		if (this.electricField != null) {
+			this.electricField.setCharge(charge);
+		} else {
+			this.electricField = new EntityElectricFieldSizable(this.exterminator.getWorld(), charge, this.exterminator.getPersistentID());
+		}
+	}
+
+	public void destroyElectricField() {
+		try {
+			this.electricField.destroyField();
+		} catch (NullPointerException npe) {
+			// Ignore
+		}
 	}
 
 }
