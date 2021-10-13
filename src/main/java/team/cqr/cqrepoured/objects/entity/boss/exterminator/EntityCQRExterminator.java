6fdbe76fab0103c4f42aa53c02bd1ec505203efc
DerToaster98 <seiler.oli@web.de>
2021-10-13 20:16:22 +0200

    apply same patch for bugs like #287

--- src/main/java/team/cqr/cqrepoured/objects/entity/boss/exterminator/EntityCQRExterminator.java
+++ src/main/java/team/cqr/cqrepoured/objects/entity/boss/exterminator/EntityCQRExterminator.java
@@ -217,11 +217,11 @@ public class EntityCQRExterminator extends AbstractEntityCQRBoss implements IMec
 	public static final String ANIM_NAME_CANNON_SHOOT = ANIM_NAME_PREFIX + "shoot_cannon";
 	public static final String ANIM_NAME_THROW = ANIM_NAME_PREFIX + "throw";
 	public static final String ANIM_NAME_GROUND_SMASH = ANIM_NAME_PREFIX + "ground_slam";
-	@SideOnly(Side.CLIENT)
+	//@SideOnly(Side.CLIENT)
 	private boolean shootIndicator = false;
-	@SideOnly(Side.CLIENT)
+	//@SideOnly(Side.CLIENT)
 	private boolean throwIndicator = false;
-	@SideOnly(Side.CLIENT)
+	//@SideOnly(Side.CLIENT)
 	private boolean smashIndicator = false;
 
 	private <E extends IAnimatable> PlayState predicateBigAnimations(AnimationEvent<E> event) {
@@ -679,7 +679,7 @@ public class EntityCQRExterminator extends AbstractEntityCQRBoss implements IMec
 	
 	
 	//Kick stuff
-	@SideOnly(Side.CLIENT)
+	//@SideOnly(Side.CLIENT)
 	private boolean kickInProgressClient;
 
 	@SideOnly(Side.CLIENT)
