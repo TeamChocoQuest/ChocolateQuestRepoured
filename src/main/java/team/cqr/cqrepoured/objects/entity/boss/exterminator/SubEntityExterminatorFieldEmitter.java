5927081185c9f8b2363ee7260bd5523e583dee0e
DerToaster98 <seiler.oli@web.de>
2021-10-17 12:41:54 +0200

    update emitters

--- src/main/java/team/cqr/cqrepoured/objects/entity/boss/exterminator/SubEntityExterminatorFieldEmitter.java
+++ src/main/java/team/cqr/cqrepoured/objects/entity/boss/exterminator/SubEntityExterminatorFieldEmitter.java
@@ -1,14 +1,11 @@
 package team.cqr.cqrepoured.objects.entity.boss.exterminator;
 
 import java.util.Random;
+import java.util.function.Function;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.network.datasync.DataParameter;
-import net.minecraft.network.datasync.DataSerializers;
-import net.minecraft.network.datasync.EntityDataManager;
 import team.cqr.cqrepoured.capability.electric.CapabilityElectricShockProvider;
 import team.cqr.cqrepoured.objects.entity.MultiPartEntityPartSizable;
 import team.cqr.cqrepoured.objects.entity.misc.EntityElectricFieldSizable;
@@ -18,25 +15,22 @@ public class SubEntityExterminatorFieldEmitter extends MultiPartEntityPartSizabl
 
 	private EntityElectricFieldSizable electricField = null;
 	private EntityCQRExterminator exterminator;
+	
+	private final Function<Object, EntityLivingBase> funcGetElectrocuteTarget;
 
 	private int remainingActiveTime;
 	private int activeTimeNoTarget;
 	private int cooldown;
-	// When the target changes it gets inactive
-	private EntityLivingBase target;
 	
-	protected static final DataParameter<Integer> TARGETED_ENTITY_ID = EntityDataManager.<Integer>createKey(SubEntityExterminatorFieldEmitter.class, DataSerializers.VARINT);
-
-	public SubEntityExterminatorFieldEmitter(EntityCQRExterminator parent, String partName) {
+	public SubEntityExterminatorFieldEmitter(EntityCQRExterminator parent, String partName, final Function<Object, EntityLivingBase> funcGetElectrocuteTarget) {
 		super(parent, partName, 0.5F, 0.5F);
 		this.exterminator = parent;
+		this.funcGetElectrocuteTarget = funcGetElectrocuteTarget;
 	}
 
 	@Override
 	protected void entityInit() {
 		super.entityInit();
-
-		this.dataManager.register(TARGETED_ENTITY_ID, Integer.valueOf(0));
 	}
 
 	@Override
@@ -86,7 +80,7 @@ public class SubEntityExterminatorFieldEmitter extends MultiPartEntityPartSizabl
 	}
 
 	public boolean isActive() {
-		return this.exterminator.areElectricCoilsActive() && this.remainingActiveTime > 0;
+		return this.exterminator.canElectricCoilsBeActive() && this.remainingActiveTime > 0;
 	}
 
 	@Override
@@ -98,10 +92,10 @@ public class SubEntityExterminatorFieldEmitter extends MultiPartEntityPartSizabl
 			if(this.ticksExisted % 10 == 0) {
 				//Play a sound
 			}
-			
+			EntityLivingBase target = this.getTargetedEntity();
 			this.remainingActiveTime--;
 			// If we don't have a target we wait a bit and then deactivate
-			if (this.target == null) {
+			if (target == null) {
 				this.activeTimeNoTarget++;
 				if (this.activeTimeNoTarget > 60) {
 					// We didn'T have a target for 3 seconds, we deactivate forcefully
@@ -109,10 +103,13 @@ public class SubEntityExterminatorFieldEmitter extends MultiPartEntityPartSizabl
 				}
 			} else {
 				//Damage the target by making it electrocuted
-				this.target.getCapability(CapabilityElectricShockProvider.ELECTROCUTE_HANDLER_CQR, null).setRemainingTicks(200);
+				target.getCapability(CapabilityElectricShockProvider.ELECTROCUTE_HANDLER_CQR, null).setRemainingTicks(200);
 			}
 		} else {
 			this.cooldown--;
+			if(this.remainingActiveTime > 0) {
+				this.remainingActiveTime = -1;
+			}
 			if (this.cooldown <= 0) {
 				this.cooldown = DungeonGenUtils.randomBetween(60, 180, this.rand);
 				// Now, re-set the active time
@@ -122,34 +119,9 @@ public class SubEntityExterminatorFieldEmitter extends MultiPartEntityPartSizabl
 		}
 	}
 
-	public void setTarget(@Nonnull EntityLivingBase target) {
-		if(target == null) {
-			this.dataManager.set(TARGETED_ENTITY_ID, 0);
-		}
-		if (((target != null && this.target == null) || (target != null && !target.equals(this.target))) && !this.world.isRemote) {
-			// New target => Deactivate if null => handled in update
-			this.target = target;
-			this.dataManager.set(TARGETED_ENTITY_ID, target.getEntityId());
-		}
-	}
-
 	@Nullable
 	public EntityLivingBase getTargetedEntity() {
-		if(this.world.isRemote) {
-			if(this.dataManager.get(TARGETED_ENTITY_ID) == 0) {
-				return null;
-			}
-		}
-		return this.target;
-	}
-
-	public void notifyDataManagerChange(DataParameter<?> key) {
-		super.notifyDataManagerChange(key);
-
-		if (TARGETED_ENTITY_ID.equals(key) && this.world.isRemote) {
-			int targetEntID = this.dataManager.get(TARGETED_ENTITY_ID);
-			this.target = (EntityLivingBase) this.world.getEntityByID(targetEntID);
-		}
+		return this.funcGetElectrocuteTarget.apply(this);
 	}
 
 	public Random getRNG() {
