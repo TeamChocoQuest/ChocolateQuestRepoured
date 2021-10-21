ccc7bce45ddaeffbe543ad680e24d89716849da2
DerToaster98 <seiler.oli@web.de>
2021-10-21 21:25:59 +0200

    add to do note

--- src/main/java/team/cqr/cqrepoured/client/mcmetaserializers/GlowingMetadataSectionSerializer.java
+++ src/main/java/team/cqr/cqrepoured/client/mcmetaserializers/GlowingMetadataSectionSerializer.java
@@ -14,6 +14,7 @@ import net.minecraft.client.resources.data.BaseMetadataSectionSerializer;
 import net.minecraft.util.JsonUtils;
 import net.minecraft.util.Tuple;
 
+//TODO: Change to be completely json based, not some weird string monsters
 public class GlowingMetadataSectionSerializer extends BaseMetadataSectionSerializer<GlowingMetadataSection> implements JsonSerializer<GlowingMetadataSection> {
 
 	@Override
@@ -31,18 +32,26 @@ public class GlowingMetadataSectionSerializer extends BaseMetadataSectionSeriali
 				JsonElement jsonelement = jsonarray.get(i);
 				String entry = JsonUtils.getString(jsonelement.getAsJsonObject(), "corners");
 
-				if (entry.split("|").length == 3 && entry.split("-").length == 2) {
-					String par1 = entry.split("-")[0];
-					String[] split = par1.split("|");
-					Tuple<Integer, Integer> pos1 = new Tuple<>(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
-					String par2 = entry.split("-")[1];
-					split = par2.split("|");
-					Tuple<Integer, Integer> pos2 = new Tuple<>(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
-					if (pos1.getFirst() <= pos2.getFirst() && pos1.getSecond() <= pos2.getSecond()) {
-						result.addSection(pos1, pos2);
-					}
-
+				if (entry.split("-").length != 2) {
+					System.out.println(entry.split("-"));
+					continue;
 				}
+				
+				if (entry.split(";").length != 3) {
+					System.out.println(entry.split(";").length);
+					continue;
+				}
+
+				String par1 = entry.split("-")[0];
+				String[] split = par1.split(";");
+				Tuple<Integer, Integer> pos1 = new Tuple<>(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+				String par2 = entry.split("-")[1];
+				split = par2.split(";");
+				Tuple<Integer, Integer> pos2 = new Tuple<>(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+				if (pos1.getFirst() <= pos2.getFirst() && pos1.getSecond() <= pos2.getSecond()) {
+					result.addSection(pos1, pos2);
+				}
+
 			}
 			if (!result.isEmpty()) {
 				return result;
@@ -54,21 +63,21 @@ public class GlowingMetadataSectionSerializer extends BaseMetadataSectionSeriali
 	@Override
 	public JsonElement serialize(GlowingMetadataSection src, Type typeOfSrc, JsonSerializationContext context) {
 		JsonObject jsonobject = new JsonObject();
-		
-		if(!src.isEmpty()) {
+
+		if (!src.isEmpty()) {
 			JsonArray jsonarray = new JsonArray();
-			
-			for(Tuple<Tuple<Integer, Integer>,Tuple<Integer, Integer>> entry : src.getGlowingSections()) {
+
+			for (Tuple<Tuple<Integer, Integer>, Tuple<Integer, Integer>> entry : src.getGlowingSections()) {
 				String estring = entry.getFirst().getFirst() + "|" + entry.getFirst().getSecond() + "-" + entry.getSecond().getFirst() + "|" + entry.getSecond().getSecond();
 				JsonObject jsonobject1 = new JsonObject();
 				jsonobject1.addProperty("corners", estring);
-				
+
 				jsonarray.add(jsonobject1);
 			}
-			
+
 			jsonobject.add("sections", jsonarray);
 		}
-		
+
 		return jsonobject;
 	}
 
