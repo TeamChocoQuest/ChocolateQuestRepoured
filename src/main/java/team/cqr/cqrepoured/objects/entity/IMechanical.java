acaab00c6ad23b381f6e995c87eec66d2e5ade10
DerToaster98 <seiler.oli@web.de>
2021-10-22 20:51:14 +0200

    machines can't get potion effects, they don't live

--- src/main/java/team/cqr/cqrepoured/objects/entity/IMechanical.java
+++ src/main/java/team/cqr/cqrepoured/objects/entity/IMechanical.java
@@ -1,9 +1,16 @@
 package team.cqr.cqrepoured.objects.entity;
 
+import net.minecraft.potion.PotionEffect;
+
 public interface IMechanical {
 	
 	public default boolean isImmuneToFire() {
 		return true;
 	}
 
+	//We're an machine, we don't live
+	public default boolean isPotionApplicable(PotionEffect potioneffectIn) {
+		return false;
+	}
+	
 }
