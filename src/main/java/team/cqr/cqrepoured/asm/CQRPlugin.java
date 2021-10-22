13e6141750f96b961e5e04b4d24453f4bf1d5d42
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-10-22 15:29:41 +0200

    Setup ASM transformer

---
+++ src/main/java/team/cqr/cqrepoured/asm/CQRPlugin.java
@@ -0,0 +1,37 @@
+package team.cqr.cqrepoured.asm;
+
+import java.util.Map;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
+
+@MCVersion("1.12.2")
+public class CQRPlugin implements IFMLLoadingPlugin {
+
+	@Override
+	public String[] getASMTransformerClass() {
+		return new String[] {
+				CQRClassTransformer.class.getName() };
+	}
+
+	@Override
+	public String getModContainerClass() {
+		return null;
+	}
+
+	@Override
+	public String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+
+	}
+
+	@Override
+	public String getAccessTransformerClass() {
+		return null;
+	}
+
+}
