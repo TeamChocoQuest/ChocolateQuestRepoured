4a9f72c15150a2ff4213b00c7eb5227d576bec69
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-10-28 14:28:22 +0200

    Mark class as deprecated

--- src/main/java/team/cqr/cqrepoured/gentest/util/NeighborNotifyUtil.java
+++ src/main/java/team/cqr/cqrepoured/gentest/util/NeighborNotifyUtil.java
@@ -14,6 +14,7 @@ import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import net.minecraftforge.event.ForgeEventFactory;
 
+@Deprecated
 public class NeighborNotifyUtil {
 
 	private static final MutableBlockPos MUTABLE = new MutableBlockPos();
