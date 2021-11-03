8c4700ad8370fd895257764649a21cc7ed0eb277
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-11-03 19:45:03 +0100

    Format AutoGlowingTexture

--- src/main/java/team/cqr/cqrepoured/client/render/texture/AutoGlowingTexture.java
+++ src/main/java/team/cqr/cqrepoured/client/render/texture/AutoGlowingTexture.java
@@ -21,15 +21,15 @@ import team.cqr.cqrepoured.client.mcmetaserializers.GlowingMetadataSection;
 public class AutoGlowingTexture extends AbstractTexture {
 
 	private static final Logger LOGGER = LogManager.getLogger();
-	
+
 	protected final ResourceLocation originalTexture;
 	protected final ResourceLocation texture;
-	
+
 	public AutoGlowingTexture(ResourceLocation originalTexture, ResourceLocation texture) {
 		this.originalTexture = originalTexture;
 		this.texture = texture;
 	}
-	
+
 	public static ResourceLocation get(ResourceLocation originalTexture) {
 		String path = originalTexture.getPath();
 		int i = path.lastIndexOf('.');
@@ -40,13 +40,13 @@ public class AutoGlowingTexture extends AbstractTexture {
 		}
 		return glowingTexture;
 	}
-	
+
 	@Override
 	public void loadTexture(IResourceManager resourceManager) throws IOException {
 		this.deleteGlTexture();
 
 		try (IResource iresource = resourceManager.getResource(this.originalTexture)) {
-			//Needed to get the GL-texture id
+			// Needed to get the GL-texture id
 			ITextureObject ito = Minecraft.getMinecraft().renderEngine.getTexture(iresource.getResourceLocation());
 			BufferedImage bufferedimage = TextureUtil.readBufferedImage(iresource.getInputStream());
 			BufferedImage glowingBI = new BufferedImage(bufferedimage.getWidth(), bufferedimage.getHeight(), bufferedimage.getType());
@@ -56,41 +56,41 @@ public class AutoGlowingTexture extends AbstractTexture {
 
 			if (iresource.hasMetadata()) {
 				try {
-					//DONE: Fix this for the CTS!! Cts for whatever reason tries to load png as mcmeta file...
+					// DONE: Fix this for the CTS!! Cts for whatever reason tries to load png as mcmeta file...
 					TextureMetadataSection texturemetadatasection = (TextureMetadataSection) iresource.getMetadata("texture");
 
 					if (texturemetadatasection != null) {
 						flag = texturemetadatasection.getTextureBlur();
 						flag1 = texturemetadatasection.getTextureClamp();
 					}
-					
+
 					GlowingMetadataSection glowInformation = (GlowingMetadataSection) iresource.getMetadata("glowsections");
-					if(glowInformation != null) {
-						for(Tuple<Tuple<Integer, Integer>, Tuple<Integer, Integer>> area : glowInformation.getGlowingSections()) {
-							for(int ix = area.getFirst().getFirst(); ix < area.getSecond().getFirst(); ix++) {
-								for(int iy = area.getFirst().getSecond(); iy < area.getSecond().getSecond(); iy++) {
-									final int rgbEmpty = glowingBI.getRGB(ix, iy);
+					if (glowInformation != null) {
+						for (Tuple<Tuple<Integer, Integer>, Tuple<Integer, Integer>> area : glowInformation.getGlowingSections()) {
+							for (int ix = area.getFirst().getFirst(); ix < area.getSecond().getFirst(); ix++) {
+								for (int iy = area.getFirst().getSecond(); iy < area.getSecond().getSecond(); iy++) {
 									glowingBI.setRGB(ix, iy, bufferedimage.getRGB(ix, iy));
-									
-									//Remove it from the original
-									bufferedimage.setRGB(ix, iy, rgbEmpty);
+
+									// Remove it from the original
+									bufferedimage.setRGB(ix, iy, 0);
 								}
 							}
 						}
 					}
-					
-					/*String name = this.texture.getPath().replace("/", "-");
-					File outputFile = new File(CQRMain.CQ_CONFIG_FOLDER, name);
-					ImageIO.write(glowingBI, "png", outputFile);*/
-					
+
+					/*
+					 * String name = this.texture.getPath().replace("/", "-");
+					 * File outputFile = new File(CQRMain.CQ_CONFIG_FOLDER, name);
+					 * ImageIO.write(glowingBI, "png", outputFile);
+					 */
 				} catch (RuntimeException runtimeexception) {
 					LOGGER.warn("Failed reading metadata of: {}", this.originalTexture, runtimeexception);
 				}
 			}
 
 			TextureUtil.uploadTextureImageAllocate(this.getGlTextureId(), glowingBI, flag, flag1);
-		
-			//Also upload the changes to the original texture...
+
+			// Also upload the changes to the original texture...
 			TextureUtil.uploadTextureImage(ito.getGlTextureId(), bufferedimage);
 		}
 	}
