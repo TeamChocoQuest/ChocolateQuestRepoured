ac008397c265738ac08a3e6a2029bf5ed82af9f5
DerToaster98 <dertoaster@cq-repoured.net>
2021-11-09 17:50:02 +0100

    only set entity on fire if it was actually attacked

--- src/main/java/team/cqr/cqrepoured/objects/entity/projectiles/ProjectileBullet.java
+++ src/main/java/team/cqr/cqrepoured/objects/entity/projectiles/ProjectileBullet.java
@@ -52,8 +52,10 @@ public class ProjectileBullet extends ProjectileBase implements IEntityAdditiona
 						damage += 5.0F;
 					} else if (this.type == 4) {
 						damage += 5.0F;
-						entity.setFire(3);
-						entity.attackEntityFrom(new EntityDamageSourceIndirect("onFire", this, this.thrower).setFireDamage(), damage / 2);
+						
+						if(entity.attackEntityFrom(new EntityDamageSourceIndirect("onFire", this, this.thrower).setFireDamage(), damage / 2)) {
+							entity.setFire(3);
+						}
 					}
 					if (EntityUtil.isEntityFlying(entity)) {
 						damage *= 2;
