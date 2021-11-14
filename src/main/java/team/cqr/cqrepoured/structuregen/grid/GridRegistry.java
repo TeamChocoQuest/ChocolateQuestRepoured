09b25752004e43fca6be271fa0d7f121168a3424
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-11-14 03:12:31 +0100

    Return unmodifiable collection

--- src/main/java/team/cqr/cqrepoured/structuregen/grid/GridRegistry.java
+++ src/main/java/team/cqr/cqrepoured/structuregen/grid/GridRegistry.java
@@ -3,6 +3,7 @@ package team.cqr.cqrepoured.structuregen.grid;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -66,7 +67,7 @@ public class GridRegistry {
 	}
 
 	public Collection<DungeonGrid> getGrids() {
-		return this.name2grid.values();
+		return Collections.unmodifiableCollection(this.grids);
 	}
 
 }
