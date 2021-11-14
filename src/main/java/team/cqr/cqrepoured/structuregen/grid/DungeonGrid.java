8ea87984f12317986f76e948ea958e21c1d7448f
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-11-14 03:24:53 +0100

    Clean up dungeon grid

--- src/main/java/team/cqr/cqrepoured/structuregen/grid/DungeonGrid.java
+++ src/main/java/team/cqr/cqrepoured/structuregen/grid/DungeonGrid.java
@@ -1,6 +1,7 @@
 package team.cqr.cqrepoured.structuregen.grid;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Properties;
@@ -32,45 +33,30 @@ import team.cqr.cqrepoured.util.VanillaStructureHelper;
 public class DungeonGrid {
 
 	private final String name;
-	
 	private final List<DungeonBase> dungeons;
 	private int distance;
 	private int spread;
 	private double rarityFactor;
 	private int checkRadiusInChunks;
-
-	// chance to generate a dungeon at a grid position
 	private int chance;
-	// TODO used to determine which grid comes first
 	private int priority;
-	// TODO the index of all grids which is used to calculate the seed
-	private int id;
-	// different seed for each grid (used to calculate dungeon spread)
-	private int seed = new Random(id).nextInt();
-	// TODO Maybe also include grid offset based on id?
 	private int offset;
-	
-	public String getName() {
-		return this.name;
-	}
-	
+	private int id;
+	private int seed;
+
 	public DungeonGrid(final String name, Properties properties) {
 		this.name = name;
-		this.distance = PropertyFileHelper.getIntProperty(properties, "distance", CQRConfig.general.dungeonSeparation);
-		this.spread = PropertyFileHelper.getIntProperty(properties, "spread", CQRConfig.general.dungeonSpread);
-		this.rarityFactor = PropertyFileHelper.getDoubleProperty(properties, "rarityFactor", CQRConfig.general.dungeonRarityFactor);
-		this.chance = PropertyFileHelper.getIntProperty(properties, "chance", CQRConfig.general.overallDungeonChance);
-		this.priority = PropertyFileHelper.getIntProperty(properties, "priority", 10);
+		this.distance = PropertyFileHelper.getIntProperty(properties, "distance", 20);
+		this.spread = PropertyFileHelper.getIntProperty(properties, "spread", 10);
+		this.rarityFactor = PropertyFileHelper.getDoubleProperty(properties, "rarityFactor", 0.0D);
 		this.checkRadiusInChunks = PropertyFileHelper.getIntProperty(properties, "checkRadius", 4);
+		this.chance = PropertyFileHelper.getIntProperty(properties, "chance", 100);
+		this.priority = PropertyFileHelper.getIntProperty(properties, "priority", 10);
 		this.offset = PropertyFileHelper.getIntProperty(properties, "offset", 0);
 		this.dungeons = Arrays.stream(PropertyFileHelper.getStringArrayProperty(properties, "dungeons", new String[0], true))
 				.map(s -> DungeonRegistry.getInstance().getDungeon(s)).filter(Objects::nonNull).collect(Collectors.toList());
 	}
-	
-	public List<DungeonBase> getDungeons() {
-		return this.dungeons;
-	}
-	
+
 	@Nullable
 	public DungeonBase getDungeonAt(World world, int chunkX, int chunkZ) {
 		return this.getDungeonAt(world, chunkX, chunkZ, Predicates.alwaysTrue());
@@ -96,8 +82,8 @@ public class DungeonGrid {
 
 		int weight = dungeon.getWeight();
 		int totalWeight = possibleDungeons.getTotalWeight();
-		double chanceModifier = 1.0D / Math.pow((double) weight / (double) totalWeight, rarityFactor);
-		if (!DungeonGenUtils.percentageRandom((double) dungeon.getChance() / 100.0D * chanceModifier, random)) {
+		double chanceModifier = 1.0D / Math.pow((double) weight / (double) totalWeight, this.rarityFactor);
+		if (!DungeonGenUtils.percentageRandom(dungeon.getChance() / 100.0D * chanceModifier, random)) {
 			log(world, chunkX, chunkZ, "Specific dungeon generation chance check failed for dungeon: %s", dungeon);
 			return null;
 		}
@@ -105,33 +91,6 @@ public class DungeonGrid {
 		return dungeon;
 	}
 
-	/**
-	 * @return true when the passed chunk coords are on the dungeon grid
-	 */
-	public boolean canSpawnStructureAtCoords(World world, int chunkX, int chunkZ) {
-		int dungeonSeparation = this.getDistance();
-		// Check whether this chunk is farther north than the wall
-		if (CQRConfig.wall.enabled && chunkZ < -CQRConfig.wall.distance && CQRConfig.general.moreDungeonsBehindWall) {
-			dungeonSeparation = MathHelper.ceil((double) dungeonSeparation / CQRConfig.general.densityBehindWallFactor);
-		}
-		int dungeonSpread = Math.min(this.getSpread() + 1, dungeonSeparation);
-
-		int cx = chunkX + offset - (DungeonGenUtils.getSpawnX(world) >> 4);
-		int cz = chunkZ + offset - (DungeonGenUtils.getSpawnZ(world) >> 4);
-		if (dungeonSpread <= 1) {
-			return cx % dungeonSeparation == 0 && cz % dungeonSeparation == 0;
-		}
-
-		int x = MathHelper.intFloorDiv(cx, dungeonSeparation);
-		int z = MathHelper.intFloorDiv(cz, dungeonSeparation);
-		Random random = world.setRandomSeed(x, z, seed);
-		x *= dungeonSeparation;
-		z *= dungeonSeparation;
-		x += random.nextInt(dungeonSpread);
-		z += random.nextInt(dungeonSpread);
-		return x == cx && z == cz;
-	}
-	
 	/**
 	 * Checks if<br>
 	 * - the chunk coords are on the dungeon grid<br>
@@ -143,7 +102,7 @@ public class DungeonGrid {
 	 */
 	public boolean canSpawnDungeonAtCoords(World world, int chunkX, int chunkZ, Random random) {
 		// Check if the chunk is on the grid
-		if (!canSpawnStructureAtCoords(world, chunkX, chunkZ)) {
+		if (!this.isChunkOnGrid(world, chunkX, chunkZ)) {
 			return false;
 		}
 
@@ -157,7 +116,34 @@ public class DungeonGrid {
 			return false;
 		}
 
-		return !isOtherStructureNearby(world, chunkX, chunkZ);
+		return !this.isOtherStructureNearby(world, chunkX, chunkZ);
+	}
+
+	/**
+	 * @return true when the passed chunk coords are on the dungeon grid
+	 */
+	public boolean isChunkOnGrid(World world, int chunkX, int chunkZ) {
+		int dungeonSeparation = this.getDistance();
+		// Check whether this chunk is farther north than the wall
+		if (CQRConfig.wall.enabled && chunkZ < -CQRConfig.wall.distance && CQRConfig.general.moreDungeonsBehindWall) {
+			dungeonSeparation = MathHelper.ceil(dungeonSeparation / CQRConfig.general.densityBehindWallFactor);
+		}
+		int dungeonSpread = Math.min(this.getSpread() + 1, dungeonSeparation);
+
+		int cx = chunkX + this.offset - (DungeonGenUtils.getSpawnX(world) >> 4);
+		int cz = chunkZ + this.offset - (DungeonGenUtils.getSpawnZ(world) >> 4);
+		if (dungeonSpread <= 1) {
+			return cx % dungeonSeparation == 0 && cz % dungeonSeparation == 0;
+		}
+
+		int x = MathHelper.intFloorDiv(cx, dungeonSeparation);
+		int z = MathHelper.intFloorDiv(cz, dungeonSeparation);
+		Random random = world.setRandomSeed(x, z, this.seed);
+		x *= dungeonSeparation;
+		z *= dungeonSeparation;
+		x += random.nextInt(dungeonSpread);
+		z += random.nextInt(dungeonSpread);
+		return x == cx && z == cz;
 	}
 
 	/**
@@ -205,7 +191,7 @@ public class DungeonGrid {
 
 		return false;
 	}
-	
+
 	private static void log(World world, int chunkX, int chunkZ, String message, Object... params) {
 		if (!CQRConfig.advanced.debugDungeonGen) {
 			return;
@@ -213,8 +199,8 @@ public class DungeonGrid {
 		CQRMain.logger.info("Failed to generate structure at x={} z={} dim={}: {}", (chunkX << 4) + 8, (chunkZ << 4) + 8, world.provider.getDimension(),
 				String.format(message, params));
 	}
-	
-	public CQRWeightedRandom<DungeonBase> getDungeonsForPos(World world, Biome biome, int chunkX, int chunkZ) {
+
+	private CQRWeightedRandom<DungeonBase> getDungeonsForPos(World world, Biome biome, int chunkX, int chunkZ) {
 		CQRWeightedRandom<DungeonBase> dungeonsForChunk = new CQRWeightedRandom<>();
 
 		for (DungeonBase dungeon : this.dungeons) {
@@ -226,26 +212,82 @@ public class DungeonGrid {
 		return dungeonsForChunk;
 	}
 
+	public String getName() {
+		return this.name;
+	}
+
+	public List<DungeonBase> getDungeons() {
+		return Collections.unmodifiableList(this.dungeons);
+	}
+
 	public int getDistance() {
 		return this.distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
 	}
 
 	public int getSpread() {
 		return this.spread;
 	}
-	
+
+	public void setSpread(int spread) {
+		this.spread = spread;
+	}
+
 	public double getRarityFactor() {
 		return this.rarityFactor;
 	}
 
+	public void setRarityFactor(double rarityFactor) {
+		this.rarityFactor = rarityFactor;
+	}
+
+	public int getCheckRadiusInChunks() {
+		return this.checkRadiusInChunks;
+	}
+
+	public void setCheckRadiusInChunks(int checkRadiusInChunks) {
+		this.checkRadiusInChunks = checkRadiusInChunks;
+	}
+
+	public int getChance() {
+		return this.chance;
+	}
+
+	public void setChance(int chance) {
+		this.chance = chance;
+	}
+
 	public int getPriority() {
-		return priority;
+		return this.priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	public int getId() {
+		return this.id;
 	}
 
 	public void setId(int id) {
 		this.id = id;
 		ThreadLocalRandom.current().setSeed(id);
 		this.seed = ThreadLocalRandom.current().nextInt();
+	}
+
+	public int getSeed() {
+		return this.seed;
+	}
+
+	public int getOffset() {
+		return this.offset;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
 	}
 
 }
