d47a7f80b97abbfc50b15bb91252ef62b7391e86
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-11-17 22:24:00 +0100

    Update map generation to not load unnecessary chunks

--- src/main/java/team/cqr/cqrepoured/structuregen/generation/generatable/GeneratableMapInfo.java
+++ src/main/java/team/cqr/cqrepoured/structuregen/generation/generatable/GeneratableMapInfo.java
@@ -19,6 +19,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
@@ -63,7 +64,7 @@ public class GeneratableMapInfo extends GeneratablePosInfo {
 		boolean flag = BlockPlacingHelper.setBlockState(world, chunk, blockStorage, pos, Blocks.AIR.getDefaultState(), null, 16);
 		ItemStack stack = ItemMap.setupNewMap(world, this.mapX, this.mapZ, this.scale, true, true);
 		if (this.fillMap) {
-			updateMapData(world, this.mapX, this.mapZ, this.fillRadius, ((ItemMap) stack.getItem()).getMapData(stack, world));
+			updateMapData(world, pos.getX(), pos.getZ(), this.fillRadius, ((ItemMap) stack.getItem()).getMapData(stack, world));
 		}
 		this.entity.setDisplayedItem(stack);
 		world.spawnEntity(this.entity);
@@ -81,124 +82,125 @@ public class GeneratableMapInfo extends GeneratablePosInfo {
 	}
 
 	private static void updateMapData(World worldIn, int x, int z, int radius, MapData data) {
-		if (worldIn.provider.getDimension() == data.dimension) {
-			int scale = 1 << data.scale;
-			int xCenter = data.xCenter;
-			int zCenter = data.zCenter;
+		if (worldIn.provider.getDimension() != data.dimension) {
+			return;
+		}
 
-			Multiset<MapColor> multiset = HashMultiset.<MapColor>create();
-			BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
+		for (int ix = 0; ix < 128; ix++) {
+			int worldX = data.xCenter + (ix - 64) * (1 << data.scale);
 
-			for (int k1 = 0; k1 < 128; k1++) {
-				double d0 = 0.0D;
+			for (int iz = 0; iz < 128; iz++) {
+				int worldZ = data.zCenter + (iz - 64) * (1 << data.scale);
 
-				for (int l1 = -1; l1 < 128; l1++) {
-					int k2 = xCenter + (k1 - 64) * scale;
-					int l2 = zCenter + (l1 - 64) * scale;
-					multiset.clear();
-					Chunk chunk = worldIn.getChunk(k2 >> 4, l2 >> 4);
+				if (!DungeonGenUtils.isInsideCircle(worldX - x, worldZ - z, radius)) {
+					continue;
+				}
 
-					if (!chunk.isEmpty()) {
-						int i3 = k2 & 15;
-						int j3 = l2 & 15;
-						int k3 = 0;
-						double d1 = 0.0D;
+				updateMapColorAt(worldIn, ix, iz, data);
+			}
+		}
+	}
 
-						if (worldIn.provider.isNether()) {
-							int l3 = k2 + l2 * 231871;
-							l3 = l3 * l3 * 31287121 + l3 * 11;
+	private static void updateMapColorAt(World world, int mapX, int mapZ, MapData data) {
+		int i = 1 << data.scale;
+		double d0 = 0.0D;
+		int k2 = (data.xCenter / i + mapX - 64) * i;
+		int l2 = (data.zCenter / i + mapZ - 64) * i;
+		Multiset<MapColor> multiset = HashMultiset.create();
+		Chunk chunk = world.getChunk(k2 >> 4, l2 >> 4);
+		int i3 = k2 & 15;
+		int j3 = l2 & 15;
+		int k3 = 0;
+		double d1 = 0.0D;
 
-							if ((l3 >> 20 & 1) == 0) {
-								multiset.add(Blocks.DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.DIRT).getMapColor(worldIn,
-										BlockPos.ORIGIN), 10);
-							} else {
-								multiset.add(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.STONE).getMapColor(worldIn,
-										BlockPos.ORIGIN), 100);
+		if (world.provider.isNether()) {
+			int l3 = k2 + l2 * 231871;
+			l3 = l3 * l3 * 31287121 + l3 * 11;
+
+			if ((l3 >> 20 & 1) == 0) {
+				multiset.add(Blocks.DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.DIRT).getMapColor(world, BlockPos.ORIGIN), 10);
+			} else {
+				multiset.add(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.STONE).getMapColor(world, BlockPos.ORIGIN),
+						100);
+			}
+
+			d1 = 100.0D;
+		} else {
+			MutableBlockPos pos = new MutableBlockPos();
+
+			for (int i4 = 0; i4 < i; ++i4) {
+				for (int j4 = 0; j4 < i; ++j4) {
+					int k4 = chunk.getHeightValue(i4 + i3, j4 + j3) + 1;
+					IBlockState state = Blocks.AIR.getDefaultState();
+
+					if (k4 <= 1) {
+						state = Blocks.BEDROCK.getDefaultState();
+					} else {
+						label175: {
+							while (true) {
+								--k4;
+								state = chunk.getBlockState(i4 + i3, k4, j4 + j3);
+								pos.setPos((chunk.x << 4) + i4 + i3, k4, (chunk.z << 4) + j4 + j3);
+
+								if (state.getMapColor(world, pos) != MapColor.AIR || k4 <= 0) {
+									break;
+								}
 							}
 
-							d1 = 100.0D;
-						} else {
-							for (int i4 = 0; i4 < scale; ++i4) {
-								for (int j4 = 0; j4 < scale; ++j4) {
-									int k4 = chunk.getHeightValue(i4 + i3, j4 + j3) + 1;
-									IBlockState iblockstate = Blocks.AIR.getDefaultState();
+							if (k4 > 0 && state.getMaterial().isLiquid()) {
+								int l4 = k4 - 1;
 
-									if (k4 <= 1) {
-										iblockstate = Blocks.BEDROCK.getDefaultState();
-									} else {
-										label175: {
-											while (true) {
-												--k4;
-												iblockstate = chunk.getBlockState(i4 + i3, k4, j4 + j3);
-												mutablePos.setPos((chunk.x << 4) + i4 + i3, k4, (chunk.z << 4) + j4 + j3);
+								while (true) {
+									IBlockState iblockstate1 = chunk.getBlockState(i4 + i3, l4--, j4 + j3);
+									++k3;
 
-												if (iblockstate.getMapColor(worldIn, mutablePos) != MapColor.AIR || k4 <= 0) {
-													break;
-												}
-											}
-
-											if (k4 > 0 && iblockstate.getMaterial().isLiquid()) {
-												int l4 = k4 - 1;
-
-												while (true) {
-													IBlockState iblockstate1 = chunk.getBlockState(i4 + i3, l4--, j4 + j3);
-													++k3;
-
-													if (l4 <= 0 || !iblockstate1.getMaterial().isLiquid()) {
-														break label175;
-													}
-												}
-											}
-										}
+									if (l4 <= 0 || !iblockstate1.getMaterial().isLiquid()) {
+										break label175;
 									}
-
-									d1 += (double) k4 / (double) (scale * scale);
-									multiset.add(iblockstate.getMapColor(worldIn, mutablePos));
 								}
 							}
 						}
-
-						k3 = k3 / (scale * scale);
-						double d2 = (d1 - d0) * 4.0D / (scale + 4) + ((k1 + l1 & 1) - 0.5D) * 0.4D;
-						int i5 = 1;
-
-						if (d2 > 0.6D) {
-							i5 = 2;
-						}
-
-						if (d2 < -0.6D) {
-							i5 = 0;
-						}
-
-						MapColor mapcolor = Iterables.getFirst(Multisets.copyHighestCountFirst(multiset), MapColor.AIR);
-
-						if (mapcolor == MapColor.WATER) {
-							d2 = k3 * 0.1D + (k1 + l1 & 1) * 0.2D;
-							i5 = 1;
-
-							if (d2 < 0.5D) {
-								i5 = 2;
-							}
-
-							if (d2 > 0.9D) {
-								i5 = 0;
-							}
-						}
-
-						d0 = d1;
-
-						if (l1 >= 0 && DungeonGenUtils.isInsideCircle(k2 - x, l2 - z, radius)) {
-							byte b0 = data.colors[k1 + l1 * 128];
-							byte b1 = (byte) (mapcolor.colorIndex * 4 + i5);
-
-							if (b0 != b1) {
-								data.colors[k1 + l1 * 128] = b1;
-								data.updateMapData(k1, l1);
-							}
-						}
 					}
+
+					d1 += (double) k4 / (double) (i * i);
+					multiset.add(state.getMapColor(world, pos));
 				}
 			}
+		}
+
+		k3 = k3 / (i * i);
+		double d2 = (d1 - d0) * 4.0D / (i + 4) + ((mapX + mapZ & 1) - 0.5D) * 0.4D;
+		int i5 = 1;
+
+		if (d2 > 0.6D) {
+			i5 = 2;
+		}
+
+		if (d2 < -0.6D) {
+			i5 = 0;
+		}
+
+		MapColor mapcolor = Iterables.getFirst(Multisets.copyHighestCountFirst(multiset), MapColor.AIR);
+
+		if (mapcolor == MapColor.WATER) {
+			d2 = k3 * 0.1D + (mapX + mapZ & 1) * 0.2D;
+			i5 = 1;
+
+			if (d2 < 0.5D) {
+				i5 = 2;
+			}
+
+			if (d2 > 0.9D) {
+				i5 = 0;
+			}
+		}
+
+		byte b0 = data.colors[mapX + mapZ * 128];
+		byte b1 = (byte) (mapcolor.colorIndex * 4 + i5);
+
+		if (b0 != b1) {
+			data.colors[mapX + mapZ * 128] = b1;
+			data.updateMapData(mapX, mapZ);
 		}
 	}
 
