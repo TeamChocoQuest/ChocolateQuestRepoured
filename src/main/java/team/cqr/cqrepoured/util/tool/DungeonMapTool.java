3a40772db4983299c4bfe408768fe76145be5fc8
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-11-19 18:30:27 +0100

    Update DungeonMapTool.java

--- src/main/java/team/cqr/cqrepoured/util/tool/DungeonMapTool.java
+++ src/main/java/team/cqr/cqrepoured/util/tool/DungeonMapTool.java
@@ -65,7 +65,8 @@ public class DungeonMapTool {
 		}
 	}).toArray(BufferedImage[]::new);
 
-	public static void run(int radiusC, long seedIn, int distanceIn, int spreadIn, double rarityFactorIn, boolean generateBiomes) {
+	public static void run(int radiusC, long seedIn, int distanceIn, int spreadIn, double rarityFactorIn,
+			boolean generateBiomes) {
 		try {
 			// TODO move these elsewhere
 			boolean exportDungeonCounts = false;
@@ -120,7 +121,7 @@ public class DungeonMapTool {
 						}
 					}
 				}
-	
+
 				for (int x = 0; x < 16; x++) {
 					int ix = x + radiusB + spawnX;
 					for (int z = 0; z < 16; z++) {
@@ -141,7 +142,7 @@ public class DungeonMapTool {
 			for (int x = -radiusC; x <= radiusC; x++) {
 				for (int z = -radiusC; z <= radiusC; z++) {
 					// TODO adjust gui to allow modification of all grids
-					//WorldDungeonGenerator.setup(distanceIn, spreadIn, rarityFactorIn, false);
+					// WorldDungeonGenerator.setup(distanceIn, spreadIn, rarityFactorIn, false);
 					DungeonBase dungeonAtPos = WorldDungeonGenerator.getDungeonAt(world, x, z);
 
 					if (dungeonAtPos != null) {
@@ -171,11 +172,12 @@ public class DungeonMapTool {
 							Graphics2D graphics = bufferedImage.createGraphics();
 							graphics.setColor(Color.BLACK);
 							graphics.setFont(new Font("Arial", Font.BOLD, 24));
-							graphics.drawString(dungeonAtPos.getDungeonName(), (x + radiusC << 4) + 8 - 9 * scale, (z + radiusC << 4) + 8 - 10 * scale);
+							graphics.drawString(dungeonAtPos.getDungeonName(), (x + radiusC << 4) + 8 - 9 * scale,
+									(z + radiusC << 4) + 8 - 10 * scale);
 						}
 					}
-					//Now, reset to default
-					//WorldDungeonGenerator.setup(null, null, null, true);
+					// Now, reset to default
+					// WorldDungeonGenerator.setup(null, null, null, true);
 				}
 			}
 
@@ -186,7 +188,8 @@ public class DungeonMapTool {
 				ImageIO.write(bufferedImage, "png", new File("dungeon_map.png"));
 			} else {
 				File file = new File("dungeon_count.prop");
-				if (!file.exists()) file.createNewFile();
+				if (!file.exists())
+					file.createNewFile();
 				Properties prop = new Properties();
 				if (overrideOldDungeonCounts) {
 					try (InputStream in = new BufferedInputStream(new FileInputStream(file))) {
@@ -195,7 +198,8 @@ public class DungeonMapTool {
 				}
 				dungeonCountMap.object2IntEntrySet().forEach(e -> {
 					String k = e.getKey().getDungeonName();
-					prop.setProperty(k, Integer.toString(PropertyFileHelper.getIntProperty(prop, k, 0) + e.getIntValue()));
+					prop.setProperty(k,
+							Integer.toString(PropertyFileHelper.getIntProperty(prop, k, 0) + e.getIntValue()));
 				});
 				try (OutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
 					prop.store(out, null);
@@ -319,8 +323,8 @@ public class DungeonMapTool {
 		public final int l;
 
 		protected DummyWorld(WorldSettings settings, int dimension, int x, int z, int w, int l) {
-			super(new SaveHandlerMP(), new WorldInfo(settings, "MpServer"), DimensionManager.getProviderType(dimension).createDimension(), new Profiler(),
-					true);
+			super(new SaveHandlerMP(), new WorldInfo(settings, "MpServer"),
+					DimensionManager.getProviderType(dimension).createDimension(), new Profiler(), true);
 			this.provider.setWorld(this);
 			hardResetIntCache();
 			this.biomes = this.provider.getBiomeProvider().getBiomes(null, x, z, w, l, false);
