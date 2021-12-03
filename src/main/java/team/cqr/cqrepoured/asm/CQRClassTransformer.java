ec2d27bef8de7f7e237efb3d9fa1e45ed874eaf7
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-12-03 23:34:35 +0100

    Change creature attribute of vanilla mobs

--- src/main/java/team/cqr/cqrepoured/asm/CQRClassTransformer.java
+++ src/main/java/team/cqr/cqrepoured/asm/CQRClassTransformer.java
@@ -2,10 +2,12 @@ package team.cqr.cqrepoured.asm;
 
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.JumpInsnNode;
 import org.objectweb.asm.tree.LabelNode;
 import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.VarInsnNode;
 
 import meldexun.asmutil.ASMUtil;
@@ -30,6 +32,30 @@ public class CQRClassTransformer extends AbstractClassTransformer implements ICl
 				new InsnNode(Opcodes.IRETURN),
 				popNode1
 			));
+		});
+		this.registerClassTransformer("", "net/minecraft/entity/monster/EntityEnderman", classNode -> {
+			MethodNode methodGetCreatureAttribute = new MethodNode(Opcodes.ACC_PUBLIC, "getCreatureAttribute", "()Lnet/minecraft/entity/EnumCreatureAttribute;", null, null);
+			methodGetCreatureAttribute.instructions.insert(ASMUtil.listOf(
+				new FieldInsnNode(Opcodes.GETSTATIC, "team/cqr/cqrepoured/init/CQRCreatureAttributes", "VOID", "Lnet/minecraft/entity/EnumCreatureAttribute;"),
+				new InsnNode(Opcodes.ARETURN)
+			));
+			classNode.methods.add(methodGetCreatureAttribute);
+		});
+		this.registerClassTransformer("", "net/minecraft/entity/boss/EntityDragon", classNode -> {
+			MethodNode methodGetCreatureAttribute = new MethodNode(Opcodes.ACC_PUBLIC, "getCreatureAttribute", "()Lnet/minecraft/entity/EnumCreatureAttribute;", null, null);
+			methodGetCreatureAttribute.instructions.insert(ASMUtil.listOf(
+				new FieldInsnNode(Opcodes.GETSTATIC, "team/cqr/cqrepoured/init/CQRCreatureAttributes", "VOID", "Lnet/minecraft/entity/EnumCreatureAttribute;"),
+				new InsnNode(Opcodes.ARETURN)
+			));
+			classNode.methods.add(methodGetCreatureAttribute);
+		});
+		this.registerClassTransformer("", "net/minecraft/entity/monster/EntityShulker", classNode -> {
+			MethodNode methodGetCreatureAttribute = new MethodNode(Opcodes.ACC_PUBLIC, "getCreatureAttribute", "()Lnet/minecraft/entity/EnumCreatureAttribute;", null, null);
+			methodGetCreatureAttribute.instructions.insert(ASMUtil.listOf(
+				new FieldInsnNode(Opcodes.GETSTATIC, "team/cqr/cqrepoured/init/CQRCreatureAttributes", "VOID", "Lnet/minecraft/entity/EnumCreatureAttribute;"),
+				new InsnNode(Opcodes.ARETURN)
+			));
+			classNode.methods.add(methodGetCreatureAttribute);
 		});
 		// @formatter:on
 	}
