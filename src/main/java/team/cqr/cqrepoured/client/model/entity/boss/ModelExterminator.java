31ed59cdb93454b1ba488703ffe6a9daefb3c908
DerToaster98 <seiler.oli@web.de>
2021-12-14 18:16:58 +0100

    forgot this one

--- src/main/java/team/cqr/cqrepoured/client/model/entity/boss/ModelExterminator.java
+++ src/main/java/team/cqr/cqrepoured/client/model/entity/boss/ModelExterminator.java
@@ -3,42 +3,22 @@ package team.cqr.cqrepoured.client.model.entity.boss;
 import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.processor.IBone;
-import software.bernie.geckolib3.model.AnimatedGeoModel;
 import team.cqr.cqrepoured.CQRMain;
+import team.cqr.cqrepoured.client.model.geo.ModelGeoCQRBase;
 import team.cqr.cqrepoured.entity.boss.exterminator.EntityCQRExterminator;
 import team.cqr.cqrepoured.util.PartialTicksUtil;
 
-public class ModelExterminator extends AnimatedGeoModel<EntityCQRExterminator> {
+public class ModelExterminator extends ModelGeoCQRBase<EntityCQRExterminator> {
+
+	public ModelExterminator(ResourceLocation model, ResourceLocation textureDefault, String entityName) {
+		super(model, textureDefault, entityName);
+	}
 
 	static final ResourceLocation ANIMATION_RESLOC = new ResourceLocation(CQRMain.MODID, "animations/exterminator.animation.json");
-	static final ResourceLocation MODEL_RESLOC = new ResourceLocation(CQRMain.MODID, "geo/exterminator.geo.json");
-
-	private ResourceLocation texture;
-
-	public ModelExterminator(ResourceLocation texture) {
-		super();
-		this.texture = texture;
-	}
 
 	@Override
 	public ResourceLocation getAnimationFileLocation(EntityCQRExterminator animatable) {
 		return ANIMATION_RESLOC;
-	}
-
-	@Override
-	public ResourceLocation getModelLocation(EntityCQRExterminator object) {
-		return MODEL_RESLOC;
-	}
-
-	@Override
-	public ResourceLocation getTextureLocation(EntityCQRExterminator entity) {
-		// Custom texture start
-		if (entity.hasTextureOverride()) {
-			return entity.getTextureOverride();
-		}
-		// Custom texture end
-		return entity.getTextureCount() > 1 ? new ResourceLocation(CQRMain.MODID, "textures/entity/boss/exterminator_" + entity.getTextureIndex() + ".png")
-				: this.texture;
 	}
 
 	private static final String BONE_IDENT_LEFT_LEG = "leg_left";
