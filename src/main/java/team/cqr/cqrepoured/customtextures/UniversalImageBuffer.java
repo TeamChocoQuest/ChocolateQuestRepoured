2fd20f1fde1d6166f52eb41c51318adb5101498d
DerToaster98 <seiler.oli@web.de>
2021-12-22 17:00:22 +0100

    remove unused code

--- src/main/java/team/cqr/cqrepoured/customtextures/UniversalImageBuffer.java
+++ src/main/java/team/cqr/cqrepoured/customtextures/UniversalImageBuffer.java
@@ -1,8 +1,6 @@
 package team.cqr.cqrepoured.customtextures;
 
-import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
 
 import javax.annotation.Nullable;
 
@@ -10,25 +8,10 @@ import net.minecraft.client.renderer.IImageBuffer;
 
 public class UniversalImageBuffer implements IImageBuffer {
 
-	private int imageWidth;
-	private int imageHeight;
-
 	@Override
 	@Nullable
 	public BufferedImage parseUserSkin(BufferedImage image) {
-		if (image == null) {
-			return null;
-		} else {
-			this.imageWidth = image.getWidth();
-			this.imageHeight = image.getHeight();
-			BufferedImage bufferedimage = new BufferedImage(this.imageWidth, this.imageHeight, 2);
-			Graphics graphics = bufferedimage.getGraphics();
-			graphics.drawImage(image, 0, 0, (ImageObserver) null);
-
-			graphics.dispose();
-
-			return bufferedimage;
-		}
+		return image;
 	}
 
 	@Override
