79c59d5cc3fbb6b828c9854a8fb945dc9997d30b
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2021-12-22 07:23:40 +0100

    Update LayerGlowingAreasGeo.java
    edited to match the structure of this here: https://github.com/bernie-https://github.com/bernie-g/geckolib/blob/1.12/src/main/java/software/bernie/example/client/layer/GeoExampleLayer.java (GLib example layer)

--- src/main/java/team/cqr/cqrepoured/client/render/entity/layers/geo/LayerGlowingAreasGeo.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/layers/geo/LayerGlowingAreasGeo.java
@@ -3,37 +3,42 @@ package team.cqr.cqrepoured.client.render.entity.layers.geo;
 import java.awt.Color;
 import java.util.function.Function;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib3.core.IAnimatable;
-import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
+import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 import team.cqr.cqrepoured.client.render.texture.AutoGlowingTexture;
 import team.cqr.cqrepoured.client.util.EmissiveUtil;
 
 public class LayerGlowingAreasGeo<T extends EntityLiving & IAnimatable> extends GeoLayerRenderer<T> {
 
-	protected final GeoEntityRenderer<T> renderer;
 	protected final Function<T, ResourceLocation> funcGetCurrentTexture;
 	protected final Function<T, ResourceLocation> funcGetCurrentModel;
 
 	public LayerGlowingAreasGeo(GeoEntityRenderer<T> renderer, Function<T, ResourceLocation> funcGetCurrentTexture, Function<T, ResourceLocation> funcGetCurrentModel) {
 		super(renderer);
-		this.renderer = renderer;
 		this.funcGetCurrentTexture = funcGetCurrentTexture;
 		this.funcGetCurrentModel = funcGetCurrentModel;
 	}
 
 	@Override
-	public void render(T entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+	public void render(T entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, Color renderColor) {
 
 		EmissiveUtil.preEmissiveTextureRendering();
 
-		this.renderer.bindTexture(AutoGlowingTexture.get(this.funcGetCurrentTexture.apply(entitylivingbaseIn)));
+		Minecraft.getMinecraft().getRenderManager().renderEngine.bindTexture(AutoGlowingTexture.get(this.funcGetCurrentTexture.apply(entitylivingbaseIn)));
 
-		Color renderColor = this.renderer.getRenderColor(entitylivingbaseIn, partialTicks);
-
-		this.renderer.render(this.getEntityModel().getModel(this.funcGetCurrentModel.apply(entitylivingbaseIn)), entitylivingbaseIn, partialTicks, renderColor.getRed(), renderColor.getGreen(), renderColor.getBlue(), renderColor.getAlpha());
+		this.entityRenderer.render(
+			this.entityRenderer.getGeoModelProvider().getModel(this.funcGetCurrentModel.apply(entitylivingbaseIn)), 
+			entitylivingbaseIn, 
+			partialTicks, 
+			(float) renderColor.getRed() / 255f, 
+			(float) renderColor.getBlue() / 255f,
+			(float) renderColor.getGreen() / 255f, 
+			(float) renderColor.getAlpha() / 255
+		);
 
 		EmissiveUtil.postEmissiveTextureRendering();
 	}
