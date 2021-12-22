4f8319a51b5f22263e60e87059c737605ae4876a
DerToaster98 <seiler.oli@web.de>
2021-12-22 15:24:32 +0100

    create base class for geo layers

---
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/layers/geo/AbstractCQRLayerGeo.java
@@ -0,0 +1,43 @@
+package team.cqr.cqrepoured.client.render.entity.layers.geo;
+
+import java.awt.Color;
+import java.util.function.Function;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
+
+public abstract class AbstractCQRLayerGeo<T extends EntityLiving & IAnimatable> extends GeoLayerRenderer<T> {
+	
+	protected final Function<T, ResourceLocation> funcGetCurrentTexture;
+	protected final Function<T, ResourceLocation> funcGetCurrentModel;
+
+	protected GeoEntityRenderer<T> geoRendererInstance;
+	
+	public AbstractCQRLayerGeo(GeoEntityRenderer<T> renderer, Function<T, ResourceLocation> funcGetCurrentTexture, Function<T, ResourceLocation> funcGetCurrentModel) {
+		super(renderer);
+		this.geoRendererInstance = renderer;
+		this.funcGetCurrentTexture = funcGetCurrentTexture;
+		this.funcGetCurrentModel = funcGetCurrentModel;
+	}
+	
+	@Override
+	public boolean shouldCombineTextures() {
+		return false;
+	}
+	
+	protected void reRenderCurrentModelInRenderer(T entity, float partialTicks, Color renderColor) {
+		this.entityRenderer.render(
+			this.getEntityModel().getModel(this.funcGetCurrentModel.apply(entity)), 
+			entity, 
+			partialTicks, 
+			(float) renderColor.getRed() / 255f, 
+			(float) renderColor.getBlue() / 255f,
+			(float) renderColor.getGreen() / 255f, 
+			(float) renderColor.getAlpha() / 255
+		);
+	}
+
+}
