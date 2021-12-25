0e4b5578ae90c2cd276c3a1f4e72b6795c2b7f7b
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-12-25 06:34:01 +0100

    Update illager model, texture

--- src/main/java/team/cqr/cqrepoured/client/model/entity/mobs/ModelCQRIllager.java
+++ src/main/java/team/cqr/cqrepoured/client/model/entity/mobs/ModelCQRIllager.java
@@ -5,49 +5,50 @@ import team.cqr.cqrepoured.client.model.entity.ModelCQRBiped;
 
 public class ModelCQRIllager extends ModelCQRBiped {
 
-	private final ModelRenderer nose;
+	public ModelRenderer nose;
+	public ModelRenderer bodyOverlay;
 
 	public ModelCQRIllager() {
-		super(64, 64);
+		super(128, 64);
+	}
 
-		this.bipedHead = new ModelRenderer(this);
+	@Override
+	protected void initModelParts() {
+		super.initModelParts();
+
+		this.bipedHead = new ModelRenderer(this, 0, 0);
+		this.bipedHead.addBox(-4.0F, -10.0F, -4.0F, 8, 10, 8, 0.0F);
 		this.bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
-		this.bipedHead.setTextureOffset(0, 0).addBox(-4.0F, -10.0F, -4.0F, 8, 10, 8, 0.0F);
 
-		this.bipedHeadwear = new ModelRenderer(this, 32, 0);
-		this.bipedHeadwear.addBox(-4.0F, -10.0F, -4.0F, 8, 12, 8, 0.0F + 0.45F);
-
-		this.bipedHead.addChild(this.bipedHeadwear);
-		this.bipedHeadwear.showModel = false;
-
-		this.nose = new ModelRenderer(this);
+		this.nose = new ModelRenderer(this, 0, 32);
+		this.nose.addBox(-1.0F, -1.0F, -6.0F, 2, 4, 2, 0.0F);
 		this.nose.setRotationPoint(0.0F, -2.0F, 0.0F);
-		this.nose.setTextureOffset(24, 0).addBox(-1.0F, -1.0F, -6.0F, 2, 4, 2, 0.0F);
-
 		this.bipedHead.addChild(this.nose);
 
-		this.bipedBody = new ModelRenderer(this);
+		this.bipedBody = new ModelRenderer(this, 32, 0);
+		this.bipedBody.addBox(-4.0F, 0.0F, -3.0F, 8, 12, 6, 0.0F);
 		this.bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F);
-		this.bipedBody.setTextureOffset(16, 20).addBox(-4.0F, 0.0F, -3.0F, 8, 12, 6, 0.0F);
-		this.bipedBody.setTextureOffset(0, 38).addBox(-4.0F, 0.0F, -3.0F, 8, 18, 6, 0.0F + 0.5F);
 
-		this.bipedRightLeg = new ModelRenderer(this, 0, 22);
-		this.bipedRightLeg.setRotationPoint(-2.0F, 12.0F, 0.0F);
-		this.bipedRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
+		this.bodyOverlay = new ModelRenderer(this, 8, 32);
+		this.bodyOverlay.addBox(-4.0F, 0.0F, -3.0F, 8, 18, 6, 0.5F);
+		this.bodyOverlay.setRotationPoint(0.0F, 0.0F, 0.0F);
+		this.bipedBody.addChild(this.bodyOverlay);
 
-		this.bipedLeftLeg = new ModelRenderer(this, 0, 22);
-		this.bipedLeftLeg.mirror = true;
-		this.bipedLeftLeg.setRotationPoint(2.0F, 12.0F, 0.0F);
-		this.bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
-
-		this.bipedRightArm = new ModelRenderer(this, 40, 46);
+		this.bipedRightArm = new ModelRenderer(this, 60, 0);
 		this.bipedRightArm.addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
 		this.bipedRightArm.setRotationPoint(-5.0F, 2.0F, 0.0F);
 
-		this.bipedLeftArm = new ModelRenderer(this, 40, 46);
-		this.bipedLeftArm.mirror = true;
+		this.bipedLeftArm = new ModelRenderer(this, 76, 0);
 		this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
 		this.bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
+
+		this.bipedRightLeg = new ModelRenderer(this, 92, 0);
+		this.bipedRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
+		this.bipedRightLeg.setRotationPoint(-2.0F, 12.0F, 0.0F);
+
+		this.bipedLeftLeg = new ModelRenderer(this, 108, 0);
+		this.bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
+		this.bipedLeftLeg.setRotationPoint(2.0F, 12.0F, 0.0F);
 	}
 
 }
