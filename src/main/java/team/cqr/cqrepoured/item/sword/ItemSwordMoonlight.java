bdef90111bb0fb0f207780a7d605099b3c3b9e78
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-12-27 03:37:35 +0100

    Update moonlight/sunshine implementation to use attribute modifiers

--- src/main/java/team/cqr/cqrepoured/item/sword/ItemSwordMoonlight.java
+++ src/main/java/team/cqr/cqrepoured/item/sword/ItemSwordMoonlight.java
@@ -1,6 +1,7 @@
 package team.cqr.cqrepoured.item.sword;
 
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.Nullable;
 
@@ -8,30 +9,58 @@ import org.lwjgl.input.Keyboard;
 
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import team.cqr.cqrepoured.item.IEquipListener;
 
-public class ItemSwordMoonlight extends ItemSword {
+public class ItemSwordMoonlight extends ItemSword implements IEquipListener {
+
+	protected static final UUID ATTACK_DAMAGE_MODIFIER = UUID.fromString("89ADDE87-B021-417C-9112-2E2C94CBB0D1");
+	private static final double DAMAGE_BONUS = 3.0D;
 
 	public ItemSwordMoonlight(ToolMaterial material) {
 		super(material);
 	}
 
 	@Override
-	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-		if (!target.world.isDaytime()) {
-			//Warning: This overrides the invincbility frame
-			target.hurtResistantTime = 0;
-			target.attackEntityFrom(DamageSource.GENERIC, 3.0F);
+	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+		super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
+		if (!(entityIn instanceof EntityLivingBase)) {
+			return;
 		}
+		if (!isSelected) {
+			return;
+		}
+		IAttributeInstance attribute = ((EntityLivingBase) entityIn).getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
+		if (!worldIn.isDaytime()) {
+			if (attribute.getModifier(ATTACK_DAMAGE_MODIFIER) != null) {
+				return;
+			}
+			attribute.applyModifier(new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "moonlight_damage_bonus", DAMAGE_BONUS, 0).setSaved(false));
+		} else {
+			attribute.removeModifier(ATTACK_DAMAGE_MODIFIER);
+		}
+	}
 
-		return super.hitEntity(stack, target, attacker);
+	@Override
+	public void onEquip(EntityLivingBase entity, ItemStack stack, EntityEquipmentSlot slot) {
+
+	}
+
+	@Override
+	public void onUnequip(EntityLivingBase entity, ItemStack stack, EntityEquipmentSlot slot) {
+		IAttributeInstance attribute = entity.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
+		attribute.removeModifier(ATTACK_DAMAGE_MODIFIER);
 	}
 
 	@Override
@@ -43,7 +72,7 @@ public class ItemSwordMoonlight extends ItemSword {
 			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
 		}
 
-		tooltip.add(TextFormatting.BLUE + "+3 " + I18n.format("description.attack_damage_at_night.name"));
+		tooltip.add(TextFormatting.DARK_AQUA + "+3 " + I18n.format("description.attack_damage_at_night.name"));
 	}
 
 }
--- src/main/java/team/cqr/cqrepoured/item/sword/ItemSwordSunshine.java
+++ src/main/java/team/cqr/cqrepoured/item/sword/ItemSwordSunshine.java
@@ -1,6 +1,7 @@
 package team.cqr.cqrepoured.item.sword;
 
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.Nullable;
 
@@ -8,29 +9,58 @@ import org.lwjgl.input.Keyboard;
 
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import team.cqr.cqrepoured.item.IEquipListener;
 
-public class ItemSwordSunshine extends ItemSword {
+public class ItemSwordSunshine extends ItemSword implements IEquipListener {
+
+	protected static final UUID ATTACK_DAMAGE_MODIFIER = UUID.fromString("126A7773-3B53-4AC4-8A90-1CD46C888FD4");
+	private static final double DAMAGE_BONUS = 3.0D;
 
 	public ItemSwordSunshine(ToolMaterial material) {
 		super(material);
 	}
 
 	@Override
-	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-		if (target.world.isDaytime()) {
-			//Warning: This overrides the invincbility frame
-			target.hurtResistantTime = 0;
-			target.attackEntityFrom(DamageSource.GENERIC, 3.0F);
+	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+		super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
+		if (!(entityIn instanceof EntityLivingBase)) {
+			return;
 		}
-		return super.hitEntity(stack, target, attacker);
+		if (!isSelected) {
+			return;
+		}
+		IAttributeInstance attribute = ((EntityLivingBase) entityIn).getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
+		if (worldIn.isDaytime()) {
+			if (attribute.getModifier(ATTACK_DAMAGE_MODIFIER) != null) {
+				return;
+			}
+			attribute.applyModifier(new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "sunshine_damage_bonus", DAMAGE_BONUS, 0).setSaved(false));
+		} else {
+			attribute.removeModifier(ATTACK_DAMAGE_MODIFIER);
+		}
+	}
+
+	@Override
+	public void onEquip(EntityLivingBase entity, ItemStack stack, EntityEquipmentSlot slot) {
+
+	}
+
+	@Override
+	public void onUnequip(EntityLivingBase entity, ItemStack stack, EntityEquipmentSlot slot) {
+		IAttributeInstance attribute = entity.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
+		attribute.removeModifier(ATTACK_DAMAGE_MODIFIER);
 	}
 
 	@Override
@@ -42,7 +72,7 @@ public class ItemSwordSunshine extends ItemSword {
 			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
 		}
 
-		tooltip.add(TextFormatting.BLUE + "+3 " + I18n.format("description.attack_damage_at_day.name"));
+		tooltip.add(TextFormatting.GOLD + "+3 " + I18n.format("description.attack_damage_at_day.name"));
 	}
 
 }
