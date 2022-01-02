5bbd64e66369d9fd2b6b6a67d3aa9f72f30f659a
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-02 15:00:33 +0100

    update to 1.16

--- src/main/java/team/cqr/cqrepoured/item/IRangedWeapon.java
+++ src/main/java/team/cqr/cqrepoured/item/IRangedWeapon.java
@@ -1,12 +1,14 @@
 package team.cqr.cqrepoured.item;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 
 public interface IRangedWeapon {
 
-	void shoot(World world, EntityLivingBase shooter, Entity target, EnumHand hand);
+	void shoot(World world, LivingEntity shooter, Entity target, Hand hand);
 
 	SoundEvent getShootSound();
 
