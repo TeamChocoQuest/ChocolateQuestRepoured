be8749e79ff1b1c6be8c41fff71b17c542630944
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-02 13:01:23 +0100

    recreate entity creation methods

--- src/main/java/net/minecraft/entity/EntityList.java
+++ src/main/java/net/minecraft/entity/EntityList.java
@@ -1,20 +1,40 @@
 package net.minecraft.entity;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.registries.ForgeRegistries;
+import team.cqr.cqrepoured.CQRMain;
 
 public class EntityList {
 
 	@Nullable
 	public static Entity createEntityFromNBT(CompoundNBT tag, World worldIn) {
-		try {
-			Entity ent = createEntityByIDFromName(null, worldIn)
-		} catch(Exception ex) {
-			ex.printStackTrace();
-		}
-		return null;
+		ResourceLocation resourcelocation = new ResourceLocation(tag.getString("id"));
+		
+		Entity entity = createEntityByIDFromName(resourcelocation, worldIn);
+
+        if (entity == null)
+        {
+            CQRMain.logger.warn("Skipping Entity with id {}", (Object)resourcelocation);
+        }
+        else
+        {
+            try
+            {
+            entity.load(tag);
+            }
+            catch (Exception e)
+            {
+               CQRMain.logger.error("An Entity {}({}) has thrown an exception during loading, its state cannot be restored. Report this to the mod author",
+                        tag.getString("id"), entity.getName(), e);
+                entity = null;
+            }
+        }
+
+        return entity;
 	}
 	
 	public static Entity createEntityByIDFromName(ResourceLocation id, World world) {
