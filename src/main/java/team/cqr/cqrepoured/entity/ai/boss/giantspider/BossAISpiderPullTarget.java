eca422092941c829ecb7d5b341b73fb73d6b2e2d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-02 12:50:56 +0100

    deleted cause handled by hookshot

--- src/main/java/team/cqr/cqrepoured/entity/ai/boss/giantspider/BossAISpiderPullTarget.java
+++
@@ -1,9 +0,0 @@
-package team.cqr.cqrepoured.entity.ai.boss.giantspider;
-
-public class BossAISpiderPullTarget {
-
-	public BossAISpiderPullTarget() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
