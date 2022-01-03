4ea5052618135d42e19fb6f0fa1bbfa573d0ca58
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-04 00:34:35 +0100

    update to 1.16
    TODO: Check if this is correct

--- src/main/java/team/cqr/cqrepoured/capability/BasicCapabilityProvider.java
+++ src/main/java/team/cqr/cqrepoured/capability/BasicCapabilityProvider.java
@@ -1,7 +1,9 @@
 package team.cqr.cqrepoured.capability;
 
+import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
 
 public class BasicCapabilityProvider<C> implements ICapabilityProvider {
 
@@ -13,14 +15,18 @@ public class BasicCapabilityProvider<C> implements ICapabilityProvider {
 		this.instance = instance;
 	}
 
-	@Override
-	public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+	//@Override
+	public boolean hasCapability(Capability<?> capability, Direction facing) {
 		return capability == this.capability;
 	}
 
 	@Override
-	public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
-		return capability == this.capability ? this.capability.<T>cast(this.instance) : null;
+	public <T> LazyOptional<T> getCapability(Capability<T> capability, Direction facing) {
+		if(capability == this.capability) {
+			return LazyOptional.of(() -> (T)this.instance);
+		}
+		return null;
+		//Old 1.12.2: return capability == this.capability ? this.capability.<T>cast(this.instance) : null;
 	}
 
 }
