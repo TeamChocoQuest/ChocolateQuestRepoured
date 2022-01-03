a4344ad8b8ec687b8d500cd230a21c763477b58d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-03 10:42:10 +0100

    update itemLore to 1.16

--- src/main/java/team/cqr/cqrepoured/item/ItemLore.java
+++ src/main/java/team/cqr/cqrepoured/item/ItemLore.java
@@ -4,30 +4,47 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import org.lwjgl.input.Keyboard;
+import org.lwjgl.glfw.GLFW;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class ItemLore extends Item {
 
+	public ItemLore(Properties itemProperties) {
+		super(itemProperties);
+	}
+	
+	@OnlyIn(Dist.CLIENT)
+	public static boolean isLShiftPressed() {
+		return GLFW.glfwGetKey(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_SHIFT) != GLFW.GLFW_PRESS;
+	}
+	
+	@OnlyIn(Dist.CLIENT)
+	public static boolean isRShiftPressed() {
+		return GLFW.glfwGetKey(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_RIGHT_SHIFT) != GLFW.GLFW_PRESS;
+	}
+	
 	@Override
 	@OnlyIn(Dist.CLIENT)
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+	public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
 		if(this.hasLore(stack)) {
-			if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
-				tooltip.add(TextFormatting.BLUE + I18n.format("description." + this.getRegistryName().getPath() + ".name", '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n'));
+			if (isLShiftPressed() || isRShiftPressed()) {
+				tooltip.add(new StringTextComponent(TextFormatting.BLUE + I18n.get("description." + this.getRegistryName().getPath() + ".name", '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n')));
 			} else {
-				tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
+				tooltip.add(new StringTextComponent(TextFormatting.BLUE + I18n.get("description.click_shift.name")));
 			}
 		} else {
-			super.addInformation(stack, worldIn, tooltip, flagIn);
+			super.appendHoverText(stack, worldIn, tooltip, flagIn);
 		}
 	}
 	
