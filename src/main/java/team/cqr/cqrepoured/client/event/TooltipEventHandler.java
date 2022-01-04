4dab11308ed046c18549508e7a32e3031b372bb5
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-04 14:48:42 +0100

    update ToolTipEventHandler to 1.16

--- src/main/java/team/cqr/cqrepoured/client/event/TooltipEventHandler.java
+++ src/main/java/team/cqr/cqrepoured/client/event/TooltipEventHandler.java
@@ -1,11 +1,12 @@
 package team.cqr.cqrepoured.client.event;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.item.armor.ItemCrown;
@@ -18,7 +19,7 @@ public class TooltipEventHandler {
 	public static void onItemTooltipEvent(ItemTooltipEvent event) {
 		ItemStack stack = event.getItemStack();
 		if (!stack.isEmpty() && ItemCrown.hasCrown(stack)) {
-			event.getToolTip().add(0, TextFormatting.GOLD + "Crown attached");
+			event.getToolTip().add(0, new StringTextComponent(TextFormatting.GOLD + "Crown attached"));
 		}
 	}
 
