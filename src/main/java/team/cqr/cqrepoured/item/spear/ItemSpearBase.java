f27720bb5c980d82a6c8b325bec378ad3c9f6065
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-05 13:32:07 +0100

    update parts of spearBase class to 1.16

--- src/main/java/team/cqr/cqrepoured/item/spear/ItemSpearBase.java
+++ src/main/java/team/cqr/cqrepoured/item/spear/ItemSpearBase.java
@@ -6,36 +6,39 @@ import java.util.UUID;
 
 import javax.annotation.Nullable;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.UseAction;
-import net.minecraft.util.*;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.world.server.ServerWorld;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import org.lwjgl.input.Keyboard;
-
 import com.google.common.base.Predicate;
 import com.google.common.collect.Multimap;
 
-import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.entity.ai.attributes.AttributeModifier.Operation;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
+import net.minecraft.item.UseAction;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ForgeMod;
 import team.cqr.cqrepoured.config.CQRConfig;
 import team.cqr.cqrepoured.init.CQRPotions;
+import team.cqr.cqrepoured.item.ItemLore;
 import team.cqr.cqrepoured.item.sword.ItemCQRWeapon;
 import team.cqr.cqrepoured.util.ItemUtil;
 
@@ -48,8 +51,8 @@ public class ItemSpearBase extends ItemCQRWeapon {
 	private static final float SPECIAL_REACH_MULTIPLIER = 1.5F;
 	private final double reachDistanceBonus;
 
-	public ItemSpearBase(ToolMaterial material) {
-		super(material, CQRConfig.materials.toolMaterials.spearAttackDamageBonus, CQRConfig.materials.toolMaterials.spearAttackSpeedBonus);
+	public ItemSpearBase(Properties props, IItemTier material) {
+		super(material, CQRConfig.materials.toolMaterials.spearAttackDamageBonus, CQRConfig.materials.toolMaterials.spearAttackSpeedBonus, props);
 		this.reachDistanceBonus = CQRConfig.materials.toolMaterials.spearReachDistanceBonus;
 	}
 
@@ -68,11 +71,11 @@ public class ItemSpearBase extends ItemCQRWeapon {
 	}
 
 	@Override
-	public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
-		Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(slot, stack);
+	public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlotType slot) {
+		Multimap<Attribute, AttributeModifier> multimap = super.getDefaultAttributeModifiers(slot);
 
 		if (slot == EquipmentSlotType.MAINHAND) {
-			multimap.put(PlayerEntity.REACH_DISTANCE.getName(), new AttributeModifier(REACH_DISTANCE_MODIFIER, "Weapon modifier", this.reachDistanceBonus, 0));
+			multimap.put(ForgeMod.REACH_DISTANCE.get(), new AttributeModifier(REACH_DISTANCE_MODIFIER, "Weapon modifier", this.reachDistanceBonus, Operation.ADDITION));
 		}
 
 		return multimap;
@@ -80,36 +83,36 @@ public class ItemSpearBase extends ItemCQRWeapon {
 
 	// Makes the right click a "charge attack" action
 	@Override
-	public UseAction getItemUseAction(ItemStack stack) {
+	public UseAction getUseAnimation(ItemStack stack) {
 		return UseAction.BOW;
 	}
 
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-		ItemStack stack = playerIn.getHeldItem(handIn);
-		playerIn.setActiveHand(handIn);
+	public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn) {
+		ItemStack stack = playerIn.getItemInHand(handIn);
+		playerIn.startUsingItem(handIn);
 		return new ActionResult<>(ActionResultType.SUCCESS, stack);
 	}
-
+	
 	@Override
-	public int getMaxItemUseDuration(ItemStack stack) {
+	public int getUseDuration(ItemStack stack) {
 		return 72000;
 	}
 
 	@Override
-	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, LivingEntity entityLiving, int timeLeft) {
+	public void releaseUsing(ItemStack stack, World worldIn, LivingEntity entityLiving, int timeLeft) {
 		if (entityLiving instanceof PlayerEntity) {
 			PlayerEntity player = (PlayerEntity) entityLiving;
 
-			if (!worldIn.isRemote) {
-				Vector3d vec1 = player.getPositionEyes(1.0F);
-				Vector3d vec2 = player.getLookVec();
-				double reachDistance = player.getEntityAttribute(PlayerEntity.REACH_DISTANCE).getAttributeValue();
+			if (!worldIn.isClientSide) {
+				Vector3d vec1 = player.getEyePosition(1.0F);
+				Vector3d vec2 = player.getLookAngle();
+				double reachDistance = player.getAttributeValue(ForgeMod.REACH_DISTANCE.get());
 				float charge = Math.min((float) player.getItemInUseMaxCount() / (float) 40, 1.0F);
 
 				for (LivingEntity entity : this.getEntities(worldIn, LivingEntity.class, player, vec1, vec2, reachDistance, null)) {
 					// TODO apply enchantments
-					entity.attackEntityFrom(DamageSource.causePlayerDamage(player), (1.0F + this.getAttackDamage()) * charge);
+					entity.attackEntityFrom(DamageSource.causePlayerDamage(player), (1.0F + this.getDamage()) * charge);
 				}
 
 				Vector3d vec3 = vec1.add(new Vector3d(0.0D, -0.5D, 0.0D).rotatePitch((float) Math.toRadians(-player.rotationPitch))).add(new Vector3d(-0.4D, 0.0D, 0.0D).rotateYaw((float) Math.toRadians(-player.rotationYaw)));
@@ -118,10 +121,10 @@ public class ItemSpearBase extends ItemCQRWeapon {
 					((ServerWorld) worldIn).spawnParticle(ParticleTypes.SMOKE_NORMAL, vec4.x, vec4.y, vec4.z, 1, 0.05D, 0.05D, 0.05D, 0.0D);
 				}
 
-				player.world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_PLAYER_ATTACK_KNOCKBACK, player.getSoundSource(), 1.0F, 1.0F);
+				player.level.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_PLAYER_ATTACK_KNOCKBACK, player.getSoundSource(), 1.0F, 1.0F);
 				player.getCooldownTracker().setCooldown(stack.getItem(), 200);
 			} else {
-				player.swingArm(Hand.MAIN_HAND);
+				player.swing(Hand.MAIN_HAND);
 			}
 		}
 	}
@@ -133,13 +136,13 @@ public class ItemSpearBase extends ItemCQRWeapon {
 		Vector3d vec4 = rayTraceResult1 != null ? rayTraceResult1.hitVec : vec3;
 		AxisAlignedBB aabb1 = new AxisAlignedBB(vec1.x, vec1.y, vec1.z, vec4.x, vec4.y, vec4.z);
 
-		for (T entity : world.getEntitiesWithinAABB(entityClass, aabb1, predicate)) {
+		for (T entity : world.getEntitiesOfClass(entityClass, aabb1, predicate)) {
 			if (entity == toIgnore) {
 				continue;
 			}
 
-			AxisAlignedBB aabb2 = entity.getEntityBoundingBox().grow(entity.getCollisionBorderSize());
-			RayTraceResult rayTraceResult2 = aabb2.calculateIntercept(vec1, vec4);
+			AxisAlignedBB aabb2 = entity.getBoundingBox().inflate(entity.getCollisionBorderSize());
+			RayTraceResult rayTraceResult2 = aabb2.intersects(vec1, vec4);
 
 			if (rayTraceResult2 != null) {
 				list.add(entity);
@@ -151,26 +154,22 @@ public class ItemSpearBase extends ItemCQRWeapon {
 
 	@Override
 	@OnlyIn(Dist.CLIENT)
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description.spear_diamond.name"));
-		} else {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
-		}
+	public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		ItemLore.addHoverTextLogic(tooltip, flagIn, this.getRegistryName().getPath());
 	}
 
 	@Override
-	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+	public void onUseTick(World worldIn, LivingEntity entityIn, ItemStack stack, int itemSlot) {
 		if (!(entityIn instanceof LivingEntity)) {
 			return;
 		}
-		if (!isSelected) {
+		if (entityIn.getUseItem().getItem() != this) {
 			return;
 		}
 		LivingEntity entityLiving = (LivingEntity) entityIn;
 		ItemStack offhand = entityLiving.getMainHandItem();
 		if (!offhand.isEmpty()) {
-			entityLiving.addPotionEffect(new EffectInstance(CQRPotions.TWOHANDED, 30, 1));
+			entityLiving.addEffect(new EffectInstance(CQRPotions.TWOHANDED, 30, 1));
 		}
 	}
 
