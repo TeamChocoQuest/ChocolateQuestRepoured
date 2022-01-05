006c6657847c19f7d3259ab0dfd2c0290362aba4
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-05 14:31:53 +0100

    update to 1.16

--- src/main/java/team/cqr/cqrepoured/inventory/IInteractable.java
+++ src/main/java/team/cqr/cqrepoured/inventory/IInteractable.java
@@ -1,9 +1,10 @@
 package team.cqr.cqrepoured.inventory;
 
 import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.PlayerEntity;
 
 public interface IInteractable {
 
-	void onClickButton(EntityPlayer player, int button, ByteBuf extraData);
+	void onClickButton(PlayerEntity player, int button, ByteBuf extraData);
 
 }
