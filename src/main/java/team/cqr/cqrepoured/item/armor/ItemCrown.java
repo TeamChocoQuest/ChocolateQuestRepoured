71adae0fe106ec6a841c872ee21d93b828366836
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-05 13:31:57 +0100

    update crown item
    TODO: Model

--- src/main/java/team/cqr/cqrepoured/item/armor/ItemCrown.java
+++ src/main/java/team/cqr/cqrepoured/item/armor/ItemCrown.java
@@ -4,42 +4,43 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import org.lwjgl.input.Keyboard;
-
-import net.minecraft.client.model.ModelBiped;
-import net.minecraft.client.resources.I18n;
+import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.common.util.LazyOptional;
 import team.cqr.cqrepoured.capability.armor.kingarmor.CapabilityDynamicCrown;
 import team.cqr.cqrepoured.capability.armor.kingarmor.CapabilityDynamicCrownProvider;
 import team.cqr.cqrepoured.client.init.CQRArmorModels;
+import team.cqr.cqrepoured.item.ItemLore;
 
 public class ItemCrown extends ArmorItem {
 
 	public static final String NBT_KEY_CROWN = "CQR Crown";
 
-	public ItemCrown(ArmorMaterial materialIn, int renderIndexIn) {
-		super(materialIn, renderIndexIn, EquipmentSlotType.HEAD);
+	public ItemCrown(IArmorMaterial materialIn, Properties props) {
+		super(materialIn, EquipmentSlotType.HEAD, props);
 	}
 
-	@Override
 	@OnlyIn(Dist.CLIENT)
-	public ModelBiped getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, ModelBiped _default) {
+	@Override
+	public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
 		return CQRArmorModels.crown;
 	}
-
+	
 	@Override
 	public ICapabilityProvider initCapabilities(ItemStack stack, CompoundNBT nbt) {
 		return CapabilityDynamicCrownProvider.createProvider();
@@ -47,49 +48,45 @@ public class ItemCrown extends ArmorItem {
 
 	@Nullable
 	public Item getAttachedItem(ItemStack stack) {
-		if (stack.hasCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null)) {
-			return stack.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).getAttachedItem();
+		if (stack.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).isPresent()) {
+			return stack.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).resolve().get().getAttachedItem();
 		}
 		return null;
 	}
 
 	public void attachItem(ItemStack crown, Item toAttach) {
-		if (crown.hasCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null)) {
-			crown.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).attachItem(toAttach);
+		if (crown.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).isPresent()) {
+			crown.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).resolve().get().attachItem(toAttach);
 		}
 	}
 
 	public void attachItem(ItemStack crown, ItemStack toAttach) {
-		if (crown.hasCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null)) {
-			crown.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).attachItem(toAttach);
+		if (crown.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).isPresent()) {
+			crown.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).resolve().get().attachItem(toAttach);
 		}
 	}
 
 	public void attachItem(ItemStack crown, ResourceLocation toAttach) {
-		if (crown.hasCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null)) {
-			crown.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).attachItem(toAttach);
+		if (crown.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).isPresent()) {
+			crown.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null).resolve().get().attachItem(toAttach);
 		}
 	}
 
 	@Override
-	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		CapabilityDynamicCrown capability = stack.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null);
-		if (capability.getAttachedItem() != null) {
-			tooltip.add("Attached helmet: " + new ItemStack(capability.getAttachedItem(), 1).getDisplayName());
+	public void appendHoverText(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		LazyOptional<CapabilityDynamicCrown> lOpCap = stack.getCapability(CapabilityDynamicCrownProvider.DYNAMIC_CROWN, null);
+		if (lOpCap.isPresent()) {
+			tooltip.add(new StringTextComponent("Attached helmet: " + new ItemStack(lOpCap.resolve().get().getAttachedItem(), 1).getDisplayName()));
 		}
 
-		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description." + this.getRegistryName().getPath() + ".name"));
-		} else {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
-		}
+		ItemLore.addHoverTextLogic(tooltip, flagIn, this.getRegistryName().getPath());
 	}
 
 	// TODO: Tooltip that shows the attachment
 	// TODO: Stats are affected by attachment
 
 	public static boolean hasCrown(ItemStack stack) {
-		return stack.hasTagCompound() && stack.getTagCompound().hasKey(NBT_KEY_CROWN, Constants.NBT.TAG_COMPOUND);
+		return stack.hasTag() && stack.getTag().contains(NBT_KEY_CROWN, Constants.NBT.TAG_COMPOUND);
 	}
 
 }
