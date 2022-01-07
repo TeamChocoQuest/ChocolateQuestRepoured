55666b2c2d0acae427189a1f1983d7654642e45c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-07 14:42:02 +0100

    update ITileEntitySyncable to 1.16

--- src/main/java/team/cqr/cqrepoured/tileentity/ITileEntitySyncable.java
+++ src/main/java/team/cqr/cqrepoured/tileentity/ITileEntitySyncable.java
@@ -1,12 +1,12 @@
 package team.cqr.cqrepoured.tileentity;
 
-import net.minecraft.util.ITickable;
+import net.minecraft.tileentity.ITickableTileEntity;
 import team.cqr.cqrepoured.network.datasync.TileEntityDataManager;
 
-public interface ITileEntitySyncable extends ITickable {
+public interface ITileEntitySyncable extends ITickableTileEntity {
 
 	@Override
-	default void update() {
+	default void tick() {
 		this.getDataManager().checkIfDirtyAndSync();
 	}
 
