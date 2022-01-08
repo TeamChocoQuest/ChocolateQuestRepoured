db1f45c0687bf9fdbbcaf69b7d1ba37caccb004f
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-08 13:32:33 +0100

    update sizable part

--- src/main/java/team/cqr/cqrepoured/entity/MultiPartEntityPartSizable.java
+++ src/main/java/team/cqr/cqrepoured/entity/MultiPartEntityPartSizable.java
@@ -2,20 +2,31 @@ package team.cqr.cqrepoured.entity;
 
 import com.github.alexthe666.iceandfire.entity.IBlacklistedFromStatues;
 
-import net.minecraft.entity.IEntityMultiPart;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntitySize;
+import net.minecraft.entity.Pose;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.entity.PartEntity;
 
-public class MultiPartEntityPartSizable<T extends IEntityMultiPart & ISizable> extends PartEntity implements ISizable, IBlacklistedFromStatues, IDontRenderFire {
+public class MultiPartEntityPartSizable<T extends Entity & IEntityMultiPart<?> & ISizable> extends PartEntity<T> implements ISizable, IBlacklistedFromStatues, IDontRenderFire {
 
 	private final float dw, dh;
+	private final EntitySize size;
 
 	public MultiPartEntityPartSizable(T parent, String partName, float width, float height) {
-		super(parent, partName, width, height);
+		super(parent);
 
 		this.dw = width;
 		this.dh = height;
 
+		this.size = new EntitySize(width, height, false);
+		
 		this.initializeSize();
+	}
+
+	@Override
+	public boolean isPickable() {
+		return true;
 	}
 
 	@Override
@@ -30,7 +41,7 @@ public class MultiPartEntityPartSizable<T extends IEntityMultiPart & ISizable> e
 
 	@Override
 	public float getSizeVariation() {
-		return ((ISizable) this.parent).getSizeVariation();
+		return ((ISizable) this.getParent()).getSizeVariation();
 	}
 
 	@Override
@@ -41,6 +52,39 @@ public class MultiPartEntityPartSizable<T extends IEntityMultiPart & ISizable> e
 	@Override
 	public boolean canBeTurnedToStone() {
 		return false;
+	}
+
+	@Override
+	public Pose getPose() {
+		return this.getParent().getPose();
+	}
+
+	@Override
+	public EntitySize getDimensions(Pose pPose) {
+		return callOnGetDimensions(this.size);
+	}
+	
+	public EntitySize getSize() {
+		return this.size;
+	}
+
+	@Override
+	protected void defineSynchedData() {
+
+	}
+
+	@Override
+	protected void readAdditionalSaveData(CompoundNBT pCompound) {
+
+	}
+
+	@Override
+	protected void addAdditionalSaveData(CompoundNBT pCompound) {
+
+	}
+
+	public boolean is(Entity pEntity) {
+		return this == pEntity || this.getParent() == pEntity;
 	}
 
 }
