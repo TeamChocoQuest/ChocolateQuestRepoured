44b7ec248b17de7f261d917ad088a81142970cae
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-08 16:52:15 +0100

    update CreatureAttributes to 1.16

--- src/main/java/team/cqr/cqrepoured/init/CQRCreatureAttributes.java
+++ src/main/java/team/cqr/cqrepoured/init/CQRCreatureAttributes.java
@@ -1,12 +1,11 @@
 package team.cqr.cqrepoured.init;
 
 import net.minecraft.entity.CreatureAttribute;
-import net.minecraftforge.common.util.EnumHelper;
 
 public class CQRCreatureAttributes {
 
-	public static final CreatureAttribute VOID = EnumHelper.addCreatureAttribute("VOID");
-	public static final CreatureAttribute WATER = EnumHelper.addCreatureAttribute("WATER");
-	public static final CreatureAttribute MECHANICAL = EnumHelper.addCreatureAttribute("MECHANICAL");
+	public static final CreatureAttribute VOID = new CreatureAttribute();
+	public static final CreatureAttribute WATER = new CreatureAttribute();
+	public static final CreatureAttribute MECHANICAL = new CreatureAttribute();
 
 }
