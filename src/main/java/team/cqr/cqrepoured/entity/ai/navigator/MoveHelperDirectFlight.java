d46e55c5a8a751fbb6361a235405bb790b2a1bff
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-08 13:33:47 +0100

    partially update direct flight movehelper

--- src/main/java/team/cqr/cqrepoured/entity/ai/navigator/MoveHelperDirectFlight.java
+++ src/main/java/team/cqr/cqrepoured/entity/ai/navigator/MoveHelperDirectFlight.java
@@ -12,13 +12,13 @@ public class MoveHelperDirectFlight extends MovementController {
 	}
 
 	@Override
-	public void onUpdateMoveHelper() {
-		if (this.action == MovementController.Action.MOVE_TO) {
-			this.action = MovementController.Action.WAIT;
-			this.entity.setNoGravity(true);
-			double d0 = this.posX - this.entity.posX;
-			double d1 = this.posY - this.entity.posY;
-			double d2 = this.posZ - this.entity.posZ;
+	public void tick() {
+		if (this.operation == MovementController.Action.MOVE_TO) {
+			this.operation = MovementController.Action.WAIT;
+			this.mob.setNoGravity(true);
+			double d0 = this.wantedX - this.mob.getX();
+			double d1 = this.wantedY - this.mob.getY();
+			double d2 = this.wantedZ - this.mob.getZ();
 			/*
 			 * double d3 = d0 * d0 + d1 * d1 + d2 * d2;
 			 * 
@@ -26,20 +26,20 @@ public class MoveHelperDirectFlight extends MovementController {
 			 */
 
 			float f = (float) (MathHelper.atan2(d2, d0) * (180D / Math.PI)) - 90.0F;
-			this.entity.rotationYaw = f;// this.limitAngle(this.entity.rotationYaw, f, 10.0F);
+			this.mob.yRot = f;// this.limitAngle(this.entity.rotationYaw, f, 10.0F);
 			float f1;
 
-			if (this.entity.onGround) {
-				f1 = (float) (this.speed * this.entity.getEntityAttribute(Attributes.MOVEMENT_SPEED).getAttributeValue());
+			if (this.mob.isOnGround()) {
+				f1 = (float) (this.speedModifier * this.mob.getAttribute(Attributes.MOVEMENT_SPEED).getValue());
 			} else {
-				f1 = (float) (this.speed * this.entity.getEntityAttribute(Attributes.FLYING_SPEED).getAttributeValue());
+				f1 = (float) (this.speedModifier * this.mob.getAttribute(Attributes.FLYING_SPEED).getValue());
 			}
 
-			this.entity.setAIMoveSpeed(f1);
+			this.mob.setSpeed(f1);
 			double d4 = MathHelper.sqrt(d0 * d0 + d2 * d2);
 			float f2 = (float) (-(MathHelper.atan2(d1, d4) * (180D / Math.PI)));
-			this.entity.rotationPitch = f2;// this.limitAngle(this.entity.rotationPitch, f2, 10.0F);
-			this.entity.setMoveVertical(d1 > 0.0D ? f1 : -f1);
+			this.mob.xRot = f2;// this.limitAngle(this.entity.rotationPitch, f2, 10.0F);
+			this.mob.setMoveVertical(d1 > 0.0D ? f1 : -f1);
 		}
 		/*
 		 * } else { this.entity.setNoGravity(false); this.entity.setMoveVertical(0.0F); this.entity.setMoveForward(0.0F); }
