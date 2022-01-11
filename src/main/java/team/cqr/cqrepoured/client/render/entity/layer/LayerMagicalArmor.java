61d289c6936de3518a9d693e9ff0f2b6617dab30
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-01-11 05:06:54 +0100

    Update magical armor layer to 1.16

--- src/main/java/team/cqr/cqrepoured/client/render/entity/layer/LayerMagicalArmor.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/layer/LayerMagicalArmor.java
@@ -1,59 +1,57 @@
 package team.cqr.cqrepoured.client.render.entity.layer;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.renderer.GlStateManager;
+import org.lwjgl.opengl.GL11;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.platform.GlStateManager;
+
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.IEntityRenderer;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
-import team.cqr.cqrepoured.client.render.entity.RenderCQREntity;
 import team.cqr.cqrepoured.entity.bases.AbstractEntityCQR;
 
-public class LayerMagicalArmor extends AbstractLayerCQR {
+public class LayerMagicalArmor<T extends AbstractEntityCQR, M extends EntityModel<T>> extends LayerRenderer<T, M> {
 
-	protected final ResourceLocation ARMOR_TEXTURE;
-	protected final RenderCQREntity<? extends AbstractEntityCQR> RENDERER;
-	protected final ModelBase MODEL;
+	private final ResourceLocation texture;
 
-	public LayerMagicalArmor(RenderCQREntity<? extends AbstractEntityCQR> renderer, ResourceLocation texture, ModelBase model) {
+	public LayerMagicalArmor(IEntityRenderer<T, M> renderer, ResourceLocation texture) {
 		super(renderer);
-		this.RENDERER = renderer;
-		this.ARMOR_TEXTURE = texture;
-		this.MODEL = model;
+		this.texture = texture;
 	}
 
 	@Override
-	public void doRenderLayer(AbstractEntityCQR entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-
-		if (entitylivingbaseIn.isMagicArmorActive()) {
-			GlStateManager.depthMask(!entitylivingbaseIn.isInvisible());
-			this.RENDERER.bindTexture(this.ARMOR_TEXTURE);
-			GlStateManager.matrixMode(5890);
-			GlStateManager.loadIdentity();
-			float f = entitylivingbaseIn.ticksExisted + partialTicks;
+	public void render(MatrixStack pMatrixStack, IRenderTypeBuffer pBuffer, int pPackedLight, T pLivingEntity, float pLimbSwing, float pLimbSwingAmount,
+			float pPartialTicks, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch) {
+		if (pLivingEntity.isMagicArmorActive()) {
+			// TODO does this work?
+			GlStateManager._matrixMode(GL11.GL_TEXTURE);
+			GlStateManager._loadIdentity();
+			float f = pLivingEntity.tickCount + pPartialTicks;
 			float f1 = MathHelper.cos(f * 0.02F) * 3.0F;
 			float f2 = f * 0.01F;
-			GlStateManager.translate(f1, f2, 0.0F);
-			GlStateManager.matrixMode(5888);
-			GlStateManager.enableBlend();
-			GlStateManager.color(0.5F, 0.5F, 0.5F, 1.0F);
-			GlStateManager.disableLighting();
-			GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
-			this.MODEL.setLivingAnimations(entitylivingbaseIn, limbSwing, limbSwingAmount, partialTicks);
-			this.MODEL.setModelAttributes(this.RENDERER.getMainModel());
-			Minecraft.getMinecraft().entityRenderer.setupFogColor(true);
-			this.MODEL.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
-			Minecraft.getMinecraft().entityRenderer.setupFogColor(false);
-			GlStateManager.matrixMode(5890);
-			GlStateManager.loadIdentity();
-			GlStateManager.matrixMode(5888);
-			GlStateManager.enableLighting();
-			GlStateManager.disableBlend();
-		}
-	}
+			GlStateManager._translated(f1, f2, 0.0F);
+			GlStateManager._matrixMode(GL11.GL_MODELVIEW);
 
-	@Override
-	public boolean shouldCombineTextures() {
-		return false;
+			// TODO use appropriate render type
+			/**
+			 * GlStateManager.depthMask(!entitylivingbaseIn.isInvisible());
+			 * GlStateManager.disableLighting();
+			 * GlStateManager.enableBlend();
+			 * GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
+			 * Minecraft.getMinecraft().entityRenderer.setupFogColor(true);
+			 */
+			this.getParentModel().renderToBuffer(pMatrixStack, pBuffer.getBuffer(RenderType.entityTranslucent(this.texture)), pPackedLight,
+					OverlayTexture.NO_OVERLAY, 0.5F, 0.5F, 0.5F, 1.0F);
+
+			GlStateManager._matrixMode(GL11.GL_TEXTURE);
+			GlStateManager._loadIdentity();
+			GlStateManager._matrixMode(GL11.GL_MODELVIEW);
+		}
 	}
 
 }
