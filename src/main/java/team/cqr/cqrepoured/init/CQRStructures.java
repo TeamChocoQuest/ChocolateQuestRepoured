a5481fcbedcb985a06906e6a104e31e51570b9f6
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-12 18:08:02 +0100

    add CQRStructures and register the wall

---
+++ src/main/java/team/cqr/cqrepoured/init/CQRStructures.java
@@ -0,0 +1,115 @@
+package team.cqr.cqrepoured.init;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.settings.DimensionStructuresSettings;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import team.cqr.cqrepoured.CQRMain;
+import team.cqr.cqrepoured.world.structure.generation.thewall.WallStructure;
+
+public class CQRStructures {
+	
+	public static final DeferredRegister<Structure<?>> DEFERRED_REGISTRY_STRUCTURE = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, CQRMain.MODID);
+	
+	public static final RegistryObject<Structure<NoFeatureConfig>> WALL_IN_THE_NORTH = DEFERRED_REGISTRY_STRUCTURE.register("wall_in_the_north", () -> (new WallStructure(NoFeatureConfig.CODEC)));
+
+	public static void setupStructures() {
+		Structure.STRUCTURES_REGISTRY.put(WALL_IN_THE_NORTH.get().getRegistryName().toString(), WALL_IN_THE_NORTH.get());
+    }
+
+    /**
+     * Adds the provided structure to the registry, and adds the separation settings.
+     * The rarity of the structure is determined based on the values passed into
+     * this method in the structureSeparationSettings argument.
+     * This method is called by setupStructures above.
+     */
+    public static <F extends Structure<?>> void setupMapSpacingAndLand(
+            F structure,
+            StructureSeparationSettings structureSeparationSettings,
+            boolean transformSurroundingLand)
+    {
+        /*
+         * We need to add our structures into the map in Structure class
+         * alongside vanilla structures or else it will cause errors.
+         *
+         * If the registration is setup properly for the structure,
+         * getRegistryName() should never return null.
+         */
+        Structure.STRUCTURES_REGISTRY.put(structure.getRegistryName().toString(), structure);
+
+        /*
+         * Whether surrounding land will be modified automatically to conform to the bottom of the structure.
+         * Basically, it adds land at the base of the structure like it does for Villages and Outposts.
+         * Doesn't work well on structure that have pieces stacked vertically or change in heights.
+         *
+         * Note: The air space this method will create will be filled with water if the structure is below sealevel.
+         * This means this is best for structure above sealevel so keep that in mind.
+         *
+         * NOISE_AFFECTING_FEATURES requires AccessTransformer  (See resources/META-INF/accesstransformer.cfg)
+         */
+        if(transformSurroundingLand){
+            Structure.NOISE_AFFECTING_FEATURES =
+                    ImmutableList.<Structure<?>>builder()
+                            .addAll(Structure.NOISE_AFFECTING_FEATURES)
+                            .add(structure)
+                            .build();
+        }
+
+        /*
+         * This is the map that holds the default spacing of all structures.
+         * Always add your structure to here so that other mods can utilize it if needed.
+         *
+         * However, while it does propagate the spacing to some correct dimensions from this map,
+         * it seems it doesn't always work for code made dimensions as they read from this list beforehand.
+         *
+         * Instead, we will use the WorldEvent.Load event in StructureTutorialMain to add the structure
+         * spacing from this list into that dimension or to do dimension blacklisting properly.
+         * We also use our entry in DimensionStructuresSettings.DEFAULTS in WorldEvent.Load as well.
+         *
+         * DEFAULTS requires AccessTransformer  (See resources/META-INF/accesstransformer.cfg)
+         */
+        DimensionStructuresSettings.DEFAULTS =
+                ImmutableMap.<Structure<?>, StructureSeparationSettings>builder()
+                        .putAll(DimensionStructuresSettings.DEFAULTS)
+                        .put(structure, structureSeparationSettings)
+                        .build();
+
+
+        /*
+         * There are very few mods that relies on seeing your structure in the noise settings registry before the world is made.
+         *
+         * You may see some mods add their spacings to DimensionSettings.BUILTIN_OVERWORLD instead of the NOISE_GENERATOR_SETTINGS loop below but
+         * that field only applies for the default overworld and won't add to other worldtypes or dimensions (like amplified or Nether).
+         * So yeah, don't do DimensionSettings.BUILTIN_OVERWORLD. Use the NOISE_GENERATOR_SETTINGS loop below instead if you must.
+         */
+        WorldGenRegistries.NOISE_GENERATOR_SETTINGS.entrySet().forEach(settings -> {
+            Map<Structure<?>, StructureSeparationSettings> structureMap = settings.getValue().structureSettings().structureConfig();
+
+            /*
+             * Pre-caution in case a mod makes the structure map immutable like datapacks do.
+             * I take no chances myself. You never know what another mods does...
+             *
+             * structureConfig requires AccessTransformer  (See resources/META-INF/accesstransformer.cfg)
+             */
+            if(structureMap instanceof ImmutableMap){
+                Map<Structure<?>, StructureSeparationSettings> tempMap = new HashMap<>(structureMap);
+                tempMap.put(structure, structureSeparationSettings);
+                settings.getValue().structureSettings().structureConfig = tempMap;
+            }
+            else{
+                structureMap.put(structure, structureSeparationSettings);
+            }
+        });
+    }
+	
+}
