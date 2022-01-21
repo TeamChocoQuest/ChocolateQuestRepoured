41345fce2d728146b736128f2c68efae555a121f
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-01-21 15:51:58 +0100

    create CQR block tags

---
+++ src/main/java/team/cqr/cqrepoured/init/CQRBlockTags.java
@@ -0,0 +1,12 @@
+package team.cqr.cqrepoured.init;
+
+import net.minecraft.block.Block;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ITag;
+import team.cqr.cqrepoured.CQRMain;
+
+public class CQRBlockTags {
+
+	public static final ITag.INamedTag<Block> SPIDER_WEBS = BlockTags.bind(CQRMain.MODID + ":spider_webs");
+	
+}
