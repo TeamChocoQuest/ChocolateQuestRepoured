eebef717c2b35e047e86e5193ff134ae23b3ccfd
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-02-20 14:52:59 +0100

    update GeneratableMapInfo to 1.16

--- src/main/java/team/cqr/cqrepoured/world/structure/generation/generation/generatable/GeneratableMapInfo.java
+++ src/main/java/team/cqr/cqrepoured/world/structure/generation/generation/generatable/GeneratableMapInfo.java
@@ -5,19 +5,18 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Multiset;
 import com.google.common.collect.Multisets;
 
-import net.minecraft.block.BlockDirt;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockStone;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.entity.item.ItemFrameEntity;
 import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockPos.MutableBlockPos;
+import net.minecraft.util.math.BlockPos.Mutable;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.gen.Heightmap.Type;
 import net.minecraft.world.storage.MapData;
 import team.cqr.cqrepoured.util.BlockPlacingHelper;
 import team.cqr.cqrepoured.util.DungeonGenUtils;
@@ -58,25 +57,25 @@ public class GeneratableMapInfo extends GeneratablePosInfo {
 			dungeon.markRemovedLight(pos.getX(), pos.getY(), pos.getZ(), light);
 		}
 		boolean flag = BlockPlacingHelper.setBlockState(world, chunk, blockStorage, pos, Blocks.AIR.defaultBlockState(), null, 16);
-		ItemStack stack = FilledMapItem.setupNewMap(world, this.mapX, this.mapZ, this.scale, true, true);
+		ItemStack stack = FilledMapItem.create(world, this.mapX, this.mapZ, this.scale, true, true);
 		if (this.fillMap) {
-			updateMapData(world, this.mapOriginX, this.mapOriginZ, this.fillRadius, ((FilledMapItem) stack.getItem()).getMapData(stack, world));
+			updateMapData(world, this.mapOriginX, this.mapOriginZ, this.fillRadius, FilledMapItem.getSavedData(stack, world));
 		}
-		this.entity.setDisplayedItem(stack);
-		world.spawnEntity(this.entity);
+		this.entity.setItem(stack);
+		world.addFreshEntity(this.entity);
 		return flag;
 	}
 
 	private static void updateMapData(World worldIn, int x, int z, int radius, MapData data) {
-		if (worldIn.provider.getDimension() != data.dimension) {
+		if (worldIn.dimension() != data.dimension) {
 			return;
 		}
 
 		for (int ix = 0; ix < 128; ix++) {
-			int worldX = data.xCenter + (ix - 64) * (1 << data.scale);
+			int worldX = data.x + (ix - 64) * (1 << data.scale);
 
 			for (int iz = 0; iz < 128; iz++) {
-				int worldZ = data.zCenter + (iz - 64) * (1 << data.scale);
+				int worldZ = data.z + (iz - 64) * (1 << data.scale);
 
 				if (!DungeonGenUtils.isInsideCircle(worldX - x, worldZ - z, radius)) {
 					continue;
@@ -90,8 +89,8 @@ public class GeneratableMapInfo extends GeneratablePosInfo {
 	private static void updateMapColorAt(World world, int mapX, int mapZ, MapData data) {
 		int i = 1 << data.scale;
 		double d0 = 0.0D;
-		int k2 = (data.xCenter / i + mapX - 64) * i;
-		int l2 = (data.zCenter / i + mapZ - 64) * i;
+		int k2 = (data.x / i + mapX - 64) * i;
+		int l2 = (data.z / i + mapZ - 64) * i;
 		Multiset<MaterialColor> multiset = HashMultiset.create();
 		Chunk chunk = world.getChunk(k2 >> 4, l2 >> 4);
 		int i3 = k2 & 15;
@@ -99,35 +98,35 @@ public class GeneratableMapInfo extends GeneratablePosInfo {
 		int k3 = 0;
 		double d1 = 0.0D;
 
-		if (world.provider.isNether()) {
+		if (world.dimension().equals(World.NETHER)) {
 			int l3 = k2 + l2 * 231_871;
 			l3 = l3 * l3 * 31_287_121 + l3 * 11;
 
 			if ((l3 >> 20 & 1) == 0) {
-				multiset.add(Blocks.DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.DIRT).getMapColor(world, BlockPos.ORIGIN), 10);
+				multiset.add(Blocks.DIRT.defaultBlockState().getMapColor(world, BlockPos.ZERO), 10);
 			} else {
-				multiset.add(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.STONE).getMapColor(world, BlockPos.ORIGIN), 100);
+				multiset.add(Blocks.STONE.defaultBlockState().getMapColor(world, BlockPos.ZERO), 100);
 			}
 
 			d1 = 100.0D;
 		} else {
-			MutableBlockPos pos = new MutableBlockPos();
+			Mutable pos = new Mutable();
 
 			for (int i4 = 0; i4 < i; ++i4) {
 				for (int j4 = 0; j4 < i; ++j4) {
-					int k4 = chunk.getHeightValue(i4 + i3, j4 + j3) + 1;
-					BlockState state = Blocks.AIR.getDefaultState();
+					int k4 = chunk.getHeight(Type.WORLD_SURFACE, i4 + i3, j4 + j3) + 1; //Correct replacement?
+					BlockState state = Blocks.AIR.defaultBlockState();
 
 					if (k4 <= 1) {
-						state = Blocks.BEDROCK.getDefaultState();
+						state = Blocks.BEDROCK.defaultBlockState();
 					} else {
 						label175: {
 							while (true) {
 								--k4;
-								state = chunk.getBlockState(i4 + i3, k4, j4 + j3);
-								pos.setPos((chunk.x << 4) + i4 + i3, k4, (chunk.z << 4) + j4 + j3);
+								state = chunk.getBlockState(new BlockPos(i4 + i3, k4, j4 + j3));
+								pos.set((chunk.getPos().x << 4) + i4 + i3, k4, (chunk.getPos().z << 4) + j4 + j3);
 
-								if (state.getMapColor(world, pos) != MaterialColor.AIR || k4 <= 0) {
+								if (state.getMapColor(world, pos) != MaterialColor.NONE || k4 <= 0) {
 									break;
 								}
 							}
@@ -136,7 +135,7 @@ public class GeneratableMapInfo extends GeneratablePosInfo {
 								int l4 = k4 - 1;
 
 								while (true) {
-									BlockState iblockstate1 = chunk.getBlockState(i4 + i3, l4--, j4 + j3);
+									BlockState iblockstate1 = chunk.getBlockState(new BlockPos(i4 + i3, l4--, j4 + j3));
 									++k3;
 
 									if (l4 <= 0 || !iblockstate1.getMaterial().isLiquid()) {
@@ -165,7 +164,7 @@ public class GeneratableMapInfo extends GeneratablePosInfo {
 			i5 = 0;
 		}
 
-		MaterialColor mapcolor = Iterables.getFirst(Multisets.copyHighestCountFirst(multiset), MaterialColor.AIR);
+		MaterialColor mapcolor = Iterables.getFirst(Multisets.copyHighestCountFirst(multiset), MaterialColor.NONE);
 
 		if (mapcolor == MaterialColor.WATER) {
 			d2 = k3 * 0.1D + (mapX + mapZ & 1) * 0.2D;
@@ -181,11 +180,11 @@ public class GeneratableMapInfo extends GeneratablePosInfo {
 		}
 
 		byte b0 = data.colors[mapX + mapZ * 128];
-		byte b1 = (byte) (mapcolor.colorIndex * 4 + i5);
+		byte b1 = (byte) (mapcolor.col * 4 + i5); //Correct replacement?
 
 		if (b0 != b1) {
 			data.colors[mapX + mapZ * 128] = b1;
-			data.updateMapData(mapX, mapZ);
+			data.setDirty(mapX, mapZ);
 		}
 	}
 
