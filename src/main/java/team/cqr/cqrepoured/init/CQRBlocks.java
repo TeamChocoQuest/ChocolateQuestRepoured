40e13a3aa61d9a21e807770150597672749f1679
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-03-08 19:06:34 +0100

    update CQRBlocks

--- src/main/java/team/cqr/cqrepoured/init/CQRBlocks.java
+++ src/main/java/team/cqr/cqrepoured/init/CQRBlocks.java
@@ -33,105 +33,107 @@ public class CQRBlocks {
 
 	// Dungeon Blocks
 	// Andesite
-	public static final RegistryObject<BlockDungeonBrick> ANDESITE_CARVED = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockPillarDungeonBrick> ANDESITE_PILLAR = BLOCKS.register("", BlockPillarDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> ANDESITE_CUBE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> ANDESITE_SCALE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> ANDESITE_SQUARE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> ANDESITE_SMALL = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> ANDESITE_LARGE = BLOCKS.register("", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ANDESITE_CARVED = BLOCKS.register("andesite_carved", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockPillarDungeonBrick> ANDESITE_PILLAR = BLOCKS.register("andesite_pillar", BlockPillarDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ANDESITE_CUBE = BLOCKS.register("andesite_cube", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ANDESITE_SCALE = BLOCKS.register("andesite_scale", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ANDESITE_SQUARE = BLOCKS.register("andesite_square", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ANDESITE_SMALL = BLOCKS.register("andesite_small", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ANDESITE_LARGE = BLOCKS.register("andesite_large", BlockDungeonBrick::new);
 
 	// Diorite
-	public static final RegistryObject<BlockDungeonBrick> DIORITE_CARVED = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockPillarDungeonBrick> DIORITE_PILLAR = BLOCKS.register("", BlockPillarDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> DIORITE_CUBE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> DIORITE_SCALE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> DIORITE_SQUARE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> DIORITE_SMALL = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> DIORITE_LARGE = BLOCKS.register("", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> DIORITE_CARVED = BLOCKS.register("diorite_carved", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockPillarDungeonBrick> DIORITE_PILLAR = BLOCKS.register("diorite_pillar", BlockPillarDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> DIORITE_CUBE = BLOCKS.register("diorite_cube", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> DIORITE_SCALE = BLOCKS.register("diorite_scale", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> DIORITE_SQUARE = BLOCKS.register("diorite_square", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> DIORITE_SMALL = BLOCKS.register("diorite_small", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> DIORITE_LARGE = BLOCKS.register("diorite_large", BlockDungeonBrick::new);
 
 	// Granite
-	public static final RegistryObject<BlockDungeonBrick> GRANITE_CARVED = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockPillarDungeonBrick> GRANITE_PILLAR = BLOCKS.register("", BlockPillarDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> GRANITE_CUBE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> GRANITE_SCALE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> GRANITE_SQUARE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> GRANITE_SMALL = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> GRANITE_LARGE = BLOCKS.register("", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> GRANITE_CARVED = BLOCKS.register("granite_carved", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockPillarDungeonBrick> GRANITE_PILLAR = BLOCKS.register("granite_pillar", BlockPillarDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> GRANITE_CUBE = BLOCKS.register("granite_cube", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> GRANITE_SCALE = BLOCKS.register("granite_scale", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> GRANITE_SQUARE = BLOCKS.register("granite_square", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> GRANITE_SMALL = BLOCKS.register("granite_small", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> GRANITE_LARGE = BLOCKS.register("granite_large", BlockDungeonBrick::new);
 
 	// Prismarine
-	public static final RegistryObject<BlockDungeonBrick> PRISMARINE_CARVED = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockPillarDungeonBrick> PRISMARINE_PILLAR = BLOCKS.register("", BlockPillarDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> PRISMARINE_CUBE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> PRISMARINE_SQUARE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> PRISMARINE_SMALL = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> PRISMARINE_LARGE = BLOCKS.register("", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> PRISMARINE_CARVED = BLOCKS.register("prismarine_carved", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockPillarDungeonBrick> PRISMARINE_PILLAR = BLOCKS.register("prismarine_pillar", BlockPillarDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> PRISMARINE_CUBE = BLOCKS.register("prismarine_cube", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> PRISMARINE_SQUARE = BLOCKS.register("prismarine_square", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> PRISMARINE_SMALL = BLOCKS.register("prismarine_small", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> PRISMARINE_LARGE = BLOCKS.register("prismarine_large", BlockDungeonBrick::new);
 
 	// Endstone
-	public static final RegistryObject<BlockDungeonBrick> ENDSTONE_CARVED = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockPillarDungeonBrick> ENDSTONE_PILLAR = BLOCKS.register("", BlockPillarDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> ENDSTONE_CUBE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> ENDSTONE_SCALE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> ENDSTONE_SQUARE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> ENDSTONE_SMALL = BLOCKS.register("", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ENDSTONE_CARVED = BLOCKS.register("endstone_carved", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockPillarDungeonBrick> ENDSTONE_PILLAR = BLOCKS.register("endstone_pillar", BlockPillarDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ENDSTONE_CUBE = BLOCKS.register("endstone_cube", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ENDSTONE_SCALE = BLOCKS.register("endstone_scale", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ENDSTONE_SQUARE = BLOCKS.register("endstone_square", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> ENDSTONE_SMALL = BLOCKS.register("endstone_small", BlockDungeonBrick::new);
 
 	// Purpur
-	public static final RegistryObject<BlockDungeonBrick> PURPUR_CARVED = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> PURPUR_CUBE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> PURPUR_SCALE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> PURPUR_SMALL = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> PURPUR_LARGE = BLOCKS.register("", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> PURPUR_CARVED = BLOCKS.register("purpur_carved", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> PURPUR_CUBE = BLOCKS.register("purpur_cube", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> PURPUR_SCALE = BLOCKS.register("purpur_scale", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> PURPUR_SMALL = BLOCKS.register("purpur_small", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> PURPUR_LARGE = BLOCKS.register("purpur_large", BlockDungeonBrick::new);
 
 	// Red Netherbrick
-	public static final RegistryObject<BlockDungeonBrick> RED_NETHERBRICK_CARVED = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockPillarDungeonBrick> RED_NETHERBRICK_PILLAR = BLOCKS.register("", BlockPillarDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> RED_NETHERBRICK_CUBE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> RED_NETHERBRICK_SCALE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> RED_NETHERBRICK_SQUARE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> RED_NETHERBRICK_LARGE = BLOCKS.register("", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> RED_NETHERBRICK_CARVED = BLOCKS.register("red_netherbrick_carved", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockPillarDungeonBrick> RED_NETHERBRICK_PILLAR = BLOCKS.register("red_netherbrick_pillar", BlockPillarDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> RED_NETHERBRICK_CUBE = BLOCKS.register("red_netherbrick_cube", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> RED_NETHERBRICK_SCALE = BLOCKS.register("red_netherbrick_scale", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> RED_NETHERBRICK_SQUARE = BLOCKS.register("red_netherbrick_square", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> RED_NETHERBRICK_LARGE = BLOCKS.register("red_netherbrick_large", BlockDungeonBrick::new);
 
 	// Stone
-	public static final RegistryObject<BlockPillarDungeonBrick> STONE_PILLAR = BLOCKS.register("", BlockPillarDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> STONE_CUBE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> STONE_SCALE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> STONE_SQUARE = BLOCKS.register("", BlockDungeonBrick::new);
-	public static final RegistryObject<BlockDungeonBrick> STONE_SMALL = BLOCKS.register("", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockPillarDungeonBrick> STONE_PILLAR = BLOCKS.register("stone_pillar", BlockPillarDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> STONE_CUBE = BLOCKS.register("stone_cube", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> STONE_SCALE = BLOCKS.register("stone_scale", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> STONE_SQUARE = BLOCKS.register("stone_square", BlockDungeonBrick::new);
+	public static final RegistryObject<BlockDungeonBrick> STONE_SMALL = BLOCKS.register("stone_small", BlockDungeonBrick::new);
 
 	// Other
-	public static final RegistryObject<BlockTable> TABLE_OAK = BLOCKS.register("", BlockTable::new);
-	public static final RegistryObject<BlockTable> TABLE_SPRUCE = BLOCKS.register("", BlockTable::new);
-	public static final RegistryObject<BlockTable> TABLE_BIRCH = BLOCKS.register("", BlockTable::new);
-	public static final RegistryObject<BlockTable> TABLE_JUNGLE = BLOCKS.register("", BlockTable::new);
-	public static final RegistryObject<BlockTable> TABLE_ACACIA = BLOCKS.register("", BlockTable::new);
-	public static final RegistryObject<BlockTable> TABLE_DARK = BLOCKS.register("", BlockTable::new);
-	public static final RegistryObject<BlockUnlitTorch> UNLIT_TORCH = BLOCKS.register("", BlockUnlitTorch::new);
+	//TODO: Register tables per wood type in the game dynamically
+	public static final RegistryObject<BlockTable> TABLE_OAK = BLOCKS.register("table_oak", BlockTable::new);
+	public static final RegistryObject<BlockTable> TABLE_SPRUCE = BLOCKS.register("table_spruce", BlockTable::new);
+	public static final RegistryObject<BlockTable> TABLE_BIRCH = BLOCKS.register("table_birch", BlockTable::new);
+	public static final RegistryObject<BlockTable> TABLE_JUNGLE = BLOCKS.register("table_jungle", BlockTable::new);
+	public static final RegistryObject<BlockTable> TABLE_ACACIA = BLOCKS.register("table_acacia", BlockTable::new);
+	public static final RegistryObject<BlockTable> TABLE_DARK = BLOCKS.register("table_dark", BlockTable::new);
+	//TODO: Maybe replace with a mixin into the torch block?
+	public static final RegistryObject<BlockUnlitTorch> UNLIT_TORCH = BLOCKS.register("unlit_torch", BlockUnlitTorch::new);
 
 	// Creative
-	public static final RegistryObject<BlockExporter> EXPORTER = BLOCKS.register("", BlockExporter::new);
-	public static final RegistryObject<BlockNull> NULL_BLOCK = BLOCKS.register("", BlockNull::new);
-	public static final RegistryObject<BlockSpawner> SPAWNER = BLOCKS.register("", BlockSpawner::new);
-	public static final RegistryObject<BlockBossBlock> BOSS_BLOCK = BLOCKS.register("", BlockBossBlock::new);
-	public static final RegistryObject<BlockForceFieldNexus> FORCE_FIELD_NEXUS = BLOCKS.register("", BlockForceFieldNexus::new);
-	public static final RegistryObject<BlockMapPlaceholder> MAP_PLACEHOLDER = BLOCKS.register("", BlockMapPlaceholder::new);
-	public static final RegistryObject<BlockTNTCQR> TNT = BLOCKS.register("", BlockTNTCQR::new);
+	public static final RegistryObject<BlockExporter> EXPORTER = BLOCKS.register("exporter", BlockExporter::new);
+	public static final RegistryObject<BlockNull> NULL_BLOCK = BLOCKS.register("null_block", BlockNull::new);
+	public static final RegistryObject<BlockSpawner> SPAWNER = BLOCKS.register("spawner", BlockSpawner::new);
+	public static final RegistryObject<BlockBossBlock> BOSS_BLOCK = BLOCKS.register("boss_block", BlockBossBlock::new);
+	public static final RegistryObject<BlockForceFieldNexus> FORCE_FIELD_NEXUS = BLOCKS.register("force_field_nexus", BlockForceFieldNexus::new);
+	public static final RegistryObject<BlockMapPlaceholder> MAP_PLACEHOLDER = BLOCKS.register("map_placeholder", BlockMapPlaceholder::new);
+	public static final RegistryObject<BlockTNTCQR> TNT = BLOCKS.register("tnt", BlockTNTCQR::new);
 
 	// Loot Chests
 	// CQR
-	public static final RegistryObject<BlockExporterChestCQR> EXPORTER_CHEST_VALUABLE = BLOCKS.register("", () -> new BlockExporterChestCQR(CQRLoottables.CHESTS_TREASURE, "textures/items/diamond.png"));
-	public static final RegistryObject<BlockExporterChestCQR> EXPORTER_CHEST_FOOD = BLOCKS.register("", () -> new BlockExporterChestCQR(CQRLoottables.CHESTS_FOOD, "textures/items/porkchop_raw.png"));
-	public static final RegistryObject<BlockExporterChestCQR> EXPORTER_CHEST_EQUIPMENT = BLOCKS.register("", () -> new BlockExporterChestCQR(CQRLoottables.CHESTS_EQUIPMENT, "textures/items/iron_pickaxe.png"));
-	public static final RegistryObject<BlockExporterChestCQR> EXPORTER_CHEST_UTILITY = BLOCKS.register("", () -> new BlockExporterChestCQR(CQRLoottables.CHESTS_MATERIAL, "textures/items/iron_ingot.png"));
-	public static final RegistryObject<BlockExporterChestCQR> EXPORTER_CHEST_CLUTTER = BLOCKS.register("", () -> new BlockExporterChestCQR(CQRLoottables.CHESTS_CLUTTER, "textures/items/gunpowder.png"));
+	public static final RegistryObject<BlockExporterChestCQR> EXPORTER_CHEST_VALUABLE = BLOCKS.register("exporter_chest_valueable", () -> new BlockExporterChestCQR(CQRLoottables.CHESTS_TREASURE, "textures/items/diamond.png"));
+	public static final RegistryObject<BlockExporterChestCQR> EXPORTER_CHEST_FOOD = BLOCKS.register("exporter_chest_food", () -> new BlockExporterChestCQR(CQRLoottables.CHESTS_FOOD, "textures/items/porkchop_raw.png"));
+	public static final RegistryObject<BlockExporterChestCQR> EXPORTER_CHEST_EQUIPMENT = BLOCKS.register("exporter_chest_equipment", () -> new BlockExporterChestCQR(CQRLoottables.CHESTS_EQUIPMENT, "textures/items/iron_pickaxe.png"));
+	public static final RegistryObject<BlockExporterChestCQR> EXPORTER_CHEST_UTILITY = BLOCKS.register("exporter_chest_utility", () -> new BlockExporterChestCQR(CQRLoottables.CHESTS_MATERIAL, "textures/items/iron_ingot.png"));
+	public static final RegistryObject<BlockExporterChestCQR> EXPORTER_CHEST_CLUTTER = BLOCKS.register("exporter_chest_clutter", () -> new BlockExporterChestCQR(CQRLoottables.CHESTS_CLUTTER, "textures/items/gunpowder.png"));
 
 	// Custom
-	public static final RegistryObject<BlockExporterChestCustom> EXPORTER_CHEST_CUSTOM = BLOCKS.register("", () -> new BlockExporterChestCustom("textures/items/blaze_rod.png"));
+	public static final RegistryObject<BlockExporterChestCustom> EXPORTER_CHEST_CUSTOM = BLOCKS.register("exporter_chest_custom", () -> new BlockExporterChestCustom("textures/items/blaze_rod.png"));
 
 	// Vanilla
 	public static final Set<RegistryObject<? extends BlockExporterChest>> VANILLA_LOOT_CHESTS = new HashSet<>();
 
 	// Technical
-	public static final RegistryObject<BlockPhylactery> PHYLACTERY = BLOCKS.register("", BlockPhylactery::new);
-	public static final RegistryObject<BlockTemporaryWeb> TEMPORARY_WEB = BLOCKS.register("", BlockTemporaryWeb::new);
+	public static final RegistryObject<BlockPhylactery> PHYLACTERY = BLOCKS.register("phylactery", BlockPhylactery::new);
+	public static final RegistryObject<BlockTemporaryWeb> TEMPORARY_WEB = BLOCKS.register("temporary_web", BlockTemporaryWeb::new);
 
 	public static void registerBlocks() {
 		LootTables.all().stream()
