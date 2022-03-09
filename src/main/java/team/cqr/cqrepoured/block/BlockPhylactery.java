efed94806d3439bb55411de1d915b39ae5675bcd
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-03-09 18:13:19 +0100

    update phylactery to 1.16

--- src/main/java/team/cqr/cqrepoured/block/BlockPhylactery.java
+++ src/main/java/team/cqr/cqrepoured/block/BlockPhylactery.java
@@ -3,86 +3,79 @@ package team.cqr.cqrepoured.block;
 import java.util.List;
 import java.util.Random;
 
-import net.minecraft.block.*;
-import net.minecraft.block.material.MaterialColor;
-import net.minecraft.block.material.Material;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.item.Items;
-import net.minecraft.item.Item;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.Explosion.Mode;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.world.server.ServerWorld;
 import team.cqr.cqrepoured.entity.boss.EntityCQRLich;
 
 public class BlockPhylactery extends Block {
 
 	private static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(0.25D, 0.0D, 0.25D, 0.75D, 1.0D, 0.75D);
+	private static final VoxelShape VOXEL_SHAPE = VoxelShapes.create(BOUNDING_BOX);
 
 	public BlockPhylactery() {
-		super(Material.GLASS);
-		this.setHardness(0.5F);
-		this.setLightOpacity(8);
-		this.setLightLevel(1.0F);
-		this.setSoundType(SoundType.GLASS);
-		this.setTickRandomly(true);
+		super(Properties
+				.of(Material.GLASS)
+				.strength(0.5F, 0.5F)
+				.sound(SoundType.GLASS)
+				.randomTicks()
+				.noDrops()
+				.lightLevel((state) -> 3)
+		);
 	}
 
 	@Override
-	public void onPlayerDestroy(World worldIn, BlockPos pos, BlockState state) {
-		super.onPlayerDestroy(worldIn, pos, state);
-		worldIn.createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 1.5F, true);
+	public void playerDestroy(World pLevel, PlayerEntity pPlayer, BlockPos pPos, BlockState pState, TileEntity pTe, ItemStack pStack) {
+		super.playerDestroy(pLevel, pPlayer, pPos, pState, pTe, pStack);
+		pLevel.explode(null, pPos.getX(), pPos.getY(), pPos.getZ(), 1.5F, Mode.BREAK);
 	}
-
-	@Deprecated
+	
 	@Override
-	public boolean isFullCube(BlockState state) {
-		return false;
+	public VoxelShape getVisualShape(BlockState pState, IBlockReader pReader, BlockPos pPos, ISelectionContext pContext) {
+		return VoxelShapes.empty();
 	}
-
-	@Deprecated
+	
 	@Override
-	public boolean isOpaqueCube(BlockState state) {
-		return false;
+	public boolean propagatesSkylightDown(BlockState pState, IBlockReader pReader, BlockPos pPos) {
+		return true;
 	}
-
+	
 	@Override
-	public Item getItemDropped(BlockState state, Random rand, int fortune) {
-		return Items.AIR;
+	public float getShadeBrightness(BlockState pState, IBlockReader pLevel, BlockPos pPos) {
+		return 1.0F;
 	}
-
-	@Deprecated
+	
 	@Override
-	public AxisAlignedBB getBoundingBox(BlockState state, IBlockAccess source, BlockPos pos) {
-		return BOUNDING_BOX;
+	public VoxelShape getShape(BlockState pState, IBlockReader pLevel, BlockPos pPos, ISelectionContext pContext) {
+		return VOXEL_SHAPE;
 	}
-
-	@Deprecated
+	
 	@Override
-	public BlockRenderType getRenderType(BlockState state) {
-		return BlockRenderType.MODEL;
-	}
-
-	@OnlyIn(Dist.CLIENT)
-	@Override
-	public BlockRenderLayer getRenderLayer() {
-		return BlockRenderLayer.CUTOUT;
-	}
-
-	@Override
-	public void randomTick(World worldIn, BlockPos pos, BlockState state, Random rand) {
-		super.randomTick(worldIn, pos, state, rand);
-		AxisAlignedBB aabb = new AxisAlignedBB(pos.add(3, 2, 3), pos.add(-3, -2, -3));
-		List<EntityCQRLich> lichesInRange = worldIn.getEntitiesWithinAABB(EntityCQRLich.class, aabb);
+	public void tick(BlockState pState, ServerWorld worldIn, BlockPos pos, Random pRand) {
+		super.tick(pState, worldIn, pos, pRand);
+		
+		AxisAlignedBB aabb = new AxisAlignedBB(pos.offset(3, 2, 3), pos.offset(-3, -2, -3));
+		List<EntityCQRLich> lichesInRange = worldIn.getEntitiesOfClass(EntityCQRLich.class, aabb);
 		if (!lichesInRange.isEmpty()) {
 			int i = 0;
 			while (i < lichesInRange.size()) {
 				EntityCQRLich lich = lichesInRange.get(i);
-				if (lich != null && !lich.isDead) {
+				if (lich != null && !lich.isDeadOrDying()) {
 					if (!lich.hasPhylactery()) {
 						lich.setCurrentPhylacteryBlock(pos);
 						i = lichesInRange.size();
@@ -92,6 +85,12 @@ public class BlockPhylactery extends Block {
 				}
 			}
 		}
+	}
+	
+	@Override
+	public void onProjectileHit(World pLevel, BlockState pState, BlockRayTraceResult pHit, ProjectileEntity pProjectile) {
+		this.destroy(pLevel, pHit.getBlockPos(), pState);
+		pLevel.explode(null, pHit.getBlockPos().getX(), pHit.getBlockPos().getY(), pHit.getBlockPos().getZ(), 1.5F, Mode.BREAK);
 	}
 
 }
