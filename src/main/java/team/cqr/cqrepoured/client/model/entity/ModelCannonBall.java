7553b0ad5d6463647401e2bea5550ad4a457e8df
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-03-09 19:25:41 +0100

    update cannon ball model

--- src/main/java/team/cqr/cqrepoured/client/model/entity/ModelCannonBall.java
+++ src/main/java/team/cqr/cqrepoured/client/model/entity/ModelCannonBall.java
@@ -1,39 +1,47 @@
 package team.cqr.cqrepoured.client.model.entity;
 
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelBox;
-import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.entity.Entity;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 
-// Made with Blockbench 4.0.0
-// Exported for Minecraft version 1.7 - 1.12
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import team.cqr.cqrepoured.entity.projectiles.ProjectileCannonBall;
+
+// Made with Blockbench 4.1.5
+// Exported for Minecraft version 1.15 - 1.16 with Mojang mappings
 // Paste this class into your mod and generate all required imports
 
-public class ModelCannonBall extends ModelBase {
+
+public class ModelCannonBall<T extends ProjectileCannonBall> extends EntityModel<T> {
 	private final ModelRenderer bb_main;
 
 	public ModelCannonBall() {
-		this.textureWidth = 16;
-		this.textureHeight = 16;
+		texWidth = 16;
+		texHeight = 16;
 
-		this.bb_main = new ModelRenderer(this);
-		this.bb_main.setRotationPoint(0.0F, 0.0F, 0.0F);
-		this.bb_main.cubeList.add(new ModelBox(this.bb_main, 0, 0, -2.0F, -1.0F, -2.0F, 4, 1, 4, 0.0F, false));
-		this.bb_main.cubeList.add(new ModelBox(this.bb_main, 0, 0, -2.0F, -5.0F, 2.0F, 4, 4, 1, 0.0F, false));
-		this.bb_main.cubeList.add(new ModelBox(this.bb_main, 0, 0, -2.0F, -5.0F, -3.0F, 4, 4, 1, 0.0F, false));
-		this.bb_main.cubeList.add(new ModelBox(this.bb_main, 0, 0, -2.0F, -6.0F, -2.0F, 4, 1, 4, 0.0F, false));
-		this.bb_main.cubeList.add(new ModelBox(this.bb_main, 0, 0, 2.0F, -5.0F, -2.0F, 1, 4, 4, 0.0F, false));
-		this.bb_main.cubeList.add(new ModelBox(this.bb_main, 0, 0, -3.0F, -5.0F, -2.0F, 1, 4, 4, 0.0F, false));
+		bb_main = new ModelRenderer(this);
+		bb_main.setPos(0.0F, 24.0F, 0.0F);
+		bb_main.texOffs(0, 0).addBox(-2.0F, -1.0F, -2.0F, 4.0F, 1.0F, 4.0F, 0.0F, false);
+		bb_main.texOffs(0, 0).addBox(-2.0F, -5.0F, 2.0F, 4.0F, 4.0F, 1.0F, 0.0F, false);
+		bb_main.texOffs(0, 0).addBox(-2.0F, -5.0F, -3.0F, 4.0F, 4.0F, 1.0F, 0.0F, false);
+		bb_main.texOffs(0, 0).addBox(-2.0F, -6.0F, -2.0F, 4.0F, 1.0F, 4.0F, 0.0F, false);
+		bb_main.texOffs(0, 0).addBox(2.0F, -5.0F, -2.0F, 1.0F, 4.0F, 4.0F, 0.0F, false);
+		bb_main.texOffs(0, 0).addBox(-3.0F, -5.0F, -2.0F, 1.0F, 4.0F, 4.0F, 0.0F, false);
 	}
 
 	@Override
-	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-		this.bb_main.render(f5);
+	public void setupAnim(ProjectileCannonBall entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
+		//previously the render function, render code was moved to a method below
+	}
+
+	@Override
+	public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		bb_main.render(matrixStack, buffer, packedLight, packedOverlay);
 	}
 
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-		modelRenderer.rotateAngleX = x;
-		modelRenderer.rotateAngleY = y;
-		modelRenderer.rotateAngleZ = z;
+		modelRenderer.xRot = x;
+		modelRenderer.yRot = y;
+		modelRenderer.zRot = z;
 	}
 }
