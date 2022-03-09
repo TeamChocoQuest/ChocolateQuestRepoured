fd3f4a3a1fb4b5aa6d126744390d87c88c964c6c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-03-09 18:13:08 +0100

    update DungeonBrick to 1.16

--- src/main/java/team/cqr/cqrepoured/block/BlockDungeonBrick.java
+++ src/main/java/team/cqr/cqrepoured/block/BlockDungeonBrick.java
@@ -7,11 +7,16 @@ import net.minecraft.block.material.Material;
 public class BlockDungeonBrick extends Block {
 
 	public BlockDungeonBrick() {
-		super(Material.ROCK);
-
-		this.setSoundType(SoundType.STONE);
-		this.setHardness(1.5F);
-		this.setResistance(10.0F);
+		this(Properties.
+				of(Material.STONE)
+				.sound(SoundType.STONE)
+				.requiresCorrectToolForDrops()
+				.strength(1.5F, 10.0F)
+		);
+	}
+	
+	protected BlockDungeonBrick(Properties prop) {
+		super(prop);
 	}
 
 }
