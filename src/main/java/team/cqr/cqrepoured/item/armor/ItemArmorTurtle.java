426be23a96362416e5278af2dd592e698654213b
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-03-15 18:28:36 +0100

    update ItemARmorTurtle

--- src/main/java/team/cqr/cqrepoured/item/armor/ItemArmorTurtle.java
+++ src/main/java/team/cqr/cqrepoured/item/armor/ItemArmorTurtle.java
@@ -2,48 +2,49 @@ package team.cqr.cqrepoured.item.armor;
 
 import java.util.List;
 
-import javax.annotation.Nullable;
-
 import com.google.common.collect.Multimap;
 
-import net.java.games.input.Keyboard;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.ModelBiped;
-import net.minecraft.client.resources.I18n;
+import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.AttributeModifier.Operation;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import team.cqr.cqrepoured.capability.armor.CapabilityCooldownHandlerHelper;
 import team.cqr.cqrepoured.client.init.CQRArmorModels;
 import team.cqr.cqrepoured.init.CQRItems;
+import team.cqr.cqrepoured.item.ItemLore;
 
 public class ItemArmorTurtle extends ArmorItem {
 
 	private AttributeModifier health;
 
-	public ItemArmorTurtle(ArmorMaterial materialIn, int renderIndexIn, EquipmentSlotType equipmentSlotIn) {
-		super(materialIn, renderIndexIn, equipmentSlotIn);
+	public ItemArmorTurtle(IArmorMaterial materialIn, EquipmentSlotType equipmentSlotIn, Properties prop) {
+		super(materialIn, equipmentSlotIn, prop);
 
-		this.health = new AttributeModifier("TurtleHealthModifier", 2.0D, 0);
+		this.health = new AttributeModifier("TurtleHealthModifier", 2.0D, Operation.ADDITION);
 	}
 
 	@Override
-	public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
-		Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(slot, stack);
+	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
+		Multimap<Attribute, AttributeModifier> multimap = super.getAttributeModifiers(slot, stack);
 
-		if (slot == MobEntity.getSlotForItemStack(stack)) {
-			multimap.put(Attributes.MAX_HEALTH.getName(), this.health);
+		if (slot == MobEntity.getEquipmentSlotForItem(stack)) {
+			multimap.put(Attributes.MAX_HEALTH, this.health);
 		}
 
 		return multimap;
@@ -51,20 +52,16 @@ public class ItemArmorTurtle extends ArmorItem {
 
 	@Override
 	@OnlyIn(Dist.CLIENT)
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		PlayerEntity player = Minecraft.getMinecraft().player;
+	public void appendHoverText(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		PlayerEntity player = Minecraft.getInstance().player;
 		if (player != null) {
 			int cooldown = CapabilityCooldownHandlerHelper.getCooldown(player, CQRItems.CHESTPLATE_TURTLE);
 			if (cooldown > 0) {
-				tooltip.add(TextFormatting.RED + I18n.format("description.turtle_armor_charging.name") + this.convertCooldown(cooldown));
+				tooltip.add(new TranslationTextComponent(TextFormatting.RED + "description.turtle_armor_charging.name", this.convertCooldown(cooldown)));
 			}
 		}
-
-		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description.turtle_armor.name"));
-		} else {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
-		}
+		
+		ItemLore.addHoverTextLogic(tooltip, flagIn, this.getRegistryName().getPath());
 	}
 
 	private String convertCooldown(int cd) {
@@ -78,12 +75,12 @@ public class ItemArmorTurtle extends ArmorItem {
 
 		return minutes + ":" + seconds;
 	}
-
-	@Override
+	
+	@SuppressWarnings("unchecked")
 	@OnlyIn(Dist.CLIENT)
-	@Nullable
-	public ModelBiped getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, ModelBiped _default) {
-		return armorSlot == EquipmentSlotType.LEGS ? CQRArmorModels.turtleArmorLegs : CQRArmorModels.turtleArmor;
+	@Override
+	public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+		return armorSlot == EquipmentSlotType.LEGS ? (A) CQRArmorModels.TURTLE_ARMOR_LEGS : (A) CQRArmorModels.TURTLE_ARMOR;
 	}
 
 }
