a5f93eadc717744e9d455a58d3d3478e02cd7d6b
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-03-16 19:07:48 +0100

    update base multipart part renderer

--- src/main/java/team/cqr/cqrepoured/client/render/entity/RenderMultiPartPart.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/RenderMultiPartPart.java
@@ -1,31 +1,38 @@
 package team.cqr.cqrepoured.client.render.entity;
 
-import net.minecraft.client.renderer.culling.ICamera;
-import net.minecraftforge.entity.PartEntity;
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.culling.ClippingHelper;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.entity.PartEntity;
 
-public class RenderMultiPartPart<T extends PartEntity> extends Render<T> {
+public class RenderMultiPartPart<T extends PartEntity<?>> extends EntityRenderer<T> {
 
-	public RenderMultiPartPart(RenderManager renderManager) {
-		super(renderManager);
+	protected RenderMultiPartPart(EntityRendererManager p_i46179_1_) {
+		super(p_i46179_1_);
 	}
 
-	protected boolean superShouldRender(T livingEntity, ICamera camera, double camX, double camY, double camZ) {
+	protected boolean superShouldRender(T livingEntity, ClippingHelper camera, double camX, double camY, double camZ) {
 		return super.shouldRender(livingEntity, camera, camX, camY, camZ);
 	}
 
 	@Override
-	public boolean shouldRender(T livingEntity, ICamera camera, double camX, double camY, double camZ) {
+	public boolean shouldRender(T livingEntity, ClippingHelper camera, double camX, double camY, double camZ) {
 		return false;
 	}
 
 	@Override
-	protected ResourceLocation getEntityTexture(T entity) {
-		return null;
+	public void render(T pEntity, float pEntityYaw, float pPartialTicks, MatrixStack pMatrixStack, IRenderTypeBuffer pBuffer, int pPackedLight) {
+		super.render(pEntity, pEntityYaw, pPartialTicks, pMatrixStack, pBuffer, pPackedLight);
 	}
-
+	
 	@Override
-	public void doRender(T entity, double x, double y, double z, float entityYaw, float partialTicks) {
+	public ResourceLocation getTextureLocation(T pEntity) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
