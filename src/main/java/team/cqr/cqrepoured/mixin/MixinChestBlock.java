641fe5be8ea4503809b1773b81955114d1df15e0
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-03-17 16:31:17 +0100

    Fix vanilla chests not connecting to cqr chests

---
+++ src/main/java/team/cqr/cqrepoured/mixin/MixinChestBlock.java
@@ -0,0 +1,24 @@
+package team.cqr.cqrepoured.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.ChestBlock;
+import team.cqr.cqrepoured.block.BlockExporterChest;
+
+@Mixin(ChestBlock.class)
+public abstract class MixinChestBlock extends Block {
+
+	public MixinChestBlock(Properties properties) {
+		super(properties);
+	}
+
+	@Override
+	public boolean is(Block block) {
+		if (((Block) (Object) this).getClass() == ChestBlock.class && block instanceof BlockExporterChest) {
+			return true;
+		}
+		return super.is(block);
+	}
+
+}
