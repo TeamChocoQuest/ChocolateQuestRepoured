b7e15bae2cb3574f9dbbed77d96d2187434a00f3
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-03-17 16:20:09 +0100

    Fix for NPE (temporary)

--- src/main/java/team/cqr/cqrepoured/tileentity/TileEntitySpawner.java
+++ src/main/java/team/cqr/cqrepoured/tileentity/TileEntitySpawner.java
@@ -85,6 +85,7 @@ public class TileEntitySpawner extends BlockEntityContainer implements ITileEnti
 
 	@Override
 	public void tick() {
+		if (true) return;
 		if (!this.level.isClientSide && this.level.getDifficulty() != Difficulty.PEACEFUL && this.isNonCreativePlayerInRange(CQRConfig.general.spawnerActivationDistance)) {
 			this.turnBackIntoEntity();
 		} else {
