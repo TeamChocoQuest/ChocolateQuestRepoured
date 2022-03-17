d07e73c8cb7a5f03b0260367f63c2f565ea2ff87
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-03-17 16:18:40 +0100

    Clean up TileEntityExporterChest

--- src/main/java/team/cqr/cqrepoured/tileentity/TileEntityExporterChest.java
+++ src/main/java/team/cqr/cqrepoured/tileentity/TileEntityExporterChest.java
@@ -6,9 +6,8 @@ import net.minecraft.util.ResourceLocation;
 
 public abstract class TileEntityExporterChest extends TileEntity {
 
-	public TileEntityExporterChest(TileEntityType<? extends TileEntityExporterChest> p_i48289_1_) {
-		super(p_i48289_1_);
-		// TODO Auto-generated constructor stub
+	public TileEntityExporterChest(TileEntityType<? extends TileEntityExporterChest> type) {
+		super(type);
 	}
 
 	public abstract ResourceLocation getLootTable();
