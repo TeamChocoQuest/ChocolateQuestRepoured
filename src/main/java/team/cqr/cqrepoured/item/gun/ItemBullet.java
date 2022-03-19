6ddf99cc59b1f629ffc2becd4b03b63f9aea2ac7
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-03-19 12:50:59 +0100

    Update ItemBullet.java

--- src/main/java/team/cqr/cqrepoured/item/gun/ItemBullet.java
+++ src/main/java/team/cqr/cqrepoured/item/gun/ItemBullet.java
@@ -4,36 +4,47 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 import team.cqr.cqrepoured.init.CQRItems;
 
-public class ItemBullet extends Item {
+public class ItemBullet extends Item
+{
+	public ItemBullet(Properties properties)
+	{
+		super(properties);
+	}
 
 	@Override
 	@OnlyIn(Dist.CLIENT)
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		if (stack.getItem() == CQRItems.BULLET_IRON) {
-			tooltip.add(TextFormatting.BLUE + "+2.5 " + I18n.format("description.bullet_damage.name"));
+	public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		if (stack.getItem() == CQRItems.BULLET_IRON.get())
+		{
+			tooltip.add(new StringTextComponent("+2.5 " + new TranslationTextComponent("description.bullet_damage.name")).withStyle(TextFormatting.BLUE));
+			//tooltip.add(TextFormatting.BLUE + "+2.5 " + I18n.exists("description.bullet_damage.name"));
 		}
 
-		if (stack.getItem() == CQRItems.BULLET_GOLD) {
-			tooltip.add(TextFormatting.BLUE + "+3.75 " + I18n.format("description.bullet_damage.name"));
+		if (stack.getItem() == CQRItems.BULLET_GOLD.get())
+		{
+			tooltip.add(new StringTextComponent("+3.75 " + new TranslationTextComponent("description.bullet_damage.name")).withStyle(TextFormatting.BLUE));
 		}
 
-		if (stack.getItem() == CQRItems.BULLET_DIAMOND) {
-			tooltip.add(TextFormatting.BLUE + "+5 " + I18n.format("description.bullet_damage.name"));
+		if (stack.getItem() == CQRItems.BULLET_DIAMOND.get()) {
+			tooltip.add(new StringTextComponent("+5 " + new TranslationTextComponent("description.bullet_damage.name")).withStyle(TextFormatting.BLUE));
 		}
 
-		if (stack.getItem() == CQRItems.BULLET_FIRE) {
-			tooltip.add(TextFormatting.RED + "+5 " + I18n.format("description.bullet_damage.name"));
-			tooltip.add(TextFormatting.DARK_RED + I18n.format("description.bullet_fire.name"));
+		if (stack.getItem() == CQRItems.BULLET_FIRE.get())
+		{
+			tooltip.add(new StringTextComponent("+5 " + new TranslationTextComponent("description.bullet_damage.name")).withStyle(TextFormatting.RED));
+			tooltip.add(new TranslationTextComponent("description.bullet_fire.name").withStyle(TextFormatting.DARK_RED));
 		}
 	}
 
