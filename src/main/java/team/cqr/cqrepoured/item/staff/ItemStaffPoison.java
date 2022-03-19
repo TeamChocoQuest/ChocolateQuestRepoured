d2df8bc255e34c31655feae8d2bed82de81be6da
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-03-19 13:17:43 +0100

    Update ItemStaffPoison.java

--- src/main/java/team/cqr/cqrepoured/item/staff/ItemStaffPoison.java
+++ src/main/java/team/cqr/cqrepoured/item/staff/ItemStaffPoison.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
@@ -18,52 +17,55 @@ import team.cqr.cqrepoured.item.ItemLore;
 
 public class ItemStaffPoison extends ItemLore implements IRangedWeapon {
 
-	public ItemStaffPoison() {
-		this.setMaxDamage(2048);
-		this.setMaxStackSize(1);
+	public ItemStaffPoison(Properties properties)
+	{
+		super(properties.durability(2048).stacksTo(1));
+		//this.setMaxDamage(2048);
+		//this.setMaxStackSize(1);
 	}
 
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-		ItemStack stack = playerIn.getHeldItem(handIn);
+	public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn) {
+		ItemStack stack = playerIn.getItemInHand(handIn);
 		this.shoot(stack, worldIn, playerIn, handIn);
-		return new ActionResult<>(ActionResultType.SUCCESS, stack);
+		return ActionResult.success(stack);
 	}
 
 	public void shoot(ItemStack stack, World worldIn, PlayerEntity player, Hand handIn) {
-		worldIn.playSound(player.posX, player.posY, player.posZ, SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.MASTER, 4.0F, (1.0F + (itemRand.nextFloat() - itemRand.nextFloat()) * 0.2F) * 0.7F, false);
-		player.swingArm(handIn);
+		worldIn.playLocalSound(player.position().x, player.position().y, player.position().z, SoundEvents.SNOWBALL_THROW, SoundCategory.MASTER, 4.0F, (1.0F + (random.nextFloat() - random.nextFloat()) * 0.2F) * 0.7F, false);
+		player.swing(handIn);
 
-		if (!worldIn.isRemote) {
+		if (!worldIn.isClientSide) {
 			ProjectilePoisonSpell spell = new ProjectilePoisonSpell(worldIn, player);
-			spell.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, 2.0F, 0F);
-			worldIn.spawnEntity(spell);
-			stack.damageItem(1, player);
-			player.getCooldownTracker().setCooldown(stack.getItem(), 20);
+			spell.shootFromRotation(player, player.xRot, player.yRot, 0.0F, 2.0F, 0F);
+			worldIn.addFreshEntity(spell);
+			stack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(handIn));
+			player.getCooldowns().addCooldown(stack.getItem(), 20);
 		}
 	}
 
 	@Override
 	public void shoot(World worldIn, LivingEntity shooter, Entity target, Hand handIn) {
-		shooter.swingArm(handIn);
+		shooter.swing(handIn);
 
-		if (!worldIn.isRemote) {
+		if (!worldIn.isClientSide) {
 			ProjectilePoisonSpell spell = new ProjectilePoisonSpell(worldIn, shooter);
 			Vector3d v = target.position().subtract(shooter.position());
 			v = v.normalize();
 			v = v.scale(2D);
 			// spell.setVelocity(v.x, v.y, v.z);
-			spell.motionX = v.x;
-			spell.motionY = v.y;
-			spell.motionZ = v.z;
-			spell.velocityChanged = true;
-			worldIn.spawnEntity(spell);
+			spell.setDeltaMovement(v);
+			//spell.motionX = v.x;
+			//spell.motionY = v.y;
+			//spell.motionZ = v.z;
+			//spell.velocityChanged = true;
+			worldIn.addFreshEntity(spell);
 		}
 	}
 
 	@Override
 	public SoundEvent getShootSound() {
-		return SoundEvents.ENTITY_SNOWBALL_THROW;
+		return SoundEvents.SNOWBALL_THROW;
 	}
 
 	@Override
