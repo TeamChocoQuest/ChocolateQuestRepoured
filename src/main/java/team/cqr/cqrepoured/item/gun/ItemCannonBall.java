7aa89be584fbc5e7b3b6d90c4a71e5de65eb099a
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-03-19 12:51:02 +0100

    Update ItemCannonBall.java

--- src/main/java/team/cqr/cqrepoured/item/gun/ItemCannonBall.java
+++ src/main/java/team/cqr/cqrepoured/item/gun/ItemCannonBall.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -16,12 +18,16 @@ import team.cqr.cqrepoured.init.CQRItems;
 
 public class ItemCannonBall extends Item {
 
-	@Override
-	@OnlyIn(Dist.CLIENT)
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		if (stack.getItem() == CQRItems.CANNON_BALL) {
-			tooltip.add(TextFormatting.BLUE + "+5 " + I18n.format("description.bullet_damage.name"));
-		}
+	public ItemCannonBall(Properties properties)
+	{
+		super(properties);
 	}
 
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		if (stack.getItem() == CQRItems.CANNON_BALL.get()) {
+			tooltip.add(new StringTextComponent("+5 " + new TranslationTextComponent("description.bullet_damage.name")).withStyle(TextFormatting.BLUE));
+		}
+	}
 }
