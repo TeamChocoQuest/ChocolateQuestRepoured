505c0e8cb879ff87c795ba73a52f8919c5742e85
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-03-19 13:22:29 +0100

    Update ItemStaffVampiric.java

--- src/main/java/team/cqr/cqrepoured/item/staff/ItemStaffVampiric.java
+++ src/main/java/team/cqr/cqrepoured/item/staff/ItemStaffVampiric.java
@@ -1,78 +1,67 @@
 package team.cqr.cqrepoured.item.staff;
 
-import java.util.List;
-
-import javax.annotation.Nullable;
-
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.*;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import org.lwjgl.input.Keyboard;
-
-import net.minecraft.client.resources.I18n;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.SoundEvents;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
 import team.cqr.cqrepoured.entity.projectiles.ProjectileVampiricSpell;
 import team.cqr.cqrepoured.item.IRangedWeapon;
 import team.cqr.cqrepoured.item.ItemLore;
 
 public class ItemStaffVampiric extends ItemLore implements IRangedWeapon {
 
-	public ItemStaffVampiric() {
-		this.setMaxDamage(2048);
-		this.setMaxStackSize(1);
+	public ItemStaffVampiric(Properties properties)
+	{
+		super(properties.durability(2048).stacksTo(1));
+		//this.setMaxDamage(2048);
+		//this.setMaxStackSize(1);
 	}
 
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-		ItemStack stack = playerIn.getHeldItem(handIn);
+	public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn) {
+		ItemStack stack = playerIn.getItemInHand(handIn);
 		this.shoot(stack, worldIn, playerIn, handIn);
-		return new ActionResult<>(ActionResultType.SUCCESS, stack);
+		return ActionResult.success(stack);
 	}
 
 	public void shoot(ItemStack stack, World worldIn, PlayerEntity player, Hand handIn) {
-		worldIn.playSound(player.posX, player.posY, player.posZ, SoundEvents.ENTITY_ENDERPEARL_THROW, SoundCategory.MASTER, 4.0F, (1.0F + (itemRand.nextFloat() - itemRand.nextFloat()) * 0.2F) * 0.7F, false);
-		player.swingArm(handIn);
+		worldIn.playLocalSound(player.position().x, player.position().y, player.position().z, SoundEvents.ENDER_PEARL_THROW, SoundCategory.MASTER, 4.0F, (1.0F + (random.nextFloat() - random.nextFloat()) * 0.2F) * 0.7F, false);
+		player.swing(handIn);
 
-		if (!worldIn.isRemote) {
+		if (!worldIn.isClientSide) {
 			ProjectileVampiricSpell spell = new ProjectileVampiricSpell(worldIn, player);
-			spell.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, 2.0F, 0F);
-			worldIn.spawnEntity(spell);
-			stack.damageItem(1, player);
-			player.getCooldownTracker().setCooldown(stack.getItem(), 20);
+			spell.shootFromRotation(player, player.xRot, player.yRot, 0.0F, 2.0F, 0F);
+			worldIn.addFreshEntity(spell);
+			stack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(handIn));
+			player.getCooldowns().addCooldown(stack.getItem(), 20);
 		}
 	}
 
 	@Override
 	public void shoot(World worldIn, LivingEntity shooter, Entity target, Hand handIn) {
-		shooter.swingArm(handIn);
+		shooter.swing(handIn);
 
-		if (!worldIn.isRemote) {
+		if (!worldIn.isClientSide) {
 			ProjectileVampiricSpell spell = new ProjectileVampiricSpell(worldIn, shooter);
 			Vector3d v = target.position().subtract(shooter.position());
 			v = v.normalize();
 			v = v.scale(0.75D);
+			spell.setDeltaMovement(v);
 			// spell.setVelocity(v.x, v.y, v.z);
-			spell.motionX = v.x;
-			spell.motionY = v.y;
-			spell.motionZ = v.z;
-			spell.velocityChanged = true;
-			worldIn.spawnEntity(spell);
+			//spell.motionX = v.x;
+			//spell.motionY = v.y;
+			//spell.motionZ = v.z;
+			//spell.velocityChanged = true;
+			worldIn.addFreshEntity(spell);
 		}
 	}
 
 	@Override
 	public SoundEvent getShootSound() {
-		return SoundEvents.ENTITY_ENDERPEARL_THROW;
+		return SoundEvents.ENDER_PEARL_THROW;
 	}
 
 	@Override
