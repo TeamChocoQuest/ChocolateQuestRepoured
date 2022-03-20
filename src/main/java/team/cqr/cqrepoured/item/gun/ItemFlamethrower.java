f333492debdcdf65c6ed547a70bf97fe52518bd2
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-03-20 21:56:19 +0100

    Update ItemFlamethrower.java

--- src/main/java/team/cqr/cqrepoured/item/gun/ItemFlamethrower.java
+++ src/main/java/team/cqr/cqrepoured/item/gun/ItemFlamethrower.java
@@ -1,7 +1,6 @@
 package team.cqr.cqrepoured.item.gun;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.Entity;
@@ -12,7 +11,6 @@ import net.minecraft.item.UseAction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
@@ -22,64 +20,59 @@ import team.cqr.cqrepoured.item.ItemMagazineBased;
 
 public class ItemFlamethrower extends ItemMagazineBased {
 
-	public ItemFlamethrower() {
-		super(new Predicate<ItemStack>() {
-
-			@Override
-			public boolean test(ItemStack t) {
-				return t != null && t.getItem() == Items.SLIME_BALL;
-			}
-		});
+	public ItemFlamethrower(Properties properties)
+	{
+		super(properties, t -> t != null && t.getItem() == Items.SLIME_BALL);
 	}
 
 	@Override
-	public int getMaxItemUseDuration(ItemStack stack) {
+	public int getUseDuration(ItemStack stack) {
 		return this.getAmmoInItem(stack);
 	}
 
 	@Override
-	public UseAction getItemUseAction(ItemStack stack) {
+	public UseAction getUseAnimation(ItemStack stack) {
 		return UseAction.BOW;
 	}
 
 	public void shootFlames(LivingEntity entity) {
-		World world = entity.world;
-		float rotationYaw = MathHelper.wrapDegrees(entity.rotationYawHead);
+		World world = entity.level;
+		float rotationYaw = MathHelper.wrapDegrees(entity.yHeadRot);
 		double armDist = 1.0D;
 		if(entity instanceof ISizable) {
 			armDist *= ((ISizable)entity).getSizeVariation();
 		}
-		double offY = entity.height * 0.75D;
-		double posX = entity.posX - Math.sin(Math.toRadians(rotationYaw)) * armDist;
-		double posY = entity.posY + offY;
-		double posZ = entity.posZ + Math.cos(Math.toRadians(rotationYaw)) * armDist;
+		double offY = entity.getBbHeight() * 0.75D;
+		double posX = entity.position().x - Math.sin(Math.toRadians(rotationYaw)) * armDist;
+		double posY = entity.position().y + offY;
+		double posZ = entity.position().z + Math.cos(Math.toRadians(rotationYaw)) * armDist;
 
 		float vx = (float) -Math.sin(Math.toRadians(rotationYaw));
 		float vz = (float) Math.cos(Math.toRadians(rotationYaw));
-		double vy = -Math.sin(Math.toRadians(entity.rotationPitch));
+		double vy = -Math.sin(Math.toRadians(entity.xRot));
 		vx = (float) (vx * (1.0D - Math.abs(vy)));
 		vz = (float) (vz * (1.0D - Math.abs(vy)));
 
-		if (world.isRemote) {
+		if (world.isClientSide) {
 			for (int i = 0; i < 8; i++) {
-				world.spawnParticle(ParticleTypes.FLAME, posX, posY, posZ, (vx + itemRand.nextFloat() - 0.5D) / 3.0D, (vy + itemRand.nextFloat() - 0.5D) / 8.0D, (vz + itemRand.nextFloat() - 0.5D) / 3.0D);
+				world.addParticle(ParticleTypes.FLAME, posX, posY, posZ, (vx + random.nextFloat() - 0.5D) / 3.0D, (vy + random.nextFloat() - 0.5D) / 8.0D, (vz + random.nextFloat() - 0.5D) / 3.0D);
 			}
 		} else {
 			int dist = 10;
-			List<Entity> list = world.getEntitiesWithinAABBExcludingEntity(entity, entity.getEntityBoundingBox().grow(entity.getLookVec().x * dist, entity.getLookVec().y * dist, entity.getLookVec().z * dist).expand(1.0D, 1.0D, 1.0D));
+			List<Entity> list = world.getEntities(entity, entity.getBoundingBox().inflate(entity.getLookAngle().x * dist, entity.getLookAngle().y * dist, entity.getLookAngle().z * dist).expandTowards(1.0D, 1.0D, 1.0D));
 
 			for (Entity e : list) {
-				if (e instanceof LivingEntity && !e.isWet() && !e.isBeingRidden()) {
-					double d = posX - e.posX;
-					double d2 = posZ - e.posZ;
+				if (e instanceof LivingEntity && !e.isInWaterOrRain() && !e.isVehicle()) {
+					double d = posX - e.position().x;
+					double d2 = posZ - e.position().z;
 					double rotDiff = Math.atan2(d, d2);
 					rotDiff = rotDiff * 180.0D / 3.141592D;
 					rotDiff = -MathHelper.wrapDegrees(rotDiff - 180.0D);
 					rotDiff -= rotationYaw;
 
 					if (Math.abs(rotDiff) < 30.0D) {
-						e.setFire(2);
-						e.attackEntityFrom(DamageSource.IN_FIRE, 2.5F);
+						e.setSecondsOnFire(2);
+						e.hurt(DamageSource.IN_FIRE, 2.5F);
 					}
 				}
 			}
@@ -87,25 +80,25 @@ public class ItemFlamethrower extends ItemMagazineBased {
 	}
 
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-		if (this.getAmmoInItem(playerIn.getHeldItem(handIn)) <= 0) {
-			return new ActionResult<>(ActionResultType.FAIL, playerIn.getHeldItem(handIn));
+	public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn) {
+		if (this.getAmmoInItem(playerIn.getItemInHand(handIn)) <= 0) {
+			return ActionResult.fail(playerIn.getItemInHand(handIn));
 		}
-		playerIn.setActiveHand(handIn);
-		return new ActionResult<>(ActionResultType.SUCCESS, playerIn.getHeldItem(handIn));
+		playerIn.startUsingItem(handIn);
+		return ActionResult.success(playerIn.getItemInHand(handIn));
 	}
 
 	@Override
-	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
-		super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
+	public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+		super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
 		if (entityIn instanceof LivingEntity) {
 			LivingEntity user = (LivingEntity) entityIn;
-			if (user.getActiveItemStack() == stack) {
-				if (user.isHandActive()) {
+			if (user.getUseItem() == stack) {
+				if (user.isUsingItem()) {
 					this.shootFlames((LivingEntity) entityIn);
 					this.removeAmmoFromItem(stack, 1);
 				}
-			} else if ((((LivingEntity) entityIn).getHeldItemMainhand() == stack || ((LivingEntity) entityIn).getMainHandItem() == stack) && entityIn.ticksExisted % 5 == 0 && this.getAmmoInItem(stack) < this.getMaxAmmo()) {
+			} else if ((((LivingEntity) entityIn).getMainHandItem() == stack || ((LivingEntity) entityIn).getMainHandItem() == stack) && entityIn.tickCount % 5 == 0 && this.getAmmoInItem(stack) < this.getMaxAmmo()) {
 				if (entityIn instanceof PlayerEntity) {
 					this.reloadFromInventory(((PlayerEntity) user).inventory, stack, !((PlayerEntity) user).isCreative());
 				}
