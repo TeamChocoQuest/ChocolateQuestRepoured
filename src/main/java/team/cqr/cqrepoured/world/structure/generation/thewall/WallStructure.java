681c9ec0c177fd906cc01bc9b34236509de72a30
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-03-22 18:58:00 +0100

    add-in roofed variants for snow biomes

--- src/main/java/team/cqr/cqrepoured/world/structure/generation/thewall/WallStructure.java
+++ src/main/java/team/cqr/cqrepoured/world/structure/generation/thewall/WallStructure.java
@@ -11,9 +11,11 @@ import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biome.Category;
 import net.minecraft.world.biome.provider.BiomeProvider;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.GenerationStage.Decoration;
+import net.minecraft.world.gen.Heightmap.Type;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.structure.AbstractVillagePiece;
 import net.minecraft.world.gen.feature.structure.Structure;
@@ -59,16 +61,19 @@ public class WallStructure extends Structure<NoFeatureConfig> {
 
 		final ResourceLocation START_POOL_WALL = CQRMain.prefix("wall/start_wall");
 		final ResourceLocation START_POOL_TOWER = CQRMain.prefix("wall/start_tower");
+		final ResourceLocation START_POOL_WALL_ROOFED = CQRMain.prefix("wall/start_wall_roofed");
+		final ResourceLocation START_POOL_TOWER_ROOFED = CQRMain.prefix("wall/start_tower_roofed");
 		
 		@Override
 		public void generatePieces(DynamicRegistries dynamicRegistryManager, ChunkGenerator chunkGenerator, TemplateManager templateManagerIn, int chunkX, int chunkZ, Biome biomeIn, NoFeatureConfig config) {
 			boolean tower = (chunkX % 3/*CQRConfig.wall.towerDistance*/) == 0;
-			BlockPos centered = new BlockPos((chunkX * 16) -16, 140, (chunkZ * 16) -16);
+			BlockPos centered = new BlockPos(chunkX * 16, 140, chunkZ * 16);
 			ResourceLocation startPool;
+			boolean isSnowy = this.isSnowChunk(chunkX, chunkZ, chunkGenerator);
 			if(tower) {
-				startPool = START_POOL_TOWER;
+				startPool = isSnowy ? START_POOL_TOWER_ROOFED : START_POOL_TOWER;
 			} else {
-				startPool = START_POOL_WALL;
+				startPool = isSnowy ? START_POOL_WALL_ROOFED : START_POOL_WALL;
 			}
 			
 			CQRJigsawManager.addPieces(
@@ -86,6 +91,25 @@ public class WallStructure extends Structure<NoFeatureConfig> {
 			);
 			
 			this.calculateBoundingBox();
+		}
+		
+		static final int TOTAL_CHUNK_AREA = 256; //16 * 16
+		
+		private boolean isSnowChunk(int chunkX, int chunkZ, ChunkGenerator cg) {
+			int startX = chunkX * 16;
+			int startZ = chunkZ * 16;
+			int snowyBiomeBlocks = 0;
+			for(int ix = startX; ix < startX + 16; ix++) {
+				for(int iz = startZ; iz < startZ + 16; iz++) {
+					int y = cg.getFirstFreeHeight(ix, iz, Type.WORLD_SURFACE);
+					for(Biome b : cg.getBiomeSource().getBiomesWithin(ix, y, iz, 2)) {
+						if(b.getBiomeCategory() == Category.ICY) {
+							snowyBiomeBlocks++;
+						}
+					}
+				}
+			}
+			return (snowyBiomeBlocks / TOTAL_CHUNK_AREA) >= 0.25;
 		}
 		
 	}
