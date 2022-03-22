0c2339eebb536ca7b2eee02da175acdf6fe5afc5
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-03-22 20:40:38 +0100

    Update ItemBullBattleAxe.java

--- src/main/java/team/cqr/cqrepoured/item/ItemBullBattleAxe.java
+++ src/main/java/team/cqr/cqrepoured/item/ItemBullBattleAxe.java
@@ -1,93 +1,132 @@
 package team.cqr.cqrepoured.item;
 
 import com.google.common.collect.Multimap;
-import net.java.games.input.Keyboard;
+import com.google.common.collect.Sets;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.item.SwordItem;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ToolType;
 import team.cqr.cqrepoured.entity.projectiles.ProjectileEarthQuake;
 
 import javax.annotation.Nullable;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 public class ItemBullBattleAxe extends SwordItem {
 
-	public ItemBullBattleAxe(ToolMaterial material) {
-		super(material);
+	private static final Set<Material> DIGGABLE_MATERIALS = Sets.newHashSet(Material.WOOD, Material.NETHER_WOOD, Material.PLANT, Material.REPLACEABLE_PLANT, Material.BAMBOO, Material.VEGETABLE);
+	private static final Set<Block> OTHER_DIGGABLE_BLOCKS = Sets.newHashSet(Blocks.LADDER, Blocks.SCAFFOLDING, Blocks.OAK_BUTTON, Blocks.SPRUCE_BUTTON, Blocks.BIRCH_BUTTON, Blocks.JUNGLE_BUTTON, Blocks.DARK_OAK_BUTTON, Blocks.ACACIA_BUTTON, Blocks.CRIMSON_BUTTON, Blocks.WARPED_BUTTON);
+
+	public ItemBullBattleAxe(IItemTier material, int attackDamage, Properties props)
+	{
+		super(material, attackDamage, material.getSpeed(), props);
 	}
 
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-		if (!playerIn.isSwingInProgress && playerIn.onGround) {
-			playerIn.posY += 0.1D;
-			playerIn.motionY += 0.35D;
+	public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn)
+	{
+		if (!playerIn.swinging && playerIn.isOnGround())
+		{
+			playerIn.setPos(playerIn.position().x, playerIn.position().y + 0.1D, playerIn.position().z);
+			playerIn.setDeltaMovement(playerIn.getDeltaMovement().x, playerIn.getDeltaMovement().y + 0.35D, playerIn.getDeltaMovement().z);
+			//playerIn.posY += 0.1D;
+			//playerIn.motionY += 0.35D;
 
-			if (!worldIn.isRemote) {
+			if (!worldIn.isClientSide) {
 				ProjectileEarthQuake quake = new ProjectileEarthQuake(worldIn, playerIn);
-				quake.shoot(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0.0F, 0.25F, 1.0F);
-				worldIn.spawnEntity(quake);
+				quake.shootFromRotation(playerIn, playerIn.xRot, playerIn.yRot, 0.0F, 0.25F, 1.0F);
+				worldIn.addFreshEntity(quake);
 
-				playerIn.getCooldownTracker().setCooldown(playerIn.getHeldItem(handIn).getItem(), 20);
-				return new ActionResult<>(ActionResultType.SUCCESS, playerIn.getHeldItem(handIn));
+				playerIn.getCooldowns().addCooldown(playerIn.getItemInHand(handIn).getItem(), 20);
+				return ActionResult.success(playerIn.getItemInHand(handIn));
 			}
-			playerIn.swingArm(handIn);
+			playerIn.swing(handIn);
 		}
-		return new ActionResult<>(ActionResultType.FAIL, playerIn.getHeldItem(handIn));
+		return ActionResult.fail(playerIn.getItemInHand(handIn));
 	}
 
 	@Override
-	public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
-		Multimap<String, AttributeModifier> modifiers = super.getAttributeModifiers(slot, stack);
+	public ActionResultType useOn(ItemUseContext pContext)
+	{
+		World world = pContext.getLevel();
+		BlockPos blockpos = pContext.getClickedPos();
+		BlockState blockstate = world.getBlockState(blockpos);
+		BlockState block = blockstate.getToolModifiedState(world, blockpos, pContext.getPlayer(), pContext.getItemInHand(), ToolType.AXE);
+
+		if(block != null)
+		{
+			PlayerEntity playerentity = pContext.getPlayer();
+			world.playSound(playerentity, blockpos, SoundEvents.AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+			if(!world.isClientSide)
+			{
+				world.setBlock(blockpos, block, 11);
+				if(playerentity != null)
+				{
+					pContext.getItemInHand().hurtAndBreak(1, playerentity, (p_220040_1_) -> {
+						p_220040_1_.broadcastBreakEvent(pContext.getHand());
+					});
+				}
+			}
+
+			return ActionResultType.sidedSuccess(world.isClientSide);
+		} else {
+			return ActionResultType.PASS;
+		}
+	}
+
+	@Override
+	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
+		Multimap<Attribute, AttributeModifier> modifiers = super.getAttributeModifiers(slot, stack);
 
 		if (slot == EquipmentSlotType.MAINHAND) {
-			this.replaceModifier(modifiers, Attributes.ATTACK_SPEED, ATTACK_SPEED_MODIFIER, 0.6);
+			this.replaceModifier(modifiers, Attributes.ATTACK_SPEED, BASE_ATTACK_SPEED_UUID, 0.6);
 		}
 
 		return modifiers;
 	}
 
-	protected void replaceModifier(Multimap<String, AttributeModifier> modifierMultimap, Attribute attribute, UUID id, double multiplier) {
-		Collection<AttributeModifier> modifiers = modifierMultimap.get(attribute.getName());
-		Optional<AttributeModifier> modifierOptional = modifiers.stream().filter(attributeModifier -> attributeModifier.getID().equals(id)).findFirst();
+	protected void replaceModifier(Multimap<Attribute, AttributeModifier> modifierMultimap, Attribute attribute, UUID id, double multiplier) {
+		Collection<AttributeModifier> modifiers = modifierMultimap.get(attribute);
+		Optional<AttributeModifier> modifierOptional = modifiers.stream().filter(attributeModifier -> attributeModifier.getId().equals(id)).findFirst();
 
 		if (modifierOptional.isPresent()) {
 			final AttributeModifier modifier = modifierOptional.get();
 			modifiers.remove(modifier);
-			modifiers.add(new AttributeModifier(modifier.getID(), modifier.getName(), modifier.getAmount() - multiplier, modifier.getOperation()));
+			modifiers.add(new AttributeModifier(modifier.getId(), modifier.getName(), modifier.getAmount() - multiplier, modifier.getOperation()));
 		}
 	}
 
 	@Override
 	@OnlyIn(Dist.CLIENT)
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description.bull_battle_axe.name"));
-		} else {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
-		}
+	public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn)
+	{
+		ItemLore.addHoverTextLogic(tooltip, flagIn, "bull_battle_axe");
 	}
 
 	@Override
-	public float getDestroySpeed(ItemStack stack, BlockState state) {
-		return (state.getMaterial() == Material.WOOD || state.getMaterial() == Material.PLANTS || state.getMaterial() == Material.VINE) ? 7.0F : super.getDestroySpeed(stack, state);
+	public float getDestroySpeed(ItemStack pStack, BlockState pState)
+	{
+		Material material = pState.getMaterial();
+		if(pState.isToolEffective(ToolType.AXE))
+		{
+			return this.getTier().getSpeed();
+		}
+		return DIGGABLE_MATERIALS.contains(material) || OTHER_DIGGABLE_BLOCKS.contains(pState.getBlock()) ? this.getTier().getSpeed() : 1.0F;
 	}
-
 }
