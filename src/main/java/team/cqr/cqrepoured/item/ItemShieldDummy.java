70c058084722d75e529d51e586c6260ccc1a723b
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-03-28 22:25:28 +0200

    Update ItemShieldDummy.java

--- src/main/java/team/cqr/cqrepoured/item/ItemShieldDummy.java
+++ src/main/java/team/cqr/cqrepoured/item/ItemShieldDummy.java
@@ -1,11 +1,8 @@
 package team.cqr.cqrepoured.item;
 
-import net.java.games.input.Keyboard;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -13,20 +10,23 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import javax.annotation.Nullable;
 import java.util.List;
 
-public class ItemShieldDummy extends Item {
+public class ItemShieldDummy extends ItemLore {
 
-	public ItemShieldDummy() {
+	public ItemShieldDummy(Properties properties)
+	{
+		super(properties);
 		// TODO: ToolTip
 	}
 
 	@Override
 	@OnlyIn(Dist.CLIENT)
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description.dummy_shield.name"));
-		} else {
-			tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
-		}
+	public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		super.appendHoverText(stack, worldIn, tooltip, flagIn);
+		//if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+		//	tooltip.add(TextFormatting.BLUE + I18n.format("description.dummy_shield.name"));
+		//} else {
+		//	tooltip.add(TextFormatting.BLUE + I18n.format("description.click_shift.name"));
+		//}
 	}
 
 }
