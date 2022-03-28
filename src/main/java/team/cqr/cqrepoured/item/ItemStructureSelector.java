b2676ab8904cbc83eca251f3a1dc2af1e32fe502
Tiviacz1337 <34790800+Tiviacz1337@users.noreply.github.com>
2022-03-28 22:25:20 +0200

    Update ItemStructureSelector.java

--- src/main/java/team/cqr/cqrepoured/item/ItemStructureSelector.java
+++ src/main/java/team/cqr/cqrepoured/item/ItemStructureSelector.java
@@ -1,17 +1,19 @@
 package team.cqr.cqrepoured.item;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.NBTUtil;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants;
@@ -23,28 +25,35 @@ import java.util.List;
 
 public class ItemStructureSelector extends Item {
 
-	public ItemStructureSelector() {
-		this.setMaxStackSize(1);
+	public ItemStructureSelector(Properties properties)
+	{
+		super(properties.stacksTo(1));
+		//this.setMaxStackSize(1);
 	}
 
 	@Override
-	public boolean canDestroyBlockInCreative(World world, BlockPos pos, ItemStack stack, PlayerEntity player) {
-		Block block = world.getBlockState(pos).getBlock();
-		return block != CQRBlocks.EXPORTER;
+	public boolean canAttackBlock(BlockState state, World level, BlockPos pos, PlayerEntity player) {
+		Block block = level.getBlockState(pos).getBlock();
+		return block != CQRBlocks.EXPORTER.get();
 	}
 
 	@Override
-	public ActionResultType onItemUseFirst(PlayerEntity player, World world, BlockPos pos, Direction side, float hitX, float hitY, float hitZ, Hand hand) {
-		ItemStack stack = player.getHeldItem(hand);
+	public ActionResultType useOn(ItemUseContext pContext) {
+		PlayerEntity player = pContext.getPlayer();
+		Hand hand = pContext.getHand();
+		World world = pContext.getLevel();
+		BlockPos pos = pContext.getClickedPos();
 
-		if (world.getTileEntity(pos) instanceof TileEntityExporter) {
-			if (world.isRemote) {
+		ItemStack stack = player.getItemInHand(hand);
+
+		if (world.getBlockEntity(pos) instanceof TileEntityExporter) {
+			if (world.isClientSide) {
 				if (!this.hasFirstAndSecondPos(stack)) {
-					player.sendMessage(new StringTextComponent("Set both positions before using on a exporter"));
+					player.sendMessage(new StringTextComponent("Set both positions before using on a exporter"), player.getUUID());
 					return ActionResultType.SUCCESS;
 				}
 
-				TileEntityExporter tileEntity = (TileEntityExporter) world.getTileEntity(pos);
+				TileEntityExporter tileEntity = (TileEntityExporter) world.getBlockEntity(pos);
 				BlockPos pos1 = this.getFirstPos(stack);
 				BlockPos pos2 = this.getSecondPos(stack);
 
@@ -55,14 +64,14 @@ public class ItemStructureSelector extends Item {
 
 				tileEntity.setValues(tileEntity.getStructureName(), DungeonGenUtils.getMinPos(pos1, pos2), DungeonGenUtils.getMaxPos(pos1, pos2), tileEntity.isRelativeMode(), tileEntity.isIgnoreEntities(), tileEntity.getUnprotectedBlocks());
 			}
-		} else if (!world.isRemote) {
-			if (player.isSneaking()) {
-				BlockPos pos1 = new BlockPos(player);
+		} else if (!world.isClientSide) {
+			if (player.isCrouching()) {
+				BlockPos pos1 = player.blockPosition();
 				this.setSecondPos(stack, pos1);
-				player.sendMessage(new StringTextComponent("Second position set to " + pos1));
+				player.sendMessage(new StringTextComponent("Second position set to " + pos1), player.getUUID());
 			} else {
 				this.setSecondPos(stack, pos);
-				player.sendMessage(new StringTextComponent("Second position set to " + pos));
+				player.sendMessage(new StringTextComponent("Second position set to " + pos), player.getUUID());
 			}
 		}
 
@@ -70,67 +79,67 @@ public class ItemStructureSelector extends Item {
 	}
 
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-		ItemStack stack = playerIn.getHeldItem(handIn);
+	public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn) {
+		ItemStack stack = playerIn.getItemInHand(handIn);
 
-		if (!playerIn.world.isRemote && playerIn.isSneaking()) {
-			BlockPos pos1 = new BlockPos(playerIn);
+		if (!playerIn.level.isClientSide && playerIn.isCrouching()) {
+			BlockPos pos1 = playerIn.blockPosition();
 			this.setSecondPos(stack, pos1);
-			playerIn.sendMessage(new StringTextComponent("Second position set to " + pos1));
+			playerIn.sendMessage(new StringTextComponent("Second position set to " + pos1), playerIn.getUUID());
 		}
 
 		return new ActionResult<>(ActionResultType.SUCCESS, stack);
 	}
 
 	@Override
-	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+	public void appendHoverText(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
 		BlockPos pos1 = this.getFirstPos(stack);
 		BlockPos pos2 = this.getSecondPos(stack);
 
 		if (pos1 != null) {
-			tooltip.add("First position: " + pos1);
+			tooltip.add(new StringTextComponent("First position: " + pos1));
 		} else {
-			tooltip.add("First position: not set");
+			tooltip.add(new StringTextComponent("First position: not set"));
 		}
 		if (pos2 != null) {
-			tooltip.add("Second position: " + pos2);
+			tooltip.add(new StringTextComponent("Second position: " + pos2));
 		} else {
-			tooltip.add("Second position: not set");
+			tooltip.add(new StringTextComponent("Second position: not set"));
 		}
 	}
 
 	public void setFirstPos(ItemStack stack, BlockPos pos) {
-		CompoundNBT compound = stack.getTagCompound();
+		CompoundNBT compound = stack.getTag();
 		if (compound == null) {
 			compound = new CompoundNBT();
-			stack.setTagCompound(compound);
+			stack.setTag(compound);
 		}
-		compound.setTag("pos1", NBTUtil.createPosTag(pos));
+		compound.put("pos1", NBTUtil.writeBlockPos(pos));
 	}
 
 	public void setSecondPos(ItemStack stack, BlockPos pos) {
-		CompoundNBT compound = stack.getTagCompound();
+		CompoundNBT compound = stack.getTag();
 		if (compound == null) {
 			compound = new CompoundNBT();
-			stack.setTagCompound(compound);
+			stack.setTag(compound);
 		}
-		compound.setTag("pos2", NBTUtil.createPosTag(pos));
+		compound.put("pos2", NBTUtil.writeBlockPos(pos));
 	}
 
 	public BlockPos getFirstPos(ItemStack stack) {
-		CompoundNBT compound = stack.getTagCompound();
-		if (compound == null || !compound.hasKey("pos1", Constants.NBT.TAG_COMPOUND)) {
+		CompoundNBT compound = stack.getTag();
+		if (compound == null || !compound.contains("pos1", Constants.NBT.TAG_COMPOUND)) {
 			return null;
 		}
-		return NBTUtil.getPosFromTag(compound.getCompoundTag("pos1"));
+		return NBTUtil.readBlockPos(compound.getCompound("pos1"));
 	}
 
 	public BlockPos getSecondPos(ItemStack stack) {
-		CompoundNBT compound = stack.getTagCompound();
-		if (compound == null || !compound.hasKey("pos2", Constants.NBT.TAG_COMPOUND)) {
+		CompoundNBT compound = stack.getTag();
+		if (compound == null || !compound.contains("pos2", Constants.NBT.TAG_COMPOUND)) {
 			return null;
 		}
-		return NBTUtil.getPosFromTag(compound.getCompoundTag("pos2"));
+		return NBTUtil.readBlockPos(compound.getCompound("pos2"));
 	}
 
 	public boolean hasFirstPos(ItemStack stack) {
