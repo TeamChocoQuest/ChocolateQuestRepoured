2c0b4b17c26d0546bc4e236ee29a8e7ecb794159
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-03-31 18:57:55 +0200

    Fix ground level setup ignoring rotation/mirror

--- src/main/java/team/cqr/cqrepoured/world/structure/generation/generation/part/PlateauDungeonPart.java
+++ src/main/java/team/cqr/cqrepoured/world/structure/generation/generation/part/PlateauDungeonPart.java
@@ -2,6 +2,8 @@ package team.cqr.cqrepoured.world.structure.generation.generation.part;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
@@ -182,15 +184,18 @@ public class PlateauDungeonPart implements IDungeonPart {
 			return this;
 		}
 
-		public void markGround(CQStructure structure, BlockPos pos) {
+		public void markGround(CQStructure structure, BlockPos pos, Mirror mirror, Rotation rotation) {
 			List<PreparablePosInfo> blocks = structure.getBlockInfoList();
 			BlockPos size = structure.getSize();
 			for (int x = 0; x < structure.getSize().getX(); x++) {
-				if (x + pos.getX() < this.startX || x + pos.getX() > this.endX) {
-					continue;
-				}
 				for (int z = 0; z < structure.getSize().getZ(); z++) {
-					if (z + pos.getZ() < this.startZ || z + pos.getZ() > this.endZ) {
+					MutableBlockPos transformed = DungeonPlacement.transform(x, 0, z, mirror, rotation);
+					int x1 = transformed.getX();
+					int z1 = transformed.getZ();
+					if (x1 + pos.getX() < this.startX || x1 + pos.getX() > this.endX) {
+						continue;
+					}
+					if (z1 + pos.getZ() < this.startZ || z1 + pos.getZ() > this.endZ) {
 						continue;
 					}
 					int y = Math.min(this.endY + 1 - pos.getY(), size.getY() - 1);
@@ -198,9 +203,9 @@ public class PlateauDungeonPart implements IDungeonPart {
 						y--;
 					}
 					if (y < 0) {
-						this.ground[x][z] = -1;
+						this.ground[x1][z1] = -1;
 					} else {
-						this.ground[x][z] = Math.min(pos.getY() + y + 2, this.endY + 1);
+						this.ground[x1][z1] = Math.min(pos.getY() + y + 2, this.endY + 1);
 					}
 				}
 			}
