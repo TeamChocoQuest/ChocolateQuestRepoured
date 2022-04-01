9aa9ef33c0a3cd5d5d93079791fbb475d2e5d31a
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-01 19:44:12 +0200

    finish table renderer

--- src/main/java/team/cqr/cqrepoured/client/render/tileentity/TileEntityTableRenderer.java
+++ src/main/java/team/cqr/cqrepoured/client/render/tileentity/TileEntityTableRenderer.java
@@ -4,11 +4,13 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.vector.Vector3f;
+import net.minecraftforge.client.ForgeHooksClient;
 import team.cqr.cqrepoured.tileentity.TileEntityTable;
 
 public class TileEntityTableRenderer extends TileEntityRenderer<TileEntityTable> {
@@ -22,56 +24,25 @@ public class TileEntityTableRenderer extends TileEntityRenderer<TileEntityTable>
 		float rotation = te.getRotationInDegree();
 
 		if (!stack.isEmpty()) {
+			IBakedModel itemModel = Minecraft.getInstance().getItemRenderer().getModel(stack, te.getLevel(), null);
+			itemModel = ForgeHooksClient.handleCameraTransforms(pMatrixStack, itemModel, TransformType.NONE, false);
+			
 			pMatrixStack.pushPose();
 			
-			pMatrixStack.translate(0.5, 1.25, 0.5);
-			pMatrixStack.scale(0.5F, 0.5F, 0.5F);
-			pMatrixStack.mulPose(Vector3f.YN.rotationDegrees(rotation));
+			if(itemModel.isGui3d()) {
+				pMatrixStack.translate(0.5, 1.25, 0.5);
+				pMatrixStack.scale(0.5F, 0.5F, 0.5F);
+				pMatrixStack.mulPose(Vector3f.YN.rotationDegrees(rotation));
+			} else {
+				pMatrixStack.translate(0.5, 1.02, 0.5);
+				pMatrixStack.mulPose(Vector3f.YP.rotationDegrees(rotation));
+				pMatrixStack.mulPose(Vector3f.XP.rotationDegrees(90));
+				pMatrixStack.scale(0.7F, 0.7F, 0.7F);
+			}
 			
 			Minecraft.getInstance().getItemRenderer().renderStatic(stack, TransformType.NONE, pCombinedLight, pCombinedOverlay, pMatrixStack, pBuffer);
 			
 			pMatrixStack.popPose();
 		}
-			/*IBakedModel model = Minecraft.getMinecraft().getRenderItem().getItemModelWithOverrides(stack, te.getWorld(), null);
-			model = ForgeHooksClient.handleCameraTransforms(model, TransformType.NONE, false);
-
-			if (model.isGui3d()) {
-				GlStateManager.enableRescaleNormal();
-				GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1f);
-				GlStateManager.enableBlend();
-				RenderHelper.enableStandardItemLighting();
-				GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
-				GlStateManager.pushMatrix();
-				GlStateManager.translate(x + 0.5, y + 1.25, z + 0.5);
-				GlStateManager.scale(0.5F, 0.5F, 0.5F);
-				GlStateManager.rotate(rotation, 0, -1, 0);
-
-				Minecraft.getMinecraft().getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
-				Minecraft.getMinecraft().getRenderItem().renderItem(stack, model);
-
-				GlStateManager.popMatrix();
-				GlStateManager.disableRescaleNormal();
-				GlStateManager.disableBlend();
-			}
-
-			else {
-				GlStateManager.enableRescaleNormal();
-				GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1f);
-				GlStateManager.enableBlend();
-				RenderHelper.enableStandardItemLighting();
-				GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
-				GlStateManager.pushMatrix();
-				GlStateManager.translate(x + 0.5, y + 1.02, z + 0.5);
-				GlStateManager.rotate(90F, 1, 0, 0);
-				GlStateManager.rotate(rotation, 0, 0, 1);
-				GlStateManager.scale(0.7F, 0.7F, 0.7F);
-
-				Minecraft.getMinecraft().getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
-				Minecraft.getMinecraft().getRenderItem().renderItem(stack, model);
-
-				GlStateManager.popMatrix();
-				GlStateManager.disableRescaleNormal();
-				GlStateManager.disableBlend();
-			}*/
 	}
 }
