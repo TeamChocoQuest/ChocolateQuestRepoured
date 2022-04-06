87ad3576869f4721d2d0a412793c67f9504521d6
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-06 18:44:51 +0200

    create constants for the standard biped bone idents

---
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/StandardBipedBones.java
@@ -0,0 +1,38 @@
+package team.cqr.cqrepoured.client.render.entity;
+
+public class StandardBipedBones {
+
+	//BONES
+	public static final String ROOT_BONE = "root";
+	
+	public static final String HIP_BONE = "bipedLegs";
+	public static final String LEFT_LEG_BONE = "bipedLeLeft";
+	public static final String RIGHT_LEG_BONE = "bipedLegRight";
+	
+	public static final String BODY_BONE = "bipedBody";
+	public static final String SHOULDER_BONE = "bipedArms";
+	public static final String LEFT_ARM_BONE = "bipedArmLeft";
+	public static final String RIGHT_ARM_BONE = "bipedArmRight";
+	
+	public static final String HEAD_BONE_BASE = "bipedHeadBaseRotator";
+	public static final String HEAD_BONE = "bipedHead";
+	
+	public static final String CAPE_BONE = "bipedCape";
+	
+	//ITEM SLOTS
+	public static final String POTION_BONE = "bipedPotionSlot";
+	public static final String LEFT_HAND = "bipedHandLeft";
+	public static final String RIGHT_HAND = "bipedHandRight";
+
+	//ARMOR SLOTS
+	public static final String ARMOR_HEAD = "armorBipedHead";
+	public static final String ARMOR_BODY = "armorBipedBody";
+	public static final String ARMOR_LEFT_ARM = "armorBipedLeftArm";
+	public static final String ARMOR_RIGHT_ARM = "armorBipedRightArm";
+	public static final String ARMOR_LEFT_LEG = "armorBipedLeftLeg";
+	public static final String ARMOR_RIGHT_LEG = "armorBipedRightLeg";
+	public static final String ARMOR_LEFT_FOOT = "armroBipedLeftFoot";
+	public static final String ARMOR_RIGHT_FOOT = "armorBipedRightFoot"; 
+	
+
+}
