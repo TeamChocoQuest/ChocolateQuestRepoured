09272a23304edf2d0e3b450198e12187e048a7a1
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-06 19:55:44 +0200

    patch bubble renderer

--- src/main/java/team/cqr/cqrepoured/client/render/entity/RenderBubble.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/RenderBubble.java
@@ -18,8 +18,7 @@ public class RenderBubble extends EntityRenderer<EntityBubble> {
 
 	private final Model model;
 
-	public RenderBubble(EntityRendererManager renderManager)
-	{
+	public RenderBubble(EntityRendererManager renderManager) {
 		super(renderManager);
 		this.model = new ModelBall();
 	}
@@ -28,35 +27,19 @@ public class RenderBubble extends EntityRenderer<EntityBubble> {
 	public ResourceLocation getTextureLocation(EntityBubble pEntity) {
 		return this.TEXTURE;
 	}
-
-	/*@Override
-	public void doRenderShadowAndFire(Entity entityIn, double x, double y, double z, float yaw, float partialTicks) {
-		// Supposed to be empty
-	} */
+	
+	@Override
+	public EntityRendererManager getDispatcher() {
+		return super.getDispatcher();
+	}
 
 	@Override
-	public void render(EntityBubble entity, float entityYaw, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int packedLight)
-	{
-		if(entity.isBeingRidden())
-		{
-			matrixStack.pushPose();
-			this.model.renderToBuffer(matrixStack, buffer.getBuffer(RenderType.entityTranslucent(TEXTURE)), packedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 0.5F);
-			matrixStack.popPose();
-			//GlStateManager.pushAttrib();
-			//GlStateManager.enableAlpha();
-			//GlStateManager.enableBlend();
-			//GlStateManager.disableCull();
-			//GlStateManager.pushMatrix();
+	public void render(EntityBubble entity, float entityYaw, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int packedLight) {
+		matrixStack.pushPose();
+		float scale = entity.getBbHeight() / 0.9F;
+		matrixStack.scale(scale, -scale, scale);
+		this.model.renderToBuffer(matrixStack, buffer.getBuffer(RenderType.entityTranslucent(TEXTURE, true)), packedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 0.5F);
 
-			//GlStateManager.color(1.0F, 1.0F, 1.0F, 0.5F);
-			//GlStateManager.translate(x, y, z);
-			//float scale = entity.height / 0.875F;
-			//GlStateManager.scale(-scale, -scale, scale);
-			//this.bindTexture(this.getEntityTexture(entity));
-			//this.model.render(entity, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
-
-			//GlStateManager.popMatrix();
-			//GlStateManager.popAttrib();
-		}
+		matrixStack.popPose();
 	}
 }
