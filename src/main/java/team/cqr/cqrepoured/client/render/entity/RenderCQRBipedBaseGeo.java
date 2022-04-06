3751206071bf01bc9f271fe0e6f4654806bea930
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-06 18:45:09 +0200

    add in stnadard logic (NOT CALLED BY DEFAULT!!!!) for stnadard bipeds

--- src/main/java/team/cqr/cqrepoured/client/render/entity/RenderCQRBipedBaseGeo.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/RenderCQRBipedBaseGeo.java
@@ -13,6 +13,9 @@ import net.minecraft.item.ItemStack;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import team.cqr.cqrepoured.entity.bases.AbstractEntityCQR;
+import team.cqr.cqrepoured.init.CQRItems;
+
+import static team.cqr.cqrepoured.client.render.entity.StandardBipedBones.*;
 
 public abstract class RenderCQRBipedBaseGeo<T extends AbstractEntityCQR & IAnimatable> extends RenderCQREntityGeo<T> {
 
@@ -49,6 +52,73 @@ public abstract class RenderCQRBipedBaseGeo<T extends AbstractEntityCQR & IAnima
 	
 	protected abstract void calculateArmorStuffForBone(String boneName, T currentEntity);
 	protected abstract void calculateItemStuffForBone(String boneName, T currentEntity);
+	
+	protected void standardArmorCalculationForBone(String boneName, T currentEntity) {
+		switch(boneName) {
+			case ARMOR_BODY:
+				this.currentArmorAtBone = chestplate;
+				this.currentArmorPartAtBone = GET_ARMOR_BODY;
+				this.currentSlotAtBone = EquipmentSlotType.CHEST;
+				break;
+			case ARMOR_HEAD:
+				this.currentArmorAtBone = helmet;
+				this.currentArmorPartAtBone = GET_ARMOR_HEAD;
+				this.currentSlotAtBone = EquipmentSlotType.HEAD;
+				break;
+			case ARMOR_LEFT_ARM:
+				this.currentArmorAtBone = chestplate;
+				this.currentArmorPartAtBone = GET_ARMOR_ARM_LEFT;
+				this.currentSlotAtBone = EquipmentSlotType.CHEST;
+				break;
+			case ARMOR_RIGHT_ARM:
+				this.currentArmorAtBone = chestplate;
+				this.currentArmorPartAtBone = GET_ARMOR_ARM_RIGHT;
+				this.currentSlotAtBone = EquipmentSlotType.CHEST;
+				break;
+			case ARMOR_LEFT_FOOT:
+				this.currentArmorAtBone = boots;
+				this.currentArmorPartAtBone = GET_ARMOR_LEG_LEFT;
+				this.currentSlotAtBone = EquipmentSlotType.FEET;
+				break;
+			case ARMOR_RIGHT_LEG:
+				this.currentArmorAtBone = boots;
+				this.currentArmorPartAtBone = GET_ARMOR_LEG_RIGHT;
+				this.currentSlotAtBone = EquipmentSlotType.FEET;
+				break;
+			case ARMOR_LEFT_LEG:
+				this.currentArmorAtBone = leggings;
+				this.currentArmorPartAtBone = GET_ARMOR_LEG_LEFT;
+				this.currentSlotAtBone = EquipmentSlotType.LEGS;
+				break;
+			case ARMOR_RIGHT_FOOT:
+				this.currentArmorAtBone = leggings;
+				this.currentArmorPartAtBone = GET_ARMOR_LEG_RIGHT;
+				this.currentSlotAtBone = EquipmentSlotType.LEGS;
+				break;
+			default:
+				break;
+		}
+	}
+	
+	public static final ItemStack HEALING_POTION_FOR_DISPLAY = new ItemStack(CQRItems.POTION_HEALING.get());
+	
+	protected void standardItemCalculationForBone(String boneName, T currentEntity) {
+		switch(boneName) {
+			case LEFT_HAND:
+				this.currentItemAtBone = currentEntity.isLeftHanded() ? this.mainHand : this.offHand;
+				this.currentTransformTypeAtBone = TransformType.THIRD_PERSON_RIGHT_HAND;
+				break;
+			case RIGHT_HAND:
+				this.currentItemAtBone = !currentEntity.isLeftHanded() ? this.mainHand : this.offHand;
+				this.currentTransformTypeAtBone = TransformType.THIRD_PERSON_RIGHT_HAND;
+				break;
+			case POTION_BONE:
+				this.currentItemAtBone = HEALING_POTION_FOR_DISPLAY;
+				this.currentTransformTypeAtBone = TransformType.NONE;
+				break;
+			default: break;
+		}
+	}
 	
 	@Override
 	protected ItemStack getArmorForBone(String boneName, T currentEntity) {
