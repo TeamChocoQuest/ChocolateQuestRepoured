a5640d0b9fa2d5d7b74b2c1cc607f7ebbf7946c2
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-08 14:48:26 +0200

    proeprly implement potions

--- src/main/java/team/cqr/cqrepoured/init/CQRPotions.java
+++ src/main/java/team/cqr/cqrepoured/init/CQRPotions.java
@@ -5,17 +5,20 @@ import static team.cqr.cqrepoured.util.InjectionUtil.Null;
 import net.minecraft.potion.Effect;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.ObjectHolder;
 import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.potion.PotionTwohanded;
 
 @ObjectHolder(CQRMain.MODID)
+@EventBusSubscriber
 public class CQRPotions {
 
 	public static final Effect TWOHANDED = Null();
 
-	//@EventBusSubscriber(modid = CQRMain.MODID)
+	@EventBusSubscriber(modid = CQRMain.MODID, bus = Bus.MOD)
 	public static class EventHandler {
 
 		@SubscribeEvent
