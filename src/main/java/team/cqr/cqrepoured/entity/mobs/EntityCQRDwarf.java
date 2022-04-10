3a1b2e76634bc88a8af2fe21abac7f43d7e951e6
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-10 16:21:29 +0200

    implemnt CQR biped interface

--- src/main/java/team/cqr/cqrepoured/entity/mobs/EntityCQRDwarf.java
+++ src/main/java/team/cqr/cqrepoured/entity/mobs/EntityCQRDwarf.java
@@ -1,16 +1,20 @@
 package team.cqr.cqrepoured.entity.mobs;
 
+import java.util.Set;
+
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.world.World;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
 import team.cqr.cqrepoured.config.CQRConfig;
+import team.cqr.cqrepoured.entity.IAnimatableCQR;
 import team.cqr.cqrepoured.entity.bases.AbstractEntityCQR;
 import team.cqr.cqrepoured.faction.EDefaultFaction;
 import team.cqr.cqrepoured.init.CQRSounds;
 
-public class EntityCQRDwarf extends AbstractEntityCQR {
+public class EntityCQRDwarf extends AbstractEntityCQR implements IAnimatableCQR {
 
 	public EntityCQRDwarf(EntityType<? extends AbstractEntityCQR> type, World worldIn) {
 		super(type, worldIn);
@@ -62,6 +66,24 @@ public class EntityCQRDwarf extends AbstractEntityCQR {
 	@Override
 	public float getDefaultHeight() {
 		return 1.235F;
+	}
+
+	// Geckolib
+	private AnimationFactory factory = new AnimationFactory(this);
+
+	@Override
+	public AnimationFactory getFactory() {
+		return this.factory;
+	}
+
+	@Override
+	public Set<String> getAlwaysPlayingAnimations() {
+		return null;
+	}
+
+	@Override
+	public boolean isSwinging() {
+		return this.swinging;
 	}
 
 }
