3d511004346c2571020b513933a719c3cc9635d0
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-10 20:00:35 +0200

    fix compile errors

--- src/main/java/team/cqr/cqrepoured/event/capability/ItemCooldownEventHandler.java
+++ src/main/java/team/cqr/cqrepoured/event/capability/ItemCooldownEventHandler.java
@@ -2,29 +2,37 @@ package team.cqr.cqrepoured.event.capability;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.network.PacketDistributor;
 import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.capability.armor.CapabilityCooldownHandler;
 import team.cqr.cqrepoured.capability.armor.CapabilityCooldownHandlerProvider;
 import team.cqr.cqrepoured.network.server.packet.SPacketArmorCooldownSync;
 
-//@EventBusSubscriber(modid = CQRMain.MODID)
+@EventBusSubscriber(modid = CQRMain.MODID)
 public class ItemCooldownEventHandler {
 
 	@SubscribeEvent
 	public static void onLivingUpdateEvent(LivingUpdateEvent event) {
 		LivingEntity entity = event.getEntityLiving();
-		CapabilityCooldownHandler capabilityCooldown = entity.getCapability(CapabilityCooldownHandlerProvider.CAPABILITY_ITEM_COOLDOWN_CQR, null);
-		capabilityCooldown.tick();
+		LazyOptional<CapabilityCooldownHandler> lOpCap = entity.getCapability(CapabilityCooldownHandlerProvider.CAPABILITY_ITEM_COOLDOWN_CQR, null);
+		lOpCap.ifPresent(capa -> capa.tick());
 	}
 
 	@SubscribeEvent
 	public static void onPlayerLoggedInEvent(PlayerEvent.PlayerLoggedInEvent event) {
-		if (!event.player.world.isRemote) {
-			CapabilityCooldownHandler capabilityCooldown = event.player.getCapability(CapabilityCooldownHandlerProvider.CAPABILITY_ITEM_COOLDOWN_CQR, null);
-			CQRMain.NETWORK.sendTo(new SPacketArmorCooldownSync(capabilityCooldown.getItemCooldownMap()), (ServerPlayerEntity) event.player);
+		if (!event.getPlayer().level.isClientSide) {
+			LazyOptional<CapabilityCooldownHandler> lOpCap = event.getPlayer().getCapability(CapabilityCooldownHandlerProvider.CAPABILITY_ITEM_COOLDOWN_CQR, null);
+			
+			if(event.getPlayer() instanceof ServerPlayerEntity) {
+				lOpCap.ifPresent(capa -> {
+					CQRMain.NETWORK.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity)event.getPlayer()), new SPacketArmorCooldownSync(capa.getItemCooldownMap()));
+				});
+			}
 		}
 	}
 
