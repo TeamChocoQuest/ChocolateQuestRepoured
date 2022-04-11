257f13d6b93e09100ec9ad84604459f2f4b64cc6
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-11 19:26:05 +0200

    update most of EntityAIOpenCLoseDoor to 1.16

--- src/main/java/team/cqr/cqrepoured/entity/ai/EntityAIOpenCloseDoor.java
+++ src/main/java/team/cqr/cqrepoured/entity/ai/EntityAIOpenCloseDoor.java
@@ -5,6 +5,8 @@ import net.minecraft.block.DoorBlock.EnumDoorHalf;
 import net.minecraft.block.material.Material;
 import net.minecraft.pathfinding.Path;
 import net.minecraft.pathfinding.PathPoint;
+import net.minecraft.state.properties.DoubleBlockHalf;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -43,16 +45,16 @@ public class EntityAIOpenCloseDoor extends AbstractCQREntityAI<AbstractEntityCQR
 		BlockState state = this.world.getBlockState(this.doorPos);
 		if (state.getBlock() instanceof DoorBlock) {
 			this.doorBlock = (DoorBlock) state.getBlock();
-			this.doorEnterFacing = this.entity.getHorizontalFacing().getOpposite();
+			this.doorEnterFacing = this.entity.getDirection().getOpposite();
 			return canMoveThroughDoor(this.world, this.doorPos, this.doorEnterFacing, true);
 		}
 
 		Path path = this.entity.getNavigation().getPath();
-		int end = Math.min(path.getCurrentPathIndex() + 2, path.getCurrentPathLength());
-		for (int i = path.getCurrentPathIndex(); i < end; i++) {
-			PathPoint pathPoint = path.getPathPointFromIndex(i);
+		int end = Math.min(path.getNextNodeIndex() + 1, path.getNodeCount());
+		for (int i = path.getNextNodeIndex() -1; i < end; i++) {
+			PathPoint pathPoint = path.getNode(i);
 			this.doorPos.set(pathPoint.x, pathPoint.y, pathPoint.z);
-			if (this.entity.getDistanceSq(this.doorPos.getX() + 0.5D, this.doorPos.getY(), this.doorPos.getZ() + 0.5D) >= 1.5D * 1.5D) {
+			if (this.entity.distanceToSqr(this.doorPos.getX() + 0.5D, this.doorPos.getY(), this.doorPos.getZ() + 0.5D) >= 1.5D * 1.5D) {
 				continue;
 			}
 
@@ -60,10 +62,10 @@ public class EntityAIOpenCloseDoor extends AbstractCQREntityAI<AbstractEntityCQR
 			if (state.getBlock() instanceof DoorBlock) {
 				this.doorBlock = (DoorBlock) state.getBlock();
 				if (i > 0) {
-					PathPoint pathPoint1 = path.getPathPointFromIndex(i - 1);
+					PathPoint pathPoint1 = path.getNode(i - 1);
 					this.doorEnterFacing = Direction.getFacingFromVector(pathPoint1.x - pathPoint.x, pathPoint1.y - pathPoint.y, pathPoint1.z - pathPoint.z);
 				} else {
-					this.doorEnterFacing = this.entity.getHorizontalFacing().getOpposite();
+					this.doorEnterFacing = this.entity.getDirection().getOpposite();
 				}
 				return canMoveThroughDoor(this.world, this.doorPos, this.doorEnterFacing, true);
 			}
@@ -115,30 +117,30 @@ public class EntityAIOpenCloseDoor extends AbstractCQREntityAI<AbstractEntityCQR
 		state = state.getActualState(this.world, this.doorPos);
 		boolean doorOpen = state.getValue(DoorBlock.OPEN);
 		Direction doorFacing = state.getValue(DoorBlock.FACING);
-		Direction.Axis blockedAxis = doorOpen ? doorFacing.rotateY().getAxis() : doorFacing.getAxis();
+		Direction.Axis blockedAxis = doorOpen ? doorFacing.getClockWise().getAxis() : doorFacing.getAxis(); //OLD instead of getClockWise() -> rotateY()
 		if (this.doorEnterFacing.getAxis() != blockedAxis) {
 			return false;
 		}
 		if (state.getMaterial() == Material.WOOD) {
-			this.doorBlock.toggleDoor(this.world, this.doorPos, !doorOpen);
+			this.doorBlock.setOpen(this.world, state, this.doorPos, !doorOpen);
 			return true;
 		}
 		if (!doorOpen) {
-			BlockPos pos = state.getValue(DoorBlock.HALF) == EnumDoorHalf.UPPER ? this.doorPos.down() : this.doorPos;
+			BlockPos pos = state.getValue(DoorBlock.HALF) == DoubleBlockHalf.UPPER ? this.doorPos.below() : this.doorPos;
 			BlockPos.Mutable mutablePos = new BlockPos.Mutable();
-			if (this.isPressurePlate(mutablePos.setPos(pos).move(this.doorEnterFacing))) {
+			if (this.isPressurePlate(mutablePos.set(pos).move(this.doorEnterFacing))) {
 				return true;
 			}
-			if (this.activateButtonOrLeverWithOrientation(mutablePos.setPos(pos).move(this.doorEnterFacing).move(this.doorEnterFacing.rotateY()).move(Direction.UP), this.doorEnterFacing)) {
+			if (this.activateButtonOrLeverWithOrientation(mutablePos.set(pos).move(this.doorEnterFacing).move(this.doorEnterFacing.getClockWise()).move(Direction.UP), this.doorEnterFacing)) {
 				return true;
 			}
-			if (this.activateButtonOrLeverWithOrientation(mutablePos.setPos(pos).move(this.doorEnterFacing).move(this.doorEnterFacing.rotateYCCW()).move(Direction.UP), this.doorEnterFacing)) {
+			if (this.activateButtonOrLeverWithOrientation(mutablePos.set(pos).move(this.doorEnterFacing).move(this.doorEnterFacing.getCounterClockWise()).move(Direction.UP), this.doorEnterFacing)) {
 				return true;
 			}
-			if (this.activateButtonOrLeverWithOrientation(mutablePos.setPos(pos).move(this.doorEnterFacing).move(this.doorEnterFacing.rotateY()), this.doorEnterFacing)) {
+			if (this.activateButtonOrLeverWithOrientation(mutablePos.set(pos).move(this.doorEnterFacing).move(this.doorEnterFacing.getClockWise()), this.doorEnterFacing)) {
 				return true;
 			}
-			if (this.activateButtonOrLeverWithOrientation(mutablePos.setPos(pos).move(this.doorEnterFacing).move(this.doorEnterFacing.rotateYCCW()), this.doorEnterFacing)) {
+			if (this.activateButtonOrLeverWithOrientation(mutablePos.set(pos).move(this.doorEnterFacing).move(this.doorEnterFacing.getCounterClockWise()), this.doorEnterFacing)) {
 				return true;
 			}
 		}
@@ -149,7 +151,7 @@ public class EntityAIOpenCloseDoor extends AbstractCQREntityAI<AbstractEntityCQR
 		BlockState state = this.world.getBlockState(pos);
 		Block block = state.getBlock();
 		Material material = state.getMaterial();
-		return block instanceof PressurePlateBlock && (material == Material.WOOD || material == Material.ROCK);
+		return block.is(BlockTags.PRESSURE_PLATES) || ( block instanceof PressurePlateBlock && (material == Material.WOOD || material == Material.STONE));
 	}
 
 	private boolean activateButtonOrLeverWithOrientation(BlockPos pos, Direction facing) {
@@ -175,30 +177,30 @@ public class EntityAIOpenCloseDoor extends AbstractCQREntityAI<AbstractEntityCQR
 			return false;
 		}
 		boolean shouldCloseDoor = true;
-		double x = this.entity.posX;
-		double y = this.entity.posY;
-		double z = this.entity.posZ;
+		double x = this.entity.getX();
+		double y = this.entity.getY();
+		double z = this.entity.getZ();
 		double r = 4.0D;
 		AxisAlignedBB aabb = new AxisAlignedBB(x - r, y - r * 0.5D, z - r, x + r, y + r * 0.5D, z + r);
-		List<AbstractEntityCQR> allies = this.world.getEntitiesWithinAABB(AbstractEntityCQR.class, aabb, e -> TargetUtil.isAllyCheckingLeaders(this.entity, e));
+		List<AbstractEntityCQR> allies = this.world.getEntitiesOfClass(AbstractEntityCQR.class, aabb, e -> TargetUtil.isAllyCheckingLeaders(this.entity, e));
 		for (AbstractEntityCQR ally : allies) {
 			if (ally == this.entity) {
 				continue;
 			}
-			if (!ally.hasPath()) {
+			if (!ally.isPathFinding()) { //OLD: hasPath()
 				continue;
 			}
-			if (ally.getDistanceSq(this.entity) >= r * r) {
+			if (ally.distanceToSqr(this.entity) >= r * r) {
 				continue;
 			}
-			if (MathHelper.floor(ally.posX) == this.doorPos.getX() && MathHelper.floor(ally.posY) == this.doorPos.getY() && MathHelper.floor(ally.posZ) == this.doorPos.getZ()) {
+			if (MathHelper.floor(ally.getX()) == this.doorPos.getX() && MathHelper.floor(ally.getY()) == this.doorPos.getY() && MathHelper.floor(ally.getZ()) == this.doorPos.getZ()) {
 				shouldCloseDoor = false;
 				break;
 			}
 			Path path = ally.getNavigation().getPath();
-			int end = Math.min(path.getCurrentPathIndex() + 2, path.getCurrentPathLength());
-			for (int i = path.getCurrentPathIndex(); i < end; i++) {
-				PathPoint pathPoint = path.getPathPointFromIndex(i);
+			int end = Math.min(path.getNextNodeIndex() + 1, path.getNodeCount());
+			for (int i = path.getNextNodeIndex() -1; i < end; i++) {
+				PathPoint pathPoint = path.getNode(i);
 				if (pathPoint.x == this.doorPos.getX() && pathPoint.y == this.doorPos.getY() && pathPoint.z == this.doorPos.getZ()) {
 					shouldCloseDoor = false;
 					break;
@@ -206,7 +208,7 @@ public class EntityAIOpenCloseDoor extends AbstractCQREntityAI<AbstractEntityCQR
 			}
 		}
 		if (shouldCloseDoor) {
-			this.doorBlock.toggleDoor(this.world, this.doorPos, false);
+			this.doorBlock.setOpen(this.world, state, this.doorPos, false);
 			return true;
 		}
 		return false;
@@ -220,7 +222,7 @@ public class EntityAIOpenCloseDoor extends AbstractCQREntityAI<AbstractEntityCQR
 		state = state.getActualState(world, pos);
 		boolean doorOpen = state.getValue(DoorBlock.OPEN);
 		Direction doorFacing = state.getValue(DoorBlock.FACING);
-		Direction.Axis blockedAxis = doorOpen ? doorFacing.rotateY().getAxis() : doorFacing.getAxis();
+		Direction.Axis blockedAxis = doorOpen ? doorFacing.getClockWise().getAxis() : doorFacing.getAxis();
 		if (enterFacing.getAxis() != blockedAxis) {
 			return true;
 		}
@@ -235,22 +237,22 @@ public class EntityAIOpenCloseDoor extends AbstractCQREntityAI<AbstractEntityCQR
 			return false;
 		}
 		BlockPos.Mutable mutablePos = new BlockPos.Mutable();
-		if (state.getValue(DoorBlock.HALF) == EnumDoorHalf.UPPER) {
-			pos = pos.down();
+		if (state.getValue(DoorBlock.HALF) == DoubleBlockHalf.UPPER) {
+			pos = pos.below();
 		}
-		if (isPressurePlate(world, mutablePos.setPos(pos).move(enterFacing))) {
+		if (isPressurePlate(world, mutablePos.set(pos).move(enterFacing))) {
 			return true;
 		}
-		if (isButtonOrLeverWithOrientation(world, mutablePos.setPos(pos).move(enterFacing).move(enterFacing.rotateY()).move(Direction.UP), enterFacing)) {
+		if (isButtonOrLeverWithOrientation(world, mutablePos.set(pos).move(enterFacing).move(enterFacing.getClockWise()).move(Direction.UP), enterFacing)) {
 			return true;
 		}
-		if (isButtonOrLeverWithOrientation(world, mutablePos.setPos(pos).move(enterFacing).move(enterFacing.rotateYCCW()).move(Direction.UP), enterFacing)) {
+		if (isButtonOrLeverWithOrientation(world, mutablePos.set(pos).move(enterFacing).move(enterFacing.getCounterClockWise()).move(Direction.UP), enterFacing)) {
 			return true;
 		}
-		if (isButtonOrLeverWithOrientation(world, mutablePos.setPos(pos).move(enterFacing).move(enterFacing.rotateY()), enterFacing)) {
+		if (isButtonOrLeverWithOrientation(world, mutablePos.set(pos).move(enterFacing).move(enterFacing.getClockWise()), enterFacing)) {
 			return true;
 		}
-		if (isButtonOrLeverWithOrientation(world, mutablePos.setPos(pos).move(enterFacing).move(enterFacing.rotateYCCW()), enterFacing)) {
+		if (isButtonOrLeverWithOrientation(world, mutablePos.set(pos).move(enterFacing).move(enterFacing.getCounterClockWise()), enterFacing)) {
 			return true;
 		}
 		return false;
@@ -260,7 +262,7 @@ public class EntityAIOpenCloseDoor extends AbstractCQREntityAI<AbstractEntityCQR
 		BlockState state = world.getBlockState(pos);
 		Block block = state.getBlock();
 		Material material = state.getMaterial();
-		return block instanceof PressurePlateBlock && (material == Material.WOOD || material == Material.ROCK);
+		return block.is(BlockTags.PRESSURE_PLATES) || (block instanceof PressurePlateBlock && (material == Material.WOOD || material == Material.STONE));
 	}
 
 	private static boolean isButtonOrLeverWithOrientation(IBlockReader world, BlockPos pos, Direction facing) {
@@ -270,7 +272,7 @@ public class EntityAIOpenCloseDoor extends AbstractCQREntityAI<AbstractEntityCQR
 			return state.getValue(DirectionalBlock.FACING) == facing;
 		}
 		if (block instanceof LeverBlock) {
-			return state.getValue(LeverBlock.FACING).getFacing() == facing;
+			return state.getValue(LeverBlock.FACING)/*.getFacing()*/ == facing;
 		}
 		return false;
 	}
