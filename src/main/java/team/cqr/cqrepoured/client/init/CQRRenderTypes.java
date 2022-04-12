82e189c4962a13bd9417ee89083a2734a9f50309
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-12 18:07:21 +0200

    forgot to commit change

--- src/main/java/team/cqr/cqrepoured/client/init/CQRRenderTypes.java
+++ src/main/java/team/cqr/cqrepoured/client/init/CQRRenderTypes.java
@@ -15,7 +15,7 @@ public class CQRRenderTypes extends RenderType {
 
 	public static RenderType emissive(ResourceLocation texture) {
 		return RenderType.create("emissive", DefaultVertexFormats.NEW_ENTITY, GL11.GL_QUADS, 256, State.builder()
-				//.setAlphaState(DEFAULT_ALPHA)
+				.setAlphaState(DEFAULT_ALPHA)
 				.setCullState(NO_CULL)
 				.setTextureState(new TextureState(texture, false, false))
 				.setTransparencyState(TRANSLUCENT_TRANSPARENCY)
