8955301126c71168941cbf93c1d06c565d3ccce1
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-13 17:32:02 +0200

    fix transparent armor model being solid (more or less)

--- src/main/java/team/cqr/cqrepoured/client/model/armor/ModelArmorTransparent.java
+++ src/main/java/team/cqr/cqrepoured/client/model/armor/ModelArmorTransparent.java
@@ -1,37 +1,12 @@
 package team.cqr.cqrepoured.client.model.armor;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.entity.LivingEntity;
 
 public class ModelArmorTransparent<T extends LivingEntity> extends ModelCustomArmorBase<T> {
 
 	public ModelArmorTransparent(float scale) {
-		super(scale, 64, 32);
+		super(RenderType::entityTranslucent , scale, 64, 32);
 	}
-
-	/*@Override
-	public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-		GlStateManager.pushMatrix();
-		GlStateManager.enableBlendProfile(GlStateManager.Profile.PLAYER_SKIN);
-		super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
-		GlStateManager.disableBlendProfile(GlStateManager.Profile.PLAYER_SKIN);
-		GlStateManager.popMatrix();
-	}*/
 	
-	//TODO
-	@Override
-	public void renderToBuffer(MatrixStack pMatrixStack, IVertexBuilder pBuffer, int pPackedLight, int pPackedOverlay, float pRed, float pGreen, float pBlue, float pAlpha) {
-		pMatrixStack.pushPose();
-		
-		RenderSystem.enableBlend();
-		
-		super.renderToBuffer(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
-		
-		RenderSystem.disableBlend();
-		
-		pMatrixStack.popPose();
-	}
-
 }
