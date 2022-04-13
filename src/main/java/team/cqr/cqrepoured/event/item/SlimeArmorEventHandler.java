3c7019446d64302bfa6d9a77c939cadb7b8bbabd
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-13 18:33:21 +0200

    update SlimeARmorEventHandler to 1.16

--- src/main/java/team/cqr/cqrepoured/event/item/SlimeArmorEventHandler.java
+++ src/main/java/team/cqr/cqrepoured/event/item/SlimeArmorEventHandler.java
@@ -2,30 +2,33 @@ package team.cqr.cqrepoured.event.item;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.capability.armor.CapabilityCooldownHandlerHelper;
 import team.cqr.cqrepoured.entity.EntitySlimePart;
+import team.cqr.cqrepoured.init.CQREntityTypes;
 import team.cqr.cqrepoured.init.CQRItems;
 import team.cqr.cqrepoured.item.armor.ItemArmorSlime;
 import team.cqr.cqrepoured.util.ItemUtil;
 
-//@EventBusSubscriber(modid = CQRMain.MODID)
+@EventBusSubscriber(modid = CQRMain.MODID)
 public class SlimeArmorEventHandler {
 
 	@SubscribeEvent
 	public static void onLivingHurtEvent(LivingHurtEvent event) {
 		LivingEntity entity = event.getEntityLiving();
 
-		if (ItemUtil.hasFullSet(entity, ItemArmorSlime.class) && !CapabilityCooldownHandlerHelper.onCooldown(entity, CQRItems.CHESTPLATE_SLIME)) {
-			if (!entity.world.isRemote) {
-				EntitySlimePart slime = new EntitySlimePart(entity.world, entity);
-				double x = entity.posX - 5.0D + 2.5D * slime.getRNG().nextDouble();
-				double y = entity.posY;
-				double z = entity.posZ - 5.0D + 2.5D * slime.getRNG().nextDouble();
-				slime.setPosition(x, y, z);
-				entity.world.spawnEntity(slime);
+		if (ItemUtil.hasFullSet(entity, ItemArmorSlime.class) && !CapabilityCooldownHandlerHelper.onCooldown(entity, CQRItems.CHESTPLATE_SLIME.get())) {
+			if (!entity.level.isClientSide) {
+				EntitySlimePart slime = new EntitySlimePart(CQREntityTypes.SMALL_SLIME.get(), entity.level, entity);
+				double x = entity.getX() - 5.0D + 2.5D * slime.getRandom().nextDouble();
+				double y = entity.getY();
+				double z = entity.getZ() - 5.0D + 2.5D * slime.getRandom().nextDouble();
+				slime.setPos(x, y, z);
+				entity.level.addFreshEntity(slime);
 			}
-			CapabilityCooldownHandlerHelper.setCooldown(entity, CQRItems.CHESTPLATE_SLIME, 160);
+			CapabilityCooldownHandlerHelper.setCooldown(entity, CQRItems.CHESTPLATE_SLIME.get(), 160);
 		}
 	}
 
