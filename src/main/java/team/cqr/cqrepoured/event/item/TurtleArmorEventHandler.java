cb577974863240a9c547f0c9ed734c6199048579
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-13 18:36:50 +0200

    update turtleArmorEventHandler to 1.16

--- src/main/java/team/cqr/cqrepoured/event/item/TurtleArmorEventHandler.java
+++ src/main/java/team/cqr/cqrepoured/event/item/TurtleArmorEventHandler.java
@@ -13,13 +13,15 @@ import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.capability.armor.CapabilityCooldownHandlerHelper;
 import team.cqr.cqrepoured.init.CQRItems;
 import team.cqr.cqrepoured.item.armor.ItemArmorTurtle;
 import team.cqr.cqrepoured.util.ItemUtil;
 
-//@EventBusSubscriber(modid = CQRMain.MODID)
+@EventBusSubscriber(modid = CQRMain.MODID)
 public class TurtleArmorEventHandler {
 
 	@SubscribeEvent
@@ -27,12 +29,12 @@ public class TurtleArmorEventHandler {
 		LivingEntity entity = event.getEntityLiving();
 
 		if (ItemUtil.hasFullSet(entity, ItemArmorTurtle.class)) {
-			if (!entity.world.isRemote && entity.ticksExisted % 100 == 0) {
+			if (!entity.level.isClientSide && entity.tickCount % 100 == 0) {
 				entity.heal(1.0F);
-				double x = entity.posX;
-				double y = entity.posY + entity.getEyeHeight();
-				double z = entity.posZ;
-				((ServerWorld) entity.world).spawnParticle(ParticleTypes.HEART, x, y, z, 2, 0.5D, 0.5D, 0.5D, 1.0D);
+				double x = entity.getX();
+				double y = entity.getY() + entity.getEyeHeight();
+				double z = entity.getZ();
+				((ServerWorld) entity.level).addParticle(ParticleTypes.HEART, x, y, z, /*2,*/ 0.5D, 0.5D, 0.5D/*, 1.0D*/);
 			}
 		}
 	}
@@ -43,7 +45,7 @@ public class TurtleArmorEventHandler {
 		if (!ItemUtil.hasFullSet(entity, ItemArmorTurtle.class)) {
 			return;
 		}
-		if (CapabilityCooldownHandlerHelper.onCooldown(entity, CQRItems.CHESTPLATE_TURTLE)) {
+		if (CapabilityCooldownHandlerHelper.onCooldown(entity, CQRItems.CHESTPLATE_TURTLE.get())) {
 			return;
 		}
 		if (entity.getHealth() - event.getAmount() > entity.getMaxHealth() * 0.2F) {
@@ -51,18 +53,18 @@ public class TurtleArmorEventHandler {
 		}
 		event.setAmount(Math.min(event.getAmount(), entity.getHealth() - 1.0F));
 		entity.heal(entity.getMaxHealth() * 0.2F);
-		entity.addPotionEffect(new EffectInstance(Effects.SLOWNESS, 100, 0, false, true));
-		entity.addPotionEffect(new EffectInstance(Effects.REGENERATION, 100, 4, false, true));
-		entity.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 100, 1, false, true));
+		entity.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 100, 0, false, true));
+		entity.addEffect(new EffectInstance(Effects.REGENERATION, 100, 4, false, true));
+		entity.addEffect(new EffectInstance(Effects.DAMAGE_RESISTANCE, 100, 1, false, true));
 
-		double x = entity.posX;
-		double y = entity.posY + entity.getEyeHeight();
-		double z = entity.posZ;
-		((ServerWorld) entity.world).spawnParticle(ParticleTypes.HEART, x, y, z, 4, 0.5D, 0.5D, 0.5D, 1.0D);
+		double x = entity.getX();
+		double y = entity.getY() + entity.getEyeHeight();
+		double z = entity.getZ();
+		((ServerWorld) entity.level).addParticle(ParticleTypes.HEART, x, y, z, /*4,*/ 0.5D, 0.5D, 0.5D/*, 1.0D*/);
 
-		entity.world.playSound(null, x, y, z, SoundEvents.ENTITY_GENERIC_DRINK, SoundCategory.PLAYERS, 0.6F, 1.2F);
+		entity.level.playSound(null, x, y, z, SoundEvents.GENERIC_DRINK, SoundCategory.PLAYERS, 0.6F, 1.2F);
 
-		CapabilityCooldownHandlerHelper.setCooldown(entity, CQRItems.CHESTPLATE_TURTLE, 12000);
+		CapabilityCooldownHandlerHelper.setCooldown(entity, CQRItems.CHESTPLATE_TURTLE.get(), 12000);
 	}
 
 	@SubscribeEvent
@@ -70,19 +72,19 @@ public class TurtleArmorEventHandler {
 		LivingEntity entity = event.getEntityLiving();
 		DamageSource source = event.getSource();
 
-		if (entity.getItemStackFromSlot(EquipmentSlotType.CHEST).getItem() == CQRItems.CHESTPLATE_TURTLE && source.getDamageLocation() != null) {
-			Vector3d hitVec = source.getDamageLocation();
-			double x = entity.posX - hitVec.x;
-			double z = entity.posZ - hitVec.z;
+		if (entity.getItemBySlot(EquipmentSlotType.CHEST).getItem() == CQRItems.CHESTPLATE_TURTLE.get() && source.getSourcePosition() != null) {
+			Vector3d hitVec = source.getSourcePosition();
+			double x = entity.getX() - hitVec.x;
+			double z = entity.getZ() - hitVec.z;
 			double yaw = Math.toDegrees(Math.atan2(-x, z));
 
-			if (ItemUtil.compareRotations(entity.renderYawOffset, yaw, 45.0D)) {
-				double y = (entity.posY + entity.height * 0.5D) - hitVec.y;
+			if (ItemUtil.compareRotations(entity.yBodyRot/*correct replacement for Yaw offset?*/, yaw, 45.0D)) {
+				double y = (entity.getY() + entity.getBbHeight() * 0.5D) - hitVec.y;
 				double pitch = -Math.toDegrees(Math.asin(y));
 
 				if (ItemUtil.compareRotations(0.0D, pitch, 60.0D)) {
-					if (!entity.world.isRemote) {
-						entity.world.playSound(null, hitVec.x, hitVec.y, hitVec.z, SoundEvents.ITEM_SHIELD_BLOCK, SoundCategory.NEUTRAL, 0.5F, 0.8F);
+					if (!entity.level.isClientSide) {
+						entity.level.playSound(null, hitVec.x, hitVec.y, hitVec.z, SoundEvents.SHIELD_BLOCK, SoundCategory.NEUTRAL, 0.5F, 0.8F);
 					}
 					event.setAmount(event.getAmount() * 0.5F);
 				}
