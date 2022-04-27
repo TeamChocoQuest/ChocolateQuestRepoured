2656dc8ad258967cba217f99416d4fcdeab54423
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-04-27 17:56:45 +0200

    fix section dimensions

--- src/main/java/team/cqr/cqrepoured/client/resources/data/GlowingMetadataSection.java
+++ src/main/java/team/cqr/cqrepoured/client/resources/data/GlowingMetadataSection.java
@@ -44,8 +44,8 @@ public class GlowingMetadataSection {
 		}
 
 		public void forEach(BiIntConsumer action) {
-			for (int x = x1; x <= x2; x++) {
-				for (int y = y1; y <= y2; y++) {
+			for (int x = x1; x < x2; x++) {
+				for (int y = y1; y < y2; y++) {
 					action.accept(x, y);
 				}
 			}
