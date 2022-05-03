449c157baaa3882d39839240af12789feac63000
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-05-03 19:21:32 +0200

    migrate walker king illusion to geckolib

--- src/main/java/team/cqr/cqrepoured/client/render/entity/boss/RenderCQRWalkerKingIllusion.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/boss/RenderCQRWalkerKingIllusion.java
@@ -1,14 +1,62 @@
 package team.cqr.cqrepoured.client.render.entity.boss;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+import net.minecraft.block.BlockState;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import team.cqr.cqrepoured.client.render.entity.RenderCQREntity;
-import team.cqr.cqrepoured.client.render.entity.layer.LayerGlowingAreas;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.core.processor.IBone;
+import team.cqr.cqrepoured.client.model.geo.entity.boss.ModelWalkerKingGeo;
+import team.cqr.cqrepoured.client.render.entity.RenderCQRBipedBaseGeo;
+import team.cqr.cqrepoured.client.render.entity.layer.geo.LayerGlowingAreasGeo;
 import team.cqr.cqrepoured.entity.misc.EntityWalkerKingIllusion;
 
-public class RenderCQRWalkerKingIllusion extends RenderCQREntity<EntityWalkerKingIllusion> {
+public class RenderCQRWalkerKingIllusion extends RenderCQRBipedBaseGeo<EntityWalkerKingIllusion> {
 
 	public RenderCQRWalkerKingIllusion(EntityRendererManager rendermanagerIn) {
-		super(rendermanagerIn, "boss/walker_king", true);
-		this.addLayer(new LayerGlowingAreas<>(this, this::getEntityTexture));
+		super(rendermanagerIn, new ModelWalkerKingGeo<>(STANDARD_BIPED_GEO_MODEL, RenderCQRWalkerKing.TEXTURE_WALKER_KING_DEFAULT, "boss/walker_king"));
+		
+		this.addLayer(new LayerGlowingAreasGeo<>(this, this.TEXTURE_GETTER, this.MODEL_ID_GETTER));
+	}
+
+	@Override
+	protected void calculateArmorStuffForBone(String boneName, EntityWalkerKingIllusion currentEntity) {
+		standardArmorCalculationForBone(boneName, currentEntity);
+	}
+
+	@Override
+	protected void calculateItemStuffForBone(String boneName, EntityWalkerKingIllusion currentEntity) {
+		standardItemCalculationForBone(boneName, currentEntity);
+	}
+
+	@Override
+	protected ResourceLocation getTextureForBone(String boneName, EntityWalkerKingIllusion currentEntity) {
+		return null;
+	}
+
+	@Override
+	protected BlockState getHeldBlockForBone(String boneName, EntityWalkerKingIllusion currentEntity) {
+		return null;
+	}
+
+	@Override
+	protected void preRenderItem(MatrixStack matrixStack, ItemStack item, String boneName, EntityWalkerKingIllusion currentEntity, IBone bone) {
+		
+	}
+
+	@Override
+	protected void preRenderBlock(BlockState block, String boneName, EntityWalkerKingIllusion currentEntity) {
+		
+	}
+
+	@Override
+	protected void postRenderItem(MatrixStack matrixStack, ItemStack item, String boneName, EntityWalkerKingIllusion currentEntity, IBone bone) {
+		
+	}
+
+	@Override
+	protected void postRenderBlock(BlockState block, String boneName, EntityWalkerKingIllusion currentEntity) {
+		
 	}
 }
