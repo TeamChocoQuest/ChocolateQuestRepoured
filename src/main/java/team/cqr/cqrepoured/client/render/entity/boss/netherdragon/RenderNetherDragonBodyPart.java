0eb114d1e4b1a8d6df61d740a9378490fd203bef
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-05-21 17:26:18 +0200

    add geo renderer for nether dragon body

---
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/boss/netherdragon/RenderNetherDragonBodyPart.java
@@ -0,0 +1,24 @@
+package team.cqr.cqrepoured.client.render.entity.boss.netherdragon;
+
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+import team.cqr.cqrepoured.CQRMain;
+import team.cqr.cqrepoured.client.model.geo.entity.boss.ModelNetherDragonBodyGeo;
+import team.cqr.cqrepoured.client.render.entity.RenderEntityGeo;
+import team.cqr.cqrepoured.entity.boss.netherdragon.SubEntityNetherDragonSegment;
+
+public class RenderNetherDragonBodyPart extends RenderEntityGeo<SubEntityNetherDragonSegment> {
+	
+	private static final ResourceLocation TEXTURE = CQRMain.prefix("textures/entity/boss/nether_dragon_body.png");
+	private static final ResourceLocation MODEL_RESLOC = CQRMain.prefix("geo/entity/boss/netherdragon/body_normal.geo.json");
+	
+	public RenderNetherDragonBodyPart(EntityRendererManager renderManager) {
+		this(renderManager, new ModelNetherDragonBodyGeo(MODEL_RESLOC, TEXTURE, "boss/nether_dragon"));
+	}
+
+	public RenderNetherDragonBodyPart(EntityRendererManager renderManager, AnimatedGeoModel<SubEntityNetherDragonSegment> modelProvider) {
+		super(renderManager, modelProvider);
+	}
+
+}
---
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/boss/netherdragon/RenderNetherDragonHead.java
@@ -0,0 +1,5 @@
+package team.cqr.cqrepoured.client.render.entity.boss.netherdragon;
+
+public class RenderNetherDragonHead {
+
+}
