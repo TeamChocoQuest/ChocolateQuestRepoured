65a3432afd7a73c55e787c463939745bdf652a38
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-05-22 20:49:19 +0200

    add line-strip rendertype

--- src/main/java/team/cqr/cqrepoured/client/init/CQRRenderTypes.java
+++ src/main/java/team/cqr/cqrepoured/client/init/CQRRenderTypes.java
@@ -1,10 +1,14 @@
 package team.cqr.cqrepoured.client.init;
 
+import java.util.OptionalDouble;
+
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.renderer.RenderState;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.util.ResourceLocation;
-import org.lwjgl.opengl.GL11;
 
 public class CQRRenderTypes extends RenderType {
 
@@ -14,13 +18,23 @@ public class CQRRenderTypes extends RenderType {
 	}
 
 	public static RenderType emissive(ResourceLocation texture) {
-		return RenderType.create("emissive", DefaultVertexFormats.NEW_ENTITY, GL11.GL_QUADS, 256, State.builder()
+		return RenderType.create("cqrepoured_emissive", DefaultVertexFormats.NEW_ENTITY, GL11.GL_QUADS, 256, State.builder()
 				.setAlphaState(DEFAULT_ALPHA)
 				.setCullState(NO_CULL)
 				.setTextureState(new TextureState(texture, false, false))
 				.setTransparencyState(TRANSLUCENT_TRANSPARENCY)
 				.setOverlayState(OVERLAY)
 				.createCompositeState(true));
+	}
+	
+	public static RenderType lineStrip(double lineWidth) {
+		return RenderType.create("cqrepoured_line_strip", DefaultVertexFormats.POSITION_COLOR, GL11.GL_LINE_STRIP, 256, State.builder()
+				.setLineState(new RenderState.LineState(OptionalDouble.of(lineWidth)))
+				.setLayeringState(VIEW_OFFSET_Z_LAYERING)
+				.setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+				.setOutputState(ITEM_ENTITY_TARGET)
+				.setWriteMaskState(COLOR_DEPTH_WRITE)
+				.createCompositeState(false));
 	}
 
 }
