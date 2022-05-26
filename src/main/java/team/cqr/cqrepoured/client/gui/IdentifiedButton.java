3d4bf9a9a15604f1f90e0c01897e25ff63e6f2d2
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-05-26 12:41:27 +0200

    add identified buttons

---
+++ src/main/java/team/cqr/cqrepoured/client/gui/IdentifiedButton.java
@@ -0,0 +1,24 @@
+package team.cqr.cqrepoured.client.gui;
+
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.util.text.ITextComponent;
+
+public class IdentifiedButton extends Button implements INumericIDButton {
+
+	protected final int id;
+	
+	public IdentifiedButton(final int id, int pX, int pY, int pWidth, int pHeight, ITextComponent pMessage, Button.IPressable pOnPress) {
+		this(id, pX, pY, pWidth, pHeight, pMessage, pOnPress, NO_TOOLTIP);
+	}
+	
+	public IdentifiedButton(final int id, int pX, int pY, int pWidth, int pHeight, ITextComponent pMessage, IPressable pOnPress, ITooltip pOnTooltip) {
+		super(pX, pY, pWidth, pHeight, pMessage, pOnPress, pOnTooltip);
+		this.id = id;
+	}
+
+	@Override
+	public int getId() {
+		return this.id;
+	}
+
+}
