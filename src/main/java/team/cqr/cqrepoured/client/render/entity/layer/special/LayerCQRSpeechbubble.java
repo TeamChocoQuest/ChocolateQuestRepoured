d97422eb5b46e3fe59ee1019c63528b753215d33
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-06-02 20:11:16 +0200

    fix speechbubble layer
    TODO: Fix red tint

--- src/main/java/team/cqr/cqrepoured/client/render/entity/layer/special/LayerCQRSpeechbubble.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/layer/special/LayerCQRSpeechbubble.java
@@ -10,8 +10,8 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3f;
 import software.bernie.geckolib3.core.IAnimatable;
-import software.bernie.geckolib3.renderers.geo.ExtendedGeoEntityRenderer;
 import team.cqr.cqrepoured.client.init.CQRRenderTypes;
+import team.cqr.cqrepoured.client.render.entity.RenderCQREntityGeo;
 import team.cqr.cqrepoured.client.render.entity.layer.AbstractCQRLayerGeo;
 import team.cqr.cqrepoured.config.CQRConfig;
 import team.cqr.cqrepoured.entity.bases.AbstractEntityCQR;
@@ -20,43 +20,24 @@ public class LayerCQRSpeechbubble<T extends AbstractEntityCQR & IAnimatable>  ex
 
 	public static final int CHANGE_BUBBLE_INTERVAL = 80;
 
-	//private final ExtendedGeoEntityRenderer<T> extendedRenderer;
-	
-	public LayerCQRSpeechbubble(ExtendedGeoEntityRenderer<T> renderer, Function<T, ResourceLocation> funcGetCurrentTexture, Function<T, ResourceLocation> funcGetCurrentModel) {
+	public LayerCQRSpeechbubble(RenderCQREntityGeo<T> renderer, Function<T, ResourceLocation> funcGetCurrentTexture, Function<T, ResourceLocation> funcGetCurrentModel) {
 		super(renderer, funcGetCurrentTexture, funcGetCurrentModel);
-		//this.extendedRenderer = renderer;
 	}
 
 	@Override
 	public void render(MatrixStack matrixStack, IRenderTypeBuffer bufferIn, int packedLightIn, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
 		if (CQRConfig.general.enableSpeechBubbles && entity.isChatting()) {
-			//Tessellator tessellator = Tessellator.getInstance();
-			//Minecraft minecraft = Minecraft.getInstance();
-
 			matrixStack.pushPose();
+			matrixStack.scale(1F, -1F, 1F);
 			matrixStack.mulPose(Vector3f.YP.rotation(netHeadYaw));
 			//Potentially unneeded
 			// DONE : This does not really line up with scaling mob size
-			/*if (this.extendedRenderer.heightScale != 1.0D || this.extendedRenderer.widthScale != 1.0D) {
-				matrixstack.translate(0.0D, 1.5D, 0.0D);
-				matrixstack.scale(1.0D / this.extendedRenderer.widthScale, 1.0D / this.extendedRenderer.heightScale, 1.0D / this.extendedRenderer.widthScale);
-				matrixstack.translate(0.0D, -1.5D, 0.0D);
-			}*/
 			matrixStack.translate(-0.5D, (-1.0D * entity.getBbHeight()) / entity.getSizeVariation(), 0.0D);
-
-			/*minecraft.getTextureManager().bindTexture(entity.getCurrentSpeechBubble().getResourceLocation());
-
-			BufferBuilder buffer = tessellator.getBuffer();
-			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-			buffer.pos(0, 1, 0).tex(0, 1).endVertex();
-			buffer.pos(1, 1, 0).tex(1, 1).endVertex();
-			buffer.pos(1, 0, 0).tex(1, 0).endVertex();
-			buffer.pos(0, 0, 0).tex(0, 0).endVertex();
-
-			tessellator.draw();*/
+			matrixStack.translate(0, -1.25, 0);
 			
 			IVertexBuilder builder = bufferIn.getBuffer(CQRRenderTypes.speechbubble(entity.getCurrentSpeechBubble().getResourceLocation()));
 			Matrix4f matrix = matrixStack.last().pose();
+
 			builder.vertex(matrix, 0, 1, 0).uv(0, 1).endVertex();
 			builder.vertex(matrix, 1, 1, 0).uv(1, 1).endVertex();
 			builder.vertex(matrix, 1, 0, 0).uv(1, 0).endVertex();
