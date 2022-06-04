9cd44753e9ca383f8c07b6287a8c5d6cd17ad51b
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-06-04 13:05:46 +0200

    remove old code and use constant

--- src/main/java/team/cqr/cqrepoured/client/util/BlockRenderUtil.java
+++ src/main/java/team/cqr/cqrepoured/client/util/BlockRenderUtil.java
@@ -7,6 +7,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.Entity;
 
 public class BlockRenderUtil {
@@ -19,26 +20,8 @@ public class BlockRenderUtil {
 		matrixStack.translate(-0.25F, -0.25F, -0.25F);
 		matrixStack.scale(0.5F, 0.5F, 0.5F);
 
-		//TODO: Recreate in 1.16... find how i made it in the first place...
-		Minecraft.getInstance().getBlockRenderer().renderBlock(iBlockState, matrixStack, rtb, packedLightIn, 655360 /*No overlay*/, null);
+		Minecraft.getInstance().getBlockRenderer().renderBlock(iBlockState, matrixStack, rtb, packedLightIn, OverlayTexture.NO_OVERLAY, null);
 		matrixStack.popPose();
-		
-		/*GlStateManager.enableRescaleNormal();
-		GlStateManager.pushMatrix();
-
-		GlStateManager.translate(-0.25F, -0.25F, 0.25F);
-		GlStateManager.scale(0.5F, 0.5F, 0.5F);
-
-		int i = currentEntity.getBrightness();
-		int j = i % 65536;
-		int k = i / 65536;
-		OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, j, k);
-		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-		renderer.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
-		blockrendererdispatcher.renderBlockBrightness(iBlockState, 1.0F);
-
-		GlStateManager.popMatrix();
-		GlStateManager.disableRescaleNormal();*/
 	}
 
 }
