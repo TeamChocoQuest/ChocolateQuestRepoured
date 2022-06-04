e48b49e8976b29524e23b643be4eb55f000b2394
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-06-05 01:51:56 +0200

    Clean up end laser renderer

--- src/main/java/team/cqr/cqrepoured/client/render/entity/RenderLaser.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/RenderLaser.java
@@ -3,12 +3,10 @@ package team.cqr.cqrepoured.client.render.entity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.MathHelper;
@@ -34,12 +32,10 @@ public class RenderLaser<T extends AbstractEntityLaser> extends EntityRenderer<T
 
 	@Override
 	public void render(T entity, float pEntityYaw, float partialTicks, MatrixStack pMatrixStack, IRenderTypeBuffer pBuffer, int pPackedLight) {
-		super.render(entity, pEntityYaw, partialTicks, pMatrixStack, pBuffer, pPackedLight);
-		
 		if (entity.caster == null) {
 			return;
 		}
-		Minecraft mc = Minecraft.getInstance();
+
 		double x1 = entity.caster.xOld + (entity.caster.getX() - entity.caster.xOld) * partialTicks;
 		double y1 = entity.caster.yOld + (entity.caster.getY() - entity.caster.yOld) * partialTicks;
 		double z1 = entity.caster.zOld + (entity.caster.getZ() - entity.caster.zOld) * partialTicks;
@@ -49,130 +45,46 @@ public class RenderLaser<T extends AbstractEntityLaser> extends EntityRenderer<T
 		z1 += offset.z;
 		float yaw = this.getYaw(entity, partialTicks);
 		float pitch = this.getPitch(entity, partialTicks);
-		Entity renderViewEntity = mc.getCameraEntity();
 		double x2 = entity.xOld + (entity.getX() - entity.xOld) * partialTicks;
 		double y2 = entity.yOld + (entity.getY() - entity.yOld) * partialTicks;
 		double z2 = entity.zOld + (entity.getZ() - entity.zOld) * partialTicks;
 
 		pMatrixStack.pushPose();
-		//Replaced by rendertype
-		/*GlStateManager.disableTexture2D();
-		GlStateManager.disableLighting();
-		OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
-		GlStateManager.enableBlendProfile(GlStateManager.Profile.PLAYER_SKIN);
-		GlStateManager.depthMask(false);
-		this.bindEntityTexture(entity);*/
 		pMatrixStack.translate(x1 - x2, y1 - y2, z1 - z2);
 		pMatrixStack.mulPose(Vector3f.YP.rotationDegrees(180.0F - yaw));
 		pMatrixStack.mulPose(Vector3f.XP.rotationDegrees(-pitch));
 		pMatrixStack.scale(-1.0F, -1.0F, 1.0F);
 
 		float laserLength = this.getLaserLength(entity, pitch, yaw);
-		
-		IVertexBuilder ivb = pBuffer.getBuffer(CQRRenderTypes.laser());
+		this.renderModel(entity, yaw, pitch, partialTicks, pMatrixStack, pBuffer, pPackedLight, laserLength);
 
-		pMatrixStack.pushPose();
-		float d3 = 1.0F;
-		pMatrixStack.scale(d3, d3, laserLength);
-		float f3 = 1.0F / 3.0F;
-		//RenderSystem.color4f(entity.getColorR(), entity.getColorG(), entity.getColorB(), f3);
-		this.model.renderToBuffer(pMatrixStack, ivb, pPackedLight, 0, entity.getColorR(), entity.getColorG(), entity.getColorB(), f3);
 		pMatrixStack.popPose();
-
-		pMatrixStack.pushPose();
-		float d2 = 2.0F / 3.0F;
-		pMatrixStack.scale(d2, d2, laserLength);
-		float f2 = 2.0F / 3.0F;
-		//RenderSystem.color4f(entity.getColorR(), entity.getColorG(), entity.getColorB(), f2);
-		this.model.renderToBuffer(pMatrixStack, ivb, pPackedLight, 0, entity.getColorR(), entity.getColorG(), entity.getColorB(), f2);
-		pMatrixStack.popPose();
-
-		pMatrixStack.pushPose();
-		float d1 = 1.0F / 3.0F;
-		pMatrixStack.scale(d1, d1, laserLength);
-		float f1 = 1.0F;
-		//RenderSystem.color4f(1.0F, 1.0F, 1.0F, f1);
-		this.model.renderToBuffer(pMatrixStack, ivb, pPackedLight, 0, 1.0F, 1.0F, 1.0F, f1);
-		pMatrixStack.popPose();
-
-		//Replaced by rendertype
-		/*GlStateManager.depthMask(true);
-		GlStateManager.disableBlendProfile(GlStateManager.Profile.PLAYER_SKIN);
-		GlStateManager.enableLighting();
-		GlStateManager.enableTexture2D();*/
-		
-		pMatrixStack.popPose();
-		
 	}
-	
-	/*@Override
-	public void doRender(T entity, double x, double y, double z, float entityYaw, float partialTicks) {
-		EmissiveUtil.preEmissiveTextureRendering();
 
-		if (entity.caster == null) {
-			return;
-		}
-		Minecraft mc = Minecraft.getInstance();
-		double x1 = entity.caster.xOld + (entity.caster.getX() - entity.caster.xOld) * partialTicks;
-		double y1 = entity.caster.yOld + (entity.caster.getY() - entity.caster.yOld) * partialTicks;
-		double z1 = entity.caster.zOld + (entity.caster.getZ() - entity.caster.zOld) * partialTicks;
-		Vec3d offset = entity.getOffsetVector();
-		x1 += offset.x;
-		y1 += offset.y;
-		z1 += offset.z;
-		float yaw = this.getYaw(entity, partialTicks);
-		float pitch = this.getPitch(entity, partialTicks);
-		Entity renderViewEntity = mc.getRenderViewEntity();
-		double x2 = renderViewEntity.xOld + (renderViewEntity.getX() - renderViewEntity.xOld) * partialTicks;
-		double y2 = renderViewEntity.yOld + (renderViewEntity.getY() - renderViewEntity.yOld) * partialTicks;
-		double z2 = renderViewEntity.zOld + (renderViewEntity.getZ() - renderViewEntity.zOld) * partialTicks;
+	public void renderModel(T entity, float yaw, float pitch, float partialTicks, MatrixStack pMatrixStack, IRenderTypeBuffer pBuffer, int pPackedLight, float laserLength) {
+		IVertexBuilder vertexBuilder = pBuffer.getBuffer(CQRRenderTypes.laser());
 
-		GlStateManager.pushPose();
-		GlStateManager.disableTexture2D();
-		GlStateManager.disableLighting();
-		OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
-		GlStateManager.enableBlendProfile(GlStateManager.Profile.PLAYER_SKIN);
-		GlStateManager.depthMask(false);
-		this.bindEntityTexture(entity);
-		GlStateManager.translate(x1 - x2, y1 - y2, z1 - z2);
-		GlStateManager.rotate(180.0F - yaw, 0.0F, 1.0F, 0.0F);
-		GlStateManager.rotate(-pitch, 1.0F, 0.0F, 0.0F);
-		GlStateManager.scale(-1.0D, -1.0D, 1.0D);
+		pMatrixStack.pushPose();
+		float width1 = 1.0F;
+		pMatrixStack.scale(width1, width1, laserLength);
+		float alpha1 = 1.0F / 3.0F;
+		this.model.renderToBuffer(pMatrixStack, vertexBuilder, pPackedLight, 0, entity.getColorR(), entity.getColorG(), entity.getColorB(), alpha1);
+		pMatrixStack.popPose();
 
-		double laserLength = this.getLaserLength(entity, pitch, yaw);
+		pMatrixStack.pushPose();
+		float width2 = 2.0F / 3.0F;
+		pMatrixStack.scale(width2, width2, laserLength);
+		float alpha2 = 2.0F / 3.0F;
+		this.model.renderToBuffer(pMatrixStack, vertexBuilder, pPackedLight, 0, entity.getColorR(), entity.getColorG(), entity.getColorB(), alpha2);
+		pMatrixStack.popPose();
 
-		GlStateManager.pushPose();
-		double d3 = 1.0D;
-		GlStateManager.scale(d3, d3, laserLength);
-		float f3 = 1.0F / 3.0F;
-		GlStateManager.color(entity.getColorR(), entity.getColorG(), entity.getColorB(), f3);
-		this.model.render(entity, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
-		GlStateManager.popPose();
-
-		GlStateManager.pushPose();
-		double d2 = 2.0D / 3.0D;
-		GlStateManager.scale(d2, d2, laserLength);
-		float f2 = 2.0F / 3.0F;
-		GlStateManager.color(entity.getColorR(), entity.getColorG(), entity.getColorB(), f2);
-		this.model.render(entity, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
-		GlStateManager.popPose();
-
-		GlStateManager.pushPose();
-		double d1 = 1.0D / 3.0D;
-		GlStateManager.scale(d1, d1, laserLength);
-		float f1 = 1.0F;
-		GlStateManager.color(1.0F, 1.0F, 1.0F, f1);
-		this.model.render(entity, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
-		GlStateManager.popPose();
-
-		GlStateManager.depthMask(true);
-		GlStateManager.disableBlendProfile(GlStateManager.Profile.PLAYER_SKIN);
-		GlStateManager.enableLighting();
-		GlStateManager.enableTexture2D();
-		GlStateManager.popPose();
-
-		EmissiveUtil.postEmissiveTextureRendering();
-	}*/
+		pMatrixStack.pushPose();
+		float width3 = 1.0F / 3.0F;
+		pMatrixStack.scale(width3, width3, laserLength);
+		float alpha3 = 1.0F;
+		this.model.renderToBuffer(pMatrixStack, vertexBuilder, pPackedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, alpha3);
+		pMatrixStack.popPose();
+	}
 
 	protected float getPitch(T entity, float partialTicks) {
 		return this.interpolateRotation(entity.prevRotationPitchCQR, entity.rotationPitchCQR, partialTicks);
@@ -190,9 +102,8 @@ public class RenderLaser<T extends AbstractEntityLaser> extends EntityRenderer<T
 		Vector3d start = entity.position();
 		Vector3d end = start.add(Vector3d.directionFromRotation(pitch, yaw).scale(entity.length));
 		RayTraceContext rtc = new RayTraceContext(start, end, BlockMode.COLLIDER, FluidMode.NONE, entity);
-		BlockRayTraceResult result = entity.level.clip(rtc);//rayTraceBlocks(start, end, false, true, false);
+		BlockRayTraceResult result = entity.level.clip(rtc);
 		float d = result != null ? (float) result.getLocation().subtract(entity.position()).length() : entity.length;
-
 		return d;
 	}
 
--- src/main/java/team/cqr/cqrepoured/client/render/entity/boss/endercalamity/RenderEndLaser.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/boss/endercalamity/RenderEndLaser.java
@@ -2,11 +2,9 @@ package team.cqr.cqrepoured.client.render.entity.boss.endercalamity;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.entity.Entity;
-import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3f;
 import team.cqr.cqrepoured.client.init.CQRRenderTypes;
 import team.cqr.cqrepoured.client.render.entity.RenderLaser;
@@ -18,70 +16,25 @@ public class RenderEndLaser<T extends AbstractEntityLaser> extends RenderLaser<T
 	public RenderEndLaser(EntityRendererManager renderManager) {
 		super(renderManager);
 	}
-	
+
 	@Override
-	public void render(T entity, float pEntityYaw, float partialTicks, MatrixStack pMatrixStack, IRenderTypeBuffer pBuffer, int pPackedLight) {
-		super.render(entity, pEntityYaw, partialTicks, pMatrixStack, pBuffer, pPackedLight);
-		//int renderPass = MinecraftForgeClient.getRenderPass();
-		// Solid objects
-		//if (renderPass == 0) {
-			float yaw = this.getYaw(entity, partialTicks);
-			float pitch = this.getPitch(entity, partialTicks);
+	public void renderModel(T entity, float yaw, float pitch, float partialTicks, MatrixStack pMatrixStack, IRenderTypeBuffer pBuffer, int pPackedLight, float laserLength) {
+		pMatrixStack.pushPose();
+		pMatrixStack.translate(0.0D, 0.0D, -1.0D);
+		for (int i = 0; i < Math.min(Math.ceil(laserLength / 4.0D), 3); i++) {
+			pMatrixStack.translate(0.0D, 0.0D, -4.0D);
+			float colorMultiplier = 0.75F + 0.25F * MathHelper.sin((entity.tickCount + partialTicks) * 0.25F + (float) Math.PI * 0.5F * i);
+			this.renderRing(pMatrixStack, 9 - i * 2, entity, pitch, yaw, 2.0F - i * 0.5F, partialTicks, colorMultiplier);
+		}
+		pMatrixStack.popPose();
 
-			// World coordinates
-			double x1 = entity.caster.xOld + (entity.caster.getX() - entity.caster.xOld) * partialTicks;
-			double y1 = entity.caster.yOld + (entity.caster.getY() - entity.caster.yOld) * partialTicks + entity.caster.getBbHeight() * 0.6D;
-			double z1 = entity.caster.zOld + (entity.caster.getZ() - entity.caster.zOld) * partialTicks;
-
-			Vector3d laserDirection = Vector3d.directionFromRotation(pitch, yaw).scale(0.5D);
-			x1 += laserDirection.x;
-			y1 += laserDirection.y;
-			z1 += laserDirection.z;
-
-			Vector3d worldPos = new Vector3d(x1, y1, z1);
-
-			// REnder ring 1
-			float ticks = 0.25F * entity.tickCount;
-			float colorMultiplier = (float) (0.5F + 0.25F * (1 + Math.sin(ticks)));
-			this.renderRing(pMatrixStack, 5, worldPos, entity, pitch, yaw, 1F, partialTicks, laserDirection, colorMultiplier);
-			if (entity.length >= 4) {
-				Vector3d increment = Vector3d.directionFromRotation(pitch, yaw).normalize().scale(4);
-				worldPos = worldPos.add(increment);
-				colorMultiplier = (float) (0.5F + 0.25F * (1 + Math.sin(ticks + (Math.PI / 2))));
-				this.renderRing(pMatrixStack, 7, worldPos, entity, pitch, yaw, 1.5F, partialTicks, laserDirection, colorMultiplier);
-				if (entity.length >= 8) {
-					worldPos = worldPos.add(increment);
-					colorMultiplier = (float) (0.5F + 0.25F * (1 + Math.sin(ticks + Math.PI)));
-					this.renderRing(pMatrixStack, 9, worldPos, entity, pitch, yaw, 2F, partialTicks, laserDirection, colorMultiplier);
-				}
-			}
-		/*}
-		// Transparent objects
-		else if (renderPass == 1) {
-			GlStateManager.pushAttrib();
-			super.doRender(entity, x, y, z, entityYaw, partialTicks);
-			GlStateManager.popAttrib();
-		}*/
-
+		super.renderModel(entity, yaw, pitch, partialTicks, pMatrixStack, pBuffer, pPackedLight, laserLength);
 	}
 
-	private void renderRing(MatrixStack matrixStack, int corners, Vector3d worldPos, T entity, float pitch, float yaw, float scale, float partialTicks, Vector3d laserDirection, float colorMultiplier) {
+	private void renderRing(MatrixStack matrixStack, int corners, T entity, float pitch, float yaw, float scale, float partialTicks, float colorMultiplier) {
 		matrixStack.pushPose();
-
-		// View coordinates
-		Entity renderViewEntity = Minecraft.getInstance().getCameraEntity();
-		double x2 = renderViewEntity.xOld + (renderViewEntity.getX() - renderViewEntity.xOld) * partialTicks;
-		double y2 = renderViewEntity.yOld + (renderViewEntity.getY() - renderViewEntity.yOld) * partialTicks;
-		double z2 = renderViewEntity.zOld + (renderViewEntity.getZ() - renderViewEntity.zOld) * partialTicks;
-		// From world to view coordinates...
-		matrixStack.translate(worldPos.x - x2, worldPos.y - y2, worldPos.z - z2);
-
+		matrixStack.mulPose(Vector3f.XP.rotationDegrees(90.0F));
 		matrixStack.scale(scale, scale, scale);
-
-		// Rotate pentagram
-		matrixStack.mulPose(Vector3f.YP.rotationDegrees(180.0F - yaw));
-		matrixStack.mulPose(Vector3f.XP.rotationDegrees(-pitch - 90.0F));
-		matrixStack.scale(-1.0F, -1.0F, 1.0F);
 		PentagramUtil.renderPentagram(matrixStack, CQRRenderTypes.emissiveSolid(), entity.getColorR() * colorMultiplier, entity.getColorG() * colorMultiplier, entity.getColorB() * colorMultiplier, corners);
 		matrixStack.popPose();
 	}
