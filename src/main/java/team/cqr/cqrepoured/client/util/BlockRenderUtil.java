04269e855e8eaf1266b2f1712cf15219fdc12fdf
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-06-04 12:31:58 +0200

    update BlockRenderUtil to 1.16.5

--- src/main/java/team/cqr/cqrepoured/client/util/BlockRenderUtil.java
+++ src/main/java/team/cqr/cqrepoured/client/util/BlockRenderUtil.java
@@ -1,12 +1,12 @@
 package team.cqr.cqrepoured.client.util;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.Entity;
 
 public class BlockRenderUtil {
@@ -16,11 +16,11 @@ public class BlockRenderUtil {
 			return;
 		}
 		matrixStack.pushPose();
-		matrixStack.translate(-0.25F, -0.25F, 0.25F);
+		matrixStack.translate(-0.25F, -0.25F, -0.25F);
 		matrixStack.scale(0.5F, 0.5F, 0.5F);
 
 		//TODO: Recreate in 1.16... find how i made it in the first place...
-		Minecraft.getInstance().getBlockRenderer().renderSingleBlock(iBlockState, matrixStack, rtb, packedLightIn, OverlayTexture.NO_OVERLAY);
+		Minecraft.getInstance().getBlockRenderer().renderBlock(iBlockState, matrixStack, rtb, packedLightIn, 655360 /*No overlay*/, null);
 		matrixStack.popPose();
 		
 		/*GlStateManager.enableRescaleNormal();
