6703e1ba01227ce5f28d96ff110307306bab1242
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-06-15 15:48:15 +0200

    update ExporterChestEventHandler

--- src/main/java/team/cqr/cqrepoured/event/block/ExporterChestEventHandler.java
+++ src/main/java/team/cqr/cqrepoured/event/block/ExporterChestEventHandler.java
@@ -2,16 +2,18 @@ package team.cqr.cqrepoured.event.block;
 
 import net.minecraft.block.Blocks;
 import net.minecraftforge.event.world.BlockEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.block.BlockExporterChest;
 
-//@EventBusSubscriber(modid = CQRMain.MODID)
+@EventBusSubscriber(modid = CQRMain.MODID)
 public class ExporterChestEventHandler {
 
 	@SubscribeEvent(priority = EventPriority.HIGH)
 	public static void onPlaceBlockEvent(BlockEvent.EntityPlaceEvent event) {
-		if (event.getPlacedBlock().getBlock() == Blocks.CHEST && !BlockExporterChest.canPlaceChestAt(event.getWorld(), event.getPos())) {
+		if (event.getPlacedBlock().getBlock() == Blocks.CHEST && !BlockExporterChest.canSupportRigidBlock(event.getWorld(), event.getPos())) {
 			event.setCanceled(true);
 		}
 	}
