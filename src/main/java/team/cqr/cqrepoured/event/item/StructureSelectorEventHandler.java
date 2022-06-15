db262f40ec05f2679198f612edb157d6a4296cb3
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-06-15 15:40:31 +0200

    update StructureSelectorEventHandler

--- src/main/java/team/cqr/cqrepoured/event/item/StructureSelectorEventHandler.java
+++ src/main/java/team/cqr/cqrepoured/event/item/StructureSelectorEventHandler.java
@@ -5,31 +5,32 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.item.ItemStructureSelector;
 import team.cqr.cqrepoured.network.client.packet.CPacketStructureSelector;
 
-//@EventBusSubscriber(modid = CQRMain.MODID)
+@EventBusSubscriber(modid = CQRMain.MODID)
 public class StructureSelectorEventHandler {
 
 	@SubscribeEvent
 	public static void onLeftClickBlockEvent(PlayerInteractEvent.LeftClickBlock event) {
-		PlayerEntity player = event.getEntityPlayer();
-		ItemStack stack = player.getHeldItem(event.getHand());
+		PlayerEntity player = event.getPlayer();
+		ItemStack stack = player.getItemInHand(event.getHand());
 
 		if (stack.getItem() instanceof ItemStructureSelector) {
-			if (!player.world.isRemote) {
+			if (!player.level.isClientSide()) {
 				ItemStructureSelector structureSelector = (ItemStructureSelector) stack.getItem();
 
-				if (player.isSneaking()) {
-					BlockPos pos = new BlockPos(player);
+				if (player.isCrouching()) {
+					BlockPos pos = player.blockPosition();
 					structureSelector.setFirstPos(stack, pos);
-					player.sendMessage(new StringTextComponent("First position set to " + pos));
+					player.sendMessage(new StringTextComponent("First position set to " + pos), null);
 				} else {
 					BlockPos pos = event.getPos();
 					structureSelector.setFirstPos(stack, pos);
-					player.sendMessage(new StringTextComponent("First position set to " + pos));
+					player.sendMessage(new StringTextComponent("First position set to " + pos), null);
 				}
 			}
 
@@ -39,10 +40,10 @@ public class StructureSelectorEventHandler {
 
 	@SubscribeEvent
 	public static void onLeftClickEmptyEvent(PlayerInteractEvent.LeftClickEmpty event) {
-		PlayerEntity player = event.getEntityPlayer();
-		ItemStack stack = player.getHeldItem(event.getHand());
+		PlayerEntity player = event.getPlayer();
+		ItemStack stack = player.getItemInHand(event.getHand());
 
-		if (stack.getItem() instanceof ItemStructureSelector && player.isSneaking()) {
+		if (stack.getItem() instanceof ItemStructureSelector && player.isCrouching()) {
 			CQRMain.NETWORK.sendToServer(new CPacketStructureSelector(event.getHand()));
 		}
 	}
