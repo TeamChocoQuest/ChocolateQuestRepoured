a77644233ee3c53121482bad557a2f6c9cac8488
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-06-15 15:42:25 +0200

    update DungeonPlacerEventHandler

--- src/main/java/team/cqr/cqrepoured/event/item/DungeonPlacerEventHandler.java
+++ src/main/java/team/cqr/cqrepoured/event/item/DungeonPlacerEventHandler.java
@@ -1,19 +1,22 @@
 package team.cqr.cqrepoured.event.item;
 
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.network.PacketDistributor;
 import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.network.server.packet.SPacketDungeonSync;
 import team.cqr.cqrepoured.world.structure.generation.DungeonRegistry;
 
-//@EventBusSubscriber(modid = CQRMain.MODID)
+@EventBusSubscriber(modid = CQRMain.MODID)
 public class DungeonPlacerEventHandler {
 
 	@SubscribeEvent
 	public static void onPlayerLoggedInEvent(PlayerEvent.PlayerLoggedInEvent event) {
-		if (!event.player.world.isRemote) {
-			CQRMain.NETWORK.sendTo(new SPacketDungeonSync(DungeonRegistry.getInstance().getDungeons()), (ServerPlayerEntity) event.player);
+		if (!event.getPlayer().level.isClientSide()) {
+			CQRMain.NETWORK.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity)event.getPlayer()), new SPacketDungeonSync(DungeonRegistry.getInstance().getDungeons()));
+			//CQRMain.NETWORK.sendTo(new SPacketDungeonSync(DungeonRegistry.getInstance().getDungeons()), (ServerPlayerEntity) event.player);
 		}
 	}
 
