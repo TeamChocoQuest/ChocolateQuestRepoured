ba0b48e95ad6d730d835eae880fd0891e92b5b41
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-06-19 15:08:30 +0200

    Fix item tier config default values

--- src/main/java/team/cqr/cqrepoured/config/ItemTierConfig.java
+++ src/main/java/team/cqr/cqrepoured/config/ItemTierConfig.java
@@ -15,10 +15,10 @@ public class ItemTierConfig {
 	public ItemTierConfig(ForgeConfigSpec.Builder builder, String name, int uses, float speed, float attackDamageBonus, int level, int enchantmentValue) {
 		builder.comment("").push(name);
 		this.uses = builder.comment("").defineInRange("uses", uses, 1, Integer.MAX_VALUE);
-		this.speed = builder.comment("").defineInRange("speed", uses, 0.0D, Double.MAX_VALUE);
-		this.attackDamageBonus = builder.comment("").defineInRange("attackDamageBonus", uses, 0.0D, Double.MAX_VALUE);
-		this.level = builder.comment("").defineInRange("level", uses, 1, Integer.MAX_VALUE);
-		this.enchantmentValue = builder.comment("").defineInRange("enchantmentValue", uses, 1, Integer.MAX_VALUE);
+		this.speed = builder.comment("").defineInRange("speed", speed, 0.0D, Double.MAX_VALUE);
+		this.attackDamageBonus = builder.comment("").defineInRange("attackDamageBonus", attackDamageBonus, 0.0D, Double.MAX_VALUE);
+		this.level = builder.comment("").defineInRange("level", level, 1, Integer.MAX_VALUE);
+		this.enchantmentValue = builder.comment("").defineInRange("enchantmentValue", enchantmentValue, 1, Integer.MAX_VALUE);
 		builder.pop();
 	}
 
