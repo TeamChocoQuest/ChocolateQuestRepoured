086fecb19a3e8464097b5076bca4aff2c1088f59
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-08-17 19:07:36 +0200

    register vec3d serializer

--- src/main/java/team/cqr/cqrepoured/init/CQRSerializers.java
+++ src/main/java/team/cqr/cqrepoured/init/CQRSerializers.java
@@ -6,13 +6,14 @@ import net.minecraft.network.datasync.IDataSerializer;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DataSerializerEntry;
 import team.cqr.cqrepoured.CQRMain;
 
 /**
  * Copyright (c) 15 Feb 2019 Developed by KalgogSmash GitHub: https://github.com/KalgogSmash
  */
-//@EventBusSubscriber(modid = CQRMain.MODID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CQRSerializers {
 
 	public static final IDataSerializer<Vector3d> VEC3D = new IDataSerializer<Vector3d>() {
@@ -38,13 +39,20 @@ public class CQRSerializers {
 			return value;
 		}
 	};
+	
+	static boolean registered = false;
 
 	@SubscribeEvent
 	public static void registerSerializers(RegistryEvent.Register<DataSerializerEntry> event) {
+		//Yes, this is sadly necessary. Without it the event handler got called two times for me
+		if(registered) {
+			return;
+		}
 		// Create a new DataSerializerEntry (can't register the serializer directly)
 		// and add it to the forge registry list so our classes can use it.
 		// The register() function takes an IForgeRegistryEntry so we create that here from the DataSerializerEntry.
-		event.getRegistry().register(new DataSerializerEntry(VEC3D).setRegistryName(CQRMain.MODID, "serializerVec3d"));
+		event.getRegistry().register(new DataSerializerEntry(VEC3D).setRegistryName(CQRMain.MODID, "serializer_vector_double"));
+		registered = true;
 	}
 
 }
