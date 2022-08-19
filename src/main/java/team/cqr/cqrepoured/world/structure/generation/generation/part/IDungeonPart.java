5c28635b937452efc6886bd603e11f636e20337d
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-08-19 17:10:23 +0200

    Deleted unused classes

--- src/main/java/team/cqr/cqrepoured/world/structure/generation/generation/part/IDungeonPart.java
+++
@@ -1,7 +0,0 @@
-package team.cqr.cqrepoured.world.structure.generation.generation.part;
-
-import team.cqr.cqrepoured.world.structure.generation.generation.generatable.IGeneratable;
-
-public interface IDungeonPart extends IGeneratable {
-
-}
