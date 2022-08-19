87a5e77848dc58c0b0cc5f9956526b839d69667b
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-08-19 16:55:18 +0200

    Remove unused import

--- src/main/java/team/cqr/cqrepoured/mixin/structure/MixinWorldLoadProgressScreen.java
+++ src/main/java/team/cqr/cqrepoured/mixin/structure/MixinWorldLoadProgressScreen.java
@@ -3,7 +3,6 @@ package team.cqr.cqrepoured.mixin.structure;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
