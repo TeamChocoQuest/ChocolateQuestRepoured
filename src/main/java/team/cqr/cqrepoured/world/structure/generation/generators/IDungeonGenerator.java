9ad3ecafb724f58e7465a133d976dc0662e6d952
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-08-22 12:22:15 +0200

    Add parameters required for jigsaw

--- src/main/java/team/cqr/cqrepoured/world/structure/generation/generators/IDungeonGenerator.java
+++ src/main/java/team/cqr/cqrepoured/world/structure/generation/generators/IDungeonGenerator.java
@@ -4,8 +4,10 @@ import java.util.Random;
 import java.util.stream.StreamSupport;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.feature.structure.StructurePiece;
+import net.minecraft.world.gen.feature.template.TemplateManager;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
 import team.cqr.cqrepoured.world.structure.generation.dungeons.DungeonBase;
@@ -13,14 +15,13 @@ import team.cqr.cqrepoured.world.structure.generation.generation.GeneratableDung
 
 public interface IDungeonGenerator<T extends DungeonBase> {
 
-	default StructurePiece prepare(ChunkGenerator chunkGenerator, BlockPos pos, Random random, T config) {
-		ServerWorld level = StreamSupport.stream(ServerLifecycleHooks.getCurrentServer().getAllLevels().spliterator(), false)
-				.filter(serverWorld -> serverWorld.getChunkSource().getGenerator() == chunkGenerator).findFirst().get();
+	default StructurePiece prepare(DynamicRegistries dynamicRegistries, ChunkGenerator chunkGenerator, TemplateManager templateManager, BlockPos pos, Random random, T config) {
+		ServerWorld level = StreamSupport.stream(ServerLifecycleHooks.getCurrentServer().getAllLevels().spliterator(), false).filter(serverWorld -> serverWorld.getChunkSource().getGenerator() == chunkGenerator).findFirst().get();
 		GeneratableDungeon.Builder dungeonBuilder = new GeneratableDungeon.Builder(level, pos, config);
-		prepare(chunkGenerator, pos, random, config, dungeonBuilder);
+		prepare(dynamicRegistries, chunkGenerator, templateManager, pos, random, config, dungeonBuilder);
 		return dungeonBuilder.build();
 	}
 
-	void prepare(ChunkGenerator chunkGenerator, BlockPos pos, Random random, T config, GeneratableDungeon.Builder dungeonBuilder);
+	void prepare(DynamicRegistries dynamicRegistries, ChunkGenerator chunkGenerator, TemplateManager templateManager, BlockPos pos, Random random, T config, GeneratableDungeon.Builder dungeonBuilder);
 
 }
