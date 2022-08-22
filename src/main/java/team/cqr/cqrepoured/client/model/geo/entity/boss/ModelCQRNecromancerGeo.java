97dba4787dbd5e8a2c2df691e451e00c0849a9b0
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-08-22 18:59:04 +0200

    add in new bone shield

--- src/main/java/team/cqr/cqrepoured/client/model/geo/entity/boss/ModelCQRNecromancerGeo.java
+++ src/main/java/team/cqr/cqrepoured/client/model/geo/entity/boss/ModelCQRNecromancerGeo.java
@@ -1,12 +1,23 @@
 package team.cqr.cqrepoured.client.model.geo.entity.boss;
 
 import net.minecraft.util.ResourceLocation;
+import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.entity.boss.EntityCQRNecromancer;
 
 public class ModelCQRNecromancerGeo extends AbstractModelMageGeo<EntityCQRNecromancer> {
 
+	static final ResourceLocation ANIMATION_RESLOC = new ResourceLocation(CQRMain.MODID, "animations/biped_necromancer.animation.json");
+
 	public ModelCQRNecromancerGeo(ResourceLocation model, ResourceLocation textureDefault, String entityName) {
 		super(model, textureDefault, STANDARD_MODEL_HIDDEN_MAGE, STANDARD_ANIMATIONS_HIDDEN_MAGE, STANDARD_TEXTURE_HIDDEN_MAGE, entityName);
+	}
+	
+	@Override
+	public ResourceLocation getAnimationFileLocation(EntityCQRNecromancer animatable) {
+		if(animatable.isIdentityHidden()) {
+			return super.getAnimationFileLocation(animatable);
+		}
+		return ANIMATION_RESLOC;
 	}
 
 }
