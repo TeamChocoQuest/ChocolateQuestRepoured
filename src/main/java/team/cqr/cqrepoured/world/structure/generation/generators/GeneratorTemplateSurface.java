fda1a86274904bf6591dbc4c4f0c28ade92ecd41
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-08-22 18:18:50 +0200

    Update GeneratorTemplateSurface to implement IDungeonGenerator

--- src/main/java/team/cqr/cqrepoured/world/structure/generation/generators/GeneratorTemplateSurface.java
+++ src/main/java/team/cqr/cqrepoured/world/structure/generation/generators/GeneratorTemplateSurface.java
@@ -1,74 +1,40 @@
 package team.cqr.cqrepoured.world.structure.generation.generators;
 
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import team.cqr.cqrepoured.config.CQRConfig;
-import team.cqr.cqrepoured.util.DungeonGenUtils;
-import team.cqr.cqrepoured.world.structure.generation.dungeons.DungeonTemplateSurface;
-import team.cqr.cqrepoured.world.structure.generation.generation.DungeonPlacement;
-import team.cqr.cqrepoured.world.structure.generation.generation.part.CoverDungeonPart;
-import team.cqr.cqrepoured.world.structure.generation.generation.part.PlateauDungeonPart;
-import team.cqr.cqrepoured.world.structure.generation.structurefile.CQStructure;
-import team.cqr.cqrepoured.world.structure.generation.structurefile.Offset;
-
 import java.io.File;
 import java.util.Random;
 
-public class GeneratorTemplateSurface extends AbstractDungeonGenerator<DungeonTemplateSurface> {
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.DynamicRegistries;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.feature.template.TemplateManager;
+import team.cqr.cqrepoured.world.structure.generation.dungeons.DungeonTemplateSurface;
+import team.cqr.cqrepoured.world.structure.generation.generation.GeneratableDungeon.Builder;
+import team.cqr.cqrepoured.world.structure.generation.structurefile.CQStructure;
+import team.cqr.cqrepoured.world.structure.generation.structurefile.Offset;
 
-	private CQStructure structure;
-	private Mirror mirror = Mirror.NONE;
-	private Rotation rotation = Rotation.NONE;
-	private BlockPos structurePos;
-
-	public GeneratorTemplateSurface(World world, BlockPos pos, DungeonTemplateSurface dungeon, Random rand) {
-		super(world, pos, dungeon, rand);
-	}
+public class GeneratorTemplateSurface implements IDungeonGenerator<DungeonTemplateSurface> {
 
 	@Override
-	protected void preProcess() {
-		File file = this.dungeon.getStructureFileFromDirectory(this.dungeon.getStructureFolderPath(), this.random);
+	public void prepare(DynamicRegistries dynamicRegistries, ChunkGenerator chunkGenerator, TemplateManager templateManager, BlockPos pos, Random random, DungeonTemplateSurface config, Builder dungeonBuilder) {
+		File file = config.getStructureFileFromDirectory(config.getStructureFolderPath(), random);
 		if (file == null) {
-			throw new NullPointerException("No structure file found in folder " + this.dungeon.getStructureFolderPath());
+			throw new NullPointerException("No structure file found in folder " + config.getStructureFolderPath());
 		}
-		this.structure = this.loadStructureFromFile(file);
+		CQStructure template = CQStructure.createFromFile(file);
 
-		if (this.dungeon.rotateDungeon()) {
-			this.rotation = Rotation.values()[this.random.nextInt(Rotation.values().length)];
-			this.mirror = Mirror.values()[this.random.nextInt(Mirror.values().length)];
+		Rotation rotation;
+		Mirror mirror;
+		if (config.rotateDungeon()) {
+			rotation = Rotation.values()[random.nextInt(Rotation.values().length)];
+			mirror = Mirror.values()[random.nextInt(Mirror.values().length)];
+		} else {
+			rotation = Rotation.NONE;
+			mirror = Mirror.NONE;
 		}
 
-		this.structurePos = Offset.CENTER.apply(this.pos, this.structure, this.mirror, this.rotation);
-
-		if (this.dungeon.doBuildSupportPlatform()) {
-			BlockPos startPos = this.structurePos.above(this.dungeon.getUnderGroundOffset()).below();
-			BlockPos endPos = startPos.offset(DungeonPlacement.transform(this.structure.getSize().getX() - 1, 0, this.structure.getSize().getZ() - 1, this.mirror, this.rotation));
-			BlockPos pos1 = DungeonGenUtils.getValidMinPos(startPos, endPos);
-			BlockPos pos2 = DungeonGenUtils.getValidMaxPos(startPos, endPos);
-			PlateauDungeonPart.Builder partBuilder = new PlateauDungeonPart.Builder(pos1.getX(), pos1.getZ(), pos2.getX(), pos2.getY(), pos2.getZ(), CQRConfig.general.supportHillWallSize);
-			partBuilder.setSupportHillBlock(this.dungeon.getSupportBlock());
-			partBuilder.setSupportHillTopBlock(this.dungeon.getSupportTopBlock());
-			partBuilder.markGround(structure, structurePos, mirror, rotation);
-			this.dungeonBuilder.add(partBuilder);
-		}
-	}
-
-	@Override
-	protected void buildStructure() {
-		this.structure.addAll(this.dungeonBuilder, this.pos, Offset.CENTER, this.mirror, this.rotation);
-	}
-
-	@Override
-	protected void postProcess() {
-		if (this.dungeon.isCoverBlockEnabled()) {
-			BlockPos startPos = this.structurePos;
-			BlockPos endPos = startPos.offset(DungeonPlacement.transform(this.structure.getSize().getX() - 1, 0, this.structure.getSize().getZ() - 1, this.mirror, this.rotation));
-			BlockPos pos1 = DungeonGenUtils.getValidMinPos(startPos, endPos);
-			BlockPos pos2 = DungeonGenUtils.getValidMaxPos(startPos, endPos);
-			this.dungeonBuilder.add(new CoverDungeonPart.Builder(pos1.getX(), pos1.getZ(), pos2.getX(), pos2.getZ(), this.dungeon.getCoverBlock()));
-		}
+		template.addAll(dungeonBuilder, pos, Offset.CENTER, mirror, rotation);
 	}
 
 }
