9bca10913681cd60bf5ea4ef87e9e435b3658879
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-08-23 20:39:32 +0200

    make lightning protection percentage based

--- src/main/java/team/cqr/cqrepoured/event/enchantment/LightningProtectionEventHandler.java
+++ src/main/java/team/cqr/cqrepoured/event/enchantment/LightningProtectionEventHandler.java
@@ -4,6 +4,7 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.EntityStruckByLightningEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -20,8 +21,16 @@ public class LightningProtectionEventHandler {
 			ItemStack helmet = living.getItemStackFromSlot(EntityEquipmentSlot.HEAD);
 
 			int lvl = EnchantmentHelper.getEnchantmentLevel(CQREnchantments.LIGHTNING_PROTECTION, helmet);
-			if (lvl > 0 && lvl > living.getRNG().nextInt(10)) {
+			if(lvl > 0) {
+				float percentage = 0.1F * lvl;
 				event.setCanceled(true);
+				//TODO: Replace with ASM or mixin
+				//Lightning damage is by default 5
+				living.onStruckByLightning(event.getLightning());
+				//Undo the damage caused by above call
+				living.heal(5.0F);
+				//Apply the correct damage
+				living.attackEntityFrom(DamageSource.LIGHTNING_BOLT, percentage * 5.0F);
 			}
 		}
 	}
