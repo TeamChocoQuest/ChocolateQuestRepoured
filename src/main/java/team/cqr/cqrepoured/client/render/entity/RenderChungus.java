60d1d56a286f73af28f351d944af0ec1a3b41b57
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-08-25 12:10:50 +0200

    update RenderChungus to 1.16

--- src/main/java/team/cqr/cqrepoured/client/render/entity/RenderChungus.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/RenderChungus.java
@@ -1,18 +1,15 @@
 package team.cqr.cqrepoured.client.render.entity;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.entity.EntityRenderer;
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
-import org.lwjgl.opengl.GL11;
 import team.cqr.cqrepoured.CQRMain;
+import team.cqr.cqrepoured.client.render.RenderSpriteBase;
 import team.cqr.cqrepoured.entity.bases.AbstractEntityCQR;
 
-public class RenderChungus<T extends AbstractEntityCQR> extends EntityRenderer<T> {
+public class RenderChungus<T extends AbstractEntityCQR> extends RenderSpriteBase<T> {
 
 	public double widthScale = 1;
 	public double heightScale = 1;
@@ -20,42 +17,17 @@ public class RenderChungus<T extends AbstractEntityCQR> extends EntityRenderer<T
 	private static final ResourceLocation TEXTURE = new ResourceLocation(CQRMain.MODID, "textures/entity/chungus.png");
 
 	public RenderChungus(EntityRendererManager rendermanagerIn) {
-		super(rendermanagerIn);
+		super(rendermanagerIn, TEXTURE);
 	}
 
 	@Override
-	public void doRender(T entity, double x, double y, double z, float entityYaw, float partialTicks) {
-		GlStateManager.pushMatrix();
-		GlStateManager.disableCull();
-		GlStateManager.translate((float) x, (float) y + 0.02, (float) z);
-		GlStateManager.scale(-1, -1, -1);
-		double w = entity.getSizeVariation();
-		double h = entity.getSizeVariation();
-		GlStateManager.scale(w, h, w);
-		GlStateManager.rotate(-entity.rotationYawHead, 0.0F, 1.0F, 0.0F);
-
-		GlStateManager.translate(-0.5D, (-1 * entity.height) / entity.getSizeVariation(), 0.0D);
-		Tessellator tessellator = Tessellator.getInstance();
-		Minecraft minecraft = Minecraft.getMinecraft();
-
-		minecraft.getTextureManager().bindTexture(this.getEntityTexture(entity));
-
-		BufferBuilder buffer = tessellator.getBuffer();
-		buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-		buffer.pos(0, 1.82, 0).tex(0, 1).endVertex();
-		buffer.pos(1, 1.82, 0).tex(1, 1).endVertex();
-		buffer.pos(1, 0, 0).tex(1, 0).endVertex();
-		buffer.pos(0, 0, 0).tex(0, 0).endVertex();
-
-		tessellator.draw();
-
-		GlStateManager.enableCull();
-		GlStateManager.popMatrix();
+	public void render(T entity, float entityYaw, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int packedLight) {
+		matrixStack.pushPose();
+		float w = entity.getSizeVariation();
+		float h = entity.getSizeVariation();
+		matrixStack.scale(w, h, w);
+		super.render(entity, entityYaw, partialTicks, matrixStack, buffer, packedLight);
+		matrixStack.popPose();
 	}
-
-	@Override
-	protected ResourceLocation getEntityTexture(T entity) {
-		return TEXTURE;
-	}
-
+	
 }
