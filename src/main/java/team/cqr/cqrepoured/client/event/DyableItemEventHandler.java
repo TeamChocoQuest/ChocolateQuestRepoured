f4c656b312be6c850b961bf84558327e5fe8a8bc
Patrigan <patrick.provinciael@gmail.com>
2022-10-26 22:10:42 +0200

    Cleanup Chain & DyableItemEventHandler

--- src/main/java/team/cqr/cqrepoured/client/event/DyableItemEventHandler.java
+++ src/main/java/team/cqr/cqrepoured/client/event/DyableItemEventHandler.java
@@ -1,19 +1,27 @@
 package team.cqr.cqrepoured.client.event;
 
+import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.item.Item;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
+import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.init.CQRItems;
 import team.cqr.cqrepoured.item.armor.ItemArmorDyable;
 
-//@EventBusSubscriber(modid = CQRMain.MODID, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = CQRMain.MODID, value = Dist.CLIENT)
 public class DyableItemEventHandler {
 
 	@SubscribeEvent
 	public static void colorItemArmors(ColorHandlerEvent.Item event) {
-		Item[] dyables = CQRItems.EventHandler.ITEMS.stream().filter(ItemArmorDyable.class::isInstance).toArray(Item[]::new);
+		Item[] dyables = CQRItems.ITEMS.getEntries().stream().filter(itemRegistryObject -> itemRegistryObject.get() instanceof ItemArmorDyable).map(RegistryObject::get).toArray(Item[]::new);
 
-		event.getItemColors().registerItemColorHandler((stack, tintIndex) -> tintIndex > 0 ? -1 : ((ItemArmorDyable) stack.getItem()).getColor(stack), dyables);
+		ItemColors itemColors = event.getItemColors();
+		itemColors.register(
+				(stack, tintIndex) -> tintIndex > 0 ? -1 : ((ItemArmorDyable) stack.getItem()).getColor(stack),
+				dyables);
 	}
 
 }
--- src/main/java/team/cqr/cqrepoured/client/model/ModelChain.java
+++ src/main/java/team/cqr/cqrepoured/client/model/ModelChain.java
@@ -1,35 +1,39 @@
 package team.cqr.cqrepoured.client.model;
 
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelBox;
-import net.minecraft.client.model.ModelRenderer;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelChain extends ModelBase {
+public class ModelChain extends EntityModel {
 
 	private final ModelRenderer bone;
 
 	public ModelChain() {
-		this.textureWidth = 16;
-		this.textureHeight = 16;
+		this.texWidth = 16;
+		this.texHeight = 16;
 
 		this.bone = new ModelRenderer(this);
-		this.bone.setRotationPoint(0.0F, 0.0F, 0.0F);
-		this.bone.cubeList.add(new ModelBox(this.bone, 0, 0, -1.5F, -0.5F, -1.0F, 1, 1, 8, 0.0F, false));
-		this.bone.cubeList.add(new ModelBox(this.bone, 0, 0, 0.5F, -0.5F, -1.0F, 1, 1, 8, 0.0F, false));
-		this.bone.cubeList.add(new ModelBox(this.bone, 0, 0, -0.5F, -0.5F, -1.0F, 1, 1, 1, 0.0F, false));
-		this.bone.cubeList.add(new ModelBox(this.bone, 0, 0, -0.5F, -0.5F, 6.0F, 1, 1, 1, 0.0F, false));
+		this.bone.setPos(0.0F, 0.0F, 0.0F);
+		this.bone.texOffs(0, 0).addBox(-1.5F, -0.5F, -1.0F, 1, 1, 8, 0.0F, false);
+		this.bone.texOffs(0, 0).addBox(0.5F, -0.5F, -1.0F, 1, 1, 8, 0.0F, false);
+		this.bone.texOffs(0, 0).addBox(-0.5F, -0.5F, -1.0F, 1, 1, 1, 0.0F, false);
+		this.bone.texOffs(0, 0).addBox(-0.5F, -0.5F, 6.0F, 1, 1, 1, 0.0F, false);
 	}
 
 	@Override
-	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-		this.bone.render(f5);
+	public void setupAnim(Entity pEntity, float pLimbSwing, float pLimbSwingAmount, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch) {
+	}
+
+	@Override
+	public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		bone.render(matrixStack, buffer, packedLight, packedOverlay);
 	}
 
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-		modelRenderer.rotateAngleX = x;
-		modelRenderer.rotateAngleY = y;
-		modelRenderer.rotateAngleZ = z;
+		modelRenderer.xRot = x;
+		modelRenderer.yRot = y;
+		modelRenderer.zRot = z;
 	}
-
 }
