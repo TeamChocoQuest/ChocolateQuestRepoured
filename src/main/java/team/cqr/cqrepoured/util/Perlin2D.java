3121d878dff84762551641a70c67b780d73b0905
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-10-30 15:23:06 +0100

    Improve speed of perlin noise implementations

--- src/main/java/team/cqr/cqrepoured/util/Perlin2D.java
+++ src/main/java/team/cqr/cqrepoured/util/Perlin2D.java
@@ -1,58 +1,43 @@
 package team.cqr.cqrepoured.util;
 
-import net.minecraft.util.math.MathHelper;
-
 import java.util.Random;
+
+import meldexun.randomutil.FastRandom;
+import net.minecraft.util.math.MathHelper;
+import team.cqr.cqrepoured.util.math.InterpolationUtil;
 
 public class Perlin2D {
 
-	private final Random rand = new Random();
-	private long seed;
+	private final Random rand = new FastRandom();
 	private float tanSeed;
 	private float frequency;
 
-	public Perlin2D(long seed, float octave) {
-		this.seed = seed;
-		this.frequency = octave;
-		this.tanSeed = (float) Math.tan(this.seed);
+	public Perlin2D(long seed, int octave) {
+		this.setup(seed, octave);
 	}
 
-	public void setup(long seed, float octave) {
-		this.seed = seed;
-		this.frequency = octave;
-		this.tanSeed = (float) Math.tan(this.seed);
+	public void setup(long seed, int octave) {
+		this.tanSeed = (float) Math.tan((double) seed);
+		this.frequency = 1.0F / (float) octave;
 	}
 
 	public float getNoiseAt(float x, float y) {
-		float fx1 = x / this.frequency;
-		float xmin = MathHelper.floor(fx1);
-		float xmax = xmin + 1.0F;
-		float fx2 = fx1 - xmin;
+		x *= this.frequency;
+		int minX = MathHelper.floor(x);
+		int maxX = minX + 1;
+		float fracX = x - minX;
 
-		float fy1 = y / this.frequency;
-		float ymin = MathHelper.floor(fy1);
-		float ymax = ymin + 1.0F;
-		float fy2 = fy1 - ymin;
+		y *= this.frequency;
+		int minY = MathHelper.floor(y);
+		int maxY = minY + 1;
+		float fracY = y - minY;
 
-		float f1 = this.getRandomAtPosition(xmin, ymin);
-		float f2 = this.getRandomAtPosition(xmin, ymax);
-		float f3 = this.getRandomAtPosition(xmax, ymin);
-		float f4 = this.getRandomAtPosition(xmax, ymax);
+		float f00 = this.getRandomAtPosition(minX, minY);
+		float f01 = this.getRandomAtPosition(minX, maxY);
+		float f10 = this.getRandomAtPosition(maxX, minY);
+		float f11 = this.getRandomAtPosition(maxX, maxY);
 
-		float f11 = cosineInterpolate(f1, f2, fy2);
-		float f12 = cosineInterpolate(f3, f4, fy2);
-
-		return cosineInterpolate(f11, f12, fx2);
-	}
-
-	private static float cosineInterpolate(float a, float b, float x) {
-		float d = (1.0F - MathHelper.cos(x * (float) Math.PI)) * 0.5F;
-		return a * (1.0F - d) + b * d;
-	}
-
-	@SuppressWarnings("unused")
-	private static float linearInterpolate(float a, float b, float x) {
-		return a * (1.0F - x) + b * x;
+		return InterpolationUtil.SMOOTHSTEP.interpolate(f00, f01, f10, f11, fracX, fracY);
 	}
 
 	private float getRandomAtPosition(float x, float y) {
