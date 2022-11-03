b8b0cadaa1bb00a11da79f1d0bab10606be1b484
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-11-03 14:54:19 +0100

    Create Translator.java

---
+++ src/main/java/team/cqr/cqrepoured/util/Translator.java
@@ -0,0 +1,37 @@
+package team.cqr.cqrepoured.util;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TranslationTextComponent;
+
+public class Translator {
+
+	public static TranslationTextComponent translate(String key, Object... args) {
+		return new TranslationTextComponent(key, args);
+	}
+
+	public static TranslationTextComponent translateItem(Item item, String suffix, Object... args) {
+		return translateItem(item.getRegistryName(), suffix, args);
+	}
+
+	public static TranslationTextComponent translateItem(ResourceLocation resourceLocation, String suffix, Object... args) {
+		return translateItem(resourceLocation.getNamespace(), resourceLocation.getPath(), suffix, args);
+	}
+
+	public static TranslationTextComponent translateItem(String namespace, String path, String suffix, Object... args) {
+		return translateItem(namespace, path + suffix, args);
+	}
+
+	public static TranslationTextComponent translateItem(Item item, Object... args) {
+		return translateItem(item.getRegistryName(), args);
+	}
+
+	public static TranslationTextComponent translateItem(ResourceLocation resourceLocation, Object... args) {
+		return translateItem(resourceLocation.getNamespace(), resourceLocation.getPath(), args);
+	}
+
+	public static TranslationTextComponent translateItem(String namespace, String path, Object... args) {
+		return translate("item." + namespace + "." + path, args);
+	}
+
+}
