57800499c333c38fe1243a7ebdebc1aff665558f
Meldexun <27779321+Meldexun@users.noreply.github.com>
2023-03-24 18:46:05 +0100

    Clean up CopyHelper

--- src/main/java/team/cqr/cqrepoured/util/CopyHelper.java
+++ src/main/java/team/cqr/cqrepoured/util/CopyHelper.java
@@ -1,51 +1,50 @@
 package team.cqr.cqrepoured.util;
 
-import team.cqr.cqrepoured.CQRMain;
-
-import java.io.File;
 import java.io.IOException;
 import java.net.JarURLConnection;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.*;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
+
+import team.cqr.cqrepoured.CQRMain;
 
 public class CopyHelper {
 
 	/**
 	 * Small utility by meldexun to extract a folder from a jar
 	 */
-	public static void copyFromJarOrWorkspace(String source, File target, boolean overrideExisitingFiles) {
-		boolean flag = target.exists();
-
-		if (!overrideExisitingFiles && flag) {
-			return;
-		}
-
-		if (!flag) {
-			target.mkdirs();
-		}
-
+	public static void copyFromJarOrWorkspace(String sourceName, Path target, boolean overrideExisitingFiles) {
 		try {
-			Path sourcePath;
-			Path targetPath = target.toPath();
+			if (!overrideExisitingFiles && Files.exists(target)) {
+				return;
+			}
+
+			Path source;
 
 			URL url = CQRMain.class.getResource("");
 			if (url.getProtocol().equals("jar")) {
 				JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
 				try (FileSystem fileSystem = FileSystems.newFileSystem(Paths.get(jarURLConnection.getJarFileURL().toURI()), CQRMain.class.getClassLoader())) {
-					sourcePath = fileSystem.getPath(source);
-					copyFiles(sourcePath, targetPath);
+					source = fileSystem.getPath(sourceName);
+					copyFiles(source, target);
 				}
 			} else {
-				URL resource = CQRMain.class.getResource(source);
+				URL resource = CQRMain.class.getResource(sourceName);
 				if (resource != null) {
-					sourcePath = new File(resource.toURI()).toPath();
-					copyFiles(sourcePath, targetPath);
+					source = Paths.get(resource.toURI());
+					copyFiles(source, target);
 				}
 			}
 		} catch (IOException | URISyntaxException e) {
-			CQRMain.logger.error("Failed to copy file(s) from {} to {}", source, target.getName());
+			CQRMain.logger.error("Failed to copy file(s) from {} to {}", sourceName, target);
 		}
 	}
 
@@ -54,13 +53,13 @@ public class CopyHelper {
 
 			@Override
 			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-				Files.createDirectories(target.resolve(source.relativize(dir).toString()));
+				Files.createDirectories(target.resolve(source.relativize(dir)));
 				return FileVisitResult.CONTINUE;
 			}
 
 			@Override
 			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-				Files.copy(file, target.resolve(source.relativize(file).toString()), StandardCopyOption.REPLACE_EXISTING);
+				Files.copy(file, target.resolve(source.relativize(file)), StandardCopyOption.REPLACE_EXISTING);
 				return FileVisitResult.CONTINUE;
 			}
 
