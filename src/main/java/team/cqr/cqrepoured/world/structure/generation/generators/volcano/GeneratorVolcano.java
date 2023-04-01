c4760e76e24b817ff8b10b5e15d8e780537560d7
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-04-01 20:20:00 +0200

    tweak entrance pos

--- src/main/java/team/cqr/cqrepoured/world/structure/generation/generators/volcano/GeneratorVolcano.java
+++ src/main/java/team/cqr/cqrepoured/world/structure/generation/generators/volcano/GeneratorVolcano.java
@@ -271,7 +271,7 @@ public class GeneratorVolcano extends LegacyDungeonGenerator<DungeonVolcano> {
 				// Second: Place the segments
 				int segmentCount = tunnelLength / EntranceBuilderHelper.SEGMENT_LENGTH;
 				BlockPos segmentPos = new BlockPos(endX, highestPlatformY - this.caveDepth, endZ);
-				segmentPos = segmentPos.relative(direction.getOpposite(), EntranceBuilderHelper.SEGMENT_LENGTH);
+				segmentPos = segmentPos.relative(direction.getOpposite(), -EntranceBuilderHelper.SEGMENT_LENGTH);
 
 				for (int i = 0; i < segmentCount; i++) {
 					EntranceBuilderHelper.buildEntranceSegment(this.pos.offset(segmentPos), this.dungeonBuilder, direction);
