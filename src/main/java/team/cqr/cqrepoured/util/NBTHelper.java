41eda73720095e0fa0b027cf70d3eb7556ee8d62
Meldexun <27779321+Meldexun@users.noreply.github.com>
2023-04-03 15:34:18 +0200

    Add method to write/read BlockPos to/from IntArrayNBT

--- src/main/java/team/cqr/cqrepoured/util/NBTHelper.java
+++ src/main/java/team/cqr/cqrepoured/util/NBTHelper.java
@@ -10,10 +10,12 @@ import java.util.zip.GZIPInputStream;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.INBT;
 import net.minecraft.nbt.INBTType;
+import net.minecraft.nbt.IntArrayNBT;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.nbt.NBTSizeTracker;
 import net.minecraft.nbt.NBTTypes;
 import net.minecraft.nbt.StringNBT;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.util.Constants.NBT;
 
 public class NBTHelper {
@@ -52,6 +54,22 @@ public class NBTHelper {
 			throw new IllegalArgumentException("Expected List-Tag elements to be of type " + expectedElementType.getName() + ", but found " + elementType.getName() + ".");
 		}
 		return (Stream<T>) listNbt.stream();
+	}
+
+	public static IntArrayNBT createBlockPos(BlockPos pos) {
+		return new IntArrayNBT(new int[] { pos.getX(), pos.getY(), pos.getZ() });
+	}
+
+	public static BlockPos loadBlockPos(INBT tag) {
+		INBTType<?> type = tag.getType();
+		if (type != IntArrayNBT.TYPE) {
+			throw new IllegalArgumentException("Expected UUID-Tag to be of type " + IntArrayNBT.TYPE.getName() + ", but found " + type.getName() + ".");
+		}
+		int[] data = ((IntArrayNBT) tag).getAsIntArray();
+		if (data.length != 4) {
+			throw new IllegalArgumentException("Expected UUID-Array to be of length 4, but found " + data.length + ".");
+		}
+		return new BlockPos(data[0], data[1], data[2]);
 	}
 
 }
