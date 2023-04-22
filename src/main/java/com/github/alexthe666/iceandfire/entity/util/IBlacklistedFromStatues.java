57065667ac4806223175ac03553fff7093670d6f
Kasualix <670067575@qq.com>
2023-04-23 07:23:42 +0800

    correct IAF integration interface

--- src/main/java/com/github/alexthe666/iceandfire/entity/util/IBlacklistedFromStatues.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/util/IBlacklistedFromStatues.java
@@ -1,9 +1,9 @@
-package com.github.alexthe666.iceandfire.entity;
+package com.github.alexthe666.iceandfire.entity.util;
 
 /*
  * Copied from Ice and fire
  * Source:
- * https://github.com/Alex-the-666/Ice_and_Fire/blob/1.12.2/src/main/java/com/github/alexthe666/iceandfire/entity/
+ * https://github.com/AlexModGuy/Ice_and_Fire/blob/1.16.3/src/main/java/com/github/alexthe666/iceandfire/entity/util/
  * IBlacklistedFromStatues.java
  * Reason: Is an interface to provide integration
  */
