ffc683f8c77d93081aedda3332521197f6fe9042
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-06-11 13:20:24 +0200

    update CQRVertexFormats

--- src/main/java/team/cqr/cqrepoured/client/init/CQRVertexFormats.java
+++ src/main/java/team/cqr/cqrepoured/client/init/CQRVertexFormats.java
@@ -1,17 +1,16 @@
 package team.cqr.cqrepoured.client.init;
 
-import com.google.common.collect.ImmutableList;
-
+import com.google.common.collect.ImmutableMap;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
-import net.minecraft.client.renderer.vertex.VertexFormatElement;
+import com.mojang.blaze3d.vertex.VertexFormatElement;
 
 public class CQRVertexFormats {
 
-	public static final VertexFormat POSITION_TEX_LIGHTMAP = new VertexFormat(ImmutableList.<VertexFormatElement>builder()
-			.add(DefaultVertexFormat.ELEMENT_POSITION)
-			.add(DefaultVertexFormat.ELEMENT_UV0)
-			.add(DefaultVertexFormat.ELEMENT_UV2)
+	public static final VertexFormat POSITION_TEX_LIGHTMAP = new VertexFormat(ImmutableMap.<String, VertexFormatElement>builder()
+			.put("Position", DefaultVertexFormat.ELEMENT_POSITION)
+			.put("UV", DefaultVertexFormat.ELEMENT_UV0)
+			.put("UV2", DefaultVertexFormat.ELEMENT_UV2)
 			.build());
 
 }
