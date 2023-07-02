59b9adc6f766f9b96c1056b092254986db2c01e9
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-02 16:58:56 +0200

    small correction

--- src/main/java/team/cqr/cqrepoured/entity/profile/EntityBaseData.java
+++ src/main/java/team/cqr/cqrepoured/entity/profile/EntityBaseData.java
@@ -1,20 +1,23 @@
 package team.cqr.cqrepoured.entity.profile;
 
+import java.util.List;
+
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.entity.MobType;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.EntityType;
 
 public record EntityBaseData(
-		MobType entityType
+		MobType mobType,
+		List<EntityType<?>> allowedMounts
 		// TODO: To be extended => intelligence of mob maybe?
 	) {
 	
 	public static final Codec<EntityBaseData> CODEC = RecordCodecBuilder.create(instance -> {
 		return instance.group(
-				// TODO: Create a mixin that introduces a map that holds the mobtypes by their "id" and then create a proper codec here 
+				// TODO: Create a mixin that introduces a map that holds the mobtypes by their "id" and then create a proper codec here
+				BuiltInRegistries.ENTITY_TYPE.byNameCodec().listOf().fieldOf("mounts").forGetter(EntityBaseData::allowedMounts)
 			).apply(instance, EntityBaseData::new);
 	});
 
