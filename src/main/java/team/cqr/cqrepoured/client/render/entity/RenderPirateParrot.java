86da346815ccb3c8761f430577a0fdce59c27556
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-16 13:55:42 +0200

    update parrot renderer

--- src/main/java/team/cqr/cqrepoured/client/render/entity/RenderPirateParrot.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/RenderPirateParrot.java
@@ -2,10 +2,11 @@ package team.cqr.cqrepoured.client.render.entity;
 
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.entity.passive.ParrotEntity;
-import net.minecraft.util.Mth;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.animal.Parrot;
 import team.cqr.cqrepoured.CQRMain;
+import team.cqr.cqrepoured.client.init.CQRModelLayers;
 import team.cqr.cqrepoured.client.model.entity.ModelCQRPirateParrot;
 import team.cqr.cqrepoured.client.render.entity.layer.special.LayerCQRPirateParrotHeldItem;
 import team.cqr.cqrepoured.entity.boss.EntityCQRPirateParrot;
@@ -22,7 +23,7 @@ public class RenderPirateParrot extends MobRenderer<EntityCQRPirateParrot, Model
 	/**
 	 * Defines what float the third param in setRotationAngles of ModelBase is
 	 */
-	public float getBob(ParrotEntity pLivingBase, float pPartialTicks) {
+	public float getBob(Parrot pLivingBase, float pPartialTicks) {
 		float f = Mth.lerp(pPartialTicks, pLivingBase.oFlap, pLivingBase.flap);
 		float f1 = Mth.lerp(pPartialTicks, pLivingBase.oFlapSpeed, pLivingBase.flapSpeed);
 		return (Mth.sin(f) + 1.0F) * f1;
@@ -31,9 +32,9 @@ public class RenderPirateParrot extends MobRenderer<EntityCQRPirateParrot, Model
 	public static final ResourceLocation TEXTURE = new ResourceLocation(CQRMain.MODID, "textures/entity/pirate_parrot.png");
 
 	public RenderPirateParrot(EntityRendererProvider.Context renderManager) {
-		super(renderManager, new ModelCQRPirateParrot(), 0.3F);
+		super(renderManager, new ModelCQRPirateParrot(renderManager.getModelSet().bakeLayer(CQRModelLayers.PIRATE_PARROT)), 0.3F);
 
-		this.addLayer(new LayerCQRPirateParrotHeldItem(this));
+		this.addLayer(new LayerCQRPirateParrotHeldItem(this, renderManager.getItemInHandRenderer()));
 	}
 
 }
