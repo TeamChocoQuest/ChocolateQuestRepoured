31712add27add1dd9695153ac73d7921c082612d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-16 13:46:43 +0200

    add constants class

---
+++ src/main/java/team/cqr/cqrepoured/CQRConstants.java
@@ -0,0 +1,11 @@
+package team.cqr.cqrepoured;
+
+public class CQRConstants {
+	
+	public static class Entity {
+		public static class PirateCaptain {
+			public static final String PARROT_BONE_NAME = "shoulderEntityLeft";
+		}
+	}
+
+}
