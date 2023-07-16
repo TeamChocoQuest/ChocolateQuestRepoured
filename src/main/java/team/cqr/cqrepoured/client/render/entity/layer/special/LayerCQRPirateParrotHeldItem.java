e025b31717ecffb1ccacf392364a2acbb918d8a0
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-16 13:49:25 +0200

    update parrot heldItemLayer i guess

--- src/main/java/team/cqr/cqrepoured/client/render/entity/layer/special/LayerCQRPirateParrotHeldItem.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/layer/special/LayerCQRPirateParrotHeldItem.java
@@ -1,17 +1,18 @@
 package team.cqr.cqrepoured.client.render.entity.layer.special;
 
-import net.minecraft.client.renderer.entity.IEntityRenderer;
-import net.minecraft.client.renderer.entity.layers.HeldItemLayer;
+import net.minecraft.client.renderer.ItemInHandRenderer;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import team.cqr.cqrepoured.client.model.entity.ModelCQRPirateParrot;
 import team.cqr.cqrepoured.entity.boss.EntityCQRPirateParrot;
 
 @OnlyIn(Dist.CLIENT)
-public class LayerCQRPirateParrotHeldItem extends HeldItemLayer<EntityCQRPirateParrot, ModelCQRPirateParrot> {
+public class LayerCQRPirateParrotHeldItem extends ItemInHandLayer<EntityCQRPirateParrot, ModelCQRPirateParrot> {
 
-	public LayerCQRPirateParrotHeldItem(IEntityRenderer<EntityCQRPirateParrot, ModelCQRPirateParrot> renderCQREntity) {
-		super(renderCQREntity);
+	public LayerCQRPirateParrotHeldItem(RenderLayerParent<EntityCQRPirateParrot, ModelCQRPirateParrot> pRenderer, ItemInHandRenderer pItemInHandRenderer) {
+		super(pRenderer, pItemInHandRenderer);
 	}
 	
 }
