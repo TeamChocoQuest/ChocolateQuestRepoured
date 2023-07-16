d454af0d163b0a7633e64b91eb1ec93436bc02a3
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-07-16 14:07:01 +0200

    update ResourceSupplier

--- src/main/java/team/cqr/cqrepoured/client/render/shader/ResourceSupplier.java
+++ src/main/java/team/cqr/cqrepoured/client/render/shader/ResourceSupplier.java
@@ -1,14 +1,15 @@
 package team.cqr.cqrepoured.client.render.shader;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.resources.IResource;
-import net.minecraft.resources.ResourceLocation;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.Resource;
 
 public class ResourceSupplier implements Supplier<String> {
 
@@ -22,16 +23,17 @@ public class ResourceSupplier implements Supplier<String> {
 	public String get() {
 		StringBuilder sb = new StringBuilder();
 
-		try (IResource resource = Minecraft.getInstance().getResourceManager().getResource(this.file)) {
-			try (Stream<String> stream = new BufferedReader(new InputStreamReader(resource.getInputStream())).lines()) {
+		Optional<Resource> optResource = Minecraft.getInstance().getResourceManager().getResource(this.file);
+		optResource.ifPresent(resource -> {
+			try (Stream<String> stream = new BufferedReader(new InputStreamReader(resource.open())).lines()) {
 				stream.forEach(s -> {
 					sb.append(s);
 					sb.append('\n');
 				});
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		});
 
 		return sb.toString();
 	}
