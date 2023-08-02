3402902eafc729d454e6156d6fa1731535fd734d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-02 19:45:48 +0200

    update walkerKignIllusion renderer

--- src/main/java/team/cqr/cqrepoured/client/render/entity/boss/RenderCQRWalkerKingIllusion.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/boss/RenderCQRWalkerKingIllusion.java
@@ -1,51 +1,17 @@
 package team.cqr.cqrepoured.client.render.entity.boss;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.item.ItemStack;
-import software.bernie.geckolib3.core.processor.IBone;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 import team.cqr.cqrepoured.client.model.geo.entity.humanoid.boss.ModelWalkerKingGeo;
 import team.cqr.cqrepoured.client.render.entity.RenderCQRBipedBaseGeo;
-import team.cqr.cqrepoured.client.render.entity.layer.geo.LayerGlowingAreasGeo;
 import team.cqr.cqrepoured.entity.misc.EntityWalkerKingIllusion;
 
 public class RenderCQRWalkerKingIllusion extends RenderCQRBipedBaseGeo<EntityWalkerKingIllusion> {
 
 	public RenderCQRWalkerKingIllusion(EntityRendererProvider.Context rendermanagerIn) {
 		super(rendermanagerIn, new ModelWalkerKingGeo<>(STANDARD_BIPED_GEO_MODEL, RenderCQRWalkerKing.TEXTURE_WALKER_KING_DEFAULT, "boss/walker_king"));
-		
-		this.addLayer(new LayerGlowingAreasGeo<>(this, this.TEXTURE_GETTER, this.MODEL_ID_GETTER));
+
+		this.addRenderLayer(new AutoGlowingGeoLayer<>(this));
 	}
 
-	@Override
-	protected void calculateArmorStuffForBone(String boneName, EntityWalkerKingIllusion currentEntity) {
-		standardArmorCalculationForBone(boneName, currentEntity);
-	}
-
-	@Override
-	protected void calculateItemStuffForBone(String boneName, EntityWalkerKingIllusion currentEntity) {
-		standardItemCalculationForBone(boneName, currentEntity);
-	}
-
-	@Override
-	protected BlockState getHeldBlockForBone(String boneName, EntityWalkerKingIllusion currentEntity) {
-		return null;
-	}
-
-	@Override
-	protected void preRenderBlock(PoseStack stack, BlockState block, String boneName, EntityWalkerKingIllusion currentEntity) {
-		
-	}
-
-	@Override
-	protected void postRenderItem(PoseStack matrixStack, ItemStack item, String boneName, EntityWalkerKingIllusion currentEntity, IBone bone) {
-		
-	}
-
-	@Override
-	protected void postRenderBlock(PoseStack stack, BlockState block, String boneName, EntityWalkerKingIllusion currentEntity) {
-		
-	}
 }
