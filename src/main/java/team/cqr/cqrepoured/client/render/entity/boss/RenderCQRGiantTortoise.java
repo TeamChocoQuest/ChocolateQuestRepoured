15cc5d851b18844d5cf743bbdafcabdde1ee32e1
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-02 19:42:33 +0200

    update tortoise renderer

--- src/main/java/team/cqr/cqrepoured/client/render/entity/boss/RenderCQRGiantTortoise.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/boss/RenderCQRGiantTortoise.java
@@ -1,19 +1,15 @@
 package team.cqr.cqrepoured.client.render.entity.boss;
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import java.util.Optional;
+
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
 import net.minecraft.resources.ResourceLocation;
-import software.bernie.geckolib3.core.processor.IBone;
-import software.bernie.geckolib3.geo.render.built.GeoBone;
+import software.bernie.geckolib.renderer.layer.BlockAndItemGeoLayer;
+import software.bernie.geckolib.renderer.layer.ItemArmorGeoLayer;
 import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.client.model.geo.entity.boss.ModelGiantTortoiseGeo;
 import team.cqr.cqrepoured.client.render.entity.RenderCQREntityGeo;
 import team.cqr.cqrepoured.entity.boss.gianttortoise.EntityCQRGiantTortoise;
-
-import javax.annotation.Nullable;
 
 public class RenderCQRGiantTortoise extends RenderCQREntityGeo<EntityCQRGiantTortoise> {
 	private static final ResourceLocation TEXTURE = new ResourceLocation(CQRMain.MODID, "textures/entity/boss/giant_tortoise.png");
@@ -23,56 +19,16 @@ public class RenderCQRGiantTortoise extends RenderCQREntityGeo<EntityCQRGiantTor
 		super(renderManager, new ModelGiantTortoiseGeo(MODEL_RESLOC, TEXTURE, "boss/giant_tortoise"));
 	}
 
-	// we do not hold items, so we can ignore this
 	@Override
-	protected ItemStack getHeldItemForBone(String boneName, EntityCQRGiantTortoise currentEntity) {
-		return null;
+	protected Optional<ItemArmorGeoLayer<EntityCQRGiantTortoise>> createArmorLayer(RenderCQREntityGeo<EntityCQRGiantTortoise> renderCQREntityGeo) {
+		// TODO: Create layer instance that can display armor on the limbs
+		return Optional.empty();
 	}
-
-	@Nullable
+	
 	@Override
-	protected BlockState getHeldBlockForBone(String boneName, EntityCQRGiantTortoise currentEntity) {
-		return null;
+	protected Optional<BlockAndItemGeoLayer<EntityCQRGiantTortoise>> createBlockAndItemLayer(RenderCQREntityGeo<EntityCQRGiantTortoise> renderCQREntityGeo) {
+		return Optional.empty();
 	}
-
-	@Override
-	protected void preRenderBlock(PoseStack stack, BlockState block, String boneName, EntityCQRGiantTortoise currentEntity) {
-		// Unused
-	}
-
-	@Override
-	protected void postRenderBlock(PoseStack stack, BlockState block, String boneName, EntityCQRGiantTortoise currentEntity) {
-		// Unused
-	}
-
-	@Override
-	protected ResourceLocation getTextureForBone(String boneName, EntityCQRGiantTortoise currentEntity) {
-		return null;
-	}
-
-	@Override
-	protected float getDeathMaxRotation(EntityCQRGiantTortoise entityLivingBaseIn) {
-		return 0;
-	}
-
-	@Override
-	protected TransformType getCameraTransformForItemAtBone(ItemStack boneItem, String boneName) {
-		return TransformType.NONE;
-	}
-
-	@Override
-	protected void preRenderItem(PoseStack matrixStack, ItemStack item, String boneName, EntityCQRGiantTortoise currentEntity, IBone bone) {
-		
-	}
-
-	@Override
-	protected void postRenderItem(PoseStack matrixStack, ItemStack item, String boneName, EntityCQRGiantTortoise currentEntity, IBone bone) {
-		
-	}
-
-	@Override
-	protected boolean isArmorBone(GeoBone bone) {
-		return false;
-	}
+	
 
 }
