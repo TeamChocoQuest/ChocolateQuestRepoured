124efe1985e9480095042e070e2c53bf05fb170e
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-06 12:59:04 +0200

    update ColoredLightningBolt

--- src/main/java/team/cqr/cqrepoured/entity/misc/EntityColoredLightningBolt.java
+++ src/main/java/team/cqr/cqrepoured/entity/misc/EntityColoredLightningBolt.java
@@ -2,34 +2,27 @@ package team.cqr.cqrepoured.entity.misc;
 
 import java.util.List;
 
-import net.minecraft.world.level.block.AbstractFireBlock;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.GameRules;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.entity.effect.LightningBoltEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.Difficulty;
+import net.minecraftforge.entity.IEntityAdditionalSpawnData;
 import net.minecraftforge.event.ForgeEventFactory;
-import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
 import net.minecraftforge.network.NetworkHooks;
 import team.cqr.cqrepoured.config.CQRConfig;
 import team.cqr.cqrepoured.init.CQREntityTypes;
 import team.cqr.cqrepoured.util.EntityUtil;
 
-public class EntityColoredLightningBolt extends LightningBoltEntity implements IEntityAdditionalSpawnData {
+public class EntityColoredLightningBolt extends LightningBolt implements IEntityAdditionalSpawnData {
 
 	/** Declares which state the lightning bolt is in. Whether it's in the air, hit the ground, etc. */
 	private int lightningState;
@@ -78,9 +71,11 @@ public class EntityColoredLightningBolt extends LightningBoltEntity implements I
 		this.green = green;
 		this.blue = blue;
 		this.alpha = alpha;
-		BlockPos blockpos = this.blockPosition();
+		
 
-		if (spreadFire && !worldIn.isClientSide && this.level.getGameRules().getBoolean(GameRules.RULE_DOFIRETICK) && (worldIn.getDifficulty() == Difficulty.NORMAL || worldIn.getDifficulty() == Difficulty.HARD) && worldIn.isAreaLoaded(blockpos, 10)) {
+		//WHY?!
+		/*BlockPos blockpos = this.blockPosition();
+		if (spreadFire && !worldIn.isClientSide && this.level().getGameRules().getBoolean(GameRules.RULE_DOFIRETICK) && (worldIn.getDifficulty() == Difficulty.NORMAL || worldIn.getDifficulty() == Difficulty.HARD) && worldIn.isAreaLoaded(blockpos, 10)) {
 			 BlockState fireBlockState = AbstractFireBlock.getState(this.level, blockpos);
 			if (worldIn.getBlockState(blockpos).getMaterial() == Material.AIR && fireBlockState.canSurvive(worldIn, blockpos)) {
 				worldIn.setBlockAndUpdate(blockpos, Blocks.FIRE.defaultBlockState());
@@ -93,7 +88,7 @@ public class EntityColoredLightningBolt extends LightningBoltEntity implements I
 					worldIn.setBlockAndUpdate(blockpos1, Blocks.FIRE.defaultBlockState());
 				}
 			}
-		}
+		}*/
 	}
 
 	/*@Override
@@ -103,50 +98,44 @@ public class EntityColoredLightningBolt extends LightningBoltEntity implements I
 
 	@Override
 	public void tick() {
-		if (!this.level.isClientSide) {
-			this.setSharedFlag(6, this.isGlowing());
+		if (!this.level().isClientSide) {
+			this.setSharedFlag(6, this.isCurrentlyGlowing());
 		}
 
 		this.baseTick();
 
 		if (this.lightningState == 2) {
-			this.level.playSound((Player)null, this.getX(), this.getY(), this.getZ(), SoundEvents.LIGHTNING_BOLT_THUNDER, SoundSource.WEATHER, 10000.0F, 0.8F + this.random.nextFloat() * 0.2F);
-	         this.level.playSound((Player)null, this.getX(), this.getY(), this.getZ(), SoundEvents.LIGHTNING_BOLT_IMPACT, SoundSource.WEATHER, 2.0F, 0.5F + this.random.nextFloat() * 0.2F);
+			this.level().playSound((Player)null, this.getX(), this.getY(), this.getZ(), SoundEvents.LIGHTNING_BOLT_THUNDER, SoundSource.WEATHER, 10000.0F, 0.8F + this.random.nextFloat() * 0.2F);
+	         this.level().playSound((Player)null, this.getX(), this.getY(), this.getZ(), SoundEvents.LIGHTNING_BOLT_IMPACT, SoundSource.WEATHER, 2.0F, 0.5F + this.random.nextFloat() * 0.2F);
 		}
 
 		--this.lightningState;
 
 		if (this.lightningState < 0) {
 			if (this.boltLivingTime == 0) {
-				this.remove();
+				this.discard();
 			} else if (this.lightningState < -this.random.nextInt(10)) {
 				--this.boltLivingTime;
 				this.lightningState = 1;
+				this.seed = this.random.nextLong();
 
-				if (this.spreadFire && !this.level.isClientSide) {
-					this.seed = this.random.nextLong();
-					BlockPos blockpos = this.blockPosition();
-
-					if (this.level.getGameRules().getBoolean(GameRules.RULE_DOFIRETICK) && this.level.isAreaLoaded(blockpos, 10) && this.level.getBlockState(blockpos).getMaterial() == Material.AIR && Blocks.FIRE.defaultBlockState().canSurvive(this.level, blockpos)) {
-						this.level.setBlockAndUpdate(blockpos, Blocks.FIRE.defaultBlockState());
-					}
-				}
+				this.spawnFire(0);
 			}
 		}
 
 		if (this.lightningState >= 0) {
-			if (this.level.isClientSide) {
-				this.level.setSkyFlashTime(2);
+			if (this.level().isClientSide) {
+				this.level().setSkyFlashTime(2);
 			} else if (this.hitEntities) {
 				AABB aabb = new AABB(this.getX() - 3.0D, this.getY() - 3.0D, this.getZ() - 3.0D, this.getX() + 3.0D, this.getY() + 6.0D + 3.0D, this.getZ() + 3.0D);
-				List<Entity> list = this.level.getEntities(this, aabb);
+				List<Entity> list = this.level().getEntities(this, aabb);
 
 				for (Entity entity : list) {
 					if (!ForgeEventFactory.onEntityStruckByLightning(entity, this)) {
 						if (CQRConfig.SERVER_CONFIG.advanced.flyingCowardPenaltyEnabled.get() && (EntityUtil.isEntityFlying(entity) || EntityUtil.isEntityFlying(entity.getControllingPassenger()))) {
-							entity.hurt(DamageSource.MAGIC, (float)(double)(CQRConfig.SERVER_CONFIG.advanced.flyingCowardPenaltyDamage.get()));
+							entity.hurt(this.damageSources().magic(), (float)(double)(CQRConfig.SERVER_CONFIG.advanced.flyingCowardPenaltyDamage.get()));
 						}
-						entity.thunderHit((ServerLevel)this.level, this);
+						entity.thunderHit((ServerLevel)this.level(), this);
 					}
 				}
 			}
@@ -188,7 +177,7 @@ public class EntityColoredLightningBolt extends LightningBoltEntity implements I
 	}
 	
 	@Override
-	public Packet<?> getAddEntityPacket() {
+	public Packet<ClientGamePacketListener> getAddEntityPacket() {
 		return NetworkHooks.getEntitySpawningPacket(this);
 	}
 
