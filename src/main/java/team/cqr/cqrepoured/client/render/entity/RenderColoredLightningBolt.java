4b1f35b43e1808da57fafe4eb631b08c46b9887e
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-06 12:59:16 +0200

    update renderer for colored lightning bolt

--- src/main/java/team/cqr/cqrepoured/client/render/entity/RenderColoredLightningBolt.java
+++ src/main/java/team/cqr/cqrepoured/client/render/entity/RenderColoredLightningBolt.java
@@ -2,10 +2,11 @@ package team.cqr.cqrepoured.client.render.entity;
 
 import java.util.Random;
 
+import org.joml.Matrix4f;
+
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 
-import com.mojang.math.Matrix4f;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
