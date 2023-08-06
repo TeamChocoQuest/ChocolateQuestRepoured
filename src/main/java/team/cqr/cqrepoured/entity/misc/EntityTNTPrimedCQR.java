07c8f11674e328238024e485d89639aba8728335
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-06 13:00:04 +0200

    update EntityTNTPrimedCQR

--- src/main/java/team/cqr/cqrepoured/entity/misc/EntityTNTPrimedCQR.java
+++ src/main/java/team/cqr/cqrepoured/entity/misc/EntityTNTPrimedCQR.java
@@ -2,11 +2,11 @@ package team.cqr.cqrepoured.entity.misc;
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.entity.item.TNTEntity;
+import net.minecraft.world.entity.item.PrimedTnt;
 import net.minecraft.world.level.Level;
 import team.cqr.cqrepoured.init.CQREntityTypes;
 
-public class EntityTNTPrimedCQR extends TNTEntity {
+public class EntityTNTPrimedCQR extends PrimedTnt {
 
 	public EntityTNTPrimedCQR(Level worldIn) {
 		this(CQREntityTypes.TNT_CQR.get(), worldIn);
