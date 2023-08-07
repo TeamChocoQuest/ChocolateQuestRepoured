f5ffbea3ccda4a25b1cb934c74453591d84c57cd
Meldexun <27779321+Meldexun@users.noreply.github.com>
2023-08-07 15:12:11 +0200

    Update INoiseAffectingStructurePiece to 1.20.1

--- src/main/java/team/cqr/cqrepoured/world/structure/generation/generation/INoiseAffectingStructurePiece.java
+++ src/main/java/team/cqr/cqrepoured/world/structure/generation/generation/INoiseAffectingStructurePiece.java
@@ -3,11 +3,11 @@ package team.cqr.cqrepoured.world.structure.generation.generation;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.Structure;
 
 public interface INoiseAffectingStructurePiece {
 
-	List<Structure<?>> NOISE_AFFECTING_STRUCTURES = new ArrayList<>();
+	List<Structure> NOISE_AFFECTING_STRUCTURES = new ArrayList<>();
 
 	double getContribution(int x, int y, int z);
 
