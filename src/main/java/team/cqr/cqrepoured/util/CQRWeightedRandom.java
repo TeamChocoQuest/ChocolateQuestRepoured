34c5ba98dccecff74c1c7e643fe7748807f5481c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-12 15:45:01 +0200

    add codec to cqrweightedRandom

--- src/main/java/team/cqr/cqrepoured/util/CQRWeightedRandom.java
+++ src/main/java/team/cqr/cqrepoured/util/CQRWeightedRandom.java
@@ -1,19 +1,43 @@
 package team.cqr.cqrepoured.util;
 
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 
+import javax.annotation.Nullable;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
 public class CQRWeightedRandom<T> {
+	
+	public static <T> Codec<CQRWeightedRandom<T>> createCodec(final Codec<T> innerCodec) {
+		return RecordCodecBuilder.create(instance -> {
+			return instance.group(
+					WeightedObject.<T>createCodec(innerCodec).listOf().fieldOf("entries").forGetter(CQRWeightedRandom::getEntries)
+			).apply(instance, CQRWeightedRandom::new);
+		});
+	}
 
 	public static record WeightedObject<T>(T object, int weight) {
+		public static <T> Codec<WeightedObject<T>> createCodec(final Codec<T> innerCodec) {
+			return RecordCodecBuilder.create(instance -> {
+				return instance.group(
+						innerCodec.fieldOf("entry").forGetter(WeightedObject::object),
+						Codec.INT.fieldOf("weight").forGetter(WeightedObject::weight)
+				).apply(instance, WeightedObject::new);
+			});
+		}
 	}
 
 	private static final Random RAND = new Random();
 	private List<WeightedObject<T>> items = new ArrayList<>();
 	private int totalWeight = 0;
+	
+	public List<WeightedObject<T>> getEntries() {
+		return this.items;
+	}
 
 	public CQRWeightedRandom() {
 
@@ -29,6 +53,12 @@ public class CQRWeightedRandom<T> {
 	public CQRWeightedRandom(Collection<WeightedObject<T>> weightedObjects) {
 		for (WeightedObject<T> weightedObject : weightedObjects) {
 			this.add(weightedObject);
+		}
+	}
+	
+	public CQRWeightedRandom(List<WeightedObject<T>> entries) {
+		for (WeightedObject<T> wo : entries) {
+			this.add(wo);
 		}
 	}
 
