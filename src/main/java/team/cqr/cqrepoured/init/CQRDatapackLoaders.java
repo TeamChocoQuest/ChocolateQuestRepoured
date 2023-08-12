f1f0a323966735371bff9582969274c6a5742789
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-12 16:28:09 +0200

    add example access methods

--- src/main/java/team/cqr/cqrepoured/init/CQRDatapackLoaders.java
+++ src/main/java/team/cqr/cqrepoured/init/CQRDatapackLoaders.java
@@ -1,7 +1,12 @@
 package team.cqr.cqrepoured.init;
 
-import de.dertoaster.multihitboxlib.shadow.commoble.databuddy.data.CodecJsonDataManager;
+import java.util.Optional;
+
+import commoble.databuddy.data.CodecJsonDataManager;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.customtextures.TextureSet;
 import team.cqr.cqrepoured.network.datapacksynch.packet.SPacketSyncTextureSet;
@@ -13,6 +18,18 @@ public class CQRDatapackLoaders {
 	
 	public static void init() {
 		TEXTURE_SETS.subscribeAsSyncable(CQRMain.NETWORK, SPacketSyncTextureSet::new);
+	}
+	
+	public static <T> Optional<T> getValueGeneral(final CodecJsonDataManager<T> manager, final ResourceLocation id) {
+		return Optional.ofNullable(manager.getData().getOrDefault(id, null));
+	}
+	
+	public static Optional<TextureSet> getTextureSet(ResourceLocation entityID) {
+		return getValueGeneral(TEXTURE_SETS, entityID);
+	}
+	
+	public static Optional<TextureSet> getTextureSet(EntityType<?> entityType) {
+		return getTextureSet(ForgeRegistries.ENTITY_TYPES.getKey(entityType));
 	}
 	
 }
