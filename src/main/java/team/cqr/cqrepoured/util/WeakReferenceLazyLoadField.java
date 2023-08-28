b4c04200a0642ba4000e1c551998bcc98eb15434
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-28 15:52:45 +0200

    add weakreference lazyLaod field

---
+++ src/main/java/team/cqr/cqrepoured/util/WeakReferenceLazyLoadField.java
@@ -0,0 +1,51 @@
+package team.cqr.cqrepoured.util;
+
+import java.lang.ref.WeakReference;
+
+import com.google.common.base.Supplier;
+
+public class WeakReferenceLazyLoadField<T>  implements Supplier<T> {
+	
+	private WeakReference<T> value;
+	private int lifetime;
+	private long lastSet;
+	private final Supplier<T> function;
+
+	public WeakReferenceLazyLoadField(Supplier<T> function) {
+		this.value = null;
+		this.lifetime = -1;
+		this.lastSet = System.currentTimeMillis();
+		this.function = function;
+	}
+
+	public WeakReferenceLazyLoadField(Supplier<T> function, int lifetime) {
+		this(function);
+		this.lifetime = lifetime;
+	}
+
+	public void reset() {
+		this.value = null;
+	}
+
+	public T get() {
+		if (this.lifetime > 0 && System.currentTimeMillis() - this.lastSet >= (long) this.lifetime) {
+			this.lastSet = System.currentTimeMillis();
+			T functionObject = this.function.get();
+			if (functionObject != null) {
+				this.value = new WeakReference<T>(functionObject);
+			}
+		}
+
+		if (this.value == null || this.value.get() == null) {
+			T functionObject = this.function.get();
+			if (functionObject != null) {
+				this.value = new WeakReference<T>(functionObject);
+			}
+		}
+
+		if (this.value != null) {
+			return this.value.get();
+		}
+		return null;
+	}
+}
