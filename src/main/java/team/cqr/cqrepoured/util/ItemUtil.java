e6b5f4e3f09d4fe061e41cf941fdafb8fc5cdbdb
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-01 23:11:06 +0200

    mostly update ItemUtil

--- src/main/java/team/cqr/cqrepoured/util/ItemUtil.java
+++ src/main/java/team/cqr/cqrepoured/util/ItemUtil.java
@@ -12,28 +12,29 @@ import com.google.common.collect.Multimap;
 import com.google.common.collect.SetMultimap;
 
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.play.server.SEntityVelocityPacket;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.stats.Stats;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.entity.CreatureAttribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier.Operation;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.item.SwordItem;
-import net.minecraft.network.play.server.SEntityVelocityPacket;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.ForgeHooks;
@@ -158,7 +159,7 @@ public class ItemUtil {
 				if (targetEntity instanceof LivingEntity) {
 					f1 = EnchantmentHelper.getDamageBonus(player.getMainHandItem(), ((LivingEntity) targetEntity).getMobType());
 				} else {
-					f1 = EnchantmentHelper.getDamageBonus(player.getMainHandItem(), CreatureAttribute.UNDEFINED);
+					f1 = EnchantmentHelper.getDamageBonus(player.getMainHandItem(), MobType.UNDEFINED);
 				}
 
 				float f2 = player.getAttackStrengthScale(0.5F);
@@ -173,12 +174,12 @@ public class ItemUtil {
 					i = i + EnchantmentHelper.getKnockbackBonus(player);
 
 					if (player.isSprinting() && flag) {
-						player.level.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_KNOCKBACK, player.getSoundSource(), 1.0F, 1.0F);
+						player.level().playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_KNOCKBACK, player.getSoundSource(), 1.0F, 1.0F);
 						++i;
 						flag1 = true;
 					}
 
-					boolean flag2 = flag && player.fallDistance > 0.0F && !player.isOnGround() && !player.onClimbable() && !player.isInWater() && !player.hasEffect(MobEffects.BLINDNESS) && !player.isPassenger() && targetEntity instanceof LivingEntity;
+					boolean flag2 = flag && player.fallDistance > 0.0F && !player.onGround() && !player.onClimbable() && !player.isInWater() && !player.hasEffect(MobEffects.BLINDNESS) && !player.isPassenger() && targetEntity instanceof LivingEntity;
 					flag2 = flag2 && !player.isSprinting();
 
 					CriticalHitEvent hitResult = ForgeHooks.getCriticalHit(player, targetEntity, flag2, flag2 ? 1.5F : 1.0F);
@@ -194,10 +195,10 @@ public class ItemUtil {
 					double d0 = player.walkDist - player.walkDistO;
 
 					// CQR: Disable sweep attack when sweepingEnabled is false
-					if (sweepingEnabled && flag && !flag2 && !flag1 && player.isOnGround() && d0 < player.getSpeed()) {
+					if (sweepingEnabled && flag && !flag2 && !flag1 && player.onGround() && d0 < player.getSpeed()) {
 						ItemStack itemstack = player.getItemInHand(InteractionHand.MAIN_HAND);
 
-						if (itemstack.getItem() instanceof SwordItem) {
+						if (itemstack.getItem() instanceof SwordItem || itemstack.is(ItemTags.SWORDS)) {
 							flag3 = true;
 						}
 					}
@@ -217,14 +218,14 @@ public class ItemUtil {
 
 					Vec3 motion = player.getDeltaMovement();
 					
-					boolean flag5 = targetEntity.hurt(DamageSource.playerAttack(player), f);
+					boolean flag5 = targetEntity.hurt(player.damageSources().playerAttack(player), f);
 
 					if (flag5) {
 						if (i > 0) {
 							if (targetEntity instanceof LivingEntity) {
-								((LivingEntity) targetEntity).knockback(i * 0.5F, Mth.sin(player.yRot * 0.017453292F), (-Mth.cos(player.yRot * 0.017453292F)));
+								((LivingEntity) targetEntity).knockback(i * 0.5F, Mth.sin(player.getYRot() * 0.017453292F), (-Mth.cos(player.getYRot() * 0.017453292F)));
 							} else {
-								targetEntity.push(-Mth.sin(player.yRot * 0.017453292F) * i * 0.5F, 0.1D, Mth.cos(player.yRot * 0.017453292F) * i * 0.5F);
+								targetEntity.push(-Mth.sin(player.getYRot() * 0.017453292F) * i * 0.5F, 0.1D, Mth.cos(player.getYRot() * 0.017453292F) * i * 0.5F);
 							}
 
 							player.setDeltaMovement(player.getDeltaMovement().multiply(0.6, 1.0, 0.6));
@@ -239,16 +240,16 @@ public class ItemUtil {
 							double entityReachDistanceSqr = getEntityReachDistanceSqr(player);
 							// CQR: Allow modification of sweeping hitbox
 							AABB aabb = targetEntity.getBoundingBox().expandTowards(sweepingRangeHorizontal, sweepingRangeVertical, sweepingRangeHorizontal);
-							for (LivingEntity entitylivingbase : player.level.getEntitiesOfClass(LivingEntity.class, aabb)) {
+							for (LivingEntity entitylivingbase : player.level().getEntitiesOfClass(LivingEntity.class, aabb)) {
 								// CQR: Increase sweeping range when players reach distance is higher
 								if (entitylivingbase != player && entitylivingbase != targetEntity && !player.isAlliedTo(entitylivingbase) && player.distanceToSqr(entitylivingbase) < entityReachDistanceSqr) {
 									// CQR: Allow modification of sweeping knockback strength
-									entitylivingbase.knockback(sweepingKnockback, Mth.sin(player.yRot * 0.017453292F), (-Mth.cos(player.yRot * 0.017453292F)));
-									entitylivingbase.hurt(DamageSource.playerAttack(player), f3);
+									entitylivingbase.knockback(sweepingKnockback, Mth.sin(player.getYRot() * 0.017453292F), (-Mth.cos(player.getYRot() * 0.017453292F)));
+									entitylivingbase.hurt(player.damageSources().playerAttack(player), f3);
 								}
 							}
 
-							player.level.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_SWEEP, player.getSoundSource(), 1.0F, 1.0F);
+							player.level().playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_SWEEP, player.getSoundSource(), 1.0F, 1.0F);
 							player.sweepAttack();
 						}
 
@@ -259,18 +260,18 @@ public class ItemUtil {
 						}
 
 						if (flag2) {
-							player.level.playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_CRIT, player.getSoundSource(), 1.0F, 1.0F);
+							player.level().playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_CRIT, player.getSoundSource(), 1.0F, 1.0F);
 							player.crit(targetEntity);
 						} else if (fakeCrit) { // CQR: Allow fake crits to happen
-							player.level.playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_CRIT, player.getSoundSource(), 1.0F, 1.2F);
+							player.level().playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_CRIT, player.getSoundSource(), 1.0F, 1.2F);
 							player.crit(targetEntity);
 						}
 
 						if (!flag2 && !fakeCrit && !flag3) {
 							if (flag) {
-								player.level.playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_STRONG, player.getSoundSource(), 1.0F, 1.0F);
+								player.level().playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_STRONG, player.getSoundSource(), 1.0F, 1.0F);
 							} else {
-								player.level.playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_WEAK, player.getSoundSource(), 1.0F, 1.0F);
+								player.level().playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_WEAK, player.getSoundSource(), 1.0F, 1.0F);
 							}
 						}
 
@@ -314,15 +315,15 @@ public class ItemUtil {
 								targetEntity.setSecondsOnFire(j * 4);
 							}
 
-							if (player.level instanceof ServerLevel && f5 > 2.0F) {
+							if (player.level() instanceof ServerLevel && f5 > 2.0F) {
 								int k = (int) (f5 * 0.5D);
-								((ServerLevel)targetEntity.level).sendParticles(ParticleTypes.DAMAGE_INDICATOR, targetEntity.getX(), targetEntity.getY(0.5D), targetEntity.getZ(), k, 0.1D, 0.0D, 0.1D, 0.2D);
+								((ServerLevel)targetEntity.level()).sendParticles(ParticleTypes.DAMAGE_INDICATOR, targetEntity.getX(), targetEntity.getY(0.5D), targetEntity.getZ(), k, 0.1D, 0.0D, 0.1D, 0.2D);
 							}
 						}
 
 						player.causeFoodExhaustion(0.1F);
 					} else {
-						player.level.playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_NODAMAGE, player.getSoundSource(), 1.0F, 1.0F);
+						player.level().playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_NODAMAGE, player.getSoundSource(), 1.0F, 1.0F);
 
 						if (flag4) {
 							targetEntity.clearFire();
@@ -334,7 +335,7 @@ public class ItemUtil {
 	}
 
 	private static double getEntityReachDistanceSqr(Player player) {
-		double d = player.getAttribute(ForgeMod.REACH_DISTANCE.get()).getValue();
+		double d = player.getAttribute(ForgeMod.ENTITY_REACH.get()).getValue();
 		if (!player.isCreative()) {
 			d -= 0.5D;
 		}
