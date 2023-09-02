9e5a4bb38468c38b1319eefaedfb710655809c8b
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-02 10:53:49 +0200

    update nullblock

--- src/main/java/team/cqr/cqrepoured/block/BlockNull.java
+++ src/main/java/team/cqr/cqrepoured/block/BlockNull.java
@@ -1,38 +1,35 @@
 package team.cqr.cqrepoured.block;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.GlassBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.GlassBlock;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
-import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.Shapes;
-import net.minecraft.world.level.BlockGetter;
-import team.cqr.cqrepoured.init.CQRBlocks;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class BlockNull extends GlassBlock {
 
 	public static final BooleanProperty PASSABLE = BooleanProperty.create("passable");
 
 	public BlockNull() {
-		super(Properties.of(Material.GLASS)
+		super(Properties.of()
 				.sound(SoundType.GLASS)
 				.strength(-1.0F, 3600000.0F)
-				.noDrops()
 				.noOcclusion()
-				.isValidSpawn(CQRBlocks::never)
-				.isRedstoneConductor(CQRBlocks::never)
-				.isSuffocating(CQRBlocks::never)
-				.isViewBlocking(CQRBlocks::never)
+				.isValidSpawn((bs, l, p, e) -> false)
+				.isRedstoneConductor((bs, l, p) -> false)
+				.isSuffocating((bs, l, p) -> false)
+				.isViewBlocking((bs, l, p) -> false)
 		);
 		this.registerDefaultState(this.stateDefinition.any().setValue(PASSABLE, false));
 	}
