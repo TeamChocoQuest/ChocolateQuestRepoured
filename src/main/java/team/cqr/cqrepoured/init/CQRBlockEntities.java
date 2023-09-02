ce150aa1e26d110741d964f8692842036c046cfb
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-02 11:09:45 +0200

    update TE registry

--- src/main/java/team/cqr/cqrepoured/init/CQRBlockEntities.java
+++ src/main/java/team/cqr/cqrepoured/init/CQRBlockEntities.java
@@ -2,6 +2,11 @@ package team.cqr.cqrepoured.init;
 
 import java.util.stream.Stream;
 
+import com.mojang.datafixers.types.Type;
+
+import net.minecraft.Util;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.util.datafix.fixes.References;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -10,6 +15,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import team.cqr.cqrepoured.CQRConstants;
+import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.block.BlockExporterChestFixed;
 import team.cqr.cqrepoured.block.BlockTable;
 import team.cqr.cqrepoured.tileentity.BlockEntityPhylactery;
@@ -29,16 +35,21 @@ public class CQRBlockEntities {
 	public static final RegistryObject<BlockEntityType<TileEntityExporter>> EXPORTER = register("exporter", TileEntityExporter::new, CQRBlocks.EXPORTER);
 	public static final RegistryObject<BlockEntityType<TileEntityTable>> TABLE = register("table", TileEntityTable::new, blocks(BlockTable.class));
 	public static final RegistryObject<BlockEntityType<TileEntitySpawner>> SPAWNER = register("spawner", TileEntitySpawner::new, CQRBlocks.SPAWNER);
-	public static final RegistryObject<BlockEntityType<TileEntityForceFieldNexus>> FORCE_FIELD_NEXUS = register("force_field_nexus", TileEntityForceFieldNexus::new, CQRBlocks.FORCE_FIELD_NEXUS);
+	public static final RegistryObject<BlockEntityType<TileEntityForceFieldNexus>> FORCE_FIELD_NEXUS = register("force_field_nexus", BlockEntityType.Builder.of(TileEntityForceFieldNexus::new, CQRBlocks.FORCE_FIELD_NEXUS));
 	public static final RegistryObject<BlockEntityType<TileEntityExporterChestFixed>> EXPORTER_CHEST_CQR = register("exporter_chest_cqr", TileEntityExporterChestFixed::new, blocks(BlockExporterChestFixed.class));
 	public static final RegistryObject<BlockEntityType<TileEntityExporterChestCustom>> EXPORTER_CHEST_CUSTOM = register("exporter_chest_custom", TileEntityExporterChestCustom::new, CQRBlocks.EXPORTER_CHEST_CUSTOM);
-	public static final RegistryObject<BlockEntityType<TileEntityBoss>> BOSS = register("boss", TileEntityBoss::new, CQRBlocks.BOSS_BLOCK);
+	public static final RegistryObject<BlockEntityType<TileEntityBoss>> BOSS = register("boss", BlockEntityType.Builder.of(TileEntityBoss::new, CQRBlocks.BOSS_BLOCK));
 	public static final RegistryObject<BlockEntityType<TileEntityMap>> MAP = register("map", TileEntityMap::new, CQRBlocks.MAP_PLACEHOLDER);
 
-	public static final RegistryObject<BlockEntityType<BlockEntityPhylactery>> PHYLACTERY = register("phylactery", BlockEntityPhylactery::new, CQRBlocks.PHYLACTERY);
+	public static final RegistryObject<BlockEntityType<BlockEntityPhylactery>> PHYLACTERY = register("phylactery", BlockEntityType.Builder.of(BlockEntityPhylactery::new, CQRBlocks.PHYLACTERY));
 	
-	private static <T extends BlockEntity> RegistryObject<BlockEntityType<T>> register(String name, BlockEntityType.BlockEntitySupplier<T> supplier, RegistryObject<Block> b) {
-		return TILE_ENTITIES.register(name, () -> BlockEntityType.Builder.of(supplier, b.get()).build(null));
+	private static <T extends BlockEntity> RegistryObject<BlockEntityType<T>> register(String name, BlockEntityType.Builder<T> builder) {
+		 if (builder.validBlocks.isEmpty()) {
+	         CQRMain.logger.warn("Block entity type {} requires at least one valid block to be defined!", (Object)pKey);
+	      }
+
+	      Type<?> type = Util.fetchChoiceType(References.BLOCK_ENTITY, name);
+	      return TILE_ENTITIES.register(name, () -> builder.build(type));
 	}
 
 	private static Stream<RegistryObject<Block>> blocks(Class<? extends Block> blockClass) {
