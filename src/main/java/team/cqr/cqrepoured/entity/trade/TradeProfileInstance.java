b9fbcddc9e0a25ad8fb13c928ff11818e53dd5de
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-04 16:17:04 +0200

    implement TradeProfileInstance => attached to actual trader
    Also holds generic logic that can be used (save/load, buying, checking
    methods)

---
+++ src/main/java/team/cqr/cqrepoured/entity/trade/TradeProfileInstance.java
@@ -0,0 +1,191 @@
+package team.cqr.cqrepoured.entity.trade;
+
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.annotation.Nullable;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import it.unimi.dsi.fastutil.ints.Int2IntArrayMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import team.cqr.cqrepoured.CQRConstants;
+import team.cqr.cqrepoured.init.CQRDatapackLoaders;
+import team.cqr.cqrepoured.util.WeakReferenceLazyLoadField;
+
+public class TradeProfileInstance {
+
+	private final ResourceLocation referencedProfile;
+	private WeakReference<Entity> trader;
+	
+	protected final WeakReferenceLazyLoadField<TradeProfile> TRADE_PROFILE = new WeakReferenceLazyLoadField<>(this::loadTradeProfile);
+	protected final Map<Integer, TradeData> TRADE_OVERRIDES = new Int2ObjectArrayMap<>();
+	protected final Map<Integer, Integer> TRADE_STOCK = new Int2IntArrayMap();
+	
+	// Codec to save and load from/to NBT
+	public static final Codec<TradeProfileInstance> CODEC = RecordCodecBuilder.create(instance -> {
+		return instance.group(
+				ResourceLocation.CODEC.fieldOf("referenced-profile").forGetter(tpi -> tpi.referencedProfile),
+				Codec.unboundedMap(Codec.INT, TradeData.CODEC).optionalFieldOf("trade-overrides").forGetter(tpi -> Optional.ofNullable(tpi.TRADE_OVERRIDES)),
+				Codec.unboundedMap(Codec.INT, Codec.INT).optionalFieldOf("trade-stock").forGetter(tpi -> Optional.ofNullable(tpi.TRADE_STOCK))
+			).apply(instance, TradeProfileInstance::new);
+	});
+	
+	protected TradeProfileInstance(ResourceLocation referencedTradeProfile, Optional<Map<Integer, TradeData>> overrides, Optional<Map<Integer, Integer>> stockData) {
+		this.referencedProfile = referencedTradeProfile;
+		if (overrides.isPresent()) {
+			this.TRADE_OVERRIDES.putAll(overrides.get());
+		}
+		if (stockData.isPresent()) {
+			this.TRADE_STOCK.putAll(stockData.get());
+		}
+	}
+	
+	@Nullable
+	protected final TradeProfile loadTradeProfile() {
+		if (this.referencedProfile == null) {
+			return null;
+		}
+		return CQRDatapackLoaders.getTradeProfile(this.referencedProfile).get();
+	}
+	
+	public TradeProfileInstance(final ResourceLocation referencedProfile, final Entity trader) {
+		this.referencedProfile = referencedProfile;
+		if (this.referencedProfile != null) {
+			// Load directly
+			this.TRADE_PROFILE.get();
+		}
+		this.trader = new WeakReference<Entity>(trader);
+	}
+	
+	// Logic
+	public boolean hasAnyTrades() {
+		TradeProfile tp = this.TRADE_PROFILE.get();
+		if (tp == null) {
+			return false;
+		}
+		return !tp.isEmpty();
+	}
+	
+	@Nullable
+	protected TradeData getTradeAt(final int index) {
+		if (!this.TRADE_OVERRIDES.containsKey(index) || this.TRADE_PROFILE.get() == null) {
+			return null;
+		}
+		if (this.TRADE_PROFILE.get().size() <= index) {
+			return null;
+		}
+		return this.TRADE_OVERRIDES.getOrDefault(index, this.TRADE_PROFILE.get().get(index));
+	}
+	
+	public boolean isInStock(final int tradeIndex, final Entity customer) {
+		TradeData tradeData = this.getTradeAt(tradeIndex);
+		if (tradeData == null) {
+			return false;
+		}
+		
+		Optional<StockData> optStockData = tradeData.getStockData();
+		if (optStockData.isPresent()) {
+			StockData stockData = optStockData.get();
+			
+			// Get or compute stock count
+			final RandomSource rs = customer instanceof LivingEntity le ? le.getRandom() : customer.level().getRandom();
+			final int inStock = this.TRADE_STOCK.computeIfAbsent(tradeIndex, key -> stockData.defaultStockSupplier().sample(rs));
+			
+			if (inStock <= 0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public boolean doesCustomerMeetRequirements(final int tradeIndex, final Entity customer) {
+		TradeData tradeData = this.getTradeAt(tradeIndex);
+		if (tradeData == null) {
+			return false;
+		}
+		
+		return tradeData.matchesConstraints(customer, this.trader.get());
+	}
+	
+	public boolean doesInputMeetRequirements(final int tradeIndex, final ItemStack... inputs) {
+		TradeData tradeData = this.getTradeAt(tradeIndex);
+		if (tradeData == null) {
+			return false;
+		}
+		
+		return tradeData.matches(inputs);
+	}
+	
+	public ItemStack getTradeResult(final int tradeIndex, final Entity customer, final ItemStack... inputs) {
+		ItemStack result = ItemStack.EMPTY;
+		
+		TradeData tradeData = this.getTradeAt(tradeIndex);
+		if (tradeData == null) {
+			return result;
+		}
+
+		if (!this.isInStock(tradeIndex, customer)) {
+			return result;
+		}
+		
+		// Now, let's actually ask the trade if we can buy it...
+		if (this.doesCustomerMeetRequirements(tradeIndex, customer) && this.doesInputMeetRequirements(tradeIndex, inputs)) {
+			return tradeData.getResultingItem();
+		}
+		
+		
+		return result;
+	}
+	
+	// Saving & Loading
+	
+	public static CompoundTag saveToNBT(CompoundTag nbt, final TradeProfileInstance tpi) {
+		if (tpi == null) {
+			return nbt;
+		}
+		return tpi.saveToNBT(nbt);
+	}
+	
+	public CompoundTag saveToNBT(CompoundTag nbt) {
+		DataResult<Tag> dataResult = CODEC.encodeStart(NbtOps.INSTANCE, this);
+		Optional<Tag> optResult = dataResult.result();
+		if (optResult.isPresent()) {
+			nbt.put(CQRConstants.NBT.KEY_TRADE_PROFILE_DATA, optResult.get());
+			return nbt;
+		} else {
+			return nbt;
+		}
+	}
+	
+	@Nullable
+	public static TradeProfileInstance readFromNBT(CompoundTag nbt, final Entity trader) {
+		if (!nbt.contains(CQRConstants.NBT.KEY_TRADE_PROFILE_DATA)) {
+			return null;
+		}
+		Tag nbtTag = nbt.get(CQRConstants.NBT.KEY_TRADE_PROFILE_DATA);
+		DataResult<TradeProfileInstance> dataResult = CODEC.parse(NbtOps.INSTANCE, nbtTag);
+		Optional<TradeProfileInstance> optResult = dataResult.result();
+		if (optResult.isEmpty()) {
+			return null;
+		}
+		
+		TradeProfileInstance result = optResult.get();
+		result.trader = new WeakReference<Entity>(trader);
+		
+		return result;
+	}
+
+}
