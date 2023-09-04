5e4c7d51f45c4e5d6ed48f94c73d588dd85365f7
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-04 17:11:46 +0200

    implement MatchEnchants

--- src/main/java/team/cqr/cqrepoured/entity/trade/rules/input/MatchEnchants.java
+++ src/main/java/team/cqr/cqrepoured/entity/trade/rules/input/MatchEnchants.java
@@ -1,16 +1,23 @@
 package team.cqr.cqrepoured.entity.trade.rules.input;
 
+import java.util.Map;
+
 import com.mojang.serialization.Codec;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
 import team.cqr.cqrepoured.init.CQRTradeRules;
 
 public class MatchEnchants implements ITradeMatchRule {
 
 	@Override
 	public boolean matches(ItemStack input, ItemStack toMatch) {
-		// TODO Auto-generated method stub
-		return false;
+		for (Map.Entry<Enchantment, Integer> entry : toMatch.getAllEnchantments().entrySet()) {
+			if (input.getAllEnchantments().getOrDefault(entry.getKey(), -1) == entry.getValue()) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
