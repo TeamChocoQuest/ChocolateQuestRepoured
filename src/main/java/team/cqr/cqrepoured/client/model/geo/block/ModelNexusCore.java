7d5dc590c6283686ef0fb633971ff4149bb1b698
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-06 14:07:48 +0200

    fix base class

--- src/main/java/team/cqr/cqrepoured/client/model/geo/block/ModelNexusCore.java
+++ src/main/java/team/cqr/cqrepoured/client/model/geo/block/ModelNexusCore.java
@@ -1,28 +1,28 @@
 package team.cqr.cqrepoured.client.model.geo.block;
 
 import net.minecraft.resources.ResourceLocation;
-import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib.model.GeoModel;
 import team.cqr.cqrepoured.CQRMain;
 import team.cqr.cqrepoured.client.init.CQRAnimations;
 import team.cqr.cqrepoured.tileentity.TileEntityForceFieldNexus;
 
-public class ModelNexusCore extends AnimatedGeoModel<TileEntityForceFieldNexus>{
+public class ModelNexusCore extends GeoModel<TileEntityForceFieldNexus>{
 	
 	protected static final ResourceLocation MODEL_RESLOC = CQRMain.prefix("geo/block/force_field_nexus.geo.json");
 	protected static final ResourceLocation TEXTURE_RESLOC = CQRMain.prefix("textures/entity/force_field_nexus/force_field_nexus.png");
 
 	@Override
-	public ResourceLocation getAnimationFileLocation(TileEntityForceFieldNexus animatable) {
+	public ResourceLocation getAnimationResource(TileEntityForceFieldNexus animatable) {
 		return CQRAnimations.Block.NEXUS_CORE;
 	}
 
 	@Override
-	public ResourceLocation getModelLocation(TileEntityForceFieldNexus object) {
+	public ResourceLocation getModelResource(TileEntityForceFieldNexus object) {
 		return MODEL_RESLOC;
 	}
 
 	@Override
-	public ResourceLocation getTextureLocation(TileEntityForceFieldNexus object) {
+	public ResourceLocation getTextureResource(TileEntityForceFieldNexus object) {
 		return TEXTURE_RESLOC;
 	}
 
