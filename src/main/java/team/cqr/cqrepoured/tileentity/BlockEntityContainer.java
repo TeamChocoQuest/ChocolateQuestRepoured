70f1609c804b5369677b7006a0d79207b04f5f3d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-10 13:49:44 +0200

    update BlockEntityContainer

--- src/main/java/team/cqr/cqrepoured/tileentity/BlockEntityContainer.java
+++ src/main/java/team/cqr/cqrepoured/tileentity/BlockEntityContainer.java
@@ -1,26 +1,29 @@
 package team.cqr.cqrepoured.tileentity;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.core.Direction;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.wrapper.InvWrapper;
 import team.cqr.cqrepoured.inventory.InventoryBlockEntity;
 
-public class BlockEntityContainer extends BlockEntity {
+public abstract class BlockEntityContainer extends BaseContainerBlockEntity {
 
 	protected final InventoryBlockEntity inventory;
 
-	public BlockEntityContainer(BlockEntityType<?> type, int invSize) {
-		this(type, invSize, 64);
+	public BlockEntityContainer(BlockEntityType<?> type, int invSize, BlockPos pos, BlockState state) {
+		this(type, invSize, 64, pos, state);
 	}
 
-	public BlockEntityContainer(BlockEntityType<?> type, int invSize, int maxStackSize) {
-		super(type);
+	public BlockEntityContainer(BlockEntityType<?> type, int invSize, int maxStackSize, BlockPos pos, BlockState state) {
+		super(type, pos, state);
 		this.inventory = new InventoryBlockEntity(this, invSize, true) {
 			@Override
 			public int getMaxStackSize() {
@@ -28,23 +31,23 @@ public class BlockEntityContainer extends BlockEntity {
 			}
 		};
 	}
-
+	
 	@Override
-	public CompoundTag save(CompoundTag compound) {
-		super.save(compound);
+	public CompoundTag serializeNBT() {
+		CompoundTag compound = super.serializeNBT();
 		this.inventory.save(compound);
 		return compound;
 	}
-
+	
 	@Override
-	public void load(BlockState state, CompoundTag compound) {
-		super.load(state, compound);
-		this.inventory.load(compound);
+	public void deserializeNBT(CompoundTag nbt) {
+		super.deserializeNBT(nbt);
+		this.inventory.load(nbt);
 	}
-
+	
 	@Override
 	public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
-		if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+		if (cap == ForgeCapabilities.ITEM_HANDLER) {
 			return LazyOptional.of(() -> new InvWrapper(this.inventory)).cast();
 		}
 		return super.getCapability(cap, side);
@@ -52,6 +55,46 @@ public class BlockEntityContainer extends BlockEntity {
 
 	public InventoryBlockEntity getInventory() {
 		return this.inventory;
+	}
+
+	@Override
+	public int getContainerSize() {
+		return this.inventory.getContainerSize();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.inventory.isEmpty();
+	}
+
+	@Override
+	public ItemStack getItem(int pSlot) {
+		return this.inventory.getItem(pSlot);
+	}
+
+	@Override
+	public ItemStack removeItem(int pSlot, int pAmount) {
+		return this.inventory.removeItem(pSlot, pAmount);
+	}
+
+	@Override
+	public ItemStack removeItemNoUpdate(int pSlot) {
+		return this.inventory.removeItemNoUpdate(pSlot);
+	}
+
+	@Override
+	public void setItem(int pSlot, ItemStack pStack) {
+		this.inventory.setItem(pSlot, pStack);
+	}
+
+	@Override
+	public boolean stillValid(Player pPlayer) {
+		return this.inventory.stillValid(pPlayer);
+	}
+
+	@Override
+	public void clearContent() {
+		this.inventory.clearContent();
 	}
 
 }
