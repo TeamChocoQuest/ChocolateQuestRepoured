322c5e65d799cfc715d47c9f0dcb0511c59e1fb8
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-10 13:05:04 +0200

    update InventoryBlockEntity

--- src/main/java/team/cqr/cqrepoured/inventory/InventoryBlockEntity.java
+++ src/main/java/team/cqr/cqrepoured/inventory/InventoryBlockEntity.java
@@ -1,18 +1,19 @@
 package team.cqr.cqrepoured.inventory;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
-public class InventoryBlockEntity extends Inventory {
+public class InventoryBlockEntity extends SimpleContainer {
 
 	private final BlockEntity blockEntity;
 	private final boolean creativeOnly;
-
+	
 	public InventoryBlockEntity(BlockEntity blockEntity, int size, boolean creativeOnly) {
 		super(size);
 		this.blockEntity = blockEntity;
@@ -25,30 +26,33 @@ public class InventoryBlockEntity extends Inventory {
 		if (this.creativeOnly && !pPlayer.isCreative()) {
 			return false;
 		}
-		if (pPlayer.level.getBlockEntity(this.blockEntity.getBlockPos()) != this.blockEntity) {
+		if (pPlayer.level().getBlockEntity(this.blockEntity.getBlockPos()) != this.blockEntity) {
 			return false;
 		}
-		return this.blockEntity.getBlockPos().distSqr(pPlayer.position(), true) < 64.0D;
+		return this.blockEntity.getBlockPos().distSqr(pPlayer.blockPosition()) < 64.0D;
 	}
 
 	public CompoundTag save(CompoundTag compound) {
-		ItemStackHelper.saveAllItems(compound, this.items);
+		compound.put("container-content", this.createTag());
 		return compound;
 	}
 
 	public void load(CompoundTag compound) {
-		ItemStackHelper.loadAllItems(compound, this.items);
+		Tag tag = compound.get("container-content");
+		if (tag instanceof ListTag lt) {
+			this.fromTag(lt);
+		}
 	}
 
 	public void write(FriendlyByteBuf buf) {
 		for (int i = 0; i < this.getContainerSize(); i++) {
-			buf.writeNbt(this.items.get(i).save(new CompoundTag()));
+			buf.writeNbt(this.getItem(i).save(new CompoundTag()));
 		}
 	}
 
 	public void read(FriendlyByteBuf buf) {
 		for (int i = 0; i < this.getContainerSize(); i++) {
-			this.items.set(i, ItemStack.of(buf.readNbt()));
+			this.setItem(i, ItemStack.of(buf.readNbt()));
 		}
 	}
 
