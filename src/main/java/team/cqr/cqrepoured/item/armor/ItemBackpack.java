a5c780d50e23423dc4de31a763eb737ec296db39
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-10 12:49:46 +0200

    update ItemBackpack

--- src/main/java/team/cqr/cqrepoured/item/armor/ItemBackpack.java
+++ src/main/java/team/cqr/cqrepoured/item/armor/ItemBackpack.java
@@ -1,24 +1,18 @@
 package team.cqr.cqrepoured.item.armor;
 
-import java.awt.TextComponent;
 import java.util.List;
 
 import javax.annotation.Nullable;
 
-import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.Container;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -29,12 +23,9 @@ import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.network.NetworkHooks;
 import net.minecraftforge.registries.ForgeRegistries;
 import software.bernie.geckolib.animatable.GeoItem;
-import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager.ControllerRegistrar;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
-import software.bernie.geckolib.util.GeckoLibUtil;
 import team.cqr.cqrepoured.capability.itemhandler.item.CapabilityItemHandlerItemProvider;
-import team.cqr.cqrepoured.client.init.CQRArmorModels;
 import team.cqr.cqrepoured.client.render.armor.RenderArmorBackpack;
 import team.cqr.cqrepoured.init.CQRContainerTypes;
 import team.cqr.cqrepoured.item.ItemLore;
@@ -51,15 +42,15 @@ public class ItemBackpack extends CQRGeoArmorBase implements GeoItem {
 		{
 			if(handIn == InteractionHand.MAIN_HAND)
 			{
-				NetworkHooks.openGui((ServerPlayer) playerIn, new INamedContainerProvider() {
+				NetworkHooks.openScreen((ServerPlayer) playerIn, new MenuProvider() {
 					@Override
-					public TextComponent getDisplayName() {
-						return new TranslationTextComponent("backpack.container"); //#TODO name
+					public Component getDisplayName() {
+						return Component.translatable("backpack.container"); //#TODO name
 					}
 
 					@Nullable
 					@Override
-					public Container createMenu(int windowId, Inventory inventory, Player player) {
+					public AbstractContainerMenu createMenu(int windowId, Inventory inventory, Player player) {
 						return CQRContainerTypes.BACKPACK.get().create(windowId, inventory);
 					}
 				});
