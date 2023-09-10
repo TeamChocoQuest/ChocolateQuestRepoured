4eb1f8612d30d868fe539ebf33f8fe0bbf874f85
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-10 13:27:36 +0200

    update ITileEntitySyncable

--- src/main/java/team/cqr/cqrepoured/tileentity/ITileEntitySyncable.java
+++ src/main/java/team/cqr/cqrepoured/tileentity/ITileEntitySyncable.java
@@ -1,12 +1,16 @@
 package team.cqr.cqrepoured.tileentity;
 
-import net.minecraft.world.level.block.entity.TickingBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.state.BlockState;
 import team.cqr.cqrepoured.network.datasync.TileEntityDataManager;
 
-public interface ITileEntitySyncable extends TickingBlockEntity {
+public interface ITileEntitySyncable<T extends BlockEntity> extends BlockEntityTicker<T> {
 
 	@Override
-	default void tick() {
+	default void tick(Level pLevel, BlockPos pPos, BlockState pState, T pBlockEntity) {
 		this.getDataManager().checkIfDirtyAndSync();
 	}
 
