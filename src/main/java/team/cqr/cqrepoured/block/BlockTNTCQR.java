9d7c8cc7417388d22c1bec472791a3a7cacda4bd
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-24 14:52:47 +0200

    update BlockTNTCQR

--- src/main/java/team/cqr/cqrepoured/block/BlockTNTCQR.java
+++ src/main/java/team/cqr/cqrepoured/block/BlockTNTCQR.java
@@ -1,19 +1,18 @@
 package team.cqr.cqrepoured.block;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Explosion;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.TNTBlock;
-import net.minecraft.world.level.block.TntBlock;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.StateDefinition.Builder;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.TntBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition.Builder;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import team.cqr.cqrepoured.entity.misc.EntityTNTPrimedCQR;
 
 public class BlockTNTCQR extends TntBlock {
@@ -38,14 +37,14 @@ public class BlockTNTCQR extends TntBlock {
 	@Override
 	public void wasExploded(Level pLevel, BlockPos pPos, Explosion pExplosion) {
 		if (!pLevel.isClientSide) {
-			EntityTNTPrimedCQR entityTNT = new EntityTNTPrimedCQR(pLevel, pPos.getX() + 0.5D, pPos.getY(), pPos.getZ() + 0.5D, pExplosion.getSourceMob());
-			entityTNT.setFuse((short) (pLevel.random.nextInt(entityTNT.getLife() / 4) + entityTNT.getLife() / 8));
+			EntityTNTPrimedCQR entityTNT = new EntityTNTPrimedCQR(pLevel, pPos.getX() + 0.5D, pPos.getY(), pPos.getZ() + 0.5D, pExplosion.getIndirectSourceEntity());
+			entityTNT.setFuse((short) (pLevel.random.nextInt(entityTNT.getFuse() / 4) + entityTNT.getFuse() / 8));
 			pLevel.addFreshEntity(entityTNT);
 		}
 	}
 
 	@Override
-	public void catchFire(BlockState state, Level worldIn, BlockPos pos, Direction face, LivingEntity igniter) {
+	public void onCaughtFire(BlockState state, Level worldIn, BlockPos pos, Direction face, LivingEntity igniter) {
 		if (!worldIn.isClientSide) {
 			EntityTNTPrimedCQR entityTNT = new EntityTNTPrimedCQR(worldIn, pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D, igniter);
 			worldIn.addFreshEntity(entityTNT);
