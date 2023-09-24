51c3a3c7706e79618a68104d524ae1088d9c0a6a
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-24 14:46:33 +0200

    update TooltipEventHandler

--- src/main/java/team/cqr/cqrepoured/client/event/TooltipEventHandler.java
+++ src/main/java/team/cqr/cqrepoured/client/event/TooltipEventHandler.java
@@ -1,24 +1,23 @@
 package team.cqr.cqrepoured.client.event;
 
-import java.awt.TextComponent;
-
 import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import team.cqr.cqrepoured.CQRConstants;
 import team.cqr.cqrepoured.item.armor.ItemArmorCrown;
 
-@OnlyIn(Dist.CLIENT)
-//@EventBusSubscriber(modid = CQRConstants.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = CQRConstants.MODID, value = Dist.CLIENT)
 public class TooltipEventHandler {
 
 	@SubscribeEvent
 	public static void onItemTooltipEvent(ItemTooltipEvent event) {
 		ItemStack stack = event.getItemStack();
 		if (!stack.isEmpty() && ItemArmorCrown.hasCrown(stack)) {
-			event.getToolTip().add(0, new TextComponent(ChatFormatting.GOLD + "Crown attached"));
+			event.getToolTip().add(0, Component.translatable(ChatFormatting.GOLD + "Crown attached"));
 		}
 	}
 
