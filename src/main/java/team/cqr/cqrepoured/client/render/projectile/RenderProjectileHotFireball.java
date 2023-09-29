e7c635e05295f92bf745f18b56ff0c5bc9b87450
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-29 20:02:27 +0200

    update RenderProjectileHotFireball

--- src/main/java/team/cqr/cqrepoured/client/render/projectile/RenderProjectileHotFireball.java
+++ src/main/java/team/cqr/cqrepoured/client/render/projectile/RenderProjectileHotFireball.java
@@ -1,18 +1,19 @@
 package team.cqr.cqrepoured.client.render.projectile;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Blocks;
 import org.lwjgl.opengl.GL11;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.InventoryMenu;
-import net.minecraftforge.client.model.data.EmptyModelData;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.client.model.data.ModelData;
 import team.cqr.cqrepoured.entity.projectiles.ProjectileHotFireball;
 
 public class RenderProjectileHotFireball extends EntityRenderer<ProjectileHotFireball> {
@@ -34,7 +35,7 @@ public class RenderProjectileHotFireball extends EntityRenderer<ProjectileHotFir
 	@Override
 	public void render(ProjectileHotFireball entity, float entityYaw, float partialTicks, PoseStack matrixStack, MultiBufferSource buffer, int packedLight)
 	{
-		BlockRendererDispatcher blockrendererdispatcher = Minecraft.getInstance().getBlockRenderer();
+		BlockRenderDispatcher blockrendererdispatcher = Minecraft.getInstance().getBlockRenderer();
 		matrixStack.pushPose();
 		//GL11.glPushMatrix();
 		//GL11.glTranslated(x, y, z);
@@ -45,7 +46,7 @@ public class RenderProjectileHotFireball extends EntityRenderer<ProjectileHotFir
 		// GlStateManager.rotate(entity.ticksExisted * 7, 1.0F, 1.0F, 1.0F);
 		//GlStateManager.scale(0.5F, 0.5F, 0.5F);
 		
-		blockrendererdispatcher.renderBlock(Blocks.OBSIDIAN.defaultBlockState(), matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, EmptyModelData.INSTANCE);
+		blockrendererdispatcher.renderSingleBlock(Blocks.OBSIDIAN.defaultBlockState(), matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, ModelData.EMPTY, null);
 		// GlStateManager.translate(0.25F, 0.0F, 0.55F);
 		GL11.glPopMatrix();
 	}
