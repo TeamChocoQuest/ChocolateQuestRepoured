e61b84f55b90fef6f9e14e71e7f01295d2c80f73
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-09-29 20:00:59 +0200

    update RenderProjectileThrownBlock

--- src/main/java/team/cqr/cqrepoured/client/render/projectile/RenderProjectileThrownBlock.java
+++ src/main/java/team/cqr/cqrepoured/client/render/projectile/RenderProjectileThrownBlock.java
@@ -3,14 +3,14 @@ package team.cqr.cqrepoured.client.render.projectile;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.InventoryMenu;
-import net.minecraftforge.client.model.data.EmptyModelData;
+import net.minecraftforge.client.model.data.ModelData;
 import team.cqr.cqrepoured.entity.projectiles.ProjectileThrownBlock;
 
 public class RenderProjectileThrownBlock extends EntityRenderer<ProjectileThrownBlock> {
@@ -27,7 +27,7 @@ public class RenderProjectileThrownBlock extends EntityRenderer<ProjectileThrown
 
 	@Override
 	public void render(ProjectileThrownBlock entity, float entityYaw, float partialTicks, PoseStack matrixStack, MultiBufferSource buffer, int packedLight) {
-		BlockRendererDispatcher blockrendererdispatcher = Minecraft.getInstance().getBlockRenderer();
+		BlockRenderDispatcher blockrendererdispatcher = Minecraft.getInstance().getBlockRenderer();
 		//GL11.glPushMatrix();
 		//GL11.glTranslated(x, y, z);
 		matrixStack.pushPose();
@@ -37,7 +37,7 @@ public class RenderProjectileThrownBlock extends EntityRenderer<ProjectileThrown
 		//GlStateManager.translate(-0.35F, 0F, 0.35F);
 		// GlStateManager.rotate(entity.ticksExisted * 7, 1.0F, 1.0F, 1.0F);
 		//GlStateManager.scale(0.7F, 0.7F, 0.7F);
-		blockrendererdispatcher.renderBlock(entity.getBlock(), matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, EmptyModelData.INSTANCE);
+		blockrendererdispatcher.renderSingleBlock(entity.getBlock(), matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, ModelData.EMPTY, null);
 		// GlStateManager.translate(0.25F, 0.0F, 0.55F);
 		//GL11.glPopMatrix();
 		matrixStack.popPose();
