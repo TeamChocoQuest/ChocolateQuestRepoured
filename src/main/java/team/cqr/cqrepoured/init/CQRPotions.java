46ceb16212a697f6be042213d4f2b92a28b5bf49
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-10-07 16:13:50 +0200

    register potions

--- src/main/java/team/cqr/cqrepoured/init/CQRPotions.java
+++ src/main/java/team/cqr/cqrepoured/init/CQRPotions.java
@@ -1,33 +1,29 @@
 package team.cqr.cqrepoured.init;
 
-import static team.cqr.cqrepoured.util.InjectionUtil.Null;
+import java.util.function.Supplier;
 
 import net.minecraft.world.effect.MobEffect;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.registries.IForgeRegistry;
-import net.minecraftforge.registries.ObjectHolder;
-import team.cqr.cqrepoured.CQRMain;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import team.cqr.cqrepoured.CQRConstants;
+import team.cqr.cqrepoured.potion.ElectrocutionMobEffect;
 import team.cqr.cqrepoured.potion.PotionTwohanded;
 
-@ObjectHolder(CQRConstants.MODID)
-@EventBusSubscriber
 public class CQRPotions {
+	
+	public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, CQRConstants.MODID);
 
-	public static final MobEffect TWOHANDED = Null();
-
-	@EventBusSubscriber(modid = CQRConstants.MODID, bus = Bus.MOD)
-	public static class EventHandler {
-
-		@SubscribeEvent
-		public static void onEvent(final RegistryEvent.Register<MobEffect> event) {
-			final IForgeRegistry<MobEffect> registry = event.getRegistry();
-
-			registry.registerAll(new PotionTwohanded());
-		}
-
+	public static final RegistryObject<MobEffect> TWOHANDED = register("twohanded", PotionTwohanded::new);
+	public static final RegistryObject<MobEffect> ELECTROCUTION = register("electrocution", ElectrocutionMobEffect::new); 
+			
+	public static <T extends MobEffect> RegistryObject<MobEffect> register(String name, Supplier<T> supplier) {
+		return EFFECTS.register(name, supplier);
+	}
+	
+	public static void registerEffects() {
+		EFFECTS.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 
 }
