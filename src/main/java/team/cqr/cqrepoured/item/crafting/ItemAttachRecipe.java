8216fe4c7088ddf845bc334a817c22ef10b16f9f
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-10-07 12:44:53 +0200

    "finish" ItemAttachRecipe
    => Refactor into base class to make detach recipe easier to implement

--- src/main/java/team/cqr/cqrepoured/item/crafting/ItemAttachRecipe.java
+++ src/main/java/team/cqr/cqrepoured/item/crafting/ItemAttachRecipe.java
@@ -2,6 +2,7 @@ package team.cqr.cqrepoured.item.crafting;
 
 import java.util.Optional;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import com.google.gson.JsonObject;
@@ -15,18 +16,22 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.Container;
+import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.registries.ForgeRegistries;
+import team.cqr.cqrepoured.capability.armor.attachment.ICapabilityArmorAttachment;
+import team.cqr.cqrepoured.init.CQRCapabilities;
 import team.cqr.cqrepoured.serialization.CodecUtil;
 
-public class ItemAttachRecipe implements Recipe<Container> {
+public class ItemAttachRecipe implements Recipe<CraftingContainer> {
 	
 	private ResourceLocation id;
 	private Item attachment;
@@ -46,15 +51,73 @@ public class ItemAttachRecipe implements Recipe<Container> {
 		this.slot = slot;
 		this.allowedBaseItems = allowedBases;
 	}
-
-	@Override
-	public boolean matches(Container pContainer, Level pLevel) {
-		return false;
+	
+	@Nullable
+	protected Tuple<ItemStack, ItemStack> getTopAndBottom(CraftingContainer pContainer) {
+		int topY = 0;
+		ItemStack top = null;
+		ItemStack bottom = null;
+		int slot = 0;
+		for (int y = 0; y < pContainer.getHeight(); y++) {
+			for (int x = 0; x < pContainer.getWidth(); x++) {
+				ItemStack item = pContainer.getItem(slot);
+				@NotNull LazyOptional<ICapabilityArmorAttachment> lazyOpt = item.getCapability(CQRCapabilities.ARMOR_ATTACHMENT);
+				if (top == null) {
+					// If this has an attached item => continue, we can't use it!
+					if (lazyOpt.isPresent()) {
+						ICapabilityArmorAttachment cap = lazyOpt.resolve().get();
+						if (cap != null && (cap.getAttachment() != null || !cap.getAttachment().isEmpty())) {
+							continue;
+						}
+					}
+					if (item.is(this.attachment)) {
+						top = item;
+					}
+				} else if (topY < y && bottom == null) {
+					if (item.getItem().getEquipmentSlot(item).equals(this.slot)) {
+						if (item.is(this.allowedBaseItems)) {
+							// Check if that item has an attachment
+							// => Check for capability and it's content
+							if (lazyOpt.isPresent()) {
+								ICapabilityArmorAttachment cap = lazyOpt.resolve().get();
+								if (cap != null && (cap.getAttachment() == null || cap.getAttachment().isEmpty())) {
+									bottom = item;
+								}
+							}
+						}
+					}
+				}
+				if (top != null && bottom != null) {
+					return new Tuple<>(top, bottom);
+				}
+				slot++;
+			}
+		}
+		return null;
 	}
 
 	@Override
-	public ItemStack assemble(Container pContainer, RegistryAccess pRegistryAccess) {
-		return null;
+	public boolean matches(CraftingContainer pContainer, Level pLevel) {
+		return this.getTopAndBottom(pContainer) != null;
+	}
+
+	@Override
+	public ItemStack assemble(CraftingContainer pContainer, RegistryAccess pRegistryAccess) {
+		ItemStack result = ItemStack.EMPTY;
+		Tuple<ItemStack, ItemStack> topAndBottom = this.getTopAndBottom(pContainer);
+		if (topAndBottom != null) {
+			result = topAndBottom.getB().copy();
+			// Attach top
+			@NotNull LazyOptional<ICapabilityArmorAttachment> lazyOpt = result.getCapability(CQRCapabilities.ARMOR_ATTACHMENT);
+			if (lazyOpt.isPresent()) {
+				ICapabilityArmorAttachment cap = lazyOpt.resolve().get();
+				if (cap != null && (cap.getAttachment() == null || cap.getAttachment().isEmpty())) {
+					cap.setAttachment(topAndBottom.getA().copy());
+				}
+			}
+
+		}
+		return result;
 	}
 
 	@Override
