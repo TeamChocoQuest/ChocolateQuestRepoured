b83d1f02ea44a321d6842e19e24cd536ae1dc609
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-10-07 13:26:02 +0200

    mostly update PathGroundNavigation

--- src/main/java/team/cqr/cqrepoured/entity/pathfinding/PathNavigateGroundCQR.java
+++ src/main/java/team/cqr/cqrepoured/entity/pathfinding/PathNavigateGroundCQR.java
@@ -6,39 +6,40 @@ import javax.annotation.Nullable;
 
 import com.google.common.collect.Sets;
 
-import net.minecraft.world.level.block.AbstractRailBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.PathNavigationRegion;
+import net.minecraft.world.level.block.BaseRailBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.block.FenceBlock;
 import net.minecraft.world.level.block.FenceGateBlock;
 import net.minecraft.world.level.block.WallBlock;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.entity.MobEntity;
-import net.minecraft.pathfinding.GroundPathNavigator;
-import net.minecraft.pathfinding.Path;
-import net.minecraft.pathfinding.PathFinder;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
-import net.minecraft.pathfinding.PathPoint;
-import net.minecraft.pathfinding.PathType;
-import net.minecraft.pathfinding.WalkNodeProcessor;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.core.Direction;
-import net.minecraft.core.BlockPos;
-import net.minecraft.util.Mth;
+import net.minecraft.world.level.pathfinder.Node;
+import net.minecraft.world.level.pathfinder.Path;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.level.pathfinder.PathFinder;
+import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
 import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.Region;
 import team.cqr.cqrepoured.entity.ai.EntityAIOpenCloseDoor;
 import team.cqr.cqrepoured.world.ChunkCacheCQR;
 
 /**
  * Copied from {@link GroundPathNavigator}
  */
-public class PathNavigateGroundCQR extends GroundPathNavigator {
+public class PathNavigateGroundCQR extends GroundPathNavigation {
 
 	private int ticksAtLastPos;
 	private Vec3 lastPosCheck = Vec3.ZERO;
@@ -52,88 +53,88 @@ public class PathNavigateGroundCQR extends GroundPathNavigator {
 	private PathFinder pathFinder;
 	protected float maxPathSearchRange = 256;
 
-	public PathNavigateGroundCQR(MobEntity entitylivingIn, Level worldIn) {
+	public PathNavigateGroundCQR(Mob entitylivingIn, Level worldIn) {
 		super(entitylivingIn, worldIn);
 	}
 
 	@Override
 	protected PathFinder createPathFinder(int pMaxVisitedNodes) {
-		this.nodeEvaluator = new WalkNodeProcessor() {
+		this.nodeEvaluator = new WalkNodeEvaluator() {
 
 			@Override
-			public BlockPathTypes getBlockPathTypes(BlockGetter level, int x, int y, int z, int xSize, int ySize, int zSize, boolean canOpenDoorsIn, boolean canEnterDoorsIn, EnumSet<BlockPathTypes> pNodeTypeEnum, BlockPathTypes pNodeType, BlockPos pPos) {
-				for (int i = 0; i < xSize; ++i) {
-					for (int j = 0; j < ySize; ++j) {
-						for (int k = 0; k < zSize; ++k) {
-							int l = i + x;
-							int i1 = j + y;
-							int j1 = k + z;
+			public BlockPathTypes getBlockPathTypes(BlockGetter pLevel, int pXOffset, int pYOffset, int pZOffset, EnumSet<BlockPathTypes> pOutput, BlockPathTypes p_265458_, BlockPos pPos) {
+				for (int i = 0; i < this.entityWidth; ++i) {
+					for (int j = 0; j < this.entityHeight; ++j) {
+						for (int k = 0; k < this.entityDepth; ++k) {
+							int l = i + pXOffset;
+							int i1 = j + pYOffset;
+							int j1 = k + pZOffset;
 							BlockPathTypes pathnodetype = this.getBlockPathType(level, l, i1, j1);
 
-							if (pathnodetype == BlockPathTypes.DOOR_WOOD_CLOSED && canOpenDoorsIn && canEnterDoorsIn) {
+							if (pathnodetype == BlockPathTypes.DOOR_WOOD_CLOSED && this.canOpenDoors() && this.canPassDoors()) {
 								pathnodetype = BlockPathTypes.WALKABLE;
 							}
 
 							// TODO better method for calculating the facing from which the door will be entered
-							if (pathnodetype == BlockPathTypes.DOOR_IRON_CLOSED && canOpenDoorsIn && canEnterDoorsIn
+							if (pathnodetype == BlockPathTypes.DOOR_IRON_CLOSED && this.canOpenDoors() && this.canPassDoors()
 									&& EntityAIOpenCloseDoor.canMoveThroughDoor(level, new BlockPos(l, i1, j1), Direction.fromNormal/*Correct replacement???*/(l - pPos.getX(), i1 - pPos.getY(), j1 - pPos.getZ()).getOpposite(), true)) {
 								pathnodetype = BlockPathTypes.WALKABLE;
 							}
 
-							if (pathnodetype == BlockPathTypes.DOOR_OPEN && !canEnterDoorsIn) {
+							if (pathnodetype == BlockPathTypes.DOOR_OPEN && !this.canPassDoors()) {
 								pathnodetype = BlockPathTypes.BLOCKED;
 							}
 
-							if (pathnodetype == BlockPathTypes.RAIL && !(level.getBlockState(pPos).getBlock() instanceof AbstractRailBlock) && !(level.getBlockState(pPos.below()).getBlock() instanceof AbstractRailBlock)) {
+							if (pathnodetype == BlockPathTypes.RAIL && !(level.getBlockState(pPos).getBlock() instanceof BaseRailBlock) && !(level.getBlockState(pPos.below()).getBlock() instanceof BaseRailBlock)) {
 								pathnodetype = BlockPathTypes.FENCE;
 							}
 
 							if (i == 0 && j == 0 && k == 0) {
-								pNodeType = pathnodetype;
+								p_265458_ = pathnodetype;
 							}
 
-							pNodeTypeEnum.add(pathnodetype);
+							pOutput.add(pathnodetype);
 						}
 					}
 				}
 
-				return pNodeType;
+				return p_265458_;
 			}
 			
 			@Override
 			public BlockPathTypes getBlockPathType(BlockGetter pLevel, int pX, int pY, int pZ) {
 				BlockPos blockpos = new BlockPos(pX, pY, pZ);
 				BlockState iblockstate = pLevel.getBlockState(blockpos);
+				FluidState fluidState = pLevel.getFluidState(blockpos);
 				Block block = iblockstate.getBlock();
-				Material material = iblockstate.getMaterial();
 
-				BlockPathTypes type = block.getAiPathNodeType(iblockstate, pLevel, blockpos, this.mob);
+				BlockPathTypes type = block.getBlockPathType(iblockstate, pLevel, blockpos, this.mob);
 				if (type != null) {
 					return type;
 				}
 
-				if (material == Material.AIR) {
+				if (iblockstate.isAir()) {
 					return BlockPathTypes.OPEN;
-				} else if (!block.is(BlockTags.TRAPDOORS) && !block.is(Blocks.LILY_PAD)) {
+				} else if (!iblockstate.is(BlockTags.TRAPDOORS) && !iblockstate.is(Blocks.LILY_PAD)) {
 					if (block == Blocks.FIRE) {
 						return BlockPathTypes.DAMAGE_FIRE;
 					} else if (block == Blocks.CACTUS) {
-						return BlockPathTypes.DAMAGE_CACTUS;
-					} else if (block instanceof DoorBlock && material == Material.WOOD && !iblockstate/*.getActualState(pLevel, blockpos)*/.getValue(DoorBlock.OPEN)) {
+						return BlockPathTypes.DAMAGE_CAUTIOUS;
+					} else if (block instanceof DoorBlock && DoorBlock.isWoodenDoor(iblockstate) && !iblockstate/*.getActualState(pLevel, blockpos)*/.getValue(DoorBlock.OPEN)) {
 						return BlockPathTypes.DOOR_WOOD_CLOSED;
-					} else if (block instanceof DoorBlock && material == Material.METAL && !iblockstate/*.getActualState(pLevel, blockpos)*/.getValue(DoorBlock.OPEN)) {
+					} else if (block instanceof DoorBlock && !DoorBlock.isWoodenDoor(iblockstate) && !iblockstate/*.getActualState(pLevel, blockpos)*/.getValue(DoorBlock.OPEN)) {
 						return BlockPathTypes.DOOR_IRON_CLOSED;
 					} else if (block instanceof DoorBlock && iblockstate/*.getActualState(pLevel, blockpos)*/.getValue(DoorBlock.OPEN)) {
 						return BlockPathTypes.DOOR_OPEN;
-					} else if (block instanceof AbstractRailBlock) {
+					} else if (block instanceof BaseRailBlock) {
 						return BlockPathTypes.RAIL;
 					} else if (!(block instanceof FenceBlock) && !(block instanceof WallBlock) && (!(block instanceof FenceGateBlock) || iblockstate.getValue(FenceGateBlock.OPEN).booleanValue())) {
-						if (material == Material.WATER) {
+						if (fluidState != null && fluidState.is(FluidTags.WATER)) {
 							return BlockPathTypes.WATER;
-						} else if (material == Material.LAVA) {
+						} else if (fluidState != null && fluidState.is(FluidTags.LAVA)) {
 							return BlockPathTypes.LAVA;
 						} else {
-							return iblockstate.isPathfindable(pLevel, blockpos, PathType.LAND) ? BlockPathTypes.OPEN : BlockPathTypes.BLOCKED;
+							return iblockstate.isPathfindable(pLevel, blockpos, PathComputationType.LAND) ? BlockPathTypes.OPEN : BlockPathTypes.BLOCKED;
 						}
 					} else {
 						return BlockPathTypes.FENCE;
@@ -180,15 +181,15 @@ public class PathNavigateGroundCQR extends GroundPathNavigator {
 	}
 
 	private boolean hasMount() {
-		return this.mob.getVehicle() instanceof MobEntity;
+		return this.mob.getVehicle() instanceof Mob;
 	}
 	
 	@Override
 	public Path createPath(BlockPos pos, int pAccuracy) {
-		if (this.level.getBlockState(pos).getMaterial() == Material.AIR) {
+		if (this.level.getBlockState(pos).isAir()) {
 			BlockPos blockpos;
 
-			for (blockpos = pos.below(); blockpos.getY() > 0 && this.level.getBlockState(blockpos).getMaterial() == Material.AIR; blockpos = blockpos.below()) {
+			for (blockpos = pos.below(); blockpos.getY() > 0 && this.level.getBlockState(blockpos).isAir(); blockpos = blockpos.below()) {
 
 			}
 
@@ -196,19 +197,19 @@ public class PathNavigateGroundCQR extends GroundPathNavigator {
 				return this.getPathToPosCQR(blockpos.above());
 			}
 
-			while (blockpos.getY() < this.level.getHeight() && this.level.getBlockState(blockpos).getMaterial() == Material.AIR) {
+			while (blockpos.getY() < this.level.getHeight() && this.level.getBlockState(blockpos).isAir()) {
 				blockpos = blockpos.above();
 			}
 
 			pos = blockpos;
 		}
 
-		if (!this.level.getBlockState(pos).getMaterial().isSolid()) {
+		if (!this.level.getBlockState(pos).isSolid()) {
 			return this.getPathToPosCQR(pos);
 		} else {
 			BlockPos blockpos1;
 
-			for (blockpos1 = pos.above(); blockpos1.getY() < this.level.getHeight() && this.level.getBlockState(blockpos1).getMaterial().isSolid(); blockpos1 = blockpos1.above()) {
+			for (blockpos1 = pos.above(); blockpos1.getY() < this.level.getHeight() && this.level.getBlockState(blockpos1).isSolid(); blockpos1 = blockpos1.above()) {
 
 			}
 
@@ -231,8 +232,8 @@ public class PathNavigateGroundCQR extends GroundPathNavigator {
 
 			this.level.getProfiler().push("pathfind");
 			BlockPos entityPos =this.hasMount() ? this.getMount().blockPosition() : this.mob.blockPosition();
-			Region chunkcache = new ChunkCacheCQR(this.level, entityPos, pos, entityPos, 32, false);
-			MobEntity mob = (this.hasMount() ? this.getMount() : this.mob);
+			PathNavigationRegion chunkcache = new ChunkCacheCQR(this.level, entityPos, pos, entityPos, 32, false);
+			Mob mob = (this.hasMount() ? this.getMount() : this.mob);
 			Path path = this.pathFinder.findPath(chunkcache, mob, Sets.newHashSet(pos), Mth.ceil(distance + 32.0F), (int) (mob.getBbWidth() / 2), 1.0F);
 			this.level.getProfiler().pop();
 			return path;
@@ -264,7 +265,7 @@ public class PathNavigateGroundCQR extends GroundPathNavigator {
 				this.targetPos = null;
 				return false;
 			} else {
-				PathPoint finalPathPoint = pathentityIn.getEndNode();
+				Node finalPathPoint = pathentityIn.getEndNode();
 				this.targetPos = new BlockPos(finalPathPoint.x, finalPathPoint.y, finalPathPoint.z);
 				this.speedModifier = speedIn;
 				this.ticksAtLastPos = this.tick;
@@ -295,7 +296,7 @@ public class PathNavigateGroundCQR extends GroundPathNavigator {
 		int currentNodeIndex = (this.path.getNextNodeIndex() - 1);
 		currentNodeIndex = currentNodeIndex > 0 ? currentNodeIndex : 0;
 		if (this.path != null && !this.path.isDone() && currentNodeIndex < this.path.getNodeCount()) {
-			PathPoint currentNode = this.path.getNode(currentNodeIndex);
+			Node currentNode = this.path.getNode(currentNodeIndex);
 			Vec3 vec3d = new Vec3(currentNode.x, currentNode.y, currentNode.z);
 
 			if (!vec3d.equals(this.timeoutCachedNode)) {
@@ -322,9 +323,9 @@ public class PathNavigateGroundCQR extends GroundPathNavigator {
 	}
 
 	@Nullable
-	private MobEntity getMount() {
+	private Mob getMount() {
 		try {
-			return (MobEntity) this.mob.getVehicle();
+			return (Mob) this.mob.getVehicle();
 		} catch (NullPointerException npe) {
 			return null;
 		}
