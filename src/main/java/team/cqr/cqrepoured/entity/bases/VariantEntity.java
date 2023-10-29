3b1cc86778f9b810ffcc2fcff1d1b3affe045469
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-10-29 13:54:23 +0100

    add base class for entities with variants => uses datapack stuff

---
+++ src/main/java/team/cqr/cqrepoured/entity/bases/VariantEntity.java
@@ -0,0 +1,213 @@
+package team.cqr.cqrepoured.entity.bases;
+
+import java.util.Map;
+import java.util.Optional;
+
+import com.mojang.serialization.DataResult;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.VariantHolder;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.living.MobSpawnEvent.FinalizeSpawn;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import team.cqr.cqrepoured.CQRConstants;
+import team.cqr.cqrepoured.entity.profile.EntityProfile;
+import team.cqr.cqrepoured.entity.profile.variant.AssetEntry;
+import team.cqr.cqrepoured.entity.profile.variant.AttributeEntry;
+import team.cqr.cqrepoured.entity.profile.variant.DamageCap;
+import team.cqr.cqrepoured.entity.profile.variant.DamageEntry;
+import team.cqr.cqrepoured.entity.profile.variant.EntityVariant;
+import team.cqr.cqrepoured.entity.profile.variant.SizeEntry;
+import team.cqr.cqrepoured.init.CQRDatapackLoaders;
+import team.cqr.cqrepoured.util.WeakReferenceLazyLoadField;
+
+public class VariantEntity extends Monster implements VariantHolder<EntityVariant> {
+	
+	/*
+	 * Variants can change the following things:
+	 *  - size
+	 *  - damage (weaknesses and resistances)
+	 *  - attributes
+	 *  - model, texture, animations
+	 */
+	
+	// Forge crap
+	@EventBusSubscriber(modid = CQRConstants.MODID, bus = Bus.FORGE)
+	static class EventListener {
+		
+		@SubscribeEvent
+		public static void onFinalizeSpawn(FinalizeSpawn event) {
+			if (event.getEntity() instanceof VariantEntity ve) {
+				EntityType<?> type = ve.getType();
+				Optional<EntityProfile> profile = CQRDatapackLoaders.getProfile(type);
+				if (profile.isPresent()) {
+					EntityVariant variant = profile.get().getRandomVariant(ve.getRandom());
+					if (variant != null) {
+						ve.setVariant(variant);
+						
+						if (variant.hasAssetVariants()) {
+							ve.assetEntry = variant.getRandomAssetIndex(ve.getRandom());
+						}
+						
+						// And now: Attributes
+						for (AttributeEntry entry : variant.attributes()) {
+							Attribute attribute = entry.attribute();
+							double value = entry.value();
+							
+							if (ve.getAttribute(attribute) == null) {
+								// TODO: Warning
+							} else {
+								ve.getAttribute(attribute).setBaseValue(value);
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		@SuppressWarnings("deprecation")
+		public static void onEntitySize(EntityEvent.Size event) {
+			Entity entity = event.getEntity();
+			if (entity != null && entity instanceof VariantEntity ve) {
+				if (ve.getVariant() == null || ve.getVariant().size() == null) {
+					return;
+				}
+				SizeEntry sizeEntry = ve.getVariant().size();
+				switch(event.getPose()) {
+				case CROUCHING:
+					event.setNewSize(EntityDimensions.scalable(sizeEntry.width(), sizeEntry.height()).scale(ve.getScale()).scale(1F, 0.75F), true);
+					break;
+				case SITTING:
+					event.setNewSize(EntityDimensions.scalable(sizeEntry.width(), sizeEntry.height()).scale(ve.getScale()).scale(1F, 0.66F), true);
+					break;
+				case SLEEPING:
+					event.setNewSize(EntityDimensions.scalable(sizeEntry.width(), sizeEntry.height()).scale(ve.getScale()).scale(1F, 0.5F), true);
+					break;
+				default:
+					event.setNewSize(EntityDimensions.scalable(sizeEntry.width(), sizeEntry.height()).scale(ve.getScale()), true);
+					break;
+				}
+				
+			}
+		}
+		
+	}
+	
+	private EntityVariant variant;
+	private int assetEntry = -1;
+	private WeakReferenceLazyLoadField<AssetEntry> assetEntryCache = new WeakReferenceLazyLoadField<>(this::loadAssetEntryLazily);
+	
+	private AssetEntry loadAssetEntryLazily() {
+		if (this.assetEntry < 0) {
+			return null;
+		}
+		if (this.getVariant() == null || !this.getVariant().hasAssetVariants()) {
+			return null;
+		}
+		AssetEntry result = this.getVariant().getAssetAt(this.assetEntry);
+		// Out of list
+		if (result == null) {
+			this.assetEntry = this.getVariant().getRandomAssetIndex(this.getRandom());
+			result = this.getVariant().getAssetAt(this.assetEntry);
+		}
+		return result;
+	}
+	
+	@Override
+	public boolean hurt(DamageSource pSource, float pAmount) {
+		if (this.getVariant() == null) {
+			return super.hurt(pSource, pAmount);
+		}
+		// Otherwise, we'll need to intercept
+		
+		DamageEntry damageConfig = this.getVariant().damageConfig();
+		// First: damage multipliers
+		if (pSource.is(DamageTypeTags.IS_FIRE) && damageConfig.fireImmune()) {
+			return false;
+		}
+		// TODO: This is not ideal as it will loop through all multipliers
+		for (Map.Entry<ResourceLocation, Float> entry : damageConfig.damageTypeMultipliers().entrySet()) {
+			if (pSource.typeHolder().is(entry.getKey())) {
+				pAmount *= entry.getValue();
+			}
+		}
+		
+		// Second: min damage
+		if (pAmount < damageConfig.minDamage()) {
+			return false;
+		}
+		
+		// Third: damage cap
+		if (damageConfig.damageCap().isPresent()) {
+			DamageCap cap = damageConfig.damageCap().get();
+			pAmount = cap.capDamage(pAmount, this::getMaxHealth);
+		}
+		
+		return super.hurt(pSource, pAmount);
+	}
+	
+	public Optional<AssetEntry> getClientOverrides() {
+		if (this.assetEntry >= 0 && this.getVariant() != null && this.assetEntryCache != null) {
+			return Optional.ofNullable(this.assetEntryCache.get());
+		}
+		return Optional.empty();
+	}
+
+	public VariantEntity(EntityType<? extends VariantEntity> pEntityType, Level pLevel) {
+		super(pEntityType, pLevel);
+	}
+	
+	@Override
+	public void addAdditionalSaveData(CompoundTag pCompound) {
+		super.addAdditionalSaveData(pCompound);
+		
+		if (this.variant != null) {
+			DataResult<Tag> dataResult = EntityVariant.CODEC.encodeStart(NbtOps.INSTANCE, this.getVariant());
+			Optional<Tag> optResult = dataResult.result();
+			if (optResult.isPresent()) {
+				pCompound.put(CQRConstants.NBT.KEY_ENTITY_VARIANT, optResult.get());
+			} 
+		}
+	}
+	
+	@Override
+	public void readAdditionalSaveData(CompoundTag pCompound) {
+		super.readAdditionalSaveData(pCompound);
+		
+		if (pCompound.contains(CQRConstants.NBT.KEY_ENTITY_VARIANT)) {
+			Tag nbtTag = pCompound.get(CQRConstants.NBT.KEY_ENTITY_VARIANT);
+			DataResult<EntityVariant> dataResult = EntityVariant.CODEC.parse(NbtOps.INSTANCE, nbtTag);
+			Optional<EntityVariant> optResult = dataResult.result();
+			if (!optResult.isEmpty()) {
+				this.setVariant(optResult.get());
+			}
+		}
+		if (pCompound.contains(CQRConstants.NBT.KEY_ENTITY_VARIANT_ASSETS, Tag.TAG_INT)) {
+			
+		}
+	}
+	
+	@Override
+	public void setVariant(EntityVariant pVariant) {
+		this.variant = pVariant;
+	}
+
+	@Override
+	public EntityVariant getVariant() {
+		return this.variant;
+	}
+	
+}
