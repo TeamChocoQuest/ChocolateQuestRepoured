7ca98d25dc98c7a270d21b1adbf91280c5dde5a0
Meldexun <27779321+Meldexun@users.noreply.github.com>
2024-03-08 17:15:34 +0100

    Clean up PlacementSettings

--- projects/generation/src/main/java/team/cqr/cqrepoured/generation/world/level/levelgen/structure/placement/PlacementSettings.java
+++ projects/generation/src/main/java/team/cqr/cqrepoured/generation/world/level/levelgen/structure/placement/PlacementSettings.java
@@ -15,14 +15,15 @@ import net.minecraft.world.level.levelgen.structure.Structure.GenerationContext;
 import team.cqr.cqrepoured.generation.world.level.levelgen.structure.DungeonDataManager;
 import team.cqr.cqrepoured.generation.world.level.levelgen.structure.WorldDungeonGenerator;
 
-public record PlacementSettings(double chance, double rarityFactor, List<ResourceLocation> dungeonDependencies, int spawnLimit, PositionValidator positionValidator, List<ResourceLocation> structuresPreventingGeneration, int structureCheckRadius, PositionFinder positionFinder) {
+public record PlacementSettings(double chance, double rarityFactor, int spawnLimit, List<ResourceLocation> dungeonDependencies,
+		PositionValidator positionValidator, List<ResourceLocation> structuresPreventingGeneration, int structureCheckRadius, PositionFinder positionFinder) {
 
 	public static final Codec<PlacementSettings> CODEC = RecordCodecBuilder.create(instance -> {
 		return instance.group(
 				Codec.DOUBLE.fieldOf("chance").forGetter(PlacementSettings::chance),
 				Codec.DOUBLE.fieldOf("rarity_factor").forGetter(PlacementSettings::rarityFactor),
-				Codec.list(ResourceLocation.CODEC).fieldOf("dungeon_dependencies").forGetter(PlacementSettings::dungeonDependencies),
 				Codec.INT.fieldOf("spawn_limit").forGetter(PlacementSettings::spawnLimit),
+				Codec.list(ResourceLocation.CODEC).fieldOf("dungeon_dependencies").forGetter(PlacementSettings::dungeonDependencies),
 				PositionValidator.CODEC.fieldOf("position_validator").forGetter(PlacementSettings::positionValidator),
 				Codec.list(ResourceLocation.CODEC).fieldOf("structures_preventing_generation").forGetter(PlacementSettings::structuresPreventingGeneration),
 				Codec.INT.fieldOf("structure_check_radius").forGetter(PlacementSettings::structureCheckRadius),
