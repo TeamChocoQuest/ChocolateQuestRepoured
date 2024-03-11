c16bd2d6dead19e793589ef3d8e6e1b8845ec7ab
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-03-11 19:49:00 +0100

    use DatapackRegistry

--- projects/faction/src/main/java/team/cqr/cqrepoured/faction/init/FactionDatapackLoaders.java
+++ projects/faction/src/main/java/team/cqr/cqrepoured/faction/init/FactionDatapackLoaders.java
@@ -4,56 +4,62 @@ import java.util.Optional;
 
 import javax.annotation.Nullable;
 
-import commoble.databuddy.data.CodecJsonDataManager;
+import de.dertoaster.multihitboxlib.api.DatapackRegistry;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DataPackRegistryEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 import team.cqr.cqrepoured.common.CQRepoured;
-import team.cqr.cqrepoured.common.datapack.CQRCodecJsonDataManager;
 import team.cqr.cqrepoured.common.datapack.DatapackLoaderHelper;
-import team.cqr.cqrepoured.common.services.CQRServices;
 import team.cqr.cqrepoured.faction.EntityFactionInformation;
 import team.cqr.cqrepoured.faction.Faction;
-import team.cqr.cqrepoured.faction.network.datapacksynch.packet.SPacketSyncFaction;
-import team.cqr.cqrepoured.faction.network.datapacksynch.packet.SPacketSyncTextureSet;
 import team.cqr.cqrepoured.faction.textureset.TextureSetNew;
 
-@Mod.EventBusSubscriber(modid = CQRepoured.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber(modid = CQRepoured.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class FactionDatapackLoaders implements DatapackLoaderHelper {
 	
 	// TODO: Create reload listener that forces all holders to reload
 	
-	public static final CQRCodecJsonDataManager<TextureSetNew> TEXTURE_SETS = new CQRCodecJsonDataManager<>("cqr/texture_sets", TextureSetNew.CODEC);
-	public static final CQRCodecJsonDataManager<Faction> FACTIONS = new CQRCodecJsonDataManager<>("cqr/factions", Faction.CODEC);
-	public static final CQRCodecJsonDataManager<EntityFactionInformation> ENTITY_FACTION_INFORMATIONS = new CQRCodecJsonDataManager<>("entity/cqr_faction_information", EntityFactionInformation.CODEC);
+	public static final DatapackRegistry<TextureSetNew> TEXTURE_SETS = new DatapackRegistry<>(CQRepoured.prefix("cqr/texture_sets"), TextureSetNew.CODEC);
+	public static final DatapackRegistry<Faction> FACTIONS = new DatapackRegistry<>(CQRepoured.prefix("cqr/factions"), Faction.CODEC);
+	public static final DatapackRegistry<EntityFactionInformation> ENTITY_FACTION_INFORMATIONS = new DatapackRegistry<>(CQRepoured.prefix("entity/cqr_faction_information"), EntityFactionInformation.CODEC);
 	
 	public static void init() {
-		TEXTURE_SETS.subscribeAsSyncable(CQRServices.NETWORK.network(), SPacketSyncTextureSet::new);
-		FACTIONS.subscribeAsSyncable(CQRServices.NETWORK.network(), SPacketSyncFaction::new);
+		//TEXTURE_SETS.subscribeAsSyncable(CQRServices.NETWORK.network(), SPacketSyncTextureSet::new);
+		//FACTIONS.subscribeAsSyncable(CQRServices.NETWORK.network(), SPacketSyncFaction::new);
 	}
 	
 	/* Access methods */
-	public static <T> Optional<T> getValueGeneral(final CodecJsonDataManager<T> manager, final ResourceLocation id) {
-		return Optional.ofNullable(manager.getData().getOrDefault(id, null));
+	public static <T> Optional<T> getValueGeneral(final DatapackRegistry<T> manager, final ResourceLocation id, RegistryAccess registryAccess) {
+		return Optional.ofNullable(manager.get(id, registryAccess));
 	}
 	
 	/* Type specific */
-	public static Optional<TextureSetNew> getTextureSet(ResourceLocation textureSetId) {
-		return getValueGeneral(TEXTURE_SETS, textureSetId);
+	public static Optional<TextureSetNew> getTextureSet(ResourceLocation textureSetId, RegistryAccess registryAccess) {
+		return getValueGeneral(TEXTURE_SETS, textureSetId, registryAccess);
 	}
 	
-	public static Optional<TextureSetNew> getTextureSet(EntityType<?> entityType) {
-		return getTextureSet(ForgeRegistries.ENTITY_TYPES.getKey(entityType));
+	public static Optional<TextureSetNew> getTextureSet(EntityType<?> entityType, RegistryAccess registryAccess) {
+		return getTextureSet(ForgeRegistries.ENTITY_TYPES.getKey(entityType), registryAccess);
 	}
 	
-	public static Optional<Faction> getFaction(ResourceLocation factionId) {
-		return getValueGeneral(FACTIONS, factionId);
+	public static Optional<Faction> getFaction(ResourceLocation factionId, RegistryAccess registryAccess) {
+		return getValueGeneral(FACTIONS, factionId, registryAccess);
 	}
 	
 	@Nullable
-	public static EntityFactionInformation getEntityFactionInformation(EntityType<?> entityType) {
-		return ENTITY_FACTION_INFORMATIONS.getData().getOrDefault(ForgeRegistries.ENTITY_TYPES.getKey(entityType), null);
+	public static EntityFactionInformation getEntityFactionInformation(EntityType<?> entityType, RegistryAccess registryAccess) {
+		return ENTITY_FACTION_INFORMATIONS.get(ForgeRegistries.ENTITY_TYPES.getKey(entityType), registryAccess);
+	}
+
+	@SubscribeEvent
+	public static void register(DataPackRegistryEvent.NewRegistry event) {
+		TEXTURE_SETS.registerSynchable(event);
+		FACTIONS.registerSynchable(event);
+		ENTITY_FACTION_INFORMATIONS.register(event);
 	}
 	
 }
