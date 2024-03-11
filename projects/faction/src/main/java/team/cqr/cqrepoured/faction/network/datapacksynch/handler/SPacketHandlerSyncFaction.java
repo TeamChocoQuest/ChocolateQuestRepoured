41cf25f5d052ae18497a04de1c9fe5e32f7a0957
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-03-11 19:48:46 +0100

    remove some manual synch packets (shouldn't be necessary anymore)

--- projects/faction/src/main/java/team/cqr/cqrepoured/faction/network/datapacksynch/handler/SPacketHandlerSyncFaction.java
+++
@@ -1,9 +0,0 @@
-package team.cqr.cqrepoured.faction.network.datapacksynch.handler;
-
-import de.dertoaster.multihitboxlib.api.network.AbstractSPacketHandlerSyncDatapackContent;
-import team.cqr.cqrepoured.faction.Faction;
-import team.cqr.cqrepoured.faction.network.datapacksynch.packet.SPacketSyncFaction;
-
-public class SPacketHandlerSyncFaction extends AbstractSPacketHandlerSyncDatapackContent<Faction, SPacketSyncFaction> {
-
-}
--- projects/faction/src/main/java/team/cqr/cqrepoured/faction/network/datapacksynch/handler/SPacketHandlerSyncTextureSet.java
+++
@@ -1,9 +0,0 @@
-package team.cqr.cqrepoured.faction.network.datapacksynch.handler;
-
-import de.dertoaster.multihitboxlib.api.network.AbstractSPacketHandlerSyncDatapackContent;
-import team.cqr.cqrepoured.faction.network.datapacksynch.packet.SPacketSyncTextureSet;
-import team.cqr.cqrepoured.faction.textureset.TextureSetNew;
-
-public class SPacketHandlerSyncTextureSet extends AbstractSPacketHandlerSyncDatapackContent<TextureSetNew, SPacketSyncTextureSet> {
-
-}
