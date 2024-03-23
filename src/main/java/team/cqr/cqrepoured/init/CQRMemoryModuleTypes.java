cce1befa55b4e498f4bce4ab10991372ec916fc7
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-03-23 14:29:17 +0100

    add memoryModuleTypes that i needed

--- src/main/java/team/cqr/cqrepoured/init/CQRMemoryModuleTypes.java
+++ src/main/java/team/cqr/cqrepoured/init/CQRMemoryModuleTypes.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.mojang.serialization.Codec;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.ai.behavior.PositionTracker;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraftforge.registries.RegistryObject;
 import team.cqr.cqrepoured.common.services.CQRServices;
@@ -13,6 +14,9 @@ public class CQRMemoryModuleTypes {
 	
 	public static void init() {}
 
+	// Generic
+	public static final RegistryObject<MemoryModuleType<PositionTracker>> CHAT_TARGET = CQRServices.ENTITY_AI.registerMemoryModuleType("chat_target");
+	
 	// Undead
 	public static final RegistryObject<MemoryModuleType<Boolean>> BURIED = CQRServices.ENTITY_AI.registerMemoryModuleType("buried", Codec.BOOL);
 	public static final RegistryObject<MemoryModuleType<List<BlockPos>>> GRAVE_SPOTS = CQRServices.ENTITY_AI.registerMemoryModuleType("grave_spot", BlockPos.CODEC.listOf());
