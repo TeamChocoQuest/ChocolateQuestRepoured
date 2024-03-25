a8386b55a85fab0c1b29edca05dabfee431e49b5
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-03-25 18:55:08 +0100

    use Either => let's user define the inhabitant directly in the selector, could be useful

--- projects/generation/src/main/java/team/cqr/cqrepoured/generation/world/level/levelgen/structure/inhabitant/FixedInhabitantSelector.java
+++ projects/generation/src/main/java/team/cqr/cqrepoured/generation/world/level/levelgen/structure/inhabitant/FixedInhabitantSelector.java
@@ -1,5 +1,6 @@
 package team.cqr.cqrepoured.generation.world.level.levelgen.structure.inhabitant;
 
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
@@ -8,18 +9,23 @@ import net.minecraft.world.level.levelgen.structure.Structure.GenerationContext;
 import team.cqr.cqrepoured.generation.init.CQRDataPackRegistries;
 
 public record FixedInhabitantSelector(
-		DungeonInhabitant inhabitant
+		Either<DungeonInhabitant, DungeonInhabitant> inhabitant
 	) implements InhabitantSelector {
 	
 	public static final Codec<FixedInhabitantSelector> CODEC = RecordCodecBuilder.create(instance -> {
 		return instance.group(
-				CQRDataPackRegistries.INHABITANTS.byNameCodec().fieldOf("inhabitant").forGetter(FixedInhabitantSelector::inhabitant)
+				Codec.either(CQRDataPackRegistries.INHABITANTS.byNameCodec(), DungeonInhabitant.CODEC).fieldOf("inhabitant").forGetter(FixedInhabitantSelector::inhabitant)
 			).apply(instance, FixedInhabitantSelector::new);
 	});
 
 	@Override
 	public DungeonInhabitant get(GenerationContext context, BlockPos pos) {
-		return this.inhabitant;
+		if (this.inhabitant.left().isPresent()) {
+			return this.inhabitant.left().get();
+		} else if (this.inhabitant.right().isPresent()) {
+			return this.inhabitant.right().get();
+		}
+		return null;
 	}
 
 	@Override
