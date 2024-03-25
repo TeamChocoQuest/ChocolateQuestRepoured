f1e5db7d7a9f3c90b751681d808f18229c3a1206
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-03-25 19:04:22 +0100

    allow direct definition here too

--- projects/generation/src/main/java/team/cqr/cqrepoured/generation/world/level/levelgen/structure/inhabitant/DistanceInhabitantSelector.java
+++ projects/generation/src/main/java/team/cqr/cqrepoured/generation/world/level/levelgen/structure/inhabitant/DistanceInhabitantSelector.java
@@ -1,11 +1,12 @@
 package team.cqr.cqrepoured.generation.world.level.levelgen.structure.inhabitant;
 
+import java.util.List;
+
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.HolderSet;
-import net.minecraft.core.RegistryCodecs;
 import net.minecraft.world.level.levelgen.structure.Structure.GenerationContext;
 import net.minecraft.world.phys.Vec3;
 import team.cqr.cqrepoured.generation.init.CQRDataPackRegistries;
@@ -13,14 +14,14 @@ import team.cqr.cqrepoured.generation.init.CQRDataPackRegistries;
 public record DistanceInhabitantSelector(
 		double sectionRadius, 
 		boolean randomOutsideRange, 
-		HolderSet<DungeonInhabitant> inhabitants
+		List<Either<DungeonInhabitant, DungeonInhabitant>> inhabitants
 	) implements InhabitantSelector {
 
 	public static final Codec<DistanceInhabitantSelector> CODEC = RecordCodecBuilder.create(instance -> {
 		return instance.group(
 				Codec.doubleRange(1, Integer.MAX_VALUE).fieldOf("changeRadius").forGetter(DistanceInhabitantSelector::sectionRadius),
 				Codec.BOOL.optionalFieldOf("selectRandomWhenOutOfMaxRange", true).forGetter(DistanceInhabitantSelector::randomOutsideRange),
-				RegistryCodecs.homogeneousList(CQRDataPackRegistries.INHABITANTS.registryKey()).optionalFieldOf("inhabitants", HolderSet.direct()).forGetter(DistanceInhabitantSelector::inhabitants)
+				Codec.either(CQRDataPackRegistries.INHABITANTS.byNameCodec(), DungeonInhabitant.CODEC).listOf().fieldOf("inhabitants").forGetter(DistanceInhabitantSelector::inhabitants)
 			).apply(instance, DistanceInhabitantSelector::new);
 	});
 	
@@ -34,7 +35,14 @@ public record DistanceInhabitantSelector(
 		double distance = Vec3.atCenterOf(pos).length();
 		int index = (int)distance % (int)sectionRadius;
 		index = Math.min(index, inhabitants.size() -1);
-		return inhabitants.get(index).get();
+		Either<DungeonInhabitant, DungeonInhabitant> either = this.inhabitants.get(index);
+		
+		if (either.left().isPresent()) {
+			return either.left().get();
+		} else if (either.right().isPresent()) {
+			return either.right().get();
+		}
+		return null;
 	}
 
 	@Override
