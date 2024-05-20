a78afd2655ed5f5a4121de040b4e99ad8cd9a715
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-05-20 14:47:21 +0200

    fix compile errors in RepuUpdate packet

--- projects/faction/src/main/java/team/cqr/cqrepoured/faction/network/client/handler/CPacketHandlerUpdateReputation.java
+++ projects/faction/src/main/java/team/cqr/cqrepoured/faction/network/client/handler/CPacketHandlerUpdateReputation.java
@@ -1,28 +1,37 @@
 package team.cqr.cqrepoured.faction.network.client.handler;
 
+import java.util.Optional;
 import java.util.function.Supplier;
+
+import org.jetbrains.annotations.NotNull;
 
 import de.dertoaster.multihitboxlib.api.network.AbstractPacketHandler;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.network.NetworkEvent.Context;
-import team.cqr.cqrepoured.faction.Faction;
-import team.cqr.cqrepoured.faction.FactionRegistry;
+import team.cqr.cqrepoured.faction.capability.IFactionRelationCapability;
+import team.cqr.cqrepoured.faction.init.FactionCapabilities;
 import team.cqr.cqrepoured.faction.network.server.packet.SPacketUpdatePlayerReputation;
 
 public class CPacketHandlerUpdateReputation extends AbstractPacketHandler<SPacketUpdatePlayerReputation> {
 
 	@Override
 	protected void execHandlePacket(SPacketUpdatePlayerReputation message, Supplier<Context> context, Level world, Player player) {
-		FactionRegistry FAC_REG = FactionRegistry.instance(world);
-		try {
-			Faction faction = FAC_REG.getFactionInstance(message.getFaction());
-			if (faction != null) {
-				FAC_REG.setReputation(message.getPlayerId(), message.getScore(), faction);
-			}
-		} catch (Exception ex) {
-			// IGNORE
-			ex.printStackTrace();
+		if (player == null) 
+			return;
+		
+		// First: Grab capability for relations
+		@NotNull LazyOptional<IFactionRelationCapability> lOpCap = player.getCapability(FactionCapabilities.FACTION_RELATION);
+		if (lOpCap.isPresent()) {
+			Optional<IFactionRelationCapability> opCap = lOpCap.resolve();
+			if (opCap.isEmpty())
+				return;
+			
+			// We do have data!
+			opCap.ifPresent(cap -> {
+				cap.setReputationTowards(message.getFaction(), message.getScore());
+			});
 		}
 	}
 
