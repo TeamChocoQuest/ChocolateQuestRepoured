92051906778896f5f487bb95e23afee75ffd9798
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-05-20 15:37:35 +0200

    allow to pass just the bounding box

--- projects/protection/src/main/java/team/cqr/cqrepoured/protection/ProtectionIndicatorHelper.java
+++ projects/protection/src/main/java/team/cqr/cqrepoured/protection/ProtectionIndicatorHelper.java
@@ -1,19 +1,28 @@
 package team.cqr.cqrepoured.protection;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.HitResult;
-import team.cqr.cqrepoured.CQRMain;
-import team.cqr.cqrepoured.protection.network.server.packet.SPacketAddOrResetProtectedRegionIndicator;
+import java.util.UUID;
 
 import javax.annotation.Nullable;
-import java.util.UUID;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.phys.HitResult;
+import team.cqr.cqrepoured.common.services.CQRServices;
+import team.cqr.cqrepoured.protection.network.server.packet.SPacketAddOrResetProtectedRegionIndicator;
 
 public class ProtectionIndicatorHelper {
 
+	public static void addOrResetProtectedRegionIndicator(Level world, UUID uuid, BoundingBox boundingBox, BlockPos pos, @Nullable ServerPlayer player) {
+		BlockPos p1 = new BlockPos(boundingBox.minX(), boundingBox.minY(), boundingBox.minZ());
+		BlockPos p2 = new BlockPos(boundingBox.maxX(), boundingBox.maxY(), boundingBox.maxZ());
+		
+		addOrResetProtectedRegionIndicator(world, uuid, p1, p2, pos, player);
+	}
+	
 	public static void addOrResetProtectedRegionIndicator(Level world, UUID uuid, BlockPos start, BlockPos end, BlockPos pos, @Nullable ServerPlayer player) {
 		if (world.isClientSide) {
 			HitResult result = Minecraft.getInstance().hitResult; //Old: objectMouseOver
@@ -21,7 +30,7 @@ public class ProtectionIndicatorHelper {
 				world.addParticle(ParticleTypes.HAPPY_VILLAGER, result.getLocation().x, result.getLocation().y, result.getLocation().z, 0.0D, 0.0D, 0.0D);
 			}
 		} else if (player != null) {
-			CQRMain.NETWORK.sendToServer(new SPacketAddOrResetProtectedRegionIndicator(uuid, start, end, pos)); //Correct?
+			CQRServices.NETWORK.sendToServer(new SPacketAddOrResetProtectedRegionIndicator(uuid, start, end, pos)); //Correct?
 		}
 	}
 
