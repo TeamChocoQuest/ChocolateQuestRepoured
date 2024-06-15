f5171a1e634308660c85e7b2b3bcc9db755355fc
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-06-15 21:12:00 +0200

    improve extraData handling and use SimpleWeightedList

--- projects/common/src/main/java/team/cqr/cqrepoured/common/entity/profile/variant/EntityVariant.java
+++ projects/common/src/main/java/team/cqr/cqrepoured/common/entity/profile/variant/EntityVariant.java
@@ -1,8 +1,9 @@
 package team.cqr.cqrepoured.common.entity.profile.variant;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.Random;
 
 import javax.annotation.Nullable;
 
@@ -12,9 +13,10 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import de.dertoaster.multihitboxlib.entity.hitbox.HitboxProfile;
 import de.dertoaster.multihitboxlib.util.LazyLoadField;
 import net.minecraft.util.RandomSource;
+import net.minecraft.util.random.SimpleWeightedRandomList;
+import net.minecraftforge.registries.RegistryObject;
 import team.cqr.cqrepoured.common.datapack.EntityProfileDatapackRegistries;
 import team.cqr.cqrepoured.common.entity.profile.variant.extradata.IVariantExtraData;
-import team.cqr.cqrepoured.common.random.CQRWeightedRandom;
 
 public class EntityVariant {
 	protected final int weight;
@@ -24,9 +26,10 @@ public class EntityVariant {
 	protected final List<AssetEntry> assets;
 	protected final Optional<HitboxProfile> hitboxConfig;
 	// TODO: Change extra data to be a Map<ResourceLocation, Object> instead
-	protected final Optional<IVariantExtraData<?>> optionalExtraData;
+	protected final List<IVariantExtraData<?>> extraDataList;
+	protected final Map<Codec<? extends IVariantExtraData<?>>, Object> extraDataMap = new HashMap<>();
 	
-	private final LazyLoadField<CQRWeightedRandom<AssetEntry>> assetWeightedList = new LazyLoadField<>(this::generateWeightedAssetList);
+	private final LazyLoadField<SimpleWeightedRandomList<AssetEntry>> assetWeightedList = new LazyLoadField<>(this::generateWeightedAssetList);
 	
 	public static final Codec<EntityVariant> CODEC = RecordCodecBuilder.create(instance -> {
 		return instance.group(
@@ -36,51 +39,63 @@ public class EntityVariant {
 				AttributeEntry.CODEC.listOf().fieldOf("attributes").forGetter(EntityVariant::attributes),
 				AssetEntry.CODEC.listOf().fieldOf("assets").forGetter(ev -> {return ev.assets;}),
 				HitboxProfile.CODEC.optionalFieldOf("hitbox").forGetter(ev -> {return ev.hitboxConfig;}),
-				EntityProfileDatapackRegistries.VARIANT_EXTRA_DATA_DISPATCHER.dispatchedCodec().optionalFieldOf("extra-data").forGetter(EntityVariant::extraData)
+				EntityProfileDatapackRegistries.VARIANT_EXTRA_DATA_DISPATCHER.dispatchedCodec().listOf().fieldOf("extra-data").forGetter(EntityVariant::extraDataList)
 				
 			).apply(instance, EntityVariant::new);
 	});
 	
-	public EntityVariant(int weight, SizeEntry size, DamageEntry damageConfig, List<AttributeEntry> attributes, List<AssetEntry> assets, Optional<HitboxProfile> hbProfile, Optional<IVariantExtraData<?>> extraData) {
+	public EntityVariant(int weight, SizeEntry size, DamageEntry damageConfig, List<AttributeEntry> attributes, List<AssetEntry> assets, Optional<HitboxProfile> hbProfile, List<IVariantExtraData<?>> extraData) {
 		this.weight = weight;
 		this.size = size;
 		this.damageConfig = damageConfig;
 		this.attributes = attributes;
 		this.assets = assets;
 		this.hitboxConfig = hbProfile;
-		this.optionalExtraData = extraData;
+		this.extraDataList = extraData;
+		
+		this.extraDataList.forEach( ed -> {
+				this.extraDataMap.put(ed.getType(), ed.getExtraData());
+			}
+		);
 	}
 	
 	public Optional<HitboxProfile> getOptHitboxProfile() {
 		return this.hitboxConfig;
 	}
 	
-	private final CQRWeightedRandom<AssetEntry> generateWeightedAssetList() {
-		CQRWeightedRandom<AssetEntry> result = new CQRWeightedRandom<>();
+	private final SimpleWeightedRandomList<AssetEntry> generateWeightedAssetList() {
+		SimpleWeightedRandomList.Builder<AssetEntry> builder = new SimpleWeightedRandomList.Builder<>();
 		for(AssetEntry variant : this.assets) {
-			result.add(variant, variant.weight());
+			builder.add(variant, variant.weight());
+		}
+		
+		return builder.build();
+	}
+	
+	@Nullable
+	public AssetEntry getRandomAssets(final RandomSource random) {
+		return this.assetWeightedList.get().getRandomValue(random).get();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> T getExtraData(final RegistryObject<Codec<? extends IVariantExtraData<? extends T>>> key) {
+		T result = null;
+		final Codec<? extends IVariantExtraData<? extends T>> actualKey = key.get();
+		try {
+			Object object = this.extraDataMap.getOrDefault(actualKey, null);
+			if (object != null) {
+				result = (T) object;
+			}
+		} catch(ClassCastException ex) {
+			this.extraDataMap.remove(actualKey);
+			return null;
 		}
 		
 		return result;
 	}
 	
-	@Nullable
-	public AssetEntry getRandomAssets() {
-		return this.assetWeightedList.get().next();
-	}
-	
-	@Nullable
-	public AssetEntry getRandomAssets(final Random random) {
-		return this.assetWeightedList.get().next(random);
-	}
-	
-	@Nullable
-	public AssetEntry getRandomAssets(final RandomSource random) {
-		return this.assetWeightedList.get().next(random);
-	}
-	
-	public Optional<IVariantExtraData<?>> extraData() {
-		return this.optionalExtraData;
+	public List<IVariantExtraData<?>> extraDataList() {
+		return this.extraDataList;
 	}
 	
 	public int getRandomAssetIndex(final RandomSource random) {
