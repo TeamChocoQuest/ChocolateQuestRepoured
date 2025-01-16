52d5df5f9040b1278813330a6087fb3f6b3198a7
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-01-16 16:23:04 +0100

    Lower magic bell sound volume

--- src/main/java/team/cqr/cqrepoured/item/ItemMagicBell.java
+++ src/main/java/team/cqr/cqrepoured/item/ItemMagicBell.java
@@ -67,7 +67,7 @@ public class ItemMagicBell extends ItemLore {
 				}
 			});
 
-			worldIn.playSound(null, entityLiving.posX, entityLiving.posY, entityLiving.posZ, CQRSounds.BELL_USE, entityLiving.getSoundCategory(), 1.0F, 1.0F);
+			worldIn.playSound(null, entityLiving.posX, entityLiving.posY, entityLiving.posZ, CQRSounds.BELL_USE, entityLiving.getSoundCategory(), 0.5F, 1.0F);
 			if (entityLiving instanceof EntityPlayer) {
 				if (protectedRegions.isEmpty()) {
 					((EntityPlayer) entityLiving).getCooldownTracker().setCooldown(stack.getItem(), 60);
