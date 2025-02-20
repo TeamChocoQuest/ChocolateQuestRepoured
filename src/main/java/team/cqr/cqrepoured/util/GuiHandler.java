74b51a556eb82427f5a87fbf95ebdff634d838c3
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-02-20 03:10:04 +0100

    Use incrementing id for GUIs

--- src/main/java/team/cqr/cqrepoured/util/GuiHandler.java
+++ src/main/java/team/cqr/cqrepoured/util/GuiHandler.java
@@ -36,20 +36,20 @@ import team.cqr.cqrepoured.tileentity.TileEntitySpawner;
 
 public class GuiHandler implements IGuiHandler {
 
-	public static final int EXPORTER_GUI_ID = 1;
-	public static final int SPAWNER_GUI_ID = 2;
-	public static final int BADGE_GUI_ID = 3;
-	public static final int BACKPACK_GUI_ID = 4;
-	public static final int ALCHEMY_BAG_GUI_ID = 5;
-	public static final int CQR_ENTITY_GUI_ID = 6;
-	public static final int REPUTATION_GUI_ID = 7;
-	public static final int MERCHANT_GUI_ID = 8;
-	public static final int MERCHANT_EDIT_TRADE_GUI_ID = 9;
-	public static final int EXPORTER_CHEST_GUI_ID = 10;
-	public static final int BOSS_BLOCK_GUI_ID = 11;
-	public static final int MAP_GUI_ID = 12;
-	public static final int MAP_GUI_SIMPLE_ID = 13;
-	public static final int ADD_PATH_NODE_GUI_ID = 14;
+	private static int guiID = 1;
+	public static final int EXPORTER_GUI_ID = guiID++;
+	public static final int SPAWNER_GUI_ID = guiID++;
+	public static final int BADGE_GUI_ID = guiID++;
+	public static final int BACKPACK_GUI_ID = guiID++;
+	public static final int ALCHEMY_BAG_GUI_ID = guiID++;
+	public static final int CQR_ENTITY_GUI_ID = guiID++;
+	public static final int MERCHANT_GUI_ID = guiID++;
+	public static final int MERCHANT_EDIT_TRADE_GUI_ID = guiID++;
+	public static final int EXPORTER_CHEST_GUI_ID = guiID++;
+	public static final int BOSS_BLOCK_GUI_ID = guiID++;
+	public static final int MAP_GUI_ID = guiID++;
+	public static final int MAP_GUI_SIMPLE_ID = guiID++;
+	public static final int ADD_PATH_NODE_GUI_ID = guiID++;
 
 	// SERVER
 	@Override
