105e7ce2694b4a662265e5691dd07426f2b26b8c
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-03-02 11:52:58 +0100

    Fix matrix util IndexOutOfBoundsException

--- src/main/java/team/cqr/cqrepoured/client/util/MatrixUtil.java
+++ src/main/java/team/cqr/cqrepoured/client/util/MatrixUtil.java
@@ -20,41 +20,41 @@ public class MatrixUtil {
 
 	public static Matrix4f loadMatrixFromBuffer(Matrix4f matrix) {
 		matrix.m00 = FLOAT_BUFFER.get(0);
-		matrix.m10 = FLOAT_BUFFER.get(4);
-		matrix.m20 = FLOAT_BUFFER.get(8);
-		matrix.m30 = FLOAT_BUFFER.get(12);
-		matrix.m01 = FLOAT_BUFFER.get(16);
-		matrix.m11 = FLOAT_BUFFER.get(20);
-		matrix.m21 = FLOAT_BUFFER.get(24);
-		matrix.m31 = FLOAT_BUFFER.get(28);
-		matrix.m02 = FLOAT_BUFFER.get(32);
-		matrix.m12 = FLOAT_BUFFER.get(36);
-		matrix.m22 = FLOAT_BUFFER.get(40);
-		matrix.m32 = FLOAT_BUFFER.get(44);
-		matrix.m03 = FLOAT_BUFFER.get(48);
-		matrix.m13 = FLOAT_BUFFER.get(52);
-		matrix.m23 = FLOAT_BUFFER.get(56);
-		matrix.m33 = FLOAT_BUFFER.get(60);
+		matrix.m10 = FLOAT_BUFFER.get(1);
+		matrix.m20 = FLOAT_BUFFER.get(2);
+		matrix.m30 = FLOAT_BUFFER.get(3);
+		matrix.m01 = FLOAT_BUFFER.get(4);
+		matrix.m11 = FLOAT_BUFFER.get(5);
+		matrix.m21 = FLOAT_BUFFER.get(6);
+		matrix.m31 = FLOAT_BUFFER.get(7);
+		matrix.m02 = FLOAT_BUFFER.get(8);
+		matrix.m12 = FLOAT_BUFFER.get(9);
+		matrix.m22 = FLOAT_BUFFER.get(10);
+		matrix.m32 = FLOAT_BUFFER.get(11);
+		matrix.m03 = FLOAT_BUFFER.get(12);
+		matrix.m13 = FLOAT_BUFFER.get(13);
+		matrix.m23 = FLOAT_BUFFER.get(14);
+		matrix.m33 = FLOAT_BUFFER.get(15);
 		return matrix;
 	}
 
 	public static void storeMatrixInBuffer(Matrix4f matrix) {
 		FLOAT_BUFFER.put(0, matrix.m00);
-		FLOAT_BUFFER.put(4, matrix.m10);
-		FLOAT_BUFFER.put(8, matrix.m20);
-		FLOAT_BUFFER.put(12, matrix.m30);
-		FLOAT_BUFFER.put(16, matrix.m01);
-		FLOAT_BUFFER.put(20, matrix.m11);
-		FLOAT_BUFFER.put(24, matrix.m21);
-		FLOAT_BUFFER.put(28, matrix.m31);
-		FLOAT_BUFFER.put(32, matrix.m02);
-		FLOAT_BUFFER.put(36, matrix.m12);
-		FLOAT_BUFFER.put(40, matrix.m22);
-		FLOAT_BUFFER.put(44, matrix.m32);
-		FLOAT_BUFFER.put(48, matrix.m03);
-		FLOAT_BUFFER.put(52, matrix.m13);
-		FLOAT_BUFFER.put(56, matrix.m23);
-		FLOAT_BUFFER.put(60, matrix.m33);
+		FLOAT_BUFFER.put(1, matrix.m10);
+		FLOAT_BUFFER.put(2, matrix.m20);
+		FLOAT_BUFFER.put(3, matrix.m30);
+		FLOAT_BUFFER.put(4, matrix.m01);
+		FLOAT_BUFFER.put(5, matrix.m11);
+		FLOAT_BUFFER.put(6, matrix.m21);
+		FLOAT_BUFFER.put(7, matrix.m31);
+		FLOAT_BUFFER.put(8, matrix.m02);
+		FLOAT_BUFFER.put(9, matrix.m12);
+		FLOAT_BUFFER.put(10, matrix.m22);
+		FLOAT_BUFFER.put(11, matrix.m32);
+		FLOAT_BUFFER.put(12, matrix.m03);
+		FLOAT_BUFFER.put(13, matrix.m13);
+		FLOAT_BUFFER.put(14, matrix.m23);
+		FLOAT_BUFFER.put(15, matrix.m33);
 	}
 
 }
