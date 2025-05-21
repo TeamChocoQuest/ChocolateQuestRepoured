95084da641f191776f2c0a3eb63b2198bcb8c93d
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-05-21 21:16:05 +0200

    Rewrite loot table loader
    - Fix `.properties` loot tables being loaded when loading `.json` loot table fails
    - Prevent crash when loading `.json` loot table with invalid entries

--- src/main/java/team/cqr/cqrepoured/world/structure/generation/lootchests/LootTableLoader.java
+++ src/main/java/team/cqr/cqrepoured/world/structure/generation/lootchests/LootTableLoader.java
@@ -2,11 +2,13 @@ package team.cqr.cqrepoured.world.structure.generation.lootchests;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Deque;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -15,8 +17,12 @@ import java.util.stream.Stream;
 
 import org.apache.commons.io.FileUtils;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
+import meldexun.reflectionutil.ReflectionConstructor;
 import meldexun.reflectionutil.ReflectionField;
 import net.minecraft.item.Item;
 import net.minecraft.launchwrapper.Launch;
@@ -44,71 +50,82 @@ import team.cqr.cqrepoured.init.CQRLoottables;
 /**
  * Copyright (c) 29.04.2019 Developed by DerToaster98 GitHub: https://github.com/DerToaster98
  */
+@SuppressWarnings("unchecked")
 public class LootTableLoader {
 
-	private static final ReflectionField<Boolean> LootTable_isFrozen = new ReflectionField<>(LootTable.class, "isFrozen", null);
-	private static final ReflectionField<Boolean> LootPool_isFrozen = new ReflectionField<>(LootPool.class, "isFrozen", null);
-	// ATs don't work in dev for some reason
-	private static final ReflectionField<List<LootPool>> LootTable_pools = new ReflectionField<>(LootTable.class, "field_186466_c", "pools");
-	private static final ReflectionField<List<LootEntry>> LootPool_lootEntries = new ReflectionField<>(LootPool.class, "field_186453_a", "lootEntries");
+	private static final ReflectionField<List<JsonElement>> JsonArray_elements = new ReflectionField<>(JsonArray.class, "elements", null);
+	private static final ThreadLocal<Deque<?>> lootContext = new ReflectionField<ThreadLocal<Deque<?>>>(ForgeHooks.class, "lootContext", null).get(null);
+	private static final ReflectionConstructor<?> LootTableContext = new ReflectionConstructor<>("net.minecraftforge.common.ForgeHooks$LootTableContext", ResourceLocation.class, boolean.class);
 
 	public static void loadLootTableFromConfig(LootTableLoadEvent event) {
-		LootTable lootTable = loadJsonLootTable(event.getName(), event.getLootTableManager());
-		if (lootTable == null) {
-			lootTable = loadPropertiesLootTable(event.getName());
-		}
-
-		if (lootTable != null) {
-			// remove invalid entries and empty pools
-			LootTable_pools.get(lootTable).removeIf(pool -> {
-				LootPool_lootEntries.get(pool).removeIf(entry -> entry instanceof LootEntryItem && ((LootEntryItem) entry).item == null);
-				return LootPool_lootEntries.get(pool).isEmpty();
-			});
-
-			event.setTable(lootTable);
+		if (Files.exists(jsonFile(event.getName()))) {
+			loadJsonLootTable(event);
+		} else if (Files.exists(propertiesFile(event.getName()))) {
+			loadPropertiesLootTable(event);
 		}
 	}
 
-	private static LootTable loadJsonLootTable(ResourceLocation name, LootTableManager lootTableManager) {
-		Path jsonFile = new File(CQRMain.CQ_CHEST_FOLDER, name.getPath() + ".json").toPath();
-		if (!Files.exists(jsonFile)) {
-			return null;
-		}
+	private static Path jsonFile(ResourceLocation name) {
+		return new File(CQRMain.CQ_CHEST_FOLDER, name.getPath() + ".json").toPath();
+	}
+
+	private static Path propertiesFile(ResourceLocation name) {
+		return new File(CQRMain.CQ_CHEST_FOLDER, name.getPath() + ".properties").toPath();
+	}
+
+	@SuppressWarnings("rawtypes")
+	private static void loadJsonLootTable(LootTableLoadEvent event) {
+		Path jsonFile = jsonFile(event.getName());
 
 		try {
-			String data = new String(Files.readAllBytes(jsonFile), StandardCharsets.UTF_8);
-			LootTable lootTable = ForgeHooks.loadLootTable(LootTableManager.GSON_INSTANCE, name, data, true, lootTableManager);
-
-			// unfreeze to fix crash when another mod wants to modify the loot table
-			if (lootTable != null) {
-				LootTable_isFrozen.setBoolean(lootTable, false);
-				LootTable_pools.get(lootTable).forEach(pool -> LootPool_isFrozen.setBoolean(pool, false));
+			JsonObject json;
+			try (Reader reader = Files.newBufferedReader(jsonFile)) {
+				json = LootTableManager.GSON_INSTANCE.fromJson(reader, JsonObject.class);
 			}
 
-			return lootTable;
+			// remove invalid entries and empty pools
+			if (json.has("pools")) {
+				JsonArray_elements.get(json.get("pools")).removeIf(pool -> {
+					if (!pool.getAsJsonObject().has("entries")) {
+						return true;
+					}
+					JsonArray entries = pool.getAsJsonObject().getAsJsonArray("entries");
+					JsonArray_elements.get(entries).removeIf(entry -> {
+						return entry.getAsJsonObject().get("type").getAsString().equals("item") && Item.getByNameOrId(entry.getAsJsonObject().get("name").getAsString()) == null;
+					});
+					return entries.size() == 0;
+				});
+			}
+
+			Deque que = lootContext.get();
+			if (que == null) {
+				lootContext.set(que = new ArrayDeque<>());
+			}
+
+			que.push(LootTableContext.newInstance(event.getName(), true));
+			try {
+				event.setTable(LootTableManager.GSON_INSTANCE.fromJson(json, LootTable.class));
+			} finally {
+				que.pop();
+			}
 		} catch (IOException | JsonParseException e) {
 			CQRMain.logger.error("Failed to read json loot table {}", Launch.minecraftHome.toPath().relativize(jsonFile), e);
-			return null;
 		}
 	}
 
-	private static LootTable loadPropertiesLootTable(ResourceLocation name) {
-		Path propertiesFile = new File(CQRMain.CQ_CHEST_FOLDER, name.getPath() + ".properties").toPath();
-		if (!Files.exists(propertiesFile)) {
-			return null;
-		}
+	private static void loadPropertiesLootTable(LootTableLoadEvent event) {
+		Path propertiesFile = propertiesFile(event.getName());
 
 		try {
 			LootPool[] pools;
 			if (CQRConfig.general.singleLootPoolPerLootTable) {
-				pools = new LootPool[] { new LootPool(LootTableLoader.parseLootEntries(propertiesFile).toArray(LootEntry[]::new), new LootCondition[0], new RandomValueRange(CQRConfig.general.minItemsPerLootChest, CQRConfig.general.maxItemsPerLootChest), new RandomValueRange(0), name.getPath()) };
+				pools = new LootPool[] { new LootPool(LootTableLoader.parseLootEntries(propertiesFile).toArray(LootEntry[]::new), new LootCondition[0], new RandomValueRange(CQRConfig.general.minItemsPerLootChest, CQRConfig.general.maxItemsPerLootChest), new RandomValueRange(0), event.getName().getPath()) };
 			} else {
 				pools = LootTableLoader.parseLootEntries(propertiesFile).map(entry -> new LootPool(new LootEntry[] { entry }, new LootCondition[0], new RandomValueRange(1), new RandomValueRange(0), entry.getEntryName())).toArray(LootPool[]::new);
 			}
-			return new LootTable(pools);
+			event.setTable(new LootTable(pools));
 		} catch (IOException e) {
 			CQRMain.logger.error("Failed to read prop loot table {}", Launch.minecraftHome.toPath().relativize(propertiesFile), e);
-			return null;
 		}
 	}
 
@@ -130,6 +147,9 @@ public class LootTableLoader {
 		String name = s.substring(0, i).trim();
 		StringTokenizer tokenizer = new StringTokenizer(s.substring(i + 1), ",");
 		Item item = Item.getByNameOrId(tokenizer.nextToken().trim());
+		if (item == null) {
+			return null;
+		}
 		int meta = parseInt(tokenizer, 0);
 		int countMin = parseInt(tokenizer, 1);
 		int countMax = parseInt(tokenizer, 1);
