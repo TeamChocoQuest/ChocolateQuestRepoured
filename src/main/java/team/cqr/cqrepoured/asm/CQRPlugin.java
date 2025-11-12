ff8ef9ff5942cd81eb2bab079de042db62e73428
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-11-13 00:30:16 +0100

    Fix log spam in dev

--- src/main/java/team/cqr/cqrepoured/asm/CQRPlugin.java
+++ src/main/java/team/cqr/cqrepoured/asm/CQRPlugin.java
@@ -54,6 +54,11 @@ public class CQRPlugin implements IFMLLoadingPlugin {
 		if (Boolean.FALSE.equals(data.get("runtimeDeobfuscationEnabled"))) {
 			MixinBootstrap.init();
 			MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+			CoreModManager.getIgnoredMods().add("mixin-0.8.5.jar");
+			CoreModManager.getIgnoredMods().add("asm-util-6.2.jar");
+			CoreModManager.getIgnoredMods().add("asm-analysis-6.2.jar");
+			CoreModManager.getIgnoredMods().add("asm-tree-6.2.jar");
+			CoreModManager.getIgnoredMods().add("asm-6.2.jar");
 		}
 	}
 
