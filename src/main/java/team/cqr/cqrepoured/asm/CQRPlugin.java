4ac8101ffad1954bcffdf454420306d29fd2cc7f
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-06-12 17:31:53 +0800

    remove CQR Tweaker & ClassTransformer

--- src/main/java/team/cqr/cqrepoured/asm/CQRPlugin.java
+++ src/main/java/team/cqr/cqrepoured/asm/CQRPlugin.java
@@ -1,37 +1,18 @@
 package team.cqr.cqrepoured.asm;
 
-import java.lang.reflect.Field;
-import java.util.List;
 import java.util.Map;
 
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 
-import net.minecraft.launchwrapper.ITweaker;
-import net.minecraft.launchwrapper.Launch;
-import net.minecraftforge.fml.common.launcher.FMLInjectionAndSortingTweaker;
 import net.minecraftforge.fml.relauncher.CoreModManager;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import team.cqr.cqrepoured.asm.tweaker.CQRTweaker;
 
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 @IFMLLoadingPlugin.TransformerExclusions({ "team.cqr.cqrepoured.asm", "meldexun.asmutil2" })
 public class CQRPlugin implements IFMLLoadingPlugin {
 
-	@SuppressWarnings("unchecked")
 	public CQRPlugin() {
-		try {
-			if (((List<ITweaker>) Launch.blackboard.get("Tweaks")).stream().noneMatch(FMLInjectionAndSortingTweaker.class::isInstance)) {
-				((List<String>) Launch.blackboard.get("TweakClasses")).add(CQRTweaker.class.getName());
-			} else {
-				((List<ITweaker>) Launch.blackboard.get("Tweaks")).add(new CQRTweaker());
-			}
-			Field _tweakSorting = CoreModManager.class.getDeclaredField("tweakSorting");
-			_tweakSorting.setAccessible(true);
-			((Map<String, Integer>) _tweakSorting.get(null)).put(CQRTweaker.class.getName(), 1001);
-		} catch (ReflectiveOperationException e) {
-			throw new UnsupportedOperationException(e);
-		}
 	}
 
 	@Override
